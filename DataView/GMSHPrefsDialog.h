ae4cc246bc7f40b06d79de0c00331431f9a18bc6
Karsten Rink <karsten.rink@ufz.de>
2011-01-24 16:22:59 +0100

    gmsh-file delete option added

--- DataView/GMSHPrefsDialog.h
+++ DataView/GMSHPrefsDialog.h
@@ -47,7 +47,7 @@ private slots:
 	void reject();
 
 signals:
-	void requestMeshing(std::vector<std::string> const &, size_t, double, double, double);
+	void requestMeshing(std::vector<std::string> const &, size_t, double, double, double, bool);
 
 };
 
