10e201d961537c84aefb3922dd3b68838daf77ab
Thomas Fischer <thomas.fischer@ufz.de>
2011-10-11 15:16:29 +0200

    made time measurement in windows working

--- Base/RunTimeTimer.cpp
+++ Base/RunTimeTimer.cpp
@@ -2,16 +2,27 @@
 
 void RunTimeTimer::start()
 {
+#ifndef _WIN32
 	gettimeofday(&_start, 0);
+#else
+	_start = timeGetTime();
+#endif
 }
 
 void RunTimeTimer::stop()
 {
+#ifndef _WIN32
 	gettimeofday(&_stop, 0);
+#else
+	_stop = timeGetTime();
+#endif
 }
 
 double RunTimeTimer::elapsed()
 {
+#ifndef _WIN32
 	return (_stop.tv_sec + _stop.tv_usec/1000000.0 - (_start.tv_sec + _start.tv_usec/1000000.0));
+#else
+	return (_stop - _start) / 1000;
+#endif
 }
-
--- Base/RunTimeTimer.h
+++ Base/RunTimeTimer.h
@@ -1,21 +1,31 @@
 #ifndef RUNTIMETIMER_H
 #define RUNTIMETIMER_H
 
+#include "TimeMeasurementBase.h"
+
+#ifndef _WIN32
 #include <sys/time.h>
+#else
+#include <windows.h>
+#endif
 
 #include "TimeMeasurementBase.h"
 
-class RunTimeTimer
+class RunTimeTimer : public TimeMeasurementBase
 {
 public:
-        virtual void start();
-        virtual void stop();
-        virtual double elapsed();
+	virtual void start();
+	virtual void stop();
+	virtual double elapsed();
 	~RunTimeTimer() {};
 private:
+#ifndef _WIN32
 	timeval _start;
 	timeval _stop;
+#else
+	unsigned long _start;
+	unsigned long _stop;
+#endif
 };
 
 #endif
-
