940476323839f87e117ad06e15e881fbce2084ef
Thomas Fischer <thomas.fischer@ufz.de>
2011-11-10 07:45:34 +0100

    made amuxCRS a template function with two template parameters - one for the floating point number type, the second for the index type

--- MathLib/LinAlg/Sparse/amuxCRS.h
+++ MathLib/LinAlg/Sparse/amuxCRS.h
@@ -3,9 +3,19 @@
 
 namespace MathLib {
 
-void amuxCRS (double a,
-	unsigned n, unsigned const * const iA, unsigned const * const jA,
-        double const * const A, double const * const x, double* y);
+template<typename FP_TYPE, typename IDX_TYPE>
+void amuxCRS(FP_TYPE a, IDX_TYPE n, IDX_TYPE const * const iA, IDX_TYPE const * const jA,
+				FP_TYPE const * const A, FP_TYPE const * const x, FP_TYPE* y)
+{
+	for (IDX_TYPE i(0); i < n; i++) {
+		y[i] = 0.0;
+		const IDX_TYPE end(iA[i + 1]);
+		for (IDX_TYPE j(iA[i]); j < end; j++) {
+			y[i] += A[j] * x[jA[j]];
+		}
+		y[i] *= a;
+	}
+}
 
 void amuxCRSParallelPThreads (double a,
 	unsigned n, unsigned const * const iA, unsigned const * const jA,
