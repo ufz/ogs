3d1348ea53aa253a700cf6da7d0bcfc8cce5b6eb
Thomas Fischer <thomas.fischer@ufz.de>
2011-12-05 13:56:18 +0100

    made amuxCRSParallelOpenMP a function template

--- MathLib/LinAlg/Sparse/amuxCRS.h
+++ MathLib/LinAlg/Sparse/amuxCRS.h
@@ -23,10 +23,25 @@ void amuxCRSParallelPThreads (double a,
 	unsigned num_of_pthreads);
 
 #ifdef _OPENMP
-void amuxCRSParallelOpenMP (double a,
-	unsigned n, unsigned const * const iA, unsigned const * const jA,
-        double const * const A, double const * const x, double* y,
-	unsigned num_of_omp_threads);
+template<typename FP_TYPE, typename IDX_TYPE>
+void amuxCRSParallelOpenMP (FP_TYPE a,
+				IDX_TYPE n, IDX_TYPE const * const iA, IDX_TYPE const * const jA,
+				FP_TYPE const * const A, FP_TYPE const * const x, FP_TYPE* y,
+				unsigned num_of_omp_threads)
+{
+	unsigned i;
+	{
+#pragma omp parallel for
+		for (i = 0; i < n; i++) {
+			FP_TYPE temp (0.0);
+			const IDX_TYPE end(iA[i + 1]);
+			for (IDX_TYPE j(iA[i]); j < end; j++) {
+				temp += A[j] * x[jA[j]];
+			}
+			y[i] = a*temp;
+		}
+	}
+}
 #endif
 
 void amuxCRSSym (double a,
