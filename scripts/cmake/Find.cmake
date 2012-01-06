b250b7a3e0c9385d3f73e6b496d4f6cf7886f22c
Lars Bilke <lars.bilke@ufz.de>
2012-01-06 15:01:16 +0100

    Introduced Find cmake script and enabled OGS_PROFILE option.

---
+++ scripts/cmake/Find.cmake
@@ -0,0 +1,26 @@
+######################
+### Find tools     ###
+######################
+
+# Find dot tool from graphviz
+FIND_PROGRAM(DOT_TOOL_PATH dot DOC "Dot tool from graphviz")
+
+# Find doxygen
+FIND_PACKAGE(Doxygen)
+
+# Find gnu profiler gprof
+FIND_PROGRAM(GPROF_PATH gprof DOC "GNU profiler gprof")
+
+FIND_PACKAGE(cppcheck)
+
+
+######################
+### Find libraries ###
+######################
+
+FIND_PACKAGE(OpenMP)
+IF(OPENMP_FOUND)
+	SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+	SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+ENDIF()
+
