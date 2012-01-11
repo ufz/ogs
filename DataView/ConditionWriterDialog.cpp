cab54c125589b78e24b0615bb0d950808e4e8463
Karsten Rink <karsten.rink@ufz.de>
2012-01-11 18:13:23 +0100

    added missing dialog files

---
+++ DataView/ConditionWriterDialog.cpp
@@ -0,0 +1,95 @@
+/**
+ * \file ConditionWriterDialog.cpp
+ * 2012/01/11 KR Initial implementation
+ */
+
+#include "ConditionWriterDialog.h"
+#include "FEMCondition.h"
+#include "OGSError.h"
+
+#include <QFileDialog>
+#include <QFileInfo>
+#include <QSettings>
+
+ConditionWriterDialog::ConditionWriterDialog(const GEOLIB::GEOObjects *geo_objects, QDialog* parent)
+	: QDialog(parent)
+{
+	setupUi(this);
+
+	std::vector<std::string> geo_names;
+	geo_objects->getGeometryNames(geo_names);
+
+	for (size_t i=0; i<geo_names.size(); i++)
+		this->geoBox->addItem(QString::fromStdString(geo_names[i]));
+
+}
+
+ConditionWriterDialog::~ConditionWriterDialog()
+{
+}
+
+void ConditionWriterDialog::on_fileNameButton_pressed()
+{
+	QString filetypes("");
+	int geo_idx = this->geoBox->currentIndex();
+	int cnd_idx = this->condTypeBox->currentIndex();
+	if ((geo_idx == 0) || (cnd_idx == 0)) filetypes = "OpenGeoSys FEM Condition file (*.cnd)";
+	else if ((geo_idx != 0) && (cnd_idx == 1)) filetypes = "OpenGeoSys FEM Condition file (*.cnd);;GeoSys Boundary Condition (*.bc)";
+	else if ((geo_idx != 0) && (cnd_idx == 2)) filetypes = "OpenGeoSys FEM Condition file (*.cnd);;GeoSys Initial Condition (*.ic)";
+	else if ((geo_idx != 0) && (cnd_idx == 3)) filetypes = "OpenGeoSys FEM Condition file (*.cnd);;GeoSys Source Condition (*.st)";
+
+	QSettings settings("UFZ", "OpenGeoSys-5");
+	QString fileName = QFileDialog::getSaveFileName(this, "Select path",
+					settings.value("lastOpenedFileDirectory").toString(), filetypes);
+
+	if (!fileName.isEmpty())
+	{
+		this->fileNameEdit->setText(fileName);
+
+		QDir dir = QDir(fileName);
+		settings.setValue("lastOpenedFileDirectory", dir.absolutePath());
+	}
+}
+
+
+void ConditionWriterDialog::accept()
+{
+	const QString file_name = this->fileNameEdit->text();
+	QFileInfo fi(file_name);
+
+	if ((fi.suffix().toLower().compare("cnd") != 0) &&
+		((this->geoBox->currentIndex()==0) || (this->condTypeBox->currentIndex()==0)))
+	{
+		OGSError::box("Multiple geometries or multiple types of conditions\ncan only be saved to *.cnd files.","Inconsistent selection of parameters");
+	}
+	else
+	{
+		QString geo_name = this->geoBox->currentText();
+		if (this->geoBox->currentIndex() == 0) geo_name = "";
+
+		FEMCondition::CondType cond_type;
+		switch (this->condTypeBox->currentIndex())
+		{
+			case 0: 
+				cond_type = FEMCondition::UNSPECIFIED; break;
+			case 1: 
+				cond_type = FEMCondition::BOUNDARY_CONDITION; break;
+			case 2: 
+				cond_type = FEMCondition::INITIAL_CONDITION; break;
+			case 3: 
+				cond_type = FEMCondition::SOURCE_TERM; break;
+			default:
+				std::cout << "Error in ConditionWriterDialog..." << std::endl;
+		}
+
+		emit saveFEMConditionsRequested(geo_name, cond_type, file_name);
+
+		this->done(QDialog::Accepted);
+	}
+}
+
+void ConditionWriterDialog::reject()
+{
+	this->done(QDialog::Rejected);
+}
+
---
+++ DataView/ConditionWriterDialog.h
@@ -0,0 +1,40 @@
+/**
+ * \file ConditionWriterDialog.h
+ * 2012/01/11 KR Initial implementation
+ */
+
+#ifndef CONDITIONWRITERDIALOG_H
+#define CONDITIONWRITERDIALOG_H
+
+#include "ui_ConditionWriter.h"
+#include <QDialog>
+
+#include "GeoObjects.h"
+#include "FEMCondition.h"
+
+/**
+ * \brief A dialog window for creating DIRECT boundary conditions from raster files
+ */
+class ConditionWriterDialog : public QDialog, private Ui_ConditionWriter
+{
+	Q_OBJECT
+
+public:
+	ConditionWriterDialog(const GEOLIB::GEOObjects* geoObjects, QDialog* parent = 0);
+	~ConditionWriterDialog(void);
+	
+private slots:
+	void on_fileNameButton_pressed();
+
+	/// Instructions if the OK-Button has been pressed.
+	void accept();
+
+	/// Instructions if the Cancel-Button has been pressed.
+	void reject();
+	
+signals:
+	void saveFEMConditionsRequested(const QString&, const FEMCondition::CondType, const QString&);
+
+};
+
+#endif //CONDITIONWRITERDIALOG_H
