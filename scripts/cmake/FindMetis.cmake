81b1f763968a0434173170da1521aa2c661c136e
Lars Bilke <lars.bilke@ufz.de>
2012-04-27 12:54:52 +0200

    FindMetis uses CMake default find functions now.

--- scripts/cmake/FindMetis.cmake
+++ scripts/cmake/FindMetis.cmake
@@ -1,14 +1,15 @@
 #
 # Find the METIS includes and libraries
 #
+#    METIS_INCLUDE_DIR - where to find metis.h
+#    METIS_LIBRARIES   - List of fully qualified libraries to link against.
+#    METIS_FOUND       - Do not attempt to use if "no" or undefined.
+#
 # METIS is an library that implements a variety of algorithms for
 # partitioning unstructured graphs, meshes, and for computing fill-reducing orderings of
 # sparse matrices. It can be found at:
 # http://glaros.dtc.umn.edu/gkhome/metis/metis/download
 #
-# METIS_INCLUDE_DIR - where to find autopack.h
-# METIS_LIBRARIES   - List of fully qualified libraries to link against.
-# METIS_FOUND       - Do not attempt to use if "no" or undefined.
 
 FIND_PATH(METIS_INCLUDE_DIR metis.h
 	/usr/include/metis
@@ -22,10 +23,5 @@ FIND_LIBRARY(METIS_LIBRARY metis
 	${CMAKE_SOURCE_DIR}/../libs/lib
 )
 
-IF(METIS_INCLUDE_DIR)
-  IF(METIS_LIBRARY)
-    SET( METIS_LIBRARIES ${METIS_LIBRARY})
-    SET( METIS_FOUND "YES" )
-    MESSAGE(STATUS "Metis found.")
-  ENDIF(METIS_LIBRARY)
-ENDIF(METIS_INCLUDE_DIR)
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Metis DEFAULT_MSG METIS_LIBRARY METIS_INCLUDE_DIR)
