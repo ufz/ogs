81fd41f193f65e9e774e8d6f79788b05d530e9f8
Lars Bilke <lars.bilke@ufz.de>
2012-04-27 12:54:19 +0200

    OpenMP is not supported on clang atm, see #8.

--- scripts/cmake/Find.cmake
+++ scripts/cmake/Find.cmake
@@ -17,7 +17,10 @@ FIND_PACKAGE(cppcheck)
 ### Find libraries ###
 ######################
 
-FIND_PACKAGE(OpenMP)
+# Clang does not have OpenMP support atm, see https://github.com/ufz/ogs/issues/8
+IF(CMAKE_CXX_COMPILER MATCHES "!clang")
+	FIND_PACKAGE(OpenMP)
+ENDIF () # !clang
 IF(OPENMP_FOUND)
 	SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
 	SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
