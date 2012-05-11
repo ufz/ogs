acaf0b858a7554d34e500e741c4318d4cf680597
Thomas Fischer <thomas.fischer@ufz.de>
2012-05-11 16:55:25 +0200

    - added class MemWatch - modified test programm in order to test another example

---
+++ BaseLib/MemWatch.cpp
@@ -0,0 +1,69 @@
+/*
+ * MemWatch.cpp
+ *
+ *  Created on: May 7, 2012
+ *      Author: TF
+ */
+
+#include "MemWatch.h"
+
+#ifndef WIN32
+
+namespace BaseLib {
+
+MemWatch::MemWatch ()
+{
+        updateMemUsage ();
+}
+
+unsigned MemWatch::updateMemUsage ()
+{
+        std::string fname ("/proc/");
+        std::stringstream str_pid;
+        str_pid << (unsigned) getpid();
+        fname += str_pid.str();
+        fname += "/statm";
+        unsigned pages;
+
+        std::ifstream in (fname.c_str(), std::ios::in);
+        if (in == NULL) {
+            perror( "open" );
+            return 1;
+        }
+
+        in >> pages;
+        _vmem_size = ((unsigned long) pages) * ((unsigned long) getpagesize());
+        in >> pages;
+        _rmem_size = ((unsigned long) pages) * ((unsigned long) getpagesize());
+        in >> pages;
+        _smem_size = ((unsigned long) pages) * ((unsigned long) getpagesize());
+        in >> pages;
+        _cmem_size = ((unsigned long) pages) * ((unsigned long) getpagesize());
+        in.close ();
+        return 0;
+}
+
+unsigned long MemWatch::getVirtMemUsage ()
+{
+        updateMemUsage ();
+        return _vmem_size;
+}
+
+unsigned long MemWatch::getResMemUsage () {
+        updateMemUsage ();
+        return _rmem_size;
+}
+
+unsigned long MemWatch::getShrMemUsage () {
+        updateMemUsage ();
+        return _smem_size;
+}
+
+unsigned long MemWatch::getCodeMemUsage () {
+        updateMemUsage ();
+        return _cmem_size;
+}
+
+} // end namespace BaseLib
+
+#endif // WIN
---
+++ BaseLib/MemWatch.h
@@ -0,0 +1,42 @@
+/*
+ * MemWatch.h
+ *
+ *  Created on: May 7, 2012
+ *      Author: TF
+ */
+
+#ifndef MEMWATCH_H_
+#define MEMWATCH_H_
+
+#ifndef WIN32
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+namespace BaseLib {
+
+class MemWatch {
+public:
+	MemWatch ();
+	unsigned long getVirtMemUsage ();
+	unsigned long getResMemUsage ();
+	unsigned long getShrMemUsage ();
+	unsigned long getCodeMemUsage ();
+
+private:
+	unsigned updateMemUsage ();
+	unsigned long _vmem_size;
+	unsigned long _rmem_size;
+	unsigned long _smem_size;
+	unsigned long _cmem_size;
+};
+
+}
+
+#endif // not Windows
+
+#endif /* MEMWATCH_H_ */
