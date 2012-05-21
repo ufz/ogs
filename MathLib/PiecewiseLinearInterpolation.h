9ed855b2f75f8f1e682316b240c81df6cdbfaf14
Thomas Fischer <thomas.fischer@ufz.de>
2012-05-21 16:58:35 +0200

    renamed files MathLib/LinearInterpolation.{cpp,h} to MathLib/PiecewiseLinearInterpolation.{cpp,h}

---
+++ MathLib/PiecewiseLinearInterpolation.h
@@ -0,0 +1,35 @@
+/*
+ * PiecewiseLinearInterpolation.h
+ *
+ *  Created on: Sep 7, 2010
+ *      Author: TF
+ */
+
+#ifndef PIECEWISELINEARINTERPOLATION_H_
+#define PIECEWISELINEARINTERPOLATION_H_
+
+#include <limits>
+#include <vector>
+
+namespace MathLib
+{
+class PiecewiseLinearInterpolation
+{
+public:
+	PiecewiseLinearInterpolation(const std::vector<double>& supporting_points,
+	                    const std::vector<double>& values_at_supp_pnts);
+	PiecewiseLinearInterpolation(const std::vector<double>& supporting_points,
+	                    const std::vector<double>& values_at_supp_pnts,
+	                    const std::vector<double>& points_to_interpolate,
+	                    std::vector<double>& values_at_interpol_pnts);
+	virtual ~PiecewiseLinearInterpolation();
+
+	double getValue ( double pnt_to_interpolate );
+
+private:
+	const std::vector<double>& _supporting_points;
+	const std::vector<double>& _values_at_supp_pnts;
+};
+} // end namespace MathLib
+
+#endif /* PIECEWISELINEARINTERPOLATION_H_ */
