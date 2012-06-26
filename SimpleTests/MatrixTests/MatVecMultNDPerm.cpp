606ef3657f7aa074667fa4ca879a7cbbe85b3688
Thomas Fischer <thomas.fischer@ufz.de>
2012-06-26 16:09:19 +0200

    [SimpleTests/MatrixTests/MatMult.cpp and MatVecMultNDPerm.cpp] - added binary name to output - added number of MVM to output

--- SimpleTests/MatrixTests/MatVecMultNDPerm.cpp
+++ SimpleTests/MatrixTests/MatVecMultNDPerm.cpp
@@ -80,7 +80,7 @@ int main(int argc, char *argv[])
 	logogCout->SetFormatter(*custom_format);
 
 #ifdef OGS_BUILD_INFO
-	INFO("compiler: %s", CMAKE_CXX_COMPILER);
+	INFO("%s was build with compiler %s", argv[0], CMAKE_CXX_COMPILER);
 	if (std::string(CMAKE_BUILD_TYPE).compare("Release") == 0) {
 		INFO("CXX_FLAGS: %s %s", CMAKE_CXX_FLAGS, CMAKE_CXX_FLAGS_RELEASE);
 	} else {
@@ -171,7 +171,7 @@ int main(int argc, char *argv[])
 #endif
 
 	if (verbose) {
-		INFO("*** matrix vector multiplication (MVM) with Toms amuxCRS ... ");
+		INFO("*** %d matrix vector multiplications (MVM) with Toms amuxCRS ... ", n_mults);
 	}
 	run_timer.start();
 	cpu_timer.start();
