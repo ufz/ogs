547a2b9488f5c6378bff5da7a483a30490daefbd
Thomas Fischer <thomas.fischer@ufz.de>
2012-09-03 15:24:38 +0200

    removed double qualification and warnings

--- MeshLib/Elements/Edge.h
+++ MeshLib/Elements/Edge.h
@@ -13,6 +13,8 @@
 #ifndef EDGE_H_
 #define EDGE_H_
 
+#include <limits>
+
 #include "Element.h"
 
 namespace MeshLib {
@@ -84,7 +86,7 @@ public:
 	virtual Element* reviseElement() const;
 
 protected:
-	double Edge::computeVolume();
+	double computeVolume();
 	/// 1D elements have no edges.
 	Node const* getEdgeNode(unsigned edge_id, unsigned node_id) const { (void)edge_id; (void)node_id; return NULL; };
 
@@ -92,7 +94,7 @@ protected:
 	Node* getFaceNode(unsigned face_id, unsigned node_id) const { (void)face_id; (void)node_id; return NULL; };
 
 	/// Returns the ID of a face given an array of nodes (but is not applicable for edges!).
-	unsigned identifyFace(Node* nodes[3]) const { return std::numeric_limits<unsigned>::max(); };
+	unsigned identifyFace(Node* [3]/*nodes[3]*/) const { return std::numeric_limits<unsigned>::max(); };
 
 	double _length;
 
