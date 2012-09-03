1a02eecad0c893dc0a566607a36cb4aaf79bec0f
Thomas Fischer <thomas.fischer@ufz.de>
2012-09-03 10:55:50 +0200

    moved declaration and implementation of operator<= to namespace GeoLib

--- GeoLib/Point.cpp
+++ GeoLib/Point.cpp
@@ -16,26 +16,6 @@
 
 #include "Point.h"
 
-bool operator<= (const GeoLib::Point& p0, const GeoLib::Point& p1)
-{
-	double tol (sqrt (std::numeric_limits<double>::min()));
-
-	if (fabs (p0[0]-p1[0]) > tol * fabs(p0[0])) {
-		if (p0[0] < p1[0]) return true;
-		else return false;
-	} else {
-		// assume p0[0] == p1[0]
-		if (fabs (p0[1]-p1[1]) > tol * fabs(p0[1])) {
-			if (p0[1] < p1[1]) return true;
-			else return false;
-		} else {
-			// assume p0[1] == p1[1] and p0[0] == p1[0]
-			if (p0[2] < p1[2]) return true;
-			else return false;
-		}
-	}
-}
-
 namespace GeoLib {
 
 bool lessX (GeoLib::Point const & p0, GeoLib::Point const & p1)
@@ -56,5 +36,24 @@ bool lessZ (GeoLib::Point const & p0, GeoLib::Point const & p1)
 	return false;
 }
 
+bool operator<= (const GeoLib::Point& p0, const GeoLib::Point& p1)
+{
+	double tol (sqrt (std::numeric_limits<double>::min()));
+
+	if (fabs (p0[0]-p1[0]) > tol * fabs(p0[0])) {
+		if (p0[0] < p1[0]) return true;
+		else return false;
+	} else {
+		// assume p0[0] == p1[0]
+		if (fabs (p0[1]-p1[1]) > tol * fabs(p0[1])) {
+			if (p0[1] < p1[1]) return true;
+			else return false;
+		} else {
+			// assume p0[1] == p1[1] and p0[0] == p1[0]
+			if (p0[2] < p1[2]) return true;
+			else return false;
+		}
+	}
+}
 
 } // end namespace GeoLib
