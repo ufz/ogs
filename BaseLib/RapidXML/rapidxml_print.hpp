dd5c7d4797d05b2fb7c1c71c5ea681f98922be6c
Lars Bilke <lars.bilke@ufz.de>
2012-09-04 11:54:22 +0200

    Fixed a 'variable unused' warning.

--- BaseLib/RapidXML/rapidxml_print.hpp
+++ BaseLib/RapidXML/rapidxml_print.hpp
@@ -118,6 +118,7 @@ namespace rapidxml
         template<class OutIt, class Ch>
         inline OutIt print_attributes(OutIt out, const xml_node<Ch> *node, int flags)
         {
+            (void)flags; // Unused
             for (xml_attribute<Ch> *attribute = node->first_attribute(); attribute; attribute = attribute->next_attribute())
             {
                 if (attribute->name() && attribute->value())
