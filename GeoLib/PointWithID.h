8e18e667dfba887298783bacfdad76c0fac0c643
Thomas Fischer <thomas.fischer@ufz.de>
2012-09-06 08:31:26 +0200

    added standard constructor and changed formating a little bit

--- GeoLib/PointWithID.h
+++ GeoLib/PointWithID.h
@@ -20,22 +20,29 @@ namespace GeoLib
  * class PointWithID is derived from class Point in
  * order to extend the class Point with an ID.
  */
-class PointWithID : public Point
-{
+class PointWithID: public Point {
 public:
-	PointWithID (double x0, double x1, double x2, size_t id) :
-		Point (x0, x1, x2), _id (id)
+	PointWithID(double x0, double x1, double x2, size_t id) :
+		Point(x0, x1, x2), _id(id)
 	{}
 
-	PointWithID (double const* const coords, size_t id) :
-		Point (coords), _id (id)
+	PointWithID(double const* const coords, size_t id) :
+		Point(coords), _id(id)
 	{}
 
-	PointWithID (GeoLib::Point const& pnt, size_t id) :
-		Point (pnt), _id (id)
+	PointWithID(GeoLib::Point const& pnt, size_t id) :
+		Point(pnt), _id(id)
 	{}
 
-	size_t getID () const { return _id; }
+	/**
+	 * standard constructor that initializes the id with 0 and calls
+	 * the standard constructor of class Point
+	 */
+	PointWithID() :
+		Point(), _id(0)
+	{}
+
+	size_t getID() const { return _id; }
 
 protected:
 	size_t _id;
