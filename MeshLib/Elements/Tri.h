cb52e79498dee02ae4a1740b9658bf9509f09aa6
Thomas Fischer <thomas.fischer@ufz.de>
2012-09-27 09:02:41 +0200

    added typedef for first order Triangle in Tri.h

---
+++ MeshLib/Elements/Tri.h
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2012, OpenGeoSys Community (http://www.opengeosys.net)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/LICENSE.txt
+ *
+ * \file Tri.h
+ *
+ *  Created on  Sep 27, 2012 by Thomas Fischer
+ */
+
+#ifndef TRI_H_
+#define TRI_H_
+
+#include "TemplateTri.h"
+
+namespace MeshLib {
+
+typedef TemplateTri<1,3> Tri;
+
+}
+
+#endif /* TRI_H_ */
