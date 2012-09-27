e95a4f78445ad88e15231904f10384e29649c525
Thomas Fischer <thomas.fischer@ufz.de>
2012-09-27 09:16:35 +0200

    added typedef for first order Edge in Edge.h

---
+++ MeshLib/Elements/Edge.h
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2012, OpenGeoSys Community (http://www.opengeosys.net)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/LICENSE.txt
+ *
+ * \file Edge.h
+ *
+ *  Created on  Sep 27, 2012 by Thomas Fischer
+ */
+
+#ifndef EDGE_H_
+#define EDGE_H_
+
+#include "TemplateEdge.h"
+
+namespace MeshLib {
+
+typedef TemplateEdge<1,2> Edge;
+
+}
+
+
+#endif /* EDGE_H_ */
