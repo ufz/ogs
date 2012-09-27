00000be787ecc4fa6cdeebe67852729e4a841a45
Thomas Fischer <thomas.fischer@ufz.de>
2012-09-27 09:52:44 +0200

    added typedef for first order Hex in Hex.cpp

---
+++ MeshLib/Elements/Hex.h
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2012, OpenGeoSys Community (http://www.opengeosys.net)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/LICENSE.txt
+ *
+ * \file Hex.h
+ *
+ *  Created on  Sep 27, 2012 by Thomas Fischer
+ */
+
+#ifndef HEX_H_
+#define HEX_H_
+
+#include "TemplateHex.h"
+
+namespace MeshLib {
+typedef TemplateHex<1,8> Hex;
+}
+
+#endif /* HEX_H_ */
