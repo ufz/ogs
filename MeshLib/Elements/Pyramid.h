32baf93c83078a7f53d25798093a8b6cc93e4bf6
Thomas Fischer <thomas.fischer@ufz.de>
2012-09-27 10:06:24 +0200

    added typedef for first order Pyramid in Pyramid.h

---
+++ MeshLib/Elements/Pyramid.h
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2012, OpenGeoSys Community (http://www.opengeosys.net)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/LICENSE.txt
+ *
+ * \file Pyramid.h
+ *
+ *  Created on  Sep 27, 2012 by Thomas Fischer
+ */
+
+#ifndef PYRAMID_H_
+#define PYRAMID_H_
+
+#include "TemplatePyramid.h"
+
+namespace MeshLib {
+
+typedef TemplatePyramid<1,5> Pyramid;
+
+}
+
+#endif /* PYRAMID_H_ */
