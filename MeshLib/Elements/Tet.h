490137e3589b67e226020daee0bb987c1632c0c8
Thomas Fischer <thomas.fischer@ufz.de>
2012-09-27 09:34:50 +0200

    added typedef for first order Tet in Tet.h

---
+++ MeshLib/Elements/Tet.h
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2012, OpenGeoSys Community (http://www.opengeosys.net)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/LICENSE.txt
+ *
+ * \file Tet.h
+ *
+ *  Created on  Sep 27, 2012 by Thomas Fischer
+ */
+
+#ifndef TET_H_
+#define TET_H_
+
+#include "TemplateTet.h"
+
+namespace MeshLib {
+
+typedef TemplateTet<1,4> Tet;
+
+}
+
+#endif /* TET_H_ */
