47899e625fe74c7bf1d475809d3f1f0c38ae1802
Thomas Fischer <thomas.fischer@ufz.de>
2012-09-27 10:01:30 +0200

    added typedef for first order Prism in Prism.h

---
+++ MeshLib/Elements/Prism.h
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2012, OpenGeoSys Community (http://www.opengeosys.net)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/LICENSE.txt
+ *
+ * \file Prism.h
+ *
+ *  Created on  Sep 27, 2012 by Thomas Fischer
+ */
+
+#ifndef PRISM_H_
+#define PRISM_H_
+
+#include "TemplatePrism.h"
+
+namespace MeshLib {
+
+typedef TemplatePrism<1,6> Prism;
+
+}
+
+#endif /* PRISM_H_ */
