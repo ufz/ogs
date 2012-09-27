387390d528d0d6ed5af8afc13519546fd984b412
Thomas Fischer <thomas.fischer@ufz.de>
2012-09-27 09:27:31 +0200

    added typedef for first order Quad in Quad.h

---
+++ MeshLib/Elements/Quad.h
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2012, OpenGeoSys Community (http://www.opengeosys.net)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/LICENSE.txt
+ *
+ * \file Quad.h
+ *
+ *  Created on  Sep 27, 2012 by Thomas Fischer
+ */
+
+#ifndef QUAD_H_
+#define QUAD_H_
+
+#include "TemplateQuad.h"
+
+namespace MeshLib {
+
+typedef TemplateQuad<1,4> Quad;
+
+}
+
+#endif /* QUAD_H_ */
