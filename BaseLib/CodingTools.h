f96320252a8062b47efae1931b11465f90b256ed
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2012-10-26 00:09:04 +0200

    add CodingTools.h

---
+++ BaseLib/CodingTools.h
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2012, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ *
+ * \file CodingTools.h
+ *
+ * Created on 2012-02-17 by Norihiro Watanabe
+ */
+
+#ifndef CODINGTOOLS_H
+#define CODINGTOOLS_H
+
+#define DISALLOW_COPY_AND_ASSIGN(TypeName) \
+    TypeName(const TypeName&);   \
+    TypeName &operator=(const TypeName&)
+
+#endif
