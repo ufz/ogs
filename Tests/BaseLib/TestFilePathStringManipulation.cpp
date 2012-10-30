87554f20c0391269c3805c6febfa2aa6687e37e7
Dmitri Naumov <dmitri.naumov@ufz.de>
2012-10-30 17:46:11 +0100

    Add empty test for FileTools.

---
+++ Tests/BaseLib/TestFilePathStringManipulation.cpp
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) 2012, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ */
+
+#include "gtest.h"
+
+#include "FileTools.h"
+
