6d2781b1d06466596289a2b87e4a03e3110bac80
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2012-11-03 20:00:59 +0100

    merge Options from the draft and use RapidXML instead of tinyxml

---
+++ BaseLib/Options.h
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2012, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ *
+ * \file Options.h
+ *
+ * Created on 2012-02-02 by Norihiro Watanabe
+ */
+
+#pragma once
+
+#include "OptionGroup.h"
+
+namespace BaseLib
+{
+
+typedef OptionGroup Options;
+
+}
