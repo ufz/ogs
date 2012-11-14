d9b9e81082be41f7303961324ca8e67ef74c94e8
Thomas Fischer <thomas.fischer@ufz.de>
2012-11-14 16:16:31 +0100

    refactored convert raster to mesh functionality from class VtkMeshConverter

---
+++ MeshLib/ConvertRasterToMesh.h
@@ -0,0 +1,56 @@
+/**
+ * @file ConvertRasterToMesh.h
+ * @author Thomas Fischer
+ * @date Nov 14, 2012
+ *
+ * @copyright
+ * Copyright (c) 2012, OpenGeoSys Community (http://www.opengeosys.net)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/LICENSE.txt
+ */
+
+#ifndef CONVERTRASTERTOMESH_H_
+#define CONVERTRASTERTOMESH_H_
+
+// GeoLib
+#include "Raster.h"
+
+// MeshLib
+#include "MshEnums.h"
+
+namespace MeshLib {
+
+// forward declaration
+class Mesh;
+
+/// Selection of possible interpretations for intensities
+struct UseIntensityAs
+{
+	enum type {
+		ELEVATION,
+		MATERIAL,
+		NONE
+	};
+};
+
+/**
+ * Class to convert raster data into meshes.
+ */
+class ConvertRasterToMesh {
+public:
+	ConvertRasterToMesh(GeoLib::Raster const& raster, MshElemType::type elem_type,
+					  UseIntensityAs::type intensity_type);
+	~ConvertRasterToMesh();
+	MeshLib::Mesh* execute() const;
+private:
+	double getExistingValue(GeoLib::Raster::const_iterator beg, GeoLib::Raster::const_iterator last) const;
+	MeshLib::Mesh* constructMesh(const double* pix_vals, int* node_idx_map, const bool* vis_nodes) const;
+	GeoLib::Raster const& _raster;
+	MshElemType::type _elem_type;
+	UseIntensityAs::type _intensity_type;
+};
+
+} // end namespace MeshLib
+
+#endif /* CONVERTRASTERTOMESH_H_ */
