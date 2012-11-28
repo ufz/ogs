85a7176ad239e0d4854868d3e6c600dabdd2ff40
Thomas Fischer <thomas.fischer@ufz.de>
2012-11-28 12:13:23 +0100

    style

--- GeoLib/Point.cpp
+++ GeoLib/Point.cpp
@@ -62,18 +62,24 @@ bool lessEq(const GeoLib::Point& p0, const GeoLib::Point& p1)
 
 	// test a relative and an absolute criterion
 	if (fabs(p0[0]-p1[0]) > tol * std::min(fabs(p1[0]), fabs(p0[0])) && fabs(p0[0]-p1[0]) > tol) {
-		if (p0[0] <= p1[0]) return true;
-		else return false;
+		if (p0[0] <= p1[0])
+			return true;
+		else
+			return false;
 	} else {
 		// assume p0[0] == p1[0]
 		if (fabs (p0[1]-p1[1]) > tol * fabs(p0[1]) && fabs(p0[1]-p1[1]) > tol) {
-			if (p0[1] <= p1[1]) return true;
-			else return false;
+			if (p0[1] <= p1[1])
+				return true;
+			else
+				return false;
 		} else {
 			// assume p0[1] == p1[1] and p0[0] == p1[0]
 			if (fabs (p0[2]-p1[2]) > tol * fabs(p0[2]) && fabs(p0[2]-p1[2]) > tol) {
-				if (p0[2] <= p1[2]) return true;
-				else return false;
+				if (p0[2] <= p1[2])
+					return true;
+				else
+					return false;
 			} else {
 				return true;
 			}
