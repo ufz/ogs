3efd65977cc07bad63afd7c1eb6de26a547c3650
Lars Bilke <lars.bilke@ufz.de>
2012-11-28 12:30:04 +0100

    Boost setup working on Windows.
    - Changed Boost version to 1.52.0
    - Should work on MSVC 2012 as well
    - Fixed Boost_CONFIG_OPTIONS
    - tar.bz2 archive is used on Windows as well because zip archives are
    not fully supported by CMake's ExternalProject_Add (was uncompressed on
    every build)
    - Boost lib dir was added to linker path
    - Boost libs to link are extracted from specified library names on
    !Visual Studio
    - Boost libs to link get automatically linked on Visual Studio

--- scripts/cmake/ExternalProjectBoost.cmake
+++ scripts/cmake/ExternalProjectBoost.cmake
@@ -1,18 +1,25 @@
 INCLUDE(ExternalProject)
 # Set Boost version and which libraries to compile
-SET(Boost_Version 1.49.0)
+SET(Boost_Version 1.52.0)
 SET(BOOST_LIBS_TO_BUILD
-	--with-program_options
-	--with-filesystem
-	--with-system
-	--with-date_time
+	program_options
+	filesystem
+	system
+	date_time
 )
+
+# Prefix with --with- for bjam (b2) build command
+FOREACH(LIB_TO_BUILD ${BOOST_LIBS_TO_BUILD})
+	SET(BOOST_LIBS_TO_BUILD_CMD ${BOOST_LIBS_TO_BUILD_CMD} --with-${LIB_TO_BUILD})
+ENDFOREACH()
 
 STRING(REPLACE "." "_" Boost_Version_Underscore ${Boost_Version})
 
 # Set boost toolset
-IF(MSVC)
-	SET(BOOST_TOOLSET msvc-10.0) # TODO: VS 2010 only
+IF(MSVC10)
+	SET(BOOST_TOOLSET msvc-10.0)
+ELSEIF(MSVC11)
+	SET(BOOST_TOOLSET msvc-11.0)
 ENDIF()
 IF(COMPILER_IS_GCC)
 	SET(BOOST_TOOLSET gcc)
@@ -32,19 +39,14 @@ ENDIF()
 SET(BOOST_CONFIG_OPTIONS "")
 IF(WIN32)
 	IF(HAVE_64_BIT)
-		SET(BOOST_CONFIG_OPTIONS "architecture=x86 address-model=64")
+		SET(BOOST_CONFIG_OPTIONS "architecture=x86;address-model=64")
 	ENDIF()
 ENDIF()
 
 # Set archive sources
 SET(BOOST_ARCHIVE_EXT "tar.bz2")
-SET(BOOST_ARCHIVE_MD5 0d202cb811f934282dea64856a175698)
-#.gz SET(BOOST_ARCHIVE_EXT "tar.gz")
-#.gz SET(BOOST_ARCHIVE_MD5 e0defc8c818e4f1c5bbb29d0292b76ca)
-IF(WIN32)
-	SET(BOOST_ARCHIVE_EXT "zip")
-	SET(BOOST_ARCHIVE_MD5 854dcbbff31b896c85c38247060b7713)
-ENDIF()
+SET(BOOST_ARCHIVE_MD5 3a855e0f919107e0ca4de4d84ad3f750)
+
 SET(BOOST_URL "http://switch.dl.sourceforge.net/project/boost/boost/${Boost_Version}/boost_${Boost_Version_Underscore}.${BOOST_ARCHIVE_EXT}")
 
 ExternalProject_Add(Boost
@@ -53,19 +55,21 @@ ExternalProject_Add(Boost
 	URL_MD5 ${BOOST_ARCHIVE_MD5}
 	UPDATE_COMMAND "${BOOST_UPDATE_COMMAND}"
 	CONFIGURE_COMMAND ""
-	BUILD_COMMAND ./b2 ${BOOST_LIBS_TO_BUILD} -j ${NUM_PROCESSORS} toolset=${BOOST_TOOLSET} link=static stage ${BOOST_CONFIG_OPTIONS}
+	BUILD_COMMAND ./b2 ${BOOST_LIBS_TO_BUILD_CMD} -j ${NUM_PROCESSORS} toolset=${BOOST_TOOLSET} link=static stage ${BOOST_CONFIG_OPTIONS}
 	BUILD_IN_SOURCE 1
 	INSTALL_COMMAND ""
 )
 ExternalProject_Get_Property( Boost source_dir )
 
-#IF(NOT Boost_INCLUDE_DIRS)
+IF(NOT Boost_INCLUDE_DIRS)
 	SET( Boost_INCLUDE_DIRS ${source_dir} CACHE INTERNAL "Boost include directories")
-	# TODO this has to be set manually!
-	FILE( GLOB Boost_LIBRARIES "${source_dir}/stage/lib/" "${source_dir}/stage/lib/*.a")
-	SET( Boost_LIBRARIES ${Boost_LIBRARIES} CACHE INTERNAL "Boost libraries")
-	MESSAGE(STATUS "Boost inc: ${Boost_INCLUDE_DIRS}")
-	MESSAGE(STATUS "Boost libs: ${Boost_LIBRARIES}")
-#ENDIF()
+	# On Visual Studio Boost libs get automatically linked
+	IF(MSVC)
+		SET( Boost_LIBRARIES "" CACHE INTERNAL "Boost libraries")
+	ELSE()
+		SET( Boost_LIBRARIES ${BOOST_LIBS_TO_BUILD_CMD} CACHE INTERNAL "Boost libraries")
+	ENDIF()
+ENDIF()
 
 INCLUDE_DIRECTORIES( ${Boost_INCLUDE_DIRS} )
+LINK_DIRECTORIES( ${source_dir}/stage/lib/ )
