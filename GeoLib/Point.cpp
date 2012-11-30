c92b4e6a82718b866a08b4f48240852306647417
Thomas Fischer <thomas.fischer@ufz.de>
2012-11-30 11:31:04 +0100

    reimplemented operator<=(Point&, Point&) (not using an epsilon)
    removed unused comparsion operations lessX(), lessY(), lessZ()

--- GeoLib/Point.cpp
+++ GeoLib/Point.cpp
@@ -18,41 +18,30 @@
 
 namespace GeoLib {
 
-bool lessX (GeoLib::Point const & p0, GeoLib::Point const & p1)
-{
-	if (p0[0] <= p1[0]) return true;
-	return false;
-}
-
-bool lessY (GeoLib::Point const & p0, GeoLib::Point const & p1)
-{
-	if (p0[1] <= p1[1]) return true;
-	return false;
-}
-
-bool lessZ (GeoLib::Point const & p0, GeoLib::Point const & p1)
-{
-	if (p0[2] <= p1[2]) return true;
-	return false;
-}
-
 bool operator<= (const GeoLib::Point& p0, const GeoLib::Point& p1)
 {
-	const double tol(std::numeric_limits<double>::epsilon());
-
-	if (fabs (p0[0]-p1[0]) > tol * fabs(p0[0])) {
-		if (p0[0] < p1[0]) return true;
-		else return false;
+	if (p0[0] > p1[0]) {
+		return false;
 	} else {
-		// assume p0[0] == p1[0]
-		if (fabs (p0[1]-p1[1]) > tol * fabs(p0[1])) {
-			if (p0[1] < p1[1]) return true;
-			else return false;
-		} else {
-			// assume p0[1] == p1[1] and p0[0] == p1[0]
-			if (p0[2] < p1[2]) return true;
-			else return false;
+		if (p0[0] < p1[0]) {
+			return true;
 		}
+	}
+	// => p0[0] == p1[0]
+
+	if (p0[1] > p1[1]) {
+		return false;
+	} else {
+		if (p0[1] < p1[1]) {
+			return true;
+		}
+	}
+	// => p0[1] == p1[1]
+
+	if (p0[2] > p1[2]) {
+		return false;
+	} else {
+		return true;
 	}
 }
 
--- GeoLib/Point.h
+++ GeoLib/Point.h
@@ -24,30 +24,6 @@ namespace GeoLib {
 typedef TemplatePoint<double> Point;
 
 /**
- * comparison based on the x coordinate
- * @param p0 first point
- * @param p1 second point
- * @return true if the x coordinate of p0 is smaller equal the x coordinate of p1, else false
- */
-bool lessX (Point const & p0, Point const & p1);
-
-/**
- * comparison based on the y coordinate
- * @param p0 first point
- * @param p1 second point
- * @return true if the y coordinate of p0 is smaller equal the y coordinate of p1, else false
- */
-bool lessY (Point const & p0, Point const & p1);
-
-/**
- * comparison based on the z coordinate
- * @param p0 first point
- * @param p1 second point
- * @return true if the z coordinate of p0 is smaller equal the z coordinate of p1, else false
- */
-bool lessZ (Point const & p0, Point const & p1);
-
-/**
  * lexicographic comparison of points
  */
 bool operator<= (GeoLib::Point const & p0, GeoLib::Point const & p1);
