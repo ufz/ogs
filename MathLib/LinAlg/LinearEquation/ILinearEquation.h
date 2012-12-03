d9e2eca14f7d9325ea5620fa28eb5b80ac1041b4
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2012-12-03 18:14:57 +0100

    add protected constructor and documentation

--- MathLib/LinAlg/LinearEquation/ILinearEquation.h
+++ MathLib/LinAlg/LinearEquation/ILinearEquation.h
@@ -23,9 +23,9 @@ namespace MathLib
 {
 
 /**
- * \brief Interface to any algebraic linear equation systems Ax=b
+ * \brief Interface to any system of linear equations Ax=b
  *
- * This class defines unified interface to algebraic linear equation systems
+ * This class defines unified interface to a system of linear equations
  * including linear solvers.
  *
  */
@@ -39,7 +39,6 @@ public:
     virtual ~ILinearEquation()
     {
     }
-    ;
 
     /**
      * create a linear equation 
@@ -97,27 +96,35 @@ public:
     virtual void addA(size_t rowId, size_t colId, double v) = 0;
 
     /**
-     * add a sub-matrix into a matrix
+     * add a sub-matrix into a matrix at given row and column positions
      *
-     * @param vec_row_pos   A list of global row index
-     * @param vec_col_pos   A list of global column index
+     * @param vec_row_pos
+     *  A vector of global row index. The number of entries of vec_row_pos have
+     *  to be identical to the number of rows of the local matrix.
+     * @param vec_col_pos
+     *  A vector of global column index. The number of vec_col_pos have to be
+     *  identical to the number of columns of the local matrix.
      * @param sub_matrix    A sub-matrix
-     * @param fkt           scaling parameter
+     * @param fac           scaling parameter
      */
     template <class T_DENSE_MATRIX>
     void addAsub(const std::vector<size_t> &vec_row_pos,
             const std::vector<size_t> &vec_col_pos,
-            const T_DENSE_MATRIX &sub_matrix, double fkt = 1.0);
+            const T_DENSE_MATRIX &sub_matrix, double fac = 1.0);
 
     /**
      * add a sub-matrix into a matrix
-     * @param vec_row_col_pos   A list of global row and column index
+     *
+     * @param vec_row_col_pos
+     *  A vector of global row and column index. The number of entries of
+     *  vec_row_col_pos have to be identical to the number of rows and columns
+     *  of the local matrix.
      * @param sub_matrix        A sub-matrix
-     * @param fkt               scaling parameter
+     * @param fac               scaling parameter
      */
     template <class T_DENSE_MATRIX>
     void addAsub(const std::vector<size_t> &vec_row_col_pos,
-            const T_DENSE_MATRIX &sub_matrix, double fkt = 1.0);
+            const T_DENSE_MATRIX &sub_matrix, double fac = 1.0);
 
     /**
      * get RHS entry
@@ -152,23 +159,27 @@ public:
     /**
      * add a sub vector to RHS
      *
-     * @param vec_row_pos   A list of global row index
+     * @param vec_row_pos
+     *  A vector of global row index. The number of entries of vec_row_pos
+     *  have to be identical to the number of rows of the sub vector.
      * @param sub_vector    Pointer to a sub-vector
-     * @param fkt           Scaling parameter
+     * @param fac           Scaling parameter
      */
     inline virtual void addRHSsub(const std::vector<size_t> &vec_row_pos,
-            const double* sub_vector, double fkt = 1.0);
+            const double* sub_vector, double fac = 1.0);
 
     /**
      * add a sub vector to RHS
      *
-     * @param vec_row_pos   A list of global row index
+     * @param vec_row_pos
+     *  A vector of global row index. The number of entries of vec_row_pos
+     *  have to be identical to the number of rows of the sub vector.
      * @param sub_vector    A sub-vector
-     * @param fkt           Scaling parameter
+     * @param fac           Scaling parameter
      */
     template <class T_DENSE_VECTOR>
     void addRHSsub(const std::vector<size_t> &vec_row_pos,
-            const T_DENSE_VECTOR &sub_vector, double fkt = 1.0);
+            const T_DENSE_VECTOR &sub_vector, double fac = 1.0);
 
     /**
      * get a solution vector
@@ -200,8 +211,8 @@ public:
 
     /**
      * set prescribed value
-     * @param vec_id    A list of global row index
-     * @param vec_x     A list of prescribed values
+     * @param vec_id    A vector of global row index
+     * @param vec_x     A vector of prescribed values
      */
     virtual void setKnownX(const std::vector<size_t> &vec_id,
             const std::vector<double> &vec_x) = 0;
@@ -211,6 +222,10 @@ public:
 
     /// print out the equation for debugging
     virtual void printout(std::ostream &os = std::cout) const = 0;
+
+protected:
+    /// constructor
+    ILinearEquation() {};
 };
 
 }
