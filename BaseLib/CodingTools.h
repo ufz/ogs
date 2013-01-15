51e8948200d19b357acf0822211d89a9a891a336
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2013-01-15 18:19:19 +0100

    add helper functions to release memory

--- BaseLib/CodingTools.h
+++ BaseLib/CodingTools.h
@@ -19,4 +19,39 @@
     TypeName(const TypeName&);   \
     TypeName &operator=(const TypeName&)
 
+namespace BaseLib
+{
+
+template <typename T>
+static void releaseObjectsInStdVector(T &object)
+{
+    if (object.size()>0) {
+        const std::size_t vec_size(object.size());
+        for (std::size_t i=0; i<vec_size; i++)
+            if (object[i]!=nullptr) delete object[i];
+        object.clear();
+    }
+};
+
+template <typename T>
+static void releaseObjectsInStdMap(T &object)
+{
+    if (object.size()>0) {
+        for (typename T::iterator itr=object.begin(); itr!=object.end(); ++itr)
+            if (itr->second!=nullptr) delete itr->second;
+        object.clear();
+    }
+};
+
+template <typename T>
+static void releaseObjectsInStdQueue(T &container)
+{
+    while (!container.empty()) {
+        if (container.front() !=nullptr) delete container.front();
+        container.pop();
+    }
+};
+
+} //BaseLib
+
 #endif
