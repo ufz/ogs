eeb82a6169efa288fd2f55c244d1b4a21e4c3fe4
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2013-05-23 13:37:08 +0200

    replace spaces with tab for consistency

--- MathLib/LinAlg/Dense/Vector.h
+++ MathLib/LinAlg/Dense/Vector.h
@@ -34,8 +34,8 @@ public:
 	 * @param nrows number of rows
 	 * @return
 	 */
-    explicit Vector(std::size_t nrows=0)
-    : std::valarray<T>(nrows)
+	explicit Vector(std::size_t nrows=0)
+	: std::valarray<T>(nrows)
 	{}
 
 	/**
@@ -43,8 +43,8 @@ public:
 	 * @param original the object that is copied
 	 * @return
 	 */
-    Vector (Vector<T> const& original)
-    : std::valarray<T>(static_cast<std::valarray<T> >(original))
+	Vector (Vector<T> const& original)
+	: std::valarray<T>(static_cast<std::valarray<T> >(original))
 	{}
 
 	/**
@@ -61,48 +61,48 @@ public:
 	 */
 	Vector<T> & operator=(const T& val)
 	{
-	    this->std::valarray<T>::operator =(val);
-	    return *this;
+		this->std::valarray<T>::operator =(val);
+		return *this;
 	};
 
-    /// return a start index of the active data range
+	/// return a start index of the active data range
 	std::size_t getRangeBegin() const { return 0;}
 
-    /// return an end index of the active data range
+	/// return an end index of the active data range
 	std::size_t getRangeEnd() const { return this->size(); }
 
-    /// get entry
-    double get(std::size_t i) const { return (*this)[i]; };
+	/// get entry
+	double get(std::size_t i) const { return (*this)[i]; };
 
-    /// set a value to entry
-    void set(std::size_t i, double v) { (*this)[i] = v; };
+	/// set a value to entry
+	void set(std::size_t i, double v) { (*this)[i] = v; };
 
-    /// add a value to entry
-    void add(std::size_t i, double v) { (*this)[i] += v; };
+	/// add a value to entry
+	void add(std::size_t i, double v) { (*this)[i] += v; };
 
-    /**
-     * add a sub vector
-     * @param pos       positions of each sub-vector entry in this vector
-     * @param sub_vec   sub-vector
-     */
-    template<class T_SUBVEC>
-    void addSubVector(const std::vector<std::size_t> &pos, const T_SUBVEC &sub_vec)
-    {
-        for (std::size_t i=0; i<pos.size(); ++i) {
-            this->add(pos[i], sub_vec[i]);
-        }
-    }
+	/**
+	 * add a sub vector
+	 * @param pos       positions of each sub-vector entry in this vector
+	 * @param sub_vec   sub-vector
+	 */
+	template<class T_SUBVEC>
+	void addSubVector(const std::vector<std::size_t> &pos, const T_SUBVEC &sub_vec)
+	{
+		for (std::size_t i=0; i<pos.size(); ++i) {
+			this->add(pos[i], sub_vec[i]);
+		}
+	}
 
-    /**
-     * writes the matrix entries into a file
-     * @param output file name
-     */
-    void write (const std::string &filename) const
-    {
-        std::ofstream os(filename);
-        os << *this;
-        os.close();
-    }
+	/**
+	 * writes the matrix entries into a file
+	 * @param output file name
+	 */
+	void write (const std::string &filename) const
+	{
+		std::ofstream os(filename);
+		os << *this;
+		os.close();
+	}
 
 };
 
@@ -113,8 +113,8 @@ public:
 template <typename T>
 std::ostream& operator<<(std::ostream& os, Vector<T> const & v)
 {
-    std::copy(v.begin(), v.end(), std::ostream_iterator<T>(os, "\n"));
-    return os;
+	std::copy(v.begin(), v.end(), std::ostream_iterator<T>(os, "\n"));
+	return os;
 }
 
 }
