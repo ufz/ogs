7ccb2c26e78e0496809c5ed02396e4599ebfbdb8
Thomas Fischer <thomas.fischer@ufz.de>
2013-06-11 13:40:49 +0200

    Added FinalizeMatrixAssembly.h.

---
+++ MathLib/LinAlg/FinalizeMatrixAssembly.h
@@ -0,0 +1,23 @@
+/**
+ * @file FinalizeMatrixAssembly.h
+ * @author Thomas Fischer
+ * @date Jun 11, 2013
+ * @brief 
+ *
+ * @copyright
+ * Copyright (c) 2013, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/LICENSE.txt
+ */
+
+#ifndef FINALIZEMATRIXASSEMBLY_H_
+#define FINALIZEMATRIXASSEMBLY_H_
+
+template <typename MAT_T>
+bool finalizeMatrixAssembly(MAT_T &)
+{
+	return true;
+}
+
+#endif /* FINALIZEMATRIXASSEMBLY_H_ */
