1cc3931a8c710ccfd08aaefcd73715fbf8b6b5e4
Thomas Fischer <thomas.fischer@ufz.de>
2013-06-14 13:00:36 +0200

    Used specific properties of dense matrices in applyKnownSolution().

--- MathLib/LinAlg/Dense/DenseTools.h
+++ MathLib/LinAlg/Dense/DenseTools.h
@@ -23,30 +23,35 @@ namespace MathLib
 {
 
 /**
- * apply known solutions to a system of linear equations
+ * Apply known solutions to a system of linear equations.
  *
- * This function introduces the given constrain by diagonalizing a coefficient matrix.
- * Symmetricity of the matrix is preserved.
+ * This function introduces the given constrains by diagonalizing a coefficient matrix.
+ * Symmetry of the matrix is preserved.
  *
- * @param A                 Coefficient matrix
- * @param b                 RHS vector
- * @param _vec_knownX_id    a vector of known solution entry IDs
- * @param _vec_knownX_x     a vector of known solutions
+ * @param A                Coefficient matrix
+ * @param b                RHS vector
+ * @param vec_knownX_id    a vector of known solution entry IDs
+ * @param vec_knownX_x     a vector of known solutions
  */
-void applyKnownSolution(DenseMatrix<double> &A, DenseVector<double> &b, const std::vector<std::size_t> &_vec_knownX_id, const std::vector<double> &_vec_knownX_x);
+void applyKnownSolution(DenseMatrix<double> &A, DenseVector<double> &b,
+		const std::vector<std::size_t> &vec_knownX_id, const std::vector<double> &vec_knownX_x);
 
 /**
- * apply known solutions to a system of linear equations
+ * Apply known solutions to a system of linear equations \f$A x = b\f$.
  *
- * This function introduces the given constrain by diagonalizing a coefficient matrix.
- * Symmetricity of the matrix is preserved.
+ * This function introduces the given constrain into the system of linear
+ * equations. For this purpose it modifies the entries of \f$A\f$ in the
+ * \f$k\f$-th row and column, i.e. all those entries are set to zero except
+ * the diagonal entry that is set to one. The right
+ * hand side \f$b\f$ is modified, too. The symmetry of \f$A\f$ is preserved.
  *
  * @param A         Coefficient matrix
  * @param b         RHS vector
  * @param row_id    a known solution entry ID
- * @param x         a known solution
+ * @param val       a known solution
  */
-void applyKnownSolution(DenseMatrix<double> &A, DenseVector<double> &b, std::size_t row_id, double x);
+void applyKnownSolution(DenseMatrix<double> &A, DenseVector<double> &b, std::size_t row_id,
+		double val);
 
 } // MathLib
 
