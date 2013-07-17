3d1d3b73cc45f890efc59a8582d5c791b9a56e11
Lars Bilke <lars.bilke@ufz.de>
2013-07-17 16:28:39 +0200

    Works on Windows too.

--- scripts/cmake/ExternalProjectVtk.cmake
+++ scripts/cmake/ExternalProjectVtk.cmake
@@ -1,5 +1,7 @@
+MESSAGE(STATUS "In ExternalProjectVtk.cmake")
+
 INCLUDE(ExternalProject)
-# Set Boost version and which libraries to compile
+
 SET(OGS_VTK_VERSION 6.0.0)
 SET(OGS_VTK_REQUIRED_LIBS
 	vtkIOXML
@@ -16,37 +18,62 @@ IF(OGS_BUILD_GUI)
 	)
 ENDIF()
 
-IF(VTK_FOUND)
+IF(VTK_FOUND AND VTK_DIR MATCHES "${CMAKE_BINARY_DIR}/External/vtk/src/VTK-build")
+	MESSAGE(STATUS "Using system vtk.")
 	INCLUDE( ${VTK_USE_FILE} )
 	RETURN()
 ENDIF()
 
-IF(NOT DEFINED VTK_DIR)
-	SET(VTK_DIR ${CMAKE_BINARY_DIR}/External/vtk/src/Vtk-build)
+IF(NOT VTK_DIR)
+	MESSAGE(STATUS "Using local vtk.")
+	SET(VTK_DIR ${CMAKE_BINARY_DIR}/External/vtk/src/VTK-build CACHE PATH "" FORCE)
 ENDIF()
-FIND_PACKAGE(VTK ${OGS_VTK_VERSION} COMPONENTS ${OGS_VTK_REQUIRED_LIBS} NO_MODULE)
+FIND_PACKAGE(VTK ${OGS_VTK_VERSION} COMPONENTS ${OGS_VTK_REQUIRED_LIBS} )
+
 IF(VTK_FOUND)
 	INCLUDE( ${VTK_USE_FILE} )
-	RETURN()
 ENDIF()
 
 # Set archive sources
-SET(VTK_ARCHIVE_MD5 72ede4812c90bdc55172702f0cad02bb)
-SET(VTK_URL "http://www.vtk.org/files/release/6.0/vtk-${OGS_VTK_VERSION}.tar.gz")
+IF(WIN32)
+	SET(VTK_ARCHIVE_MD5 34c47de28660b41c5f96cb3c7f420dd2)
+	SET(VTK_URL "http://www.vtk.org/files/release/6.0/vtk-${OGS_VTK_VERSION}.zip")
+ELSE()
+	SET(VTK_ARCHIVE_MD5 72ede4812c90bdc55172702f0cad02bb)
+	SET(VTK_URL "http://www.vtk.org/files/release/6.0/vtk-${OGS_VTK_VERSION}.tar.gz")
+ENDIF()
 
 IF(OGS_BUILD_GUI)
 	SET(OGS_VTK_CMAKE_ARGS "-DVTK_Group_Qt:BOOL=ON")
 ENDIF()
 
+IF(WIN32)
+	FOREACH(VTK_LIB ${OGS_VTK_REQUIRED_LIBS})
+		IF(NOT DEFINED VTK_COMMAND)
+			SET(VTK_MAKE_COMMAND
+				cmake --build . --config Release --target ${VTK_LIB} &&
+				cmake --build . --config Debug --target)
+		ELSE()
+			SET(VTK_MAKE_COMMAND
+				${VTK_MAKE_COMMAND} &&
+				cmake --build . --config Release --target ${VTK_LIB} &&
+				cmake --build . --config Debug --target ${VTK_LIB})
+		ENDIF()
+	ENDFOREACH()
+	# MESSAGE(STATUS ${VTK_MAKE_COMMAND})
+ELSE()
+	SET(VTK_MAKE_COMMAND make -j ${NUM_PROCESSORS} ${OGS_VTK_REQUIRED_LIBS})
+ENDIF()
+
 ExternalProject_Add(VTK
 	PREFIX ${CMAKE_BINARY_DIR}/External/vtk
-	URL /Users/bilke/tmp/vtk-6.0.0.tar.gz #${VTK_URL}
+	URL ${VTK_URL}
 	URL_MD5 ${VTK_ARCHIVE_MD5}
 	CMAKE_ARGS
 		-DBUILD_TESTING:BOOL=OFF
 		-DCMAKE_BUILD_TYPE:STRING=Release
 		${OGS_VTK_CMAKE_ARGS}
-	BUILD_COMMAND make -j ${NUM_PROCESSORS} ${OGS_VTK_REQUIRED_LIBS}
+	BUILD_COMMAND ${VTK_MAKE_COMMAND}
 	INSTALL_COMMAND ""
 )
 
