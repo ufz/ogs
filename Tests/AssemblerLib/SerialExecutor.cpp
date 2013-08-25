9301458699e3969a3b0a4a06fc6fb30cf49b9852
Dmitrij Naumov <dmitrij@naumov.de>
2013-08-25 12:26:12 +0200

    Add SerialExecutor test for std::vector.

---
+++ Tests/AssemblerLib/SerialExecutor.cpp
@@ -0,0 +1,79 @@
+/**
+ * \copyright
+ * Copyright (c) 2013, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <vector>
+#include "AssemblerLib/SerialExecutor.h"
+
+template <typename Container>
+class AssemblerLibSerialExecutor : public ::testing::Test
+{
+    public:
+    AssemblerLibSerialExecutor()
+    {
+        reference.resize(size);
+        std::iota(reference.begin(), reference.end(), 0);
+
+        std::copy(reference.begin(), reference.end(),
+            std::back_inserter(container));
+    }
+
+    void
+    subtractFromReference(int const value, std::size_t const index)
+    {
+        reference[index] -= value;
+    }
+
+    bool
+    referenceIsZero() const
+    {
+        return std::all_of(reference.begin(), reference.end(),
+            [](int const reference_value)
+            {
+                return reference_value == 0;
+            });
+    }
+
+    static std::size_t const size = 100;
+    std::vector<int> reference;
+    Container container;
+};
+
+TYPED_TEST_CASE_P(AssemblerLibSerialExecutor);
+
+using namespace std::placeholders;
+
+TYPED_TEST_P(AssemblerLibSerialExecutor, ContainerArgument)
+{
+    AssemblerLib::serialExecute(
+        std::bind(&TestFixture::subtractFromReference, this, _1, _2),
+        this->container);
+    ASSERT_TRUE(this->referenceIsZero());
+}
+
+TYPED_TEST_P(AssemblerLibSerialExecutor, IteratorArgument)
+{
+    AssemblerLib::serialExecute(
+        std::bind(&TestFixture::subtractFromReference, this, _1, _2),
+        this->container.begin(), this->container.end());
+    ASSERT_TRUE(this->referenceIsZero());
+}
+
+REGISTER_TYPED_TEST_CASE_P(AssemblerLibSerialExecutor,
+    ContainerArgument, IteratorArgument);
+
+
+typedef ::testing::Types <
+      std::vector<int>
+    > TestTypes;
+
+INSTANTIATE_TYPED_TEST_CASE_P(templated, AssemblerLibSerialExecutor,
+    TestTypes);
