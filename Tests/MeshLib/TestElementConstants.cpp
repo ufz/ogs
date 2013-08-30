124b1bc53c26cc73a0b43721b08caa97d89d7d4e
Thomas Fischer <thomas.fischer@ufz.de>
2013-08-30 06:53:20 +0200

    Accessing dim and num of nodes via methods too.

--- Tests/MeshLib/TestElementConstants.cpp
+++ Tests/MeshLib/TestElementConstants.cpp
@@ -16,8 +16,19 @@ using namespace MeshLib;
 
 TEST(MeshLib, ElementConstatns)
 {
-	ASSERT_EQ(2u, Quad::Dimension);
-	ASSERT_EQ(4u, Quad::NAllNodes);
-	ASSERT_EQ(4u, Quad::NBaseNodes);
+	ASSERT_EQ(2u, Quad::_dimension);
+	ASSERT_EQ(4u, Quad::_n_all_nodes);
+	ASSERT_EQ(4u, Quad::_n_base_nodes);
+
+	std::array<Node*, 4> nodes;
+	nodes[0] = new Node(0.0, 0.0, 0.0);
+	nodes[1] = new Node(0.0, 1.0, 0.0);
+	nodes[2] = new Node(1.0, 1.0, 0.0);
+	nodes[3] = new Node(1.0, 0.0, 0.0);
+	Quad quad(nodes);
+
+	ASSERT_EQ(2u, quad.getDimension());
+	ASSERT_EQ(4u, quad.getNNodes(true));
+	ASSERT_EQ(4u, quad.getNNodes(false));
 }
 
