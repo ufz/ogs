773c77dd5909e28d6eb8bb3b3af71e0057108351
Dmitri Naumov <dmitri.naumov@ufz.de>
2013-09-03 16:16:25 +0200

    [NL] ShapeQuad4 correct argument names.

--- NumLib/Fem/ShapeFunction/ShapeQuad4.h
+++ NumLib/Fem/ShapeFunction/ShapeQuad4.h
@@ -37,7 +37,7 @@ public:
      * @param [out] N   a vector of calculated shape function.
      */
     template <class T_X, class T_N>
-    static void computeShapeFunction(const T_X &r, T_N &N4);
+    static void computeShapeFunction(const T_X &r, T_N &N);
 
     /**
      * Evaluate derivatives of the shape function at the given point
@@ -46,7 +46,7 @@ public:
      * @param [out] dN  a matrix of the derivatives
      */
     template <class T_X, class T_N>
-    static void computeGradShapeFunction(const T_X &r, T_N &dN8);
+    static void computeGradShapeFunction(const T_X &r, T_N &dN);
 };
 
 }
