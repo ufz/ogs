526123473118eb30aa680c50d32b606d1172ab9e
Thomas Fischer <thomas.fischer@ufz.de>
2013-09-05 13:53:52 +0200

    Using TemplateWeightedPoint in class IntegrationGaussRegular.

--- Tests/NumLib/TestFemIntegration.cpp
+++ Tests/NumLib/TestFemIntegration.cpp
@@ -11,7 +11,6 @@
 
 #include <gtest/gtest.h>
 
-#include <tuple>
 #include <limits>
 
 #include "NumLib/Fem/Integration/IntegrationGaussRegular.h"
@@ -46,60 +45,62 @@ TEST(NumLib, FemIntegrationGaussRegular)
 	// dim = 1
 	// weight
 	ASSERT_NEAR(1.0,
-			IntegrationGaussRegular<1>::getWeightedPoint(nGaussLevel, 0)[1], eps);
+			IntegrationGaussRegular<1>::getWeightedPoint(nGaussLevel, 0).getWeight(), eps);
 	// pos
 	ASSERT_NEAR(0.577350269189626,
 			IntegrationGaussRegular<1>::getWeightedPoint(nGaussLevel, 0)[0], eps);
 	// weight
 	ASSERT_NEAR(1.0,
-			IntegrationGaussRegular<1>::getWeightedPoint(nGaussLevel, 1)[1], eps);
+			IntegrationGaussRegular<1>::getWeightedPoint(nGaussLevel, 1).getWeight(), eps);
 	// pos
 	ASSERT_NEAR(-0.577350269189626,
 			IntegrationGaussRegular<1>::getWeightedPoint(nGaussLevel, 1)[0], eps);
+
 	// dim = 2
 	ASSERT_NEAR(1.0,
-			IntegrationGaussRegular<2>::getWeightedPoint(nGaussLevel, 0)[2], eps);
+			IntegrationGaussRegular<2>::getWeightedPoint(nGaussLevel, 0).getWeight(), eps);
 	ASSERT_NEAR(0.577350269189626,
 			IntegrationGaussRegular<2>::getWeightedPoint(nGaussLevel, 0)[0], eps);
 	ASSERT_NEAR(0.577350269189626,
 			IntegrationGaussRegular<2>::getWeightedPoint(nGaussLevel, 0)[1], eps);
 	ASSERT_NEAR(1.0,
-			IntegrationGaussRegular<2>::getWeightedPoint(nGaussLevel, 1)[2], eps);
+			IntegrationGaussRegular<2>::getWeightedPoint(nGaussLevel, 1).getWeight(), eps);
 	ASSERT_NEAR(0.577350269189626,
 			IntegrationGaussRegular<2>::getWeightedPoint(nGaussLevel, 1)[0], eps);
 	ASSERT_NEAR(-0.577350269189626,
 			IntegrationGaussRegular<2>::getWeightedPoint(nGaussLevel, 1)[1], eps);
-    // dim = 3
-    ASSERT_NEAR(1.0,
-    		IntegrationGaussRegular<3>::getWeightedPoint(nGaussLevel, 0)[3], eps);
-    ASSERT_NEAR(0.577350269189626,
-    		IntegrationGaussRegular<3>::getWeightedPoint(nGaussLevel, 0)[0], eps);
-    ASSERT_NEAR(0.577350269189626,
-    		IntegrationGaussRegular<3>::getWeightedPoint(nGaussLevel, 0)[1], eps);
-    ASSERT_NEAR(0.577350269189626,
-    	IntegrationGaussRegular<3>::getWeightedPoint(nGaussLevel, 0)[2], eps);
-    ASSERT_NEAR(1.0,
-    		IntegrationGaussRegular<3>::getWeightedPoint(nGaussLevel, 1)[3], eps);
-    ASSERT_NEAR(0.577350269189626,
-    		IntegrationGaussRegular<3>::getWeightedPoint(nGaussLevel, 1)[0], eps);
-    ASSERT_NEAR(0.577350269189626,
-    		IntegrationGaussRegular<3>::getWeightedPoint(nGaussLevel, 1)[1], eps);
-    ASSERT_NEAR(-0.577350269189626,
-    		IntegrationGaussRegular<3>::getWeightedPoint(nGaussLevel, 1)[2], eps);
 
-    // check other member functions
-    IntegrationGaussRegular<1> q1;
-    ASSERT_EQ(2u, q1.getSamplingLevel());
-    ASSERT_EQ(2u, q1.getNPoints());
-    q1.setSamplingLevel(3u);
-    ASSERT_EQ(3u, q1.getSamplingLevel());
-    ASSERT_EQ(3u, q1.getNPoints());
-    IntegrationGaussRegular<2> q2;
-    ASSERT_EQ(2u, q2.getSamplingLevel());
-    ASSERT_EQ(4u, q2.getNPoints());
-    IntegrationGaussRegular<3> q3;
-    ASSERT_EQ(2u, q3.getSamplingLevel());
-    ASSERT_EQ(8u, q3.getNPoints());
+	// dim = 3
+	ASSERT_NEAR(1.0,
+			IntegrationGaussRegular<3>::getWeightedPoint(nGaussLevel, 0).getWeight(), eps);
+	ASSERT_NEAR(0.577350269189626,
+			IntegrationGaussRegular<3>::getWeightedPoint(nGaussLevel, 0)[0], eps);
+	ASSERT_NEAR(0.577350269189626,
+			IntegrationGaussRegular<3>::getWeightedPoint(nGaussLevel, 0)[1], eps);
+	ASSERT_NEAR(0.577350269189626,
+			IntegrationGaussRegular<3>::getWeightedPoint(nGaussLevel, 0)[2], eps);
+	ASSERT_NEAR(1.0,
+			IntegrationGaussRegular<3>::getWeightedPoint(nGaussLevel, 1).getWeight(), eps);
+	ASSERT_NEAR(0.577350269189626,
+			IntegrationGaussRegular<3>::getWeightedPoint(nGaussLevel, 1)[0], eps);
+	ASSERT_NEAR(0.577350269189626,
+			IntegrationGaussRegular<3>::getWeightedPoint(nGaussLevel, 1)[1], eps);
+	ASSERT_NEAR(-0.577350269189626,
+			IntegrationGaussRegular<3>::getWeightedPoint(nGaussLevel, 1)[2], eps);
+
+	// check other member functions
+	IntegrationGaussRegular<1> q1;
+	ASSERT_EQ(2u, q1.getSamplingLevel());
+	ASSERT_EQ(2u, q1.getNPoints());
+	q1.setSamplingLevel(3u);
+	ASSERT_EQ(3u, q1.getSamplingLevel());
+	ASSERT_EQ(3u, q1.getNPoints());
+	IntegrationGaussRegular<2> q2;
+	ASSERT_EQ(2u, q2.getSamplingLevel());
+	ASSERT_EQ(4u, q2.getNPoints());
+	IntegrationGaussRegular<3> q3;
+	ASSERT_EQ(2u, q3.getSamplingLevel());
+	ASSERT_EQ(8u, q3.getNPoints());
 }
 
 
