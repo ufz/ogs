443a19970683659166f5faaaf83d496f88f94106
Thomas Fischer <thomas.fischer@ufz.de>
2013-10-14 10:16:58 +0200

    Using GEOObjects instances directly instead of always fetching it from class ProjectData.

--- FileIO/XmlIO/Qt/XmlGmlInterface.cpp
+++ FileIO/XmlIO/Qt/XmlGmlInterface.cpp
@@ -30,6 +30,7 @@ XmlGmlInterface::XmlGmlInterface(GeoLib::GEOObjects& geo_objs, const std::string
 
 int XmlGmlInterface::readFile(const QString &fileName)
 {
+	GeoLib::GEOObjects* geoObjects = _project->getGEOObjects();
 	std::string gliName("[NN]");
 
 	QFile* file = new QFile(fileName);
