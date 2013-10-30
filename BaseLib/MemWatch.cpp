3be6201f08d99759c40c8fea36bb418d3332de33
Lars Bilke <lars.bilke@ufz.de>
2013-10-30 15:03:46 +0100

    Fixed a clang compile error.

--- BaseLib/MemWatch.cpp
+++ BaseLib/MemWatch.cpp
@@ -33,7 +33,8 @@ unsigned MemWatch::updateMemUsage ()
         unsigned pages;
 
         std::ifstream in (fname.c_str(), std::ios::in);
-        if (in == NULL) {
+        if (!in.is_open())
+        {
             perror( "open" );
             return 1;
         }
