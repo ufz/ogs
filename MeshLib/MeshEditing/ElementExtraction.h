6c3da6a2c6d4b45556a8d43cb9ca81bdf7065893
Karsten Rink <karsten.rink@ufz.de>
2013-11-12 12:41:12 +0100

    fixed issue with element extraction when removal-vector was empty

--- MeshLib/MeshEditing/ElementExtraction.h
+++ MeshLib/MeshEditing/ElementExtraction.h
@@ -33,8 +33,11 @@ public:
 
 	~ElementExtraction();
 
+	/// The error code determined during element extraction (0 = no errors)
+	unsigned getErrorCode() { return _error_code; };
+
 	/// Removes all mesh elements marked by search-methods.
-	MeshLib::Mesh* removeMeshElements(const std::string &new_mesh_name) const;
+	MeshLib::Mesh* removeMeshElements(const std::string &new_mesh_name);
 	
 	/// Marks all elements with the given Material ID.
 	void searchByMaterialID(unsigned matID);
@@ -60,8 +63,12 @@ private:
 	void copyNodesElements(const std::vector<MeshLib::Node*> &src_nodes, const std::vector<MeshLib::Element*> &src_elems,
 						   std::vector<MeshLib::Node*> &dst_nodes, std::vector<MeshLib::Element*> &dst_elems) const;
 
+	/// The mesh from which elements should be removed.
 	const MeshLib::Mesh &_mesh;
+	/// The vector of element indices that should be removed.
 	std::vector<std::size_t> _marked_elements;
+	/// An error code during mesh element extraction for checking the result from outside (0 = no errors).
+	unsigned _error_code;
 };
 
 } // end namespace MeshLib
