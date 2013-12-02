aa01357e4358f0d80c5fc47c3cb9c002da50bfcc
Thomas Fischer <thomas.fischer@ufz.de>
2013-12-02 10:28:08 +0100

    [GeoLib] Activate Polyline test.

--- Tests/GeoLib/TestPolyline.cpp
+++ Tests/GeoLib/TestPolyline.cpp
@@ -19,7 +19,6 @@ using namespace GeoLib;
 
 TEST(GeoLib, PolylineTest)
 {
-/*
 	std::vector<Point*> ply_pnts;
 	Polyline ply(ply_pnts);
 
@@ -106,5 +105,4 @@ TEST(GeoLib, PolylineTest)
 
 	for (std::size_t k(0); k < ply_pnts.size(); ++k)
 		delete ply_pnts[k];
-*/
 }
