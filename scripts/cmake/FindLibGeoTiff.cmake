afa9ec3c2631ce2131156dc8ea462701ded59774
Lars Bilke <lars.bilke@ufz.de>
2014-01-27 09:30:24 +0100

    geotiff is now correctly found on Windows.

--- scripts/cmake/FindLibGeoTiff.cmake
+++ scripts/cmake/FindLibGeoTiff.cmake
@@ -7,8 +7,10 @@
 #  GEOTIFF_LIBRARIES
 
 find_path( libgeotiff_INCLUDE_DIR geotiff.h)
-
 find_library(libgeotiff_LIBRARY geotiff)
+
+find_path( xtiff_INCLUDE_DIR xtiffio.h)
+find_library(xtiff_LIBRARY xtiff)
 
 ###
 # Dependencies
@@ -17,9 +19,8 @@ set(_deps_libs)
 set(_deps_includes)
 set(_deps_check)
 
-if(NOT MSVC)
-	find_package(TIFF)
-endif()
+find_package(TIFF)
+
 list(APPEND _deps_libs ${TIFF_LIBRARIES})
 list(APPEND _deps_includes ${TIFF_INCLUDE_DIRS})
 list(APPEND _deps_check TIFF_FOUND)
@@ -29,10 +30,12 @@ find_package_handle_standard_args(GEOTIFF
 	REQUIRED_VARS
 	libgeotiff_LIBRARY
 	libgeotiff_INCLUDE_DIR
+	xtiff_LIBRARY
+	xtiff_INCLUDE_DIR
 	${_deps_check}
 )
 
 if(GEOTIFF_FOUND)
-	set(GEOTIFF_INCLUDE_DIRS ${libgeotiff_INCLUDE_DIR} ${_deps_includes})
-	set(GEOTIFF_LIBRARIES ${libgeotiff_LIBRARY} ${_deps_libs})
+	set(GEOTIFF_INCLUDE_DIRS ${libgeotiff_INCLUDE_DIR} ${xtiff_INCLUDE_DIR} ${_deps_includes})
+	set(GEOTIFF_LIBRARIES ${libgeotiff_LIBRARY} ${xtiff_LIBRARY} ${_deps_libs})
 endif()
