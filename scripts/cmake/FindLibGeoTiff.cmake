6fc84c065e2cddb86daf88a98c1120219b7f9a4b
Lars Bilke <lars.bilke@ufz.de>
2014-01-27 12:18:28 +0100

    xtiff seems to be a separate library on Windows only.

--- scripts/cmake/FindLibGeoTiff.cmake
+++ scripts/cmake/FindLibGeoTiff.cmake
@@ -6,18 +6,21 @@
 #  GEOTIFF_INCLUDE_DIRS
 #  GEOTIFF_LIBRARIES
 
-find_path( libgeotiff_INCLUDE_DIR geotiff.h)
-find_library(libgeotiff_LIBRARY geotiff)
-
-find_path( xtiff_INCLUDE_DIR xtiffio.h)
-find_library(xtiff_LIBRARY xtiff)
-
 ###
 # Dependencies
 ###
 set(_deps_libs)
 set(_deps_includes)
 set(_deps_check)
+
+find_path( libgeotiff_INCLUDE_DIR geotiff.h)
+find_library(libgeotiff_LIBRARY geotiff)
+
+find_path( xtiff_INCLUDE_DIR xtiffio.h)
+if(MSVC)
+	find_library(xtiff_LIBRARY xtiff)
+	list(APPEND _deps_libs ${xtiff_LIBRARY})
+endif()
 
 find_package(TIFF)
 
@@ -30,12 +33,11 @@ find_package_handle_standard_args(GEOTIFF
 	REQUIRED_VARS
 	libgeotiff_LIBRARY
 	libgeotiff_INCLUDE_DIR
-	xtiff_LIBRARY
 	xtiff_INCLUDE_DIR
 	${_deps_check}
 )
 
 if(GEOTIFF_FOUND)
 	set(GEOTIFF_INCLUDE_DIRS ${libgeotiff_INCLUDE_DIR} ${xtiff_INCLUDE_DIR} ${_deps_includes})
-	set(GEOTIFF_LIBRARIES ${libgeotiff_LIBRARY} ${xtiff_LIBRARY} ${_deps_libs})
+	set(GEOTIFF_LIBRARIES ${libgeotiff_LIBRARY} ${_deps_libs})
 endif()
