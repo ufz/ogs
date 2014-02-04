744d0e6b75c4beaf2bd86b605d53a7a12f952a61
Karsten Rink <karsten.rink@ufz.de>
2014-02-04 17:30:59 +0100

    added functionality for managing the last saved file path

---
+++ Gui/Base/LastSavedFileDirectory.h
@@ -0,0 +1,45 @@
+/**
+ * \file   LastSavedFileDirectory.h
+ * \author Karsten Rink
+ * \date   2014-02-04
+ * \brief  Manages the last directory used for saving a file
+ *
+ * \copyright
+ * Copyright (c) 2013, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#ifndef LASTUSEDSAVEDFILEDIRECTORY_H
+#define LASTUSEDSAVEDFILEDIRECTORY_H
+
+#include <QDir>
+#include <QFileInfo>
+#include <QSettings>
+#include <QString>
+
+class LastSavedFileDirectory
+{
+public:
+	/// Returns the directory last used for saving a file
+	static const QString getDir()
+	{
+		QSettings settings;
+		return settings.value("lastSavedFileDirectory").toString();
+	}
+
+	/// Sets the directory last used for saving a file
+	static void setDir(const QString &path)
+	{
+		QFileInfo fi(path);
+		QDir dir = QDir(fi.absolutePath());
+		QSettings settings;
+		settings.setValue("lastSavedFileDirectory", dir.absolutePath());
+	}
+
+};
+
+#endif //LASTUSEDSAVEDFILEDIRECTORY_H
+
