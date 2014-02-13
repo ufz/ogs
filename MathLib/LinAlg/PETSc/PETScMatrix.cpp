596b3ee229e27de38df4154e4e6af96fd7aef607
Wenqing Wang <wenqing.wang@ufz.de>
2014-02-13 16:30:51 +0100

    Added the files for the interface for the PETSc routines

---
+++ MathLib/LinAlg/PETSc/PETScMatrix.cpp
@@ -0,0 +1,99 @@
+/*!
+   \file  PETScMatrix.cpp
+   \brief Definition of member functions of class PETScMatrix, which provides an interface to
+          PETSc matrix routines.
+
+   \author Wenqing Wang
+   \date Nov 2011 - Sep 2013
+
+   \copyright
+   Copyright (c) 2013, OpenGeoSys Community (http://www.opengeosys.org)
+               Distributed under a Modified BSD License.
+               See accompanying file LICENSE.txt or
+               http://www.opengeosys.org/project/license
+*/
+
+#include "PETScMatrix.h"
+
+namespace MathLib
+{
+
+PETScMatrix::PETScMatrix (const PetscInt size, const PETScMatrixOption mat_opt)
+{
+    _size = size;
+    _loc_rows = PETSC_DECIDE;
+    _loc_cols = mat_opt._local_cols;
+    _is_symmetric = mat_opt._is_symmetric;
+
+    if(mat_opt._is_size_local_rows)
+    {
+        _size = PETSC_DECIDE;
+        _loc_rows = size;
+    }
+
+    MatCreate(PETSC_COMM_WORLD, &_A);
+    MatSetSizes(_A, _loc_rows, _loc_cols, _size, _size);
+
+    MatSetFromOptions(_A);
+
+    // for a dense matrix: MatSeqAIJSetPreallocation(_A, d_nz, PETSC_NULL);
+    MatMPIAIJSetPreallocation(_A, mat_opt._d_nz, PETSC_NULL, mat_opt._o_nz, PETSC_NULL);
+
+    MatGetOwnershipRange(_A, &_start_rank, &_end_rank);
+    MatGetSize(_A, &_size,  PETSC_NULL);
+    MatGetLocalSize(_A, &_loc_rows, &_loc_cols);
+}
+
+//-----------------------------------------------------------------
+void PETScMatrix::setRowsColumnsZero(std::vector<PetscInt> const& row_pos)
+{
+    // Each process indicates only rows it owns that are to be zeroed.
+    // If it is called, it must be called by all ranks of cores.
+
+    PetscScalar one = 1.0;
+    PetscInt nrows = static_cast<PetscInt> (row_pos.size());
+
+    if(isSymmetric())
+    {
+        if(nrows>0)
+            MatZeroRowsColumns(_A, nrows, &row_pos[0], one, PETSC_NULL, PETSC_NULL);
+        else
+            MatZeroRowsColumns(_A, 0, PETSC_NULL, one, PETSC_NULL, PETSC_NULL);
+    }
+    else
+    {
+        if(nrows>0)
+            MatZeroRows(_A, nrows, &row_pos[0], one, PETSC_NULL, PETSC_NULL);
+        else
+            MatZeroRows(_A, 0, PETSC_NULL, one, PETSC_NULL, PETSC_NULL);
+    }
+}
+
+void PETScMatrix::viewer(const std::string &file_name, const PetscViewerFormat vw_format)
+{
+    PetscViewer viewer;
+    PetscViewerASCIIOpen(PETSC_COMM_WORLD, file_name.c_str(), &viewer);
+    PetscViewerPushFormat(viewer, vw_format);
+
+    finalizeAssembly();
+
+    PetscObjectSetName((PetscObject)_A,"Stiffness_matrix");
+    MatView(_A,viewer);
+
+#define  nEXIT_TEST
+#ifdef EXIT_TEST
+    MatDestroy(&_A);
+    PetscFinalize();
+    exit(0);
+#endif
+
+}
+
+bool finalizeMatrixAssembly(PETScMatrix &mat, const MatAssemblyType asm_type)
+{
+    mat.finalizeAssembly(asm_type);
+    return true;
+}
+
+} //end of namespace
+
---
+++ MathLib/LinAlg/PETSc/PETScMatrixOption.h
@@ -0,0 +1,62 @@
+/*!
+   \file  PETScMatrixOption.h
+   \brief Define Data for the configuration of PETSc matrix and linear solver.
+
+   \author Wenqing Wang
+   \date 02-2014
+
+   \copyright
+    Copyright (c) 2013, OpenGeoSys Community (http://www.opengeosys.org)
+               Distributed under a Modified BSD License.
+               See accompanying file LICENSE.txt or
+               http://www.opengeosys.org/project/license
+*/
+#ifndef PETSCMATRIXOPTION_H_
+#define PETSCMATRIXOPTION_H_
+
+#include "petscmat.h"
+#include "petscksp.h"
+
+namespace MathLib
+{
+/*!
+   \struct  PETScMatrixOption
+   \brief This a struct data containing the configuration information to create a PETSc type matrix
+*/
+struct PETScMatrixOption
+{
+    PETScMatrixOption() :  _is_symmetric(false), _is_size_local_rows(false),
+        _local_cols(PETSC_DECIDE), _d_nz(10), _o_nz(10)
+    { }
+
+    /// Flag for symmetric or unsymmetric matrix. The default is false.
+    bool _is_symmetric;
+
+    /*!
+     \brief Flag for the type of the first argument of
+            the constructor of class PETScMatrix, size.
+            true:  the size is the number of local rows,
+            false: the size is the number of global rows.
+           The default is false.
+     */
+    bool _is_size_local_rows;
+
+    /// Number of local columns. The default is PETSC_DECIDE.
+    PetscInt _local_cols;
+
+    /*!
+     \brief Nnumber of nonzeros per row in DIAGONAL portion of local submatrix
+           (same value is used for all local rows), the default is 10
+    */
+    PetscInt _d_nz;
+
+    /*!
+     \brief Number of nonzeros per row in the OFF-DIAGONAL portion of local submatrix
+            (same value is used for all local rows), the default is 10
+    */
+    PetscInt _o_nz;
+};
+
+} // end namespace
+#endif
+
