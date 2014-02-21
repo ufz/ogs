cb0d7267c8c5a255cfe0e9b15b9d86de95b597c8
Wenqing Wang <wenqing.wang@ufz.de>
2014-02-21 11:25:25 +0100

    Modify testrunner for PETSc tests. Add PETSc tests.

--- Tests/testrunner.cpp
+++ Tests/testrunner.cpp
@@ -16,9 +16,15 @@
 #include "Configure.h"
 #include "gtest/gtest.h"
 #include "logog/include/logog.hpp"
+
 #ifdef USE_LIS
 #include "lis.h"
 #endif
+
+#ifdef USE_PETSC
+#include "petscksp.h"
+#endif
+
 #include "BaseLib/TemplateLogogFormatterSuppressedGCC.h"
 #ifdef QT4_FOUND
 #include <QApplication>
@@ -37,7 +43,13 @@ int main(int argc, char* argv[])
         BaseLib::TemplateLogogFormatterSuppressedGCC<TOPIC_LEVEL_FLAG | TOPIC_FILE_NAME_FLAG | TOPIC_LINE_NUMBER_FLAG> custom_format;
         out.SetFormatter(custom_format);
 
-        try {
+#ifdef USE_PETSC
+        char help[] = "ogs6 with PETSc \n";
+        PetscInitialize(&argc,&argv,(char *)0,help);
+#endif
+
+        try
+        {
             // initialize libraries which will be used while testing
 #ifdef USE_LIS
             lis_initialize(&argc, &argv);
@@ -45,17 +57,28 @@ int main(int argc, char* argv[])
             // start google test
             testing::InitGoogleTest ( &argc, argv );
             ret = RUN_ALL_TESTS();
-        } catch (char* e) {
+        }
+        catch (char* e)
+        {
             ERR(e);
-        } catch (std::exception& e) {
+        }
+        catch (std::exception& e)
+        {
             ERR(e.what());
-        } catch (...) {
+        }
+        catch (...)
+        {
             ERR("Unknown exception occurred!");
         }
         // finalize libraries
 #ifdef USE_LIS
         lis_finalize();
 #endif
+
+#ifdef USE_PETSC
+        PetscFinalize();
+#endif
+
     } // make sure no logog objects exist when LOGOG_SHUTDOWN() is called.
     LOGOG_SHUTDOWN();
 
