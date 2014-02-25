eec90df935991f5945fe7c5d2dadf2edef3f7110
Thomas Fischer <thomas.fischer@ufz.de>
2014-02-25 09:47:24 +0100

    Using TCLAP for command line parsing. Implemented opportunities to write old and new OGS mesh file format.

--- Utils/FileConverter/GMSH2OGS.cpp
+++ Utils/FileConverter/GMSH2OGS.cpp
@@ -15,15 +15,24 @@
 // STL
 #include <string>
 
+// ThirdParty
+#include "tclap/CmdLine.h"
+
 // ThirdParty/logog
 #include "logog/include/logog.hpp"
 
 // BaseLib
+#include "FileTools.h"
+#include "RunTime.h"
+#ifndef WIN32
+#include "MemWatch.h"
+#endif
 #include "LogogSimpleFormatter.h"
 
 // FileIO
 #include "Legacy/MeshIO.h"
 #include "GMSHInterface.h"
+#include "XmlIO/Boost/BoostVtuInterface.h"
 
 // MeshLib
 #include "Mesh.h"
@@ -35,39 +44,60 @@ int main (int argc, char* argv[])
 	BaseLib::LogogSimpleFormatter *custom_format (new BaseLib::LogogSimpleFormatter);
 	logog_cout->SetFormatter(*custom_format);
 
-	if (argc < 5)
-	{
-		std::cout << "Usage: " << argv[0] <<
-		" --mesh-in meshfile --mesh-out meshfile" <<  std::endl;
-		return -1;
-	}
+	TCLAP::CmdLine cmd("Converting meshes in gmsh file format to a vtk unstructured grid file (new OGS file format) or to the old OGS file format - see options.", ' ', "0.1");
+
+	TCLAP::ValueArg<std::string> ogs_mesh_arg(
+		"o",
+		"mesh-out",
+		"filename for output mesh (if extension is msh, old OGS fileformat is written)",
+		true,
+		"",
+		"filename as string");
+	cmd.add(ogs_mesh_arg);
+
+	TCLAP::ValueArg<std::string> gmsh_mesh_arg(
+		"m",
+		"mesh-in",
+		"filename for file containing the gmsh mesh",
+		true,
+		"",
+		"filename as string");
+	cmd.add(gmsh_mesh_arg);
+
+	cmd.parse(argc, argv);
 
 	// *** read mesh
-	std::string tmp (argv[1]);
-	if (tmp.find ("--mesh-in") == std::string::npos)
-	{
-		std::cout << "could not find option --mesh-in" << std::endl;
-		return -1;
+	INFO("Reading %s.", gmsh_mesh_arg.getValue().c_str());
+#ifndef WIN32
+	BaseLib::MemWatch mem_watch;
+	unsigned long mem_without_mesh (mem_watch.getVirtMemUsage());
+#endif
+	BaseLib::RunTime run_time;
+	run_time.start();
+	MeshLib::Mesh* mesh(FileIO::GMSHInterface::readGMSHMesh(gmsh_mesh_arg.getValue()));
+#ifndef WIN32
+	unsigned long mem_with_mesh (mem_watch.getVirtMemUsage());
+	INFO("Mem for mesh: %i MB", (mem_with_mesh - mem_without_mesh)/(1024*1024));
+#endif
+	run_time.stop();
+	INFO("Time for reading: %f seconds.", run_time.elapsed());
+	INFO("Read %d nodes and %d elements.", mesh->getNNodes(), mesh->getNElements());
+
+	// *** write mesh in new format
+	std::string ogs_mesh_fname(ogs_mesh_arg.getValue());
+	INFO("Writing %s.", ogs_mesh_fname.c_str());
+
+	if (BaseLib::getFileExtension(ogs_mesh_fname).compare("msh") == 0) {
+		FileIO::Legacy::MeshIO mesh_io;
+		mesh_io.setMesh(mesh);
+		mesh_io.writeToFile(ogs_mesh_fname);
+	} else {
+		FileIO::BoostVtuInterface mesh_io;
+		mesh_io.setMesh(mesh);
+		mesh_io.writeToFile(ogs_mesh_fname);
 	}
-
-	tmp = argv[2];
-	MeshLib::Mesh* mesh (FileIO::GMSHInterface::readGMSHMesh(tmp));
-
-	// *** create new mesh
-	tmp = argv[3];
-	if (tmp.find ("--mesh-out") == std::string::npos)
-	{
-		std::cout << "could not find option --mesh-out" << std::endl;
-		return -1;
-	}
-
-	tmp = argv[4];
-	std::cout << "writing mesh to file " << tmp << " ... " << std::flush;
-	FileIO::Legacy::MeshIO mesh_io;
-	mesh_io.setMesh(mesh);
-	mesh_io.writeToFile (tmp);
-	std::cout << "ok" << std::endl;
+	INFO("\tDone.");
 
 	delete mesh;
-
 }
+
