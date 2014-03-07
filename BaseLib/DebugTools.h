e740ea4cd20247a8cfac663a789e903c28443502
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2014-03-07 11:33:08 +0100

    add a helper function to output vector entries

---
+++ BaseLib/DebugTools.h
@@ -0,0 +1,29 @@
+/**
+ * \author Norihiro Watanabe
+ * \date   2014-03-07
+ * \brief  Helper tools for debugging
+ *
+ * \copyright
+ * Copyright (c) 2013, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#ifndef DEBUGTOOLS_H
+#define DEBUGTOOLS_H
+
+#include <ostream>
+#include <algorithm>
+#include <vector>
+
+template<typename T>
+std::ostream &operator <<(std::ostream &os, const std::vector<T> &v) {
+    std::copy(v.begin(), v.end(), std::ostream_iterator<T>(os, " "));
+    os << "\n";
+    return os;
+}
+
+#endif //DEBUGTOOLS_H
+
