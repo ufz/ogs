261c3742fe71eca9cfc9f06679b14a44fc8314e1
Thomas Fischer <thomas.fischer@ufz.de>
2014-03-11 09:37:32 +0100

    Restrict operator== to TemplatePoint objects.

--- MathLib/MathTools.h
+++ MathLib/MathTools.h
@@ -114,11 +114,11 @@ typename POINT_T::FP_T sqrDist(POINT_T const& p0, POINT_T const& p1)
 	return MathLib::scalarProduct<typename POINT_T::FP_T,3>(v,v);
 }
 
-template <typename POINT_T>
-bool operator==(POINT_T const& a, POINT_T const& b)
+template <typename T, std::size_t DIM>
+bool operator==(TemplatePoint<T,DIM> const& a, TemplatePoint<T,DIM> const& b)
 {
-	typename POINT_T::FP_T const sqr_dist(sqrDist(a,b));
-	return (sqr_dist < pow(std::numeric_limits<typename POINT_T::FP_T>::epsilon(),2));
+	T const sqr_dist(sqrDist(a,b));
+	return (sqr_dist < pow(std::numeric_limits<T>::epsilon(),2));
 }
 
 /** squared dist between double arrays p0 and p1 (size of arrays is 3) */
