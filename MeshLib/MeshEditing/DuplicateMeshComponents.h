6f90543b10b86cc63c754eacdffc642ef1335f13
Dmitri Naumov <dmitri.naumov@ufz.de>
2014-03-26 14:37:58 +0100

    Replace copyX by a copyElement<X> template.

--- MeshLib/MeshEditing/DuplicateMeshComponents.h
+++ MeshLib/MeshEditing/DuplicateMeshComponents.h
@@ -39,19 +39,8 @@ namespace MeshLib
                                   const std::vector<MeshLib::Node*> &nodes);
 
 	/// Creates a new line element identical with "line" but using the new nodes vector.
-	MeshLib::Element* copyLine(MeshLib::Element const*const line, const std::vector<MeshLib::Node*> &nodes);
-	/// Creates a new triangle element identical with "tri" but using the new nodes vector.
-	MeshLib::Element* copyTri(MeshLib::Element const*const tri, const std::vector<MeshLib::Node*> &nodes);
-	/// Creates a new quad element identical with "quad" but using the new nodes vector.
-	MeshLib::Element* copyQuad(MeshLib::Element const*const quad, const std::vector<MeshLib::Node*> &nodes);
-	/// Creates a new tetrahedron element identical with "tet" but using the new nodes vector.
-	MeshLib::Element* copyTet(MeshLib::Element const*const tet, const std::vector<MeshLib::Node*> &nodes);
-	/// Creates a new hexahedron element identical with "hex" but using the new nodes vector.
-	MeshLib::Element* copyHex(MeshLib::Element const*const hex, const std::vector<MeshLib::Node*> &nodes);
-	/// Creates a new pyramid element identical with "pyramid" but using the new nodes vector.
-	MeshLib::Element* copyPyramid(MeshLib::Element const*const pyramid, const std::vector<MeshLib::Node*> &nodes);
-	/// Creates a new prism element identical with "prism" but using the new nodes vector.
-	MeshLib::Element* copyPrism(MeshLib::Element const*const prism, const std::vector<MeshLib::Node*> &nodes);
+	template <typename E>
+	MeshLib::Element* copyElement(MeshLib::Element const*const element, const std::vector<MeshLib::Node*> &nodes);
 
 } // end namespace MeshLib
 
