44ef8bed04b58f524fc3a85e486414f0c67286ae
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2014-06-05 18:38:07 +0200

    document the function

--- MeshGeoToolsLib/AppendLinesAlongPolyline.h
+++ MeshGeoToolsLib/AppendLinesAlongPolyline.h
@@ -19,7 +19,22 @@ class Mesh;
 
 namespace MeshGeoToolsLib
 {
+
+/**
+ * Add line elements to a copy of a given mesh
+ *
+ * The function creates line elements from nodes located along user-provided polylines.
+ * New elements will have a distinct material ID for each polyline.
+ *
+ * \remark The function allows creation of duplicated line elements.
+ * \remark Line elements may not be placed along edges of existing elements.
+ *
+ * @param mesh      original mesh
+ * @param ply_vec   polyline vector whose nodes are used to create line elements
+ * @return a new mesh which is copied from a given mesh and additionally includes line elements
+ */
 MeshLib::Mesh* appendLinesAlongPolylines(const MeshLib::Mesh &mesh, const GeoLib::PolylineVec &ply_vec);
+
 }
 
 #endif
