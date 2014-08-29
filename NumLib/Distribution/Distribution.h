d5d4f4f2c2042c1617183d36ecd20158283eb5d1
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2014-08-29 17:23:08 +0200

    add Distribution function

---
+++ NumLib/Distribution/Distribution.h
@@ -0,0 +1,42 @@
+/**
+ * \copyright
+ * Copyright (c) 2013, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+
+#ifndef DISTRIBUTION_H_
+#define DISTRIBUTION_H_
+
+#include <vector>
+
+namespace MeshLib
+{
+class Mesh;
+}
+
+namespace GeoLib
+{
+class GeoObject;
+}
+
+namespace NumLib
+{
+class ISpatialFunction;
+
+/**
+ * Generate distributed node values from a given function
+ *
+ * @param func            a spatial function object
+ * @param msh             a mesh object
+ * @param vec_node_ids    a vector of mesh node ids where the function is evaluated
+ * @return a vector of nodal values
+ */
+std::vector<double> generateNodeValueDistribution(const NumLib::ISpatialFunction &func, const MeshLib::Mesh &msh, const std::vector<std::size_t> &vec_node_ids);
+
+} // NumLib
+
+#endif // DISTRIBUTION_H_
