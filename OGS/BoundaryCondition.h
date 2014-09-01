b665519054b4ca7c3e70db6d64d6b936b598f4ef
Dmitri Naumov <dmitri.naumov@ufz.de>
2014-09-01 20:54:08 +0200

    Implementation of IC/BC and ProcessVariable.

---
+++ OGS/BoundaryCondition.h
@@ -0,0 +1,61 @@
+/**
+ * \copyright
+ * Copyright (c) 2014, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#ifndef OGS_BOUNDARY_CONDITION_H_
+#define OGS_BOUNDARY_CONDITION_H_
+
+#include <boost/property_tree/ptree.hpp>
+#include "logog/include/logog.hpp"
+
+//#include "GeoLib/GEOObjects.h"
+
+namespace GeoLib
+{
+    class GeoObject;
+}
+
+namespace OGS
+{
+
+class BoundaryCondition
+{
+public:
+    BoundaryCondition(GeoLib::GeoObject const* const geometry)
+        : _geometry(geometry)
+    { }
+
+    virtual ~BoundaryCondition() = default;
+
+private:
+    GeoLib::GeoObject const* const _geometry;
+};
+
+
+class DirichletBoundaryCondition : public BoundaryCondition
+{
+    using ConfigTree = boost::property_tree::ptree;
+public:
+    DirichletBoundaryCondition(GeoLib::GeoObject const* const geometry,
+            ConfigTree const& config)
+        : BoundaryCondition(geometry)
+    {
+        DBUG("Constructing Dirichlet boundary condition");
+
+        _value = config.get<double>("value", 0);
+        DBUG("Read value %g", _value);
+    }
+
+private:
+    double _value;
+};
+
+
+}   // namespace OGS
+
+#endif  // OGS_BOUNDARY_CONDITION_H_
