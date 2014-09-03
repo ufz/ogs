c123887268e579bb051f4ea27a841bae356299c5
Dmitrij Naumov <dmitrij@naumov.de>
2014-09-04 01:18:05 +0200

    [PL] Add createDofMap() in Process.

--- ProcessLib/Process.h
+++ ProcessLib/Process.h
@@ -10,6 +10,9 @@
 #ifndef PROCESS_LIB_PROCESS_H_
 #define PROCESS_LIB_PROCESS_H_
 
+#include "AssemblerLib/MeshComponentMap.h"
+#include "MeshLib/Elements/Element.h"
+#include "MeshLib/Node.h"
 #include "MeshLib/Mesh.h"
 
 namespace ProcessLib
@@ -25,6 +28,42 @@ public:
     virtual ~Process() = default;
 
     virtual void initialize() = 0;
+
+    // XXX const missing
+    std::vector<std::vector<std::size_t>>
+    createDofMap(AssemblerLib::MeshComponentMap& mesh_component_map) const
+    {
+        // dof_map contains for each element vector of global indices to
+        // node/element process variables.
+        std::vector<std::vector<std::size_t>> dof_map;
+
+        // XXX Shouldn't the mesh_id (and the elements) be from the MeshSubsets?
+        // Otherwise we should create the MeshComponentMap also here using the
+        // same meshes and elements.
+        std::size_t const mesh_id = _mesh.getID();
+        std::vector<MeshLib::Element*> const& elements = _mesh.getElements();
+
+        dof_map.reserve(elements.size());
+
+        // For each element find the global indices for node/element components.
+        for (MeshLib::Element* e : elements)
+        {
+            std::size_t const nnodes = e->getNNodes();
+            std::vector<MeshLib::Location> vec_items;
+            vec_items.reserve(nnodes);
+            for (std::size_t j = 0; j < nnodes; j++)
+                vec_items.emplace_back(
+                    mesh_id,
+                    MeshLib::MeshItemType::Node,
+                    e->getNode(j)->getID());
+
+            dof_map.push_back(
+                mesh_component_map.getGlobalIndices<AssemblerLib::ComponentOrder::BY_COMPONENT>(
+                    vec_items));
+        }
+
+        return dof_map;
+    }
 
 protected:
     MeshLib::Mesh const& _mesh;
