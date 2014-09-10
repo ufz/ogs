12926a82fd6806e2c97c33cfdcda1b786275b307
Wenqing Wang <wenqing.wang@ufz.de>
2014-09-10 18:04:53 +0200

    Revised MeshDDC

--- MeshLib/MeshDDC.h
+++ MeshLib/MeshDDC.h
@@ -2,11 +2,11 @@
   \file MeshDDC.h
   \author Wenqing Wang
   \date   2014.06
-  \brief  Definition of the Mesh class for a partition for parallel computing within the
+  \brief  Definition of mesh class for partitioned mesh for parallel computing within the
           framework of domain decomposition (DDC).
 
   \copyright
-  Copyright (c) 2013, OpenGeoSys Community (http://www.opengeosys.org)
+  Copyright (c) 2014, OpenGeoSys Community (http://www.opengeosys.org)
              Distributed under a Modified BSD License.
                See accompanying file LICENSE.txt or
                http://www.opengeosys.org/project/license
@@ -18,10 +18,10 @@
 
 #include <cstdlib>
 #include <string>
-#include <vector>
 
 #include "Mesh"
 
+typedef long MyInt;
 namespace MeshLib
 {
 class Node;
@@ -31,44 +31,66 @@ class Element;
 class MeshDDC : public Mesh
 {
     public:
-        /// Constructor using a mesh name and an array of nodes and elements
+        /*!
+            \brief  Constructor
+            \param name          Name assigned to the mesh.
+            \param nodes         Vector for nodes.
+            \param elements      Vector for elements.
+            \param nnodes_global Number of nodes of the whole mesh.
+                                 0: with linear elements
+                                 1: with quadratic elemens.
+            \param nnodes_active Number of active nodes of the partition.
+                                 0: with linear elements
+                                 1: with quadratic elemens.
+
+            \param axisymmetry   Indicator for axisymmetry.
+        */
         MeshDDC(const std::string &name,
                 const std::vector<Node*> &nodes,
                 const std::vector<Element*> &elements,
-                const std::vector<unsigned> g_node_ids,
-                const std::vector<unsigned*> l_act_nodes_ids);
-
-        /*!
-            Get global index of a node
-            \param sdom_node_id Subdmain node ID
-        */
-        const unsigned getGlobalNodeID(const unsigned sdom_node_id);
-
-        /*!
-            Get number of active nodes of an element
-            \param elem_id Element ID
-            \param order   Order of element, could be 0 or 1
-        */
-        const unsigned getNumberActiveNodes(const unsigned elem_id, const int order)
+                const MyInt [] nnodes_global,
+                const MyInt [] nnodes_local
+                const bool axisymmetry)
+            : Mesh(name, nodes, elements, false), _axisymmetry(axisymmetry),
+              _nnodes_global {nnodes_global[0], nnodes_global[1] },
+        _nnodes_active {nnodes_active[0], nnodes_active[1] }
         {
-            _l_act_nodes_ids[elem_id][order];
         }
 
         /*!
-            Get local indicies of active nodes
+            \brief Get the number of nodes of the whole mesh.
+            \param order The order of elements (0 or 1).
+                         Its default value is 0 for linear elements.
         */
-        const unsigned *getLocalActiveNodeIDs(const unsigned elem_id);
+        MyInt getGlobalNNodes(const int order = 0) const
         {
-            return &_l_act_nodes_ids[elem_id][2];
+            return _nnodes_global[order];
+        }
+
+        /*!
+            \brief Get the number of nodes of the whole mesh.
+            \param order The order of elements (0 or 1).
+                         Its default value is 0 for linear elements.
+        */
+        MyInt getActiveNNodes(const int order = 0) const
+        {
+            return _nnodes_active[order];
+        }
+
+        /// Get the largest ID of active nodes for higher order elements.
+        MyInt getLargestActiveNodeID() const
+        {
+            // Note: _nodes.size() should be changed once the high order element is condidered
+            // in the root class.
+            return static_cast<MyInt>( _nodes.size() ) + _nnodes_active[1] - _nnodes_active[0];
         }
 
     private:
-        /// Global indicies of nodes. the vector size is equal to the number of all nodes
-        std::vector<unsigned> _g_node_ids;
+        /// Number of nodes of the whole mesh. 0: for linear elements; 1: for quadratic elements.
+        MyInt _nnodes_global[2];
 
-        /// Local indices of active nodes of each elements.
-        /// The vector size is equal to the number of elements
-        std::vector<unsigned*> _l_act_nodes_ids;
+        /// Number of the active nodes. 0: for linear elements; 1: for quadratic elements.
+        MyInt _nnodes_active[2];
 
 };
 
