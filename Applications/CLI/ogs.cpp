6418507e499733c7b2dac3c71d6c8f018c3f58b4
Thomas Fischer <thomas.fischer@ufz.de>
2014-09-11 17:08:46 +0200

    First OGS simulation main program.

---
+++ Applications/CLI/ogs.cpp
@@ -0,0 +1,61 @@
+/**
+ * \date   2014-08-04
+ * \brief  Implementation of OpenGeoSys simulation application
+ *
+ * \copyright
+ * Copyright (c) 2012-2014, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#include <cstdlib>
+
+// ThirdParty/logog
+#include "logog/include/logog.hpp"
+
+// ThirdParty/tclap
+#include "ThirdParty/tclap/CmdLine.h"
+
+// BaseLib
+#include "BaseLib/LogogSimpleFormatter.h"
+
+// FileIO/XmlIO/Qt
+#include "FileIO/XmlIO/Qt/XmlGspInterface.h"
+
+// OGS
+
+#include "Applications/ApplicationsLib/ProjectData.h"
+
+int main(int argc, char *argv[])
+{
+	// logog
+	LOGOG_INITIALIZE();
+	BaseLib::LogogSimpleFormatter *fmt(new BaseLib::LogogSimpleFormatter);
+	logog::Cout *logog_cout(new logog::Cout);
+	logog_cout->SetFormatter(*fmt);
+
+	// Parse CLI arguments.
+	TCLAP::CmdLine cmd(
+			"OpenGeoSys simulation application", ' ', "0.1");
+	TCLAP::ValueArg<std::string> project_arg(
+		"p", // option tag
+		"project", // long option tag
+		"file name of the project", // description
+		true, // required
+		"",
+		"string"); // type of information
+	cmd.add(project_arg);
+	cmd.parse(argc, argv);
+
+	ProjectData project_data;
+	FileIO::XmlGspInterface project_reader(project_data);
+	project_reader.readFile(project_arg.getValue());
+
+	delete fmt;
+	delete logog_cout;
+	LOGOG_SHUTDOWN();
+
+	return 0;
+}
