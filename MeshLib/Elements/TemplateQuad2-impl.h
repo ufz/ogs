1966df8607a0bb8106a8b9778bade26a55cc74b6
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2014-09-12 15:24:50 +0200

    WIP quadratic quad

---
+++ MeshLib/Elements/TemplateQuad2-impl.h
@@ -0,0 +1,150 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2014, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#include <array>
+
+#include "Node.h"
+#include "Tri.h"
+
+// MathLib
+#include "MathTools.h"
+#include "AnalyticalGeometry.h"
+
+namespace MeshLib
+{
+
+template <unsigned NNODES, CellType CELLQUADTYPE>
+const unsigned TemplateQuad2<NNODES, CELLQUADTYPE>::n_all_nodes;
+
+template <unsigned NNODES, CellType CELLQUADTYPE>
+const unsigned TemplateQuad2<NNODES, CELLQUADTYPE>::n_base_nodes;
+
+template <unsigned NNODES, CellType CELLQUADTYPE>
+const unsigned TemplateQuad2<NNODES, CELLQUADTYPE>::_edge_nodes[4][3] =
+{
+	{0, 1, 4}, // Edge 0
+	{1, 2, 5}, // Edge 1
+	{2, 3, 6}, // Edge 2
+	{0, 3, 7}  // Edge 3
+};
+
+template <unsigned NNODES, CellType CELLQUADTYPE>
+TemplateQuad2<NNODES,CELLQUADTYPE>::TemplateQuad2(Node* nodes[NNODES], unsigned value, std::size_t id)
+	: Face(value, id)
+{
+	_nodes = nodes;
+
+	_neighbors = new Element*[4];
+	std::fill(_neighbors, _neighbors + 4, nullptr);
+
+	this->_area = this->computeVolume();
+}
+
+template<unsigned NNODES, CellType CELLQUADTYPE>
+TemplateQuad2<NNODES,CELLQUADTYPE>::TemplateQuad2(std::array<Node*, NNODES> const& nodes,
+                                                unsigned value, std::size_t id)
+	: Face(value, id)
+{
+	_nodes = new Node*[NNODES];
+	std::copy(nodes.begin(), nodes.end(), _nodes);
+
+	_neighbors = new Element*[4];
+	std::fill(_neighbors, _neighbors + 4, nullptr);
+
+	this->_area = this->computeVolume();
+}
+
+template <unsigned NNODES, CellType CELLQUADTYPE>
+TemplateQuad2<NNODES,CELLQUADTYPE>::TemplateQuad2(const TemplateQuad2<NNODES,CELLQUADTYPE> &quad)
+	: Face(quad.getValue(), quad.getID())
+{
+	_nodes = new Node*[NNODES];
+	for (unsigned i=0; i<NNODES; i++) {
+		_nodes[i] = quad._nodes[i];
+	}
+
+	_neighbors = new Element*[4];
+	for (unsigned i=0; i<4; i++) {
+		_neighbors[i] = quad._neighbors[i];
+	}
+
+	_area = quad.getArea();
+}
+
+template <unsigned NNODES, CellType CELLQUADTYPE>
+TemplateQuad2<NNODES,CELLQUADTYPE>::~TemplateQuad2()
+{
+}
+
+template <unsigned NNODES, CellType CELLQUADTYPE>
+double TemplateQuad2<NNODES,CELLQUADTYPE>::computeVolume()
+{
+	//TODO
+	return GeoLib::calcTriangleArea(*_nodes[0], *_nodes[1], *_nodes[2])
+         + GeoLib::calcTriangleArea(*_nodes[2], *_nodes[3], *_nodes[0]);
+}
+
+template <unsigned NNODES, CellType CELLQUADTYPE>
+bool TemplateQuad2<NNODES,CELLQUADTYPE>::isEdge(unsigned idx1, unsigned idx2) const
+{
+	for (unsigned i(0); i<4; i++)
+	{
+		if (_edge_nodes[i][0]==idx1 && _edge_nodes[i][1]==idx2) return true;
+		if (_edge_nodes[i][1]==idx1 && _edge_nodes[i][0]==idx2) return true;
+	}
+	return false;
+}
+
+template <unsigned NNODES, CellType CELLQUADTYPE>
+bool TemplateQuad2<NNODES,CELLQUADTYPE>::isPntInside(GeoLib::Point const& pnt, double eps) const
+{
+	//TODO
+	return (GeoLib::isPointInTriangle(pnt, *_nodes[0], *_nodes[1], *_nodes[2], eps) ||
+					GeoLib::isPointInTriangle(pnt, *_nodes[0], *_nodes[2], *_nodes[3], eps));
+}
+
+template <unsigned NNODES, CellType CELLQUADTYPE>
+Element* TemplateQuad2<NNODES,CELLQUADTYPE>::clone() const
+{
+	return new TemplateQuad2(*this);
+}
+
+template <unsigned NNODES, CellType CELLQUADTYPE>
+unsigned TemplateQuad2<NNODES,CELLQUADTYPE>::identifyFace(Node* nodes[3]) const
+{
+	for (unsigned i=0; i<4; i++)
+	{
+		unsigned flag(0);
+		for (unsigned j=0; j<2; j++)
+			for (unsigned k=0; k<2; k++)
+				if (_nodes[_edge_nodes[i][j]] == nodes[k])
+					flag++;
+		if (flag==2)
+			return i;
+	}
+	return std::numeric_limits<unsigned>::max();
+}
+
+template <unsigned NNODES, CellType CELLQUADTYPE>
+ElementErrorCode TemplateQuad2<NNODES,CELLQUADTYPE>::validate() const
+{
+	//TODO
+	ElementErrorCode error_code;
+	error_code[ElementErrorFlag::ZeroVolume]  = this->hasZeroVolume();
+	error_code[ElementErrorFlag::NonCoplanar] = (!GeoLib::isCoplanar(*_nodes[0], *_nodes[1], *_nodes[2], *_nodes[3]));
+	// for collapsed quads (i.e. reduced to a line) this test might result "false" as all four points are actually located on a line.
+	if (!error_code[ElementErrorFlag::ZeroVolume]) 
+		error_code[ElementErrorFlag::NonConvex]   = (!(GeoLib::dividedByPlane(*_nodes[0], *_nodes[2], *_nodes[1], *_nodes[3]) &&
+			                                           GeoLib::dividedByPlane(*_nodes[1], *_nodes[3], *_nodes[0], *_nodes[2])));
+	error_code[ElementErrorFlag::NodeOrder]  = !this->testElementNodeOrder();
+	return error_code;
+}
+
+}
+
---
+++ MeshLib/Elements/TemplateQuad2.h
@@ -0,0 +1,126 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2014, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#ifndef TEMPLATEQUAD2_H_
+#define TEMPLATEQUAD2_H_
+
+#include <array>
+#include "MeshEnums.h"
+#include "Face.h"
+
+namespace MeshLib {
+
+/**
+ * This class represents a 2d quadliteral element. The following sketch shows the node and edge numbering.
+ * @anchor QuadNodeAndEdgeNumbering
+ * @code
+ *              2
+ *        3-----------2
+ *        |           |
+ *        |           |
+ *       3|           |1
+ *        |           |
+ *        |           |
+ *        0-----------1
+ *              0
+ * @endcode
+ */
+template <unsigned NNODES, CellType CELLQUADTYPE>
+class TemplateQuad2 : public Face
+{
+public:
+	/// Constant: The number of all nodes for this element
+	static const unsigned n_all_nodes = NNODES;
+
+	/// Constant: The number of base nodes for this element
+	static const unsigned n_base_nodes = 4u;
+
+	/// Constructor with an array of mesh nodes.
+	TemplateQuad2(Node* nodes[NNODES], unsigned value = 0, std::size_t id = std::numeric_limits<std::size_t>::max());
+
+	/// Constructs an edge from array of Node pointers.
+	TemplateQuad2(std::array<Node*, NNODES> const& nodes, unsigned value = 0, std::size_t id = std::numeric_limits<std::size_t>::max());
+
+	/// Constructs a quad from NNODES of Nodes initializing Face with
+	//  value = 0.
+	TemplateQuad2(Node* n0, Node* n1, Node* n2, Node* n3, ...);
+
+	/// Copy constructor
+	TemplateQuad2(const TemplateQuad2 &quad);
+
+	/// Destructor
+	virtual ~TemplateQuad2();
+
+	/// Get the number of edges for this element.
+	unsigned getNEdges() const { return 4; };
+
+	/// Get the number of neighbors for this element.
+	unsigned getNNeighbors() const { return 4; };
+
+	/// Get the number of nodes for this element.
+	virtual unsigned getNNodes(bool all = true) const
+	{
+		return all ? n_all_nodes : n_base_nodes;
+	}
+
+	/**
+	 * Method returns the type of the element. In this case QUAD will be returned.
+	 * @return MeshElemType::QUAD
+	 */
+	virtual MeshElemType getGeomType() const { return MeshElemType::QUAD; }
+
+	/**
+	 * Get the type of the element in context of the finite element method.
+	 * @return a value of the enum CellType
+	 */
+	virtual CellType getCellType() const { return CELLQUADTYPE; }
+
+	/// Returns true if these two indeces form an edge and false otherwise
+	bool isEdge(unsigned i, unsigned j) const;
+
+	/**
+	 * Check if the 3d GeoLib::Point is inside of the quad element.
+	 * @param pnt the 3d GeoLib::Point object
+	 * @param eps tolerance for numerical algorithm used or computing the property
+	 * @return true if the point is inside the element, false otherwise
+	 */
+	virtual bool isPntInside(GeoLib::Point const& pnt, double eps = std::numeric_limits<double>::epsilon()) const;
+
+	/**
+	 * Tests if the element is geometrically valid.
+	 * @param check_zero_volume indicates if area == 0 should be checked
+	 */
+	virtual ElementErrorCode validate() const;
+
+	/**
+	 * Method clone is inherited from class Element. It makes a deep copy of the TemplateQuad instance.
+	 * @return an exact copy of the object
+	 */
+	virtual Element* clone() const;
+
+	/// Returns the ID of a face given an array of nodes.
+	unsigned identifyFace(Node* nodes[3]) const;
+
+protected:
+	/// Calculates the area of a convex quadliteral by dividing it into two triangles.
+	double computeVolume();
+
+protected:
+	/// Return a specific edge node.
+	inline Node* getEdgeNode(unsigned edge_id, unsigned node_id) const { return _nodes[_edge_nodes[edge_id][node_id]]; };
+
+	static const unsigned _edge_nodes[4][3];
+}; /* class */
+
+} /* namespace */
+
+#include "TemplateQuad2-impl.h"
+
+#endif /* TEMPLATEQUAD2_H_ */
+
