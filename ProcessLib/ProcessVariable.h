bf802e4a450bbdd111019dfee68271f7d8fb1fa2
Dmitri Naumov <dmitri.naumov@ufz.de>
2014-09-16 17:13:58 +0200

    [PL] Split ProcessVariable.cpp from header.

--- ProcessLib/ProcessVariable.h
+++ ProcessLib/ProcessVariable.h
@@ -11,13 +11,15 @@
 #define PROCESS_LIB_PROCESS_VARIABLE_H_
 
 #include <boost/property_tree/ptree.hpp>
-#include "logog/include/logog.hpp"
 
 #include "GeoLib/GEOObjects.h"
 #include "MeshLib/Mesh.h"
 
-#include "BoundaryCondition.h"
-#include "InitialCondition.h"
+namespace ProcessLib
+{
+    class BoundaryCondition;
+    class InitialCondition;
+}
 
 namespace ProcessLib
 {
@@ -29,85 +31,11 @@ class ProcessVariable
     using ConfigTree = boost::property_tree::ptree;
 public:
     ProcessVariable(ConfigTree const& config, MeshLib::Mesh const& mesh,
-            GeoLib::GEOObjects const& geometries)
-        : _name(config.get<std::string>("name")),
-          _mesh(mesh)
-    {
-        DBUG("Constructing process variable %s", this->_name.c_str());
+            GeoLib::GEOObjects const& geometries);
 
-        // Initial condition
-        {
-            auto const& ic_config = config.find("initial_condition");
-            if (ic_config == config.not_found())
-                INFO("No initial condition found.");
+    ~ProcessVariable();
 
-
-            std::string const type =
-                config.get<std::string>("initial_condition.type");
-            if (type == "Uniform")
-            {
-                _initial_condition =
-                    new UniformInitialCondition(ic_config->second);
-            }
-            else
-            {
-                ERR("Unknown type of the initial condition.");
-            }
-        }
-
-        // Boundary conditions
-        {
-            auto const& bcs_config = config.find("boundary_conditions");
-            if (bcs_config == config.not_found())
-                INFO("No boundary conditions found.");
-
-            for (auto const& bc_iterator : bcs_config->second)
-            {
-                ConfigTree const& bc_config = bc_iterator.second;
-
-                // Find corresponding GeoObject
-                std::string const geometry_name =
-                    bc_config.get<std::string>("geometry");
-                std::string const patch_name =
-                    bc_config.get<std::string>("patch");
-
-                // TODO Currently only Polylines are supported for the boundary
-                // conditions.
-                GeoLib::GeoObject const* const geometry = geometries.getGeoObject(
-                        geometry_name, GeoLib::GEOTYPE::POLYLINE, patch_name);
-
-
-                // Construct type dependent boundary condition
-                std::string const type = bc_config.get<std::string>("type");
-
-                if (type == "UniformDirichlet")
-                {
-                    _boundary_conditions.emplace_back(
-                        new UniformDirichletBoundaryCondition(
-                            geometry, bc_config));
-                }
-                else
-                {
-                    ERR("Unknown type \'%s\' of the boundary condition.",
-                            type.c_str());
-                }
-            }
-
-        }
-    }
-
-    ~ProcessVariable()
-    {
-        delete _initial_condition;
-
-        for(auto p : _boundary_conditions)
-            delete p;
-    }
-
-    std::string const& getName() const
-    {
-        return _name;
-    }
+    std::string const& getName() const;
 
 private:
     std::string const _name;
