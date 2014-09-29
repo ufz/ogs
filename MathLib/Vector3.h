a6ab3f6ecbf97bcad37366ebe38eb4fe40a34eef
Karsten Rink <karsten.rink@ufz.de>
2014-09-29 10:19:49 +0200

    find x

--- MathLib/Vector3.h
+++ MathLib/Vector3.h
@@ -118,7 +118,7 @@ public:
 	/// Returns a normalized version of this vector
 	TemplateVector3<double> normalizeVector() const
 	{
-		TemplateVector3<double> norm_vec (_x[0], _x[1], _x[2]);
+		TemplateVector3<double> norm_vec (this->_x[0], this->_x[1], this->_x[2]);
 		norm_vec.normalize();
 		return norm_vec;
 	}
