10a17266dd0e4e48833a00114d210d603984aa1b
Dmitri Naumov <dmitri.naumov@ufz.de>
2014-10-01 10:18:22 +0200

    [A] Move Tests.cmake into CLI/. Cleanup cmake file.
    The Tests of the CLI/ogs should stay together with the binary.

--- Applications/CLI/Tests.cmake
+++ Applications/CLI/Tests.cmake
@@ -2,13 +2,8 @@
 add_test(NAME ogs_no_args COMMAND ogs)
 set_tests_properties(ogs_no_args PROPERTIES WILL_FAIL TRUE)
 
-#AddOgsBenchmark(ogs_empty_project EmptyProject.xml)
-#AddOgsBenchmark(ogs_empty_project Elliptic/quad_20x10_GroundWaterFlow.prj)
-
 ExternalData_Add_Test(
     data
-	NAME reading_GroundWaterFlow_project
+    NAME reading_GroundWaterFlow_project
     COMMAND ogs DATA{${ExternalData_SOURCE_ROOT}/Elliptic/quad_20x10_GroundWaterFlow/quad_20x10_GroundWaterFlow.prj,quad_20x10_constMat0.mesh.vtu,quad_20x10_left_right.gml}
 )
-
-#AddTest(ogs DATA{${ExternalData_SOURCE_ROOT}/Elliptic/quad_20x10_GroundWaterFlow.prj} TIME)
