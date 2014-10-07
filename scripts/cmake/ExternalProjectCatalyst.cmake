4cc899a09515dc2dc8be6f0c3884e57a193e14aa
Lars Bilke <lars.bilke@ufz.de>
2014-10-07 07:05:08 +0200

    Static linking now works on Windows.

--- scripts/cmake/ExternalProjectCatalyst.cmake
+++ scripts/cmake/ExternalProjectCatalyst.cmake
@@ -5,7 +5,8 @@ SET(CATALYST_GIT_URL https://github.com/ufz/catalyst-io.git)
 
 FIND_PACKAGE(ParaView 4.2 COMPONENTS vtkIOXML QUIET)
 
-IF(ParaView_FOUND)
+FIND_LIBRARY(VTKIO_LIB_FOUND vtkIOXML-pv4.2 HINTS ${ParaView_DIR}/lib PATH_SUFFIXES Release Debug)
+IF(ParaView_FOUND AND VTKIO_LIB_FOUND)
 	INCLUDE("${PARAVIEW_USE_FILE}")
 	# MESSAGE("Using Catalyst in ${ParaView_FOUND}")
 	RETURN()
@@ -15,21 +16,22 @@ ENDIF()
 
 SET(CATALYST_CMAKE_GENERATOR ${CMAKE_GENERATOR})
 IF(WIN32)
-	FIND_PROGRAM(NINJA_TOOL_PATH ninja DOC "Ninja build tool")
+	# Ninja tempory disabled because it builds only the Release mode.
+	# FIND_PROGRAM(NINJA_TOOL_PATH ninja DOC "Ninja build tool")
 	IF(NINJA_TOOL_PATH)
 		SET(CATALYST_CMAKE_GENERATOR Ninja)
-		SET(CATALYST_MAKE_COMMAND ninja vtkIO)
+		SET(CATALYST_MAKE_COMMAND ninja vtkIOXML)
 	ELSE()
 		SET(CATALYST_MAKE_COMMAND
-			cmake --build . --config Release --target vtkIO -- /m &&
-			cmake --build . --config Debug --target vtkIO -- /m)
+			cmake --build . --config Release --target vtkIOXML -- /m &&
+			cmake --build . --config Debug --target vtkIOXML -- /m)
 	ENDIF()
 	SET(CATALYST_CONFIGURE_COMMAND cmake.bat)
 ELSE()
 	IF($ENV{CI})
-		SET(CATALYST_MAKE_COMMAND make vtkIO)
+		SET(CATALYST_MAKE_COMMAND make vtkIOXML)
 	ELSE()
-		SET(CATALYST_MAKE_COMMAND make -j ${NUM_PROCESSORS} vtkIO)
+		SET(CATALYST_MAKE_COMMAND make -j ${NUM_PROCESSORS} vtkIOXML)
 	ENDIF()
 	SET(CATALYST_CONFIGURE_COMMAND cmake.sh)
 ENDIF()
@@ -50,3 +52,4 @@ IF(NOT ${ParaView_FOUND})
 ELSE()
 	ADD_CUSTOM_TARGET(VtkRescan) # dummy target for caching
 ENDIF()
+ 
