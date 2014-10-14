adfbcab52f79d634ce0846ff87767923d61549ca
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2014-10-14 16:22:43 +0200

    support non-square elements in generateStructuredMesh tool

--- Applications/Utils/SimpleMeshCreation/generateStructuredMesh.cpp
+++ Applications/Utils/SimpleMeshCreation/generateStructuredMesh.cpp
@@ -6,6 +6,10 @@
  *              http://www.opengeosys.org/LICENSE.txt
  */
 
+#include <algorithm>
+#include <string>
+#include <vector>
+
 // TCLAP
 #include "tclap/CmdLine.h"
 
@@ -13,17 +17,17 @@
 #include "logog/include/logog.hpp"
 
 // BaseLib
-#include "LogogSimpleFormatter.h"
-
-// FileIO
-#include "Legacy/MeshIO.h"
+#include "BaseLib/LogogSimpleFormatter.h"
 
 // MeshLib
-#include "Mesh.h"
+#include "MeshLib/Mesh.h"
 #include "MeshLib/Node.h"
-#include "Elements/Element.h"
-#include "MeshEnums.h"
-#include "MeshGenerators/MeshGenerator.h"
+#include "MeshLib/Elements/Element.h"
+#include "MeshLib/MeshEnums.h"
+#include "MeshLib/MeshGenerators/MeshGenerator.h"
+
+// FileIO
+#include "FileIO/Legacy/MeshIO.h"
 
 
 int main (int argc, char* argv[])
@@ -41,35 +45,77 @@ int main (int argc, char* argv[])
 	TCLAP::ValueArg<std::string> eleTypeArg("e", "element-type",
 	                                      "element type to be created", true, "line", "element type");
 	cmd.add(eleTypeArg);
-	TCLAP::ValueArg<double> lengthArg("l", "length",
-	                                      "length of a domain", true, 10.0, "length of a domain");
-	cmd.add(lengthArg);
-	TCLAP::ValueArg<unsigned> nsubdivArg("n", "nr-subdivision",
-	                                      "the number of subdivision", true, 10, "the number of subdivision");
-	cmd.add(nsubdivArg);
+	TCLAP::ValueArg<double> lengthXArg("", "lx",
+	                                      "length of a domain in x direction", false, 10.0, "real");
+	cmd.add(lengthXArg);
+	TCLAP::ValueArg<double> lengthYArg("", "ly",
+	                                      "length of a domain in y direction", false, 10.0, "real");
+	cmd.add(lengthYArg);
+	TCLAP::ValueArg<double> lengthZArg("", "lz",
+	                                      "length of a domain in z direction", false, 10.0, "real");
+	cmd.add(lengthZArg);
+	TCLAP::ValueArg<unsigned> nsubdivXArg("", "nx",
+	                                      "the number of subdivision in x direction", false, 10, "integer");
+	cmd.add(nsubdivXArg);
+	TCLAP::ValueArg<unsigned> nsubdivYArg("", "ny",
+	                                      "the number of subdivision in y direction", false, 10, "integer");
+	cmd.add(nsubdivYArg);
+	TCLAP::ValueArg<unsigned> nsubdivZArg("", "nz",
+	                                      "the number of subdivision in z direction", false, 10, "integer");
+	cmd.add(nsubdivZArg);
 
 	// parse arguments
 	cmd.parse(argc, argv);
 	const std::string eleTypeName(eleTypeArg.getValue());
 	const MeshElemType eleType = String2MeshElemType(eleTypeName);
-	const double length = lengthArg.getValue();
-	const unsigned n_subdivision = nsubdivArg.getValue();
+	const unsigned dim = MeshLib::Element::getDimension(eleType);
+
+	bool dim_used[3] = {false};
+	for (unsigned i=0; i<dim; i++)
+		dim_used[i] = true;
+
+	std::vector<TCLAP::ValueArg<double>*> vec_lengthArg = {&lengthXArg, &lengthYArg, &lengthZArg};
+	std::vector<TCLAP::ValueArg<unsigned>*> vec_ndivArg = {&nsubdivXArg, &nsubdivYArg, &nsubdivZArg};
+
+	const bool isLengthSet = std::any_of(vec_lengthArg.begin(), vec_lengthArg.end(),
+	                                     [&](TCLAP::ValueArg<double> *arg){return arg->isSet();});
+	if (!isLengthSet) {
+		ERR("Missing input: Length information is not provided at all.");
+		return 1;
+	} else {
+		for (unsigned i=0; i<3; i++) {
+			if (dim_used[i] && !vec_lengthArg[i]->isSet()) {
+				ERR("Missing input: Length for dimension [%d] is required but missing.", i);
+				return 1;
+			}
+		}
+	}
+
+	std::vector<double> length(dim);
+	for (unsigned i=0; i<dim; i++)
+		length[i] = vec_lengthArg[i]->getValue();
+	std::vector<unsigned> n_subdivision(dim);
+	for (unsigned i=0; i<dim; i++)
+		n_subdivision[i] = vec_ndivArg[i]->getValue();
+	std::vector<double> vec_dx(dim);
+	for (unsigned i=0; i<dim; i++)
+		vec_dx[i] = length[i] / n_subdivision[i];
 
 	// generate a mesh
 	MeshLib::Mesh* mesh = nullptr;
 	switch (eleType)
 	{
 	case MeshElemType::LINE:
-		mesh = MeshLib::MeshGenerator::generateLineMesh(length, n_subdivision);
+		mesh = MeshLib::MeshGenerator::generateLineMesh(length[0], n_subdivision[0]);
 		break;
 	case MeshElemType::TRIANGLE:
-		mesh = MeshLib::MeshGenerator::generateRegularTriMesh(length, n_subdivision);
+		mesh = MeshLib::MeshGenerator::generateRegularTriMesh(n_subdivision[0], n_subdivision[1], vec_dx[0], vec_dx[1]);
 		break;
 	case MeshElemType::QUAD:
-		mesh = MeshLib::MeshGenerator::generateRegularQuadMesh(length, n_subdivision);
+		mesh = MeshLib::MeshGenerator::generateRegularQuadMesh(n_subdivision[0], n_subdivision[1], vec_dx[0], vec_dx[1]);
 		break;
 	case MeshElemType::HEXAHEDRON:
-		mesh = MeshLib::MeshGenerator::generateRegularHexMesh(length, n_subdivision);
+		mesh = MeshLib::MeshGenerator::generateRegularHexMesh(n_subdivision[0], n_subdivision[1], n_subdivision[2], vec_dx[0], vec_dx[1], vec_dx[2]);
 		break;
 	default:
 		ERR("Given element type is not supported.");
