9538afb5bf0320002c999c48e74a69108ecc2bc6
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2014-11-14 19:11:54 +0100

    make Element::getEdgeNode() being public

--- MeshLib/Elements/Element.h
+++ MeshLib/Elements/Element.h
@@ -202,12 +202,12 @@ public:
 	/// Returns if this element is linear order
 	bool isLinearElement() const { return getNBaseNodes()==getNNodes(); }
 
+	/// Return a specific edge node.
+	virtual Node* getEdgeNode(unsigned edge_id, unsigned node_id) const = 0;
+
 protected:
 	/// Constructor for a generic mesh element without an array of mesh nodes.
 	Element(unsigned value = 0, std::size_t id = std::numeric_limits<std::size_t>::max());
-
-	/// Return a specific edge node.
-	virtual Node* getEdgeNode(unsigned edge_id, unsigned node_id) const = 0;
 
 	/// Sets the element ID.
 	virtual void setID(std::size_t id) { this->_id = id; }
