1b562cd0181fac6a6585673bd694c994c4c377be
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2014-11-17 13:08:35 +0100

    fix double free error

--- Tests/MeshLib/TestHexMesh.cpp
+++ Tests/MeshLib/TestHexMesh.cpp
@@ -16,8 +16,9 @@
 
 TEST(MeshLib, HexTests)
 {
-	MeshLib::Node** nodes = new MeshLib::Node*[20];
-	MeshLib::Hex hex(nodes);
-	MeshLib::Hex20 hex20(nodes);
+	MeshLib::Node** nodes8 = new MeshLib::Node*[8];
+	MeshLib::Hex hex(nodes8);
+	MeshLib::Node** nodes20 = new MeshLib::Node*[20];
+	MeshLib::Hex20 hex20(nodes20);
 }
 
