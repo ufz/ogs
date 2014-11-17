bbe2a7b6ee1c487c8b2447306cba61585fe5dcd0
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2014-11-17 13:34:17 +0100

    again, fix the test with dummy nodes

--- Tests/MeshLib/TestHexMesh.cpp
+++ Tests/MeshLib/TestHexMesh.cpp
@@ -11,14 +11,19 @@
 
 #include "MeshLib/MeshGenerators/MeshGenerator.h"
 #include "MeshLib/Mesh.h"
+#include "MeshLib/Node.h"
 #include "MeshLib/Elements/Hex.h"
 
 
 TEST(MeshLib, HexTests)
 {
 	MeshLib::Node** nodes8 = new MeshLib::Node*[8];
+	for (int i=0; i<8; i++)
+		nodes8[i] = new MeshLib::Node(0, 0, 0);
 	MeshLib::Hex hex(nodes8);
 	MeshLib::Node** nodes20 = new MeshLib::Node*[20];
+	for (int i=0; i<20; i++)
+		nodes20[i] = new MeshLib::Node(0, 0, 0);
 	MeshLib::Hex20 hex20(nodes20);
 }
 
