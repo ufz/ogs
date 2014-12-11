201f1ef780d841a1fceb41df6d427b50b7adbfca
Lars Bilke <lars.bilke@ufz.de>
2014-12-11 10:45:49 +0100

    Better prerequisite checking for coverage reports.

--- scripts/cmake/Coverage.cmake
+++ scripts/cmake/Coverage.cmake
@@ -8,8 +8,17 @@ SET(COVERAGE_EXCLUDES
 	'${CMAKE_SOURCE_DIR}/ThirdParty/*'
 )
 
-SETUP_TARGET_FOR_COVERAGE_COBERTURA(testrunner_coverage_cobertura testrunner "testrunner_coverage_cobertura_results" "-j;${PROCESSOR_COUNT}")
-SETUP_TARGET_FOR_COVERAGE(testrunner_coverage testrunner "testrunner_coverage_results" "-j;${PROCESSOR_COUNT}")
-IF(OGS_BUILD_GUI)
-	SETUP_TARGET_FOR_COVERAGE(DataExplorer_coverage DataExplorer "DataExplorer_coverage_results")
+IF(LCOV_PATH AND GENHTML_PATH)
+	SETUP_TARGET_FOR_COVERAGE(testrunner_coverage testrunner "testrunner_coverage_results" "-j;${PROCESSOR_COUNT}")
+	IF(OGS_BUILD_GUI)
+		SETUP_TARGET_FOR_COVERAGE(DataExplorer_coverage DataExplorer "DataExplorer_coverage_results")
+	ENDIF()
+ELSE()
+	MESSAGE(STATUS "No lcov coverage report generated because lcov or genhtml was not found.")
+ENDIF()
+
+IF(PYTHON_EXECUTABLE)
+	SETUP_TARGET_FOR_COVERAGE_COBERTURA(testrunner_coverage_cobertura testrunner "testrunner_coverage_cobertura_results" "-j;${PROCESSOR_COUNT}")
+ELSE()
+	MESSAGE(STATUS "No cobertura coverage report generated because Python executable was not found.")
 ENDIF()
