018689849b51e1e61f6387ea8b8caad8b5400cd6
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2015-01-09 16:15:19 +0100

    add writeMeshToFile

---
+++ FileIO/writeMeshToFile.h
@@ -0,0 +1,24 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2014, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ */
+
+#ifndef WRITEMESHTOFILE_H_
+#define WRITEMESHTOFILE_H_
+
+#include <string>
+
+namespace MeshLib
+{
+class Mesh;
+}
+
+namespace FileIO
+{
+void writeMeshToFile(const MeshLib::Mesh &mesh, const std::string &file_name);
+}
+
+#endif // WRITEMESHTOFILE_H_
