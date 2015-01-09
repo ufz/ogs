f54ec419fd24b8bf67e754bacf95c84efd5f2f5a
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2015-01-09 17:00:31 +0100

    [bug] set a mesh to MeshIO

--- FileIO/writeMeshToFile.cpp
+++ FileIO/writeMeshToFile.cpp
@@ -29,6 +29,7 @@ void writeMeshToFile(const MeshLib::Mesh &mesh, const std::string &file_name)
 	if (BaseLib::hasFileExtension("msh", file_name))
 	{
 		Legacy::MeshIO meshIO;
+		meshIO.setMesh(&mesh);
 		meshIO.writeToFile(file_name);
 	} else if (BaseLib::hasFileExtension("vtu", file_name)) {
 		FileIO::VtuInterface writer(&mesh);
