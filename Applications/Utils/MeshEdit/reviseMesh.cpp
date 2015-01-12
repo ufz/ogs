163e31615f29ea261bfc53783639ed58666f794a
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2015-01-12 19:19:28 +0100

    delete try-catch for TCLAP

--- Applications/Utils/MeshEdit/reviseMesh.cpp
+++ Applications/Utils/MeshEdit/reviseMesh.cpp
@@ -33,49 +33,44 @@ int main(int argc, char *argv[])
 	BaseLib::LogogSimpleFormatter *custom_format (new BaseLib::LogogSimpleFormatter);
 	logog_cout->SetFormatter(*custom_format);
 
-	try {
-		TCLAP::CmdLine cmd("Mesh revision tool", ' ', BaseLib::BuildInfo::git_version_sha1);
-		TCLAP::ValueArg<std::string> input_arg("i", "input-mesh-file","input mesh file",true,"","string");
-		cmd.add( input_arg );
-		TCLAP::ValueArg<std::string> output_arg("o", "output-mesh-file","output mesh file",true,"","string");
-		cmd.add( output_arg );
-		TCLAP::SwitchArg simplify_arg("s","simplify","simplify the mesh (removing duplicated nodes)");
-		cmd.add( simplify_arg );
-		TCLAP::ValueArg<double> eps_arg("e", "eps","Minimum distance for nodes not to be collapsed",
-										false, std::numeric_limits<double>::epsilon(),"float");
-		cmd.add( eps_arg );
-		TCLAP::ValueArg<unsigned> minDim_arg("d", "min-ele-dim","Minimum dimension of elements to be inserted into new mesh",
-										false, 1, "unsigned");
-		cmd.add( minDim_arg );
+	TCLAP::CmdLine cmd("Mesh revision tool", ' ', BaseLib::BuildInfo::git_version_sha1);
+	TCLAP::ValueArg<std::string> input_arg("i", "input-mesh-file","input mesh file",true,"","string");
+	cmd.add( input_arg );
+	TCLAP::ValueArg<std::string> output_arg("o", "output-mesh-file","output mesh file",true,"","string");
+	cmd.add( output_arg );
+	TCLAP::SwitchArg simplify_arg("s","simplify","simplify the mesh (removing duplicated nodes)");
+	cmd.add( simplify_arg );
+	TCLAP::ValueArg<double> eps_arg("e", "eps","Minimum distance for nodes not to be collapsed",
+									false, std::numeric_limits<double>::epsilon(),"float");
+	cmd.add( eps_arg );
+	TCLAP::ValueArg<unsigned> minDim_arg("d", "min-ele-dim","Minimum dimension of elements to be inserted into new mesh",
+									false, 1, "unsigned");
+	cmd.add( minDim_arg );
+	cmd.parse( argc, argv );
 
-		cmd.parse( argc, argv );
+	// read a mesh file
+	MeshLib::Mesh* org_mesh (FileIO::readMeshFromFile(input_arg.getValue()));
+	if (!org_mesh)
+		return 0;
+	INFO("Mesh read: %d nodes, %d elements.", org_mesh->getNNodes(), org_mesh->getNElements());
 
-		// read a mesh file
-		MeshLib::Mesh* org_mesh (FileIO::readMeshFromFile(input_arg.getValue()));
-		if (!org_mesh)
-			return 0;
-		INFO("Mesh read: %d nodes, %d elements.", org_mesh->getNNodes(), org_mesh->getNElements());
-
-		// revise the mesh
-		MeshLib::Mesh* new_mesh = nullptr;
-		if (simplify_arg.getValue()) {
-			INFO("Simplifying the mesh...");
-			MeshLib::MeshRevision rev(const_cast<MeshLib::Mesh&>(*org_mesh));
-			unsigned int minDim = (minDim_arg.isSet() ? minDim_arg.getValue() : org_mesh->getDimension());
-			new_mesh = rev.simplifyMesh("revised_mesh", eps_arg.getValue(), minDim);
-		}
-
-		// write into a file
-		if (new_mesh) {
-			INFO("Revised mesh: %d nodes, %d elements.", new_mesh->getNNodes(), new_mesh->getNElements());
-			FileIO::writeMeshToFile(*new_mesh, output_arg.getValue());
-		}
-
-		delete org_mesh;
-		delete new_mesh;
-	} catch (TCLAP::ArgException &e) {
-		ERR("error: %s for arg %s", e.error().c_str(), e.argId().c_str());
+	// revise the mesh
+	MeshLib::Mesh* new_mesh = nullptr;
+	if (simplify_arg.getValue()) {
+		INFO("Simplifying the mesh...");
+		MeshLib::MeshRevision rev(const_cast<MeshLib::Mesh&>(*org_mesh));
+		unsigned int minDim = (minDim_arg.isSet() ? minDim_arg.getValue() : org_mesh->getDimension());
+		new_mesh = rev.simplifyMesh("revised_mesh", eps_arg.getValue(), minDim);
 	}
+
+	// write into a file
+	if (new_mesh) {
+		INFO("Revised mesh: %d nodes, %d elements.", new_mesh->getNNodes(), new_mesh->getNElements());
+		FileIO::writeMeshToFile(*new_mesh, output_arg.getValue());
+	}
+
+	delete org_mesh;
+	delete new_mesh;
 
 	delete custom_format;
 	delete logog_cout;
