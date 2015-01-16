d0080e36b9ea95b040518969ffc3704533595aae
Lars Bilke <lars.bilke@ufz.de>
2015-01-16 14:41:30 +0100

    Small NSIS installer fixes.

--- scripts/cmake/CMakeSetup.cmake
+++ scripts/cmake/CMakeSetup.cmake
@@ -18,9 +18,6 @@ INCLUDE(GetGitRevisionDescription)
 GET_GIT_HEAD_REVISION(GIT_REFSPEC GIT_SHA1)
 STRING(SUBSTRING ${GIT_SHA1} 0 8 GIT_SHA1_SHORT)
 
-# Suppress warning on setting policies
-CMAKE_POLICY(SET CMP0011 OLD)
-
 # Get the hostname
 SITE_NAME(HOSTNAME)
 
