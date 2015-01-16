297ec7c8e12aa75b156152469118524d79fabb1f
Thomas Fischer <thomas.fischer@ufz.de>
2015-01-16 13:04:04 +0100

    [MaL] Moved operator<= and lessEq from Point to MathPoint.

--- GeoLib/Point.h
+++ GeoLib/Point.h
@@ -54,23 +54,7 @@ public:
 };
 
 static const Point ORIGIN(0, 0, 0);
-
-/**
- * lexicographic comparison of points
- */
-bool operator<= (GeoLib::Point const & p0, GeoLib::Point const & p1);
-
-/**
- * lexicographical comparison of points taking an epsilon into account
- * @param p0 first input Point
- * @param p1 first input Point
- * @param tol tolerance (if in the comparison operation the property fabs(p0[k] - p1[k]) < tol
- *     holds for the k-th coordinate the points are assumed the be equal in this coordinate)
- * @return true, if p0 is lexicographically smaller than p1
- */
-bool lessEq(const GeoLib::Point& p0,
-            const GeoLib::Point& p1,
-            double tol = std::numeric_limits<double>::epsilon());
 }
 
 #endif /* POINT_H_ */
+
--- MathLib/MathPoint.h
+++ MathLib/MathPoint.h
@@ -14,12 +14,31 @@
 #ifndef MATHPOINT_H_
 #define MATHPOINT_H_
 
+#include <cmath>
+
 #include "TemplatePoint.h"
 
 namespace MathLib
 {
 typedef MathLib::TemplatePoint<double,3> MathPoint;
 } // end namespace MathLib
+
+/**
+ * lexicographic comparison of points
+ */
+bool operator<= (MathLib::MathPoint const & p0, MathLib::MathPoint const & p1);
+
+/**
+ * lexicographical comparison of points taking an epsilon into account
+ * @param p0 first input MathPoint
+ * @param p1 first input MathPoint
+ * @param tol tolerance (if in the comparison operation the property fabs(p0[k] - p1[k]) < tol
+ *     holds for the k-th coordinate the points are assumed the be equal in this coordinate)
+ * @return true, if p0 is lexicographically smaller than p1
+ */
+bool lessEq(const MathLib::MathPoint& p0,
+            const MathLib::MathPoint& p1,
+            double tol = std::numeric_limits<double>::epsilon());
 
 #endif /* MATHPOINT_H_ */
 
