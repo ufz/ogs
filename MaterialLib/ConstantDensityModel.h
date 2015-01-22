88d9469ff4434ccd6b540c4a1e30a79a0aa3bef0
Wenqing Wang <wenqing.wang@ufz.de>
2015-01-22 17:37:51 +0100

    [Mat] Added basic density classes.

---
+++ MaterialLib/ConstantDensityModel.h
@@ -0,0 +1,47 @@
+/*!
+   \file  ConstantDensityModel.h
+   \brief Declaration of class ConstantDensityModel for constant density.
+
+   \author Wenqing Wang
+   \date Jan 2015
+
+   \copyright
+    Copyright (c) 2012-2015, OpenGeoSys Community (http://www.opengeosys.org)
+               Distributed under a Modified BSD License.
+               See accompanying file LICENSE.txt or
+               http://www.opengeosys.org/project/license
+*/
+#ifndef CONSTANT_DENSITY_MODEL_H_
+#define CONSTANT_DENSITY_MODEL_H_
+
+#include<string>
+
+namespace MaterialLib
+{
+
+/// Constant density model
+class ConstantDensityModel
+{
+    public:
+        ConstantDensityModel(const double density) : _density(density)
+        {
+        }
+
+        /// Get desity model name.
+        std::string getName() const
+        {
+            return "Constant";
+        }
+
+        /// Get density value
+        double getDensity() const
+        {
+            return _density;
+        }
+    private:
+        double _density;
+};
+
+} // end namespace
+#endif
+
---
+++ MaterialLib/Fluid/Density/IdealGasLaw.h
@@ -0,0 +1,55 @@
+/*!
+   \file  IdealGasLow.h
+   \brief Declaration of class LIdealGasLow for fluid density by ideal gas low
+          depending on a variable linearly.
+   
+   \author Wenqing Wang
+   \date Jan 2015
+
+   \copyright
+    Copyright (c) 2012-2015, OpenGeoSys Community (http://www.opengeosys.org)
+               Distributed under a Modified BSD License.
+               See accompanying file LICENSE.txt or
+               http://www.opengeosys.org/project/license
+*/
+#ifndef IDEAL_GAS_LAW_H_
+#define IDEAL_GAS_LAW_H_
+
+#include<string>
+
+namespace MaterialLib
+{
+
+/// Fluid density by ideal gas low
+class IdealGasLaw
+{
+    public:
+        ///   \param molar_mass Molar mass of the gas phase.
+        IdealGasLaw(const double molar_mass) : _molar_mass(molar_mass)
+        {
+        }
+
+        /// Get desity model name.
+        std::string getName() const
+        {
+           return "Ideal gas law";  
+        }
+
+        /// Get density value
+        /// \param T  Temperature in K.
+        /// \param pg Gas phase pressure in Pa.        
+        double getDensity(const double T, const double pg) const
+        {
+           return _molar_mass * pg /(_gas_constant * T);  
+        }
+    private:       
+       /// Normally denoted as R, unit \f$J(kmol K)^{-1}\f$.
+       static constexpr double _gas_constant = 8315.41;
+       
+       /// Molar mass of gas phase.
+       double _molar_mass;
+};
+
+} // end namespace
+#endif
+
