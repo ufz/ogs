af711d8a8f1752d356e4c7cffc86ce01207a10fc
rahv <karsten.rink@ufz.de>
2015-01-29 17:42:30 +0100

    added dialog for mesh surface extraction

---
+++ Applications/DataExplorer/DataView/SurfaceExtractionDialog.cpp
@@ -0,0 +1,45 @@
+/**
+ * \file   SurfaceExtractionDialog.cpp
+ * \author Karsten Rink
+ * \date   2015-01-29
+ * \brief  Implementation of the SaveMeshDialog class.
+ *
+ * \copyright
+ * Copyright (c) 2012-2015, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#include "SurfaceExtractionDialog.h"
+
+#include "MeshLib/MeshSurfaceExtraction.h"
+
+#include <QFileDialog>
+#include <QSettings>
+#include <QDoubleValidator>
+
+// ThirdParty/logog
+#include "logog/include/logog.hpp"
+
+
+SurfaceExtractionDialog::SurfaceExtractionDialog(QDialog* parent)
+	: QDialog(parent), _tolerance(90), _dir(0,0,-1)
+{
+	setupUi(this);
+	this->xNormalEdit->setValidator(new QDoubleValidator(-1, 1, 3, xNormalEdit));
+	this->yNormalEdit->setValidator(new QDoubleValidator(-1, 1, 3, yNormalEdit));
+	this->zNormalEdit->setValidator(new QDoubleValidator(-1, 1, 3, zNormalEdit));
+}
+
+void SurfaceExtractionDialog::accept()
+{
+	_dir = MathLib::Vector3(xNormalEdit->text().toDouble(), 
+	                        yNormalEdit->text().toDouble(), 
+	                        zNormalEdit->text().toDouble());
+	_tolerance = degreesSpinBox->text().toInt();
+
+	this->done(QDialog::Accepted);
+}
+
---
+++ Applications/DataExplorer/DataView/SurfaceExtractionDialog.h
@@ -0,0 +1,53 @@
+/**
+ * \file   SurfaceExtractionDialog.h
+ * \author Karsten Rink
+ * \date   2015-01-29
+ * \brief  Definition of the SurfaceExtractionDialog class.
+ *
+ * \copyright
+ * Copyright (c) 2012-2015, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#ifndef SURFACEEXTRACTIONDIALOG_H
+#define SURFACEEXTRACTIONDIALOG_H
+
+#include "ui_SurfaceExtraction.h"
+#include <QDialog>
+
+#include "MathLib/Vector3.h"
+
+namespace MeshLib {
+	class Mesh;
+}
+
+/**
+ * \brief A dialog window for managing properties for writing meshes to files.
+ */
+class SurfaceExtractionDialog : public QDialog, private Ui_SurfaceExtraction
+{
+	Q_OBJECT
+
+public:
+	SurfaceExtractionDialog(QDialog* parent = 0);
+	~SurfaceExtractionDialog() {}
+
+	int getTolerance() const { return _tolerance; }
+	MathLib::Vector3 const& getNormal() const { return _dir; }
+
+private slots:
+	/// Instructions if the OK-Button has been pressed.
+	void accept();
+
+	/// Instructions if the Cancel-Button has been pressed.
+	void reject() { this->done(QDialog::Rejected); };
+
+private:
+	int _tolerance;
+	MathLib::Vector3 _dir;
+};
+
+#endif //SURFACEEXTRACTIONDIALOG_H
