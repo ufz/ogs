98b1f7ecbbd69377ab4804ad1ae38d6988341702
Wenqing Wang <wenqing.wang@ufz.de>
2015-02-05 12:21:45 +0100

    [Test] Replaced constexpr with const.

--- Tests/MaterialLib/TestScalarMaterialParameter.cpp
+++ Tests/MaterialLib/TestScalarMaterialParameter.cpp
@@ -49,7 +49,7 @@ TEST(Material, checkDensity)
 {
     //-- Solid --------------------------------------------------------------
     // Constant
-    constexpr double rho = 2080.;
+    const double rho = 2080.;
     MaterialLib::ScalarParameter<DensityType, ConstantScalarModel> s_density(rho);
     ASSERT_NEAR(rho, s_density.getValue(), 1.e-10);
     ASSERT_EQ(DensityType::CONSTANT, s_density.getType());
@@ -62,12 +62,12 @@ TEST(Material, checkDensity)
     ASSERT_NEAR(lin_den_expected, lin_density.getValue(50.), 1.e-10);
 
     //-- Fluid --------------------------------------------------------------
-    constexpr double molar_air = 28.96;
+    const double molar_air = 28.96;
     MaterialLib::ScalarParameter<DensityType, IdealGasLaw> air_density(molar_air);
-    constexpr double T = 290.;
-    constexpr double p = 1.e+5;
-    constexpr double R = 8315.41;
-    constexpr double expected_air_dens = molar_air * p /(R * T);
+    const double T = 290.;
+    const double p = 1.e+5;
+    const double R = 8315.41;
+    const double expected_air_dens = molar_air * p /(R * T);
     ASSERT_NEAR(expected_air_dens, air_density.getValue(T, p), 1.e-10);
 
     TestScalar< MaterialLib::ScalarParameter<DensityType, IdealGasLaw> > test0(air_density);
@@ -86,15 +86,15 @@ TEST(Material, checkDensity)
 TEST(Material, checkViscosity)
 {
     // Constant
-    constexpr double mu = 1.e-3;
+    const double mu = 1.e-3;
     MaterialLib::ScalarParameter<ViscosityType, ConstantScalarModel> mu_const(mu);
     ASSERT_NEAR(mu, mu_const.getValue(), 1.e-10);
     ASSERT_EQ(ViscosityType::CONSTANT, mu_const.getType());
 
     // Pressure dependent
-    constexpr double mu0 = 1.e-3;
-    constexpr double p0 = 1.e+5;
-    constexpr double gamma = 10.;
+    const double mu0 = 1.e-3;
+    const double p0 = 1.e+5;
+    const double gamma = 10.;
     MaterialLib::ScalarParameter<ViscosityType, PressureDependentViscosity>
         mu_pre(mu0, p0, gamma);
     const double mu_pre_expected = mu0 * (1 + gamma* (std::max(1.e+6, 0.) - p0) );
