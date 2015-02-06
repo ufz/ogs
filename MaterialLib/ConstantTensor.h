fd87c465381e0d2c2b2451ad341aad7fa7d57983
Wenqing Wang <wenqing.wang@ufz.de>
2015-02-06 16:16:56 +0100

    [Mat] Added tensor material parameters.

---
+++ MaterialLib/ConstantTensor.h
@@ -0,0 +1,48 @@
+/*!
+   \file  ConstantTensor.h
+   \brief Declaration of constant tensor for material parameters.
+
+   \author Wenqing Wang
+   \date Feb 2015
+
+   \copyright
+    Copyright (c) 2012-2015, OpenGeoSys Community (http://www.opengeosys.org)
+               Distributed under a Modified BSD License.
+               See accompanying file LICENSE.txt or
+               http://www.opengeosys.org/project/license
+*/
+#ifndef CONSTANT_TENSOR_H_
+#define CONSTANT_TENSOR_H_
+
+namespace MaterialLib
+{
+/// Keep intrisinc permeability.
+template<typename T_MATRIX> class ConstantTensor
+{
+    public:
+        ConstantTensor(const T_MATRIX &perm) : _perm(perm)
+        {
+        }
+
+        /// Get model name.
+        std::string getName() const
+        {
+            return "Intrisince permeability";
+        }
+
+        unsigned getType() const
+        {
+            return 0;
+        }
+
+        /// Get permeability tensor.
+        T_MATRIX &getValue()
+        {
+            return _perm;
+        }
+    private:
+        T_MATRIX _perm;
+};
+
+} // end namespace
+#endif
