d69c090a914edeadd9d463761775cc37f4a090d2
rahv <karsten.rink@ufz.de>
2015-02-09 15:52:08 +0100

    fixed documentation tags for GeoMapper

--- MeshGeoToolsLib/GeoMapper.h
+++ MeshGeoToolsLib/GeoMapper.h
@@ -43,45 +43,51 @@ public:
 	GeoMapper(GeoLib::GEOObjects &geo_objects, const std::string &geo_name);
 	~GeoMapper();
 
+	/// Maps geo geometry based on a raster file
 	void mapOnDEM(const std::string &file_name);
+	
+	/// Maps geo geometry based on a mesh file
 	void mapOnMesh(const std::string &file_name);
+
+	/// Maps geo geometry based on a mesh
 	void mapOnMesh(const MeshLib::Mesh* mesh);
+
 	void advancedMapOnMesh(const MeshLib::Mesh* mesh, const std::string &new_geo_name);
 
 private:
-	// Manages the mapping geometric data (points, stations, boreholes) on a raster or mesh.
+	/// Manages the mapping geometric data (points, stations, boreholes) on a raster or mesh.
 	void mapData();
 
-	// Returns a grid containing all mesh surface points with elevation=0
+	/// Returns a grid containing all mesh surface points with elevation=0
 	GeoLib::Grid<GeoLib::PointWithID>* getFlatGrid(MeshLib::Mesh const*const mesh, std::vector<GeoLib::PointWithID*> sfc_pnts) const;
 
-	// Returns the elevation at Point (x,y) based on a mesh. This uses collision detection for triangles and nearest neighbor for quads.
-	// NOTE: This medhod only returns correct values if the node numbering of the elements is correct!
+	/// Returns the elevation at Point (x,y) based on a mesh. This uses collision detection for triangles and nearest neighbor for quads.
+	/// NOTE: This medhod only returns correct values if the node numbering of the elements is correct!
 	double getMeshElevation(double x, double y, double min_val, double max_val) const;
 
-	// Returns the elevation at Point (x,y) based on a raster
+	/// Returns the elevation at Point (x,y) based on a raster
 	float getDemElevation(GeoLib::Point const& pnt) const;
 
-	// Calculates the intersection of two lines embedded in the xy-plane
+	/// Calculates the intersection of two lines embedded in the xy-plane
 	GeoLib::Point* calcIntersection(MathLib::Point3d const*const p1, MathLib::Point3d const*const p2, GeoLib::Point const*const q1, GeoLib::Point const*const q2) const;
 
-	// Returns the position of a point within a line-segment
+	/// Returns the position of a point within a line-segment
 	unsigned getPointPosInLine(GeoLib::Polyline const*const line, unsigned start, unsigned end, GeoLib::Point const*const point, double eps) const;
 
-	// Returns the maximum segment length in a polyline vector
+	/// Returns the maximum segment length in a polyline vector
 	double getMaxSegmentLength(const std::vector<GeoLib::Polyline*> &lines) const;
 
-	// Returns if a point p is within a bounding box defined by a and b
+	/// Returns if a point p is within a bounding box defined by a and b
 	bool isPntInBoundingBox(double ax, double ay, double bx, double by, double px, double py) const;
 
 	GeoLib::GEOObjects& _geo_objects;
 	std::string& _geo_name;
 
-	// only necessary for mapping on mesh
+	/// only necessary for mapping on mesh
 	MeshLib::Mesh* _mesh;
 	GeoLib::Grid<GeoLib::PointWithID>* _grid;
 
-	// only necessary for mapping on DEM
+	/// only necessary for mapping on DEM
 	GeoLib::Raster *_raster;
 
 };
