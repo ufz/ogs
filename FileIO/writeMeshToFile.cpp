e54310745d62f4f8570f6baaa2258c00c9586c58
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2015-02-11 16:30:57 +0100

    add OGS_DONT_VTK option

--- FileIO/writeMeshToFile.cpp
+++ FileIO/writeMeshToFile.cpp
@@ -20,7 +20,9 @@
 
 // FileIO
 #include "Legacy/MeshIO.h"
+#ifndef OGS_DONT_USE_VTK
 #include "FileIO/VtkIO/VtuInterface.h"
+#endif
 
 namespace FileIO
 {
@@ -31,9 +33,11 @@ void writeMeshToFile(const MeshLib::Mesh &mesh, const std::string &file_name)
 		Legacy::MeshIO meshIO;
 		meshIO.setMesh(&mesh);
 		meshIO.writeToFile(file_name);
+#ifndef OGS_DONT_USE_VTK
 	} else if (BaseLib::hasFileExtension("vtu", file_name)) {
 		FileIO::VtuInterface writer(&mesh);
 		writer.writeToFile(file_name);
+#endif
 	} else {
 		ERR("writeMeshToFile(): Unknown mesh file format in file %s.", file_name.c_str());
 	}
