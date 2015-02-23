c34c2ac5be847b673b06270cd16a6cb06176fc84
Lars Bilke <lars.bilke@ufz.de>
2015-02-23 12:40:52 +0100

    Remove unnecessary nullptr checks.

--- MeshLib/MeshGenerators/VtkMeshConverter.cpp
+++ MeshLib/MeshGenerators/VtkMeshConverter.cpp
@@ -367,20 +367,14 @@ MeshLib::Mesh* VtkMeshConverter::convertUnstructuredGrid(vtkUnstructuredGrid* gr
 void VtkMeshConverter::convertScalarArrays(vtkUnstructuredGrid &grid, MeshLib::Mesh &mesh)
 {
 	vtkPointData* point_data = grid.GetPointData();
-	if (point_data != nullptr)
-	{
-		unsigned const n_point_arrays = static_cast<unsigned>(point_data->GetNumberOfArrays());
-		for (unsigned i=0; i<n_point_arrays; ++i)
-			convertArray(point_data->GetArray(i), mesh.getProperties(), MeshLib::MeshItemType::Node);
-	}
+	unsigned const n_point_arrays = static_cast<unsigned>(point_data->GetNumberOfArrays());
+	for (unsigned i=0; i<n_point_arrays; ++i)
+		convertArray(point_data->GetArray(i), mesh.getProperties(), MeshLib::MeshItemType::Node);
 
 	vtkCellData* cell_data = grid.GetCellData();
-	if (cell_data != nullptr)
-	{
-		unsigned const n_cell_arrays = static_cast<unsigned>(cell_data->GetNumberOfArrays());
-		for (unsigned i=0; i<n_cell_arrays; ++i)
-			convertArray(cell_data->GetArray(i), mesh.getProperties(), MeshLib::MeshItemType::Cell);
-	}
+	unsigned const n_cell_arrays = static_cast<unsigned>(cell_data->GetNumberOfArrays());
+	for (unsigned i=0; i<n_cell_arrays; ++i)
+		convertArray(cell_data->GetArray(i), mesh.getProperties(), MeshLib::MeshItemType::Cell);
 }
 
 void VtkMeshConverter::convertArray(vtkDataArray* array, MeshLib::Properties &properties, MeshLib::MeshItemType type)
