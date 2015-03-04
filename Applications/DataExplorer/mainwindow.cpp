c819c016a0122a3e85e00198dadeb625a434fb43
Thomas Fischer <thomas.fischer@ufz.de>
2015-03-04 10:28:29 +0100

    [DataExplorer] Rename: Msh -> Mesh.

--- Applications/DataExplorer/mainwindow.cpp
+++ Applications/DataExplorer/mainwindow.cpp
@@ -1039,7 +1039,7 @@ void MainWindow::showMergeGeometriesDialog()
 		OGSError::box("Points are missing for\n at least one geometry.");
 }
 
-void MainWindow::showMshQualitySelectionDialog(InSituLib::VtkMappedMeshSource* mshSource)
+void MainWindow::showMeshQualitySelectionDialog(InSituLib::VtkMappedMeshSource* mshSource)
 {
 	MshQualitySelectionDialog dlg;
 	if (dlg.exec() != QDialog::Accepted)
