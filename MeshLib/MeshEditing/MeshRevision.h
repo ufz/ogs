1f5a445404b2e459f39e7b663a402814161780fb
Thomas Fischer <thomas.fischer@ufz.de>
2015-03-18 11:00:31 +0100

    [Cleanup] [MeL/MeshEditing] Remove spaces from the end of line.

--- MeshLib/MeshEditing/MeshRevision.h
+++ MeshLib/MeshEditing/MeshRevision.h
@@ -30,13 +30,13 @@ namespace MeshLib {
 namespace MeshLib {
 
 /**
- * Collapses nodes with a distance smaller min_distance and 
+ * Collapses nodes with a distance smaller min_distance and
  * reduces elements accordingly.
  */
 class MeshRevision {
 
 public:
-	/** 
+	/**
 	 * Constructor
 	 * @param mesh The mesh which is being revised. Note that node IDs in mesh are changed during computation but are resetted after the algorithms implemented here are finished
 	 */
@@ -45,9 +45,9 @@ public:
 	virtual ~MeshRevision() {}
 
 	/**
-	 * Collapsed all nodes with distance < eps but ignores elements 
+	 * Collapsed all nodes with distance < eps but ignores elements
 	 * (i.e. elements with collapsed nodes may result)
-	 * This is implicitely called when calling simplifyMesh(), so it does not need to be 
+	 * This is implicitely called when calling simplifyMesh(), so it does not need to be
 	 * called seperately when using simplifyMesh().
 	 */
 	MeshLib::Mesh* collapseNodes(const std::string &new_mesh_name, double eps);
@@ -60,16 +60,16 @@ public:
 
 	/**
 	 * Create a new mesh where all nodes with a distance < eps from each other are collapsed.
-	 * Elements are adjusted accordingly and elements with nonplanar faces are subdivided into 
+	 * Elements are adjusted accordingly and elements with nonplanar faces are subdivided into
 	 * geometrically correct elements.
 	 * @param eps Minimum distance for nodes not to be collapsed
-	 * @param min_elem_dim Minimum dimension of elements to be inserted into new mesh (i.e. 
+	 * @param min_elem_dim Minimum dimension of elements to be inserted into new mesh (i.e.
 	 *                     min_elem_dim=3 will prevent the new mesh to contain 2D elements)
 	 */
 	MeshLib::Mesh* simplifyMesh(const std::string &new_mesh_name, double eps, unsigned min_elem_dim = 1);
 
 	/**
-	 * Create a new mesh where all elements with nonplanar faces are subdivided into simpler 
+	 * Create a new mesh where all elements with nonplanar faces are subdivided into simpler
 	 * element types. This method does not collapse or remove any nodes.
 	 */
 	MeshLib::Mesh* subdivideMesh(const std::string &new_mesh_name) const;
@@ -83,13 +83,13 @@ private:
 
 	/// Resets the node IDs of the source mesh (needs to be called after everything is done).
 	void resetNodeIDs();
-	
+
 	/// Subdivides an element if it has a face that is not coplanar
 	bool subdivideElement(MeshLib::Element const*const element, const std::vector<MeshLib::Node*> &nodes, std::vector<MeshLib::Element*> &elements) const;
 
 	// Revises an element by removing collapsed nodes, using the nodes vector from the result mesh.
-	void reduceElement(MeshLib::Element const*const element, 
-	                   unsigned n_unique_nodes, 
+	void reduceElement(MeshLib::Element const*const element,
+	                   unsigned n_unique_nodes,
 	    			   const std::vector<MeshLib::Node*> &nodes,
 	    			   std::vector<MeshLib::Element*> &elements,
 	    			   unsigned min_elem_dim) const;
@@ -116,17 +116,17 @@ private:
 	/// Creates a quad or a tet, depending if the four nodes being coplanar or not (element *should* have exactly four unique nodes!)
 	MeshLib::Element* constructFourNodeElement(MeshLib::Element const*const element, const std::vector<MeshLib::Node*> &nodes, unsigned min_elem_dim = 1) const;
 
-	/** 
+	/**
 	 * Reduces a hexahedron element by removing collapsed nodes and constructing one or more new elements from the remaining nodes.
 	 * @return The number of newly created elements
 	 */
-	unsigned reduceHex(MeshLib::Element const*const hex, 
-	                   unsigned n_unique_nodes, 
-	                   const std::vector<MeshLib::Node*> &nodes, 
+	unsigned reduceHex(MeshLib::Element const*const hex,
+	                   unsigned n_unique_nodes,
+	                   const std::vector<MeshLib::Node*> &nodes,
 	                   std::vector<MeshLib::Element*> &new_elements,
 	                   unsigned min_elem_dim) const;
 	/// Reduces a pyramid element by removing collapsed nodes and constructing a new elements from the remaining nodes.
-	void reducePyramid(MeshLib::Element const*const pyramid, 
+	void reducePyramid(MeshLib::Element const*const pyramid,
 	                   unsigned n_unique_nodes,
 	                   const std::vector<MeshLib::Node*> &nodes,
 	                   std::vector<MeshLib::Element*> &new_elements,
