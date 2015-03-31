02478bc76a38a39452a7c143a14dc262b967000c
Thomas Fischer <thomas.fischer@ufz.de>
2015-03-31 10:19:10 +0200

    [T/MaL] Added test for Point3d::operator<.

--- Tests/MathLib/TestPoint3d.cpp
+++ Tests/MathLib/TestPoint3d.cpp
@@ -1,5 +1,5 @@
 /**
- * @file TestPoint.cpp
+ * @file TestPoint3d.cpp
  * @author Thomas Fischer
  * @date Nov 8, 2012
  *
@@ -13,113 +13,112 @@
 #include <ctime>
 #include "gtest/gtest.h"
 
-#include "Point.h"
-
+#include "Point3d.h"
 #include "MathTools.h"
 
-using namespace GeoLib;
+using namespace MathLib;
 
-TEST(GeoLib, PointComparisonLessEq)
+TEST(MathLib, Point3dComparisonLessEq)
 {
 	// first coordinate
-	ASSERT_TRUE(lessEq(Point(0,1,1),Point(1,1,1)));
-	ASSERT_FALSE(lessEq(Point(1,1,1),Point(0,1,1)));
+	ASSERT_TRUE(lessEq(Point3d({0,1,1}),Point3d({1,1,1})));
+	ASSERT_FALSE(lessEq(Point3d({1,1,1}),Point3d({0,1,1})));
 	// second coordinate
-	ASSERT_TRUE(lessEq(Point(1,0,1),Point(1,1,1)));
-	ASSERT_FALSE(lessEq(Point(1,1,1),Point(1,0,1)));
+	ASSERT_TRUE(lessEq(Point3d({1,0,1}),Point3d({1,1,1})));
+	ASSERT_FALSE(lessEq(Point3d({1,1,1}),Point3d({1,0,1})));
 	// third coordinate
-	ASSERT_TRUE(lessEq(Point(1,1,0),Point(1,1,1)));
-	ASSERT_FALSE(lessEq(Point(1,1,1),Point(1,1,0)));
+	ASSERT_TRUE(lessEq(Point3d({1,1,0}),Point3d({1,1,1})));
+	ASSERT_FALSE(lessEq(Point3d({1,1,1}),Point3d({1,1,0})));
 
 	const double e(2*std::numeric_limits<double>::epsilon());
 	// first coordinate
-	ASSERT_TRUE(lessEq(Point(1-e,1,1),Point(1,1,1)));
-	ASSERT_FALSE(lessEq(Point(1,1,1),Point(1-e,1,1)));
+	ASSERT_TRUE(lessEq(Point3d({1-e,1,1}),Point3d({1,1,1})));
+	ASSERT_FALSE(lessEq(Point3d({1,1,1}),Point3d({1-e,1,1})));
 	// second coordinate
-	ASSERT_TRUE(lessEq(Point(1,1-e,1),Point(1,1,1)));
-	ASSERT_FALSE(lessEq(Point(1,1,1),Point(1,1-e,1)));
+	ASSERT_TRUE(lessEq(Point3d({1,1-e,1}),Point3d({1,1,1})));
+	ASSERT_FALSE(lessEq(Point3d({1,1,1}),Point3d({1,1-e,1})));
 	// third coordinate
-	ASSERT_TRUE(lessEq(Point(1,1,1-e),Point(1,1,1)));
-	ASSERT_FALSE(lessEq(Point(1,1,1),Point(1,1,1-e)));
+	ASSERT_TRUE(lessEq(Point3d({1,1,1-e}),Point3d({1,1,1})));
+	ASSERT_FALSE(lessEq(Point3d({1,1,1}),Point3d({1,1,1-e})));
 
-	ASSERT_TRUE(lessEq(Point(1,1,1),Point(1,1,1)));
+	ASSERT_TRUE(lessEq(Point3d({1,1,1}),Point3d({1,1,1})));
 
 	const double half_eps(0.5*std::numeric_limits<double>::epsilon());
-	ASSERT_TRUE(lessEq(Point(1+half_eps,1,1),Point(1,1,1)));
-	ASSERT_TRUE(lessEq(Point(1,1+half_eps,1),Point(1,1,1)));
-	ASSERT_TRUE(lessEq(Point(1,1,1+half_eps),Point(1,1,1)));
-	ASSERT_TRUE(lessEq(Point(1,1,1),Point(1+half_eps,1,1)));
-	ASSERT_TRUE(lessEq(Point(1,1,1),Point(1,1+half_eps,1)));
-	ASSERT_TRUE(lessEq(Point(1,1,1),Point(1,1,1+half_eps)));
+	ASSERT_TRUE(lessEq(Point3d({1+half_eps,1,1}),Point3d({1,1,1})));
+	ASSERT_TRUE(lessEq(Point3d({1,1+half_eps,1}),Point3d({1,1,1})));
+	ASSERT_TRUE(lessEq(Point3d({1,1,1+half_eps}),Point3d({1,1,1})));
+	ASSERT_TRUE(lessEq(Point3d({1,1,1}),Point3d({1+half_eps,1,1})));
+	ASSERT_TRUE(lessEq(Point3d({1,1,1}),Point3d({1,1+half_eps,1})));
+	ASSERT_TRUE(lessEq(Point3d({1,1,1}),Point3d({1,1,1+half_eps})));
 
-	ASSERT_TRUE(lessEq(Point(1-half_eps,1,1),Point(1,1,1)));
-	ASSERT_TRUE(lessEq(Point(1,1-half_eps,1),Point(1,1,1)));
-	ASSERT_TRUE(lessEq(Point(1,1,1-half_eps),Point(1,1,1)));
-	ASSERT_TRUE(lessEq(Point(1,1,1),Point(1-half_eps,1,1)));
-	ASSERT_TRUE(lessEq(Point(1,1,1),Point(1,1-half_eps,1)));
-	ASSERT_TRUE(lessEq(Point(1,1,1),Point(1,1,1-half_eps)));
+	ASSERT_TRUE(lessEq(Point3d({1-half_eps,1,1}),Point3d({1,1,1})));
+	ASSERT_TRUE(lessEq(Point3d({1,1-half_eps,1}),Point3d({1,1,1})));
+	ASSERT_TRUE(lessEq(Point3d({1,1,1-half_eps}),Point3d({1,1,1})));
+	ASSERT_TRUE(lessEq(Point3d({1,1,1}),Point3d({1-half_eps,1,1})));
+	ASSERT_TRUE(lessEq(Point3d({1,1,1}),Point3d({1,1-half_eps,1})));
+	ASSERT_TRUE(lessEq(Point3d({1,1,1}),Point3d({1,1,1-half_eps})));
 
 	const double m(std::numeric_limits<double>::min());
-	ASSERT_TRUE(lessEq(Point(m+half_eps,m,m),Point(m,m,m)));
-	ASSERT_TRUE(lessEq(Point(m,m+half_eps,m),Point(m,m,m)));
-	ASSERT_TRUE(lessEq(Point(m,m,m+half_eps),Point(m,m,m)));
-	ASSERT_TRUE(lessEq(Point(m,m,m),Point(m+half_eps,m,m)));
-	ASSERT_TRUE(lessEq(Point(m,m,m),Point(m,m+half_eps,m)));
-	ASSERT_TRUE(lessEq(Point(m,m,m),Point(m,m,m+half_eps)));
+	ASSERT_TRUE(lessEq(Point3d({m+half_eps,m,m}),Point3d({m,m,m})));
+	ASSERT_TRUE(lessEq(Point3d({m,m+half_eps,m}),Point3d({m,m,m})));
+	ASSERT_TRUE(lessEq(Point3d({m,m,m+half_eps}),Point3d({m,m,m})));
+	ASSERT_TRUE(lessEq(Point3d({m,m,m}),Point3d({m+half_eps,m,m})));
+	ASSERT_TRUE(lessEq(Point3d({m,m,m}),Point3d({m,m+half_eps,m})));
+	ASSERT_TRUE(lessEq(Point3d({m,m,m}),Point3d({m,m,m+half_eps})));
 
 	const double zero(0.0);
-	ASSERT_TRUE(lessEq(Point(zero+half_eps,zero,zero),Point(zero,zero,zero)));
-	ASSERT_TRUE(lessEq(Point(zero,zero+half_eps,zero),Point(zero,zero,zero)));
-	ASSERT_TRUE(lessEq(Point(zero,zero,zero+half_eps),Point(zero,zero,zero)));
-	ASSERT_TRUE(lessEq(Point(zero,zero,zero),Point(zero+half_eps,zero,zero)));
-	ASSERT_TRUE(lessEq(Point(zero,zero,zero),Point(zero,zero+half_eps,zero)));
-	ASSERT_TRUE(lessEq(Point(zero,zero,zero),Point(zero,zero,zero+half_eps)));
+	ASSERT_TRUE(lessEq(Point3d({zero+half_eps,zero,zero}),Point3d({zero,zero,zero})));
+	ASSERT_TRUE(lessEq(Point3d({zero,zero+half_eps,zero}),Point3d({zero,zero,zero})));
+	ASSERT_TRUE(lessEq(Point3d({zero,zero,zero+half_eps}),Point3d({zero,zero,zero})));
+	ASSERT_TRUE(lessEq(Point3d({zero,zero,zero}),Point3d({zero+half_eps,zero,zero})));
+	ASSERT_TRUE(lessEq(Point3d({zero,zero,zero}),Point3d({zero,zero+half_eps,zero})));
+	ASSERT_TRUE(lessEq(Point3d({zero,zero,zero}),Point3d({zero,zero,zero+half_eps})));
 
-	ASSERT_TRUE(lessEq(Point(m+half_eps,m,m),Point(zero,zero,zero)));
-	ASSERT_TRUE(lessEq(Point(m,m+half_eps,m),Point(zero,zero,zero)));
-	ASSERT_TRUE(lessEq(Point(m,m,m+half_eps),Point(zero,zero,zero)));
-	ASSERT_TRUE(lessEq(Point(m,m,m),Point(zero+half_eps,zero,zero)));
-	ASSERT_TRUE(lessEq(Point(m,m,m),Point(zero,zero+half_eps,zero)));
-	ASSERT_TRUE(lessEq(Point(m,m,m),Point(zero,zero,zero+half_eps)));
+	ASSERT_TRUE(lessEq(Point3d({m+half_eps,m,m}),Point3d({zero,zero,zero})));
+	ASSERT_TRUE(lessEq(Point3d({m,m+half_eps,m}),Point3d({zero,zero,zero})));
+	ASSERT_TRUE(lessEq(Point3d({m,m,m+half_eps}),Point3d({zero,zero,zero})));
+	ASSERT_TRUE(lessEq(Point3d({m,m,m}),Point3d({zero+half_eps,zero,zero})));
+	ASSERT_TRUE(lessEq(Point3d({m,m,m}),Point3d({zero,zero+half_eps,zero})));
+	ASSERT_TRUE(lessEq(Point3d({m,m,m}),Point3d({zero,zero,zero+half_eps})));
 
-	ASSERT_TRUE(lessEq(Point(zero+half_eps,zero,zero),Point(m,m,m)));
-	ASSERT_TRUE(lessEq(Point(zero,zero+half_eps,zero),Point(m,m,m)));
-	ASSERT_TRUE(lessEq(Point(zero,zero,zero+half_eps),Point(m,m,m)));
-	ASSERT_TRUE(lessEq(Point(zero,zero,zero),Point(m+half_eps,m,m)));
-	ASSERT_TRUE(lessEq(Point(zero,zero,zero),Point(m,m+half_eps,m)));
-	ASSERT_TRUE(lessEq(Point(zero,zero,zero),Point(m,m,m+half_eps)));
+	ASSERT_TRUE(lessEq(Point3d({zero+half_eps,zero,zero}),Point3d({m,m,m})));
+	ASSERT_TRUE(lessEq(Point3d({zero,zero+half_eps,zero}),Point3d({m,m,m})));
+	ASSERT_TRUE(lessEq(Point3d({zero,zero,zero+half_eps}),Point3d({m,m,m})));
+	ASSERT_TRUE(lessEq(Point3d({zero,zero,zero}),Point3d({m+half_eps,m,m})));
+	ASSERT_TRUE(lessEq(Point3d({zero,zero,zero}),Point3d({m,m+half_eps,m})));
+	ASSERT_TRUE(lessEq(Point3d({zero,zero,zero}),Point3d({m,m,m+half_eps})));
 
-	ASSERT_TRUE(lessEq(Point(half_eps+half_eps,half_eps,half_eps),Point(half_eps,half_eps,half_eps)));
-	ASSERT_TRUE(lessEq(Point(half_eps,half_eps+half_eps,zero),Point(half_eps,half_eps,half_eps)));
-	ASSERT_TRUE(lessEq(Point(zero,zero,zero+half_eps),Point(half_eps,half_eps,half_eps)));
-	ASSERT_TRUE(lessEq(Point(half_eps,half_eps,half_eps),Point(half_eps+half_eps,half_eps,half_eps)));
-	ASSERT_TRUE(lessEq(Point(half_eps,half_eps,half_eps),Point(half_eps,half_eps+half_eps,half_eps)));
-	ASSERT_TRUE(lessEq(Point(half_eps,half_eps,half_eps),Point(half_eps,half_eps,half_eps+half_eps)));
+	ASSERT_TRUE(lessEq(Point3d({half_eps+half_eps,half_eps,half_eps}),Point3d({half_eps,half_eps,half_eps})));
+	ASSERT_TRUE(lessEq(Point3d({half_eps,half_eps+half_eps,zero}),Point3d({half_eps,half_eps,half_eps})));
+	ASSERT_TRUE(lessEq(Point3d({zero,zero,zero+half_eps}),Point3d({half_eps,half_eps,half_eps})));
+	ASSERT_TRUE(lessEq(Point3d({half_eps,half_eps,half_eps}),Point3d({half_eps+half_eps,half_eps,half_eps})));
+	ASSERT_TRUE(lessEq(Point3d({half_eps,half_eps,half_eps}),Point3d({half_eps,half_eps+half_eps,half_eps})));
+	ASSERT_TRUE(lessEq(Point3d({half_eps,half_eps,half_eps}),Point3d({half_eps,half_eps,half_eps+half_eps})));
 
-	ASSERT_TRUE(lessEq(Point(10.0+half_eps,10.0,10.0),Point(10.0,10.0,10.0)));
-	ASSERT_TRUE(lessEq(Point(10.0,10.0+half_eps,10.0),Point(10.0,10.0,10.0)));
-	ASSERT_TRUE(lessEq(Point(10.0,10.0,10.0+half_eps),Point(10.0,10.0,10.0)));
-	ASSERT_TRUE(lessEq(Point(10.0,10.0,10.0),Point(10.0+half_eps,10.0,10.0)));
-	ASSERT_TRUE(lessEq(Point(10.0,10.0,10.0),Point(10.0,10.0+half_eps,10.0)));
-	ASSERT_TRUE(lessEq(Point(10.0,10.0,10.0),Point(10.0,10.0,10.0+half_eps)));
+	ASSERT_TRUE(lessEq(Point3d({10.0+half_eps,10.0,10.0}),Point3d({10.0,10.0,10.0})));
+	ASSERT_TRUE(lessEq(Point3d({10.0,10.0+half_eps,10.0}),Point3d({10.0,10.0,10.0})));
+	ASSERT_TRUE(lessEq(Point3d({10.0,10.0,10.0+half_eps}),Point3d({10.0,10.0,10.0})));
+	ASSERT_TRUE(lessEq(Point3d({10.0,10.0,10.0}),Point3d({10.0+half_eps,10.0,10.0})));
+	ASSERT_TRUE(lessEq(Point3d({10.0,10.0,10.0}),Point3d({10.0,10.0+half_eps,10.0})));
+	ASSERT_TRUE(lessEq(Point3d({10.0,10.0,10.0}),Point3d({10.0,10.0,10.0+half_eps})));
 }
 
-TEST(GeoLib, PointComparisonOperatorLessEq)
+TEST(MathLib, Point3dComparisonOperatorLessEq)
 {
 	const double my_eps(std::numeric_limits<double>::epsilon());
-	ASSERT_FALSE(Point(1.0+my_eps,1.0,1.0) <= Point(1.0,1.0,1.0));
-	ASSERT_FALSE(Point(1.0,1.0+my_eps,1.0) <= Point(1.0,1.0,1.0));
-	ASSERT_FALSE(Point(1.0,1.0,1.0+my_eps) <= Point(1.0,1.0,1.0));
-	ASSERT_TRUE(Point(1.0,1.0,1.0) <= Point(1.0+my_eps,1.0,1.0));
-	ASSERT_TRUE(Point(1.0,1.0,1.0) <= Point(1.0,1.0+my_eps,1.0));
-	ASSERT_TRUE(Point(1.0,1.0,1.0) <= Point(1.0,1.0,1.0+my_eps));
+	ASSERT_FALSE(Point3d({1.0+my_eps,1.0,1.0}) <= Point3d({1.0,1.0,1.0}));
+	ASSERT_FALSE(Point3d({1.0,1.0+my_eps,1.0}) <= Point3d({1.0,1.0,1.0}));
+	ASSERT_FALSE(Point3d({1.0,1.0,1.0+my_eps}) <= Point3d({1.0,1.0,1.0}));
+	ASSERT_TRUE(Point3d({1.0,1.0,1.0}) <= Point3d({1.0+my_eps,1.0,1.0}));
+	ASSERT_TRUE(Point3d({1.0,1.0,1.0}) <= Point3d({1.0,1.0+my_eps,1.0}));
+	ASSERT_TRUE(Point3d({1.0,1.0,1.0}) <= Point3d({1.0,1.0,1.0+my_eps}));
 
-	ASSERT_TRUE(Point(1.0-my_eps,1.0,1.0) <= Point(1.0,1.0,1.0));
-	ASSERT_TRUE(Point(1.0,1.0-my_eps,1.0) <= Point(1.0,1.0,1.0));
-	ASSERT_TRUE(Point(1.0,1.0,1.0-my_eps) <= Point(1.0,1.0,1.0));
-	ASSERT_FALSE(Point(1.0,1.0,1.0) <= Point(1.0-my_eps,1.0,1.0));
-	ASSERT_FALSE(Point(1.0,1.0,1.0) <= Point(1.0,1.0-my_eps,1.0));
-	ASSERT_FALSE(Point(1.0,1.0,1.0) <= Point(1.0,1.0,1.0-my_eps));
+	ASSERT_TRUE(Point3d({1.0-my_eps,1.0,1.0}) <= Point3d({1.0,1.0,1.0}));
+	ASSERT_TRUE(Point3d({1.0,1.0-my_eps,1.0}) <= Point3d({1.0,1.0,1.0}));
+	ASSERT_TRUE(Point3d({1.0,1.0,1.0-my_eps}) <= Point3d({1.0,1.0,1.0}));
+	ASSERT_FALSE(Point3d({1.0,1.0,1.0}) <= Point3d({1.0-my_eps,1.0,1.0}));
+	ASSERT_FALSE(Point3d({1.0,1.0,1.0}) <= Point3d({1.0,1.0-my_eps,1.0}));
+	ASSERT_FALSE(Point3d({1.0,1.0,1.0}) <= Point3d({1.0,1.0,1.0-my_eps}));
 
 	std::size_t n(10000);
 	srand ( static_cast<unsigned>(time(NULL)) );
@@ -132,42 +131,42 @@ TEST(GeoLib, PointComparisonOperatorLessEq)
 		double big_y(random_val_y * std::numeric_limits<double>::max());
 		double big_z(random_val_z * std::numeric_limits<double>::max());
 
-		ASSERT_TRUE(Point(big_x-my_eps,big_y,big_z) <= Point(big_x,big_y,big_z));
-		ASSERT_TRUE(Point(big_x,big_y-my_eps,big_z) <= Point(big_x,big_y,big_z));
-		ASSERT_TRUE(Point(big_x,big_y,big_z-my_eps) <= Point(big_x,big_y,big_z));
+		ASSERT_TRUE(Point3d({big_x-my_eps,big_y,big_z}) <= Point3d({big_x,big_y,big_z}));
+		ASSERT_TRUE(Point3d({big_x,big_y-my_eps,big_z}) <= Point3d({big_x,big_y,big_z}));
+		ASSERT_TRUE(Point3d({big_x,big_y,big_z-my_eps}) <= Point3d({big_x,big_y,big_z}));
 
-		ASSERT_TRUE(Point(big_x-my_eps,big_y-my_eps,big_z) <= Point(big_x,big_y,big_z));
-		ASSERT_TRUE(Point(big_x-my_eps,big_y,big_z-my_eps) <= Point(big_x,big_y,big_z));
-		ASSERT_TRUE(Point(big_x,big_y-my_eps,big_z-my_eps) <= Point(big_x,big_y,big_z));
+		ASSERT_TRUE(Point3d({big_x-my_eps,big_y-my_eps,big_z}) <= Point3d({big_x,big_y,big_z}));
+		ASSERT_TRUE(Point3d({big_x-my_eps,big_y,big_z-my_eps}) <= Point3d({big_x,big_y,big_z}));
+		ASSERT_TRUE(Point3d({big_x,big_y-my_eps,big_z-my_eps}) <= Point3d({big_x,big_y,big_z}));
 
-		ASSERT_TRUE(Point(big_x-my_eps,big_y-my_eps,big_z-my_eps) <= Point(big_x,big_y,big_z));
+		ASSERT_TRUE(Point3d({big_x-my_eps,big_y-my_eps,big_z-my_eps}) <= Point3d({big_x,big_y,big_z}));
 
 		double small_x(random_val_x * std::numeric_limits<double>::epsilon());
 		double small_y(random_val_y * std::numeric_limits<double>::epsilon());
 		double small_z(random_val_z * std::numeric_limits<double>::epsilon());
 
-		ASSERT_TRUE(Point(small_x-my_eps,small_y,small_z) <= Point(small_x,small_y,small_z));
-		ASSERT_TRUE(Point(small_x,small_y-my_eps,small_z) <= Point(small_x,small_y,small_z));
-		ASSERT_TRUE(Point(small_x,small_y,small_z-my_eps) <= Point(small_x,small_y,small_z));
+		ASSERT_TRUE(Point3d({small_x-my_eps,small_y,small_z}) <= Point3d({small_x,small_y,small_z}));
+		ASSERT_TRUE(Point3d({small_x,small_y-my_eps,small_z}) <= Point3d({small_x,small_y,small_z}));
+		ASSERT_TRUE(Point3d({small_x,small_y,small_z-my_eps}) <= Point3d({small_x,small_y,small_z}));
 
-		ASSERT_TRUE(Point(small_x-my_eps,small_y-my_eps,small_z) <= Point(small_x,small_y,small_z));
-		ASSERT_TRUE(Point(small_x-my_eps,small_y,small_z-my_eps) <= Point(small_x,small_y,small_z));
-		ASSERT_TRUE(Point(small_x,small_y-my_eps,small_z-my_eps) <= Point(small_x,small_y,small_z));
+		ASSERT_TRUE(Point3d({small_x-my_eps,small_y-my_eps,small_z}) <= Point3d({small_x,small_y,small_z}));
+		ASSERT_TRUE(Point3d({small_x-my_eps,small_y,small_z-my_eps}) <= Point3d({small_x,small_y,small_z}));
+		ASSERT_TRUE(Point3d({small_x,small_y-my_eps,small_z-my_eps}) <= Point3d({small_x,small_y,small_z}));
 
-		ASSERT_TRUE(Point(small_x-my_eps,small_y-my_eps,small_z-my_eps) <= Point(small_x,small_y,small_z));
+		ASSERT_TRUE(Point3d({small_x-my_eps,small_y-my_eps,small_z-my_eps}) <= Point3d({small_x,small_y,small_z}));
 	}
 }
 
 // test for operator==
-TEST(GeoLib, PointComparisonOperatorEqual)
+TEST(MathLib, Point3dComparisonOperatorEqual)
 {
 	srand(static_cast<unsigned>(time(nullptr)));
 	double x0(((double)(rand()) / RAND_MAX - 0.5));
 	double x1(((double)(rand()) / RAND_MAX - 0.5));
 	double x2(((double)(rand()) / RAND_MAX - 0.5));
 
-	GeoLib::Point a({x0, x1, x2});
-	GeoLib::Point b({x0, x1, x2});
+	MathLib::Point3d a({x0, x1, x2});
+	MathLib::Point3d b({x0, x1, x2});
 	ASSERT_TRUE(((a <= b) && (b <= a)) == (a == b));
 	ASSERT_TRUE(a == b);
 	ASSERT_TRUE((lessEq(a,b) && lessEq(b,a)) == (a == b));
@@ -180,27 +179,60 @@ TEST(GeoLib, PointComparisonOperatorEqual)
 	ASSERT_TRUE(((a <= b) && (b <= a)) == (a == b));
 
 	tol = std::numeric_limits<double>::epsilon();
-	ASSERT_FALSE(Point({tol,1.0,1.0}) == Point({1.0,1.0,1.0}));
-	ASSERT_FALSE(Point({1.0,tol,1.0}) == Point({1.0,1.0,1.0}));
-	ASSERT_FALSE(Point({1.0,1.0,tol}) == Point({1.0,1.0,1.0}));
+	ASSERT_FALSE(Point3d({tol,1.0,1.0}) == Point3d({1.0,1.0,1.0}));
+	ASSERT_FALSE(Point3d({1.0,tol,1.0}) == Point3d({1.0,1.0,1.0}));
+	ASSERT_FALSE(Point3d({1.0,1.0,tol}) == Point3d({1.0,1.0,1.0}));
 
-	ASSERT_FALSE(Point({1.0,1.0,1.0}) == Point({1.0+tol,1.0,1.0}));
-	ASSERT_FALSE(Point({1.0,1.0,1.0}) == Point({1.0,1.0+tol,1.0}));
-	ASSERT_FALSE(Point({1.0,1.0,1.0}) == Point({1.0,1.0,1.0+tol}));
+	ASSERT_FALSE(Point3d({1.0,1.0,1.0}) == Point3d({1.0+tol,1.0,1.0}));
+	ASSERT_FALSE(Point3d({1.0,1.0,1.0}) == Point3d({1.0,1.0+tol,1.0}));
+	ASSERT_FALSE(Point3d({1.0,1.0,1.0}) == Point3d({1.0,1.0,1.0+tol}));
 
 	// very small difference in one coordinate
 	tol = std::numeric_limits<double>::min();
-	ASSERT_TRUE(Point({tol,0.0,0.0}) == Point({0.0,0.0,0.0}));
-	ASSERT_TRUE(Point({0.0,tol,0.0}) == Point({0.0,0.0,0.0}));
-	ASSERT_TRUE(Point({0.0,0.0,tol}) == Point({0.0,0.0,0.0}));
+	ASSERT_TRUE(Point3d({tol,0.0,0.0}) == Point3d({0.0,0.0,0.0}));
+	ASSERT_TRUE(Point3d({0.0,tol,0.0}) == Point3d({0.0,0.0,0.0}));
+	ASSERT_TRUE(Point3d({0.0,0.0,tol}) == Point3d({0.0,0.0,0.0}));
 
-	ASSERT_TRUE(Point({0.0,0.0,0.0}) == Point({tol,0.0,0.0}));
-	ASSERT_TRUE(Point({0.0,0.0,0.0}) == Point({0.0,tol,0.0}));
-	ASSERT_TRUE(Point({0.0,0.0,0.0}) == Point({0.0,0.0,tol}));
+	ASSERT_TRUE(Point3d({0.0,0.0,0.0}) == Point3d({tol,0.0,0.0}));
+	ASSERT_TRUE(Point3d({0.0,0.0,0.0}) == Point3d({0.0,tol,0.0}));
+	ASSERT_TRUE(Point3d({0.0,0.0,0.0}) == Point3d({0.0,0.0,tol}));
 
-	a = Point({0.0,0.0,0.0});
-	b = Point({0.0,0.0,0.0});
+	a = Point3d({0.0,0.0,0.0});
+	b = Point3d({0.0,0.0,0.0});
 	a[0] = pow(std::numeric_limits<double>::epsilon(),5);
 	ASSERT_TRUE((lessEq(a,b) && lessEq(b,a)) == (a == b));
+}
+
+
+// test for operator<
+TEST(MathLib, Point3dComparisonOperatorLess)
+{
+	srand(static_cast<unsigned>(time(nullptr)));
+	double x0(((double)(rand()) / RAND_MAX - 0.5));
+	double x1(((double)(rand()) / RAND_MAX - 0.5));
+	double x2(((double)(rand()) / RAND_MAX - 0.5));
+
+	MathLib::Point3d a({x0, x1, x2});
+	MathLib::Point3d b({x0, x1, x2});
+	ASSERT_FALSE((a < b) && (b < a));
+
+	double tol = std::numeric_limits<double>::epsilon();
+	ASSERT_TRUE(Point3d({tol,1.0,1.0}) < Point3d({1.0,1.0,1.0}));
+	ASSERT_TRUE(Point3d({1.0,tol,1.0}) < Point3d({1.0,1.0,1.0}));
+	ASSERT_TRUE(Point3d({1.0,1.0,tol}) < Point3d({1.0,1.0,1.0}));
+
+	// very small difference in one coordinate
+	ASSERT_TRUE(Point3d({1.0,1.0,1.0}) < Point3d({1.0+tol,1.0,1.0}));
+	ASSERT_TRUE(Point3d({1.0,1.0,1.0}) < Point3d({1.0,1.0+tol,1.0}));
+	ASSERT_TRUE(Point3d({1.0,1.0,1.0}) < Point3d({1.0,1.0,1.0+tol}));
+
+	tol = std::numeric_limits<double>::min();
+	ASSERT_FALSE(Point3d({tol,0.0,0.0}) < Point3d({0.0,0.0,0.0}));
+	ASSERT_FALSE(Point3d({0.0,tol,0.0}) < Point3d({0.0,0.0,0.0}));
+	ASSERT_FALSE(Point3d({0.0,0.0,tol}) < Point3d({0.0,0.0,0.0}));
+
+	ASSERT_TRUE(Point3d({0.0,0.0,0.0}) < Point3d({tol,0.0,0.0}));
+	ASSERT_TRUE(Point3d({0.0,0.0,0.0}) < Point3d({0.0,tol,0.0}));
+	ASSERT_TRUE(Point3d({0.0,0.0,0.0}) < Point3d({0.0,0.0,tol}));
 }
 
