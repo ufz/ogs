d22718ee73607f3dccfdf3ae5871356839132beb
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2015-04-23 14:30:05 +0200

    support quadratic cell types in VtkMappedMesh

--- InSituLib/VtkMappedMesh.h
+++ InSituLib/VtkMappedMesh.h
@@ -71,34 +71,64 @@ private:
 	const std::vector<MeshLib::Element*>* _elements;
 	vtkIdType NumberOfCells;
 
-	static MeshElemType VtkCellTypeToOGS(int type)
+	static CellType VtkCellTypeToOGS(int type)
 	{
-		MeshElemType ogs;
+		CellType ogs;
 		switch (type)
 		{
 			case VTK_LINE:
-				ogs = MeshElemType::LINE;
+				ogs = CellType::LINE2;
+				break;
+			case VTK_QUADRATIC_EDGE:
+				ogs = CellType::LINE3;
 				break;
 			case VTK_TRIANGLE:
-				ogs = MeshElemType::TRIANGLE;
+				ogs = CellType::TRI3;
+				break;
+			case VTK_QUADRATIC_TRIANGLE:
+				ogs = CellType::TRI6;
 				break;
 			case VTK_QUAD:
-				ogs = MeshElemType::QUAD;
+				ogs = CellType::QUAD4;
+				break;
+			case VTK_QUADRATIC_QUAD:
+				ogs = CellType::QUAD8;
+				break;
+			case VTK_BIQUADRATIC_QUAD:
+				ogs = CellType::QUAD9;
 				break;
 			case VTK_HEXAHEDRON:
-				ogs = MeshElemType::HEXAHEDRON;
+				ogs = CellType::HEX8;
+				break;
+			case VTK_QUADRATIC_HEXAHEDRON:
+				ogs = CellType::HEX20;
+				break;
+			case VTK_TRIQUADRATIC_HEXAHEDRON:
+				ogs = CellType::HEX27;
 				break;
 			case VTK_TETRA:
-				ogs = MeshElemType::TETRAHEDRON;
+				ogs = CellType::TET4;
+				break;
+			case VTK_QUADRATIC_TETRA:
+				ogs = CellType::TET10;
 				break;
 			case VTK_WEDGE:
-				ogs = MeshElemType::PRISM;
+				ogs = CellType::PRISM6;
+				break;
+			case VTK_QUADRATIC_WEDGE:
+				ogs = CellType::PRISM15;
+				break;
+			case VTK_BIQUADRATIC_QUADRATIC_WEDGE:
+				ogs = CellType::PRISM18;
 				break;
 			case VTK_PYRAMID:
-				ogs = MeshElemType::PYRAMID;
+				ogs = CellType::PYRAMID5;
+				break;
+			case VTK_QUADRATIC_PYRAMID:
+				ogs = CellType::PYRAMID13;
 				break;
 			default:
-				ogs = MeshElemType::INVALID;
+				ogs = CellType::INVALID;
 				break;
 		}
 		return ogs;
