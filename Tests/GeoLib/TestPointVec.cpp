54c681decbb79573978c9d69c1f3bb1b4390dd77
Thomas Fischer <thomas.fischer@ufz.de>
2015-05-29 07:20:32 +0200

    [T] Adding ids to points in the TestPointVec test.

--- Tests/GeoLib/TestPointVec.cpp
+++ Tests/GeoLib/TestPointVec.cpp
@@ -57,7 +57,7 @@ TEST_F(PointVecTest, TestPointVecCtorEmpty)
 // Testing input vector with single point.
 TEST_F(PointVecTest, TestPointVecCtorSinglePoint)
 {
-	ps_ptr->push_back(new GeoLib::Point(0,0,0));
+	ps_ptr->push_back(new GeoLib::Point(0,0,0,0));
 	GeoLib::PointVec* point_vec = nullptr;
 	ASSERT_NO_THROW(point_vec = new GeoLib::PointVec(name, ps_ptr));
 	ASSERT_EQ(std::size_t(1), point_vec->size());
@@ -68,8 +68,8 @@ TEST_F(PointVecTest, TestPointVecCtorSinglePoint)
 // Testing input vector with two different points.
 TEST_F(PointVecTest, TestPointVecCtorTwoDiffPoints)
 {
-	ps_ptr->push_back(new GeoLib::Point(0,0,0));
-	ps_ptr->push_back(new GeoLib::Point(1,0,0));
+	ps_ptr->push_back(new GeoLib::Point(0,0,0,0));
+	ps_ptr->push_back(new GeoLib::Point(1,0,0,1));
 
 	GeoLib::PointVec* point_vec = nullptr;
 	ASSERT_NO_THROW(point_vec = new GeoLib::PointVec(name, ps_ptr));
@@ -81,8 +81,8 @@ TEST_F(PointVecTest, TestPointVecCtorTwoDiffPoints)
 // Testing input vector with two equal points.
 TEST_F(PointVecTest, TestPointVecCtorTwoEqualPoints)
 {
-	ps_ptr->push_back(new GeoLib::Point(0,0,0));
-	ps_ptr->push_back(new GeoLib::Point(0,0,0));
+	ps_ptr->push_back(new GeoLib::Point(0,0,0,0));
+	ps_ptr->push_back(new GeoLib::Point(0,0,0,1));
 
 	GeoLib::PointVec* point_vec = nullptr;
 	ASSERT_NO_THROW(point_vec = new GeoLib::PointVec(name, ps_ptr));
@@ -94,17 +94,17 @@ TEST_F(PointVecTest, TestPointVecCtorTwoEqualPoints)
 // Testing input vector with single point.
 TEST_F(PointVecTest, TestPointVecPushBack)
 {
-	ps_ptr->push_back(new GeoLib::Point(0,0,0));
-	ps_ptr->push_back(new GeoLib::Point(1,0,0));
-	ps_ptr->push_back(new GeoLib::Point(0,1,0));
-	ps_ptr->push_back(new GeoLib::Point(0,0,1));
+	ps_ptr->push_back(new GeoLib::Point(0,0,0,0));
+	ps_ptr->push_back(new GeoLib::Point(1,0,0,1));
+	ps_ptr->push_back(new GeoLib::Point(0,1,0,2));
+	ps_ptr->push_back(new GeoLib::Point(0,0,1,3));
 	GeoLib::PointVec point_vec(name, ps_ptr);
 
 	// Adding a new point with same coordinates changes nothing.
-	point_vec.push_back(new GeoLib::Point(0,0,0));
-	point_vec.push_back(new GeoLib::Point(1,0,0));
-	point_vec.push_back(new GeoLib::Point(0,1,0));
-	point_vec.push_back(new GeoLib::Point(0,0,1));
+	point_vec.push_back(new GeoLib::Point(0,0,0,4));
+	point_vec.push_back(new GeoLib::Point(1,0,0,5));
+	point_vec.push_back(new GeoLib::Point(0,1,0,6));
+	point_vec.push_back(new GeoLib::Point(0,0,1,7));
 
 	ASSERT_EQ(std::size_t(4), point_vec.size());
 }
