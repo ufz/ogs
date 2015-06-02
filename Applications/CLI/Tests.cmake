ad8beff36b511beaa9f73d831707d455fa341055
Dmitri Naumov <dmitri.naumov@ufz.de>
2015-06-02 18:04:33 +0200

    Replace numdiff tests with vtkdiff tests.

--- Applications/CLI/Tests.cmake
+++ Applications/CLI/Tests.cmake
@@ -10,8 +10,8 @@ foreach(mesh_size 1e0 1e1 1e2 1e3)
 		EXECUTABLE ogs
 		EXECUTABLE_ARGS cube_${mesh_size}.prj
 		WRAPPER time
-		TESTER numdiff
-		DIFF_DATA cube_${mesh_size}_result.dat
+		TESTER vtkdiff
+		DIFF_DATA cube_${mesh_size}.vtu Linear_1_to_minus1 Result
 		DATA cube_${mesh_size}.prj cube_1x1x1_hex_${mesh_size}.vtu cube_1x1x1.gml
 	)
 
@@ -32,8 +32,8 @@ foreach(mesh_size 1e4 2e4 3e4 4e4 5e4 1e5 1e6)
 		EXECUTABLE ogs
 		EXECUTABLE_ARGS cube_${mesh_size}.prj
 		WRAPPER time
-		TESTER numdiff
-		DIFF_DATA cube_${mesh_size}_result.dat
+		TESTER vtkdiff
+		DIFF_DATA cube_${mesh_size}.vtu Linear_1_to_minus1 Result
 		DATA cube_${mesh_size}.prj cube_1x1x1_hex_${mesh_size}.vtu cube_1x1x1.gml
 	)
 
@@ -55,8 +55,8 @@ foreach(mesh_size 1e0 1e1 1e2 1e3 1e4)
 		EXECUTABLE ogs
 		EXECUTABLE_ARGS square_${mesh_size}.prj
 		WRAPPER time
-		TESTER numdiff
-		DIFF_DATA square_${mesh_size}_result.dat
+		TESTER vtkdiff
+		DIFF_DATA square_${mesh_size}.vtu Linear_1_to_minus1 Result
 		DATA square_${mesh_size}.prj square_1x1_quad_${mesh_size}.vtu square_1x1.gml
 	)
 
@@ -77,8 +77,8 @@ foreach(mesh_size 1e5 1e6)
 		EXECUTABLE ogs
 		EXECUTABLE_ARGS square_${mesh_size}.prj
 		WRAPPER time
-		TESTER numdiff
-		DIFF_DATA square_${mesh_size}_result.dat
+		TESTER vtkdiff
+		DIFF_DATA square_${mesh_size}.vtu Linear_1_to_minus1 Result
 		DATA square_${mesh_size}.prj square_1x1_quad_${mesh_size}.vtu square_1x1.gml
 	)
 
