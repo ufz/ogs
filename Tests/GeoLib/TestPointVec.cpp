847474f80b111c50bb36e59f3df76fbf36cba815
Thomas Fischer <thomas.fischer@ufz.de>
2015-07-01 09:08:36 +0200

    [T/GL] PointVec: Checking more properties.

--- Tests/GeoLib/TestPointVec.cpp
+++ Tests/GeoLib/TestPointVec.cpp
@@ -102,13 +102,83 @@ TEST_F(PointVecTest, TestPointVecPushBack)
 	ps_ptr->push_back(new GeoLib::Point(0,0,1,3));
 	GeoLib::PointVec point_vec(name, ps_ptr);
 
-	// Adding a new point with same coordinates changes nothing.
-	point_vec.push_back(new GeoLib::Point(0,0,0,4));
-	point_vec.push_back(new GeoLib::Point(1,0,0,5));
-	point_vec.push_back(new GeoLib::Point(0,1,0,6));
-	point_vec.push_back(new GeoLib::Point(0,0,1,7));
+	ASSERT_EQ(std::size_t(0), point_vec.getIDMap()[0]);
+	ASSERT_EQ(std::size_t(1), point_vec.getIDMap()[1]);
+	ASSERT_EQ(std::size_t(2), point_vec.getIDMap()[2]);
+	ASSERT_EQ(std::size_t(3), point_vec.getIDMap()[3]);
+
+	// Adding some points that are already existing.
+	ASSERT_EQ(std::size_t(0), point_vec.push_back(new GeoLib::Point(0,0,0,4)));
+	ASSERT_EQ(std::size_t(1), point_vec.push_back(new GeoLib::Point(1,0,0,5)));
+	ASSERT_EQ(std::size_t(2), point_vec.push_back(new GeoLib::Point(0,1,0,6)));
+	ASSERT_EQ(std::size_t(3), point_vec.push_back(new GeoLib::Point(0,0,1,7)));
 
 	ASSERT_EQ(std::size_t(4), point_vec.size());
+	ASSERT_EQ(std::size_t(8), point_vec.getIDMap().size());
+
+	ASSERT_EQ(std::size_t(0), point_vec.getIDMap()[4]);
+	ASSERT_EQ(std::size_t(1), point_vec.getIDMap()[5]);
+	ASSERT_EQ(std::size_t(2), point_vec.getIDMap()[6]);
+	ASSERT_EQ(std::size_t(3), point_vec.getIDMap()[7]);
+
+	// Adding again some already existing points.
+	ASSERT_EQ(std::size_t(0), point_vec.push_back(new GeoLib::Point(0,0,0,8)));
+	ASSERT_EQ(std::size_t(1), point_vec.push_back(new GeoLib::Point(1,0,0,9)));
+	ASSERT_EQ(std::size_t(2), point_vec.push_back(new GeoLib::Point(0,1,0,10)));
+	ASSERT_EQ(std::size_t(3), point_vec.push_back(new GeoLib::Point(0,0,1,11)));
+
+	ASSERT_EQ(std::size_t(4), point_vec.size());
+	ASSERT_EQ(std::size_t(12), point_vec.getIDMap().size());
+
+	ASSERT_EQ(std::size_t(0), point_vec.getIDMap()[8]);
+	ASSERT_EQ(std::size_t(1), point_vec.getIDMap()[9]);
+	ASSERT_EQ(std::size_t(2), point_vec.getIDMap()[10]);
+	ASSERT_EQ(std::size_t(3), point_vec.getIDMap()[11]);
+
+	// Adding some new points.
+	ASSERT_EQ(std::size_t(4), point_vec.push_back(new GeoLib::Point(0.1,0.1,0.1,12)));
+	ASSERT_EQ(std::size_t(5), point_vec.push_back(new GeoLib::Point(1.1,0.1,0.1,13)));
+	ASSERT_EQ(std::size_t(6), point_vec.push_back(new GeoLib::Point(0.1,1.1,0.1,14)));
+	ASSERT_EQ(std::size_t(7), point_vec.push_back(new GeoLib::Point(0.1,0.1,1.1,15)));
+
+	ASSERT_EQ(std::size_t(8), point_vec.size());
+	ASSERT_EQ(std::size_t(16), point_vec.getIDMap().size());
+
+	ASSERT_EQ(std::size_t(4), point_vec.getIDMap()[12]);
+	ASSERT_EQ(std::size_t(5), point_vec.getIDMap()[13]);
+	ASSERT_EQ(std::size_t(6), point_vec.getIDMap()[14]);
+	ASSERT_EQ(std::size_t(7), point_vec.getIDMap()[15]);
+
+	// Adding again some already existing points.
+	ASSERT_EQ(std::size_t(0), point_vec.push_back(new GeoLib::Point(0,0,0,16)));
+	ASSERT_EQ(std::size_t(1), point_vec.push_back(new GeoLib::Point(1,0,0,17)));
+	ASSERT_EQ(std::size_t(2), point_vec.push_back(new GeoLib::Point(0,1,0,18)));
+	ASSERT_EQ(std::size_t(3), point_vec.push_back(new GeoLib::Point(0,0,1,19)));
+
+	ASSERT_EQ(std::size_t(8), point_vec.size());
+	ASSERT_EQ(std::size_t(20), point_vec.getIDMap().size());
+
+	ASSERT_EQ(std::size_t(0), point_vec.getIDMap()[16]);
+	ASSERT_EQ(std::size_t(1), point_vec.getIDMap()[17]);
+	ASSERT_EQ(std::size_t(2), point_vec.getIDMap()[18]);
+	ASSERT_EQ(std::size_t(3), point_vec.getIDMap()[19]);
+
+	ASSERT_EQ(std::size_t(0), point_vec.getIDMap()[0]);
+	ASSERT_EQ(std::size_t(1), point_vec.getIDMap()[1]);
+	ASSERT_EQ(std::size_t(2), point_vec.getIDMap()[2]);
+	ASSERT_EQ(std::size_t(3), point_vec.getIDMap()[3]);
+	ASSERT_EQ(std::size_t(0), point_vec.getIDMap()[4]);
+	ASSERT_EQ(std::size_t(1), point_vec.getIDMap()[5]);
+	ASSERT_EQ(std::size_t(2), point_vec.getIDMap()[6]);
+	ASSERT_EQ(std::size_t(3), point_vec.getIDMap()[7]);
+	ASSERT_EQ(std::size_t(0), point_vec.getIDMap()[8]);
+	ASSERT_EQ(std::size_t(1), point_vec.getIDMap()[9]);
+	ASSERT_EQ(std::size_t(2), point_vec.getIDMap()[10]);
+	ASSERT_EQ(std::size_t(3), point_vec.getIDMap()[11]);
+	ASSERT_EQ(std::size_t(4), point_vec.getIDMap()[12]);
+	ASSERT_EQ(std::size_t(5), point_vec.getIDMap()[13]);
+	ASSERT_EQ(std::size_t(6), point_vec.getIDMap()[14]);
+	ASSERT_EQ(std::size_t(7), point_vec.getIDMap()[15]);
 }
 
 // Testing random input points.
