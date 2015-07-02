f9f43a3fdf25b147774047241a3897868d44e034
Dmitry Yu. Naumov <github@naumov.de>
2015-07-02 13:23:52 +0000

    [GL] Create GeoLib::ORIGIN only once.

---
+++ GeoLib/Point.cpp
@@ -0,0 +1,17 @@
+/**
+ * \file
+ *
+ * \copyright
+ * Copyright (c) 2012-2015, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#include "Point.h"
+
+namespace GeoLib
+{
+    const Point ORIGIN(0, 0, 0);
+}
