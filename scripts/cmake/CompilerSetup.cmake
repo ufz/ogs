2454da97d2656dfb3a6c0a2658cd37d35e31e062
Dmitri Naumov <dmitri.naumov@ufz.de>
2015-07-03 18:18:46 +0200

    MSVC: Increase Number of Sections in .Obj file.

--- scripts/cmake/CompilerSetup.cmake
+++ scripts/cmake/CompilerSetup.cmake
@@ -92,7 +92,9 @@ if (WIN32)
 			-DNOMINMAX # This fixes compile errors with std::numeric_limits<T>::min() / max()
 		)
 		# Sets warning level 3 and ignores some warnings
-		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W3 /wd4290 /wd4267 /wd4996")
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W3 /wd4290 /wd4267 /wd4996 /bigobj")
+		# Allow big object files generated for template heavy code
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /bigobj")
 		set(CMAKE_CXX_FLAGS_RELWITHDEBINFO  "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} /ZI /Od /Ob0")
 
 		DisableCompilerFlag(DEBUG /RTC1)
