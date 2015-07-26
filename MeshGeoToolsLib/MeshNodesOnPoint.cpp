33c5e0c26c522944e115c52704f7bdcb9caae041
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2015-07-26 14:57:37 +0200

    support search only for base nodes

--- MeshGeoToolsLib/MeshNodesOnPoint.cpp
+++ MeshGeoToolsLib/MeshNodesOnPoint.cpp
@@ -18,8 +18,10 @@ MeshNodesOnPoint::MeshNodesOnPoint(MeshLib::Mesh const& mesh,
         GeoLib::Point const& pnt, double epsilon_radius, bool search_all_nodes)
 : _mesh(mesh), _pnt(pnt)
 {
-	for (auto node : mesh.getNodes())
+	auto const n_nodes = (search_all_nodes ? mesh.getNNodes() : mesh.getNBaseNodes());
+	for (std::size_t i=0; i< n_nodes; i++)
 	{
+		auto node = mesh.getNode(i);
 		double len(sqrt(MathLib::sqrDist(pnt, *node)));
 		if (len < epsilon_radius)
 			_msh_node_ids.push_back(node->getID());
