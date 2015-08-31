e118b539cc5bd2c4ede253405d49d94299786463
Dmitri Naumov <dmitri.naumov@ufz.de>
2015-08-31 15:47:38 +0200

    [PL] clang-format InitialCondition.

--- ProcessLib/InitialCondition.h
+++ ProcessLib/InitialCondition.h
@@ -17,31 +17,29 @@
 
 namespace ProcessLib
 {
-
 class InitialCondition
 {
 public:
-    virtual ~InitialCondition() = default;
+	virtual ~InitialCondition() = default;
 };
-
 
 class UniformInitialCondition : public InitialCondition
 {
-    using ConfigTree = boost::property_tree::ptree;
-public:
-    UniformInitialCondition(ConfigTree const& config)
-    {
-        DBUG("Constructing Uniform initial condition");
+	using ConfigTree = boost::property_tree::ptree;
 
-        _value = config.get<double>("value", 0);
-        DBUG("Read value %g", _value);
-    }
+public:
+	UniformInitialCondition(ConfigTree const& config)
+	{
+		DBUG("Constructing Uniform initial condition");
+
+		_value = config.get<double>("value", 0);
+		DBUG("Read value %g", _value);
+	}
 
 private:
-    double _value;
+	double _value;
 };
 
-
-}   // namespace ProcessLib
+}  // namespace ProcessLib
 
 #endif  // PROCESS_LIB_INITIAL_CONDITION_H_
