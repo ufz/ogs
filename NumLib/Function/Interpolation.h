d9cd7802d6a169af3d954497ba84842c6c501f70
Christoph Lehmann <christoph.lehmann@ufz.de>
2015-09-01 15:23:52 +0200

    added copyright header

--- NumLib/Function/Interpolation.h
+++ NumLib/Function/Interpolation.h
@@ -1,3 +1,12 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2015, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
 #ifndef NUMLIB_INTERPOLATION_H
 #define NUMLIB_INTERPOLATION_H
 
