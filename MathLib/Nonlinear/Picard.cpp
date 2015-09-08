35ac538862117a62d4786cb722681934679eea62
Dmitri Naumov <dmitri.naumov@ufz.de>
2015-09-08 17:40:26 +0200

    [MaL] Add missing header. Formatting.

--- MathLib/Nonlinear/Picard.cpp
+++ MathLib/Nonlinear/Picard.cpp
@@ -1,21 +1,30 @@
+/*
+ * \copyright
+ * Copyright (c) 2012-2015, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
 #include "Picard.h"
-
-
 
 namespace MathLib
 {
-
 namespace Nonlinear
 {
-
 Picard::Picard()
-: _normType(VecNormType::INFINITY_N),
-  _abs_tol(std::numeric_limits<double>::max()), _rel_tol(1e-6), _max_itr(25),
-  _printErrors(false), _n_iterations(0), _abs_error(.0), _rel_error(.0)
+    : _normType(VecNormType::INFINITY_N),
+      _abs_tol(std::numeric_limits<double>::max()),
+      _rel_tol(1e-6),
+      _max_itr(25),
+      _printErrors(false),
+      _n_iterations(0),
+      _abs_error(.0),
+      _rel_error(.0)
 {
 }
 
-} // namespace Nonlinear
+}  // namespace Nonlinear
 
-} // namespace MathLib
-
+}  // namespace MathLib
