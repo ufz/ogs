cd9d8ac406e94ac2368fb52b00234c6c0e7c192b
Thomas Fischer <thomas.fischer@ufz.de>
2015-09-30 13:03:03 +0200

    Copyright message.

--- Applications/Utils/MeshEdit/ResetPropertiesInPolygonalRegion.cpp
+++ Applications/Utils/MeshEdit/ResetPropertiesInPolygonalRegion.cpp
@@ -1,6 +1,12 @@
 /*
- * \date 2014-09-25
  * \brief Reset material properties in meshes in a polygonal region.
+ *
+ * \copyright
+ * Copyright (c) 2012-2015, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
  */
 
 #include <algorithm>
