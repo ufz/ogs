2ee61819905b68af48c6a0039b6204fc56976be9
Dmitry Yu. Naumov <github@naumov.de>
2015-10-17 17:21:18 +0000

    [T] Localize variables' scope in TestXmlGmlReader.

--- Tests/FileIO/TestXmlGmlReader.cpp
+++ Tests/FileIO/TestXmlGmlReader.cpp
@@ -32,51 +32,69 @@ TEST(FileIO, XmlGmlWriterReaderTest)
 
 	//setup test data
 	std::string geo_name("TestData");
-	auto points = std::unique_ptr<std::vector<GeoLib::Point*>>(
-	    new std::vector<GeoLib::Point*>(10));
-	auto lines = std::unique_ptr<std::vector<GeoLib::Polyline*>>(
-	    new std::vector<GeoLib::Polyline*>(5));
-	auto sfcs = std::unique_ptr<std::vector<GeoLib::Surface*>>(
-	    new std::vector<GeoLib::Surface*>(2));
-	std::map<std::string, std::size_t>* ply_names = new std::map<std::string, std::size_t>;
 
-	(*points)[0] = new GeoLib::Point(1,1,0);
-	(*points)[1] = new GeoLib::Point(1,1,0);
-	(*points)[2] = new GeoLib::Point(1,2,0);
-	(*points)[3] = new GeoLib::Point(1,3,0);
-	(*points)[4] = new GeoLib::Point(2,1,0);
-	(*points)[5] = new GeoLib::Point(2,2,0);
-	(*points)[6] = new GeoLib::Point(2,3,0);
-	(*points)[7] = new GeoLib::Point(3,1,0);
-	(*points)[8] = new GeoLib::Point(3,2,0);
-	(*points)[9] = new GeoLib::Point(3,3,0);
-	geo_objects.addPointVec(std::move(points), geo_name);
+	{   // Create points.
+		auto points = std::unique_ptr<std::vector<GeoLib::Point*>>(
+		    new std::vector<GeoLib::Point*>(10));
+
+		(*points)[0] = new GeoLib::Point(1, 1, 0);
+		(*points)[1] = new GeoLib::Point(1, 1, 0);
+		(*points)[2] = new GeoLib::Point(1, 2, 0);
+		(*points)[3] = new GeoLib::Point(1, 3, 0);
+		(*points)[4] = new GeoLib::Point(2, 1, 0);
+		(*points)[5] = new GeoLib::Point(2, 2, 0);
+		(*points)[6] = new GeoLib::Point(2, 3, 0);
+		(*points)[7] = new GeoLib::Point(3, 1, 0);
+		(*points)[8] = new GeoLib::Point(3, 2, 0);
+		(*points)[9] = new GeoLib::Point(3, 3, 0);
+		geo_objects.addPointVec(std::move(points), geo_name);
+	}
+	auto const points = geo_objects.getPointVec(geo_name);
+
 	const std::vector<std::size_t> pnt_id_map (geo_objects.getPointVecObj(geo_name)->getIDMap());
 
-	(*lines)[0] = new GeoLib::Polyline(*points);
-	(*lines)[0]->addPoint(pnt_id_map[0]); (*lines)[0]->addPoint(pnt_id_map[2]); (*lines)[0]->addPoint(pnt_id_map[3]);
-	ply_names->insert(std::pair<std::string, std::size_t>("left", 0));
-	(*lines)[1] = new GeoLib::Polyline(*points);
-	(*lines)[1]->addPoint(pnt_id_map[4]); (*lines)[1]->addPoint(pnt_id_map[5]); (*lines)[1]->addPoint(pnt_id_map[6]);
-	ply_names->insert(std::pair<std::string, std::size_t>("center", 1));
-	(*lines)[2] = new GeoLib::Polyline(*points);
-	(*lines)[2]->addPoint(pnt_id_map[1]); (*lines)[2]->addPoint(pnt_id_map[4]);
-	(*lines)[3] = new GeoLib::Polyline(*points);
-	(*lines)[3]->addPoint(pnt_id_map[4]); (*lines)[3]->addPoint(pnt_id_map[7]);
-	(*lines)[4] = new GeoLib::Polyline(*points);
-	(*lines)[4]->addPoint(pnt_id_map[7]); (*lines)[4]->addPoint(pnt_id_map[8]); (*lines)[4]->addPoint(pnt_id_map[9]);
-	ply_names->insert(std::pair<std::string, std::size_t>("right", 4));
-	geo_objects.addPolylineVec(std::move(lines), geo_name, ply_names);
+	{   // Create polylines.
+		auto lines = std::unique_ptr<std::vector<GeoLib::Polyline*>>(
+		    new std::vector<GeoLib::Polyline*>(5));
+		std::map<std::string, std::size_t>* ply_names =
+		    new std::map<std::string, std::size_t>;
+		(*lines)[0] = new GeoLib::Polyline(*points);
+		(*lines)[0]->addPoint(pnt_id_map[0]);
+		(*lines)[0]->addPoint(pnt_id_map[2]);
+		(*lines)[0]->addPoint(pnt_id_map[3]);
+		ply_names->insert(std::pair<std::string, std::size_t>("left", 0));
+		(*lines)[1] = new GeoLib::Polyline(*points);
+		(*lines)[1]->addPoint(pnt_id_map[4]);
+		(*lines)[1]->addPoint(pnt_id_map[5]);
+		(*lines)[1]->addPoint(pnt_id_map[6]);
+		ply_names->insert(std::pair<std::string, std::size_t>("center", 1));
+		(*lines)[2] = new GeoLib::Polyline(*points);
+		(*lines)[2]->addPoint(pnt_id_map[1]);
+		(*lines)[2]->addPoint(pnt_id_map[4]);
+		(*lines)[3] = new GeoLib::Polyline(*points);
+		(*lines)[3]->addPoint(pnt_id_map[4]);
+		(*lines)[3]->addPoint(pnt_id_map[7]);
+		(*lines)[4] = new GeoLib::Polyline(*points);
+		(*lines)[4]->addPoint(pnt_id_map[7]);
+		(*lines)[4]->addPoint(pnt_id_map[8]);
+		(*lines)[4]->addPoint(pnt_id_map[9]);
+		ply_names->insert(std::pair<std::string, std::size_t>("right", 4));
+		geo_objects.addPolylineVec(std::move(lines), geo_name, ply_names);
+	}
 
-	(*sfcs)[0] = new GeoLib::Surface(*points);
-	(*sfcs)[0]->addTriangle(pnt_id_map[1],pnt_id_map[4],pnt_id_map[2]);
-	(*sfcs)[0]->addTriangle(pnt_id_map[2],pnt_id_map[4],pnt_id_map[5]);
-	(*sfcs)[0]->addTriangle(pnt_id_map[2],pnt_id_map[5],pnt_id_map[3]);
-	(*sfcs)[0]->addTriangle(pnt_id_map[3],pnt_id_map[5],pnt_id_map[6]);
-	(*sfcs)[1] = new GeoLib::Surface(*points);
-	(*sfcs)[1]->addTriangle(pnt_id_map[4],pnt_id_map[7],pnt_id_map[9]);
-	(*sfcs)[1]->addTriangle(pnt_id_map[4],pnt_id_map[9],pnt_id_map[6]);
-	geo_objects.addSurfaceVec(std::move(sfcs), geo_name);
+	{   // Create surfaces.
+		auto sfcs = std::unique_ptr<std::vector<GeoLib::Surface*>>(
+		    new std::vector<GeoLib::Surface*>(2));
+		(*sfcs)[0] = new GeoLib::Surface(*points);
+		(*sfcs)[0]->addTriangle(pnt_id_map[1], pnt_id_map[4], pnt_id_map[2]);
+		(*sfcs)[0]->addTriangle(pnt_id_map[2], pnt_id_map[4], pnt_id_map[5]);
+		(*sfcs)[0]->addTriangle(pnt_id_map[2], pnt_id_map[5], pnt_id_map[3]);
+		(*sfcs)[0]->addTriangle(pnt_id_map[3], pnt_id_map[5], pnt_id_map[6]);
+		(*sfcs)[1] = new GeoLib::Surface(*points);
+		(*sfcs)[1]->addTriangle(pnt_id_map[4], pnt_id_map[7], pnt_id_map[9]);
+		(*sfcs)[1]->addTriangle(pnt_id_map[4], pnt_id_map[9], pnt_id_map[6]);
+		geo_objects.addSurfaceVec(std::move(sfcs), geo_name);
+	}
 
 	FileIO::XmlGmlInterface xml(geo_objects);
 	xml.setNameForExport(geo_name);
