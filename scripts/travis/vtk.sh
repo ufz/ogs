f2dd5a6fd4952edd2ea681b7a6cbd044f8cfa21c
Lars Bilke <lars.bilke@ufz.de>
2015-11-10 14:55:54 +0100

    Removed no-check-cert flag from wget calls.

--- scripts/travis/vtk.sh
+++ scripts/travis/vtk.sh
@@ -2,7 +2,7 @@
 set -e
 if [ ! -d "$HOME/VTK-Install/include" ]; then
 	cd $HOME
-	wget --no-check-certificate http://opengeosys.s3.amazonaws.com/ogs6-lib-sources/vtk-6.3.0.tar.gz
+	wget http://opengeosys.s3.amazonaws.com/ogs6-lib-sources/vtk-6.3.0.tar.gz
 	tar -xzf vtk-6.3.0.tar.gz
 else
   echo 'Using cached vtk directory.';
