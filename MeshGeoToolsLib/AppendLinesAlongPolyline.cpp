8c4adbc8049f7bc86d6639a261f8043d2ec23d54
Thomas Fischer <thomas.fischer@ufz.de>
2015-11-27 08:02:50 +0100

    [MGTL] Use PropertyVector for material ids.

--- MeshGeoToolsLib/AppendLinesAlongPolyline.cpp
+++ MeshGeoToolsLib/AppendLinesAlongPolyline.cpp
@@ -32,14 +32,18 @@ MeshLib::Mesh* appendLinesAlongPolylines(const MeshLib::Mesh &mesh, const GeoLib
 	// copy existing nodes and elements
 	std::vector<MeshLib::Node*> vec_new_nodes = MeshLib::copyNodeVector(mesh.getNodes());
 	std::vector<MeshLib::Element*> vec_new_eles = MeshLib::copyElementVector(mesh.getElements(), vec_new_nodes);
-	unsigned max_matID = 0;
 
-	auto materialIds = mesh.getProperties().getPropertyVector<int>("MaterialIDs");
-	if (materialIds)
+	std::vector<int> new_mat_ids;
 	{
-		for (auto e : vec_new_eles)
-			max_matID = std::max(max_matID, static_cast<unsigned>((*materialIds)[e->getID()]));
+		auto mat_ids = mesh.getProperties().getPropertyVector<int>("MaterialIDs");
+		if (mat_ids) {
+			new_mat_ids.resize((*mat_ids).size());
+			std::copy((*mat_ids).cbegin(), (*mat_ids).cend(), new_mat_ids.begin());
+		}
 	}
+	int max_matID(0);
+	if (!new_mat_ids.empty())
+		max_matID = *(std::max_element(new_mat_ids.cbegin(), new_mat_ids.cend()));
 
 	const std::size_t n_ply (ply_vec.size());
 	// for each polyline
@@ -62,13 +66,23 @@ MeshLib::Mesh* appendLinesAlongPolylines(const MeshLib::Mesh &mesh, const GeoLib
 			std::array<MeshLib::Node*, 2> element_nodes;
 			element_nodes[0] = vec_new_nodes[vec_nodes_on_ply[i]];
 			element_nodes[1] = vec_new_nodes[vec_nodes_on_ply[i+1]];
-			vec_new_eles.push_back(new MeshLib::Line(element_nodes, max_matID+k+1));
+			vec_new_eles.push_back(
+				new MeshLib::Line(element_nodes, vec_new_eles.size()));
+			new_mat_ids.push_back(max_matID+k+1);
 		}
 	}
 
 	// generate a mesh
-	const std::string new_mesh_name = mesh.getName() + "_with_lines";
-	return new MeshLib::Mesh(new_mesh_name, vec_new_nodes, vec_new_eles);
+	const std::string name = mesh.getName() + "_with_lines";
+	MeshLib::Mesh* new_mesh(new MeshLib::Mesh(name, vec_new_nodes, vec_new_eles));
+	auto opt_mat_pv = new_mesh->getProperties().createNewPropertyVector<int>(
+		"MaterialIDs", MeshLib::MeshItemType::Cell);
+	if (opt_mat_pv) {
+		auto & mat_pv = *opt_mat_pv;
+		mat_pv.resize(new_mat_ids.size());
+		std::copy(new_mat_ids.cbegin(), new_mat_ids.cend(), mat_pv.begin());
+	}
+	return new_mesh;
 }
 
 } // MeshGeoToolsLib
