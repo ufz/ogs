e7f112ed6162714d5d934fc15d686b794bdd84e8
Thomas Fischer <thomas.fischer@ufz.de>
2015-12-16 14:13:03 +0100

    [T/GL] TestAABB: Resize the original bounding box within the test like in AABB itself.

--- Tests/GeoLib/TestAABB.cpp
+++ Tests/GeoLib/TestAABB.cpp
@@ -30,8 +30,8 @@ TEST(GeoLibAABB, RandomNumberOfPointersToRandomPoints)
 	 srand ( static_cast<unsigned>(time(nullptr)) );
 	 int n (rand() % 100000);
 	 int box_size (rand());
-	 int half_box_size(box_size/2);
-	 int minus_half_box_size(-half_box_size);
+	 double half_box_size(box_size/2);
+	 double minus_half_box_size(-half_box_size);
 
 	 // fill list with points
 	 std::list<GeoLib::Point*> pnts_list;
@@ -48,6 +48,9 @@ TEST(GeoLibAABB, RandomNumberOfPointersToRandomPoints)
 	 ASSERT_LE(minus_half_box_size, min_pnt[0]) << "coordinate 0 of min_pnt is smaller than " << minus_half_box_size;
 	 ASSERT_LE(minus_half_box_size, min_pnt[1]) << "coordinate 1 of min_pnt is smaller than " << minus_half_box_size;
 	 ASSERT_LE(minus_half_box_size, min_pnt[2]) << "coordinate 2 of min_pnt is smaller than " << minus_half_box_size;
+
+	// since the interval is half-open we have to move the upper bound also
+	 half_box_size = std::nexttoward(half_box_size, std::numeric_limits<double>::max());
 	 ASSERT_GE(half_box_size, max_pnt[0]) << "coordinate 0 of max_pnt is greater than " << half_box_size;
 	 ASSERT_GE(half_box_size, max_pnt[1]) << "coordinate 1 of max_pnt is greater than " << half_box_size;
 	 ASSERT_GE(half_box_size, max_pnt[2]) << "coordinate 2 of max_pnt is greater than " << half_box_size;
@@ -63,8 +66,8 @@ TEST(GeoLibAABB, RandomNumberOfPointsRandomPointInAList)
 	 srand ( static_cast<unsigned>(time(nullptr)) );
 	 int n (rand() % 1000000);
 	 int box_size (rand());
-	 int half_box_size(box_size/2);
-	 int minus_half_box_size(-half_box_size);
+	 double half_box_size(box_size/2);
+	 double minus_half_box_size(-half_box_size);
 
 	 // fill list with points
 	 std::list<GeoLib::Point> pnts_list;
@@ -81,6 +84,9 @@ TEST(GeoLibAABB, RandomNumberOfPointsRandomPointInAList)
 	 ASSERT_LE(minus_half_box_size, min_pnt[0]) << "coordinate 0 of min_pnt is smaller than " << minus_half_box_size;
 	 ASSERT_LE(minus_half_box_size, min_pnt[1]) << "coordinate 1 of min_pnt is smaller than " << minus_half_box_size;
 	 ASSERT_LE(minus_half_box_size, min_pnt[2]) << "coordinate 2 of min_pnt is smaller than " << minus_half_box_size;
+
+	// since the interval is half-open we have to move the upper bound also
+	 half_box_size = std::nexttoward(half_box_size, std::numeric_limits<double>::max());
 	 ASSERT_GE(half_box_size, max_pnt[0]) << "coordinate 0 of max_pnt is greater than " << half_box_size;
 	 ASSERT_GE(half_box_size, max_pnt[1]) << "coordinate 1 of max_pnt is greater than " << half_box_size;
 	 ASSERT_GE(half_box_size, max_pnt[2]) << "coordinate 2 of max_pnt is greater than " << half_box_size;
@@ -92,8 +98,8 @@ TEST(GeoLibAABB, RandomNumberOfPointersToRandomPointsInAVector)
 	 srand ( static_cast<unsigned>(time(nullptr)) );
 	 int n (rand() % 100000);
 	 int box_size (rand());
-	 int half_box_size(box_size/2);
-	 int minus_half_box_size(-half_box_size);
+	 double half_box_size(box_size/2);
+	 double minus_half_box_size(-half_box_size);
 
 	 // fill list with points
 	 std::vector<GeoLib::Point*> pnts;
@@ -110,6 +116,9 @@ TEST(GeoLibAABB, RandomNumberOfPointersToRandomPointsInAVector)
 	 ASSERT_LE(minus_half_box_size, min_pnt[0]) << "coordinate 0 of min_pnt is smaller than " << minus_half_box_size;
 	 ASSERT_LE(minus_half_box_size, min_pnt[1]) << "coordinate 1 of min_pnt is smaller than " << minus_half_box_size;
 	 ASSERT_LE(minus_half_box_size, min_pnt[2]) << "coordinate 2 of min_pnt is smaller than " << minus_half_box_size;
+
+	// since the interval is half-open we have to move the upper bound also
+	 half_box_size = std::nexttoward(half_box_size, std::numeric_limits<double>::max());
 	 ASSERT_GE(half_box_size, max_pnt[0]) << "coordinate 0 of max_pnt is greater than " << half_box_size;
 	 ASSERT_GE(half_box_size, max_pnt[1]) << "coordinate 1 of max_pnt is greater than " << half_box_size;
 	 ASSERT_GE(half_box_size, max_pnt[2]) << "coordinate 2 of max_pnt is greater than " << half_box_size;
@@ -125,8 +134,8 @@ TEST(GeoLibAABB, RandomNumberOfPointsRandomPointInAVector)
 	 srand ( static_cast<unsigned>(time(nullptr)) );
 	 int n (rand() % 1000000);
 	 int box_size (rand());
-	 int half_box_size(box_size/2);
-	 int minus_half_box_size(-half_box_size);
+	 double half_box_size(box_size/2);
+	 double minus_half_box_size(-half_box_size);
 
 	 // fill list with points
 	 std::list<GeoLib::Point> pnts;
@@ -143,6 +152,9 @@ TEST(GeoLibAABB, RandomNumberOfPointsRandomPointInAVector)
 	 ASSERT_LE(minus_half_box_size, min_pnt[0]) << "coordinate 0 of min_pnt is smaller than " << minus_half_box_size;
 	 ASSERT_LE(minus_half_box_size, min_pnt[1]) << "coordinate 1 of min_pnt is smaller than " << minus_half_box_size;
 	 ASSERT_LE(minus_half_box_size, min_pnt[2]) << "coordinate 2 of min_pnt is smaller than " << minus_half_box_size;
+
+	// since the interval is half-open we have to move the upper bound also
+	 half_box_size = std::nexttoward(half_box_size, std::numeric_limits<double>::max());
 	 ASSERT_GE(half_box_size, max_pnt[0]) << "coordinate 0 of max_pnt is greater than " << half_box_size;
 	 ASSERT_GE(half_box_size, max_pnt[1]) << "coordinate 1 of max_pnt is greater than " << half_box_size;
 	 ASSERT_GE(half_box_size, max_pnt[2]) << "coordinate 2 of max_pnt is greater than " << half_box_size;
@@ -156,9 +168,9 @@ TEST(GeoLibAABB, RandomNumberOfPointsRandomBox)
 	 int box_size_x (rand());
 	 int box_size_y (rand());
 	 int box_size_z (rand());
-	 int half_box_size_x(box_size_x/2);
-	 int half_box_size_y(box_size_y/2);
-	 int half_box_size_z(box_size_z/2);
+	 double half_box_size_x(box_size_x/2);
+	 double half_box_size_y(box_size_y/2);
+	 double half_box_size_z(box_size_z/2);
 	 int minus_half_box_size_x(-half_box_size_x);
 	 int minus_half_box_size_y(-half_box_size_y);
 	 int minus_half_box_size_z(-half_box_size_z);
@@ -178,6 +190,11 @@ TEST(GeoLibAABB, RandomNumberOfPointsRandomBox)
 	 ASSERT_LE(minus_half_box_size_x, min_pnt[0]) << "coordinate 0 of min_pnt is smaller than " << minus_half_box_size_x;
 	 ASSERT_LE(minus_half_box_size_y, min_pnt[1]) << "coordinate 1 of min_pnt is smaller than " << minus_half_box_size_y;
 	 ASSERT_LE(minus_half_box_size_z, min_pnt[2]) << "coordinate 2 of min_pnt is smaller than " << minus_half_box_size_z;
+
+	// since the interval is half-open we have to move the upper bound also
+	 half_box_size_x = std::nexttoward(half_box_size_x, std::numeric_limits<double>::max());
+	 half_box_size_y = std::nexttoward(half_box_size_y, std::numeric_limits<double>::max());
+	 half_box_size_z = std::nexttoward(half_box_size_z, std::numeric_limits<double>::max());
 	 ASSERT_GE(half_box_size_x, max_pnt[0]) << "coordinate 0 of max_pnt is greater than " << half_box_size_x;
 	 ASSERT_GE(half_box_size_y, max_pnt[1]) << "coordinate 1 of max_pnt is greater than " << half_box_size_y;
 	 ASSERT_GE(half_box_size_z, max_pnt[2]) << "coordinate 2 of max_pnt is greater than " << half_box_size_z;
@@ -221,6 +238,9 @@ TEST(GeoLib, AABBAllPointsWithNegativeCoordinatesII)
 	ASSERT_NEAR(-1.0, max_pnt[2], std::numeric_limits<double>::epsilon());
 }
 
+// This test creates an AABB containing a single point.
+// This point is moved in every space direction to the next possible double value.
+// It is checked that the AABB contains the point iff it has not been moved.
 TEST(GeoLib, AABBSinglePoint)
 {
 	std::random_device rd;
