833dee64a8bfb6bda7dadbc30c5b6f046dc51fcb
Dmitry Yu. Naumov <github@naumov.de>
2016-01-11 20:16:23 +0000

    [PL] Clang-format Process.

--- ProcessLib/Process.h
+++ ProcessLib/Process.h
@@ -16,43 +16,40 @@
 
 namespace MeshLib
 {
-    class Mesh;
+class Mesh;
 }
 
 namespace ProcessLib
 {
-
-template<typename GlobalSetup>
+template <typename GlobalSetup>
 class Process
 {
 public:
-    Process(MeshLib::Mesh& mesh)
-        : _mesh(mesh)
-    { }
+	Process(MeshLib::Mesh& mesh) : _mesh(mesh) {}
+	virtual ~Process() = default;
 
-    virtual ~Process() = default;
+	virtual void initialize() = 0;
+	virtual bool solve(const double delta_t) = 0;
 
-    virtual void initialize() = 0;
-    virtual bool solve(const double delta_t) = 0;
-
-    /// Postprocessing after solve().
-    /// The file_name is indicating the name of possible output file.
-    virtual void post(std::string const& file_name) = 0;
-    virtual void postTimestep(std::string const& file_name, const unsigned timestep) = 0;
+	/// Postprocessing after solve().
+	/// The file_name is indicating the name of possible output file.
+	virtual void post(std::string const& file_name) = 0;
+	virtual void postTimestep(std::string const& file_name,
+	                          const unsigned timestep) = 0;
 
 protected:
-    MeshLib::Mesh& _mesh;
+	MeshLib::Mesh& _mesh;
 
-    GlobalSetup _global_setup;
+	GlobalSetup _global_setup;
 
-    std::unique_ptr<BaseLib::ConfigTree> _linear_solver_options;
-    std::unique_ptr<typename GlobalSetup::LinearSolver> _linear_solver;
+	std::unique_ptr<BaseLib::ConfigTree> _linear_solver_options;
+	std::unique_ptr<typename GlobalSetup::LinearSolver> _linear_solver;
 
-    std::unique_ptr<typename GlobalSetup::MatrixType> _A;
-    std::unique_ptr<typename GlobalSetup::VectorType> _rhs;
-    std::unique_ptr<typename GlobalSetup::VectorType> _x;
+	std::unique_ptr<typename GlobalSetup::MatrixType> _A;
+	std::unique_ptr<typename GlobalSetup::VectorType> _rhs;
+	std::unique_ptr<typename GlobalSetup::VectorType> _x;
 };
 
-}   // namespace ProcessLib
+}  // namespace ProcessLib
 
 #endif  // PROCESS_LIB_PROCESS_H_
