5709cf035a801791979fd1e3ba61e50aec85e28e
Lars Bilke <lars.bilke@ufz.de>
2016-02-03 12:57:35 +0100

    Updated to cmake-modules version.

--- scripts/cmake/cmake/CodeCoverage.cmake
+++ scripts/cmake/cmake/CodeCoverage.cmake
@@ -1,6 +1,42 @@
-# - Enable Code Coverage
+# Copyright (c) 2012 - 2016, Lars Bilke
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software without
+#    specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#
 #
 # 2012-01-31, Lars Bilke
+# - Enable Code Coverage
+#
+# 2013-09-17, Joakim Söderberg
+# - Added support for Clang.
+# - Some additional usage instructions.
+
+# 2016-02-03, Lars Bilke
+# - Refactored functions to use named parameters
 #
 # USAGE:
 # 1. Copy this file into your cmake modules path
@@ -17,6 +53,7 @@ find_program( GCOV_PATH gcov )
 find_program( LCOV_PATH lcov )
 find_program( GENHTML_PATH genhtml )
 find_program( GCOVR_PATH gcovr PATHS ${CMAKE_SOURCE_DIR}/scripts/test)
+find_program( SIMPLE_PYTHON_EXECUTABLE python )
 
 if(NOT GCOV_PATH)
 	message(FATAL_ERROR "gcov not found! Aborting...")
@@ -42,6 +79,8 @@ endif()
 
 # Defines a target for running and collection code coverage information
 # Builds dependencies, runs the given executable and outputs reports.
+# NOTE! The executable should always have a ZERO as exit code otherwise
+# the coverage generation will not complete.
 #
 # SETUP_TARGET_FOR_COVERAGE(
 # 	NAME testrunner_coverage                    # New target name
@@ -93,6 +132,9 @@ endfunction() # SETUP_TARGET_FOR_COVERAGE
 
 # Defines a target for running and collection code coverage information
 # Builds dependencies, runs the given executable and outputs reports.
+# NOTE! The executable should always have a ZERO as exit code otherwise
+# the coverage generation will not complete.
+
 #
 # SETUP_TARGET_FOR_COVERAGE_COBERTURA(
 # 	NAME ctest_coverage                    # New target name
@@ -106,9 +148,9 @@ function(SETUP_TARGET_FOR_COVERAGE_COBERTURA)
 	set(multiValueArgs EXECUTABLE EXECUTABLE_ARGS DEPENDENCIES)
 	cmake_parse_arguments(Coverage "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-	if(NOT PYTHON_EXECUTABLE)
-		message(FATAL_ERROR "Python not found! Aborting...")
-	endif() # NOT PYTHON_EXECUTABLE
+	if(NOT SIMPLE_PYTHON_EXECUTABLE)
+		message(FATAL_ERROR "python not found! Aborting...")
+	endif() # NOT SIMPLE_PYTHON_EXECUTABLE
 
 	if(NOT GCOVR_PATH)
 		message(FATAL_ERROR "gcovr not found! Aborting...")
