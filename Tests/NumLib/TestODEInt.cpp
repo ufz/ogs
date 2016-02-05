c186b359e2bf44d5cb048669392a8eeaea1fb18a
Christoph Lehmann <christoph.lehmann@ufz.de>
2016-02-05 15:43:20 +0100

    [T] added nonlinear test case

--- Tests/NumLib/TestODEInt.cpp
+++ Tests/NumLib/TestODEInt.cpp
@@ -4,6 +4,7 @@
 #include <logog/include/logog.hpp>
 
 
+// ODE 1 //////////////////////////////////////////////////////////
 class Ode1 final
         : public IFirstOrderImplicitOde<NonlinearSolverTag::Newton>
 {
@@ -37,7 +38,6 @@ public:
         m << 1.0, 0.0,
              0.0, 1.0;
 
-        // TODO: \dot x contribution
         Eigen::Matrix2d k;
         k << 0.0, 1.0,
             -1.0, 0.0;
@@ -51,7 +51,8 @@ public:
     }
 };
 
-Eigen::Vector2d ode1_solution(const double t) {
+Eigen::Vector2d ode1_solution(const double t)
+{
     Eigen::Vector2d v;
     v << cos(t), sin(t);
     return v;
@@ -76,19 +77,105 @@ void test_Ode1()
 
     TimeLoop<NLTag, TimeDisc> loop(ode_sys, nonlinear_solver);
 
-    const double t_end = 0.1;
-    const double delta_t = t_end/10.0;
-
     // initial condition
     const double t0 = 0.0;
     Eigen::Vector2d x0;
     x0 << 1.0, 0.0;
 
+    const double t_end = 0.1;
+    const double delta_t = (t_end-t0)/10.0;
+
     loop.loop(t0, x0, t_end, delta_t, print_result_Ode1);
 }
+// ODE 1 end //////////////////////////////////////////////////////
+
+// ODE 2 //////////////////////////////////////////////////////////
+class Ode2 final
+        : public IFirstOrderImplicitOde<NonlinearSolverTag::Newton>
+{
+public:
+    void assemble(const double t, Vector const& x,
+                  Matrix& M, Matrix& K, Vector& b) override
+    {
+        (void) t; (void) x;
+
+        Eigen::MatrixXd m(1, 1);
+        m << 1.0;
+        M = m.sparseView();
+
+        Eigen::MatrixXd k(1, 1);
+        k << 1.0 / x[0];
+        K = k.sparseView();
+
+        Eigen::VectorXd b_(1);
+        b_ << 0.0;
+        b = b_;
+
+        /*
+        M.coeffRef(0, 0) = 1.0;
+        K.coeffRef(0, 0) = 1.0 / x[0];
+        b.coeffRef(0, 0) = 0.0;
+        */
+    }
+
+    void assembleJacobian(const double t, const Vector &x,
+                          const double dxdot_dx, Matrix &Jac)
+    {
+        (void) t; (void) x;
+
+        Eigen::MatrixXd m(1, 1);
+        m << 1.0;
+
+        Eigen::MatrixXd k(1, 1);
+        k << 1.0 / x[0];
+
+        Jac = (m*dxdot_dx + k).sparseView();
+    }
+
+    bool isLinear() const override
+    {
+        return false;
+    }
+};
+
+double ode2_solution(const double t)
+{
+    return 1.0/t;
+}
+
+void print_result_Ode2(const double t, Vector const& x)
+{
+    INFO("x[0] = %10g", x[0]);
+    auto const v = ode2_solution(t);
+    INFO("x[0] = %10g (analytical solution)", v);
+}
+
+template<NonlinearSolverTag NLTag, typename TimeDisc>
+void test_Ode2()
+{
+    Ode2 ode;
+    TimeDiscretizedODESystem<NLTag, TimeDisc> ode_sys(ode);
+
+    const double tol = 1e-4;
+    const unsigned maxiter = 5;
+    NonlinearSolver<NLTag> nonlinear_solver(tol, maxiter);
+
+    TimeLoop<NLTag, TimeDisc> loop(ode_sys, nonlinear_solver);
+
+    // initial condition
+    const double t0 = 1.0;
+    Eigen::VectorXd x0(1);
+    x0[0] = 1.0;
+
+    const double t_end = 2.0;
+    const double delta_t = (t_end-t0)/10.0;
+
+    loop.loop(t0, x0, t_end, delta_t, print_result_Ode2);
+}
+// ODE 2 end //////////////////////////////////////////////////////
 
 
-TEST(NumLibODEInt, PicardNewtonBwdEuler)
+TEST(NumLibODEInt, Ode1PicardNewtonBwdEuler)
 {
     auto const NLTag = NonlinearSolverTag::Newton;
     using TimeDisc = BackwardEuler;
@@ -96,11 +183,19 @@ TEST(NumLibODEInt, PicardNewtonBwdEuler)
 }
 
 
-TEST(NumLibODEInt, PicardNewtonFwdEuler)
+TEST(NumLibODEInt, Ode1PicardNewtonFwdEuler)
 {
     auto const NLTag = NonlinearSolverTag::Newton;
     using TimeDisc = ForwardEuler;
     test_Ode1<NLTag, TimeDisc>();
+}
+
+
+TEST(NumLibODEInt, Ode2PicardNewtonFwdEuler)
+{
+    auto const NLTag = NonlinearSolverTag::Newton;
+    using TimeDisc = BackwardEuler;
+    test_Ode2<NLTag, TimeDisc>();
 }
 
 
