e00b79dd261ef0fffad62c0e879324dac4256650
Christoph Lehmann <christoph.lehmann@ufz.de>
2016-02-15 11:45:47 +0100

    [NL] documented Types.h

--- NumLib/ODESolver/Types.h
+++ NumLib/ODESolver/Types.h
@@ -9,10 +9,21 @@ namespace NumLib
 // TODO subject to change
 using IndexType = std::size_t;
 
-enum class NonlinearSolverTag : bool { Picard, Newton };
+//! Tag used to specify which nonlinear solver will be used.
+enum class NonlinearSolverTag : bool {
+    Picard /*!< Picard fixpoint iteration scheme */,
+    Newton /*!< Newton-Raphson iteration scheme */
+};
 
+//! Tag used to specify the type of ODE.
 enum class ODESystemTag : char
 {
+    /*! First order implicit quasi-linear ODE
+     *
+     * This is an ODE of the form
+     * \f$ M(x,t)\cdot \dot x + K(x,t) \cdot x - b(x,t)
+     *  =: r(\dot x, x, t) \stackrel{!}{=} 0. \f$
+     */
     FirstOrderImplicitQuasilinear
 };
 
