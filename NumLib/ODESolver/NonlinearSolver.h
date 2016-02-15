2e6914f1491e2fffb36cf3398699c52fb3c60d4c
Christoph Lehmann <christoph.lehmann@ufz.de>
2016-02-15 12:37:07 +0100

    [NL] NonlinearSolver docs

--- NumLib/ODESolver/NonlinearSolver.h
+++ NumLib/ODESolver/NonlinearSolver.h
@@ -12,55 +12,118 @@ namespace NumLib
 //! \addtogroup ODESolver
 //! @{
 
+/*! Find a solution to a nonlinear equation.
+ *
+ * \tparam Matrix the type of matrices occuring in the linearization of the equation.
+ * \tparam Vector the type of the solution vector of the equation.
+ * \tparam NLTAG  a tag indicating the method used for solving the equation.
+ */
 template<typename Matrix, typename Vector, NonlinearSolverTag NLTag>
 class NonlinearSolver;
 
+
+/*! Find a solution to a nonlinear equation using the Newton-Raphson method.
+ *
+ * \tparam Matrix the type of matrices occuring in the linearization of the equation.
+ * \tparam Vector the type of the solution vector of the equation.
+ */
 template<typename Matrix, typename Vector>
 class NonlinearSolver<Matrix, Vector, NonlinearSolverTag::Newton> final
 {
 public:
+    //! Type of the nonlinear equation system to be solved.
     using System = NonlinearSystem<Matrix, Vector, NonlinearSolverTag::Newton>;
 
+    /*! Constructs a new instance.
+     *
+     * \param tol     the tolerance of the solver. \todo Be more specific about that!
+     * \param maxiter the maximum number of iterations used to solve the equation.
+     */
     explicit
     NonlinearSolver(double const tol, const unsigned maxiter)
         : _tol(tol)
         , _maxiter(maxiter)
     {}
 
-    // for Crank-Nicolson
+    /*! Only assemble the equation system.
+     *
+     * \note This method is needed to preload CrankNicolson time discretization scheme.
+     *       It is not used for the general solver steps; in those only the solve() method
+     *       is needed.
+     *
+     * \param sys the equation system to be assembled.
+     * \param x   the state at which the equation system will be assembled.
+     */
     void assemble(System& sys, Vector& x);
 
+    /*! Assemble and solve the equation system.
+     *
+     * \param sys the equation system to be solved.
+     * \param x   in: the initial guess, out: the solution.
+     *
+     * \retval true if the equation system could be solved
+     * \retval false otherwise
+     */
     bool solve(System& sys, Vector& x);
 
 private:
-    const double _tol;
-    const unsigned _maxiter;
+    const double _tol;       //!< tolerance of the solver
+    const unsigned _maxiter; //!< maximum number of iterations
 
-    Vector _minus_delta_x;
+    Vector _minus_delta_x; //!< the Newton-Raphson method solves the linearized equation
+                           //!< \f$ J (-\Delta x) = r \f$ repeatedly.
 };
 
+
+/*! Find a solution to a nonlinear equation using the Picard fixpoint iteration method.
+ *
+ * \tparam Matrix the type of matrices occuring in the linearization of the equation.
+ * \tparam Vector the type of the solution vector of the equation.
+ */
 template<typename Matrix, typename Vector>
 class NonlinearSolver<Matrix, Vector, NonlinearSolverTag::Picard> final
 {
 public:
+    //! Type of the nonlinear equation system to be solved.
     using System = NonlinearSystem<Matrix, Vector, NonlinearSolverTag::Picard>;
 
+    /*! Constructs a new instance.
+     *
+     * \param tol     the tolerance of the solver. \todo Be more specific about that!
+     * \param maxiter the maximum number of iterations used to solve the equation.
+     */
     explicit
     NonlinearSolver(double const tol, const unsigned maxiter)
         : _tol(tol)
         , _maxiter(maxiter)
     {}
 
-    // for Crank-Nicolson
+    /*! Only assemble the equation system.
+     *
+     * \note This method is needed to preload CrankNicolson time discretization scheme.
+     *       It is not used for the general solver steps; in those only the solve() method
+     *       is needed.
+     *
+     * \param sys the equation system to be assembled.
+     * \param x   the state at which the equation system will be assembled.
+     */
     void assemble(System& sys, Vector& x);
 
+    /*! Assemble and solve the equation system.
+     *
+     * \param sys the equation system to be solved.
+     * \param x   in: the initial guess, out: the solution.
+     *
+     * \retval true if the equation system could be solved
+     * \retval false otherwise
+     */
     bool solve(System& sys, Vector& x);
 
 private:
-    const double _tol;
-    const unsigned _maxiter;
+    const double _tol;       //!< tolerance of the solver
+    const unsigned _maxiter; //!< maximum number of iterations
 
-    Vector _x_new;
+    Vector _x_new; //!< \c Vector to store intermediate solutions.
 };
 
 //! @}
