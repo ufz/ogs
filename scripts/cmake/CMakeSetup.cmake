1cf7e509a849900a12edd9feda6bc78891d0f6c2
Lars Bilke <lars.bilke@ufz.de>
2016-02-24 17:04:28 +0100

    Simplified OS X version number for packaging.

--- scripts/cmake/CMakeSetup.cmake
+++ scripts/cmake/CMakeSetup.cmake
@@ -30,21 +30,4 @@ endif()
 # Get the hostname
 site_name(HOSTNAME)
 
-# Compute OS X version number
-if(APPLE)
-	if(CMAKE_SYSTEM_VERSION VERSION_EQUAL 12.0)
-		set(OSX_VERSION 10.8 CACHE STRING "OS X version number")
-		set(OSX_VERSION_NAME CACHE STRING "Mountain Lion")
-	endif()
-	if(CMAKE_SYSTEM_VERSION VERSION_EQUAL 13.0)
-		set(OSX_VERSION 10.9 CACHE STRING "OS X version number")
-		set(OSX_VERSION_NAME CACHE STRING "Mavericks")
-	endif()
-	if(CMAKE_SYSTEM_VERSION VERSION_EQUAL 14.0)
-		set(OSX_VERSION 10.10 CACHE STRING "OS X version number")
-		set(OSX_VERSION_NAME CACHE STRING "Yosemite")
-	endif()
-endif()
-mark_as_advanced(OSX_VERSION OSX_VERSION_NAME)
-
 set(CMAKE_INCLUDE_CURRENT_DIR ON)
