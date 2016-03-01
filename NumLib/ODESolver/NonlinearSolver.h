6e6d03e5bbbf73311cbddad5d61f8d31354208a1
Christoph Lehmann <christoph.lehmann@ufz.de>
2016-03-01 11:29:41 +0100

    [NL] added base class for nonlinear solvers

--- NumLib/ODESolver/NonlinearSolver.h
+++ NumLib/ODESolver/NonlinearSolver.h
@@ -10,14 +10,57 @@
 #ifndef NUMLIB_NONLINEARSOLVER_H
 #define NUMLIB_NONLINEARSOLVER_H
 
+#include <memory>
+#include <utility>
 #include <logog/include/logog.hpp>
+
+#include "MathLib/LinAlg/LinearSolver.h"
 
 #include "Types.h"
 #include "NonlinearSystem.h"
 
+namespace BaseLib
+{
+class ConfigTree;
+}
+
+// TODO Document in the ODE solver lib, which matrices and vectors that are passed around
+//      as method arguments are guaranteed to be of the right size (and zeroed out) and
+//      which are not.
 
 namespace NumLib
 {
+
+/*! Common interface for nonlinear solvers.
+ *
+ * \tparam Matrix the type of matrices occuring in the linearization of the equation.
+ * \tparam Vector the type of the solution vector of the equation.
+ */
+template<typename Matrix, typename Vector>
+class NonlinearSolverBase
+{
+public:
+    /*! Only assemble the equation system.
+     *
+     * \note This method is needed to preload CrankNicolson time discretization scheme.
+     *       It is not used for the general solver steps; in those only the solve() method
+     *       is needed.
+     *
+     * \param x   the state at which the equation system will be assembled.
+     */
+    virtual void assemble(Vector const& x) const = 0;
+
+    /*! Assemble and solve the equation system.
+     *
+     * \param x   in: the initial guess, out: the solution.
+     *
+     * \retval true if the equation system could be solved
+     * \retval false otherwise
+     */
+    virtual bool solve(Vector& x) = 0;
+
+    virtual ~NonlinearSolverBase() = default;
+};
 
 //! \addtogroup ODESolver
 //! @{
@@ -39,44 +82,38 @@ class NonlinearSolver;
  */
 template<typename Matrix, typename Vector>
 class NonlinearSolver<Matrix, Vector, NonlinearSolverTag::Newton> final
+        : public NonlinearSolverBase<Matrix, Vector>
 {
 public:
     //! Type of the nonlinear equation system to be solved.
     using System = NonlinearSystem<Matrix, Vector, NonlinearSolverTag::Newton>;
+    using LinearSolver = MathLib::LinearSolver<Matrix, Vector>;
 
     /*! Constructs a new instance.
      *
+     * \param linear_solver the linear solver used by this nonlinear solver.
      * \param tol     the tolerance of the solver. \todo Be more specific about that!
      * \param maxiter the maximum number of iterations used to solve the equation.
      */
     explicit
-    NonlinearSolver(double const tol, const unsigned maxiter)
-        : _tol(tol)
+    NonlinearSolver(LinearSolver& linear_solver,
+                    double const tol, const unsigned maxiter)
+        : _linear_solver(linear_solver)
+        , _tol(tol)
         , _maxiter(maxiter)
     {}
 
-    /*! Only assemble the equation system.
-     *
-     * \note This method is needed to preload CrankNicolson time discretization scheme.
-     *       It is not used for the general solver steps; in those only the solve() method
-     *       is needed.
-     *
-     * \param sys the equation system to be assembled.
-     * \param x   the state at which the equation system will be assembled.
-     */
-    void assemble(System& sys, Vector const& x) const;
+    //! Set the nonlinear equation system that will be solved.
+    void setEquationSystem(System& eq) { _equation_system = &eq; }
 
-    /*! Assemble and solve the equation system.
-     *
-     * \param sys the equation system to be solved.
-     * \param x   in: the initial guess, out: the solution.
-     *
-     * \retval true if the equation system could be solved
-     * \retval false otherwise
-     */
-    bool solve(System& sys, Vector& x);
+    void assemble(Vector const& x) const override;
+
+    bool solve(Vector& x) override;
 
 private:
+    LinearSolver& _linear_solver;
+    System*       _equation_system = nullptr;
+
     const double _tol;       //!< tolerance of the solver
     const unsigned _maxiter; //!< maximum number of iterations
 
@@ -95,44 +132,38 @@ private:
  */
 template<typename Matrix, typename Vector>
 class NonlinearSolver<Matrix, Vector, NonlinearSolverTag::Picard> final
+        : public NonlinearSolverBase<Matrix, Vector>
 {
 public:
     //! Type of the nonlinear equation system to be solved.
     using System = NonlinearSystem<Matrix, Vector, NonlinearSolverTag::Picard>;
+    using LinearSolver = MathLib::LinearSolver<Matrix, Vector>;
 
     /*! Constructs a new instance.
      *
+     * \param linear_solver the linear solver used by this nonlinear solver.
      * \param tol     the tolerance of the solver. \todo Be more specific about that!
      * \param maxiter the maximum number of iterations used to solve the equation.
      */
     explicit
-    NonlinearSolver(double const tol, const unsigned maxiter)
-        : _tol(tol)
+    NonlinearSolver(LinearSolver& linear_solver,
+                    double const tol, const unsigned maxiter)
+        : _linear_solver(linear_solver)
+        , _tol(tol)
         , _maxiter(maxiter)
     {}
 
-    /*! Only assemble the equation system.
-     *
-     * \note This method is needed to preload CrankNicolson time discretization scheme.
-     *       It is not used for the general solver steps; in those only the solve() method
-     *       is needed.
-     *
-     * \param sys the equation system to be assembled.
-     * \param x   the state at which the equation system will be assembled.
-     */
-    void assemble(System& sys, Vector const& x) const;
+    //! Set the nonlinear equation system that will be solved.
+    void setEquationSystem(System& eq) { _equation_system = &eq; }
 
-    /*! Assemble and solve the equation system.
-     *
-     * \param sys the equation system to be solved.
-     * \param x   in: the initial guess, out: the solution.
-     *
-     * \retval true if the equation system could be solved
-     * \retval false otherwise
-     */
-    bool solve(System& sys, Vector& x);
+    void assemble(Vector const& x) const override;
+
+    bool solve(Vector& x) override;
 
 private:
+    LinearSolver& _linear_solver;
+    System*       _equation_system = nullptr;
+
     const double _tol;       //!< tolerance of the solver
     const unsigned _maxiter; //!< maximum number of iterations
 
@@ -141,9 +172,27 @@ private:
     Vector _x_new; //!< \c Vector to store solutions of \f$ A \cdot x = \mathit{rhs} \f$.
 };
 
+/*! Creates a new nonlinear solver from the given configuration.
+ *
+ * \param linear_solver the linear solver that will be used by the nonlinear
+ *        solver
+ * \param config configuration settings
+ *
+ * \return a pair <tt>(nl_slv, tag)</tt> where \c nl_slv is the generated
+ *         nonlinear solver instance and the \c tag indicates if it uses the
+ *         Picard or Newton-Raphson method
+ */
+template<typename Matrix, typename Vector>
+std::pair<
+    std::unique_ptr<NonlinearSolverBase<Matrix, Vector> >,
+    NonlinearSolverTag
+>
+createNonlinearSolver(MathLib::LinearSolver<Matrix, Vector>& linear_solver,
+                      BaseLib::ConfigTree const& config);
+
 //! @}
 
-}
+} // namespace NumLib
 
 #include "NonlinearSolver-impl.h"
 
