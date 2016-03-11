1b80ddd3dda2191b671789c9bbcba5e9f163d27c
Christoph Lehmann <christoph.lehmann@ufz.de>
2016-03-11 13:41:27 +0100

    [AppL] TES builder

--- Applications/ApplicationsLib/ProjectData.cpp
+++ Applications/ApplicationsLib/ProjectData.cpp
@@ -179,18 +179,14 @@ void ProjectData::buildProcesses()
 				    *_mesh_vec[0], *nl_slv, std::move(time_disc),
 				    _process_variables, _parameters, pc));
 		}
-                else if (type == "TES")
-                {
-                            // The existence check of the in the configuration referenced
-                            // process variables is checked in the physical process.
-                            // TODO at the moment we have only one mesh, later there can be
-                            // several meshes. Then we have to assign the referenced mesh
-                            // here.
-                            _processes.push_back(
-                                    new ProcessLib::TES::TESProcess<GlobalSetupType>(
-                                            *_mesh_vec[0], _process_variables, _parameters, pc));
-                }
-                else
+		else if (type == "TES")
+		{
+			_processes.emplace_back(
+				ProcessLib::TES::createTESProcess<GlobalSetupType>(
+				    *_mesh_vec[0], *nl_slv, std::move(time_disc),
+				    _process_variables, _parameters, pc));
+		}
+		else
 		{
 			ERR("Unknown process type: %s", type.c_str());
 			std::abort();
