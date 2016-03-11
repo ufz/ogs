5b4aad2b62c1468e50781064232b769528d3d7e8
Christoph Lehmann <christoph.lehmann@ufz.de>
2016-03-11 08:49:25 +0100

    [Mat] changes to adsorption

---
+++ MaterialsLib/adsorption/reaction_sinusoidal.h
@@ -0,0 +1,46 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2015, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#pragma once
+
+#include <logog/include/logog.hpp>
+
+#include "reaction.h"
+#include "BaseLib/ConfigTreeNew.h"
+
+namespace Ads
+{
+
+class ReactionSinusoidal final : public Reaction
+{
+public:
+    explicit ReactionSinusoidal(BaseLib::ConfigTreeNew const& conf)
+        : _enthalpy(conf.getConfParam<double>("reaction_enthalpy"))
+    {
+    }
+
+    double get_enthalpy(const double /*p_Ads*/, const double /*T_Ads*/,
+                        const double /*M_Ads*/) const override
+    {
+        return _enthalpy;
+    }
+
+    double get_reaction_rate(const double /*p_Ads*/, const double /*T_Ads*/, const double /*M_Ads*/,
+                             const double /*loading*/) const override
+    {
+        ERR("Method get_reaction_rate() should never be called directly");
+        std::abort();
+        return 0.0;
+    }
+
+private:
+    double _enthalpy;
+};
+
+}
