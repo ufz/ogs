058e8f0d633e3cce4259010a58f63b3a0c668d91
Dmitri Naumov <dmitri.naumov@ufz.de>
2016-03-14 15:52:57 +0100

    [MaL] Add/correct copyright messages.

--- MathLib/ODE/OdeSolver.h
+++ MathLib/ODE/OdeSolver.h
@@ -1,3 +1,12 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2016, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
 #ifndef MATHLIB_ODESOLVER_H
 #define MATHLIB_ODESOLVER_H
 
