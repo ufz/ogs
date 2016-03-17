8dddc1018937a87c17be8b57ade3f7f2c77396cc
Thomas Fischer <thomas.fischer@ufz.de>
2016-03-17 09:21:54 +0100

    [GL] Init. impl. of PolygonWithSegmentMarker.

---
+++ GeoLib/PolygonWithSegmentMarker.cpp
@@ -0,0 +1,48 @@
+/**
+ *
+ * \copyright
+ * Copyright (c) 2012-2016, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ */
+
+#include "PolygonWithSegmentMarker.h"
+
+namespace GeoLib {
+PolygonWithSegmentMarker::PolygonWithSegmentMarker(
+    GeoLib::Polyline const& polyline)
+    : GeoLib::Polygon(polyline, true),
+      _marker(polyline.getNumberOfPoints(), false)
+{
+}
+
+void PolygonWithSegmentMarker::markSegment(std::size_t seg_num, bool mark_val)
+{
+    _marker[seg_num] = mark_val;
+}
+
+bool PolygonWithSegmentMarker::isSegmentMarked(std::size_t seg_num) const
+{
+    return _marker[seg_num];
+}
+
+bool PolygonWithSegmentMarker::addPoint(std::size_t pnt_id)
+{
+    if (Polyline::addPoint(pnt_id)) {
+        _marker.push_back(false);
+        return true;
+    }
+    return false;
+}
+
+bool PolygonWithSegmentMarker::insertPoint(std::size_t pos, std::size_t pnt_id)
+{
+    if (Polyline::insertPoint(pos, pnt_id)) {
+        _marker.insert(_marker.begin()+pos, _marker[pos]);
+        return true;
+    }
+    return false;
+}
+
+} // end GeoLib
---
+++ GeoLib/PolygonWithSegmentMarker.h
@@ -0,0 +1,57 @@
+/**
+ *
+ * \copyright
+ * Copyright (c) 2012-2016, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#ifndef POLYGONWITHSEGMENTMARKER_H_
+#define POLYGONWITHSEGMENTMARKER_H_
+
+#include "Polygon.h"
+
+namespace GeoLib {
+
+class PolygonWithSegmentMarker final : public GeoLib::Polygon
+{
+public:
+    explicit PolygonWithSegmentMarker(GeoLib::Polyline const& polyline);
+
+    /**
+     * Method marks the segment (default mark is true).
+     * @param seg_num the segment number that should be marked
+     * @param mark_val the value of the flag (true or false)
+     */
+    void markSegment(std::size_t seg_num, bool mark_val = true);
+
+    /**
+     * Method returns the value of the mark for the given segment.
+     * @param seg_num segment number
+     * @return either true if the segment is marked or false else
+     */
+    bool isSegmentMarked(std::size_t seg_num) const;
+
+    /**
+     * Method calls @see Polyline::addPoint() and initializes the mark of the
+     * corresponding line segment.
+     * @see Polyline::addPoint()
+     */
+    virtual bool addPoint(std::size_t pnt_id) override;
+
+    /**
+     * Method calls the @see Polyline::insertPoint() and initializes the inserted line
+     * segment with the same value the previous line segment had.
+     * @see Polyline::insertPoint()
+     */
+    virtual bool insertPoint(std::size_t pos, std::size_t pnt_id) override;
+
+private:
+    std::vector<bool> _marker;
+};
+
+} // end namespace GeoLib
+
+#endif /* POLYGONWITHSEGMENTMARKER_H_ */
