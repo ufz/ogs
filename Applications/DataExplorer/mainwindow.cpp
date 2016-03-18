046fef70047fe96d6ff338f9aba888ef1c9b7a43
Thomas Fischer <thomas.fischer@ufz.de>
2016-03-18 07:54:11 +0100

    Set output precision to digits10.

--- Applications/DataExplorer/mainwindow.cpp
+++ Applications/DataExplorer/mainwindow.cpp
@@ -898,11 +898,11 @@ void MainWindow::callGMSH(std::vector<std::string> & selectedGeometries,
         if (!fileName.isEmpty())
         {
             if (param4 == -1) { // adaptive meshing selected
-                gmsh_io.setPrecision(20);
                 GMSHInterface gmsh_io(
                     *(_project.getGEOObjects()), true,
                     FileIO::GMSH::MeshDensityAlgorithm::AdaptiveMeshDensity,
                     param2, param3, param1, selectedGeometries);
+                gmsh_io.setPrecision(std::numeric_limits<double>::digits10);
                 gmsh_io.writeToFile(fileName.toStdString());
             } else { // homogeneous meshing selected
                 GMSHInterface gmsh_io(
