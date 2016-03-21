935d49431d8bc48bae64a8d8b5b4751d98e9f291
Thomas Fischer <thomas.fischer@ufz.de>
2016-03-21 14:53:39 +0100

    [GL] LineSegment.

--- GeoLib/LineSegment.h
+++ GeoLib/LineSegment.h
@@ -9,32 +9,45 @@
 #ifndef LINESEGMENT_H_
 #define LINESEGMENT_H_
 
-#include <iostream>
-
 #include "Point.h"
 
 namespace GeoLib
 {
 
-struct LineSegment
+class LineSegment final
 {
-	GeoLib::Point a;
-	GeoLib::Point b;
+public:
+	/// Constructs a line segment given by its begin and end points.
+	/// @remark The main purpose of class LineSegment is the use within a
+	/// polyline. For this reason the memory is not managed per default. But the
+	/// user has the freedom to hand over the responsibility for the mem
+	/// managment of the points to the object.
+	LineSegment(GeoLib::Point* const beg, GeoLib::Point* const end,
+	            bool point_mem_management_by_line_segment = false);
+
+	LineSegment(LineSegment&& line_segment);
+	LineSegment(LineSegment const& line_segment);
+
+	~LineSegment();
+
+	LineSegment& operator= (LineSegment const& rhs) = delete;
+
+	GeoLib::Point const& getBeginPoint() const;
+	GeoLib::Point & getBeginPoint();
+
+	GeoLib::Point const& getEndPoint() const;
+	GeoLib::Point & getEndPoint();
+
+private:
+	GeoLib::Point* _a = nullptr;
+	GeoLib::Point* _b = nullptr;
+	bool _point_mem_management_by_line_segment = false;
 };
 
-std::ostream& operator<< (std::ostream& os, LineSegment const& s)
-{
-	os << "{(" << s.a << "), (" << s.b << ")}";
-	return os;
-}
+std::ostream& operator<< (std::ostream& os, LineSegment const& s);
 
 std::ostream& operator<<(std::ostream& os,
                          std::pair<GeoLib::LineSegment const&,
-                                   GeoLib::LineSegment const&> const& seg_pair)
-{
-	os << seg_pair.first << " x " << seg_pair.second;
-	return os;
-}
-
+                                   GeoLib::LineSegment const&> const& seg_pair);
 }
 #endif
