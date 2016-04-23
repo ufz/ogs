cacef6af27f8e2c9eac618cbc0e2758d96ae5aba
Christoph Lehmann <christoph.lehmann@ufz.de>
2016-04-23 10:57:45 +0200

    [T] removed zeolite test case

--- Tests/MathLib/TestCVode.cpp
+++ Tests/MathLib/TestCVode.cpp
@@ -11,60 +11,11 @@
 
 #include <gtest/gtest.h>
 
-#include <array>
-
-#ifdef ZEOLITE
-#include "MaterialsLib/adsorption/adsorption.h"
-#endif
-
 #include "MathLib/ODE/CVodeSolver.h"
 #include "MathLib/ODE/OdeSolver.h"
 #include "MathLib/ODE/ConcreteOdeSolver.h"
 
 #include <cstdio>
-
-#ifdef ZEOLITE
-using namespace Ads;
-
-const unsigned NEQ = 2;  // number of equations
-
-const double T = 303.15;  // K
-const double R = 8.314;   // J/mol/K
-const double M = 0.018;   // kg/mol
-const double phi = 0.4;
-const double rho_SR0 = 1160.0;  // kg/m^3
-// const double k = 6e-3;   // s^-1
-
-Adsorption* ads;
-
-bool f_zeolite(const double, double const* const y, double* const ydot)
-{
-	const double pV = y[0];
-	const double C = y[1];
-
-	// std::printf("pV: %14.7g, C: %14.7g\n", pV, C);
-
-	// if (0.0 < pV && pV < 1e-6) pV = 1e-6;
-
-	if (pV >= 1.0)
-	{
-		ydot[1] = ads->get_reaction_rate(pV, T, M, C);
-		ydot[0] = -R * T / M / phi * (1.0 - phi) * rho_SR0 * ydot[1];
-		return true;
-	}
-	else if (pV > 0.0)
-	{
-		ydot[0] = ydot[1] = 0.0;
-		return true;
-	}
-	else
-	{
-		ydot[0] = ydot[1] = 0.0;
-		return false;
-	}
-}
-
-#endif
 
 bool f(const double,
        MathLib::MappedConstVector<1> const y,
@@ -84,8 +35,6 @@ bool df(const double /*t*/,
 	if (y[0] <= 0.0) return false;
 
 	jac(0, 0) = -15.0;
-
-	// std::printf("jac: %g\n", *jac);
 	return true;
 }
 
@@ -104,48 +53,6 @@ bool f_extra(const double,
 	ydot[0] = -data.value * y[0];
 	return true;
 }
-
-#ifdef ZEOLITE
-TEST(MathLibCVodeTest, ZeoliteAdsorption)
-{
-	// initial values
-	const double C = 0.0;
-	const double pV = 100.0;  // Pa
-
-	ads = Adsorption::newInstance(SolidReactiveSystem::Z13XBF_Hauer);
-
-	MathLib::CVodeSolver::ConfigTree config;
-	config.put("linear_multistep_method", "BDF");
-	auto ode_solver = MathLib::createOdeSolver<2>(config);
-
-	ode_solver->setFunction(f_zeolite, nullptr);
-	ode_solver->setTolerance({1.0, 0.01}, 1.0);
-
-	ode_solver->setIC(0.0, {{pV, C}});
-
-	ode_solver->preSolve();
-
-	const double dt = 1e-4;
-
-	for (unsigned i = 1; i <= 10; ++i)
-	{
-		const double time = dt * i;
-
-		ode_solver->solve(time);
-
-		double const* y = ode_solver->getSolution();
-		double time_reached = ode_solver->getTime();
-		auto const y_dot = ode_solver->getYDot(time_reached, y);
-
-		// std::printf("t: %g, pV: %g, C: %g\n", time_reached, y[0], y[1]);
-		std::printf("[ %23.16g, %23.16g, %13.16g ],\n", time_reached, y[0],
-		            y[1]);
-
-		ASSERT_NEAR(time, time_reached, std::numeric_limits<double>::epsilon());
-		// std::printf("time: %g\n", time_reached);
-	}
-}
-#endif
 
 TEST(MathLibCVodeTest, Exponential)
 {
