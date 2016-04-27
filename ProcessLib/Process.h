c8f60237ab1c7ab50474f3258bd67fcc964f6edb
Dmitri Naumov <github@naumov.de>
2016-04-27 13:05:52 +0000

    [PL] Fix Process.h/cpp includes.

--- ProcessLib/Process.h
+++ ProcessLib/Process.h
@@ -10,35 +10,14 @@
 #ifndef PROCESS_LIB_PROCESS_H_
 #define PROCESS_LIB_PROCESS_H_
 
-#include <memory>
-#include <string>
-
-#include <logog/include/logog.hpp>
-
 #include "AssemblerLib/ComputeSparsityPattern.h"
-#include "AssemblerLib/LocalToGlobalIndexMap.h"
-#include "BaseLib/ConfigTree.h"
 #include "FileIO/VtkIO/VtuInterface.h"
-#include "MeshGeoToolsLib/MeshNodeSearcher.h"
-#include "MathLib/LinAlg/SparsityPattern.h"
-#include "MeshLib/MeshSubset.h"
-#include "MeshLib/MeshSubsets.h"
-
-#include "DirichletBc.h"
-#include "NeumannBc.h"
-#include "NeumannBcAssembler.h"
-#include "Parameter.h"
-#include "ProcessVariable.h"
-#include "UniformDirichletBoundaryCondition.h"
-
-#include "NumLib/ODESolver/NonlinearSolver.h"
 #include "NumLib/ODESolver/ODESystem.h"
 #include "NumLib/ODESolver/TimeDiscretization.h"
+#include "NumLib/ODESolver/NonlinearSolver.h"
 
-namespace MeshLib
-{
-class Mesh;
-}
+#include "Parameter.h"
+#include "ProcessVariable.h"
 
 namespace ProcessLib
 {
