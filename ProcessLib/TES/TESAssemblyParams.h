1780173697e2463c09eefad02368fb7833fedb1e
Christoph Lehmann <christoph.lehmann@ufz.de>
2016-05-01 17:40:13 +0200

    [TES] added/updated copyright headers

--- ProcessLib/TES/TESAssemblyParams.h
+++ ProcessLib/TES/TESAssemblyParams.h
@@ -1,3 +1,12 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2016, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
 #ifndef PROCESS_LIB_TESPROCESS_NOTPL_H_
 #define PROCESS_LIB_TESPROCESS_NOTPL_H_
 
--- ProcessLib/TES/TESLocalAssemblerData.h
+++ ProcessLib/TES/TESLocalAssemblerData.h
@@ -1,4 +1,12 @@
-// TODO doc, header guard
+/**
+ * \copyright
+ * Copyright (c) 2012-2016, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
 #pragma once
 
 #include "TESAssemblyParams.h"
