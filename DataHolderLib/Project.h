18393baeec45b3752b22c8758b72557359961bcb
Dmitri Naumov <github@naumov.de>
2016-05-14 17:51:31 +0000

    [DHL] Tabs to whitespaces.

--- DataHolderLib/Project.h
+++ DataHolderLib/Project.h
@@ -17,7 +17,7 @@
 #include "MeshLib/Mesh.h"
 
 //namespace MeshLib {
-//	class Mesh;
+//    class Mesh;
 //}
 
 namespace DataHolderLib
@@ -31,53 +31,53 @@ namespace DataHolderLib
 class Project final
 {
 public:
-	/// Constructor
-	Project() = default;
+    /// Constructor
+    Project() = default;
 
-	Project(Project&) = delete;
+    Project(Project&) = delete;
 
-	~Project() = default;
+    ~Project() = default;
 
-	/// Returns the GEOObjects containing all points, polylines and surfaces.
-	GeoLib::GEOObjects& getGEOObjects() { return _geoObjects; }
+    /// Returns the GEOObjects containing all points, polylines and surfaces.
+    GeoLib::GEOObjects& getGEOObjects() { return _geoObjects; }
 
-	/// Adds a new mesh under a (possibly new) unique name.
-	/// \attention This might change the given mesh's name.
-	void addMesh(std::unique_ptr<MeshLib::Mesh> mesh);
+    /// Adds a new mesh under a (possibly new) unique name.
+    /// \attention This might change the given mesh's name.
+    void addMesh(std::unique_ptr<MeshLib::Mesh> mesh);
 
-	/// Returns the mesh with the given name or a \c nullptr if the mesh was not
-	/// found.
-	const MeshLib::Mesh* getMesh(const std::string &name) const;
+    /// Returns the mesh with the given name or a \c nullptr if the mesh was not
+    /// found.
+    const MeshLib::Mesh* getMesh(const std::string &name) const;
 
-	/// Returns all the meshes with their respective names
-	const std::vector<std::unique_ptr<MeshLib::Mesh>>& getMeshObjects() const
-	{
-		return _mesh_vec;
-	}
+    /// Returns all the meshes with their respective names
+    const std::vector<std::unique_ptr<MeshLib::Mesh>>& getMeshObjects() const
+    {
+        return _mesh_vec;
+    }
 
-	/// Deletes all meshes with the given name and removes them from the list of
-	/// saved meshes. If any mesh was found for removal, true is returned and
-	/// false otherwise.
-	bool removeMesh(const std::string &name);
+    /// Deletes all meshes with the given name and removes them from the list of
+    /// saved meshes. If any mesh was found for removal, true is returned and
+    /// false otherwise.
+    bool removeMesh(const std::string &name);
 
 private:
-	/// Checks if a mesh with the same name exists and provides a unique name in
-	/// case of already existing mesh. Returns true if the mesh name is unique.
-	/// Returns false and changes the provided name to a unique name otherwise.
-	bool getUniqueName(std::string &name) const;
+    /// Checks if a mesh with the same name exists and provides a unique name in
+    /// case of already existing mesh. Returns true if the mesh name is unique.
+    /// Returns false and changes the provided name to a unique name otherwise.
+    bool getUniqueName(std::string &name) const;
 
-	/// Returns true if a mesh with the same name exists and false otherwise.
-	bool meshExists(const std::string &name) const;
+    /// Returns true if a mesh with the same name exists and false otherwise.
+    bool meshExists(const std::string &name) const;
 
 
-	/// Returns an iterator to the first found mesh with the given name.
-	std::vector<std::unique_ptr<MeshLib::Mesh>>::const_iterator findMeshByName(
-	    std::string const& name) const;
-	std::vector<std::unique_ptr<MeshLib::Mesh>>::iterator findMeshByName(
-	    std::string const& name);
+    /// Returns an iterator to the first found mesh with the given name.
+    std::vector<std::unique_ptr<MeshLib::Mesh>>::const_iterator findMeshByName(
+        std::string const& name) const;
+    std::vector<std::unique_ptr<MeshLib::Mesh>>::iterator findMeshByName(
+        std::string const& name);
 
-	GeoLib::GEOObjects _geoObjects;
-	std::vector<std::unique_ptr<MeshLib::Mesh>> _mesh_vec;
+    GeoLib::GEOObjects _geoObjects;
+    std::vector<std::unique_ptr<MeshLib::Mesh>> _mesh_vec;
 };
 
 } // namespace
