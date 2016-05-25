72dd203f4c75aea8411b8b7dbafff5bd6bb9e50d
Christoph Lehmann <christoph.lehmann@ufz.de>
2016-05-25 08:47:02 +0200

    [Ads] underscore to private members

--- MaterialsLib/Adsorption/ReactionCaOH2.cpp
+++ MaterialsLib/Adsorption/ReactionCaOH2.cpp
@@ -15,15 +15,15 @@
 namespace Adsorption
 {
 
-const double ReactionCaOH2::R = GAS_CONST;
-const double ReactionCaOH2::reaction_enthalpy = -1.12e+05;
-const double ReactionCaOH2::reaction_entropy  = -143.5;
-const double ReactionCaOH2::M_carrier = M_N2;
-const double ReactionCaOH2::M_react   = M_H2O;
+const double ReactionCaOH2::_R = GAS_CONST;
+const double ReactionCaOH2::_reaction_enthalpy = -1.12e+05;
+const double ReactionCaOH2::_reaction_entropy  = -143.5;
+const double ReactionCaOH2::_M_carrier = M_N2;
+const double ReactionCaOH2::_M_react   = M_H2O;
 
-const double ReactionCaOH2::tol_l   = 1e-4;
-const double ReactionCaOH2::tol_u   = 1.0 - 1e-4;
-const double ReactionCaOH2::tol_rho = 0.1;
+const double ReactionCaOH2::_tol_l   = 1e-4;
+const double ReactionCaOH2::_tol_u   = 1.0 - 1e-4;
+const double ReactionCaOH2::_tol_rho = 0.1;
 
 const double ReactionCaOH2::rho_low = 1656.0;
 const double ReactionCaOH2::rho_up = 2200.0;
@@ -32,7 +32,7 @@ const double ReactionCaOH2::rho_up = 2200.0;
 double
 ReactionCaOH2::getEnthalpy(const double, const double, const double) const
 {
-    return - reaction_enthalpy/M_react;
+    return - _reaction_enthalpy/_M_react;
 }
 
 double
@@ -46,9 +46,9 @@ ReactionCaOH2::getReactionRate(const double, const double, const double, const d
 
 double ReactionCaOH2::getReactionRate(double const solid_density)
 {
-    rho_s = solid_density;
+    _rho_s = solid_density;
     calculateQR();
-    return qR;
+    return _qR;
 }
 
 void ReactionCaOH2::updateParam(
@@ -57,36 +57,36 @@ void ReactionCaOH2::updateParam(
         double x_react,
         double rho_s_initial)
 {
-    T_s     = T_solid;
-    this->p_gas = p_gas / 1e5; // convert Pa to bar
-    this->x_react = x_react;
-    rho_s   = rho_s_initial;
+    _T_s     = T_solid;
+    _p_gas = p_gas / 1e5; // convert Pa to bar
+    _x_react = x_react;
+    _rho_s   = rho_s_initial;
 }
 
 void ReactionCaOH2::calculateQR()
 {
     // Convert mass fraction into mole fraction
-    const double mol_frac_react = AdsorptionReaction::getMolarFraction(x_react, M_react, M_carrier);
+    const double mol_frac_react = AdsorptionReaction::getMolarFraction(_x_react, _M_react, _M_carrier);
 
-    p_r_g = std::max(mol_frac_react * p_gas, 1.0e-3); // avoid illdefined log
+    _p_r_g = std::max(mol_frac_react * _p_gas, 1.0e-3); // avoid illdefined log
     setChemicalEquilibrium();
     const double dXdt = CaHydration();
-    qR = (rho_up - rho_low) * dXdt;
+    _qR = (rho_up - rho_low) * dXdt;
 }
 
 // determine equilibrium temperature and pressure according to van't Hoff
 void ReactionCaOH2::setChemicalEquilibrium()
 {
-    X_D = (rho_s - rho_up - tol_rho)/(rho_low - rho_up - 2.0*tol_rho) ;
-    X_D = (X_D < 0.5) ? std::max(tol_l,X_D) : std::min(X_D,tol_u); // constrain to interval [tol_l;tol_u]
+    _X_D = (_rho_s - rho_up - _tol_rho)/(rho_low - rho_up - 2.0*_tol_rho) ;
+    _X_D = (_X_D < 0.5) ? std::max(_tol_l,_X_D) : std::min(_X_D,_tol_u); // constrain to interval [tol_l;tol_u]
 
-    X_H = 1.0 - X_D;
+    _X_H = 1.0 - _X_D;
 
     // calculate equilibrium
     // using the p_eq to calculate the T_eq - Clausius-Clapeyron
-    T_eq = (reaction_enthalpy/R) / ((reaction_entropy/R) + std::log(p_r_g)); // unit of p in bar
+    _T_eq = (_reaction_enthalpy/_R) / ((_reaction_entropy/_R) + std::log(_p_r_g)); // unit of p in bar
     // Alternative: Use T_s as T_eq and calculate p_eq - for Schaube kinetics
-    p_eq = std::exp((reaction_enthalpy/R)/T_s - (reaction_entropy/R));
+    _p_eq = std::exp((_reaction_enthalpy/_R)/_T_s - (_reaction_entropy/_R));
 }
 
 
@@ -97,19 +97,19 @@ double ReactionCaOH2::CaHydration()
 #ifdef SIMPLE_KINETICS
     if ( T_s < T_eq ) // hydration - simple model
 #else
-    if ( p_r_g > p_eq ) // hydration - Schaube model
+    if ( _p_r_g > _p_eq ) // hydration - Schaube model
 #endif
     {
         //X_H = max(tol_l,X_H); //lower tolerance to avoid oscillations at onset of hydration reaction. Set here so that no residual reaction rate occurs at end of hydration.
 #ifdef SIMPLE_KINETICS // this is from P. Schmidt
         dXdt = -1.0*(1.0-X_H) * (T_s - T_eq) / T_eq * 0.2 * conversion_rate::x_react;
 #else //this is from Schaube
-        if (X_H == tol_u || rho_s == rho_up)
+        if (_X_H == _tol_u || _rho_s == rho_up)
             dXdt = 0.0;
-        else if ( (T_eq-T_s) >= 50.0)
-            dXdt = 13945.0 * exp(-89486.0/R/T_s) * std::pow(p_r_g/p_eq - 1.0,0.83) * 3.0 * (X_D) * std::pow(-1.0*log(X_D),0.666);
+        else if ( (_T_eq-_T_s) >= 50.0)
+            dXdt = 13945.0 * exp(-89486.0/_R/_T_s) * std::pow(_p_r_g/_p_eq - 1.0,0.83) * 3.0 * (_X_D) * std::pow(-1.0*log(_X_D),0.666);
         else
-            dXdt = 1.0004e-34 * exp(5.3332e4/T_s) * std::pow(p_r_g, 6.0) * (X_D);
+            dXdt = 1.0004e-34 * exp(5.3332e4/_T_s) * std::pow(_p_r_g, 6.0) * (_X_D);
 #endif
     }
     else // dehydration
@@ -118,12 +118,12 @@ double ReactionCaOH2::CaHydration()
 #ifdef SIMPLE_KINETICS // this is from P. Schmidt
         dXdt = -1.0* (1.0-X_D) * (T_s - T_eq) / T_eq * 0.05;
 #else
-        if (X_D == tol_u || rho_s == rho_low)
+        if (_X_D == _tol_u || _rho_s == rho_low)
             dXdt = 0.0;
-        else if (X_D < 0.2)
-            dXdt = -1.9425e12 * exp( -1.8788e5/R/T_s ) * std::pow(1.0-p_r_g/p_eq,3.0)*(X_H);
+        else if (_X_D < 0.2)
+            dXdt = -1.9425e12 * exp( -1.8788e5/_R/_T_s ) * std::pow(1.0-_p_r_g/_p_eq,3.0)*(_X_H);
         else
-            dXdt = -8.9588e9 * exp( -1.6262e5/R/T_s ) * std::pow(1.0-p_r_g/p_eq,3.0)*2.0 * std::pow(X_H, 0.5);
+            dXdt = -8.9588e9 * exp( -1.6262e5/_R/_T_s ) * std::pow(1.0-_p_r_g/_p_eq,3.0)*2.0 * std::pow(_X_H, 0.5);
 #endif
     }
     return dXdt;
--- MaterialsLib/Adsorption/ReactionCaOH2.h
+++ MaterialsLib/Adsorption/ReactionCaOH2.h
@@ -27,7 +27,7 @@ class ReactionCaOH2 final : public Reaction
 {
 public:
     explicit ReactionCaOH2(BaseLib::ConfigTree const& conf)
-        : ode_solver_config{conf.getConfSubtree("ode_solver_config")}
+        : _ode_solver_config{conf.getConfSubtree("ode_solver_config")}
     {}
 
     double getEnthalpy(const double /*p_Ads*/, const double /*T_Ads*/,
@@ -36,14 +36,14 @@ public:
     double getReactionRate(const double /*p_Ads*/, const double /*T_Ads*/, const double /*M_Ads*/,
                              const double /*loading*/) const override;
 
-    const BaseLib::ConfigTree& getOdeSolverConfig() const { return ode_solver_config; }
+    const BaseLib::ConfigTree& getOdeSolverConfig() const { return _ode_solver_config; }
 
     // TODO merge with getReactionRate() above
     double getReactionRate(double const solid_density);
 
     void updateParam(double T_solid,
-                      double p_gas,
-                      double x_react,
+                      double _p_gas,
+                      double _x_react,
                       double rho_s_initial);
 
 private:
@@ -51,29 +51,29 @@ private:
     void setChemicalEquilibrium();
     double CaHydration();
 
-    static const double R;  //!< universal gas constant in J/mol/K
-    double rho_s;           //!< solid phase density
-    double p_gas;           //!< gas phase pressure in unit bar
-    double p_r_g;           //!< pressure of H2O on gas phase
-    double p_eq = 1.0;      //!< equilibrium pressure in bar
-    double T_eq;            //!< equilibrium temperature
-    double T_s;             //!< solid phase temperature
-    double qR;              //!< rate of solid density change
-    double x_react;         //!< mass fraction of water in gas phase
-    double X_D;             //!< mass fraction of dehydration (CaO) in the solid phase
-    double X_H;             //!< mass fraction of hydration in the solid phase
+    static const double _R;  //!< universal gas constant in J/mol/K
+    double _rho_s;           //!< solid phase density
+    double _p_gas;           //!< gas phase pressure in unit bar
+    double _p_r_g;           //!< pressure of H2O on gas phase
+    double _p_eq = 1.0;      //!< equilibrium pressure in bar
+    double _T_eq;            //!< equilibrium temperature
+    double _T_s;             //!< solid phase temperature
+    double _qR;              //!< rate of solid density change
+    double _x_react;         //!< mass fraction of water in gas phase
+    double _X_D;             //!< mass fraction of dehydration (CaO) in the solid phase
+    double _X_H;             //!< mass fraction of hydration in the solid phase
 
     //! reaction enthalpy in J/mol; negative for exothermic composition reaction
-    static const double reaction_enthalpy;
-    static const double reaction_entropy; //!< reaction entropy in J/mol/K
-    static const double M_carrier;        //!< inert component molar mass
-    static const double M_react;          //!< reactive component molar mass
+    static const double _reaction_enthalpy;
+    static const double _reaction_entropy; //!< reaction entropy in J/mol/K
+    static const double _M_carrier;        //!< inert component molar mass
+    static const double _M_react;          //!< reactive component molar mass
 
-    static const double tol_l;
-    static const double tol_u;
-    static const double tol_rho;
+    static const double _tol_l;
+    static const double _tol_u;
+    static const double _tol_rho;
 
-    const BaseLib::ConfigTree ode_solver_config;
+    const BaseLib::ConfigTree _ode_solver_config;
 
     template<typename>
     friend class ProcessLib::TESFEMReactionAdaptorCaOH2;
