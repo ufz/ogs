0f82dcd92aec279ff29643a1d4272dd0601a09dd
rinkk <karsten.rink@ufz.de>
2016-06-01 12:04:55 +0200

    added class for duplicating geometries

---
+++ GeoLib/DuplicateGeometry.cpp
@@ -0,0 +1,123 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2016, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#include "DuplicateGeometry.h"
+
+#include <memory>
+#include <vector>
+
+// ThirdParty/logog
+#include "logog/include/logog.hpp"
+
+#include "GeoLib/Point.h"
+#include "GeoLib/Polyline.h"
+#include "GeoLib/Surface.h"
+#include "GeoLib/Triangle.h"
+
+
+namespace GeoLib
+{
+
+DuplicateGeometry::DuplicateGeometry(GeoLib::GEOObjects &geo_objects,
+	std::string const& input_name,
+	std::string const& output_name)
+: _output_name(output_name), _geo_objects(geo_objects)
+{
+	duplicate(input_name);
+}
+
+void DuplicateGeometry::duplicate(std::string const& input_name)
+{
+	std::vector<GeoLib::Point*> const*const pnts (_geo_objects.getPointVec(input_name));
+	if (pnts == nullptr)
+	{
+		ERR("Geometry \"%s\" not found.", input_name.c_str());
+		return;
+	}
+
+	std::unique_ptr< std::vector<GeoLib::Point*> > new_pnts (new std::vector<GeoLib::Point*>);
+	new_pnts->reserve(pnts->size());
+	std::transform(pnts->cbegin(), pnts->cend(), std::back_inserter(*new_pnts),
+		[](GeoLib::Point* point) { return new GeoLib::Point(*point); });
+	_geo_objects.addPointVec(std::move(new_pnts), _output_name);
+
+	std::vector<GeoLib::Polyline*> const* plys (_geo_objects.getPolylineVec(input_name));
+	if (plys)
+	{
+		auto new_plys = copyPolylinesVector(*plys);
+		_geo_objects.addPolylineVec(std::move(new_plys), _output_name);
+	}
+
+	std::vector<GeoLib::Surface*> const* sfcs (_geo_objects.getSurfaceVec(input_name));
+	if (sfcs)
+	{
+		auto new_sfcs = copySurfacesVector(*sfcs);
+		_geo_objects.addSurfaceVec(std::move(new_sfcs), _output_name);
+	}
+}
+
+std::unique_ptr<std::vector<GeoLib::Polyline*>> DuplicateGeometry::copyPolylinesVector(
+	std::vector<GeoLib::Polyline*> const& polylines)
+{
+	std::size_t n_plys = polylines.size();
+	std::unique_ptr<std::vector<GeoLib::Polyline*>> new_lines
+		(new std::vector<Polyline*>(n_plys, nullptr));
+
+	for (std::size_t i=0; i<n_plys; ++i)
+	{
+		if (polylines[i] == nullptr)
+			continue;
+		(*new_lines)[i] = new GeoLib::Polyline(*_geo_objects.getPointVec(_output_name));
+		std::size_t nLinePnts (polylines[i]->getNumberOfPoints());
+		for (std::size_t j=0; j<nLinePnts; ++j)
+			(*new_lines)[i]->addPoint(polylines[i]->getPointID(j));
+	}
+	return new_lines;
+}
+
+std::unique_ptr<std::vector<Surface*>> DuplicateGeometry::copySurfacesVector(
+	std::vector<Surface*> const& surfaces)
+{
+	std::size_t n_sfc = surfaces.size();
+	std::unique_ptr<std::vector<GeoLib::Surface*>> new_surfaces
+		(new std::vector<Surface*>(n_sfc, nullptr));
+
+	for (std::size_t i=0; i<n_sfc; ++i)
+	{
+		if (surfaces[i] == nullptr)
+			continue;
+		(*new_surfaces)[i] = new GeoLib::Surface(*_geo_objects.getPointVec(_output_name));
+
+		std::size_t n_tris (surfaces[i]->getNTriangles());
+		for (std::size_t j=0; j<n_tris; ++j)
+		{
+			GeoLib::Triangle const* t = (*surfaces[i])[j];
+			(*new_surfaces)[i]->addTriangle(
+				t->getPoint(0)->getID(), t->getPoint(1)->getID(), t->getPoint(2)->getID());
+		}
+	}
+	return new_surfaces;
+}
+
+std::vector<GeoLib::Point*>& DuplicateGeometry::getPointVectorCopy()
+{
+	return const_cast<std::vector<GeoLib::Point*>&>(*_geo_objects.getPointVec(_output_name));
+}
+
+std::vector<GeoLib::Polyline*>& DuplicateGeometry::getPolylineVectorCopy()
+{
+	return const_cast<std::vector<GeoLib::Polyline*>&>(*_geo_objects.getPolylineVec(_output_name));
+}
+
+std::vector<GeoLib::Surface*>& DuplicateGeometry::getSurfaceVectorCopy()
+{
+	return const_cast<std::vector<GeoLib::Surface*>&>(*_geo_objects.getSurfaceVec(_output_name));
+}
+
+}
---
+++ GeoLib/DuplicateGeometry.h
@@ -0,0 +1,69 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2016, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#ifndef DUPLICATEGEOMETRY_H_
+#define DUPLICATEGEOMETRY_H_
+
+#include <string>
+
+#include "GeoLib/GEOObjects.h"
+
+namespace GeoLib
+{
+
+class Point;
+class Polyline;
+class Surface;
+
+/**
+ * Creates a copy of a geometry within GEOObjects
+ */
+class DuplicateGeometry final
+{
+
+public:
+	/**
+	 * Creates a copy of a geometry within GEOObjects
+	 * \param geo_objects The container for geometries
+	 * \param input_name  The geometry to be copied
+	 * \param output_name The name of the copy (note: this might be modified by GEOObjects)
+	 */
+	DuplicateGeometry(GeoLib::GEOObjects &geo_objects,
+		std::string const& input_name,
+		std::string const& output_name);
+
+	// Returns the (possibly modified) output name of the new geometry.
+	std::string const& getFinalizedOutputName() { return _output_name; }
+	// Returns a reference to the copied point vector for modification
+	std::vector<GeoLib::Point*>& getPointVectorCopy();
+	// Returns a reference to the copied polyline vector for modification
+	std::vector<GeoLib::Polyline*>& getPolylineVectorCopy();
+	// Returns a reference to the copied surface vector for modification
+	std::vector<GeoLib::Surface*>& getSurfaceVectorCopy();
+
+private:
+	// creates a deep copy of the designated geometry
+	void duplicate(std::string const& input_name);
+
+	// creates a deep copy of the polyline vector
+	std::unique_ptr<std::vector<GeoLib::Polyline*>> copyPolylinesVector(
+		std::vector<GeoLib::Polyline*> const& polylines);
+
+	// creates a deep copy of the surface vector
+	std::unique_ptr<std::vector<GeoLib::Surface*>> copySurfacesVector(
+		std::vector<GeoLib::Surface*> const& surfaces);
+
+	std::string _output_name;
+	GeoLib::GEOObjects& _geo_objects;
+
+}; // class
+
+} // namespace
+
+#endif /* DUPLICATEGEOMETRY_H_ */
