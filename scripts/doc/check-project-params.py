3cc0b73cb4a227d71a17abac1e97550c37235400
Christoph Lehmann <christoph.lehmann@ufz.de>
2016-06-03 09:02:02 +0200

    [scr/doc] removed unneeded comma

--- scripts/doc/check-project-params.py
+++ scripts/doc/check-project-params.py
@@ -35,7 +35,7 @@ for inline in sys.stdin:
         tag_name_comment = tag_path_comment.split(".")[-1]
 
         dirs = tag_path_comment.split(".")[:-1]
-        p = os.path.join(docauxdir, *dirs, )
+        p = os.path.join(docauxdir, *dirs)
         if     (not os.path.isfile(os.path.join(p,                   "t_" + tag_name_comment + ".dox"))) \
            and (not os.path.isfile(os.path.join(p,                   "a_" + tag_name_comment + ".dox"))) \
            and (not os.path.isfile(os.path.join(p, tag_name_comment, "i_" + tag_name_comment + ".dox"))) \
