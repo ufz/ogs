8de9dc04d7736d46eb617747e1dfa7d709592595
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2016-06-22 12:00:17 +0200

    rm endian checks in CMake

--- scripts/cmake/CheckTypeSizes.cmake
+++ scripts/cmake/CheckTypeSizes.cmake
@@ -15,15 +15,3 @@ else()
     add_definitions(-DHAVE_64_BIT)
     set( BITS 64)
 endif()
-
-# Check endian of the system
-include (TestBigEndian)
-TEST_BIG_ENDIAN (IS_BIG_ENDIAN)
-if (IS_BIG_ENDIAN)
-    add_definitions(-DENDIAN_IS_BIG)
-   # cannot use BIG_ENDIAN because it's reserved in Linux
-else ()
-    add_definitions(-DENDIAN_IS_LITTLE)
-   # cannot use LITTLE_ENDIAN because it's reserved in Linux
-endif () # IS_BIG_ENDIAN
-
