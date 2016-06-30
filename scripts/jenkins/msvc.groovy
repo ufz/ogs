fbe663561f448d1ca0009f5584bea46e861bbff7
Lars Bilke <lars.bilke@ufz.de>
2016-06-30 15:15:34 +0200

    Reordered msvc.groovy build.

--- scripts/jenkins/msvc.groovy
+++ scripts/jenkins/msvc.groovy
@@ -1,5 +1,7 @@
 defaultDockerArgs = '-v /home/jenkins/.ccache:/usr/src/.ccache'
 defaultCMakeOptions = '-DOGS_LIB_BOOST=System -DOGS_LIB_VTK=System -DOGS_DOWNLOAD_ADDITIONAL_CONTENT=ON'
+env32 = ['ARCH=msvc2013-x32', 'QTDIR=C:\\libs\\qt\\4.8\\$ARCH', 'Path=$Path;$QTDIR\\bin;C:\\Tools\\Conan\\conan']
+env64 = ['ARCH=msvc2013-x64', 'CMAKE_LIBRARY_SEARCH_PATH=C:\\libs\\$ARCH', 'QTDIR=C:\\libs\\qt\\4.8\\$ARCH', 'Path=$Path;$QTDIR\\bin;$CMAKE_LIBRARY_SEARCH_PATH\\bin']
 
 node('visserv3')
 {
@@ -8,8 +10,7 @@ node('visserv3')
     stage 'Checkout (Win)'
     dir('ogs') { checkout scm }
 
-    withEnv(['ARCH=msvc2013-x64', 'CMAKE_LIBRARY_SEARCH_PATH=C:\\libs\\$ARCH', 'QTDIR=C:\\libs\\qt\\4.8\\$ARCH', 'Path=$Path;$QTDIR\\bin;$CMAKE_LIBRARY_SEARCH_PATH\\bin']) {
-
+    withEnv(env64) {
         stage 'Configure (Win)'
         configure 'build', '', 'Visual Studio 12 Win64'
 
@@ -18,33 +19,28 @@ node('visserv3')
 
         stage 'Test (Win)'
         build 'build', 'tests'
-        build 'build', 'ctest'
 
         stage 'Data Explorer (Win)'
         configure 'build-de', '-DOGS_BUILD_GUI=ON -DOGS_BUILD_UTILS=ON -DOGS_BUILD_TESTS=OFF', 'Visual Studio 12 Win64'
         build 'build-de'
+    }
 
-        if (env.BRANCH_NAME == 'master') {
-            stage 'Package (Win)'
+    if (env.BRANCH_NAME == 'master') {
+        stage 'Release (Win)'
+        withEnv(env64) {
             build 'build', 'package'
             build 'build-de', 'package'
         }
-    }
-    if (env.BRANCH_NAME == 'master') {
-        withEnv(['ARCH=msvc2013-x32', 'QTDIR=C:\\libs\\qt\\4.8\\$ARCH', 'Path=$Path;$QTDIR\\bin;C:\\Tools\\Conan\\conan']) {
-            stage 'x32 (Win)'
+        withEnv(env32) {
             configure 'build-32', '-DOGS_BUILD_GUI=ON -DOGS_BUILD_UTILS=ON -DOGS_BUILD_TESTS=OFF', 'Visual Studio 12', '-u -s build_type=Release -s compiler="Visual Studio" -s compiler.version=12 -s arch=x86'
             build 'build-32', 'package'
         }
+        deploy 'build*/*.zip'
     }
 
     stage 'Post (Win)'
     publishTestReports 'build/Testing/**/*.xml', 'build/Tests/testrunner.xml',
         'ogs/scripts/jenkins/msvc-log-parser.rules'
-
-    archive 'build*/*.zip'
-
-    step([$class: 'S3BucketPublisher', dontWaitForConcurrentBuildCompletion: false, entries: [[bucket: 'opengeosys', excludedFile: '', flatten: true, gzipFiles: false, managedArtifacts: true, noUploadOnFailure: true, selectedRegion: 'eu-central-1', sourceFile: 'build*/*.zip', storageClass: 'STANDARD', uploadFromSlave: false, useServerSideEncryption: false]], profileName: 'S3 UFZ', userMetadata: []])
 
 }
 
@@ -67,13 +63,17 @@ def build(buildDir, target=null) {
            cmake --build . --config Release ${targetString}""".stripIndent())
 }
 
+def deploy(files) {
+    archive "${files}"
+    step([$class: 'S3BucketPublisher', dontWaitForConcurrentBuildCompletion: false, entries: [[bucket: 'opengeosys', excludedFile: '', flatten: true, gzipFiles: false, managedArtifacts: true, noUploadOnFailure: true, selectedRegion: 'eu-central-1', sourceFile: "${files}", storageClass: 'STANDARD', uploadFromSlave: false, useServerSideEncryption: false]], profileName: 'S3 UFZ', userMetadata: []])
+}
+
 def publishTestReports(ctestPattern, gtestPattern, parseRulefile) {
     step([$class: 'XUnitPublisher', testTimeMargin: '3000', thresholdMode: 1,
         thresholds: [
             [$class: 'FailedThreshold', failureNewThreshold: '', failureThreshold: '', unstableNewThreshold: '', unstableThreshold: ''],
             [$class: 'SkippedThreshold', failureNewThreshold: '', failureThreshold: '', unstableNewThreshold: '', unstableThreshold: '']],
         tools: [
-            [$class: 'CTestType', deleteOutputFiles: true, failIfNotNew: true, pattern: "${ctestPattern}", skipNoTestFiles: false, stopProcessingIfError: true],
             [$class: 'GoogleTestType', deleteOutputFiles: true, failIfNotNew: true, pattern: "${gtestPattern}", skipNoTestFiles: false, stopProcessingIfError: true]]
     ])
 
