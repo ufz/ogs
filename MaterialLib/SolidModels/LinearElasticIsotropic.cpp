5d8c7fb7a2910fe3029f7c11c6b3303a1c90de4e
Dmitri Naumov <dmitri.naumov@ufz.de>
2016-07-04 16:24:06 +0200

    [Mat] LinearElasticIsotropic model implementation.

---
+++ MaterialLib/SolidModels/LinearElasticIsotropic.cpp
@@ -0,0 +1,19 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2016, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#include "LinearElasticIsotropic.h"
+
+namespace Solids
+{
+
+template class LinearElasticIsotropic<2>;
+template class LinearElasticIsotropic<3>;
+
+
+}   // namespace Solids
