1cd5e30ef662a3203b009f74c5646f0df9c2ff0e
Christoph Lehmann <christoph.lehmann@ufz.de>
2016-07-06 17:28:15 +0200

    [NL] added named function provider

---
+++ NumLib/NamedFunctionProvider.h
@@ -0,0 +1,28 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2016, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#ifndef NUMLIB_NAMEDFUNCTIONPROVIDER_H
+#define NUMLIB_NAMEDFUNCTIONPROVIDER_H
+
+#include "NamedFunction.h"
+
+namespace NumLib
+{
+
+//! Interface used for providing named functions.
+class NamedFunctionProvider
+{
+public:
+    virtual std::vector<NamedFunction> getNamedFunctions() const { return {}; }
+    virtual ~NamedFunctionProvider() = default;
+};
+
+} // namespace NumLib
+
+#endif // NUMLIB_NAMEDFUNCTIONPROVIDER_H
