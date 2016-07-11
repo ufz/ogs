1283a71cc6352c72fe263daae09372a5042af15e
Wenqing Wang <wenqing.wang@ufz.de>
2016-07-11 15:33:32 +0200

    [CTest] Made all tests run with single core with PETSc

--- Applications/CLI/Tests.cmake
+++ Applications/CLI/Tests.cmake
@@ -201,14 +201,158 @@ else()
         cube_1e3_neumann_pcs_0_ts_1_t_1.000000_2.vtu pressure pressure
     )
 
+    # Single core
+    # CUBE 1x1x1 GROUNDWATER FLOW TESTS
+    foreach(mesh_size 1e0 1e1 1e2 1e3)
+        AddTest(
+            NAME GroundWaterFlowProcess_cube_1x1x1_${mesh_size}
+            PATH Elliptic/cube_1x1x1_GroundWaterFlow
+            EXECUTABLE_ARGS cube_${mesh_size}.prj
+            WRAPPER mpirun
+            WRAPPER_ARGS -np 1
+            TESTER vtkdiff
+            ABSTOL 1e-15 RELTOL 1e-15
+            DIFF_DATA cube_${mesh_size}_pcs_0_ts_1_t_1_0.vtu Linear_1_to_minus1 pressure
+        )
+
+        AddTest(
+            NAME GroundWaterFlowProcess_cube_1x1x1_Neumann_${mesh_size}
+            PATH Elliptic/cube_1x1x1_GroundWaterFlow
+            EXECUTABLE_ARGS cube_${mesh_size}_neumann.prj
+            WRAPPER mpirun
+            WRAPPER_ARGS -np 1
+            TESTER vtkdiff
+            ABSTOL 1e-1 RELTOL 1e-1
+            DIFF_DATA cube_${mesh_size}_neumann_pcs_0_ts_1_t_1_0.vtu D1_left_front_N1_right pressure
+        )
+    endforeach()
+
+
+    foreach(mesh_size 1e4 2e4 3e4 4e4 5e4 1e5 1e6)
+        AddTest(
+            NAME LARGE_GroundWaterFlowProcess_cube_1x1x1_${mesh_size}
+            PATH Elliptic/cube_1x1x1_GroundWaterFlow
+            EXECUTABLE_ARGS cube_${mesh_size}.prj
+            WRAPPER mpirun
+            WRAPPER_ARGS -np 1
+            TESTER vtkdiff
+            ABSTOL 1e-7 RELTOL 1e-7
+            DIFF_DATA cube_${mesh_size}_pcs_0_ts_1_t_1_0.vtu Linear_1_to_minus1 pressure
+        )
+
+        AddTest(
+            NAME LARGE_GroundWaterFlowProcess_cube_1x1x1_Neumann_${mesh_size}
+            PATH Elliptic/cube_1x1x1_GroundWaterFlow
+            EXECUTABLE_ARGS cube_${mesh_size}_neumann.prj
+            WRAPPER mpirun
+            WRAPPER_ARGS -np 1
+            TESTER vtkdiff
+            ABSTOL 1e-2 RELTOL 1e-2
+            DIFF_DATA cube_${mesh_size}_neumann_pcs_0_ts_1_t_1_0.vtu D1_left_front_N1_right pressure
+        )
+    endforeach()
+
+    # SQUARE 1x1 GROUNDWATER FLOW TESTS
+    foreach(mesh_size 1e0 1e1 1e2 1e3 1e4)
+        AddTest(
+            NAME GroundWaterFlowProcess_square_1x1_${mesh_size}
+            PATH Elliptic/square_1x1_GroundWaterFlow
+            EXECUTABLE_ARGS square_${mesh_size}.prj
+            WRAPPER mpirun
+            WRAPPER_ARGS -np 1
+            TESTER vtkdiff
+            ABSTOL 1e-13 RELTOL 1e-13
+            DIFF_DATA square_${mesh_size}_pcs_0_ts_1_t_1_0.vtu Linear_1_to_minus1 pressure
+        )
+
+        AddTest(
+            NAME GroundWaterFlowProcess_square_1x1_Neumann_${mesh_size}
+            PATH Elliptic/square_1x1_GroundWaterFlow
+            EXECUTABLE_ARGS square_${mesh_size}_neumann.prj
+            WRAPPER mpirun
+            WRAPPER_ARGS -np 1
+            TESTER vtkdiff
+            ABSTOL 1e-1 RELTOL 1e-1
+            DIFF_DATA square_${mesh_size}_neumann_pcs_0_ts_1_t_1_0.vtu D1_left_bottom_N1_right pressure
+        )
+    endforeach()
+
+    foreach(mesh_size 1e5 1e6)
+        AddTest(
+            NAME LARGE_GroundWaterFlowProcess_square_1x1_${mesh_size}
+            PATH Elliptic/square_1x1_GroundWaterFlow
+            EXECUTABLE_ARGS square_${mesh_size}.prj
+            WRAPPER mpirun
+            WRAPPER_ARGS -np 1
+            TESTER vtkdiff
+            ABSTOL 1e-7 RELTOL 1e-7
+            DIFF_DATA square_${mesh_size}_pcs_0_ts_1_t_1_0.vtu Linear_1_to_minus1 pressure
+        )
+
+        AddTest(
+            NAME LARGE_GroundWaterFlowProcess_square_1x1_Neumann_${mesh_size}
+            PATH Elliptic/square_1x1_GroundWaterFlow
+            EXECUTABLE_ARGS square_${mesh_size}_neumann.prj
+            WRAPPER mpirun
+            WRAPPER_ARGS -np 1
+            TESTER vtkdiff
+            ABSTOL 1e-02 RELTOL 1e-02
+            DIFF_DATA square_${mesh_size}_neumann_pcs_0_ts_1_t_1_0.vtu D1_left_bottom_N1_right pressure
+        )
+    endforeach()
+
+    # LINE 1 GROUNDWATER FLOW TESTS
+    foreach(mesh_size 1e1)
+        AddTest(
+            NAME GroundWaterFlowProcess_line_1_${mesh_size}
+            PATH Elliptic/line_1_GroundWaterFlow
+            EXECUTABLE_ARGS line_${mesh_size}.prj
+            WRAPPER mpirun
+            WRAPPER_ARGS -np 1
+            TESTER vtkdiff
+            ABSTOL 1e-15 RELTOL 1e-15
+            DIFF_DATA line_${mesh_size}_pcs_0_ts_1_t_1_0.vtu Linear_1_to_minus1 pressure
+        )
+
+        AddTest(
+            NAME GroundWaterFlowProcess_line_1_Neumann_${mesh_size}
+            PATH Elliptic/line_1_GroundWaterFlow
+            EXECUTABLE_ARGS line_${mesh_size}_neumann.prj
+            WRAPPER mpirun
+            WRAPPER_ARGS -np 1
+            TESTER vtkdiff
+            ABSTOL 1e-14 RELTOL 1e-14
+            DIFF_DATA line_${mesh_size}_neumann_pcs_0_ts_1_t_1_0.vtu D1_left_N1_right pressure
+        )
+        endforeach()
+
     AddTest(
-        NAME cube_1e3_neumann_np_1
-        PATH Elliptic/cube_1x1x1_GroundWaterFlow
-        EXECUTABLE_ARGS cube_1e3_neumann.prj
+        NAME TES_zeolite_discharge_small
+        PATH Parabolic/TES/1D
+        EXECUTABLE_ARGS tes-1D-zeolite-discharge-small.prj
         WRAPPER mpirun
         WRAPPER_ARGS -np 1
         TESTER vtkdiff
-        ABSTOL 1e-1 RELTOL 1e-1
-        DIFF_DATA cube_1e3_neumann_pcs_0_ts_1_t_1_0.vtu D1_left_front_N1_right pressure
+        ABSTOL 1e-16 RELTOL 1e-16
+        DIFF_DATA
+        tes_zeolite_discharge_small_pcs_0_ts_19_t_0.100000_0.vtu pressure pressure
+        tes_zeolite_discharge_small_pcs_0_ts_19_t_0.100000_0.vtu temperature temperature
+        tes_zeolite_discharge_small_pcs_0_ts_19_t_0.100000_0.vtu v_mass_frac v_mass_frac
+#        tes_zeolite_discharge_small_pcs_0_ts_19_t_0.100000_0.vtu solid_density solid_density
+    )
+
+    AddTest(
+        NAME LARGE_TES_zeolite_discharge
+        PATH Parabolic/TES/1D
+        EXECUTABLE_ARGS tes-1D-zeolite-discharge-large.prj
+        WRAPPER mpirun
+        WRAPPER_ARGS -np 1
+        TESTER vtkdiff
+        ABSTOL 1e-16 RELTOL 1e-16
+        DIFF_DATA
+        tes_zeolite_discharge_large_pcs_0_ts_28_t_1.000000_0.vtu pressure pressure
+        tes_zeolite_discharge_large_pcs_0_ts_28_t_1.000000_0.vtu temperature temperature
+        tes_zeolite_discharge_large_pcs_0_ts_28_t_1.000000_0.vtu v_mass_frac v_mass_frac
+#        tes_zeolite_discharge_large_pcs_0_ts_28_t_1_0.vtu solid_density solid_density
     )
 endif()
