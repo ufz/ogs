122719bc9190f2efcb52452fb02c531425840b0f
rinkk <karsten.rink@ufz.de>
2016-07-21 10:56:36 +0200

    interface to read swmm input/output files to geometry, mesh and csv

---
+++ Applications/Utils/SWMMConverter/SWMMConverter.cpp
@@ -0,0 +1,109 @@
+/**
+ * @copyright
+ * Copyright (c) 2012-2016, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ */
+
+#include <tclap/CmdLine.h>
+
+#include "SWMMInterface.h"
+#include "ThirdParty/SWMMInterface/swmm5_iface.h"
+
+#include "Applications/ApplicationsLib/LogogSetup.h"
+
+#include "BaseLib/StringTools.h"
+
+#include "GeoLib/GEOObjects.h"
+#include "GeoLib/IO/XmlIO/Boost/BoostXmlGmlInterface.h"
+
+#include "MeshLib/Mesh.h"
+#include "MeshLib/Properties.h"
+#include "MeshLib/IO/VtkIO/VtuInterface.h"
+
+#include "Applications/FileIO/CsvInterface.h"
+
+int main(int argc, char *argv[])
+{
+    ApplicationsLib::LogogSetup setup;
+
+    TCLAP::CmdLine cmd
+        ("Read files for the Storm Water Management Model (SWMM) and converts them to OGS.", ' ', "0.1");
+    TCLAP::ValueArg<std::string> mesh_output_arg
+        ("m","mesh", "mesh output file (*.vtu)", false, "", "mesh output file");
+    cmd.add(mesh_output_arg);
+    TCLAP::ValueArg<std::string> geo_output_arg
+        ("g","geo", "geometry output file (*.gml)", false, "", "geometry output file");
+    cmd.add(geo_output_arg);
+    TCLAP::ValueArg<std::string> swmm_input_arg
+        ("i","input", "SWMM input file (*.inp)", true, "", "input file");
+    cmd.add(swmm_input_arg);
+    cmd.parse( argc, argv );
+
+    if (!(geo_output_arg.isSet() || mesh_output_arg.isSet()))
+    {
+        ERR ("No output format given. Please specify OGS geometry or mesh output file.");
+        return -1;
+    }
+
+    if (geo_output_arg.isSet())
+    {
+        GeoLib::GEOObjects geo_objects;
+        if (!SwmmInterface::SwmmInputToGeometry(swmm_input_arg.getValue(), geo_objects, true))
+            return -1;
+
+        GeoLib::IO::BoostXmlGmlInterface xml(geo_objects);
+        xml.setNameForExport(BaseLib::extractBaseNameWithoutExtension(swmm_input_arg.getValue()));
+        xml.writeToFile(geo_output_arg.getValue());
+        return 0;
+    }
+
+    SwmmInterface* swmm = nullptr;
+    MeshLib::Mesh* mesh = nullptr;
+    if (mesh_output_arg.isSet())
+    {
+        swmm = SwmmInterface::create(swmm_input_arg.getValue());
+        std::size_t a = swmm->getNObjects(SwmmObject::SUBCATCHMENT);
+        if (swmm == nullptr)
+            return -1;
+
+        mesh = swmm->getMesh();
+        MeshLib::IO::VtuInterface vtkIO(mesh, 0, false);
+        vtkIO.writeToFile(mesh_output_arg.getValue());
+    }
+
+    std::cout << "Simulation time steps: " << swmm->getNTimeSteps() << std::endl;
+
+/*
+    // Writing node information to csv file
+    FileIO::CsvInterface csv;
+    csv.addIndexVectorForWriting(SWMM_Nnodes);
+    for (std::size_t i=0; i<9; ++i)
+    {
+        std::vector<double> data_vec = swmm.getArrayAtTimeStep(SwmmObject::NODE, 10, i);
+        csv.addVectorForWriting(swmm.getArrayName(SwmmObject::NODE, i), data_vec);
+    }
+    csv.writeToFile("d:/csvtest.csv");
+    std::cout << "csv written" << std::endl;
+    std::cin.ignore();
+
+    // Add simulated parameter to mesh for each timestep and write result
+    for (std::size_t i=0; i<SWMM_Nperiods; i+=10)
+    {
+        SwmmObject type = SwmmObject::NODE;
+        std::size_t p_id = 8;
+        std::string vec_name (swmm.getArrayName(type, p_id));
+        std::vector<double> data_vec = swmm.getArrayAtTimeStep(type, i, p_id);
+        bool done = swmm.addResultsToMesh(*mesh, type, vec_name, data_vec);
+        if (done)
+        {
+            MeshLib::IO::VtuInterface vtkio(mesh, 0, false);
+            std::string name ("d:/swmmresults" + BaseLib::tostring(i) + ".vtu");
+            vtkio.writeToFile(name);
+            mesh->getProperties().removePropertyVector(vec_name);
+        }
+    }
+*/
+    return 0;
+}
---
+++ Applications/Utils/SWMMConverter/SWMMInterface.h
@@ -0,0 +1,239 @@
+ /**
+ * Copyright (c) 2012-2016, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ */
+
+#ifndef SWMMINTERFACE_H_
+#define SWMMINTERFACE_H_
+
+#include <iostream>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <logog/include/logog.hpp>
+
+#include "ThirdParty/SWMMInterface/swmm5_iface.h"
+
+#include "GeoLib/Station.h"
+
+namespace GeoLib {
+    class GEOObjects;
+    class Point;
+    class Polyline;
+}
+
+namespace MeshLib {
+    class Mesh;
+    class Node;
+    class Element;
+}
+
+/// SWMM object types
+enum class SwmmObject
+{
+    SUBCATCHMENT = 0,
+    NODE = 1,
+    LINK = 2,
+    SYSTEM = 3
+};
+
+/**
+ * Interface for reading files used within the  * Storm Water Management Model (SWMM) and
+ * converting the data therein into corresponding OGS data structures.
+ * SWMM distinguishes four different object types (defined in the SwmmObject enum):
+ *     * Subcatchments
+ *     * Nodes (or junctions)
+ *     * Links (or conduits)
+ *     * System
+ * Note that each object in a SWMM input file has a name.
+ * Each of the four object types has a (different) number of parameters. There can be an arbitrary
+ * number of nodes, links or subcatchments but only ever one system.
+ * The interface can convert the SWMM input data into a geometry or a (line-)mesh.
+ * For meshes, also output data can be added to nodes and elements.
+ * The interface also provides routines for returning data as vectors as well as convenience 
+ * methods for outputting data into CSV files. CSV files will either contain all parameters for one
+ * object at all time steps or all parameters for all objects of a given type at one timestep.
+ */
+class SwmmInterface
+{
+public:
+    /// Basic method to create the interface (containing a mesh) from a SWMM file
+    /// Other non-static methods rely on this being called in the beginning.
+    static SwmmInterface* create(std::string const& file_name);
+
+    /// If a mesh has already been created, this methods allows to add node- or link-arrays as property
+    /// to that mesh. The data vectors can be created using the getArrayAtTimeStep() method.
+    static bool addResultsToMesh(MeshLib::Mesh &mesh, SwmmObject const type,
+        std::string const& vec_name, std::vector<double> const& data);
+
+    /// Returns the mesh generated from SWMM file content.
+    MeshLib::Mesh* getMesh() const { return _mesh.get(); }
+
+    /// Returns the name of the data array for the given object type and parameter index.
+    std::string getArrayName(SwmmObject obj_type, std::size_t var_idx) const;
+
+    /// Get all the object names for a given object type
+    std::vector<std::string> getNames(SwmmObject obj_type) const;
+
+    /// Returns the Name for the indexed object of the given type (or an empty string if an error occured).
+    std::string getName(SwmmObject obj_type, std::size_t idx) const;
+
+    /// Returns the number of objects of the given type.
+    std::size_t getNObjects(SwmmObject obj_type) const;
+
+    /// Returns the number of parameters (incl. pollutants) of the given type.
+    std::size_t getNParameters(SwmmObject obj_type) const;
+
+    /// Returns the number of time steps for the simulation results.
+    std::size_t getNTimeSteps() const;
+
+    /// Returns an array for a given variable at all nodes/links from a SWMM output file for a given time step.
+    std::vector<double> getArrayAtTimeStep(SwmmObject obj_type, std::size_t time_step, std::size_t var_idx) const;
+
+    /// Returns an array for a given variable for one specific object from a SWMM output file for all time steps.
+    std::vector<double> getArrayForObject(SwmmObject obj_type, std::size_t obj_idx, std::size_t var_idx) const;
+
+    /// Write a CSV file for all object of the given type at one time step
+    bool writeCsvForTimestep(std::string const& file_name, SwmmObject obj_type, std::size_t time_step) const;
+
+    /// Write a CSV file for one object of the given type for all time steps
+    bool writeCsvForObject(std::string const& file_name, SwmmObject obj_type, std::size_t obj_idx) const;
+
+    /// Checks if file is a SWMM input file
+    static bool isSwmmInputFile(std::string const& inp_file_name);
+    
+    /// Checks if file is a SWMM output file
+    static bool isSwmmOutputFile(std::string const& inp_file_name);
+
+    /// Reading a SWMM input file and conversion into OGS geometry.
+    static bool SwmmInputToGeometry(std::string const& inp_file_name, GeoLib::GEOObjects &geo_objects, bool add_subcatchments);
+
+    /// Destructor
+    ~SwmmInterface();
+
+private:
+    /// Constructor
+    SwmmInterface(std::string const& swmm_base_name);
+
+    /// Reading a SWMM input file and creating an OGS line mesh. This is automatically called when the object is created
+    bool convertSwmmInputToLineMesh();
+
+    /// Reading points from SWMM input file and converting them into OGS point-type vector.
+    /// This method is shared by geometry- and mesh conversion.
+    template <typename T>
+    static bool readCoordinates(std::ifstream &in, std::vector<T*> &points, std::vector<std::string> &names)
+    {
+        bool finished (false);
+        std::size_t id (points.size());
+        std::string line;
+        getline(in, line);
+        while (!isSectionFinished(line))
+        {
+            if (isCommentLine(line))
+            {
+                getline(in, line);
+                continue;
+            }
+
+            std::vector<std::string> split_str (BaseLib::splitString(line));
+            if (split_str.size() != 3)
+            {
+                ERR ("Format not recognised.");
+                return false;
+            }
+            names.push_back(split_str[0]);
+
+            double const x = BaseLib::str2number<double>(split_str[1]);
+            double const y = BaseLib::str2number<double>(split_str[2]);
+            T* pnt = new T(x, y, 0, id);
+            points.push_back(pnt);
+            id++;
+            getline(in, line);
+        }
+        return true;
+    }
+
+    /// Reads input information associated with nodes (elevation, depth, etc.)
+    bool readNodeData(std::ifstream &in, std::vector<MeshLib::Node*> &nodes,
+        std::map<std::string, std::size_t> const& name_id_map,
+        std::vector<double> &max_depth, bool read_max_depth);
+
+    /// Reads links/conduits and returns them as a vector of OGS line elements
+    bool readLineElements(std::ifstream &in, std::vector<MeshLib::Element*> &elements,
+        std::vector<MeshLib::Node*> const& nodes, std::map<std::string, std::size_t> const& name_id_map);
+
+    /// Reads subcatchment information
+    bool readSubcatchments(std::ifstream &in, std::map< std::string, std::size_t> const& name_id_map);
+
+    /// Reads pollutant names and parameters
+    bool readPollutants(std::ifstream &in);
+
+    /// Returns the name of an array (or an empty string if errors occured)
+    std::string getArrayName(SwmmObject obj_type, std::size_t var_idx, std::size_t n_pollutants) const;
+
+    /// Reads the location of external rain gauge time series files.
+    bool addRainGaugeTimeSeriesLocations(std::ifstream &in);
+
+    /// Matches existing subcatchment names with subsequently read polylines marking the outlines of said subcatchments.
+    bool matchSubcatchmentsWithOutlines(std::vector<GeoLib::Polyline*> const& lines, std::vector<std::string> const& names);
+
+    /// Creates a temporary string vector containing all subcatchment names in correct order
+    std::vector<std::string> getSubcatchmentNameMap() const;
+
+    /// During geometry conversion, this adds elevation values to the existing point vector
+    static bool SwmmInterface::addPointElevation(std::ifstream &in,
+        std::vector<GeoLib::Point*> &points, std::map<std::string,
+        std::size_t> const& name_id_map);
+
+    /// During geometry conversion, this reads links (conduits/pumps/weirs) and converts them into polylines.
+    static bool readLinksAsPolylines(std::ifstream &in,
+        std::vector<GeoLib::Polyline*> &lines, std::vector<std::string> &line_names,
+        std::vector<GeoLib::Point*> const& points, std::map<std::string, std::size_t> const& point_names);
+
+    /// During geometry conversion, this reads polygones representing subcatchments
+    /// from the SWMM input file and converts them into OGS polyline vector.
+    static bool readPolygons(std::ifstream &in, std::vector<GeoLib::Polyline*> &lines,
+        std::vector<std::string> &line_names, std::vector<GeoLib::Point*> &points,
+        std::vector<std::string> &pnt_names);
+
+    /// Checks if the given line string is empty. Empty strings mark the end of sections in a SWMM input file.
+    static bool isSectionFinished(std::string const& str);
+
+    /// Checks if the given line string is a comment line.
+    static bool isCommentLine(std::string const& str);
+
+    /// Subcatchment data structure.
+    /// (SWMM stores ~20 parameters for subcatchments. Depending on relevance this struct might be extended in the future.)
+    struct Subcatchment
+    {
+        std::string name;
+        std::size_t rain_gauge;
+        std::size_t outlet;
+        double area;
+        GeoLib::Polyline* outline;
+    };
+
+    //variables
+    /// All files for a given SWMM simulation have the same base name.
+    std::string const _base_name;
+    /// Vector storing the names of all nodes/junctions
+    std::vector<std::string> _id_nodename_map;
+    /// Vector storing the names of all links/conduits
+    std::vector<std::string> _id_linkname_map;
+    /// Vector storing the names of all pollutants
+    std::vector<std::string> _pollutant_names;
+    /// Vector storing information about all subcatchments
+    std::vector<Subcatchment> _subcatchments;
+    /// Seperate node vector containing points for defining subcatchment outlines
+    std::vector<GeoLib::Point*> _subcatchment_points;
+    /// Vector containing rain gauge information as well the position of external time series files
+    std::vector< std::pair<GeoLib::Station, std::string> > _rain_gauges;
+    /// Mesh generated from SWMM input (+ optional output data)
+    std::unique_ptr<MeshLib::Mesh> _mesh;
+};
+
+#endif // SWMMINTERFACE_H_
