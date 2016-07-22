b60886b9296fd7994ac37be442189c346b3518d1
Thomas Fischer <thomas.fischer@ufz.de>
2016-07-22 09:09:31 +0200

    [MGTL] Remove old GeoMapper:advancedMapOnMesh impl.

--- MeshGeoToolsLib/GeoMapper.h
+++ MeshGeoToolsLib/GeoMapper.h
@@ -56,16 +56,14 @@ public:
     void mapToConstantValue(double value);
 
     /**
-     * Maps the geometry based on the given mesh file. A new geometry is created where all
-     * geometric points are assigned an elevation value on the mesh surface. Additional points are
-     * inserted whenever a polyline from the original geometry intersects a mesh node or the edge
-     * of a mesh element.
+     * Maps the geometry based on the given mesh file. I.e., all geometric
+     * points are assigned an elevation value on the mesh surface. Additional
+     * points are inserted whenever a polyline from the original geometry
+     * intersects a mesh node or the edge of a mesh element.
      * \param mesh          Mesh the geometry is mapped on
-     * \param new_geo_name  Name of the newly created geometry
-     * \result              A new geometry with the given name is inserted into _geo_objects
+     * \result              A new geometry with the given name is inserted into
+     * _geo_objects
      */
-    void advancedMapOnMesh(const MeshLib::Mesh* mesh, const std::string &new_geo_name);
-
     void advancedMapOnMesh(MeshLib::Mesh const& mesh);
 
 private:
@@ -84,18 +82,6 @@ private:
 
     /// Returns the elevation at Point (x,y) based on a raster
     float getDemElevation(GeoLib::Point const& pnt) const;
-
-    /// Calculates the intersection of two lines embedded in the xy-plane
-    GeoLib::Point* calcIntersection(MathLib::Point3d const*const p1, MathLib::Point3d const*const p2, GeoLib::Point const*const q1, GeoLib::Point const*const q2) const;
-
-    /// Returns the position of a point within a line-segment
-    unsigned getPointPosInLine(GeoLib::Polyline const*const line, unsigned start, unsigned end, GeoLib::Point const*const point, double eps) const;
-
-    /// Returns the maximum segment length in a polyline vector
-    double getMaxSegmentLength(const std::vector<GeoLib::Polyline*> &lines) const;
-
-    /// Returns if a point p is within a bounding box defined by a and b
-    bool isPntInBoundingBox(double ax, double ay, double bx, double by, double px, double py) const;
 
     GeoLib::GEOObjects& _geo_objects;
     std::string& _geo_name;
