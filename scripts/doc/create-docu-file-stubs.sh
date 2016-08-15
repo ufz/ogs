0f2de16bce0808e236c8d76e4b98d18d556747ea
Christoph Lehmann <christoph.lehmann@ufz.de>
2016-08-15 08:49:51 +0200

    [scr] docu file stubs appear in missing docu list
    Script already contains a usage comment.

--- scripts/doc/create-docu-file-stubs.sh
+++ scripts/doc/create-docu-file-stubs.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script creates missing file in the OGS input file documentation source tree.
 # It expects input from get-project-params.sh
@@ -32,27 +32,27 @@ done \
         bdn="`basename "$dn"`"
         if [ "`expr match "$bdn" '^[A-Z]'`" -eq 0 ] && [ ! -f "$dn/i_$bdn.md" ]; then
             echo "creating $dn/i_$bdn.md"
-            echo '\todo document' >"$dn/i_$bdn.md"
+            echo '\ogs_missing_documentation' >"$dn/i_$bdn.md"
         elif [ "`expr match "$bdn" '^[A-Z]'`" -ne 0 ] && [ ! -f "$dn/c_$bdn.md" ]; then
             echo "creating $dn/c_$bdn.md"
-            echo '\todo document' >"$dn/c_$bdn.md"
+            echo '\ogs_missing_documentation' >"$dn/c_$bdn.md"
         fi
     fi
 
     if [ -d "$path" ]; then
         if [ "`expr match "$bn" '^[A-Z]'`" -eq 0 ] && [ ! -f "$path/i_$bn.md" ]; then
             echo "creating $path/i_$bn.md"
-            echo '\todo document' >"$path/i_$bn.md"
+            echo '\ogs_missing_documentation' >"$path/i_$bn.md"
         elif [ "`expr match "$bn" '^[A-Z]'`" -ne 0 ] && [ ! -f "$path/c_$bn.md" ]; then
             echo "creating $path/c_$bn.md"
-            echo '\todo document' >"$path/c_$bn.md"
+            echo '\ogs_missing_documentation' >"$path/c_$bn.md"
         fi
     elif [ "$param_or_attr" = param ] && [ ! -f "$dn/t_$bn.md" ]; then
         echo "creating $dn/t_$bn.md"
-        echo '\todo document' >"$dn/t_$bn.md"
+        echo '\ogs_missing_documentation' >"$dn/t_$bn.md"
     elif [ "$param_or_attr" = attr ] && [ ! -f "$dn/a_$bn.md" ]; then
         echo "creating $dn/a_$bn.md"
-        echo '\todo document' >"$dn/a_$bn.md"
+        echo '\ogs_missing_documentation' >"$dn/a_$bn.md"
     # else
     #     echo "OK $path"
     fi
