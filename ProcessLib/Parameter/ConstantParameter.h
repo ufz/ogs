953f53cc25e8880157c64db78248f5a4dd42d84a
Dmitri Naumov <github@naumov.de>
2016-08-27 00:09:41 +0000

    [PL] Multi-component constant parameters.
    The tag value tag still can be used for scalar values.

--- ProcessLib/Parameter/ConstantParameter.h
+++ ProcessLib/Parameter/ConstantParameter.h
@@ -16,11 +16,20 @@ namespace ProcessLib
 {
 /// Single, constant value parameter.
 template <typename T>
-struct ConstantParameter final : public Parameter<T> {
+struct ConstantParameter final : public Parameter<T>
+{
+    /// Construction with single value.
     ConstantParameter(T const& value) : _value({value}) {}
-
-    // TODO allow for different sizes
-    unsigned getNumberOfComponents() const override { return 1; }
+    /// Construction with a tuple.
+    /// The given tuple must be non-empty.
+    ConstantParameter(std::vector<T> const& value) : _value(value)
+    {
+        assert(!value.empty());
+    }
+    unsigned getNumberOfComponents() const override
+    {
+        return static_cast<unsigned>(_value.size());
+    }
 
     std::vector<T> const& getTuple(
         double const /*t*/, SpatialPosition const& /*pos*/) const override
