c14a6b5001a45311c72b6469b6fb3464d6403204
Zheng <ichbinzty@gmail.com>
2016-08-31 15:39:17 +0200

    reduce the timestep in test case to get a smoother result

--- Applications/CLI/Tests.cmake
+++ Applications/CLI/Tests.cmake
@@ -224,10 +224,10 @@ if(NOT OGS_USE_MPI)
         EXECUTABLE_ARGS line_60_heat.prj
         WRAPPER time
         TESTER vtkdiff
-        ABSTOL 1e-2 RELTOL 1e-2
+        ABSTOL 1e-4 RELTOL 1e-4
         DIFF_DATA
-        temperature_analytical.vtu line_60_heat_pcs_0_ts_13_t_5078125.000000.vtu Temperature_Analytical_2months temperature
-        temperature_analytical.vtu line_60_heat_pcs_0_ts_81_t_31640625.000000.vtu Temperature_Analytical_1year temperature
+        temperature_analytical.vtu line_60_heat_pcs_0_ts_65_t_5078125.000000.vtu Temperature_Analytical_2months temperature
+        temperature_analytical.vtu line_60_heat_pcs_0_ts_405_t_31640625.000000.vtu Temperature_Analytical_1year temperature
    )
 
    AddTest(
@@ -237,10 +237,10 @@ if(NOT OGS_USE_MPI)
         EXECUTABLE_ARGS line_60_heat.prj
         WRAPPER time
         TESTER vtkdiff
-        ABSTOL 1e-2 RELTOL 1e-2
+        ABSTOL 1e-4 RELTOL 1e-4
         DIFF_DATA
-        temperature_analytical.vtu line_60_heat_pcs_0_ts_13_t_5078125.000000.vtu Temperature_Analytical_2months temperature
-        temperature_analytical.vtu line_60_heat_pcs_0_ts_81_t_31640625.000000.vtu Temperature_Analytical_1year temperature
+        temperature_analytical.vtu line_60_heat_pcs_0_ts_65_t_5078125.000000.vtu Temperature_Analytical_2months temperature
+        temperature_analytical.vtu line_60_heat_pcs_0_ts_405_t_31640625.000000.vtu Temperature_Analytical_1year temperature
    )
 
 else()
