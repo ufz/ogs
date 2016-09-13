45a4aba3c4f3a47ef2a414d7627cf16c3a3d74ed
Thomas Fischer <thomas.fischer@ufz.de>
2016-09-13 11:07:21 +0200

    [MeL] Add PropertyVector to mesh and init the PV.

--- MeshLib/Mesh.h
+++ MeshLib/Mesh.h
@@ -20,6 +20,7 @@
 #include <vector>
 
 #include "BaseLib/Counter.h"
+#include "BaseLib/Error.h"
 
 #include "MeshEnums.h"
 #include "Properties.h"
@@ -171,6 +172,47 @@ protected:
 void scaleMeshPropertyVector(MeshLib::Mesh& mesh,
                              std::string const& property_name,
                              double factor);
+
+/// Creates a new \c PropertyVector in the given mesh and initializes it with
+/// the given data. A \c PropertyVector with the same name must not exist.
+/// \param mesh A \c Mesh the new \c ProperyVector will be created in.
+/// \param name A string that contains the name of the new \c PropertyVector.
+/// \param item_type One of the values \c MeshLib::MeshItemType::Cell or \c
+/// \c MeshLib::MeshItemType::Node that shows the association of the property
+/// values either to \c Element's / cells or \c Node's
+/// \param number_of_components the number of components of a property
+/// \param values A vector containing the values that are used for
+/// initialization.
+template <typename T>
+void addPropertyToMesh(MeshLib::Mesh& mesh, std::string const& name,
+                       MeshLib::MeshItemType item_type,
+                       std::size_t number_of_components,
+                       std::vector<T> const& values)
+{
+    if (item_type == MeshLib::MeshItemType::Node)
+        if (mesh.getNumberOfNodes() != values.size() / number_of_components)
+            OGS_FATAL(
+                "Error number of nodes (%u) does not match the number of "
+                "tuples (%u).",
+                mesh.getNumberOfNodes(), values.size() / number_of_components);
+    if (item_type == MeshLib::MeshItemType::Cell)
+        if (mesh.getNumberOfElements() != values.size() / number_of_components)
+            OGS_FATAL(
+                "Error number of elements (%u) does not match the number of "
+                "tuples (%u).",
+                mesh.getNumberOfElements(),
+                values.size() / number_of_components);
+
+    boost::optional<MeshLib::PropertyVector<T>&> property(
+        mesh.getProperties().createNewPropertyVector<T>(name, item_type,
+                                                        number_of_components));
+    if (!property)
+    {
+        OGS_FATAL("Error while creating PropertyVector \"%s\".", name.c_str());
+    }
+    property->reserve(values.size());
+    std::copy(values.cbegin(), values.cend(), std::back_inserter(*property));
+}
 
 } /* namespace */
 
