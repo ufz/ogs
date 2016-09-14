6e3df4ffe22d41471edd3270db6662f945680799
Lars Bilke <lars.bilke@ufz.de>
2016-09-14 16:11:23 +0200

    [Jenkins] Building of release jobs defined in Jenkinsfile.

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,6 +1,7 @@
 #!/usr/bin/env groovy
 
 node('master') {
+    helper = load 'scripts/jenkins/lib/helper.groovy'
 
     step([$class: 'GitHubSetCommitStatusBuilder', statusMessage: [content: 'Started Jenkins build']])
     checkout scm
@@ -8,16 +9,17 @@ node('master') {
     def builders = [:]
     builders['gcc'] = { load 'scripts/jenkins/gcc.groovy' }
     builders['msvc'] = { load 'scripts/jenkins/msvc.groovy' }
-    builders['msvc32'] = { load 'scripts/jenkins/msvc32.groovy' }
     builders['mac'] = { load 'scripts/jenkins/mac.groovy' }
+
+    if (helper.isRelease()) {
+        builders['msvc32'] = { load 'scripts/jenkins/msvc32.groovy' }
+    }
 
     parallel builders
 
     step([$class: 'GitHubCommitStatusSetter'])
 
-    if (env.BRANCH_NAME == 'master') {
-        build job: 'OGS-6/Deploy', wait: false
-    }
+    if (helper.isRelease()) { build job: 'OGS-6/Deploy', wait: false }
 }
 
 properties ([[$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', artifactDaysToKeepStr: '', artifactNumToKeepStr: '5', daysToKeepStr: '', numToKeepStr: '25']]])
--- scripts/jenkins/msvc32.groovy
+++ scripts/jenkins/msvc32.groovy
@@ -1,6 +1,5 @@
 configure = load 'scripts/jenkins/lib/configure.groovy'
 build     = load 'scripts/jenkins/lib/build.groovy'
-helper    = load 'scripts/jenkins/lib/helper.groovy'
 
 defaultCMakeOptions = '-DCMAKE_BUILD_TYPE=Release -DOGS_32_BIT=ON -DOGS_LIB_BOOST=System ' +
     '-DOGS_LIB_VTK=System -DOGS_DOWNLOAD_ADDITIONAL_CONTENT=ON'
@@ -9,14 +8,12 @@ node('win && conan') {
     stage 'Checkout (Win)'
     dir('ogs') { checkout scm }
 
-    if (helper.isRelease()) {
-        stage 'Data Explorer 32-bit (Win)'
-        withEnv(helper.getEnv('x32')) {
-            configure.win 'build-32', '-DOGS_BUILD_GUI=ON -DOGS_BUILD_UTILS=ON ' +
-                '-DOGS_BUILD_TESTS=OFF', 'Ninja', '-u -s build_type=Release -s compiler="Visual ' +
-                'Studio" -s compiler.version=12 -s arch=x86'
-            build.win 'build-32'
-        }
-        archive 'build-32/*.zip'
+    stage 'Data Explorer 32-bit (Win)'
+    withEnv(helper.getEnv('x32')) {
+        configure.win 'build-32', '-DOGS_BUILD_GUI=ON -DOGS_BUILD_UTILS=ON ' +
+            '-DOGS_BUILD_TESTS=OFF', 'Ninja', '-u -s build_type=Release -s compiler="Visual ' +
+            'Studio" -s compiler.version=12 -s arch=x86'
+        build.win 'build-32'
     }
+    archive 'build-32/*.zip'
 }
