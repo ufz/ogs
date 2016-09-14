0e95eddd8509dc25ed3521fbb31a01eae37f13b4
Lars Bilke <lars.bilke@ufz.de>
2016-09-14 14:34:26 +0200

    [Jenkins] Migrated clang and gcc to moved functions.

--- scripts/jenkins/clang.groovy
+++ scripts/jenkins/clang.groovy
@@ -1,45 +1,25 @@
+configure = load 'scripts/jenkins/lib/configure.groovy'
+build     = load 'scripts/jenkins/lib/build.groovy'
+post      = load 'scripts/jenkins/lib/post.groovy'
+
 defaultDockerArgs = '-v /home/jenkins/.ccache:/usr/src/.ccache'
 
 node('docker') {
     stage 'Checkout'
     dir('ogs') { checkout scm }
 
-    // Multiple configurations are build in parallel
-    parallel linux: {
-        docker.image('ogs6/clang-base:latest').inside(defaultDockerArgs) {
-            catchError {
-                build 'build', '-DOGS_ADDRESS_SANITIZER=ON ' +
-                    '-DOGS_UNDEFINED_BEHAVIOR_SANITIZER=ON', ''
+    docker.image('ogs6/clang-base:latest').inside(defaultDockerArgs) {
+        catchError {
+            configure.linux 'build', '-DOGS_ADDRESS_SANITIZER=ON ' +
+                '-DOGS_UNDEFINED_BEHAVIOR_SANITIZER=ON', ''
 
-                stage 'Unit tests'
-                sh '''cd build
-                      UBSAN_OPTIONS=print_stacktrace=1 make tests'''
+            stage 'Unit tests'
+            build.linux 'build', 'tests', 'UBSAN_OPTIONS=print_stacktrace=1 make'
 
-                stage 'End-to-end tests'
-                sh '''cd build
-                      UBSAN_OPTIONS=print_stacktrace=1 make ctest'''
-            }
+            stage 'End-to-end tests'
+            build.linux 'build', 'ctest', 'UBSAN_OPTIONS=print_stacktrace=1 make'
         }
-        step([$class: 'LogParserPublisher', failBuildOnError: true, unstableOnWarning: true,
-            projectRulePath: 'ogs/scripts/jenkins/clang-log-parser.rules', useProjectRule: true])
     }
-
-    step([$class: 'JUnitResultArchiver',
-        testResults: 'build/Tests/testrunner.xml'])
-
-    archive 'build*/*.tar.gz'
-} // end node
-
-
-def build(buildDir, cmakeOptions, target) {
-    sh "rm -rf ${buildDir} && mkdir ${buildDir}"
-
-    stage 'Configure'
-    sh "cd ${buildDir} && cmake ../ogs ${cmakeOptions}"
-
-    stage 'Build'
-    sh "cd ${buildDir} && make -j \$(nproc) ${target}"
+    post.publishTestReports('build/Testing/**/*.xml','build/Tests/testrunner.xml',
+        'ogs/scripts/jenkins/clang-log-parser.rules')
 }
-
-properties([[$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator',
-    artifactDaysToKeepStr: '', artifactNumToKeepStr: '5', daysToKeepStr: '', numToKeepStr: '25']]])
--- scripts/jenkins/gcc.groovy
+++ scripts/jenkins/gcc.groovy
@@ -1,3 +1,8 @@
+configure = load 'scripts/jenkins/lib/configure.groovy'
+build     = load 'scripts/jenkins/lib/build.groovy'
+post      = load 'scripts/jenkins/lib/post.groovy'
+helper    = load 'scripts/jenkins/lib/helper.groovy'
+
 defaultDockerArgs = '-v /home/jenkins/.ccache:/usr/src/.ccache'
 defaultCMakeOptions = '-DOGS_LIB_BOOST=System -DOGS_LIB_VTK=System'
 
@@ -7,54 +12,24 @@ node('docker') {
 
     docker.image('ogs6/gcc-base:latest').inside(defaultDockerArgs) {
         stage 'Configure (Linux-Docker)'
-        configure 'build', ''
+        configure.linux 'build', ''
 
         stage 'CLI (Linux-Docker)'
-        build 'build', ''
+        build.linux 'build', ''
 
         stage 'Test (Linux-Docker)'
-        build 'build', 'tests ctest'
+        build.linux 'build', 'tests ctest'
 
         if (env.BRANCH_NAME == 'master' || env.BRANCH_NAME.contains('release')) {
             stage 'Release (Linux-Docker)'
-            build 'build', 'package'
+            build.linux 'build', 'package'
         }
     }
 
-    if (env.BRANCH_NAME == 'master' || env.BRANCH_NAME.contains('release')) {
+    if (helper.isRelease())
         archive 'build/*.tar.gz'
-    }
 
     stage 'Post (Linux-Docker)'
-    publishTestReports 'build/Testing/**/*.xml', 'build/Tests/testrunner.xml',
+    helper.publishTestReports 'build/Testing/**/*.xml', 'build/Tests/testrunner.xml',
         'ogs/scripts/jenkins/clang-log-parser.rules'
 }
-
-def configure(buildDir, cmakeOptions) {
-    sh "rm -rf ${buildDir} && mkdir ${buildDir}"
-    sh "cd ${buildDir} && cmake ../ogs ${defaultCMakeOptions} ${cmakeOptions}"
-}
-
-def build(buildDir, target) {
-    sh "cd ${buildDir} && make -j \$(nproc) ${target}"
-}
-
-// *** Helper functions ***
-def publishTestReports(ctestPattern, gtestPattern, parseRulefile) {
-    step([$class: 'XUnitPublisher', testTimeMargin: '3000', thresholdMode: 1,
-        thresholds: [
-            [$class: 'FailedThreshold', failureNewThreshold: '', failureThreshold: '',
-                unstableNewThreshold: '', unstableThreshold: ''],
-            [$class: 'SkippedThreshold', failureNewThreshold: '', failureThreshold: '',
-                unstableNewThreshold: '', unstableThreshold: '']],
-        tools: [
-            [$class: 'CTestType', deleteOutputFiles: true, failIfNotNew: true, pattern:
-                "${ctestPattern}", skipNoTestFiles: false, stopProcessingIfError: true],
-            [$class: 'GoogleTestType', deleteOutputFiles: true, failIfNotNew: true, pattern:
-                "${gtestPattern}", skipNoTestFiles: false, stopProcessingIfError: true]]
-    ])
-
-    step([$class: 'LogParserPublisher', failBuildOnError: true, unstableOnWarning: false,
-        projectRulePath: "${parseRulefile}", useProjectRule: true])
-}
-// *** End helper functions ***
