e9f87050b1d6fbceebba3069e7dcfd9bccb16a6b
Wenqing Wang <wenqing.wang@ufz.de>
2016-09-15 12:53:39 +0200

    [Mat] Added classes for fluid viscosity models

---
+++ MaterialLib/Fluid/Viscosity/LinearPressureDependentViscosity.h
@@ -0,0 +1,76 @@
+/*!
+   \file  LinearPressureDependentViscosity.h
+   \brief Declaration of class for the pressure dependent viscosity model.
+
+   \copyright
+    Copyright (c) 2012-2016, OpenGeoSys Community (http://www.opengeosys.org)
+               Distributed under a Modified BSD License.
+               See accompanying file LICENSE.txt or
+               http://www.opengeosys.org/project/license
+ */
+#ifndef LINEAR_PRESSURE_DEPENDENT_VISCOSITY_H_
+#define LINEAR_PRESSURE_DEPENDENT_VISCOSITY_H_
+
+#include <string>
+
+#include "BaseLib/ConfigTree.h"
+
+#include "MaterialLib/Fluid/FluidProperty.h"
+
+namespace MaterialLib
+{
+namespace Fluid
+{
+/// Linear pressure dependent viscosity model.
+class LinearPressureDependentViscosity final : public FluidProperty
+{
+public:
+    /// \param config  ConfigTree object which contains the input data
+    ///                including <type>linear_pressure</type> and it has
+    ///                a tag of <viscosity>
+    explicit LinearPressureDependentViscosity(BaseLib::ConfigTree const& config)
+        :  //! \ogs_file_param{material__fluid__viscosity__linear_pressure_dependent__mu0}
+    _mu0(config.getConfigParameter<double>("mu0")),
+          //! \ogs_file_param{material__fluid__viscosity__linear_pressure_dependent__p0}
+    _p0(config.getConfigParameter<double>("p0")),
+          //! \ogs_file_param{material__fluid__viscosity__linear_pressure_dependent__gamma}
+    _gamma(config.getConfigParameter<double>("gamma"))
+    {
+    }
+
+    /// Get model name.
+    std::string getName() const override
+    {
+        return "Linear pressure dependent viscosity";
+    }
+
+    /// Get viscosity value.
+    /// \param var_vals Variable values in an array. The order of its elements
+    ///                 is given in enum class PropertyVariable.
+    double getValue(const double var_vals[]) const override
+    {
+        const double p = var_vals[static_cast<int> (PropertyVariable::pl)];
+        return _mu0 * (1 + _gamma * (std::max(p,0.) - _p0));
+    }
+
+    /// Get the partial differential of the viscosity with respect to pressure.
+    /// \param var_vals  Variable values  in an array. The order of its elements
+    ///                  is given in enum class PropertyVariable.
+    /// \param var       Variable type.
+    double getdValue(const double var_vals[],
+            const PropertyVariable var) const override
+    {
+        (void) var_vals;
+        (void) var;
+        return _mu0 * _gamma;
+    }
+
+private:
+    const double _mu0; ///<  Reference viscosity.
+    const double _p0; ///<  Reference pressure.
+    const double _gamma; ///<  Parameter.
+};
+
+}  // end namespace
+}  // end namespace
+#endif
---
+++ MaterialLib/Fluid/Viscosity/TemperatureDependentViscosity.h
@@ -0,0 +1,82 @@
+/**
+ *  \copyright
+ *   Copyright (c) 2012-2016, OpenGeoSys Community (http://www.opengeosys.org)
+ *              Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ * \file:   TemperatureDependentViscosity.h
+ * \author: wenqing
+ *
+ *  Created on August 10, 2016, 10:45 AM
+ */
+
+#ifndef TEMPERATUREDEPENDENTVISCOSITY_H
+#define TEMPERATUREDEPENDENTVISCOSITY_H
+
+#include <string>
+#include <cmath>
+
+#include "BaseLib/ConfigTree.h"
+
+#include "MaterialLib/Fluid/FluidProperty.h"
+
+namespace MaterialLib
+{
+namespace Fluid
+{
+/// Temperature dependent viscosity model,
+class TemperatureDependentViscosity final : public FluidProperty
+{
+public:
+    /// \param config  ConfigTree object which contains the input data
+    ///                including <type>temperature_dependent</type> and it has
+    ///                a tag of <viscosity>
+    explicit TemperatureDependentViscosity(BaseLib::ConfigTree const& config)
+        :  //! \ogs_file_param{material__fluid__viscosity__temperature_dependent__mu0}
+    _mu0(config.getConfigParameter<double>("mu0")),
+          //! \ogs_file_param{material__fluid__viscosity__temperature_dependent__tc}
+    _temperature_c(config.getConfigParameter<double>("tc")),
+          //! \ogs_file_param{material__fluid__viscosity__temperature_dependent__tv}
+    _temperature_v(config.getConfigParameter<double>("tv"))
+    {
+    }
+
+    /// Get model name.
+    std::string getName() const override
+    {
+        return "Temperature dependent viscosity";
+    }
+
+    /// Get viscosity value.
+    /// \param var_vals Variable values in an array. The order of its elements
+    ///                 is given in enum class PropertyVariable.
+    double getValue(const double var_vals[]) const override
+    {
+        const double T = var_vals[static_cast<int> (PropertyVariable::T)];
+        return _mu0 * std::exp(-(T - _temperature_c) / _temperature_v);
+    }
+
+    /// Get the partial differential of the viscosity with respect to
+    /// temperature.
+    /// \param var_vals  Variable values  in an array. The order of its elements
+    ///                   is given in enum class PropertyVariable.
+    /// \param var       Variable type.
+    double getdValue(const double var_vals[],
+            const PropertyVariable var) const override
+    {
+        (void) var;
+        const double T = var_vals[static_cast<int> (PropertyVariable::T)];
+        return -_mu0 * std::exp(-(T - _temperature_c) / _temperature_v);
+    }
+
+private:
+    const double _mu0; ///<  Inital viscosity.
+    const double _temperature_c; ///<  Reference temperature 1.
+    const double _temperature_v; ///<  Reference temperature 2.
+};
+
+}  // end namespace
+}  // end namespace
+
+#endif /* TEMPERATUREDEPENDENTVISCOSITY_H */
