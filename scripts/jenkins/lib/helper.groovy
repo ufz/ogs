04079ef46b2895d2c06358a3de6c6a10c0300a75
Lars Bilke <lars.bilke@ufz.de>
2016-09-15 11:05:03 +0200

    [Jenkins] Fix qt paths on win1.

--- scripts/jenkins/lib/helper.groovy
+++ scripts/jenkins/lib/helper.groovy
@@ -7,8 +7,12 @@ def isRelease () {
 def getEnv(arch = 'x64') {
     if (env.NODE_NAME == 'visserv3')
         qtdir = "C:\\libs\\qt\\4.8\\msvc2013-${arch}"
-    if (env.NODE_NAME == 'win1')
-        qtdir = "C:\\libs\\qt-4.8.7-${arch}-msvc2013\\qt-4.8.7-${arch}-msvc2013"
+    if (env.NODE_NAME == 'win1') {
+        if (arch == 'x32')
+            qtdir = "C:\\libs\\qt-4.8.7-x86-msvc2013\\qt-4.8.7-x86-msvc2013"
+        else
+            qtdir = "C:\\libs\\qt-4.8.7-${arch}-msvc2013\\qt-4.8.7-${arch}-msvc2013"
+    }
 
     return [
         "QTDIR=${qtdir}",
