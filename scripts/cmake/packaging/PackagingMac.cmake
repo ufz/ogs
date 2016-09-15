3b5f55afbf9c0aa7e8d5942c945ad985f3d1b50a
Lars Bilke <lars.bilke@ufz.de>
2016-09-15 10:12:11 +0200

    [Jenkins] Package TGZ on Mac as default.
    Introduced CMAKE option OGS_PACK_DMG to switch to disk image.
    Required OGS_BUILD_GUI=ON.

--- scripts/cmake/packaging/PackagingMac.cmake
+++ scripts/cmake/packaging/PackagingMac.cmake
@@ -1,4 +1,10 @@
-set(CPACK_GENERATOR DragNDrop ZIP)
+option(OGS_PACK_DMG "package targets creates a .dmg disk image instead of .tar.gz" FALSE)
+if(OGS_PACK_DMG)
+    if(NOT OGS_BUILD_GUI)
+        message(WARNING "OGS_PACK_DMG requires OGS_BUILD_GUI=ON!")
+    endif()
+    set(CPACK_GENERATOR DragNDrop)
+endif()
 set(CPACK_DMG_FORMAT "UDBZ")
 
 # See http://stackoverflow.com/a/16662169/80480 how to create the DS_Store file.
