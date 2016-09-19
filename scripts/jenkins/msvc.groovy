4282f5dbba7526173f5e2a68b34d643518e11860
Lars Bilke <lars.bilke@ufz.de>
2016-09-19 11:40:57 +0200

    [Jenkins] Explicitly allocate workspace before unstashing.
    Should fix errors on unstashing, see
    https://groups.google.com/forum/#!topicsearchin/jenkinsci-users/pipeline/jenkinsci-users/xj-yAIPjAFI

--- scripts/jenkins/msvc.groovy
+++ scripts/jenkins/msvc.groovy
@@ -16,34 +16,36 @@ node('win && conan') {
         '-DOGS_BUILD_TESTS=OFF ' +
         '-DOGS_BUILD_SWMM=ON'
 
-    dir('ogs') { unstash 'source' }
+    ws {
+        dir('ogs') { unstash 'source' }
 
-    withEnv(helper.getEnv()) {
-        stage 'Configure (Win)'
-        configure.win 'build', "${defaultCMakeOptions}", 'Ninja',
-            '-u -s build_type=Release -s compiler="Visual Studio" -s compiler.version=12 -s ' +
-                'arch=x86_64'
+        withEnv(helper.getEnv()) {
+            stage 'Configure (Win)'
+            configure.win 'build', "${defaultCMakeOptions}", 'Ninja',
+                '-u -s build_type=Release -s compiler="Visual Studio" -s compiler.version=12 -s ' +
+                    'arch=x86_64'
 
-        stage 'CLI (Win)'
-        build.win 'build'
+            stage 'CLI (Win)'
+            build.win 'build'
 
-        stage 'Test (Win)'
-        build.win 'build', 'tests'
+            stage 'Test (Win)'
+            build.win 'build', 'tests'
 
-        stage 'Data Explorer (Win)'
-        configure.win 'build', "${defaultCMakeOptions} ${guiCMakeOptions}",
-            'Ninja', '-u -s build_type=Release -s compiler="Visual Studio" -s compiler.version=12' +
-            ' -s arch=x86_64',
-            true
-        build.win 'build'
+            stage 'Data Explorer (Win)'
+            configure.win 'build', "${defaultCMakeOptions} ${guiCMakeOptions}", 'Ninja',
+                '-u -s build_type=Release -s compiler="Visual Studio" -s compiler.version=12' +
+                ' -s arch=x86_64',
+                true
+            build.win 'build'
+        }
+
+        if (helper.isRelease()) {
+            stage 'Release (Win)'
+            archive 'build/*.zip'
+        }
+
+        stage 'Post (Win)'
+        post.publishTestReports 'build/Testing/**/*.xml', 'build/Tests/testrunner.xml',
+            'ogs/scripts/jenkins/msvc-log-parser.rules'
     }
-
-    if (helper.isRelease()) {
-        stage 'Release (Win)'
-        archive 'build/*.zip'
-    }
-
-    stage 'Post (Win)'
-    post.publishTestReports 'build/Testing/**/*.xml', 'build/Tests/testrunner.xml',
-        'ogs/scripts/jenkins/msvc-log-parser.rules'
 }
