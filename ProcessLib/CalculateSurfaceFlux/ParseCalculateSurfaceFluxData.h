e6c63dd60bf061cab9b22ab4dbf6291a18988795
Thomas Fischer <thomas.fischer@ufz.de>
2016-09-20 15:21:00 +0200

    [PL] Parse meta data for CalculateSurfaceFlux.

---
+++ ProcessLib/CalculateSurfaceFlux/ParseCalculateSurfaceFluxData.h
@@ -0,0 +1,21 @@
+
+/**
+ * \copyright
+ * Copyright (c) 2012-2016, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#include <string>
+
+#include "BaseLib/ConfigTree.h"
+
+namespace ProcessLib
+{
+void parseCalculateSurfaceFluxData(BaseLib::ConfigTree const& config,
+                                   std::string& mesh_name,
+                                   std::string& property_name,
+                                   std::string& out_fname);
+}  // namespace ProcessLib
