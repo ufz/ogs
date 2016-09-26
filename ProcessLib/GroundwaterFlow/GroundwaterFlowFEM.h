c7714e98dff47acf25e1ecd93aff72523df92154
Yonghui <huangyh56@gmial.com>
2016-09-26 15:19:34 +0200

    fix a bug for initializing the size of secondary variable

--- ProcessLib/GroundwaterFlow/GroundwaterFlowFEM.h
+++ ProcessLib/GroundwaterFlow/GroundwaterFlowFEM.h
@@ -193,9 +193,9 @@ private:
     IntegrationMethod const _integration_method;
     std::vector<ShapeMatrices> _shape_matrices;
 
-    std::vector<std::vector<double>> _darcy_velocities =
-        std::vector<std::vector<double>>(
-            GlobalDim, std::vector<double>(ShapeFunction::NPOINTS));
+    std::vector<std::vector<double>> _darcy_velocities
+        = std::vector<std::vector<double>>(
+            GlobalDim, std::vector<double>(_integration_method.getNumberOfPoints()));
 };
 
 
