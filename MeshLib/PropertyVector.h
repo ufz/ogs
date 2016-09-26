403461c733f1f1fb004344d06ea358e46d788248
Yonghui <huangyh56@gmial.com>
2016-09-26 15:59:18 +0200

    fix a bug in obtaining the value for the given component stored in the given tuple.

--- MeshLib/PropertyVector.h
+++ MeshLib/PropertyVector.h
@@ -72,8 +72,7 @@ public:
     {
         assert(component < _n_components);
         assert(tuple_index < getNumberOfTuples());
-        return this->operator[](tuple_index* getNumberOfComponents() +
-                                component);
+		return this->operator[](tuple_index* _n_components + component);
     }
 
     PropertyVectorBase* clone(std::vector<std::size_t> const& exclude_positions) const
