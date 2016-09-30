02bfc9e0286f890454afc520fd25edc4454d2c91
Dmitri Naumov <dmitri.naumov@ufz.de>
2016-09-30 18:03:42 +0200

    [PL] Update HydroMechanics.

--- ProcessLib/SmallDeformation/CreateSmallDeformationProcess.h
+++ ProcessLib/SmallDeformation/CreateSmallDeformationProcess.h
@@ -23,6 +23,7 @@ std::unique_ptr<Process> createSmallDeformationProcess(
     std::unique_ptr<ProcessLib::AbstractJacobianAssembler>&& jacobian_assembler,
     std::vector<ProcessVariable> const& variables,
     std::vector<std::unique_ptr<ParameterBase>> const& parameters,
+    unsigned const integration_order,
     BaseLib::ConfigTree const& config);
 
 }  // namespace SmallDeformation
