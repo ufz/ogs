38efa4db34578fec643f3a6eef82e973dd359256
Dmitri Naumov <github@naumov.de>
2016-10-05 21:16:10 +0200

    [doc] Fix ogs_file_param for parameter__Group__*.

--- ProcessLib/Parameter/GroupBasedParameter.cpp
+++ ProcessLib/Parameter/GroupBasedParameter.cpp
@@ -22,7 +22,7 @@ std::unique_ptr<ParameterBase> createGroupBasedParameter(
     config.checkConfigParameter("type", "Group");
 
     // get a property vector of group IDs
-    //! \ogs_file_param{parameter_Group__group_id_property}
+    //! \ogs_file_param{parameter__Group__group_id_property}
     std::string const group_id_property_name = config.getConfigParameter<std::string>("group_id_property");
     DBUG("Using group_id_property %s", group_id_property_name.c_str());
 
@@ -36,13 +36,13 @@ std::unique_ptr<ParameterBase> createGroupBasedParameter(
     typedef std::vector<double> Values;
     typedef std::pair<int, Values> Index_Values;
     std::vector<Index_Values> vec_index_values;
-    //! \ogs_file_param{parameter_Group__index_values}
+    //! \ogs_file_param{parameter__Group__index_values}
     for (auto p : config.getConfigSubtreeList("index_values"))
     {
-        //! \ogs_file_param{parameter_Group__index_values__index}
+        //! \ogs_file_param{parameter__Group__index_values__index}
         auto const index = p.getConfigParameter<int>("index");
         {
-            //! \ogs_file_param{parameter_Group__index_values__value}
+            //! \ogs_file_param{parameter__Group__index_values__value}
             auto const value = p.getConfigParameterOptional<double>("value");
 
             if (value)
@@ -54,7 +54,7 @@ std::unique_ptr<ParameterBase> createGroupBasedParameter(
         }
 
         // Value tag not available; continue with required values tag.
-        //! \ogs_file_param{parameter_Group__index_values__values}
+        //! \ogs_file_param{parameter__Group__index_values__values}
         Values const values = p.getConfigParameter<Values>("values");
 
         if (values.empty())
