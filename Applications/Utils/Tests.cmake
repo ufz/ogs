80acbdd69248fce79f9a51175ffac1ed63304f24
Lars Bilke <lars.bilke@ufz.de>
2016-10-12 16:50:57 +0200

    [CMake] Removed time-wrapper from all tests.

--- Applications/Utils/Tests.cmake
+++ Applications/Utils/Tests.cmake
@@ -4,7 +4,6 @@ AddTest(
     PATH MeshGeoToolsLib/Ammer/
     EXECUTABLE MapGeometryToMeshSurface
     EXECUTABLE_ARGS -m Ammer-Homogen100m-Final-TopSurface.vtu -i Ammer-Rivers.gml -o ${CMAKE_BINARY_DIR}/Tests/Data/MeshGeoToolsLib/Ammer/Ammer-Rivers-Mapped.gml
-    WRAPPER time
     TESTER diff
     DIFF_DATA Ammer-Rivers-Mapped.gml
 )
@@ -14,7 +13,6 @@ AddTest(
     PATH MeshGeoToolsLib/Bode/
     EXECUTABLE MapGeometryToMeshSurface
     EXECUTABLE_ARGS -m BodeComplex.msh -i BodeEZG_Fliessgewaesser.gml -o ${CMAKE_BINARY_DIR}/Tests/Data/MeshGeoToolsLib/Bode/BodeEZG_Fliessgewaesser-Mapped.gml
-    WRAPPER time
     TESTER diff
     DIFF_DATA BodeEZG_Fliessgewaesser-Mapped.gml
 )
@@ -24,7 +22,6 @@ AddTest(
     PATH MeshGeoToolsLib/Naegelstedt
     EXECUTABLE MapGeometryToMeshSurface
     EXECUTABLE_ARGS -m SmallTest.vtu -i RiverNetwork.gml -o ${CMAKE_BINARY_DIR}/Tests/Data/MeshGeoToolsLib/Naegelstedt/RiverNetwork-Mapped.gml
-    WRAPPER time
     TESTER diff
     DIFF_DATA RiverNetwork-Mapped.gml
 )
