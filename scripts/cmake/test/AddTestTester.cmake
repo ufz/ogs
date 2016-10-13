eea70e646bf6f5a485a0cb6d71e1ff78e60193fd
Lars Bilke <lars.bilke@ufz.de>
2016-10-13 11:09:55 +0200

    [CMake] Run tester as cmd on Windows.

--- scripts/cmake/test/AddTestTester.cmake
+++ scripts/cmake/test/AddTestTester.cmake
@@ -1,8 +1,18 @@
-execute_process(
-    COMMAND bash -c ${TESTER_COMMAND}
-    WORKING_DIRECTORY ${case_path}
-    RESULT_VARIABLE EXIT_CODE
-)
+if(WIN32)
+    execute_process(
+        COMMAND cmd /C ${TESTER_COMMAND}
+        WORKING_DIRECTORY ${case_path}
+        RESULT_VARIABLE EXIT_CODE
+        OUTPUT_VARIABLE OUTPUT
+  )
+else()
+    execute_process(
+        COMMAND bash -c ${TESTER_COMMAND}
+        WORKING_DIRECTORY ${case_path}
+        RESULT_VARIABLE EXIT_CODE
+        OUTPUT_VARIABLE OUTPUT
+    )
+endif()
 
 if(NOT EXIT_CODE STREQUAL "0")
     message(FATAL_ERROR "Error exit code: ${EXIT_CODE}")
