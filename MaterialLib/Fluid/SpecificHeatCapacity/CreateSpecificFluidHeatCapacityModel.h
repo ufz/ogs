2e9e8ff0e62e5a110ffb0b63549ec82c4f77cc9c
Wenqing Wang <wenqing.wang@ufz.de>
2016-10-17 17:23:25 +0200

    [FL] Added a function to create heat capacity models of fluid

---
+++ MaterialLib/Fluid/SpecificHeatCapacity/CreateSpecificFluidHeatCapacityModel.h
@@ -0,0 +1,40 @@
+/**
+ *  \brief A function for creating a specific heat capacity model for fluid
+ *
+ *  \copyright
+ *   Copyright (c) 2012-2016, OpenGeoSys Community (http://www.opengeosys.org)
+ *              Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ *   \file CreateSpecificFluidHeatCapacityModel.h
+ *
+ */
+
+#ifndef OGS_CREATESPECIFICFLUIDHEATCAPACITYMODEL_H
+#define OGS_CREATESPECIFICFLUIDHEATCAPACITYMODEL_H
+
+#include <memory>
+
+namespace BaseLib
+{
+class ConfigTree;
+}
+
+namespace MaterialLib
+{
+namespace Fluid
+{
+class FluidProperty;
+
+/**
+ *  Create a specific heat capacity model
+ *  \param config  ConfigTree object has a tag of `<specific_heat_capacity>`
+ */
+std::unique_ptr<FluidProperty> createSpecificFluidHeatCapacityModel(
+    BaseLib::ConfigTree const& config);
+
+}  // end namespace
+}  // end namespace
+
+#endif /* OGS_CREATESPECIFICFLUIDHEATCAPACITY_H */
