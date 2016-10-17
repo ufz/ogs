58bb883bf2381f406788966bb36156035346fc34
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2016-10-17 14:35:14 +0200

    [App/CLI] add a SD-LIE test for an internal fracture

--- Applications/CLI/Tests.cmake
+++ Applications/CLI/Tests.cmake
@@ -561,6 +561,19 @@ if(NOT OGS_USE_MPI)
         single_joint_expected_pcs_0_ts_1_t_1.000000.vtu single_joint_pcs_0_ts_1_t_1.000000.vtu displacement_jump1 displacement_jump1
     )
 
+    AddTest(
+        NAME LIE_M_single_joint_inside
+        PATH LIE/Mechanics
+        EXECUTABLE ogs
+        EXECUTABLE_ARGS single_joint_inside.prj
+        WRAPPER time
+        TESTER vtkdiff
+        ABSTOL 1e-16 RELTOL 1e-16
+        DIFF_DATA
+        single_joint_inside_expected_pcs_0_ts_1_t_1.000000.vtu single_joint_inside_pcs_0_ts_1_t_1.000000.vtu displacement displacement
+        single_joint_inside_expected_pcs_0_ts_1_t_1.000000.vtu single_joint_inside_pcs_0_ts_1_t_1.000000.vtu displacement_jump1 displacement_jump1
+    )
+
 else()
     # MPI groundwater flow tests
     AddTest(
