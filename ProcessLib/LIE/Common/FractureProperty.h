cc4c19ddab4b58f1992e31639d43ff35739cc6b0
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2016-11-02 07:36:02 +0100

    [PCS/LIE] add hydraulic properties in FractureProperty

--- ProcessLib/LIE/Common/FractureProperty.h
+++ ProcessLib/LIE/Common/FractureProperty.h
@@ -35,6 +35,8 @@ struct FractureProperty
     Eigen::MatrixXd R;
     /// Initial aperture
     ProcessLib::Parameter<double> const* aperture0 = nullptr;
+    ProcessLib::Parameter<double> const* specific_storage = nullptr;
+    ProcessLib::Parameter<double> const* biot_coefficient = nullptr;
 };
 
 /// configure fracture property based on a fracture element assuming
