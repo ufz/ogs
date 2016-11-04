1e33c49274ab48252f3c74a8febb9b2a3a483242
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2016-11-04 09:45:56 +0100

    [CMake] set the default OGS_LOG_LEVEL to debug also in release build

--- scripts/cmake/ProjectSetup.cmake
+++ scripts/cmake/ProjectSetup.cmake
@@ -9,21 +9,6 @@ endif()
 set(EXECUTABLE_OUTPUT_PATH ${PROJECT_BINARY_DIR}/bin)
 set(LIBRARY_OUTPUT_PATH ${PROJECT_BINARY_DIR}/lib)
 
-# Logging level
-if(OGS_DISABLE_LOGGING)
-    set(OGS_LOG_LEVEL LOGOG_LEVEL_NONE)
-endif()
-
-if(NOT DEFINED OGS_LOG_LEVEL)
-    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-        add_definitions(-DLOGOG_LEVEL=LOGOG_LEVEL_DEBUG)
-    else()
-        add_definitions(-DLOGOG_LEVEL=LOGOG_LEVEL_INFO)
-    endif() # CMAKE_BUILD_TYPE = Debug
-else()
-    add_definitions(-DLOGOG_LEVEL=${OGS_LOG_LEVEL})
-endif() # NOT DEFINED OGS_LOG_LEVEL
-
 # Enable Visual Studio project folder grouping
 set_property(GLOBAL PROPERTY USE_FOLDERS ON)
 
