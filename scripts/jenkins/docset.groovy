9c9da0655be44458d2385245b3b04b5371fa7734
Lars Bilke <lars.bilke@ufz.de>
2016-11-07 08:51:15 +0100

    [Jenkins] Added docset generation job.

---
+++ scripts/jenkins/docset.groovy
@@ -0,0 +1,15 @@
+def post = new ogs.post()
+
+stage('Docset') {
+    sh("""
+    rm -rf build; mkdir build; cd build
+    cmake -DDOCS_GENERATE_DOCSET=ON ../ogs
+    cmake --build . --config Release --target doc
+
+    cd docs
+    tar --exclude='.DS_Store' -cvzf ogs6.tgz ogs6.docset
+    """.stripIndend())
+    archiveArtifacts 'build/docs/ogs6.tgz, build/docs/ogs6.xml'
+
+    post.cleanup()
+}
