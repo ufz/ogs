382a9af5b7c95601e087f42ecc675d70382572cb
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2016-11-08 12:45:15 +0100

    [Math/Eigen] reactivate EigenMatrix::write()

--- MathLib/LinAlg/Eigen/EigenMatrix.h
+++ MathLib/LinAlg/Eigen/EigenMatrix.h
@@ -156,13 +156,10 @@ public:
     /// printout this matrix for debugging
     void write(std::ostream &os) const
     {
-        (void)os;
-#if 0
         for (int k=0; k<_mat.outerSize(); ++k)
-          for (Eigen::SparseMatrix<double>::InnerIterator it(_mat,k); it; ++it)
+          for (RawMatrixType::InnerIterator it(_mat,k); it; ++it)
               os << it.row() << " " << it.col() << ": " << it.value() << "\n";
         os << std::endl;
-#endif
     }
 #endif
 
