246731ae0ffdfcff83b0c36c2491700a8cbf2cfb
Lars Bilke <lars.bilke@ufz.de>
2016-11-10 09:22:33 +0100

    [CMake] Fixed installation of shared libraries / DLLs.
    INSTALL_COMPONENT was not defined and therefore the shared libraries
    were not installed.

--- scripts/cmake/packaging/InstallDependencies.cmake
+++ scripts/cmake/packaging/InstallDependencies.cmake
@@ -1,4 +1,4 @@
 # Registers a target for installing its dependencies (dll / so files)
-macro(InstallDependencies TARGET INSTALL_COMPONENT)
+macro(InstallDependencies TARGET)
     set(INSTALL_DEPENDENCIES "${INSTALL_DEPENDENCIES};${TARGET}" CACHE INTERNAL "")
 endmacro()
