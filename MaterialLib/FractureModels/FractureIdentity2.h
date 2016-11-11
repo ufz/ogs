32f7c3e171c8dd3f513fa0e4df611a1de44500ef
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2016-11-11 12:48:03 +0100

    add a comment to FractureIdentity2

--- MaterialLib/FractureModels/FractureIdentity2.h
+++ MaterialLib/FractureModels/FractureIdentity2.h
@@ -16,7 +16,9 @@ namespace MaterialLib
 {
 namespace Fracture
 {
-
+/// Identity vector for extracting non-shear components of a fracture stress
+/// vector and a relative displacement vector, e.g. \f$\mathbf{m}=(0,1)^T\f$ in
+/// 2D case.
 template <int DisplacementDim>
 struct FractureIdentity2
 {
