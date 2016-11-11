eb92af084c88a9f2a345727f6f7879ce8d6600f6
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2016-11-11 12:12:48 +0100

    [PCS/LIE/HM] add local assemblers

---
+++ ProcessLib/LIE/HydroMechanics/LocalAssembler/HydroMechanicsLocalAssemblerFracture.h
@@ -0,0 +1,122 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2016, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#ifndef PROCESSLIB_LIE_HYDROMECHANICS_HYDROMECHANICSLOCALASSEMBLER_FRACTURE_H_
+#define PROCESSLIB_LIE_HYDROMECHANICS_HYDROMECHANICSLOCALASSEMBLER_FRACTURE_H_
+
+#include <vector>
+
+#include "NumLib/Fem/ShapeMatrixPolicy.h"
+
+#include "ProcessLib/LIE/Common/HMatrixUtils.h"
+#include "ProcessLib/LIE/HydroMechanics/HydroMechanicsProcessData.h"
+
+#include "IntegrationPointDataFracture.h"
+#include "HydroMechanicsLocalAssemblerInterface.h"
+
+namespace ProcessLib
+{
+namespace LIE
+{
+namespace HydroMechanics
+{
+
+template <typename ShapeFunctionDisplacement, typename ShapeFunctionPressure,
+          typename IntegrationMethod, unsigned GlobalDim>
+class HydroMechanicsLocalAssemblerFracture
+    : public HydroMechanicsLocalAssemblerInterface
+{
+public:
+    HydroMechanicsLocalAssemblerFracture(HydroMechanicsLocalAssemblerFracture const&) =
+        delete;
+    HydroMechanicsLocalAssemblerFracture(HydroMechanicsLocalAssemblerFracture&&) = delete;
+
+    HydroMechanicsLocalAssemblerFracture(
+        MeshLib::Element const& e,
+        std::size_t const local_matrix_size,
+        std::vector<unsigned> const& dofIndex_to_localIndex,
+        bool const is_axially_symmetric,
+        unsigned const integration_order,
+        HydroMechanicsProcessData<GlobalDim>& process_data);
+
+    void preTimestepConcrete(std::vector<double> const& /*local_x*/,
+                             double const /*t*/,
+                             double const /*delta_t*/) override
+    {
+        for (auto &data : _ip_data)
+            data.pushBackState();
+    }
+
+    void postTimestepConcrete(std::vector<double> const& local_x) override;
+
+
+    Eigen::Map<const Eigen::RowVectorXd> getShapeMatrix(
+        const unsigned integration_point) const override
+    {
+        auto const& N = _ip_data[integration_point].N_p;
+
+        // assumes N is stored contiguously in memory
+        return Eigen::Map<const Eigen::RowVectorXd>(N.data(), N.size());
+    }
+
+private:
+    void assembleWithJacobianConcrete(
+        double const t,
+        Eigen::VectorXd const& local_x,
+        Eigen::VectorXd const& local_xdot,
+        Eigen::VectorXd& local_b,
+        Eigen::MatrixXd& local_J) override;
+
+    void assembleBlockMatricesWithJacobian(
+        double const t,
+        Eigen::Ref<const Eigen::VectorXd> const& p,
+        Eigen::Ref<const Eigen::VectorXd> const& p_dot,
+        Eigen::Ref<const Eigen::VectorXd> const& g,
+        Eigen::Ref<const Eigen::VectorXd> const& g_dot,
+        Eigen::Ref<Eigen::VectorXd> rhs_p,
+        Eigen::Ref<Eigen::VectorXd> rhs_g,
+        Eigen::Ref<Eigen::MatrixXd> J_pp,
+        Eigen::Ref<Eigen::MatrixXd> J_pg,
+        Eigen::Ref<Eigen::MatrixXd> J_gg,
+        Eigen::Ref<Eigen::MatrixXd> J_gp);
+
+    // Types for displacement.
+    using ShapeMatricesTypeDisplacement =
+        ShapeMatrixPolicyType<ShapeFunctionDisplacement, GlobalDim>;
+    using HMatricesType = HMatrixPolicyType<ShapeFunctionDisplacement, GlobalDim>;
+    using HMatrixType = typename HMatricesType::HMatrixType;
+
+    // Types for pressure.
+    using ShapeMatricesTypePressure =
+        ShapeMatrixPolicyType<ShapeFunctionPressure, GlobalDim>;
+
+    // Types for the integration point data
+    using IntegrationPointDataType =
+        IntegrationPointDataFracture<HMatricesType,
+                                     ShapeMatricesTypeDisplacement,
+                                     ShapeMatricesTypePressure, GlobalDim>;
+
+    HydroMechanicsProcessData<GlobalDim>& _process_data;
+
+    std::vector<IntegrationPointDataType> _ip_data;
+
+    static const int pressure_index = 0;
+    static const int pressure_size = ShapeFunctionPressure::NPOINTS;
+    static const int displacement_index = ShapeFunctionPressure::NPOINTS;
+    static const int displacement_size =
+        ShapeFunctionDisplacement::NPOINTS * GlobalDim;
+};
+
+}  // namespace HydroMechanics
+}  // namespace LIE
+}  // namespace ProcessLib
+
+#include "HydroMechanicsLocalAssemblerFracture-impl.h"
+
+#endif  // PROCESSLIB_LIE_HYDROMECHANICS_HYDROMECHANICSLOCALASSEMBLER_FRACTURE_H_
---
+++ ProcessLib/LIE/HydroMechanics/LocalAssembler/HydroMechanicsLocalAssemblerMatrixNearFracture-impl.h
@@ -0,0 +1,124 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2016, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#ifndef PROCESSLIB_LIE_HYDROMECHANICS_HYDROMECHANICSLOCALASSEMBLER_MATRIX_NEAR_FRACTUER_IMPL_H_
+#define PROCESSLIB_LIE_HYDROMECHANICS_HYDROMECHANICSLOCALASSEMBLER_MATRIX_NEAR_FRACTUER_IMPL_H_
+
+#include "HydroMechanicsLocalAssemblerMatrixNearFracture.h"
+
+namespace ProcessLib
+{
+namespace LIE
+{
+namespace HydroMechanics
+{
+template <typename ShapeFunctionDisplacement, typename ShapeFunctionPressure,
+          typename IntegrationMethod, unsigned GlobalDim>
+HydroMechanicsLocalAssemblerMatrixNearFracture<
+    ShapeFunctionDisplacement, ShapeFunctionPressure, IntegrationMethod,
+    GlobalDim>::
+    HydroMechanicsLocalAssemblerMatrixNearFracture(
+        MeshLib::Element const& e,
+        std::size_t const n_variables,
+        std::size_t const local_matrix_size,
+        std::vector<unsigned> const& dofIndex_to_localIndex,
+        bool const is_axially_symmetric,
+        unsigned const integration_order,
+        HydroMechanicsProcessData<GlobalDim>& process_data)
+    : HydroMechanicsLocalAssemblerMatrix<ShapeFunctionDisplacement,
+                                         ShapeFunctionPressure,
+                                         IntegrationMethod, GlobalDim>(
+          e, n_variables, local_matrix_size, dofIndex_to_localIndex,
+          is_axially_symmetric, integration_order, process_data)
+{
+}
+
+
+template <typename ShapeFunctionDisplacement, typename ShapeFunctionPressure,
+          typename IntegrationMethod, unsigned GlobalDim>
+void
+HydroMechanicsLocalAssemblerMatrixNearFracture<
+    ShapeFunctionDisplacement, ShapeFunctionPressure, IntegrationMethod,
+    GlobalDim>::
+assembleWithJacobianConcrete(
+    double const t,
+    Eigen::VectorXd const& local_x,
+    Eigen::VectorXd const& local_x_dot,
+    Eigen::VectorXd& local_b,
+    Eigen::MatrixXd& local_J)
+{
+    auto const p = local_x.segment(pressure_index, pressure_size);
+    auto const p_dot = local_x_dot.segment(pressure_index, pressure_size);
+    auto const u = local_x.segment(displacement_index, displacement_size);
+    auto const u_dot =
+        local_x_dot.segment(displacement_index, displacement_size);
+
+    auto rhs_p = local_b.segment(pressure_index, pressure_size);
+    auto rhs_u = local_b.segment(displacement_index, displacement_size);
+
+    auto J_pp = local_J.block(pressure_index, pressure_index, pressure_size,
+                              pressure_size);
+    auto J_pu = local_J.block(pressure_index, displacement_index, pressure_size,
+                              displacement_size);
+    auto J_up = local_J.block(displacement_index, pressure_index,
+                              displacement_size, pressure_size);
+    auto J_uu = local_J.block(displacement_index, displacement_index,
+                              displacement_size, displacement_size);
+
+    // levelset value of the element
+    // remark: this assumes the levelset function is uniform within an element
+    auto const& fracture_props = *_process_data.fracture_property;
+    double const ele_levelset = calculateLevelSetFunction(
+        fracture_props, _element.getCenterOfGravity().getCoords());
+
+    if (ele_levelset == 0)
+    {
+        // no DoF exists for displacement jumps. do the normal assebmly
+        Base::assembleBlockMatricesWithJacobian(t, p, p_dot, u, u_dot, rhs_p, rhs_u, J_pp, J_pu, J_uu, J_up);
+        return;
+    }
+
+    // Displacement jumps should be taken into account
+
+    // compute true displacements
+    auto const g = local_x.segment(displacement_jump_index, displacement_size);
+    auto const g_dot = local_x_dot.segment(displacement_jump_index, displacement_size);
+    Eigen::VectorXd const total_u = u + ele_levelset * g;
+    Eigen::VectorXd const total_u_dot = u_dot + ele_levelset * g_dot;
+
+    // evaluate residuals and Jacobians for pressure and displacements
+    Base::assembleBlockMatricesWithJacobian(t, p, p_dot, total_u, total_u_dot, rhs_p, rhs_u, J_pp, J_pu, J_uu, J_up);
+
+    // compute residuals and Jacobians for displacement jumps
+    auto rhs_g = local_b.segment(displacement_jump_index, displacement_size);
+    auto J_pg = local_J.block(pressure_index, displacement_jump_index,
+                              pressure_size, displacement_size);
+    auto J_ug = local_J.block(displacement_index, displacement_jump_index,
+                              displacement_size, displacement_size);
+    auto J_gp = local_J.block(displacement_jump_index, pressure_index,
+                              displacement_size, pressure_size);
+    auto J_gu = local_J.block(displacement_jump_index, displacement_index,
+                              displacement_size, displacement_size);
+    auto J_gg = local_J.block(displacement_jump_index, displacement_jump_index,
+                              displacement_size, displacement_size);
+
+    rhs_g = ele_levelset * rhs_u;
+    J_pg = ele_levelset * J_pu;
+    J_ug = ele_levelset * J_uu;
+    J_gp = ele_levelset * J_up;
+    J_gu = ele_levelset * J_uu;
+    J_gg = ele_levelset * ele_levelset * J_uu;
+}
+
+
+}  // namespace HydroMechanics
+}  // namespace LIE
+}  // namespace ProcessLib
+
+#endif // PROCESSLIB_LIE_HYDROMECHANICS_HYDROMECHANICSLOCALASSEMBLER_MATRIX_NEAR_FRACTUER_IMPL_H_
