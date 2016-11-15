46480ac2070c13a9bcae61f12350d3583829dd63
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2016-11-15 14:13:45 +0100

    [Num/DOF] fix computeSparsityPatternNonPETSc() for multiple-component, mixed-order cases

--- NumLib/DOF/ComputeSparsityPattern.cpp
+++ NumLib/DOF/ComputeSparsityPattern.cpp
@@ -53,19 +53,11 @@ GlobalSparsityPattern computeSparsityPatternNonPETSc(
     // Map adjacent mesh nodes to "adjacent global indices".
     for (std::size_t n = 0; n < mesh.getNumberOfNodes(); ++n)
     {
-        auto const& node_ids = node_adjacency_table.getAdjacentNodes(n);
-        for (auto an : node_ids)
-        {
-            auto const& row_ids = global_idcs[an];
-            auto const num_components = row_ids.size();
-            for (auto r : row_ids)
-            {
-                // Each component leads to an entry in the row.
-                // For the sparsity pattern only the number of entries are
-                // needed.
-                sparsity_pattern[r] += num_components;
-            }
-        }
+        unsigned n_connected_dof = 0;
+        for (auto an : node_adjacency_table.getAdjacentNodes(n))
+            n_connected_dof += global_idcs[an].size();
+        for (auto global_index : global_idcs[n])
+            sparsity_pattern[global_index] = n_connected_dof;
     }
 
     return sparsity_pattern;
