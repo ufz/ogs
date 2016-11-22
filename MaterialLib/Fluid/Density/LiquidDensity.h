156f715314c10ae024ffba72b964753db044034e
Wenqing Wang <wenqing.wang@ufz.de>
2016-11-22 16:42:41 +0100

    [DOCFL] Fixed web link, which was broken by clang-format

--- MaterialLib/Fluid/Density/LiquidDensity.h
+++ MaterialLib/Fluid/Density/LiquidDensity.h
@@ -31,8 +31,7 @@ namespace Fluid
  *  or pressure.
  *
  *  The formula is given on
- *  <a href="engineeringtoolbox">
- * http://www.engineeringtoolbox.com/fluid-density-temperature-pressure-d_309.html</a>
+ *  <a href="engineeringtoolbox"> http://www.engineeringtoolbox.com/fluid-density-temperature-pressure-d_309.html</a>
  *   which reads
  *   \f[
  *        \rho_l = \rho_0/(1+\beta(T-T_0))/(1-(p-p_0)/E)
