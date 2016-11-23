4375315b77404949ea65f495a194e190f6b6c0a8
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2016-11-23 08:42:24 +0100

    [App/CLI] add ctest LIE_HM_TaskB

--- Applications/CLI/Tests.cmake
+++ Applications/CLI/Tests.cmake
@@ -702,6 +702,20 @@ if(NOT OGS_USE_MPI)
         expected_single_fracture_pcs_0_ts_10_t_100.000000.vtu single_fracture_pcs_0_ts_10_t_100.000000.vtu displacement_jump1 displacement_jump1
     )
 
+    AddTest(
+        NAME LIE_HM_TaskB
+        PATH LIE/HydroMechanics
+        EXECUTABLE ogs
+        EXECUTABLE_ARGS TaskB.prj
+        WRAPPER time
+        TESTER vtkdiff
+        ABSTOL 1e-12 RELTOL 1e-12
+        DIFF_DATA
+        expected_TaskB_pcs_0_ts_4_t_18.000000.vtu TaskB_pcs_0_ts_4_t_18.000000.vtu pressure pressure
+        expected_TaskB_pcs_0_ts_4_t_18.000000.vtu TaskB_pcs_0_ts_4_t_18.000000.vtu displacement displacement
+        expected_TaskB_pcs_0_ts_4_t_18.000000.vtu TaskB_pcs_0_ts_4_t_18.000000.vtu displacement_jump1 displacement_jump1
+    )
+
     # Liquid flow
     AddTest(
         NAME LiquidFlow_LineDirichletNeumannBC
