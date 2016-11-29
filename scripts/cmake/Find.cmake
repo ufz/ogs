907ad64056eb9f02a350b0484b2a5c3355c0df01
Lars Bilke <lars.bilke@ufz.de>
2016-11-29 22:10:02 +0100

    [CMake] Find chocolatey installed cmder git as well.

--- scripts/cmake/Find.cmake
+++ scripts/cmake/Find.cmake
@@ -21,6 +21,14 @@ find_package(cppcheck QUIET)
 
 find_package(PythonInterp QUIET)
 
+# Check for cmder git installed via chocolatey
+find_program(GIT_EXECUTABLE
+  NAMES git
+  PATHS C:/tools/cmder/vendor/git-for-windows
+  PATH_SUFFIXES cmd bin
+  DOC "Git command line client"
+)
+
 find_package(Git REQUIRED)
 string(REPLACE "mingw64/" "" GIT_EXECUTABLE ${GIT_EXECUTABLE}) # Windows git submodule fix
 set(GIT_TOOL_PATH ${GIT_EXECUTABLE} CACHE FILEPATH "The git command line interface" FORCE)
