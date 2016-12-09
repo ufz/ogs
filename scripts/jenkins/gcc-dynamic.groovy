2e92d855019a323c0dc591f622973c9339eb92a8
Lars Bilke <lars.bilke@ufz.de>
2016-12-09 13:13:34 +0100

    [Jenkins] Run linux dynamic job on envinf1.

--- scripts/jenkins/gcc-dynamic.groovy
+++ scripts/jenkins/gcc-dynamic.groovy
@@ -1,32 +1,28 @@
-def defaultDockerArgs = '-v /home/jenkins/.ccache:/usr/src/.ccache'
 def defaultCMakeOptions =
     '-DCMAKE_BUILD_TYPE=Release ' +
     '-DOGS_LIB_BOOST=System ' +
     '-DOGS_LIB_VTK=System ' +
-    '-DBUILD_SHARED_LIBS=ON'
+    '-DBUILD_SHARED_LIBS=ON ' +
+    '-DOGS_BUILD_UTILS=ON'
 
 def configure = new ogs.configure()
 def build = new ogs.build()
 def post = new ogs.post()
 def helper = new ogs.helper()
 
-def image = docker.image('ogs6/gcc-gui:latest')
-image.pull()
-image.inside(defaultDockerArgs) {
-    stage('Configure (Linux-Docker-Dynamic)') {
-        configure.linux 'build', "${defaultCMakeOptions}"
-    }
-
-    stage('CLI (Linux-Docker-Dynamic)') {
-        build.linux this, 'build'
-    }
-
-    stage('Test (Linux-Docker-Dynamic)') {
-        build.linux this, 'build', 'tests ctest'
-    }
+stage('Configure (Linux-Docker-Dynamic)') {
+    configure.linuxWithEnv('envinf1/cli.sh', 'build', "${defaultCMakeOptions}")
 }
 
-stage('Post (Linux-Docker)') {
+stage('CLI (Linux-Docker-Dynamic)') {
+    build.linuxWithEnv(this, 'envinf1/cli.sh', 'build')
+}
+
+stage('Test (Linux-Docker-Dynamic)') {
+    build.linuxWithEnv(this, 'envinf1/cli.sh', 'build', 'tests ctest')
+}
+
+stage('Post (Linux-Docker-Dynamic)') {
     post.publishTestReports 'build/Testing/**/*.xml', 'build/Tests/testrunner.xml',
         'ogs/scripts/jenkins/clang-log-parser.rules'
     post.cleanup()
