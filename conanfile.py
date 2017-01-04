09a135c1649047f7c665085fbaa2d78586e41dcf
Lars Bilke <lars.bilke@ufz.de>
2017-01-04 21:26:55 +0100

    [Conan] Copy shared libraries to bin folder.

--- conanfile.py
+++ conanfile.py
@@ -18,6 +18,11 @@ class OpenGeoSysConan(ConanFile):
         "Boost:header_only=True", \
         "Qt:xmlpatterns=True"
 
+    def imports(self):
+        self.copy(pattern="*.dll", dst="bin", src="bin")
+        self.copy(pattern="*.dylib*", dst="bin", src="lib")
+        self.copy(pattern="*.framework*", dst="bin", src="lib")
+
     def build(self):
         cmake = CMake(self.settings)
         self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
