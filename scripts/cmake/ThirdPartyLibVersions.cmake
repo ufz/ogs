31fabbd1f03faced8f05d1e0859d2e688a8e0625
Thomas Fischer <thomas.fischer@ufz.de>
2017-01-04 12:04:25 +0100

    Raise the VTK version number from 6.3.0 to 7.0.0.

--- scripts/cmake/ThirdPartyLibVersions.cmake
+++ scripts/cmake/ThirdPartyLibVersions.cmake
@@ -6,9 +6,9 @@ set(OGS_BOOST_MD5 "a744cf167b05d72335f27c88115f211d")
 set(OGS_EIGEN_URL "http://opengeosys.s3.amazonaws.com/ogs6-lib-sources/eigen-3.2.8.tar.gz")
 set(OGS_EIGEN_MD5 "135d8d43aaee5fb54cf5f3e981b1a6db")
 
-set(OGS_VTK_VERSION 6.3.0)
-set(OGS_VTK_URL "http://www.vtk.org/files/release/6.3/VTK-6.3.0.tar.gz")
-set(OGS_VTK_MD5 "0231ca4840408e9dd60af48b314c5b6d")
+set(OGS_VTK_VERSION 7.0.0)
+set(OGS_VTK_URL "http://www.vtk.org/files/release/7.0/VTK-7.0.0.tar.gz")
+set(OGS_VTK_MD5 "5fe35312db5fb2341139b8e4955c367d")
 
 set(OGS_TIFF_URL "https://github.com/ufz/tiff/archive/4.0.1.zip")
 set(OGS_TIFF_MD5 "8d5c18654bda9c731d8a6e2dc958751b")
