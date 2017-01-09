333e1d60c60f4125109a9eb47c8d164a74115492
Wenqing Wang <wenqing.wang@ufz.de>
2017-01-09 17:11:49 +0100

    [FL] Updated the copyright

--- MaterialLib/Fluid/ThermalConductivity/CreateFluidThermalConductivityModel.cpp
+++ MaterialLib/Fluid/ThermalConductivity/CreateFluidThermalConductivityModel.cpp
@@ -2,7 +2,7 @@
  *  \brief A function for creating a thermal conductivity model for fluid
  *
  *  \copyright
- *   Copyright (c) 2012-2016, OpenGeoSys Community (http://www.opengeosys.org)
+ *   Copyright (c) 2012-2017, OpenGeoSys Community (http://www.opengeosys.org)
  *              Distributed under a Modified BSD License.
  *              See accompanying file LICENSE.txt or
  *              http://www.opengeosys.org/project/license
