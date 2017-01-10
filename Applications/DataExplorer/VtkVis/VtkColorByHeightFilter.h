d6c7c0e858d803659581cb08a2854296c049ab67
Thomas Fischer <thomas.fischer@ufz.de>
2017-01-10 15:00:27 +0100

    [A/DE/VtkVis] Fix warnings for GetMTime().

--- Applications/DataExplorer/VtkVis/VtkColorByHeightFilter.h
+++ Applications/DataExplorer/VtkVis/VtkColorByHeightFilter.h
@@ -47,7 +47,7 @@ public:
     vtkGetObjectMacro(ColorLookupTable,VtkColorLookupTable);
 
     /// @brief This filter gets updated when the color look-up table was modified.
-    virtual unsigned long GetMTime();
+    virtual unsigned long GetMTime() override;
 
     /// @brief Sets user properties.
     void SetUserProperty(QString name, QVariant value) override
