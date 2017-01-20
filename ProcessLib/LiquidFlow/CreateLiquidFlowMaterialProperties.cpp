7ceb00b3bcd0a3c22b3869802196edcee4258931
Wenqing Wang <wenqing.wang@ufz.de>
2017-01-20 16:36:07 +0100

    [Doc] Moved the four parameter documentations to the right places

--- ProcessLib/LiquidFlow/CreateLiquidFlowMaterialProperties.cpp
+++ ProcessLib/LiquidFlow/CreateLiquidFlowMaterialProperties.cpp
@@ -53,33 +53,33 @@ createLiquidFlowMaterialProperties(
         storage_models;
 
     std::vector<int> mat_ids;
-    //! \ogs_file_param{prj__processes__process__LIQUID_FLOW__material_property__porous_medium}
     auto const& porous_medium_configs =
+        //! \ogs_file_param{prj__processes__process__LIQUID_FLOW__material_property__porous_medium}
         config.getConfigSubtree("porous_medium");
-    //! \ogs_file_param{prj__processes__process__LIQUID_FLOW__material_property__porous_medium__porous_medium}
     for (auto const& porous_medium_config :
+         //! \ogs_file_param{prj__processes__process__LIQUID_FLOW__material_property__porous_medium__porous_medium}
          porous_medium_configs.getConfigSubtreeList("porous_medium"))
     {
         //! \ogs_file_attr{prj__processes__process__LIQUID_FLOW__material_property__porous_medium__porous_medium__id}
         auto const id = porous_medium_config.getConfigAttribute<int>("id");
         mat_ids.push_back(id);
 
-        //! \ogs_file_param{prj__processes__process__LIQUID_FLOW__material_property__porous_medium__porous_medium__permeability}
         auto const& permeability_config =
+            //! \ogs_file_param{prj__processes__process__LIQUID_FLOW__material_property__porous_medium__porous_medium__permeability}
             porous_medium_config.getConfigSubtree("permeability");
         intrinsic_permeability_models.emplace_back(
             MaterialLib::PorousMedium::createPermeabilityModel(
                 permeability_config));
 
-        //! \ogs_file_param{prj__processes__process__LIQUID_FLOW__material_property__porous_medium__porous_medium__porosity}
         auto const& porosity_config =
+            //! \ogs_file_param{prj__processes__process__LIQUID_FLOW__material_property__porous_medium__porous_medium__porosity}
             porous_medium_config.getConfigSubtree("porosity");
         auto n =
             MaterialLib::PorousMedium::createPorosityModel(porosity_config);
         porosity_models.emplace_back(std::move(n));
 
-        //! \ogs_file_param{prj__processes__process__LIQUID_FLOW__material_property__porous_medium__porous_medium__storage}
         auto const& storage_config =
+            //! \ogs_file_param{prj__processes__process__LIQUID_FLOW__material_property__porous_medium__porous_medium__storage}
             porous_medium_config.getConfigSubtree("storage");
         auto beta =
             MaterialLib::PorousMedium::createStorageModel(storage_config);
