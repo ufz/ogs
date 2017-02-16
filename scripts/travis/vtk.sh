2b5db4b47a657ab25a6394bb96ae6cb0b0d5438b
Lars Bilke <lars.bilke@ufz.de>
2017-02-16 12:58:13 +0100

    [Travis] Moved VTK binaries download to Azure storage.

--- scripts/travis/vtk.sh
+++ scripts/travis/vtk.sh
@@ -2,7 +2,7 @@
 set -e
 if [ ! -d "$HOME/VTK-Install/include" ]; then
     cd $HOME
-    wget http://opengeosys.s3.amazonaws.com/ogs6-lib-sources/vtk-6.3.0.tar.gz
+    wget http://ogsstorage.blob.core.windows.net/jenkins/ogs6-lib-sources/vtk-6.3.0-binary.tar.gz
     tar -xzf vtk-6.3.0.tar.gz
 else
   echo 'Using cached vtk directory.';
