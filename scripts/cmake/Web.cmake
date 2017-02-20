b7349664a2e1b70b71f151e3345692f15c4e0d4b
Lars Bilke <lars.bilke@ufz.de>
2017-02-20 14:54:01 +0100

    [CMake] Contentful import is skipped on web-target when not all requirements were met.

--- scripts/cmake/Web.cmake
+++ scripts/cmake/Web.cmake
@@ -1,4 +1,4 @@
-if(NOT NPM OR NOT PIP OR NOT PYTHON_EXECUTABLE)
+if(NOT NPM)
     return()
 endif()
 
@@ -10,16 +10,25 @@ endif()
 
 add_custom_target(web-install
     COMMAND ${PACKAGE_MANAGER}
-    COMMAND ${PIP} install -r requirements.txt
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/web
     BYPRODUCTS ${CMAKE_SOURCE_DIR}/web/node_modules
 )
 
-add_custom_target(web-import
-    COMMAND ${PYTHON_EXECUTABLE} import.py
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/web/import
-    DEPENDS web-install
-)
+if(PIP AND PYTHON_EXECUTABLE AND
+    (EXISTS ${CMAKE_SOURCE_DIR}/web/import/secret.py OR
+     DEFINED ENV{CONTENTFUL_ACCESS_TOKEN}))
+
+    add_custom_target(web-import
+        COMMAND ${PIP} install -r ../requirements.txt
+        COMMAND ${PYTHON_EXECUTABLE} import.py
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/web/import
+        DEPENDS web-install
+    )
+
+    set(IMPORT_TARGET web-import)
+else()
+    message(STATUS "[web] Skipping import from Contentful!")
+endif()
 
 if(DEFINED OGS_WEB_BASE_URL)
     set(HUGO_ARGS --baseURL ${OGS_WEB_BASE_URL})
@@ -32,7 +41,7 @@ endif()
 add_custom_target(web
     COMMAND ${NPM} run build -- ${HUGO_ARGS}
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/web
-    DEPENDS web-install web-import
+    DEPENDS web-install ${IMPORT_TARGET}
 )
 
 add_custom_target(web-clean
