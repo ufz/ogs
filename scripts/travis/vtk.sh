4dfc9a39a90ac930e0415c04dc99e332f0a58a70
Lars Bilke <lars.bilke@ufz.de>
2017-03-09 20:36:14 +0100

    [Travis] Use system Boost and VTK.

--- scripts/travis/vtk.sh
+++ scripts/travis/vtk.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-if [ ! -d "$HOME/VTK-Install/include" ]; then
+if [ ! -d "$HOME/VTK-Install-7.1/include" ]; then
     cd $HOME
     wget http://ogsstorage.blob.core.windows.net/jenkins/ogs6-lib-sources/vtk-7.1.0-binary.tar.gz
     tar -xzf vtk-7.1.0-binary.tar.gz
