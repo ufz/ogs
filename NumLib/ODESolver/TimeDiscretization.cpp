6dfa3dd866487fff29c5c046bdb8abdb42e86bb9
Wenqing Wang <wenqing.wang@ufz.de>
2017-03-31 17:59:18 +0200

    [dt] Added a member to classes of time discretization to get relative solution error
    and moved some member definitions to cpp file.

---
+++ NumLib/ODESolver/TimeDiscretization.cpp
@@ -0,0 +1,119 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2017, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ *  \file   TimeDiscretization.cpp
+ *  Created on March 31, 2017, 10:30 AM
+ */
+
+#include "TimeDiscretization.h"
+namespace NumLib
+{
+/**
+ * Compute and return the relative change of solutions between two successive
+ * time steps by \f$ e_n = \|u^{n+1}-u^{n}\|/\|u^{n+1}\| \f$.
+ *
+ * @param x      The current solution
+ * @param x_old  The previous solution
+ * @return       \f$ e_n = \|u^{n+1}-u^{n}\|/\|u^{n+1}\| \f$.
+ *
+ *  \warning the value of x_old is changed to x - x_old after this computation.
+ */
+static double computeRelativeError(GlobalVector const& x, GlobalVector& x_old)
+{
+    const double norm2_x = MathLib::LinAlg::norm2(x);
+    assert(norm2_x > std::numeric_limits<double>::epsilon());
+
+    // dx = x - x_old --> x_old
+    MathLib::LinAlg::axpy(x_old, -1.0, x);
+    return MathLib::LinAlg::norm2(x_old) / norm2_x;
+}
+
+double BackwardEuler::getRelativeError(GlobalVector const& x)
+{
+    return computeRelativeError(x, _x_old);
+}
+
+double ForwardEuler::getRelativeError(GlobalVector const& x)
+{
+    return computeRelativeError(x, _x_old);
+}
+
+double CrankNicolson::getRelativeError(GlobalVector const& x)
+{
+    return computeRelativeError(x, _x_old);
+}
+
+double BackwardDifferentiationFormula::getRelativeError(GlobalVector const& x)
+{
+    return (_xs_old.size() < _num_steps) ? 0. : computeRelativeError(
+                                                    x, *_xs_old[_offset]);
+}
+
+void BackwardDifferentiationFormula::pushState(const double,
+                                               GlobalVector const& x,
+                                               InternalMatrixStorage const&)
+{
+    namespace LinAlg = MathLib::LinAlg;
+    // TODO use boost circular buffer?
+
+    // until _xs_old is filled, lower-order BDF formulas are used.
+    if (_xs_old.size() < _num_steps)
+    {
+        _xs_old.push_back(&NumLib::GlobalVectorProvider::provider.getVector(x));
+    }
+    else
+    {
+        LinAlg::copy(x, *_xs_old[_offset]);
+        _offset =
+            (_offset + 1) % _num_steps;  // treat _xs_old as a circular buffer
+    }
+}
+
+namespace detail
+{
+//! Coefficients used in the backward differentiation formulas.
+const double BDF_Coeffs[6][7] = {
+    // leftmost column: weight of the solution at the new timestep
+    // signs of columns > 1 are flipped compared to standard BDF tableaus
+    {1.0, 1.0},
+    {1.5, 2.0, -0.5},
+    {11.0 / 6.0, 3.0, -1.5, 1.0 / 3.0},
+    {25.0 / 12.0, 4.0, -3.0, 4.0 / 3.0, -0.25},
+    {137.0 / 60.0, 5.0, -5.0, 10.0 / 3.0, -1.25, 1.0 / 5.0},
+    {147.0 / 60.0, 6.0, -7.5, 20.0 / 3.0, -3.75, 6.0 / 5.0, -1.0 / 6.0}
+    // coefficient of (for BDF(6), the oldest state, x_n, is always rightmost)
+    //        x_+6, x_+5, x_+4,       x_+3,  x_+2, x_+1,     x_n
+};
+}
+
+double BackwardDifferentiationFormula::getNewXWeight() const
+{
+    auto const k = eff_num_steps();
+    return detail::BDF_Coeffs[k - 1][0] / _delta_t;
+}
+
+void BackwardDifferentiationFormula::getWeightedOldX(GlobalVector& y) const
+{
+    namespace LinAlg = MathLib::LinAlg;
+
+    auto const k = eff_num_steps();
+    auto const* const BDFk = detail::BDF_Coeffs[k - 1];
+
+    // compute linear combination \sum_{i=0}^{k-1} BDFk_{k-i} \cdot x_{n+i}
+    LinAlg::copy(*_xs_old[_offset], y);  // _xs_old[offset] = x_n
+    LinAlg::scale(y, BDFk[k]);
+
+    for (unsigned i = 1; i < k; ++i)
+    {
+        auto const off = (_offset + i) % k;
+        LinAlg::axpy(y, BDFk[k - i], *_xs_old[off]);
+    }
+
+    LinAlg::scale(y, 1.0 / _delta_t);
+}
+
+}  // end of namespace NumLib
--- NumLib/ODESolver/TimeDiscretization.h
+++ NumLib/ODESolver/TimeDiscretization.h
@@ -117,6 +117,16 @@ public:
     //! Sets the initial condition.
     virtual void setInitialState(const double t0, GlobalVector const& x0) = 0;
 
+    /*! Get the relative change of solutions between two successive time steps
+     *  by \f$ e_n = \|u^{n+1}-u^{n}\|/\|u^{n+1}\| \f$.
+     *
+     * \warning _x_old kept in the derived classes is changed to x - _x_old
+     *          after this computation! Therefore it can be only called just
+     *          before pushState(...), in which the value of _x_old is updated.
+     * \param x    The solution at the current timestep.
+     */
+    virtual double getRelativeError(GlobalVector const& x) = 0;
+
     /*! Indicate that the current timestep is done and that you will proceed to
      * the next one.
      *
@@ -222,6 +232,8 @@ public:
         MathLib::LinAlg::copy(x0, _x_old);
     }
 
+    double getRelativeError(GlobalVector const& x) override;
+
     void pushState(const double /*t*/, GlobalVector const& x,
                    InternalMatrixStorage const&) override
     {
@@ -271,6 +283,8 @@ public:
         _t_old = t0;
         MathLib::LinAlg::copy(x0, _x_old);
     }
+
+    double getRelativeError(GlobalVector const& x) override;
 
     void pushState(const double /*t*/, GlobalVector const& x,
                    InternalMatrixStorage const&) override
@@ -346,6 +360,8 @@ public:
         MathLib::LinAlg::copy(x0, _x_old);
     }
 
+    double getRelativeError(GlobalVector const& x) override;
+
     void pushState(const double, GlobalVector const& x,
                    InternalMatrixStorage const& strg) override
     {
@@ -381,23 +397,6 @@ private:
     double _delta_t;      //!< the timestep size
     GlobalVector& _x_old;       //!< the solution from the preceding timestep
 };
-
-namespace detail
-{
-//! Coefficients used in the backward differentiation formulas.
-const double BDF_Coeffs[6][7] = {
-    // leftmost column: weight of the solution at the new timestep
-    // signs of columns > 1 are flipped compared to standard BDF tableaus
-    {1.0, 1.0},
-    {1.5, 2.0, -0.5},
-    {11.0 / 6.0, 3.0, -1.5, 1.0 / 3.0},
-    {25.0 / 12.0, 4.0, -3.0, 4.0 / 3.0, -0.25},
-    {137.0 / 60.0, 5.0, -5.0, 10.0 / 3.0, -1.25, 1.0 / 5.0},
-    {147.0 / 60.0, 6.0, -7.5, 20.0 / 3.0, -3.75, 6.0 / 5.0, -1.0 / 6.0}
-    // coefficient of (for BDF(6), the oldest state, x_n, is always rightmost)
-    //        x_+6, x_+5, x_+4,       x_+3,  x_+2, x_+1,     x_n
-};
-}
 
 //! Backward differentiation formula.
 class BackwardDifferentiationFormula final : public TimeDiscretization
@@ -435,25 +434,10 @@ public:
             &NumLib::GlobalVectorProvider::provider.getVector(x0));
     }
 
-    void pushState(const double, GlobalVector const& x,
-                   InternalMatrixStorage const&) override
-    {
-        namespace LinAlg = MathLib::LinAlg;
-        // TODO use boost cirular buffer?
+    double getRelativeError(GlobalVector const& x) override;
 
-        // until _xs_old is filled, lower-order BDF formulas are used.
-        if (_xs_old.size() < _num_steps)
-        {
-            _xs_old.push_back(
-                &NumLib::GlobalVectorProvider::provider.getVector(x));
-        }
-        else
-        {
-            LinAlg::copy(x, *_xs_old[_offset]);
-            _offset = (_offset + 1) %
-                      _num_steps;  // treat _xs_old as a circular buffer
-        }
-    }
+    void pushState(const double, GlobalVector const& x,
+                   InternalMatrixStorage const&) override;
 
     void nextTimestep(const double t, const double delta_t) override
     {
@@ -462,31 +446,10 @@ public:
     }
 
     double getCurrentTime() const override { return _t; }
-    double getNewXWeight() const override
-    {
-        auto const k = eff_num_steps();
-        return detail::BDF_Coeffs[k - 1][0] / _delta_t;
-    }
 
-    void getWeightedOldX(GlobalVector& y) const override
-    {
-        namespace LinAlg = MathLib::LinAlg;
+    double getNewXWeight() const override;
 
-        auto const k = eff_num_steps();
-        auto const* const BDFk = detail::BDF_Coeffs[k - 1];
-
-        // compute linear combination \sum_{i=0}^{k-1} BDFk_{k-i} \cdot x_{n+i}
-        LinAlg::copy(*_xs_old[_offset], y);  // _xs_old[offset] = x_n
-        LinAlg::scale(y, BDFk[k]);
-
-        for (unsigned i = 1; i < k; ++i)
-        {
-            auto const off = (_offset + i) % k;
-            LinAlg::axpy(y, BDFk[k - i], *_xs_old[off]);
-        }
-
-        LinAlg::scale(y, 1.0 / _delta_t);
-    }
+    void getWeightedOldX(GlobalVector& y) const override;
 
 private:
     std::size_t eff_num_steps() const { return _xs_old.size(); }
