69aec182513b3ab24c2740bc9e00f59ac26b4b20
Lars Bilke <lars.bilke@ufz.de>
2017-04-26 14:16:37 +0200

    [conan] Added envinf1 conan environment.

---
+++ scripts/env/envinf1/conan.sh
@@ -0,0 +1,12 @@
+module () { eval `/usr/local/modules/3.2.10-1/Modules/3.2.10/bin/modulecmd sh $*`; }
+export MODULEPATH=$MODULEPATH:/global/apps/modulefiles
+
+module load python/2
+module load cmake/3.6.2-1
+module load gcc/4.8.1-3
+
+# Tools
+module load coreutils/8.21-1
+module load ccache/3.3.3
+
+source /global/apps/ogs/virtualenv/conan/bin/activate
