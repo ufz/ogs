f9c7b028ff78a88cfe9110933ae8653bdb2e5903
Lars Bilke <lars.bilke@ufz.de>
2017-05-17 15:29:14 +0200

    [Jenkins] envinf1 environment updated with gcc 6.2.

--- scripts/env/envinf1/petsc.sh
+++ scripts/env/envinf1/petsc.sh
@@ -1,3 +1,3 @@
 DIR=$( cd $(dirname "${BASH_SOURCE[0]}") ; pwd -P )
 source $DIR/mpi.sh
-module load petsc/3.7.2_maint_petsc_maint_3.7.2_gcc_openmpi_1.8.4-2
+module load petsc-bilke/3.7.6_gcc-6.2.0_openmpi-1.8.8
