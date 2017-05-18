9a026340b4c11991f7d4de2909bf210faeff8b3a
Lars Bilke <lars.bilke@ufz.de>
2017-05-18 14:39:21 +0200

    [Travis] Bumped Conan Travis gcc version to 4.9 too.
    Currently this config is not checked.

--- scripts/travis/conan.sh
+++ scripts/travis/conan.sh
@@ -2,4 +2,4 @@ wget http://downloads.conan.io/latest_debian -O conan.deb
 sudo dpkg -i conan.deb
 rm conan.deb
 
-conan install -u -s compiler=gcc -s compiler.version=4.8
+conan install -u -s compiler=gcc -s compiler.version=4.9
