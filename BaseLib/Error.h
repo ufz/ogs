89dd4b9cf6679f31912bb7e14008cd58f9622220
Christoph Lehmann <christoph.lehmann@ufz.de>
2017-05-23 16:34:42 +0200

    [BL] enable SOME_EXPRESSION && OGS_FATAL(...)

--- BaseLib/Error.h
+++ BaseLib/Error.h
@@ -13,25 +13,51 @@
 
 #include <cstdlib>
 #include <logog/include/logog.hpp>
-#include "StringTools.h"
 
-#define OGS_FATAL(fmt, ...)\
-    {\
-    ERR("%s", BaseLib::format(fmt, ##__VA_ARGS__).data());\
-    std::abort();\
-    }
+namespace BaseLib
+{
+namespace detail
+{
+template <typename Msg>
+[[noreturn]] bool error_impl(Msg&& msg)
+{
+    ERR("%s", msg.data());
+    std::abort();
+}
 
-#else // OGS_FATAL_ABORT
+}  // namespace detail
+
+}  // namespace BaseLib
+
+#else  // OGS_FATAL_ABORT
 
 #include <stdexcept>
+
+namespace BaseLib
+{
+namespace detail
+{
+template <typename Msg>
+[[noreturn]] bool error_impl(Msg&& msg)
+{
+    throw std::runtime_error(std::forward<Msg>(msg));
+}
+
+}  // namespace detail
+
+}  // namespace BaseLib
+
+#endif  // OGS_FATAL_ABORT
+
 #include "FileTools.h"
 #include "StringTools.h"
 
 #define OGS_STR(x) #x
 #define OGS_STRINGIFY(x) OGS_STR(x)
-#define OGS_LOCATION " at " + BaseLib::extractBaseName(__FILE__) + ", line " OGS_STRINGIFY(__LINE__)
+#define OGS_LOCATION                              \
+    " at " + BaseLib::extractBaseName(__FILE__) + \
+        ", line " OGS_STRINGIFY(__LINE__)
 
-#define OGS_FATAL(fmt, ...)\
-    throw std::runtime_error(BaseLib::format(fmt, ##__VA_ARGS__) + OGS_LOCATION);
-
-#endif // OGS_FATAL_ABORT
+#define OGS_FATAL(fmt, ...)                                           \
+    BaseLib::detail::error_impl(BaseLib::format(fmt, ##__VA_ARGS__) + \
+                                OGS_LOCATION)
