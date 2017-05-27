c9415f528d30af3385f1ac178616167109529336
xymiao <miaoxingyuan@live.cn>
2017-05-27 22:13:42 +0200

    added iglu tests as ctests.

--- ProcessLib/ThermoMechanics/Tests.cmake
+++ ProcessLib/ThermoMechanics/Tests.cmake
@@ -14,3 +14,37 @@ AddTest(
     expected_cube_1e3_tm_pcs_0_ts_80_t_72000.000000.vtu cube_1e3_tm_pcs_0_ts_80_t_72000.000000.vtu displacement displacement
     expected_cube_1e3_tm_pcs_0_ts_80_t_72000.000000.vtu cube_1e3_tm_pcs_0_ts_80_t_72000.000000.vtu temperature temperature
 )
+
+AddTest(
+    NAME 2D_ThermoElastic_IGLU_Plane_Strain
+    PATH ThermoMechanics
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS iglu_quarter_plane_strain.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
+    ABSTOL 1e-15 RELTOL 1e-15
+    DIFF_DATA
+    expected_tm_q_pcs_0_ts_20_t_20000.000000.vtu tm_q_pcs_0_ts_20_t_20000.000000.vtu displacement displacement
+    expected_tm_q_pcs_0_ts_20_t_20000.000000.vtu tm_q_pcs_0_ts_20_t_20000.000000.vtu temperature temperature
+    expected_tm_q_pcs_0_ts_20_t_20000.000000.vtu tm_q_pcs_0_ts_20_t_20000.000000.vtu sigma_xx sigma_xx
+    expected_tm_q_pcs_0_ts_20_t_20000.000000.vtu tm_q_pcs_0_ts_20_t_20000.000000.vtu sigma_yy sigma_yy
+    expected_tm_q_pcs_0_ts_20_t_20000.000000.vtu tm_q_pcs_0_ts_20_t_20000.000000.vtu sigma_zz sigma_zz
+)
+
+AddTest(
+    NAME 2D_ThermoElastic_IGLU_Axisymmetric_Plane_Strain
+    PATH ThermoMechanics
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS iglu_axisymmetric_plane_strain.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
+    ABSTOL 1e-15 RELTOL 1e-15
+    DIFF_DATA
+    expected_tm_a_pcs_0_ts_20_t_20000.000000.vtu tm_a_pcs_0_ts_20_t_20000.000000.vtu displacement displacement
+    expected_tm_a_pcs_0_ts_20_t_20000.000000.vtu tm_a_pcs_0_ts_20_t_20000.000000.vtu temperature temperature
+    expected_tm_a_pcs_0_ts_20_t_20000.000000.vtu tm_a_pcs_0_ts_20_t_20000.000000.vtu sigma_xx sigma_xx
+    expected_tm_a_pcs_0_ts_20_t_20000.000000.vtu tm_a_pcs_0_ts_20_t_20000.000000.vtu sigma_yy sigma_yy
+    expected_tm_a_pcs_0_ts_20_t_20000.000000.vtu tm_a_pcs_0_ts_20_t_20000.000000.vtu sigma_zz sigma_zz
+)
