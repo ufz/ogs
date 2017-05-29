0b637390a3c4ca76d005f82efe4aca941a13a7cb
xymiao <miaoxingyuan@live.cn>
2017-05-29 18:59:47 +0200

    renamed tests to contain 'ThermoElastic'.

--- ProcessLib/ThermoMechanics/Tests.cmake
+++ ProcessLib/ThermoMechanics/Tests.cmake
@@ -16,7 +16,7 @@ AddTest(
 )
 
 AddTest(
-    NAME 2D_IGLU_Plane_Strain
+    NAME 2D_ThermoElastic_IGLU_Plane_Strain
     PATH ThermoMechanics
     EXECUTABLE ogs
     EXECUTABLE_ARGS iglu_quarter_plane_strain.prj
@@ -25,15 +25,15 @@ AddTest(
     REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
     ABSTOL 1e-15 RELTOL 1e-15
     DIFF_DATA
-    expected_tm_q_pcs_0_ts_20_t_12000.000000.vtu tm_q_pcs_0_ts_20_t_12000.000000.vtu displacement displacement
-    expected_tm_q_pcs_0_ts_20_t_12000.000000.vtu tm_q_pcs_0_ts_20_t_12000.000000.vtu temperature temperature
-    expected_tm_q_pcs_0_ts_20_t_12000.000000.vtu tm_q_pcs_0_ts_20_t_12000.000000.vtu sigma_xx sigma_xx
-    expected_tm_q_pcs_0_ts_20_t_12000.000000.vtu tm_q_pcs_0_ts_20_t_12000.000000.vtu sigma_yy sigma_yy
-    expected_tm_q_pcs_0_ts_20_t_12000.000000.vtu tm_q_pcs_0_ts_20_t_12000.000000.vtu sigma_zz sigma_zz
+    expected_tm_q_pcs_0_ts_20_t_20000.000000.vtu tm_q_pcs_0_ts_20_t_20000.000000.vtu displacement displacement
+    expected_tm_q_pcs_0_ts_20_t_20000.000000.vtu tm_q_pcs_0_ts_20_t_20000.000000.vtu temperature temperature
+    expected_tm_q_pcs_0_ts_20_t_20000.000000.vtu tm_q_pcs_0_ts_20_t_20000.000000.vtu sigma_xx sigma_xx
+    expected_tm_q_pcs_0_ts_20_t_20000.000000.vtu tm_q_pcs_0_ts_20_t_20000.000000.vtu sigma_yy sigma_yy
+    expected_tm_q_pcs_0_ts_20_t_20000.000000.vtu tm_q_pcs_0_ts_20_t_20000.000000.vtu sigma_zz sigma_zz
 )
 
 AddTest(
-    NAME 2D_IGLU_Axisymmetric_Plane_Strain
+    NAME 2D_ThermoElastic_IGLU_Axisymmetric_Plane_Strain
     PATH ThermoMechanics
     EXECUTABLE ogs
     EXECUTABLE_ARGS iglu_axisymmetric_plane_strain.prj
@@ -42,9 +42,9 @@ AddTest(
     REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
     ABSTOL 1e-15 RELTOL 1e-15
     DIFF_DATA
-    expected_tm_a_pcs_0_ts_20_t_12000.000000.vtu tm_a_pcs_0_ts_20_t_12000.000000.vtu displacement displacement
-    expected_tm_a_pcs_0_ts_20_t_12000.000000.vtu tm_a_pcs_0_ts_20_t_12000.000000.vtu temperature temperature
-    expected_tm_a_pcs_0_ts_20_t_12000.000000.vtu tm_a_pcs_0_ts_20_t_12000.000000.vtu sigma_xx sigma_xx
-    expected_tm_a_pcs_0_ts_20_t_12000.000000.vtu tm_a_pcs_0_ts_20_t_12000.000000.vtu sigma_yy sigma_yy
-    expected_tm_a_pcs_0_ts_20_t_12000.000000.vtu tm_a_pcs_0_ts_20_t_12000.000000.vtu sigma_zz sigma_zz
+    expected_tm_a_pcs_0_ts_20_t_20000.000000.vtu tm_a_pcs_0_ts_20_t_20000.000000.vtu displacement displacement
+    expected_tm_a_pcs_0_ts_20_t_20000.000000.vtu tm_a_pcs_0_ts_20_t_20000.000000.vtu temperature temperature
+    expected_tm_a_pcs_0_ts_20_t_20000.000000.vtu tm_a_pcs_0_ts_20_t_20000.000000.vtu sigma_xx sigma_xx
+    expected_tm_a_pcs_0_ts_20_t_20000.000000.vtu tm_a_pcs_0_ts_20_t_20000.000000.vtu sigma_yy sigma_yy
+    expected_tm_a_pcs_0_ts_20_t_20000.000000.vtu tm_a_pcs_0_ts_20_t_20000.000000.vtu sigma_zz sigma_zz
 )
