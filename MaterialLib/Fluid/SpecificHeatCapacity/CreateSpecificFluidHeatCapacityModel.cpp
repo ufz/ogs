66a63aa1a68d54d77e0515875f2ee8dfe31d8eb4
Thomas Fischer <thomas.fischer@ufz.de>
2017-06-07 12:46:49 +0200

    [Material] Correct output message in create method.

--- MaterialLib/Fluid/SpecificHeatCapacity/CreateSpecificFluidHeatCapacityModel.cpp
+++ MaterialLib/Fluid/SpecificHeatCapacity/CreateSpecificFluidHeatCapacityModel.cpp
@@ -36,7 +36,7 @@ std::unique_ptr<FluidProperty> createSpecificFluidHeatCapacityModel(
     // TODO: add more models
 
     OGS_FATAL(
-        "The viscosity type %s is unavailable.\n"
+        "The specific heat capacity type %s is unavailable.\n"
         "The available type is \n\tConstant\n",
         type.data());
 }
