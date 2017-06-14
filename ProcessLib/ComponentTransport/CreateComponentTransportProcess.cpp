9799ca5749b06af7098fd0fcae599659f0098d27
Thomas Fischer <thomas.fischer@ufz.de>
2017-06-14 11:15:38 +0200

    [PL/ComponentTransport] Complete and update documentation.

--- ProcessLib/ComponentTransport/CreateComponentTransportProcess.cpp
+++ ProcessLib/ComponentTransport/CreateComponentTransportProcess.cpp
@@ -69,10 +69,10 @@ std::unique_ptr<Process> createComponentTransportProcess(
     DBUG("Use \'%s\' as fluid_reference_density parameter.",
          fluid_reference_density.name.c_str());
 
-    // Parameter for the longitudinal solute dispersivity.
+    // Parameter for the longitudinal molecular diffusion coefficient.
     auto const& molecular_diffusion_coefficient = findParameter<double>(
         config,
-        //! \ogs_file_param_special{prj__processes__process__ComponentTransport__molecular_diffusion_coefficient
+        //! \ogs_file_param_special{prj__processes__process__ComponentTransport__molecular_diffusion_coefficient}
         "molecular_diffusion_coefficient", parameters, 1);
     DBUG("Use \'%s\' as molecular diffusion coefficient parameter.",
          molecular_diffusion_coefficient.name.c_str());
@@ -80,7 +80,7 @@ std::unique_ptr<Process> createComponentTransportProcess(
     // Parameter for the longitudinal solute dispersivity.
     auto const& solute_dispersivity_longitudinal = findParameter<double>(
         config,
-        //! \ogs_file_param_special{prj__processes__process__ComponentTransport__solute_dispersivity_longitudinal
+        //! \ogs_file_param_special{prj__processes__process__ComponentTransport__solute_dispersivity_longitudinal}
         "solute_dispersivity_longitudinal", parameters, 1);
     DBUG("Use \'%s\' as longitudinal solute dispersivity parameter.",
          solute_dispersivity_longitudinal.name.c_str());
@@ -88,7 +88,7 @@ std::unique_ptr<Process> createComponentTransportProcess(
     // Parameter for the transverse solute dispersivity.
     auto const& solute_dispersivity_transverse = findParameter<double>(
         config,
-        //! \ogs_file_param_special{prj__processes__process__ComponentTransport__solute_dispersivity_transverse
+        //! \ogs_file_param_special{prj__processes__process__ComponentTransport__solute_dispersivity_transverse}
         "solute_dispersivity_transverse", parameters, 1);
     DBUG("Use \'%s\' as transverse solute dispersivity parameter.",
          solute_dispersivity_transverse.name.c_str());
