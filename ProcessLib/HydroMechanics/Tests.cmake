eb8b3d68bd294c0c0b67ad9937849c56f26a11ff
Dmitri Naumov <github@naumov.de>
2017-07-13 02:07:04 +0200

    [PL] Additional element tests for HM conf. compr.
    Testing additionaly on Quad9, Tri6, and Hex20 meshes.
    The latter with plain strain.

--- ProcessLib/HydroMechanics/Tests.cmake
+++ ProcessLib/HydroMechanics/Tests.cmake
@@ -1,7 +1,7 @@
 # HydroMechanics; Small deformations, linear poroelastic (HML)
 AddTest(
-    NAME HydroMechanics_HML_square_1e2_confined_compression
-    PATH HydroMechanics/Linear
+    NAME HydroMechanics_HML_square_1e2_quad8_confined_compression
+    PATH HydroMechanics/Linear/Confined_Compression
     EXECUTABLE ogs
     EXECUTABLE_ARGS square_1e2.prj
     WRAPPER time
@@ -26,6 +26,46 @@ AddTest(
     expected_square_1e2_pcs_0_ts_120_t_1000.000000.vtu square_1e2_pcs_0_ts_120_t_1000.000000.vtu velocity_y velocity_y
     expected_square_1e2_pcs_0_ts_420_t_4000.000000.vtu square_1e2_pcs_0_ts_420_t_4000.000000.vtu velocity_y velocity_y
 )
+AddTest(
+    NAME HydroMechanics_HML_square_1e2_quad9_confined_compression
+    PATH HydroMechanics/Linear/Confined_Compression
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS square_1e2_quad9.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    ABSTOL 1e-15 RELTOL 1e-15
+    DIFF_DATA
+    expected_square_1e2_quad9_pcs_0_ts_1_t_5.000000.vtu square_1e2_quad9_pcs_0_ts_1_t_5.000000.vtu displacement displacement
+    expected_square_1e2_quad9_pcs_0_ts_1_t_5.000000.vtu square_1e2_quad9_pcs_0_ts_1_t_5.000000.vtu pressure pressure
+)
+AddTest(
+    NAME HydroMechanics_HML_square_1e2_tri6_confined_compression
+    PATH HydroMechanics/Linear/Confined_Compression
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS square_1e2_tri.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    ABSTOL 1e-15 RELTOL 1e-15
+    DIFF_DATA
+    expected_square_1e2_tri_pcs_0_ts_1_t_5.000000.vtu square_1e2_tri_pcs_0_ts_1_t_5.000000.vtu displacement displacement
+    expected_square_1e2_tri_pcs_0_ts_1_t_5.000000.vtu square_1e2_tri_pcs_0_ts_1_t_5.000000.vtu pressure pressure
+)
+AddTest(
+    NAME HydroMechanics_HML_cube_1e3_hex20_confined_compression
+    PATH HydroMechanics/Linear/Confined_Compression
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS cube_1e3.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    ABSTOL 1e-15 RELTOL 1e-15
+    DIFF_DATA
+    expected_cube_1e3_pcs_0_ts_1_t_5.000000.vtu cube_1e3_pcs_0_ts_1_t_5.000000.vtu displacement displacement
+    expected_cube_1e3_pcs_0_ts_1_t_5.000000.vtu cube_1e3_pcs_0_ts_1_t_5.000000.vtu pressure pressure
+)
+
 # HydroMechanics; Small deformation, linear poroelastic (unconfined compression early) The drainage process is ongoing and the displacement behaviour is related to water pressure and solid properties.
 AddTest(
     NAME HydroMechanics_HML_square_1e2_unconfined_compression_early
