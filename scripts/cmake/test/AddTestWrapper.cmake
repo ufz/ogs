f500622aec54804fe1164e2af262ba7a6f5f871a
Christoph Lehmann <christoph.lehmann@ufz.de>
2017-07-20 11:46:22 +0200

    [cmake] print test commands before run

--- scripts/cmake/test/AddTestWrapper.cmake
+++ scripts/cmake/test/AddTestWrapper.cmake
@@ -4,6 +4,17 @@ foreach(FILE ${FILES_TO_DELETE})
     file(REMOVE ${BINARY_PATH}/${FILE})
 endforeach()
 
+# taken from https://stackoverflow.com/a/7216542
+function(JOIN VALUES GLUE OUTPUT)
+  string (REGEX REPLACE "([^\\]|^);" "\\1${GLUE}" _TMP_STR "${VALUES}")
+  string (REGEX REPLACE "[\\](.)" "\\1" _TMP_STR "${_TMP_STR}") #fixes escaping
+  set (${OUTPUT} "${_TMP_STR}" PARENT_SCOPE)
+endfunction()
+
+JOIN("${WRAPPER_ARGS}" " " WRAPPER_ARGS_STR)
+JOIN("${EXECUTABLE_ARGS}" " " EXECUTABLE_ARGS_STR)
+
+message(STATUS "running command generating test results: cd ${case_path} && ${WRAPPER_COMMAND} ${WRAPPER_ARGS_STR} ${EXECUTABLE} ${EXECUTABLE_ARGS_STR}")
 execute_process(
     COMMAND ${WRAPPER_COMMAND} ${WRAPPER_ARGS} ${EXECUTABLE} ${EXECUTABLE_ARGS}
     WORKING_DIRECTORY ${case_path}
