91f71cf7100dacd3adfc97fa18b2ebd604c6ffa8
xymiao <miaoxingyuan@live.cn>
2017-08-03 11:39:47 +0200

    added unconfined compression ctest.

--- ProcessLib/PhaseField/Tests.cmake
+++ ProcessLib/PhaseField/Tests.cmake
@@ -1,12 +1,13 @@
 AddTest(
-    NAME LARGE_3D_Crack_Beam_Tension
+    NAME PhaseField_3D_Unconfined_Compression
     PATH PhaseField
     EXECUTABLE ogs
-    EXECUTABLE_ARGS beam3d.prj
+    EXECUTABLE_ARGS cube_1e0.prj
     WRAPPER time
     TESTER vtkdiff
     REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
-    ABSTOL 1e-1 RELTOL 1e-2
+    ABSTOL 1e-6 RELTOL 1e-6
     DIFF_DATA
-    analytical_results.vtu beam3d_pcs_0_ts_10000_t_20.000000.vtu displacement displacement
-    analytical_results.vtu beam3d_pcs_0_ts_10000_t_20.000000.vtu phasefield phasefield
+    expected_cube_1e0_pcs_0_ts_10000_t_1.000000.vtu cube_1e0_pcs_0_ts_10000_t_1.000000.vtu displacement displacement
+    expected_cube_1e0_pcs_0_ts_10000_t_1.000000.vtu cube_1e0_pcs_0_ts_10000_t_1.000000.vtu phasefield phasefield
+   )
