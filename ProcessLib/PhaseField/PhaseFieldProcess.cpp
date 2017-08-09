7e3b66c139227b23d33fb56b75b1b58d66c2179e
xymiao <miaoxingyuan@live.cn>
2017-08-09 22:27:32 +0200

    added model description.

--- ProcessLib/PhaseField/PhaseFieldProcess.cpp
+++ ProcessLib/PhaseField/PhaseFieldProcess.cpp
@@ -16,5 +16,32 @@ namespace PhaseField
 template class PhaseFieldProcess<2>;
 template class PhaseFieldProcess<3>;
 
+/**
+ * \brief A class to simulate mechanical fracturing process
+ * using phase-field approach in solids described by
+ *
+ * \f[
+ *     \mathrm{div} \left[ \left(d^2 + k \right) \boldsymbol{\sigma}_0^+
+ *      + \boldsymbol{\sigma}_0^- \right] +
+ *        \varrho \boldsymbol{b} = \boldsymbol{0}
+ * \f]
+ * \f[
+ *     2d H(\boldsymbol{\epsilon}_\mathrm{el})
+ *      - \frac{1 - d}{2 \varepsilon} g_\mathrm{c}
+ *      - 2 \varepsilon g_\mathrm{c} \mathrm{div}(\mathrm{grad} d) = 0
+ * \f]
+ * where
+ *    \f{eqnarray*}{
+ *       &d:&                    \mbox{order parameter,}\\
+ *       &\varrho:&              \mbox{density,}\\
+ *       &H:&                    \mbox{history field,}\\
+ *       &g_\mathrm{c}:&         \mbox{fracture energy,}\\
+ *       &\varepsilon:&          \mbox{length scale}\\
+ *    \f}
+ *
+ * Detailed model description can refer
+ * <a href="Miao_Biot2017.pdff" target="_blank"><b>Phase field method</b></a>
+ */
+
 }  // namespace PhaseField
 }  // namespace ProcessLib
