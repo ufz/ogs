9f42299b752d5d5d2f66ecc39e0ccc8f67fb0316
Thomas Fischer <thomas.fischer@ufz.de>
2017-08-15 14:49:10 +0200

    [FileIO] Impl. of createSurface.

---
+++ Applications/FileIO/Legacy/createSurface.h
@@ -0,0 +1,28 @@
+/**
+ *
+ * \copyright
+ * Copyright (c) 2012-2017, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#pragma once
+
+#include <vector>
+
+
+namespace GeoLib
+{
+class Polyline;
+class Surface;
+class GEOObjects;
+}
+
+namespace FileIO
+{
+void createSurface(GeoLib::Polyline const& ply,
+                   GeoLib::GEOObjects& geometries,
+                   std::string const& geometry_name);
+}
