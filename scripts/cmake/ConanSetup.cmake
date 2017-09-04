9fdf07a46446d430c4a1353e9ab0d6f5fa12406f
Lars Bilke <lars.bilke@ufz.de>
2017-09-04 11:57:59 +0200

    [CMake] Integrate Conan install into CMake run.
    Set OGS_USE_CONAN=ON to enable this. Once enabled there is no
    conan install .. call necessary anymore.
    
    You can select individual libraries to be used from the system, e.g.:
    
    cmake -DOGS_USE_CONAN=ON -DOGS_LIB_VTK=System ..
    
    This lets Conan install all third-party libraries except of VTK.

--- scripts/cmake/ConanSetup.cmake
+++ scripts/cmake/ConanSetup.cmake
@@ -1,25 +1,48 @@
-if($ENV{CONAN_INSTALL})
-    find_program(CONAN_TOOL_PATH conan)
-    if(CONAN_TOOL_PATH)
-        execute_process(
-            COMMAND ${CONAN_TOOL_PATH} install -u ${PROJECT_SOURCE_DIR}
-            WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-        )
+if(NOT OGS_USE_CONAN)
+    return()
+endif()
+
+if(DEFINED OGS_LIB_Boost)
+    if(${OGS_LIB_Boost} STREQUAL "Default")
+        cmake_minimum_required(VERSION 3.4) # Conan Boost package requires this
     endif()
 endif()
-set(USE_CONAN false CACHE INTERNAL "")
-if(EXISTS ${PROJECT_BINARY_DIR}/conanbuildinfo.cmake)
-    include(${PROJECT_BINARY_DIR}/conanbuildinfo.cmake)
-    set(USE_CONAN true)
-elseif(EXISTS ${PROJECT_SOURCE_DIR}/conanbuildinfo.cmake)
-    include(${PROJECT_SOURCE_DIR}/conanbuildinfo.cmake)
-    set(USE_CONAN true)
+
+include(${PROJECT_SOURCE_DIR}/scripts/cmake/conan/conan.cmake)
+
+set(CONAN_REQUIRES
+    Boost/[>=1.56.0]@lasote/stable
+    Eigen3/3.2.9@bilke/stable
+    VTK/[>=7.1]@bilke/stable
+    CACHE INTERNAL ""
+)
+
+set(CONAN_OPTIONS
+    Boost:header_only=True
+    Boost:without_iostreams=True
+    Qt:xmlpatterns=True
+    CACHE INTERNAL ""
+)
+
+if(OGS_BUILD_GUI)
+    set(CONAN_REQUIRES ${CONAN_REQUIRES}
+        Shapelib/1.3.0@bilke/stable
+        libgeotiff/1.4.2@bilke/stable
+        Qt/5.6.2@bilke/testing
+    )
 endif()
-if(USE_CONAN)
-    cmake_minimum_required(VERSION 3.4) # Conan Boost package requires this
-    set(CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/scripts/cmake/conan ${CONAN_CMAKE_MODULE_PATH} ${CMAKE_MODULE_PATH})
-    message(STATUS "Using conan package manager")
-    set(OGS_LIB_VTK System CACHE INTERNAL "")
-    set(OGS_LIB_BOOST System CACHE INTERNAL "")
-    set(OGS_LIB_EIGEN System CACHE INTERNAL "")
-endif()
+
+# Remove libraries from Conan which are set to "System"
+message(STATUS "Third-party libraries:")
+foreach(LIB ${OGS_LIBS})
+    message("  - OGS_LIB_${LIB} = ${OGS_LIB_${LIB}}")
+    if("${OGS_LIB_${LIB}}" STREQUAL System)
+        list(FILTER CONAN_REQUIRES EXCLUDE REGEX ${LIB})
+    endif()
+endforeach()
+
+conan_cmake_run(REQUIRES ${CONAN_REQUIRES}
+                OPTIONS ${CONAN_OPTIONS}
+                BASIC_SETUP
+                BUILD missing
+)
