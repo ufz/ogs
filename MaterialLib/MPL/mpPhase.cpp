5a7f14a08741d17cd2fd2acabc4b94850d4ac5ba
Norbert Grunwald <n.g.boettcher@gmail.com>
2017-09-07 12:45:54 +0200

    new (empty) implementation of class Phase

---
+++ MaterialLib/MPL/mpPhase.cpp
@@ -0,0 +1,23 @@
+/**
+ * \author Norbert Grunwald
+ * \date   Sep 7, 2017
+ * \brief
+ *
+ * \copyright
+ * Copyright (c) 2012-2017, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#include "mpPhase.h"
+
+
+namespace MaterialPropertyLib
+{
+
+Phase::Phase(){};
+
+} // MaterialPropertyLib
+
