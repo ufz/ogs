bae3385b4627079fcb2a593dc4cdcb85eac50938
Norbert Grunwald <n.g.boettcher@gmail.com>
2017-09-07 12:46:10 +0200

    new (empty) implementation of class Component

---
+++ MaterialLib/MPL/mpComponent.cpp
@@ -0,0 +1,24 @@
+/**
+ * \author Norbert Grunwald
+ * \date   Sep 7, 2017
+ * \brief
+ *
+ * \copyright
+ * Copyright (c) 2012-2017, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#include "mpComponent.h"
+
+
+namespace MaterialPropertyLib
+{
+
+Component::Component(){};
+
+} // MaterialPropertyLib
+
+
---
+++ MaterialLib/MPL/mpComponent.h
@@ -0,0 +1,29 @@
+/**
+ * \author Norbert Grunwald
+ * \date   Sep 7, 2017
+ * \brief
+ *
+ * \copyright
+ * Copyright (c) 2012-2017, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+#ifndef MATERIALLIB_MPL_MPCOMPONENT_H_
+#define MATERIALLIB_MPL_MPCOMPONENT_H_
+
+namespace MaterialPropertyLib
+{
+class Component
+{
+public:
+    Component();
+};
+
+} //MaterialPropertyLib
+
+
+
+
+#endif /* MATERIALLIB_MPL_MPCOMPONENT_H_ */
