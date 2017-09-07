12d2c52b08b62df8e37dc0595c5f6623a986e52f
Norbert Grunwald <n.g.boettcher@gmail.com>
2017-09-07 11:51:13 +0200

    new sub-folder for MPL
    Sub-folder for new material library and empty class for medium properties

---
+++ MaterialLib/MPL/mpMedium.h
@@ -0,0 +1,24 @@
+/**
+ * \author Norbert Grunwald
+ * \date   05.09.2017
+ * \brief  
+ *
+ * \copyright
+ * Copyright (c) 2012-2017, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+#ifndef MATERIALLIB_MPL_MPMEDIUM_H_
+#define MATERIALLIB_MPL_MPMEDIUM_H_
+
+namespace MaterialPropertyLib
+{
+class Medium
+{
+};
+
+}
+
+#endif /* MATERIALLIB_MPL_MPMEDIUM_H_ */
