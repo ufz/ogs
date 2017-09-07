d2af9b99efc8d96f69a1ba00c05bf44401c3b52e
Lars Bilke <lars.bilke@ufz.de>
2017-09-07 14:51:16 +0200

    [Jenkins] Build partmesh and mpmetis for envinf1 / eve module.

--- scripts/jenkins/gcc-dynamic.groovy
+++ scripts/jenkins/gcc-dynamic.groovy
@@ -2,7 +2,8 @@ def defaultCMakeOptions =
     '-DCMAKE_BUILD_TYPE=Release ' +
     '-DOGS_LIB_BOOST=System ' +
     '-DOGS_LIB_VTK=System ' +
-    '-DOGS_BUILD_UTILS=ON '
+    '-DOGS_BUILD_UTILS=ON ' +
+    '-DOGS_BUILD_METIS=ON '
 
 def configure = new ogs.configure()
 def build = new ogs.build()
