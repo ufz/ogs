5d0f73b75178e8f801a4f17900f011fa4d6359a9
Norbert Grunwald <n.g.boettcher@gmail.com>
2017-09-07 12:46:38 +0200

    new (empty) implementation of class Property

---
+++ MaterialLib/MPL/mpProperty.h
@@ -0,0 +1,28 @@
+/**
+ * \author Norbert Grunwald
+ * \date   Sep 7, 2017
+ * \brief
+ *
+ * \copyright
+ * Copyright (c) 2012-2017, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+#ifndef MATERIALLIB_MPL_MPPROPERTY_H_
+#define MATERIALLIB_MPL_MPPROPERTY_H_
+
+namespace MaterialPropertyLib
+{
+class Property
+{
+public:
+    Property();
+};
+
+} //MaterialPropertyLib
+
+
+
+#endif /* MATERIALLIB_MPL_MPPROPERTY_H_ */
