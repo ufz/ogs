951483d7b313c6cd6070e377c7c82b819293d2ae
Norbert Grunwald <n.g.boettcher@gmail.com>
2017-09-07 15:50:03 +0200

    implementation of Phase::newComponent method

--- MaterialLib/MPL/mpPhase.cpp
+++ MaterialLib/MPL/mpPhase.cpp
@@ -12,7 +12,7 @@
  */
 
 #include "mpPhase.h"
-
+#include "Components/components.h"
 
 namespace MaterialPropertyLib
 {
@@ -52,7 +52,32 @@ void Phase::createProperties(BaseLib::ConfigTree const& config)
 
 Component* Phase::newComponent (boost::optional<std::string> const &name)
 {
+    /// Check whether a name is given or not
+    if (name)
+    {
+        std::string component_name = static_cast<std::string>(name.get());
+        /// If a name is given, it must conform with one of the
+        /// derived component names in the following list:
+        if (boost::iequals(component_name, "water")) // "string" == "StRiNg"
+        {
+            return new Water;
+        }
+
+        /// If the given name does not appear in the list, this
+        /// throws a fatal error.
+        else
+            {
+            OGS_FATAL("The specified component name \"%s\" was not recognized", component_name.c_str());
+            return nullptr; // to avoid the 'no return' warning.
+            }
+    }
+    /// If there is no component name given (which is common), the
+    /// method creates a custom component, where all properties have
+    /// to be specified manually.
+    else return new Component;
+
 }
+
 
 } // MaterialPropertyLib
 
