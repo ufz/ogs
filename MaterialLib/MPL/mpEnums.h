edfcdd2d37c3dde1f136bf834b5705a8d4982568
Norbert Grunwald <n.g.boettcher@gmail.com>
2017-09-11 10:19:45 +0200

    more property enums added

--- MaterialLib/MPL/mpEnums.h
+++ MaterialLib/MPL/mpEnums.h
@@ -28,6 +28,12 @@ enum PropertyEnum : std::size_t
     critical_temperature,
     density,
     viscosity,
+    thermal_conductivity,
+    effective_stress,
+    permeability,
+    phase_velocity,
+    saturation,
+    relative_permeability,
     number_of_property_enums
 };
 
@@ -36,6 +42,13 @@ inline PropertyEnum convertStringToProperty (std::string const& inString) {
     if (boost::iequals(inString, "critical_temperature")) return critical_temperature;
     if (boost::iequals(inString, "density")) return density;
     if (boost::iequals(inString, "viscosity")) return viscosity;
+    if (boost::iequals(inString, "thermal_conductivity")) return thermal_conductivity;
+    if (boost::iequals(inString, "effective_stress")) return effective_stress;
+    if (boost::iequals(inString, "permeability")) return permeability;
+    if (boost::iequals(inString, "phase_velocity")) return phase_velocity;
+    if (boost::iequals(inString, "saturation")) return saturation;
+    if (boost::iequals(inString, "relative_permeability")) return relative_permeability;
+
     OGS_FATAL("The property name \"%s\" does not correspond to any known "
             "property", inString.c_str());
     return number_of_property_enums;  // to avoid the 'no return' warning
