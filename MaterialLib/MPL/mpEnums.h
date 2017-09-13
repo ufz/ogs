e806e18167ce7f3cd927d64b22373ec7e9a05f1f
Norbert Grunwald <n.g.boettcher@gmail.com>
2017-09-13 16:48:50 +0200

    added a primary variable container and associated data type definition
    also added some new property enums

--- MaterialLib/MPL/mpEnums.h
+++ MaterialLib/MPL/mpEnums.h
@@ -16,15 +16,72 @@
 
 #include "BaseLib/Error.h"
 #include <boost/algorithm/string/predicate.hpp>
+#include <boost/variant.hpp>
 #include <cstddef>
 #include <string>
 
 namespace MaterialPropertyLib
 {
+/// Very simple vector data type for holding
+/// vector components.
+using Vector = std::array<double, 3>;
+/// Very simple tensor data type for holding
+/// tensor components.
+using Tensor = std::array<double, 9>;
 
+/**
+ * PrimaryVariables is simply a list of all commonly used primary variables.
+ * It is used to determine the size of the VariableArray. If the primary
+ * variable of your choice is missing, simply add it somewhere at the list,
+ * but above the last entry 'numberOfPrimaryVariables'
+*/
+enum PrimaryVariables : std::size_t
+{
+	p_GR,
+	p_LR,
+	p_cap,
+	T_S,
+	T_L,
+	T_G,
+	T,
+	u,
+	numberOfPrimaryVariables
+};
+
+/**
+ * Data type for primary variables, designed to contain both
+ * scalar and vector data.
+ */
+using PrimaryVariableType = boost::variant<double, Vector>;
+
+/**
+ * The VariableArray is a std::array of fixed size. Its size is determined
+ * by the PrimaryVariables enumerator list. Data type of that array is
+ * defined by the PrimaryVariableType definition.
+*/
+using VariableArray =
+		std::array<PrimaryVariableType, numberOfPrimaryVariables>;
+
+/// This method returns a value of type double from the
+/// primar variables array
+inline double getScalar (PrimaryVariableType pv)
+{
+    return boost::get<double>(pv);
+}
+
+/**
+ * PropertyEnum is an enumerator list of all known properties of a substance.
+ * This includes all properties on all scales (i.e. component, phase, and
+ * medium scales). It is used as an indexer for the PropertyArray of the
+ * materials. If a necessary property is not in the list, simply add it
+ * somewhere, but above the last entry 'number_of_property_enums'. Please note
+ * that any of these entries must appear also in the convert function
+ * 'convertStringToProperty'
+*/
 enum PropertyEnum : std::size_t
 {
-    molar_mass,
+    name,
+	molar_mass,
     critical_temperature,
     density,
     viscosity,
@@ -34,9 +91,19 @@ enum PropertyEnum : std::size_t
     phase_velocity,
     saturation,
     relative_permeability,
+	reference_density,
+	reference_temperature,
+	drhodT,
     number_of_property_enums
 };
 
+/**
+ * This function converts a string (e.g. a string from the configuration-tree)
+ * into one of the entries of the PropertyEnum enumerator. To avoid confusion,
+ * I suggest that the syntax of the properties in the input-files (i.e. the
+ * strings) have to be identical to the syntax of the entries in the
+ * enumerator.
+*/
 inline PropertyEnum convertStringToProperty (std::string const& inString) {
     if (boost::iequals(inString, "molar_mass")) return molar_mass;
     if (boost::iequals(inString, "critical_temperature")) return critical_temperature;
@@ -48,13 +115,13 @@ inline PropertyEnum convertStringToProperty (std::string const& inString) {
     if (boost::iequals(inString, "phase_velocity")) return phase_velocity;
     if (boost::iequals(inString, "saturation")) return saturation;
     if (boost::iequals(inString, "relative_permeability")) return relative_permeability;
-
+    if (boost::iequals(inString, "reference_density")) return reference_density;
+    if (boost::iequals(inString, "reference_temperature")) return reference_temperature;
+    if (boost::iequals(inString, "drhodT")) return drhodT;
     OGS_FATAL("The property name \"%s\" does not correspond to any known "
             "property", inString.c_str());
     return number_of_property_enums;  // to avoid the 'no return' warning
 }
-
-
 
 }   //MaterialPropertyLib
 
