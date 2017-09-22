936f92e6778a6a8076e13ca045c1d0f97d775803
Norbert Grunwald <n.g.boettcher@gmail.com>
2017-09-22 15:14:14 +0200

    added viscosity property for pure water

---
+++ MaterialLib/MPL/Properties/pViscosity_IAPWS.h
@@ -0,0 +1,51 @@
+/**
+ * \author Norbert Grunwald
+ * \date   Sep 21, 2017
+ * \brief
+ *
+ * \copyright
+ * Copyright (c) 2012-2017, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+#ifndef MATERIALLIB_MPL_PROPERTIES_PVISCOSITY_IAPWS_H_
+#define MATERIALLIB_MPL_PROPERTIES_PVISCOSITY_IAPWS_H_
+
+#include "../mpProperty.h"
+
+namespace MaterialPropertyLib
+{
+
+class Medium;
+class Phase;
+class Component;
+
+/**
+ *
+ *  \brief J.R. Cooper et. al: The International Association for the Properties
+ *        of Water and Steam. Berlin, Germany, September 2008.
+ */
+class ViscosityWaterIAPWS final : public Property
+{
+private:
+    Component* _component;
+    std::array<double, 4> _H;
+    std::array<std::array<double, 7>, 6> _h;
+
+public:
+    ViscosityWaterIAPWS (Medium*);
+    ViscosityWaterIAPWS (Phase*);
+    ViscosityWaterIAPWS (Component*);
+
+
+    /// This method overrides the base class implementation and
+    /// actually computes and sets the property _value.
+    PropertyDataType value (VariableArray const&) override;
+};
+
+}  // MaterialPropertyLib
+
+
+#endif /* MATERIALLIB_MPL_PROPERTIES_PVISCOSITY_IAPWS_H_ */
