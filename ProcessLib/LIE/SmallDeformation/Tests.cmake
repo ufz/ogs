d9549d92952f9d2e62f61025f9846aab3ff0cb3a
Dmitri Naumov <dmitri.naumov@ufz.de>
2017-10-04 15:12:45 +0200

    [T] Add ctests for penalty method.

--- ProcessLib/LIE/SmallDeformation/Tests.cmake
+++ ProcessLib/LIE/SmallDeformation/Tests.cmake
@@ -55,3 +55,33 @@ AddTest(
     two_joints_expected_pcs_0_ts_1_t_1.000000.vtu two_joints_pcs_0_ts_1_t_1.000000.vtu displacement_jump1 displacement_jump1
     two_joints_expected_pcs_0_ts_1_t_1.000000.vtu two_joints_pcs_0_ts_1_t_1.000000.vtu displacement_jump2 displacement_jump2
 )
+
+AddTest(
+    NAME LIE_M_single_joint_negative_aperture
+    PATH LIE/Mechanics
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS single_joint_negative_aperture.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    ABSTOL 1e-16 RELTOL 1e-16
+    DIFF_DATA
+    expected_single_joint_negative_aperture_pcs_0_ts_1_t_1.000000.vtu single_joint_negative_aperture_pcs_0_ts_1_t_1.000000.vtu displacement displacement
+    expected_single_joint_negative_aperture_pcs_0_ts_1_t_1.000000.vtu single_joint_negative_aperture_pcs_0_ts_1_t_1.000000.vtu displacement_jump1 displacement_jump1
+    expected_single_joint_negative_aperture_pcs_0_ts_1_t_1.000000.vtu single_joint_negative_aperture_pcs_0_ts_1_t_1.000000.vtu aperture aperture
+)
+
+AddTest(
+    NAME LIE_M_single_joint_displacement_controlled
+    PATH LIE/Mechanics
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS single_joint_displacement_controlled.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    ABSTOL 1e-16 RELTOL 1e-16
+    DIFF_DATA
+    expected_single_joint_displacement_controlled_pcs_0_ts_10_t_1.000000.vtu single_joint_displacement_controlled_pcs_0_ts_10_t_1.000000.vtu displacement displacement
+    expected_single_joint_displacement_controlled_pcs_0_ts_10_t_1.000000.vtu single_joint_displacement_controlled_pcs_0_ts_10_t_1.000000.vtu displacement_jump1 displacement_jump1
+    expected_single_joint_displacement_controlled_pcs_0_ts_10_t_1.000000.vtu single_joint_displacement_controlled_pcs_0_ts_10_t_1.000000.vtu aperture aperture
+)
