d8a1363af35659b9080e544c848d640b297373bb
Dmitri Naumov <dmitri.naumov@ufz.de>
2017-10-04 18:39:38 +0200

    [T] Fracture: add a linear elastic MohrCoulomb.

--- Tests/MaterialLib/TestFractureModels.cpp
+++ Tests/MaterialLib/TestFractureModels.cpp
@@ -71,6 +71,54 @@ TEST(MaterialLib_Fracture, LinearElasticIsotropic)
     ASSERT_NEAR(0, C(1,1), eps_C);
 }
 
+TEST(MaterialLib_Fracture, MohrCoulomb2D_elastic)
+{
+    ProcessLib::ConstantParameter<double> const kn("", 1e11);
+    ProcessLib::ConstantParameter<double> const ks("", 1e9);
+    ProcessLib::ConstantParameter<double> const phi("", 15);
+    ProcessLib::ConstantParameter<double> const psi("", 5);
+    ProcessLib::ConstantParameter<double> const c("", 3e16);
+    MohrCoulomb<2>::MaterialProperties const mp{kn, ks, phi, psi, c};
+
+    double const aperture0 = 1;
+    double const penalty_aperture_cutoff = aperture0;
+    bool const tension_cutoff = true;
+    MohrCoulomb<2> fractureModel{penalty_aperture_cutoff, tension_cutoff, mp};
+    std::unique_ptr<FractureModelBase<2>::MaterialStateVariables> state(
+        fractureModel.createMaterialStateVariables());
+
+    Eigen::Vector2d const w_prev = Eigen::Vector2d::Zero();
+    Eigen::Vector2d const sigma0 = Eigen::Vector2d::Zero();
+    Eigen::Vector2d const sigma_prev = sigma0;
+    Eigen::Vector2d w(-1e-5, -1e-5);
+
+    // Result vectors, not initialized.
+    Eigen::Vector2d sigma;
+    Eigen::Matrix2d C;
+
+    ProcessLib::SpatialPosition x;
+    fractureModel.computeConstitutiveRelation(0, x, aperture0, sigma0, w_prev,
+                                              w, sigma_prev, sigma, C, *state);
+
+    ASSERT_NEAR(-1e4, sigma[0], eps_sigma);
+    ASSERT_NEAR(-1e6, sigma[1], eps_sigma);
+    ASSERT_NEAR(1e9, C(0, 0), eps_C);
+    ASSERT_NEAR(0, C(0, 1), eps_C);
+    ASSERT_NEAR(0, C(1, 0), eps_C);
+    ASSERT_NEAR(1e11, C(1, 1), eps_C);
+
+    w << -1e-5, 1e-5;
+    fractureModel.computeConstitutiveRelation(0, x, aperture0, sigma0, w_prev,
+                                              w, sigma_prev, sigma, C, *state);
+
+    ASSERT_NEAR(0, sigma[0], eps_sigma);
+    ASSERT_NEAR(0, sigma[1], eps_sigma);
+    ASSERT_NEAR(0, C(0, 0), eps_C);
+    ASSERT_NEAR(0, C(0, 1), eps_C);
+    ASSERT_NEAR(0, C(1, 0), eps_C);
+    ASSERT_NEAR(0, C(1, 1), eps_C);
+}
+
 TEST(MaterialLib_Fracture, MohrCoulomb2D_negative_t)
 {
     ProcessLib::ConstantParameter<double> const kn("", 50e9);
