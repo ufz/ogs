ufz/ogs

commit 8e55d6b07da83213f12397d59d6cd9bdb4fab0ce

[Jenkins] Fix git error when checking for tag.

Jenkinsfile
@@ -3,12 +3,16 @@
 
 def builders = [:]
 def helper = new ogs.helper()
+def tag = ""
 
 timestamps {
 
 builders['gcc'] = {
     node('docker') {
-        dir('ogs') { checkoutWithTags() }
+        dir('ogs') {
+            checkoutWithTags()
+            tag = helper.getTag()
+        }
         load 'ogs/scripts/jenkins/gcc.groovy'
     }
 }
@@ -77,10 +81,7 @@ catch (err) {
     }
 }
 
-def tag = ""
 node('master') {
-    checkoutWithTags()
-    tag = helper.getTag()
     step([$class: 'LogParserPublisher',
         failBuildOnError: true,
         projectRulePath: "scripts/jenkins/all-log-parser.rules",

commit 5871d5aa105684b8bfda177ff3990c079776a62e

[Jenkins] Fix log parser publisher.
Error introduced in 8e55d6b.

Jenkinsfile
@@ -75,6 +75,7 @@ catch (err) {
 }
 
 node('master') {
+    checkoutWithTags()
     step([$class: 'LogParserPublisher',
         failBuildOnError: true,
         projectRulePath: "scripts/jenkins/all-log-parser.rules",