44f623c1db888713f7c3c0d90a8a66b07fbf90bc
Lars Bilke <lars.bilke@ufz.de>
2017-10-17 13:34:24 +0200

    Two whitespaces not allowed.

--- scripts/cmake/SubmoduleSetup.cmake
+++ scripts/cmake/SubmoduleSetup.cmake
@@ -45,11 +45,11 @@ foreach(SUBMODULE ${REQUIRED_SUBMODULES})
     if(UNINITIALIZED)
         message(STATUS "Initializing submodule ${SUBMODULE}")
         if(${SUBMODULE} STREQUAL "Tests/Data")
-            set(DEPTH "--depth 10")
+            set(DEPTH "--depth 10 ")
         endif()
         execute_process(
             COMMAND "${GIT_TOOL_PATH} "
-                "submodule update --init --recursive ${DEPTH} ${SUBMODULE}"
+                "submodule update --init --recursive ${DEPTH}${SUBMODULE}"
             WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
         )
         return()
