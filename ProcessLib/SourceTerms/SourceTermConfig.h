57205b44c69ce21aae6ca7bf472617543f63ba3c
Thomas Fischer <thomas.fischer@ufz.de>
2017-10-24 06:33:16 +0200

    [PL/SourceTerms] Impl. of class SourceTermConfig.

---
+++ ProcessLib/SourceTerms/SourceTermConfig.h
@@ -0,0 +1,40 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2017, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#pragma once
+
+#include "BaseLib/ConfigTree.h"
+#include "GeoLib/GEOObjects.h"
+
+namespace ProcessLib
+{
+struct SourceTermConfig final
+{
+    SourceTermConfig(BaseLib::ConfigTree&& config_,
+                     GeoLib::GeoObject const& geometry_,
+                     boost::optional<int> const component_id_)
+        : config(std::move(config_)),
+          geometry(geometry_),
+          component_id(component_id_)
+    {
+    }
+
+    SourceTermConfig(SourceTermConfig&& other)
+        : config(std::move(other.config)),
+          geometry(other.geometry),
+          component_id(other.component_id)
+    {
+    }
+
+    BaseLib::ConfigTree config;
+    GeoLib::GeoObject const& geometry;
+    boost::optional<int> const component_id;
+};
+
+}  // ProcessLib
