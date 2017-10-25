4c88ef16c4f34003f07f578e5b4a21611d0620d1
Wenqing Wang <wenqing.wang@ufz.de>
2017-10-25 10:57:24 +0200

    [Test] Added a new test to solve the HT monolithic test with the staggered scheme
    and removed the tests for the staggered scheme from LiquidFlow temporarily.

--- ProcessLib/HT/Tests.cmake
+++ ProcessLib/HT/Tests.cmake
@@ -11,7 +11,21 @@ AddTest(
     VIS ConstViscosityThermalConvection_pcs_0_ts_149_t_50000000000.000000.vtu
 )
 
-# MPI tests
+AddTest(
+    NAME LARGE_2D_ThermalConvection_constviscosityStaggeredScheme
+    PATH Parabolic/HT/StaggeredCoupling/ConstViscosity
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS square_5500x5500_staggered_scheme.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
+    DIFF_DATA
+    square_5500x5500.vtu ConstViscosityThermalConvection_pcs_1_ts_149_t_50000000000.000000.vtu T_ref T 1e-16  1.e-16
+    square_5500x5500.vtu ConstViscosityThermalConvection_pcs_1_ts_149_t_50000000000.000000.vtu p_ref p  1e-16  1.e-16
+    VIS ConstViscosityThermalConvection_pcs_1_ts_149_t_50000000000.000000.vtu
+)
+
+# MPI/PETSc tests
 AddTest(
     NAME Parallel_LARGE_2D_ThermalConvection_constviscosity
     PATH Parabolic/HT/ConstViscosity
@@ -29,4 +43,18 @@ AddTest(
     ConstViscosityThermalConvection_pcs_0_ts_149_t_50000000000_000000_1.vtu ConstViscosityThermalConvection_pcs_0_ts_149_t_50000000000_000000_1.vtu T T 1e-15 1e-14
     ConstViscosityThermalConvection_pcs_0_ts_149_t_50000000000_000000_2.vtu ConstViscosityThermalConvection_pcs_0_ts_149_t_50000000000_000000_2.vtu T T 1e-15 1e-14
     ConstViscosityThermalConvection_pcs_0_ts_149_t_50000000000_000000_3.vtu ConstViscosityThermalConvection_pcs_0_ts_149_t_50000000000_000000_3.vtu T T 1e-15 1e-14
+)
+
+AddTest(
+    NAME LARGE_2D_ThermalConvection_constviscosityStaggeredScheme
+    PATH Parabolic/HT/StaggeredCoupling/ConstViscosity
+    EXECUTABLE_ARGS square_5500x5500_staggered_scheme.prj
+    WRAPPER mpirun
+    WRAPPER_ARGS -np 1
+    TESTER vtkdiff
+    REQUIREMENTS OGS_USE_MPI
+    DIFF_DATA
+    square_5500x5500.vtu ConstViscosityThermalConvection_pcs_1_ts_149_t_50000000000_000000_0.vtu T_ref T  1e-14  1.e-14
+    square_5500x5500.vtu ConstViscosityThermalConvection_pcs_1_ts_149_t_50000000000_000000_0.vtu p_ref p  1e-14  1.e-14
+    VIS ConstViscosityThermalConvection_pcs_1_ts_149_t_50000000000.000000_0.vtu
 )
