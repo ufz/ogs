e1198be8323bae639b710b5fefff9965c1ecda63
Dmitri Naumov <dmitri.naumov@ufz.de>
2017-12-05 12:32:03 +0100

    [MatL] Fracture permeability models. CubicLaw.
    This adds infrastructure for other permeability models, but for now only
    the existing/implemented cubic law is put.

---
+++ MaterialLib/FractureModels/Permeability/CubicLaw.cpp
@@ -0,0 +1,29 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2019, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#include "CubicLaw.h"
+
+namespace MaterialLib::Fracture::Permeability
+{
+double CubicLaw::permeability(PermeabilityState const* const /*state*/,
+                              double const /*aperture0*/,
+                              double const aperture_m) const
+{
+    return aperture_m * aperture_m / 12;
+}
+
+double CubicLaw::dpermeability_daperture(
+    PermeabilityState const* const /*state*/,
+    double const /*aperture0*/,
+    double const aperture_m) const
+{
+    return aperture_m / 6;
+}
+}  // namespace MaterialLib::Fracture::Permeability
