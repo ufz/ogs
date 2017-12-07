53c6a3d2592f579467b476703a3bb1225cbd18f2
Wenqing Wang <wenqing.wang@ufz.de>
2017-12-07 17:15:17 +0100

    [Assm] Overloaded assembleWithJacobianAndCoupling in AnalyticalJacobianAssembler

--- ProcessLib/AnalyticalJacobianAssembler.cpp
+++ ProcessLib/AnalyticalJacobianAssembler.cpp
@@ -8,6 +8,7 @@
  */
 
 #include "AnalyticalJacobianAssembler.h"
+#include "CoupledSolutionsForStaggeredScheme.h"
 #include "LocalAssemblerInterface.h"
 
 namespace ProcessLib
@@ -17,11 +18,24 @@ void AnalyticalJacobianAssembler::assembleWithJacobian(
     std::vector<double> const& local_x, std::vector<double> const& local_xdot,
     const double dxdot_dx, const double dx_dx,
     std::vector<double>& local_M_data, std::vector<double>& local_K_data,
-    std::vector<double>& local_b_data,
-    std::vector<double>& local_Jac_data)
+    std::vector<double>& local_b_data, std::vector<double>& local_Jac_data)
 {
     local_assembler.assembleWithJacobian(t, local_x, local_xdot, dxdot_dx,
                                          dx_dx, local_M_data, local_K_data,
                                          local_b_data, local_Jac_data);
 }
+
+void AnalyticalJacobianAssembler::assembleWithJacobianAndCoupling(
+    LocalAssemblerInterface& local_assembler, double const t,
+    std::vector<double> const& local_xdot, const double dxdot_dx,
+    const double dx_dx, std::vector<double>& local_M_data,
+    std::vector<double>& local_K_data, std::vector<double>& local_b_data,
+    std::vector<double>& local_Jac_data,
+    LocalCoupledSolutions const& local_coupled_solutions)
+{
+    local_assembler.assembleWithJacobianAndCoupling(
+        t, local_xdot, dxdot_dx, dx_dx, local_M_data, local_K_data,
+        local_b_data, local_Jac_data, local_coupled_solutions);
+}
+
 }  // ProcessLib
