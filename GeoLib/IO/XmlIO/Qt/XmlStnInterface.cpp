67c1b1158becc1091fbb71caf06ecd8ff957efef
rinkk <karsten.rink@ufz.de>
2018-01-15 13:51:42 +0100

    changed write format for vector data to ensure decimal output

--- GeoLib/IO/XmlIO/Qt/XmlStnInterface.cpp
+++ GeoLib/IO/XmlIO/Qt/XmlStnInterface.cpp
@@ -242,9 +242,12 @@ bool XmlStnInterface::write()
         QString stationType =  (isBorehole) ? "borehole" : "station";
         QDomElement stationTag = doc.createElement(stationType);
         stationTag.setAttribute( "id", QString::number(i) );
-        stationTag.setAttribute( "x",  QString::number((*(*stations)[i])[0], 'f') );
-        stationTag.setAttribute( "y",  QString::number((*(*stations)[i])[1], 'f') );
-        stationTag.setAttribute( "z",  QString::number((*(*stations)[i])[2], 'f') );
+        stationTag.setAttribute( "x",  QString::number((*(*stations)[i])[0], 'f',
+            std::numeric_limits<double>::digits10));
+        stationTag.setAttribute( "y",  QString::number((*(*stations)[i])[1], 'f',
+            std::numeric_limits<double>::digits10));
+        stationTag.setAttribute( "z",  QString::number((*(*stations)[i])[2], 'f',
+            std::numeric_limits<double>::digits10));
         stationsTag.appendChild(stationTag);
 
         QDomElement stationNameTag = doc.createElement("name");
