2c402d08f30bc17a67663f34dcc8ebee4d79fa2d
Dmitri Naumov <dmitri.naumov@ufz.de>
2018-02-01 13:01:57 +0100

    [PL] TimeLoop: Move creation of process data.

---
+++ ProcessLib/CreateProcessData.h
@@ -0,0 +1,22 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2018, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#pragma once
+
+#include "ProcessData.h"
+
+namespace ProcessLib
+{
+std::vector<std::unique_ptr<SingleProcessData>> createPerProcessData(
+    BaseLib::ConfigTree const& config,
+    const std::map<std::string, std::unique_ptr<Process>>& processes,
+    std::map<std::string, std::unique_ptr<NumLib::NonlinearSolverBase>> const&
+        nonlinear_solvers);
+
+}  // namespace ProcessLib
