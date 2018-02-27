0db9b4f230eaf74d0e518e2946ec263ec81096c5
Lars Bilke <lars.bilke@ufz.de>
2018-02-27 17:06:05 +0100

    [web] Tailwind configured.

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,56 +1,27 @@
-const path = require('path');
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const parts = require('./webpack.parts');
-const pkg = require('./package.json');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const path = require('path')
 
-const commonConfig = merge([
-  {
-    entry: {
-      app: pkg.paths.src.js + 'app.js', // PATHS.app,
-    },
-    output: {
-      path: __dirname + "/" + pkg.paths.dist.js,
-      filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-              test: require.resolve("./src/js/app.js"),
-              loader: "expose-loader?MyWebApp"
-            }
-        ].concat(module.exports = parts.loaders()),
-    },
-    resolve: {
-    extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js', '.jsx'],
-    modules: [
-      path.resolve(__dirname, 'node_modules')
-    ],
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'styles.css',
   },
-    // postcss: [
-      // require('autoprefixer')({ browsers: ['last 2 versions'] }),
-    // ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            { loader: 'css-loader', options: { importLoaders: 1 } },
+            'postcss-loader'
+          ]
+        })
+      }
+    ]
   },
-  // parts.lintJavaScript({ include: pkg.paths.src.js + '/app.js' }),
-]);
-
-const productionConfig = merge([
-  parts.productionEnv(),
-  parts.minifyJavaScript({ useSourceMap: true })
-]);
-
-const developmentConfig = merge([
-  {
-    // plugins: [
-      // new webpack.NamedModulesPlugin(),
-    // ],
-  }
-]);
-
-module.exports = function(env) {
-  if (env === 'production') {
-    return merge(commonConfig, productionConfig);
-  }
-
-  return merge(commonConfig, developmentConfig);
-};
+  plugins: [
+    new ExtractTextPlugin('styles.css')
+  ]
+}
