6684d6270a9dd94e361305a4d17539bf70276656
HBShaoUFZ <haibing.shao@ufz.de>
2018-03-01 12:15:19 +0100

    correct the file name.

--- ProcessLib/LiquidFlow/Tests.cmake
+++ ProcessLib/LiquidFlow/Tests.cmake
@@ -55,7 +55,7 @@ AddTest(
     REQUIREMENTS NOT OGS_USE_MPI
     DIFF_DATA
     standard_solution_buildup_test_pcs_0_ts_107_t_424800.000000.vtu buildup_test_pcs_0_ts_107_t_424800.000000.vtu pressure pressure 1e-12 0.0
-	standard_solution_buildup_test_pcs_0_ts_107_t_720000.000000.vtu buildup_test_pcs_0_ts_107_t_720000.000000.vtu pressure pressure 1e-12 0.0
+	standard_solution_buildup_test_pcs_0_ts_211_t_720000.000000.vtu buildup_test_pcs_0_ts_211_t_720000.000000.vtu pressure pressure 1e-12 0.0
 )
 
 AddTest(
