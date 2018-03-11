76def602677d4f636d2bdc9883af2d0268079853
xingyuanmiao <miaoxingyuan@live.cn>
2018-03-11 10:04:53 +0100

    added ctest.

---
+++ ProcessLib/ThermoMechanicalPhaseField/Tests.cmake
@@ -0,0 +1,27 @@
+AddTest(
+    NAME ThermoMechanicalPhaseField_3D_Unconfined_Compression
+    PATH ThermoMechanicalPhaseField
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS cube_1e0.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
+    DIFF_DATA
+    expected_cube_1e0_tm_pcs_2_ts_1_t_100.000000.vtu cube_1e0_tm_pcs_2_ts_1_t_100.000000.vtu displacement displacement 1e-6 1e-6
+    expected_cube_1e0_tm_pcs_2_ts_1_t_100.000000.vtu cube_1e0_tm_pcs_2_ts_1_t_100.000000.vtu temperature temperature 1e-6 1e-6
+    analytical_cube_1e0_tm_pcs_2_ts_1_t_100.000000.vtu cube_1e0_tm_pcs_2_ts_1_t_100.000000.vtu phasefield phasefield 1e-6 1e-6
+   )
+
+AddTest(
+    NAME ThermoMechanicalPhaseField_3D_beam_LARGE
+    PATH ThermoMechanicalPhaseField
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS beam3d.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
+    DIFF_DATA
+    expected_beam3d_pcs_2_ts_1_t_10.000000.vtu beam3d_pcs_2_ts_1_t_10.000000.vtu displacement displacement 1e-5 0
+    expected_beam3d_pcs_2_ts_1_t_10.000000.vtu beam3d_pcs_2_ts_1_t_10.000000.vtu phasefield phasefield 1e-6 0
+    expected_beam3d_pcs_2_ts_1_t_10.000000.vtu beam3d_pcs_2_ts_1_t_10.000000.vtu temperature temperature 1e-6 0
+   )
