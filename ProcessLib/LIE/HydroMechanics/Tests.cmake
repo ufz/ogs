c0edfc46690be54d9a9adf42430c80294a9c1bbd
Dmitri Naumov <dmitri.naumov@ufz.de>
2018-03-21 18:45:59 +0100

    [T] LIE/HM; TaskB_slip

--- ProcessLib/LIE/HydroMechanics/Tests.cmake
+++ ProcessLib/LIE/HydroMechanics/Tests.cmake
@@ -200,3 +200,29 @@ AddTest(
     expected_single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu stress_yy stress_yy 1e-12 1e-15
     expected_single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu velocity velocity 1e-15 1e-15
 )
+
+AddTest(
+    NAME LIE_HM_TaskB_slip
+    PATH LIE/HydroMechanics
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS TaskB_slip.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
+    DIFF_DATA
+    TaskB_slip_pcs_0_ts_*.vtu pressure pressure 1e-16 1e-16
+    TaskB_slip_pcs_0_ts_*.vtu pressure_interpolated pressure_interpolated 1e-16 1e-16
+    TaskB_slip_pcs_0_ts_*.vtu displacement displacement 1e-16 0
+    TaskB_slip_pcs_0_ts_*.vtu displacement_jump1 displacement_jump1 1e-16 0
+    TaskB_slip_pcs_0_ts_*.vtu nodal_w nodal_w 1e-16 0
+    TaskB_slip_pcs_0_ts_*.vtu nodal_aperture nodal_aperture 1e-16 0
+    TaskB_slip_pcs_0_ts_*.vtu strain_xx strain_xx 1e-16 0
+    TaskB_slip_pcs_0_ts_*.vtu strain_yy strain_yy 1e-16 0
+    TaskB_slip_pcs_0_ts_*.vtu strain_xy strain_xy 1e-16 0
+    TaskB_slip_pcs_0_ts_*.vtu strain_zz strain_zz 1e-16 0
+    TaskB_slip_pcs_0_ts_*.vtu stress_xx stress_xx 1e-16 0
+    TaskB_slip_pcs_0_ts_*.vtu stress_yy stress_yy 1e-16 0
+    TaskB_slip_pcs_0_ts_*.vtu stress_xy stress_xy 1e-16 0
+    TaskB_slip_pcs_0_ts_*.vtu stress_zz stress_zz 1e-16 0
+    TaskB_slip_pcs_0_ts_*.vtu velocity velocity 1e-16 0
+)
