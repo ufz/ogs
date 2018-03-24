39b2e7fa3431ca29af57ded0f93dcd19abada68f
xingyuanmiao <miaoxingyuan@live.cn>
2018-03-24 15:21:33 +0100

    added a mechanical phasefield test to compare with a homogeneous temperature test.

--- ProcessLib/ThermoMechanicalPhaseField/Tests.cmake
+++ ProcessLib/ThermoMechanicalPhaseField/Tests.cmake
@@ -10,6 +10,7 @@ AddTest(
     expected_cube_1e0_tm_pcs_2_ts_1_t_100.000000.vtu cube_1e0_tm_pcs_2_ts_1_t_100.000000.vtu displacement displacement 1e-6 1e-6
     expected_cube_1e0_tm_pcs_2_ts_1_t_100.000000.vtu cube_1e0_tm_pcs_2_ts_1_t_100.000000.vtu temperature temperature 1e-6 1e-6
     analytical_cube_1e0_tm_pcs_2_ts_1_t_100.000000.vtu cube_1e0_tm_pcs_2_ts_1_t_100.000000.vtu phasefield phasefield 1e-5 1e-5
+    mechanical_cube_1e0_pcs_1_ts_1_t_100.000000.vtu cube_1e0_tm_pcs_2_ts_1_t_100.000000.vtu phasefield phasefield 1e-5 1e-5
    )
 
 AddTest(
