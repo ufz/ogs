43133ed781acc35658ed675a007edd4ea6d6cf22
Dmitri Naumov <dmitri.naumov@ufz.de>
2018-05-14 15:18:30 +0200

    [T] Add Padilla_NaCl1 test on second order mesh.
    This is for testing of the Neumann BC together with Line3 elements.

--- ProcessLib/RichardsComponentTransport/Tests.cmake
+++ ProcessLib/RichardsComponentTransport/Tests.cmake
@@ -12,6 +12,19 @@ AddTest(
 )
 
 AddTest(
+    NAME RichardsComponentTransport_1D_Padilla_NaCl1_quadratic
+    PATH Parabolic/RichardsComponentTransport/Padilla/Padilla_NaCl1
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS Padilla_NaCl1_quadratic.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    DIFF_DATA
+    GLOB Padilla_NaCl1_quadratic_pcs_0_ts_*.vtu concentration concentration 1e-2 1e-10
+    GLOB Padilla_NaCl1_quadratic_pcs_0_ts_*.vtu pressure pressure 1e-2 1e-10
+)
+
+AddTest(
     NAME RichardsComponentTransport_1D_Padilla_NaCl6
     PATH Parabolic/RichardsComponentTransport/Padilla/Padilla_NaCl6
     EXECUTABLE ogs
