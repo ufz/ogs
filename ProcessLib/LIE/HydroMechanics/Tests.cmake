79828f6105b60699217c550ada01a23f5b8780ca
Dmitri Naumov <github@naumov.de>
2018-05-16 23:49:33 +0200

    [T] LIE/HM: 3 compartment tests.
    The flow through the fracture is tested; the matrix
    flow is deactivated and mechanically everything is very
    stiff.

--- ProcessLib/LIE/HydroMechanics/Tests.cmake
+++ ProcessLib/LIE/HydroMechanics/Tests.cmake
@@ -74,3 +74,74 @@ AddTest(
     expected_TaskB_pcs_0_ts_4_t_18.000000.vtu TaskB_pcs_0_ts_4_t_18.000000.vtu stress_yy stress_yy 1e-12 1e-12
     expected_TaskB_pcs_0_ts_4_t_18.000000.vtu TaskB_pcs_0_ts_4_t_18.000000.vtu velocity velocity 1e-12 1e-12
 )
+
+AddTest(
+    NAME LIE_HM_single_fracture_3compartments_flow
+    PATH LIE/HydroMechanics
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS single_fracture_3compartments_flow.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
+    DIFF_DATA
+    expected_single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu pressure pressure 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu pressure_interpolated pressure_interpolated 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu displacement displacement 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu displacement_jump1 displacement_jump1 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu nodal_w nodal_w 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu nodal_aperture nodal_aperture 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu strain_xx strain_xx 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu strain_yy strain_yy 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu strain_xy strain_xy 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu stress_xx stress_xx 1e-12 1e-15
+    expected_single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu stress_yy stress_yy 1e-12 1e-15
+    expected_single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_pcs_0_ts_1108_t_10000.000000.vtu velocity velocity 1e-15 1e-15
+)
+
+AddTest(
+    NAME LIE_HM_single_fracture_3compartments_flow_linear_aperture0
+    PATH LIE/HydroMechanics
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS single_fracture_3compartments_flow_linear_aperture0.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
+    DIFF_DATA
+    expected_single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu pressure pressure 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu pressure_interpolated pressure_interpolated 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu displacement displacement 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu displacement_jump1 displacement_jump1 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu nodal_w nodal_w 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu nodal_aperture nodal_aperture 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu strain_xx strain_xx 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu strain_yy strain_yy 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu strain_xy strain_xy 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu stress_xx stress_xx 1e-12 1e-15
+    expected_single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu stress_yy stress_yy 1e-12 1e-15
+    expected_single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_pcs_0_ts_1108_t_10000.000000.vtu velocity velocity 1e-15 1e-15
+)
+
+# Same as the LIE_HM_single_fracture_3compartments_flow_linear_aperture0 but with
+# aperture0 defined on the elements (and discontinuous on the nodes).
+AddTest(
+    NAME LIE_HM_single_fracture_3compartments_flow_linear_aperture0_e
+    PATH LIE/HydroMechanics
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS single_fracture_3compartments_flow_linear_aperture0_e.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
+    DIFF_DATA
+    expected_single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu pressure pressure 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu pressure_interpolated pressure_interpolated 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu displacement displacement 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu displacement_jump1 displacement_jump1 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu nodal_w nodal_w 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu nodal_aperture nodal_aperture 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu strain_xx strain_xx 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu strain_yy strain_yy 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu strain_xy strain_xy 1e-15 1e-15
+    expected_single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu stress_xx stress_xx 1e-12 1e-15
+    expected_single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu stress_yy stress_yy 1e-12 1e-15
+    expected_single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu single_fracture_3compartments_flow_linear_aperture0_e_pcs_0_ts_1108_t_10000.000000.vtu velocity velocity 1e-15 1e-15
+)
