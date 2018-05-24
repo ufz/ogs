13e47c792253b42768ea8fd5123597e3ccb02635
Thomas Fischer <thomas.fischer@ufz.de>
2018-05-24 09:06:55 +0200

    [PL/HT] ConstViscosity: Test pressure and darcy vel.
    Also relax the tolerance for temperature in the test.

--- ProcessLib/HT/Tests.cmake
+++ ProcessLib/HT/Tests.cmake
@@ -1,5 +1,5 @@
 AddTest(
-    NAME LARGE_2D_ThermalConvection_constviscosity
+    NAME LARGE_2D_ThermalConvection_constviscosityMonolithic
     PATH Parabolic/HT/ConstViscosity
     EXECUTABLE ogs
     EXECUTABLE_ARGS square_5500x5500.prj
@@ -7,7 +7,9 @@ AddTest(
     TESTER vtkdiff
     REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
     DIFF_DATA
-    ThermalConvection_const_viscosity_expected.vtu ConstViscosityThermalConvection_pcs_0_ts_149_t_50000000000.000000.vtu temperature T 1e-10 1e-16
+    ThermalConvection_const_viscosity_expected.vtu ConstViscosityThermalConvection_pcs_0_ts_149_t_50000000000.000000.vtu T T 1e-7 1e-9
+    ThermalConvection_const_viscosity_expected.vtu ConstViscosityThermalConvection_pcs_0_ts_149_t_50000000000.000000.vtu p p 1e-9 1e-3
+    ThermalConvection_const_viscosity_expected.vtu ConstViscosityThermalConvection_pcs_0_ts_149_t_50000000000.000000.vtu darcy_velocity darcy_velocity 1e-10 1e-16
     VIS ConstViscosityThermalConvection_pcs_0_ts_149_t_50000000000.000000.vtu
 )
 
