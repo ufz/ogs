e9ea588429d4cc92a3d27618d596a4798d8187f6
Lars Bilke <lars.bilke@ufz.de>
2018-07-04 10:31:49 +0200

    [Jenkins] Use new VTK 8.1.1 eve module.

--- scripts/env/envinf1/cli.sh
+++ scripts/env/envinf1/cli.sh
@@ -1,5 +1,4 @@
-module () { eval `/usr/local/modules/3.2.10-1/Modules/3.2.10/bin/modulecmd sh $*`; }
-export MODULEPATH=$MODULEPATH:/global/apps/modulefiles
+module use /global/apps/modulefiles
 
 module load cmake
 module load gcc/6.2.0-1
@@ -9,7 +8,7 @@ module load git
 # Libraries
 module load boost/1.62.0-1
 module load eigen/3.2.9-1-cmake
-module load vtk/7.1.0_gcc-6.2.0_openmpi-1.8.8
+module load vtk/8.1.1_gcc-6.2.0_openmpi-1.8.8
 
 # Tools
 module load coreutils/8.21-1
