9a5bc7be6381d005078bcb9c501ce01a7b8f89ef
Lars Bilke <lars.bilke@ufz.de>
2018-07-26 10:10:43 +0200

    [CMake] Fixed OpenMP linking.

--- scripts/cmake/Find.cmake
+++ scripts/cmake/Find.cmake
@@ -86,10 +86,11 @@ if(OGS_NO_EXTERNAL_LIBS)
     return()
 endif() # OGS_NO_EXTERNAL_LIBS
 
-find_package(OpenMP QUIET)
+find_package(OpenMP)
 if(OPENMP_FOUND)
-    add_compile_options(${OpenMP_CXX_FLAGS})
-    message(STATUS "OpenMP enabled.")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${OpenMP_EXE_LINKER_FLAGS}")
 endif()
 
 find_package(Metis QUIET)
