9acf2db7f5b8321ecff468bc40044681e441f67a
Dmitry Yu. Naumov <github@naumov.de>
2018-08-16 13:46:28 +0200

    cmake; Separate compile flags with ; not with ' '.

--- scripts/cmake/ClangSanitizer.cmake
+++ scripts/cmake/ClangSanitizer.cmake
@@ -11,7 +11,7 @@ if(OGS_ADDRESS_SANITIZER)
 endif()
 
 if(OGS_UNDEFINED_BEHAVIOR_SANITIZER)
-    set(SANITIZE_FLAG_VALUE "${SANITIZE_FLAG_VALUE},undefined,integer -fsanitize-blacklist=${CMAKE_CURRENT_SOURCE_DIR}/scripts/test/clang_sanitizer_blacklist.txt")
+    set(SANITIZE_FLAG_VALUE "${SANITIZE_FLAG_VALUE},undefined,integer;-fsanitize-blacklist=${CMAKE_CURRENT_SOURCE_DIR}/scripts/test/clang_sanitizer_blacklist.txt")
 endif()
 
 if(DEFINED SANITIZE_FLAG_VALUE)
