778dd2991ff7284e737a4b6510e48f6678d478cf
Norihiro Watanabe <norihiro.watanabe@aist.go.jp>
2018-10-09 05:59:57 +0900

    [LIE] extend levelset for interesections

--- ProcessLib/LIE/Common/LevelSetFunction.h
+++ ProcessLib/LIE/Common/LevelSetFunction.h
@@ -8,20 +8,49 @@
 
 #pragma once
 
+#include <unordered_map>
+#include <vector>
+
+#include <Eigen/Eigen>
+
 namespace ProcessLib
 {
 namespace LIE
 {
 struct FractureProperty;
+struct JunctionProperty;
 
-/// calculate the level set function
-/// \f$ \psi(\mathbf{x}) = H(|\mathbf{x} - \mathbf{x_d}|
-/// \mathrm{sign}[\mathbf{n_d} \cdot (\mathbf{x}-\mathbf{x_d}]) \f$ where
-/// \f$H(u)\f$ is the Heaviside step function, \f$\mathbf{x_d}\f$ is a point on
-/// the fracture plane, and \f$\mathbf{n_d}\f$ is the normal vector of a
-/// fracture plane
-double calculateLevelSetFunction(FractureProperty const& fracture_property,
-                                 double const* x);
+/// calculate the enrichment function for displacements at the given point
+/// Remarks:
+/// * branch/junction intersections of two fractures are supported in 2D
+///
+/// @parm frac_props fracture properties
+/// @parm junction_props junction properties
+/// @parm fracID_to_local a mapping table from a fracture ID to a local index in frac_props
+/// @parm x evaluating point coordiates
+/// @return a vector of enrichment values for displacements
+std::vector<double> u_global_enrichments(
+	std::vector<FractureProperty*> const& frac_props,
+	std::vector<JunctionProperty*> const& junction_props,
+	std::unordered_map<int,int> const& fracID_to_local,
+	Eigen::Vector3d const& x);
+
+/// calculate the enrichment function for fracture relative displacements
+/// Remarks:
+/// * branch/junction intersections of two fractures are supported in 2D
+///
+/// @parm this_fracID the fracture ID
+/// @parm frac_props fracture properties
+/// @parm junction_props junction properties
+/// @parm fracID_to_local a mapping table from a fracture ID to a local index in frac_props
+/// @parm x evaluating point coordiates
+/// @return a vector of enrichment values for fracture relative displacements
+std::vector<double> du_global_enrichments(
+	std::size_t this_fracID,
+	std::vector<FractureProperty*> const& frac_props,
+	std::vector<JunctionProperty*> const& junction_props,
+	std::unordered_map<int,int> const& fracID_to_local,
+	Eigen::Vector3d const& x);
 
 }  // namespace LIE
 }  // namespace ProcessLib
