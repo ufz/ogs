46cc9d2b19b6a40f625df508f754020a33aa20e6
Dmitri Naumov <dmitri.naumov@ufz.de>
2018-12-10 17:22:40 +0100

    [PL] LIE; Fix doxygen errors in LevelSetFunction.

--- ProcessLib/LIE/Common/LevelSetFunction.h
+++ ProcessLib/LIE/Common/LevelSetFunction.h
@@ -24,11 +24,11 @@ struct JunctionProperty;
 /// Remarks:
 /// * branch/junction intersections of two fractures are supported in 2D
 ///
-/// @parm frac_props fracture properties
-/// @parm junction_props junction properties
-/// @parm fracID_to_local a mapping table from a fracture ID to a local index in
-/// frac_props
-/// @parm x evaluating point coordiates
+/// @param frac_props fracture properties
+/// @param junction_props junction properties
+/// @param fracID_to_local a mapping table from a fracture ID to a local index
+/// in frac_props
+/// @param x evaluating point coordiates
 /// @return a vector of enrichment values for displacements
 std::vector<double> u_global_enrichments(
     std::vector<FractureProperty*> const& frac_props,
@@ -40,12 +40,12 @@ std::vector<double> u_global_enrichments(
 /// Remarks:
 /// * branch/junction intersections of two fractures are supported in 2D
 ///
-/// @parm this_fracID the fracture ID
-/// @parm frac_props fracture properties
-/// @parm junction_props junction properties
-/// @parm fracID_to_local a mapping table from a fracture ID to a local index in
-/// frac_props
-/// @parm x evaluating point coordiates
+/// @param this_fracID the fracture ID
+/// @param frac_props fracture properties
+/// @param junction_props junction properties
+/// @param fracID_to_local a mapping table from a fracture ID to a local index
+/// in frac_props
+/// @param x evaluating point coordiates
 /// @return a vector of enrichment values for fracture relative displacements
 std::vector<double> du_global_enrichments(
     std::size_t this_fracID,
