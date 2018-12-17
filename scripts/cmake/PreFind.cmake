3a69b360297b892104d857d7b5170e2b9773196d
Lars Bilke <lars.bilke@ufz.de>
2018-12-17 13:32:46 +0100

    [CMake] Added check if source code is a git repo.
    Otherwise the submodule setup fails.

--- scripts/cmake/PreFind.cmake
+++ scripts/cmake/PreFind.cmake
@@ -11,3 +11,13 @@ find_package(Git REQUIRED)
 string(REPLACE "mingw64/" "" GIT_EXECUTABLE ${GIT_EXECUTABLE}) # Windows git submodule fix
 set(GIT_TOOL_PATH ${GIT_EXECUTABLE} CACHE FILEPATH "The git command line interface" FORCE)
 ### End Find Git
+
+execute_process(COMMAND ${GIT_EXECUTABLE} status
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    RESULT_VARIABLE IS_GIT_REPO
+    OUTPUT_QUIET)
+if(IS_GIT_REPO GREATER 0)
+    message(FATAL_ERROR "Source code at ${PROJECT_SOURCE_DIR} is not a git "
+        "repository! Please use git to obtain the source code! See "
+        "https://www.opengeosys.org/docs/devguide/getting-started/get-the-source-code/")
+endif()
