d96a94c8c40f65605bcb726168b7cfd072a3a523
Lars Bilke <lars.bilke@ufz.de>
2019-01-10 10:21:59 +0100

    [web] Use regular or light icons instead of bold.

--- web/assets/js/header-anchor.js
+++ web/assets/js/header-anchor.js
@@ -1,5 +1,5 @@
 $('h2,h3,h4').each(function() {
-  $('<a class="headerlink"> <i class="fas fa-link"></i></a>').
+  $('<a class="headerlink"> <i class="far fa-link"></i></a>').
   attr('href', '#' + this.id).
   appendTo(this);
 });
