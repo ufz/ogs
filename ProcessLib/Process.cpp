92f8d7e740e7d8b013099146ba231b6a1bd4e9a8
Dmitri Naumov <dmitri.naumov@ufz.de>
2019-01-14 20:02:32 +0100

    [PL] Fix source term integrate() call.
    The call was simply missing in the assembleWithJacobian().

--- ProcessLib/Process.cpp
+++ ProcessLib/Process.cpp
@@ -220,6 +220,9 @@ void Process::assembleWithJacobian(const double t, GlobalVector const& x,
     const auto pcs_id =
         (_coupled_solutions) != nullptr ? _coupled_solutions->process_id : 0;
     _boundary_conditions[pcs_id].applyNaturalBC(t, x, K, b, &Jac);
+
+    // the last argument is for the jacobian, nullptr is for a unused jacobian
+    _source_term_collections[pcs_id].integrate(t, x, b, &Jac);
 }
 
 void Process::constructDofTable()
