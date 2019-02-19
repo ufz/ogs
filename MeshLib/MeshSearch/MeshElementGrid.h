b59dc9f889fa4c12b4563d59f5d1b91a97431345
Dmitri Naumov <dmitri.naumov@ufz.de>
2019-02-19 19:48:07 +0100

    [MeL] Use vector::insert which is faster than copy
    copy happens element-wise, insert can do insertions in batches.

--- MeshLib/MeshSearch/MeshElementGrid.h
+++ MeshLib/MeshSearch/MeshElementGrid.h
@@ -62,9 +62,9 @@ public:
             for (std::size_t j(min_coords.second[1]); j<=max_coords.second[1]; j++) {
                 for (std::size_t k(min_coords.second[2]); k<=max_coords.second[2]; k++) {
                     std::size_t idx(i+j*_n_steps[0]+k*n_plane);
-                    std::copy(_elements_in_grid_box[idx].begin(),
-                              _elements_in_grid_box[idx].end(),
-                              std::back_inserter(elements_vec));
+                    elements_vec.insert(end(elements_vec),
+                                        begin(_elements_in_grid_box[idx]),
+                                        end(_elements_in_grid_box[idx]));
                 }
             }
         }
