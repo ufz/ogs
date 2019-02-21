e9e0b2c32589862ffce99058a8607d00061a59a9
Thomas Fischer <thomas.fischer@ufz.de>
2019-02-21 21:31:56 +0100

    [Mat/MPL] Update test for LinearProperty.

--- Tests/MaterialLib/TestMPLLinearProperty.cpp
+++ Tests/MaterialLib/TestMPLLinearProperty.cpp
@@ -18,29 +18,28 @@ TEST(MaterialPropertyLib, LinearProperty)
     double const m = 1.0;
     double const x_ref = 293.15;
     MaterialPropertyLib::IndependentVariable const iv{
-        MaterialPropertyLib::Variables::temperature, x_ref, m};
+        MaterialPropertyLib::Variable::temperature, x_ref, m};
     MaterialPropertyLib::LinearProperty linear_property{y_ref, iv};
 
     MaterialPropertyLib::VariableArray variable_array;
-    variable_array[MaterialPropertyLib::Variables::temperature] = 303.15;
-    ASSERT_NEAR(
-        boost::get<double>(linear_property.value(variable_array)),
-        y_ref + m * (boost::get<double>(
-                         variable_array
-                             [MaterialPropertyLib::Variables::temperature]) -
-                     x_ref),
-        1.e-10);
+    variable_array[static_cast<int>(
+        MaterialPropertyLib::Variable::temperature)] = 303.15;
+    ASSERT_NEAR(boost::get<double>(linear_property.value(variable_array)),
+                y_ref + m * (boost::get<double>(variable_array[static_cast<int>(
+                                 MaterialPropertyLib::Variable::temperature)]) -
+                             x_ref),
+                1.e-10);
     ASSERT_EQ(
         boost::get<double>(linear_property.dValue(
-            variable_array, MaterialPropertyLib::Variables::phase_pressure)),
+            variable_array, MaterialPropertyLib::Variable::phase_pressure)),
         0.0);
     ASSERT_EQ(boost::get<double>(linear_property.dValue(
-                  variable_array, MaterialPropertyLib::Variables::temperature)),
+                  variable_array, MaterialPropertyLib::Variable::temperature)),
               m);
     ASSERT_EQ(boost::get<double>(linear_property.d2Value(
                   variable_array,
-                  MaterialPropertyLib::Variables::temperature,
-                  MaterialPropertyLib::Variables::temperature)),
+                  MaterialPropertyLib::Variable::temperature,
+                  MaterialPropertyLib::Variable::temperature)),
               0.0);
 }
 
