39ddd5bf808276b623f028a252a7083605142070
Lars Bilke <lars.bilke@ufz.de>
2019-03-28 08:50:42 +0100

    [web] Added fullscreen image view on click.

--- web/assets/js/fancybox.js
+++ web/assets/js/fancybox.js
@@ -7,4 +7,23 @@ $(document).ready(function(){
     autoplaySpeed: 3000,
   });
   $('.carousel-play').slick('slickPlay');
+
+  // wrap all img elements in fancybox wrapper
+  var imgs = document.querySelectorAll("img");
+
+  for(var index=0; index < imgs.length; index++) {
+    var img = imgs[index];
+    // Check for other fancybox
+    if(img.parentNode.hasAttribute("data-fancybox")) {
+      continue;
+    }
+    var wrapper = document.createElement("a");
+    wrapper.setAttribute("data-fancybox", "");
+    if(img.hasAttribute("alt")) {
+      wrapper.setAttribute("data-caption", img.getAttribute("alt"));
+    }
+    wrapper.setAttribute("href", img.getAttribute("src"));
+    img.parentNode.insertBefore(wrapper, img);
+    wrapper.appendChild(img);
+  }
 });
