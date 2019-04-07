9bbd18a10064c337c6778157a16256c8a77682a7
Thomas Fischer <thomas.fischer@ufz.de>
2019-04-07 19:36:43 +0200

    [A/U] Clang format.

--- Applications/Utils/FileConverter/OGS2VTK.cpp
+++ Applications/Utils/FileConverter/OGS2VTK.cpp
@@ -34,19 +34,22 @@ int main (int argc, char* argv[])
             "Copyright (c) 2012-2019, OpenGeoSys Community "
             "(http://www.opengeosys.org)",
         ' ', BaseLib::BuildInfo::ogs_version);
-    TCLAP::ValueArg<std::string> mesh_in("i", "mesh-input-file",
-                                         "the name of the file containing the input mesh", true,
-                                         "", "file name of input mesh");
+    TCLAP::ValueArg<std::string> mesh_in(
+        "i", "mesh-input-file",
+        "the name of the file containing the input mesh", true, "",
+        "file name of input mesh");
     cmd.add(mesh_in);
-    TCLAP::ValueArg<std::string> mesh_out("o", "mesh-output-file",
-                                          "the name of the file the mesh will be written to", true,
-                                          "", "file name of output mesh");
+    TCLAP::ValueArg<std::string> mesh_out(
+        "o", "mesh-output-file",
+        "the name of the file the mesh will be written to", true, "",
+        "file name of output mesh");
     cmd.add(mesh_out);
     cmd.parse(argc, argv);
 
     std::unique_ptr<MeshLib::Mesh const> mesh(
         MeshLib::IO::readMeshFromFile(mesh_in.getValue()));
-    INFO("Mesh read: %d nodes, %d elements.", mesh->getNumberOfNodes(), mesh->getNumberOfElements());
+    INFO("Mesh read: %d nodes, %d elements.", mesh->getNumberOfNodes(),
+         mesh->getNumberOfElements());
 
     MeshLib::IO::VtuInterface vtu(mesh.get());
     vtu.writeToFile(mesh_out.getValue());
--- Applications/Utils/MeshEdit/createLayeredMeshFromRasters.cpp
+++ Applications/Utils/MeshEdit/createLayeredMeshFromRasters.cpp
@@ -71,23 +71,30 @@ int main (int argc, char* argv[])
             "(http://www.opengeosys.org)",
         ' ', BaseLib::BuildInfo::ogs_version);
 
-    TCLAP::ValueArg<std::string> mesh_arg("i", "input-mesh-file",
-        "The name of the file containing the 2D input mesh.", true, "", "input file name");
+    TCLAP::ValueArg<std::string> mesh_arg(
+        "i", "input-mesh-file",
+        "The name of the file containing the 2D input mesh.", true, "",
+        "input file name");
     cmd.add(mesh_arg);
 
-    TCLAP::ValueArg<std::string> mesh_out_arg("o", "output-mesh-file",
+    TCLAP::ValueArg<std::string> mesh_out_arg(
+        "o", "output-mesh-file",
         "The name of the file to which the resulting 3D mesh will be written.",
         true, "", "output file name");
     cmd.add(mesh_out_arg);
 
-    TCLAP::ValueArg<std::string> raster_path_arg("r", "raster-list",
-        "An ascii-file containing a list of raster files, starting from top (DEM) to bottom.",
+    TCLAP::ValueArg<std::string> raster_path_arg(
+        "r", "raster-list",
+        "An ascii-file containing a list of raster files, starting from top "
+        "(DEM) to bottom.",
         true, "", "list of raster files");
     cmd.add(raster_path_arg);
 
-    double min_thickness (std::numeric_limits<double>::epsilon());
-    TCLAP::ValueArg<double> min_thickness_arg("t", "thickness",
-        "The minimum thickness of a layer to be integrated at any given location.",
+    double min_thickness(std::numeric_limits<double>::epsilon());
+    TCLAP::ValueArg<double> min_thickness_arg(
+        "t", "thickness",
+        "The minimum thickness of a layer to be integrated at any given "
+        "location.",
         false, min_thickness, "minimum layer thickness");
     cmd.add(min_thickness_arg);
 
@@ -104,12 +111,15 @@ int main (int argc, char* argv[])
     }
 
     INFO("Reading mesh '%s' ... ", mesh_arg.getValue().c_str());
-    std::unique_ptr<MeshLib::Mesh> const sfc_mesh (MeshLib::IO::readMeshFromFile(mesh_arg.getValue()));
-    if (!sfc_mesh) {
+    std::unique_ptr<MeshLib::Mesh> const sfc_mesh(
+        MeshLib::IO::readMeshFromFile(mesh_arg.getValue()));
+    if (!sfc_mesh)
+    {
         ERR("Error reading mesh '%s'.", mesh_arg.getValue().c_str());
         return EXIT_FAILURE;
     }
-    if (sfc_mesh->getDimension() != 2) {
+    if (sfc_mesh->getDimension() != 2)
+    {
         ERR("Input mesh needs to be a 2D mesh.");
         return EXIT_FAILURE;
     }
@@ -134,13 +144,14 @@ int main (int argc, char* argv[])
         return EXIT_FAILURE;
     }
 
-    std::string output_name (mesh_out_arg.getValue());
+    std::string output_name(mesh_out_arg.getValue());
     if (!BaseLib::hasFileExtension("vtu", output_name))
     {
         output_name.append(".vtu");
     }
     INFO("Writing mesh '%s' ... ", output_name.c_str());
-    MeshLib::IO::writeMeshToFile(*(mapper.getMesh("SubsurfaceMesh").release()), output_name);
+    MeshLib::IO::writeMeshToFile(*(mapper.getMesh("SubsurfaceMesh").release()),
+                                 output_name);
     INFO("done.");
 
     return EXIT_SUCCESS;
