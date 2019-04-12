ec9f624e5deb30d647c7a5bc3274f6aa24dfd026
bathmann <jasper.bathmann@ufz.de>
2019-04-12 16:25:29 +0200

    [T/HC] Test for reflective boundary condition by proving mass conservation in variable density flow

--- ProcessLib/ComponentTransport/Tests.cmake
+++ ProcessLib/ComponentTransport/Tests.cmake
@@ -150,6 +150,46 @@ AddTest(
 )
 
 AddTest(
+    NAME 2D_ComponentTransport_ImpermeableBoundaries
+    PATH Parabolic/ComponentTransport/MassConservation
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS mass_conservation.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    RUNTIME 23
+    DIFF_DATA
+    mass_conservation_ogsOutput_pcs_0_ts_0_t_0.000000_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_0_t_0.000000.vtu concentration concentration 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_300_t_34895.986246_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_300_t_34895.986246.vtu concentration concentration 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_600_t_81993.310506_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_600_t_81993.310506.vtu concentration concentration 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_900_t_145558.519328_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_900_t_145558.519328.vtu concentration concentration 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_1200_t_231349.715241_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_1200_t_231349.715241.vtu concentration concentration 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_1500_t_347138.358629_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_1500_t_347138.358629.vtu concentration concentration 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_1800_t_503413.251350_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_1800_t_503413.251350.vtu concentration concentration 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_2100_t_714330.672785_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_2100_t_714330.672785.vtu concentration concentration 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_2323_t_1000000.000000_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_2323_t_1000000.000000.vtu concentration concentration 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_0_t_0.000000_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_0_t_0.000000.vtu pressure pressure 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_300_t_34895.986246_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_300_t_34895.986246.vtu pressure pressure 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_600_t_81993.310506_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_600_t_81993.310506.vtu pressure pressure 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_900_t_145558.519328_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_900_t_145558.519328.vtu pressure pressure 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_1200_t_231349.715241_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_1200_t_231349.715241.vtu pressure pressure 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_1500_t_347138.358629_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_1500_t_347138.358629.vtu pressure pressure 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_1800_t_503413.251350_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_1800_t_503413.251350.vtu pressure pressure 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_2100_t_714330.672785_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_2100_t_714330.672785.vtu pressure pressure 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_2323_t_1000000.000000_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_2323_t_1000000.000000.vtu pressure pressure 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_0_t_0.000000_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_0_t_0.000000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_300_t_34895.986246_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_300_t_34895.986246.vtu darcy_velocity darcy_velocity 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_600_t_81993.310506_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_600_t_81993.310506.vtu darcy_velocity darcy_velocity 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_900_t_145558.519328_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_900_t_145558.519328.vtu darcy_velocity darcy_velocity 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_1200_t_231349.715241_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_1200_t_231349.715241.vtu darcy_velocity darcy_velocity 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_1500_t_347138.358629_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_1500_t_347138.358629.vtu darcy_velocity darcy_velocity 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_1800_t_503413.251350_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_1800_t_503413.251350.vtu darcy_velocity darcy_velocity 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_2100_t_714330.672785_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_2100_t_714330.672785.vtu darcy_velocity darcy_velocity 1e-7 1e-10
+    mass_conservation_ogsOutput_pcs_0_ts_2323_t_1000000.000000_expected.vtu mass_conservation_ogsOutput_pcs_0_ts_2323_t_1000000.000000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
+    VIS mass_conservation_ogsOutput_pcs_0_ts_2323_t_1000000.000000.vtu
+)
+
+AddTest(
     NAME 2D_ComponentTransport_DiffusionAndStorageAndGravityAndDispersionHalf
     PATH Parabolic/ComponentTransport/SimpleSynthetics
     EXECUTABLE ogs
