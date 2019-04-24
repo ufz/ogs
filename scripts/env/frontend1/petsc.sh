fe0717713a263a6a56468cf76c6df543e9d32a46
Lars Bilke <lars.bilke@ufz.de>
2019-04-24 09:17:03 +0200

    [ci skip] Added note on CPU architecture on eve setup scripts.

--- scripts/env/frontend1/petsc.sh
+++ scripts/env/frontend1/petsc.sh
@@ -6,3 +6,11 @@ fi
 
 source $DIR/mpi.sh
 module load petsc/3.7.6_maint_gcc6.2.0_openmpi_gcc_1.8.8-1
+
+echo -e "Note: If you want to run a simulation on the cluster be aware of the "\
+     "mixed CPU architecture. There are Sandy-Bridge-based nodes (orte-28, "\
+     "frontend2, envinf1) as well as Skylake-based nodes (orte-40, frontend1)"\
+     ".\nConsider setting CMake-option OGS_CPU_ARCHITECTURE to:\n\n"\
+     "  -DOGS_CPU_ARCHITECTURE=sandybridge\n"\
+     "   -- OR --\n"\
+     "  -DOGS_CPU_ARCHITECTURE=skylake-avx512"
