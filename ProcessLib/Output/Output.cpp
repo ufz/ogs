8371840232b80226e853c6a807c4da896ed4097c
Dmitri Naumov <dmitri.naumov@ufz.de>
2019-04-29 13:21:30 +0200

    [PL] Output; Rename ctor argument.

--- ProcessLib/Output/Output.cpp
+++ ProcessLib/Output/Output.cpp
@@ -92,7 +92,7 @@ bool Output::shallDoOutput(unsigned timestep, double const t)
     return make_output;
 }
 
-Output::Output(std::string output_directory, std::string prefix,
+Output::Output(std::string output_directory, std::string output_file_prefix,
                bool const compress_output, std::string const& data_mode,
                bool const output_nonlinear_iteration_results,
                std::vector<PairRepeatEachSteps> repeats_each_steps,
@@ -101,7 +101,7 @@ Output::Output(std::string output_directory, std::string prefix,
                std::vector<std::string>&& mesh_names_for_output,
                std::vector<std::unique_ptr<MeshLib::Mesh>> const& meshes)
     : _output_directory(std::move(output_directory)),
-      _output_file_prefix(std::move(prefix)),
+      _output_file_prefix(std::move(output_file_prefix)),
       _output_file_compression(compress_output),
       _output_file_data_mode(convertVtkDataMode(data_mode)),
       _output_nonlinear_iteration_results(output_nonlinear_iteration_results),
