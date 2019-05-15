04ae04c9c4c01f7e60bd743bf73db36d8be30993
bathmann <jasper.bathmann@ufz.de>
2019-05-15 14:18:15 +0200

    [T/HC] fix the race conditions in two benchmarks

--- ProcessLib/ComponentTransport/Tests.cmake
+++ ProcessLib/ComponentTransport/Tests.cmake
@@ -295,25 +295,25 @@ AddTest(
     REQUIREMENTS NOT OGS_USE_MPI
     RUNTIME 26
     DIFF_DATA
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_0_t_0.000000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_0_t_0.000000.vtu Si Si 5e-6 5e-6
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_1_t_0.020000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_1_t_0.020000.vtu Si Si 5e-6 5e-6
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_2_t_0.040000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_2_t_0.040000.vtu Si Si 5e-6 5e-6
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_3_t_0.060000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_3_t_0.060000.vtu Si Si 5e-6 5e-6
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_4_t_0.080000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_4_t_0.080000.vtu Si Si 5e-6 5e-6
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_5_t_0.100000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_5_t_0.100000.vtu Si Si 5e-6 5e-6
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_0_t_0.000000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_0_t_0.000000.vtu pressure pressure 1e-7 1e-10
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_1_t_0.020000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_1_t_0.020000.vtu pressure pressure 1e-7 1e-10
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_2_t_0.040000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_2_t_0.040000.vtu pressure pressure 1e-7 1e-10
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_3_t_0.060000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_3_t_0.060000.vtu pressure pressure 1e-7 1e-10
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_4_t_0.080000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_4_t_0.080000.vtu pressure pressure 1e-7 1e-10
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_5_t_0.100000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_5_t_0.100000.vtu pressure pressure 1e-7 1e-10
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_0_t_0.000000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_0_t_0.000000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_1_t_0.020000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_1_t_0.020000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_2_t_0.040000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_2_t_0.040000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_3_t_0.060000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_3_t_0.060000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_4_t_0.080000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_4_t_0.080000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_5_t_0.100000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_5_t_0.100000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
-    VIS DiffusionAndAdvection_surfaceflux_pcs_0_ts_5_t_0.100000.vtu
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_0_t_0.000000_expected.vtu OpenBoundaryBC_pcs_0_ts_0_t_0.000000.vtu Si Si 5e-6 5e-6
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_1_t_0.020000_expected.vtu OpenBoundaryBC_pcs_0_ts_1_t_0.020000.vtu Si Si 5e-6 5e-6
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_2_t_0.040000_expected.vtu OpenBoundaryBC_pcs_0_ts_2_t_0.040000.vtu Si Si 5e-6 5e-6
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_3_t_0.060000_expected.vtu OpenBoundaryBC_pcs_0_ts_3_t_0.060000.vtu Si Si 5e-6 5e-6
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_4_t_0.080000_expected.vtu OpenBoundaryBC_pcs_0_ts_4_t_0.080000.vtu Si Si 5e-6 5e-6
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_5_t_0.100000_expected.vtu OpenBoundaryBC_pcs_0_ts_5_t_0.100000.vtu Si Si 5e-6 5e-6
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_0_t_0.000000_expected.vtu OpenBoundaryBC_pcs_0_ts_0_t_0.000000.vtu pressure pressure 1e-7 1e-10
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_1_t_0.020000_expected.vtu OpenBoundaryBC_pcs_0_ts_1_t_0.020000.vtu pressure pressure 1e-7 1e-10
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_2_t_0.040000_expected.vtu OpenBoundaryBC_pcs_0_ts_2_t_0.040000.vtu pressure pressure 1e-7 1e-10
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_3_t_0.060000_expected.vtu OpenBoundaryBC_pcs_0_ts_3_t_0.060000.vtu pressure pressure 1e-7 1e-10
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_4_t_0.080000_expected.vtu OpenBoundaryBC_pcs_0_ts_4_t_0.080000.vtu pressure pressure 1e-7 1e-10
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_5_t_0.100000_expected.vtu OpenBoundaryBC_pcs_0_ts_5_t_0.100000.vtu pressure pressure 1e-7 1e-10
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_0_t_0.000000_expected.vtu OpenBoundaryBC_pcs_0_ts_0_t_0.000000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_1_t_0.020000_expected.vtu OpenBoundaryBC_pcs_0_ts_1_t_0.020000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_2_t_0.040000_expected.vtu OpenBoundaryBC_pcs_0_ts_2_t_0.040000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_3_t_0.060000_expected.vtu OpenBoundaryBC_pcs_0_ts_3_t_0.060000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_4_t_0.080000_expected.vtu OpenBoundaryBC_pcs_0_ts_4_t_0.080000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_5_t_0.100000_expected.vtu OpenBoundaryBC_pcs_0_ts_5_t_0.100000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
+    VIS OpenBoundaryBC_pcs_0_ts_5_t_0.100000.vtu
 )
 
 AddTest(
@@ -326,25 +326,25 @@ AddTest(
     REQUIREMENTS NOT OGS_USE_MPI
     RUNTIME 26
     DIFF_DATA
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_0_t_0.000000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_0_t_0.000000.vtu Si Si 5e-6 5e-6
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_1_t_0.020000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_1_t_0.020000.vtu Si Si 5e-6 5e-6
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_2_t_0.040000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_2_t_0.040000.vtu Si Si 5e-6 5e-6
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_3_t_0.060000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_3_t_0.060000.vtu Si Si 5e-6 5e-6
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_4_t_0.080000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_4_t_0.080000.vtu Si Si 5e-6 5e-6
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_5_t_0.100000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_5_t_0.100000.vtu Si Si 5e-6 5e-6
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_0_t_0.000000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_0_t_0.000000.vtu pressure pressure 1e-7 1e-10
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_1_t_0.020000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_1_t_0.020000.vtu pressure pressure 1e-7 1e-10
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_2_t_0.040000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_2_t_0.040000.vtu pressure pressure 1e-7 1e-10
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_3_t_0.060000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_3_t_0.060000.vtu pressure pressure 1e-7 1e-10
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_4_t_0.080000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_4_t_0.080000.vtu pressure pressure 1e-7 1e-10
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_5_t_0.100000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_5_t_0.100000.vtu pressure pressure 1e-7 1e-10
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_0_t_0.000000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_0_t_0.000000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_1_t_0.020000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_1_t_0.020000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_2_t_0.040000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_2_t_0.040000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_3_t_0.060000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_3_t_0.060000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_4_t_0.080000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_4_t_0.080000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
-    DiffusionAndAdvection_surfaceflux_pcs_0_ts_5_t_0.100000_expected.vtu DiffusionAndAdvection_surfaceflux_pcs_0_ts_5_t_0.100000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
-    VIS DiffusionAndAdvection_surfaceflux_pcs_0_ts_5_t_0.100000.vtu
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_0_t_0.000000_expected.vtu AdvectiveNonAdvectiveComparison_pcs_0_ts_0_t_0.000000.vtu Si Si 5e-6 5e-6
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_1_t_0.020000_expected.vtu AdvectiveNonAdvectiveComparison_pcs_0_ts_1_t_0.020000.vtu Si Si 5e-6 5e-6
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_2_t_0.040000_expected.vtu AdvectiveNonAdvectiveComparison_pcs_0_ts_2_t_0.040000.vtu Si Si 5e-6 5e-6
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_3_t_0.060000_expected.vtu AdvectiveNonAdvectiveComparison_pcs_0_ts_3_t_0.060000.vtu Si Si 5e-6 5e-6
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_4_t_0.080000_expected.vtu AdvectiveNonAdvectiveComparison_pcs_0_ts_4_t_0.080000.vtu Si Si 5e-6 5e-6
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_5_t_0.100000_expected.vtu AdvectiveNonAdvectiveComparison_pcs_0_ts_5_t_0.100000.vtu Si Si 5e-6 5e-6
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_0_t_0.000000_expected.vtu AdvectiveNonAdvectiveComparison_pcs_0_ts_0_t_0.000000.vtu pressure pressure 1e-7 1e-10
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_1_t_0.020000_expected.vtu AdvectiveNonAdvectiveComparison_pcs_0_ts_1_t_0.020000.vtu pressure pressure 1e-7 1e-10
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_2_t_0.040000_expected.vtu AdvectiveNonAdvectiveComparison_pcs_0_ts_2_t_0.040000.vtu pressure pressure 1e-7 1e-10
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_3_t_0.060000_expected.vtu AdvectiveNonAdvectiveComparison_pcs_0_ts_3_t_0.060000.vtu pressure pressure 1e-7 1e-10
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_4_t_0.080000_expected.vtu AdvectiveNonAdvectiveComparison_pcs_0_ts_4_t_0.080000.vtu pressure pressure 1e-7 1e-10
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_5_t_0.100000_expected.vtu AdvectiveNonAdvectiveComparison_pcs_0_ts_5_t_0.100000.vtu pressure pressure 1e-7 1e-10
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_0_t_0.000000_expected.vtu AdvectiveNonAdvectiveComparison_pcs_0_ts_0_t_0.000000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_1_t_0.020000_expected.vtu AdvectiveNonAdvectiveComparison_pcs_0_ts_1_t_0.020000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_2_t_0.040000_expected.vtu AdvectiveNonAdvectiveComparison_pcs_0_ts_2_t_0.040000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_3_t_0.060000_expected.vtu AdvectiveNonAdvectiveComparison_pcs_0_ts_3_t_0.060000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_4_t_0.080000_expected.vtu AdvectiveNonAdvectiveComparison_pcs_0_ts_4_t_0.080000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
+    DiffusionAndAdvection_surfaceflux_pcs_0_ts_5_t_0.100000_expected.vtu AdvectiveNonAdvectiveComparison_pcs_0_ts_5_t_0.100000.vtu darcy_velocity darcy_velocity 1e-7 1e-10
+    VIS AdvectiveNonAdvectiveComparison_pcs_0_ts_5_t_0.100000.vtu
 )
 
 AddTest(
