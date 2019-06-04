0f5cd0046ad0ff3fb29c42956acc561408c518e0
renchao_lu <renchao.lu@gmail.com>
2019-06-04 18:00:52 +0200

    [CL] Create class Aqueous Solution.

---
+++ ChemistryLib/PhreeqcIOData/AqueousSolution.h
@@ -0,0 +1,66 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2019, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#pragma once
+
+#include <boost/optional/optional.hpp>
+#include <string>
+#include <vector>
+
+#include "BaseLib/Error.h"
+
+namespace BaseLib
+{
+class ConfigTree;
+}
+
+namespace ChemistryLib
+{
+struct Component
+{
+    Component(std::string name_) : name(std::move(name_)) {}
+
+    std::string const name;
+    double amount;
+};
+
+enum class MeansOfAdjustingCharge
+{
+    pH,
+    pe,
+    Unspecified
+};
+
+struct AqueousSolution
+{
+    static inline const std::string unit = "mol/kgw";
+
+    AqueousSolution(double temperature_,
+                    double pressure_,
+                    double pe_,
+                    std::vector<Component>&& components_,
+                    MeansOfAdjustingCharge means_of_adjusting_charge_)
+        : temperature(temperature_),
+          pressure(pressure_),
+          pe(pe_),
+          components(std::move(components_)),
+          means_of_adjusting_charge(means_of_adjusting_charge_)
+    {
+    }
+
+    double temperature;
+    double pressure;
+    double pH;
+    double pe;
+    std::vector<Component> components;
+    MeansOfAdjustingCharge const means_of_adjusting_charge;
+};
+
+AqueousSolution createAqueousSolution(BaseLib::ConfigTree const& config);
+}  // namespace ChemistryLib
