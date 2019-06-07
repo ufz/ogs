d52bd322d8e9f36a7ae1850e5aea34ae01bba9b8
Lars Bilke <lars.bilke@ufz.de>
2019-06-07 15:36:23 +0200

    [eve] Added check for easybuild modules.

--- scripts/env/eve/cli.sh
+++ scripts/env/eve/cli.sh
@@ -1,3 +1,10 @@
+if [ ! -f ~/.easybuild-yes ]; then
+    echo "ERROR: Easybuild modules not enabled but required!\n"
+    echo "Run 'touch ~/.easybuild-yes' and re-login to enable."
+    echo "For more details see:\n  https://www.opengeosys.org/docs/devguide/advanced/working-on-eve"
+    return 1
+fi
+
 module use /global/apps/modulefiles
 
 module load cmake
