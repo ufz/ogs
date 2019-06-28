491268a919497566be3e7c664a577c5d56fe6d69
Dmitri Naumov <dmitri.naumov@ufz.de>
2019-06-28 19:16:29 +0200

    [T] LIE/HM; single fracture const.hydro.aperture.

--- ProcessLib/LIE/HydroMechanics/Tests.cmake
+++ ProcessLib/LIE/HydroMechanics/Tests.cmake
@@ -81,6 +81,32 @@ AddTest(
 )
 
 AddTest(
+    NAME LIE_HM_single_fracture_const_hydraulic_aperture
+    PATH LIE/HydroMechanics
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS single_fracture_const_hydraulic_aperture.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
+    DIFF_DATA
+    GLOB single_fracture_const_hydraulic_aperture_pcs_0_ts_*.vtu pressure pressure 1e-16 1e-16
+    GLOB single_fracture_const_hydraulic_aperture_pcs_0_ts_*.vtu pressure_interpolated pressure_interpolated 1e-16 1e-16
+    GLOB single_fracture_const_hydraulic_aperture_pcs_0_ts_*.vtu displacement displacement 1e-16 0
+    GLOB single_fracture_const_hydraulic_aperture_pcs_0_ts_*.vtu displacement_jump1 displacement_jump1 1e-16 0
+    GLOB single_fracture_const_hydraulic_aperture_pcs_0_ts_*.vtu nodal_w nodal_w 1e-16 0
+    GLOB single_fracture_const_hydraulic_aperture_pcs_0_ts_*.vtu nodal_aperture nodal_aperture 1e-16 0
+    GLOB single_fracture_const_hydraulic_aperture_pcs_0_ts_*.vtu strain_xx strain_xx 1e-16 0
+    GLOB single_fracture_const_hydraulic_aperture_pcs_0_ts_*.vtu strain_yy strain_yy 1e-16 0
+    GLOB single_fracture_const_hydraulic_aperture_pcs_0_ts_*.vtu strain_xy strain_xy 1e-16 0
+    GLOB single_fracture_const_hydraulic_aperture_pcs_0_ts_*.vtu strain_zz strain_zz 1e-16 0
+    GLOB single_fracture_const_hydraulic_aperture_pcs_0_ts_*.vtu stress_xx stress_xx 1e-16 0
+    GLOB single_fracture_const_hydraulic_aperture_pcs_0_ts_*.vtu stress_yy stress_yy 1e-16 0
+    GLOB single_fracture_const_hydraulic_aperture_pcs_0_ts_*.vtu stress_xy stress_xy 1e-16 0
+    GLOB single_fracture_const_hydraulic_aperture_pcs_0_ts_*.vtu stress_zz stress_zz 1e-16 0
+    GLOB single_fracture_const_hydraulic_aperture_pcs_0_ts_*.vtu velocity velocity 1e-16 0
+)
+
+AddTest(
     NAME LARGE_LIE_HM_TaskB
     PATH LIE/HydroMechanics
     RUNTIME 60
