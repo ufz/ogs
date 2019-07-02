f98e460a945aa932d16a63bac5adcd2a09cde45e
Dmitri Naumov <github@naumov.de>
2019-07-02 13:58:15 +0200

    [T] relax DruckerPrager stress tol slightly

--- ProcessLib/SmallDeformation/Tests.cmake
+++ ProcessLib/SmallDeformation/Tests.cmake
@@ -73,7 +73,7 @@ AddTest(
     # See also the prj file.
     DIFF_DATA
     cube_1e0_dp_ref_created_with_OGS_Ehlers.vtu cube_1e0_dp_pcs_0_ts_203_t_5.100000.vtu displacement displacement 1e-14 0
-    cube_1e0_dp_ref_created_with_OGS_Ehlers.vtu cube_1e0_dp_pcs_0_ts_203_t_5.100000.vtu sigma sigma 1e-13 0
+    cube_1e0_dp_ref_created_with_OGS_Ehlers.vtu cube_1e0_dp_pcs_0_ts_203_t_5.100000.vtu sigma sigma 2e-13 0
     cube_1e0_dp_ref_created_with_OGS_Ehlers.vtu cube_1e0_dp_pcs_0_ts_203_t_5.100000.vtu epsilon epsilon 1e-14 0
 )
 
