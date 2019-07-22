6c129309a1afd513276455453e676f73a612bc67
Dmitri Naumov <github@naumov.de>
2019-07-22 19:56:44 +0200

    [PL] HT; slightly relax const viscosity test tols.

--- ProcessLib/HT/Tests.cmake
+++ ProcessLib/HT/Tests.cmake
@@ -189,7 +189,7 @@ AddTest(
     REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
     DIFF_DATA
     square_5500x5500.vtu ConstViscosityThermalConvection_pcs_1_ts_149_t_50000000000.000000.vtu T_ref T 1e-1  1.e-3
-    square_5500x5500.vtu ConstViscosityThermalConvection_pcs_1_ts_149_t_50000000000.000000.vtu p_ref p  1e+4  1.e-2
+    square_5500x5500.vtu ConstViscosityThermalConvection_pcs_1_ts_149_t_50000000000.000000.vtu p_ref p  2e+4  1.e-2
     square_5500x5500.vtu ConstViscosityThermalConvection_pcs_1_ts_149_t_50000000000.000000.vtu darcy_velocity_ref darcy_velocity  1e-1  1.e-3
     VIS ConstViscosityThermalConvection_pcs_1_ts_149_t_50000000000.000000.vtu
 )
