1d7353c24a419e2dd477b321fa259d0f32dec3e9
Thomas Fischer <thomas.fischer@ufz.de>
2019-08-19 14:01:12 +0200

    [PL] Drop unnecessary *HTMaterialProperties special members.

--- ProcessLib/HT/HTMaterialProperties.h
+++ ProcessLib/HT/HTMaterialProperties.h
@@ -12,7 +12,10 @@
 #include <memory>
 #include <utility>
 
+#include <Eigen/Eigen>
+
 #include "MaterialLib/MPL/MaterialSpatialDistributionMap.h"
+#include "ParameterLib/Parameter.h"
 
 namespace ProcessLib
 {
@@ -23,35 +26,11 @@ namespace HT
 {
 struct HTMaterialProperties final
 {
-    HTMaterialProperties(
-        std::unique_ptr<MaterialPropertyLib::MaterialSpatialDistributionMap>&&
-            media_map_,
-        bool const has_fluid_thermal_expansion_,
-        ParameterLib::Parameter<double> const& solid_thermal_expansion_,
-        ParameterLib::Parameter<double> const& biot_constant_,
-        Eigen::VectorXd specific_body_force_,
-        bool const has_gravity_)
-        : media_map(std::move(media_map_)),
-          has_fluid_thermal_expansion(has_fluid_thermal_expansion_),
-          solid_thermal_expansion(solid_thermal_expansion_),
-          biot_constant(biot_constant_),
-          specific_body_force(std::move(specific_body_force_)),
-          has_gravity(has_gravity_)
-    {
-    }
-
-    HTMaterialProperties(HTMaterialProperties&&) = delete;
-    HTMaterialProperties(HTMaterialProperties const&) = delete;
-    void operator=(HTMaterialProperties&&) = delete;
-    void operator=(HTMaterialProperties const&) = delete;
-
     std::unique_ptr<MaterialPropertyLib::MaterialSpatialDistributionMap>
         media_map;
-
     bool const has_fluid_thermal_expansion;
     ParameterLib::Parameter<double> const& solid_thermal_expansion;
     ParameterLib::Parameter<double> const& biot_constant;
-
     Eigen::VectorXd const specific_body_force;
     bool const has_gravity;
 };
