a9ea5e00ff101ed91b7a4e48360a9257cd28921b
Norbert Grunwald <Norbert.Grunwald@ufz.de>
2019-08-27 05:28:43 +0200

    merged TwophaseFlow_PP tests
    There have been two seperate runs of the same test setup where
    different results have been compared. The redundant test run is removed.

--- ProcessLib/TwoPhaseFlowWithPP/Tests.cmake
+++ ProcessLib/TwoPhaseFlowWithPP/Tests.cmake
@@ -1,15 +1,5 @@
 AddTest(
-    NAME 2D_TwoPhase_PP_Lia_quad_1
-    PATH Parabolic/TwoPhaseFlowPP/Liakopoulos
-    EXECUTABLE ogs
-    EXECUTABLE_ARGS TwoPhase_Lia_quad_short.prj
-    TESTER vtkdiff
-    REQUIREMENTS NOT OGS_USE_MPI
-    DIFF_DATA
-    h2_Liako_20.vtu twophaseflow_pcs_0_ts_218_t_20.000000.vtu saturation saturation 1e-2 1e-4
-)
-AddTest(
-    NAME 2D_TwoPhase_PP_Lia_quad_2
+    NAME 2D_TwoPhase_PP_Lia_quad
     PATH Parabolic/TwoPhaseFlowPP/Liakopoulos
     EXECUTABLE ogs
     EXECUTABLE_ARGS TwoPhase_Lia_quad_short.prj
@@ -17,11 +7,12 @@ AddTest(
     REQUIREMENTS NOT OGS_USE_MPI
     RUNTIME 11
     DIFF_DATA
+    h2_Liako_20.vtu twophaseflow_pcs_0_ts_218_t_20.000000.vtu saturation saturation 1e-2 1e-4
     h2_Liako_20.vtu twophaseflow_pcs_0_ts_218_t_20.000000.vtu capillary_pressure capillary_pressure 20 1e-3
     h2_Liako_20.vtu twophaseflow_pcs_0_ts_218_t_20.000000.vtu gas_pressure gas_pressure 20 1e-3
 )
 AddTest(
-    NAME 2D_TwoPhase_PP_Lia_quad_1_lg
+    NAME 2D_TwoPhase_PP_Lia_quad_lg
     PATH Parabolic/TwoPhaseFlowPP/Liakopoulos
     EXECUTABLE ogs
     EXECUTABLE_ARGS TwoPhase_Lia_quad_large.prj
@@ -29,20 +20,11 @@ AddTest(
     REQUIREMENTS NOT OGS_USE_MPI
     DIFF_DATA
     h2_Liako_1198.vtu twophaseflow_pcs_0_ts_1198_t_1000.000000.vtu SATURATION1 saturation 1e-2 1e-3
-)
-AddTest(
-    NAME 2D_TwoPhase_PP_Lia_quad_2_lg
-    PATH Parabolic/TwoPhaseFlowPP/Liakopoulos
-    EXECUTABLE ogs
-    EXECUTABLE_ARGS TwoPhase_Lia_quad_large.prj
-    TESTER vtkdiff
-    REQUIREMENTS NOT OGS_USE_MPI
-    DIFF_DATA
     h2_Liako_1198.vtu twophaseflow_pcs_0_ts_1198_t_1000.000000.vtu PRESSURE1 capillary_pressure 20 1e-2
     h2_Liako_1198.vtu twophaseflow_pcs_0_ts_1198_t_1000.000000.vtu PRESSURE2 gas_pressure 20 1e-2
 )
 AddTest(
-    NAME 1D_TwoPhase_PP_mcwt_1
+    NAME 1D_TwoPhase_PP_mcwt
     PATH Parabolic/TwoPhaseFlowPP/McWhorter
     EXECUTABLE ogs
     EXECUTABLE_ARGS TwoPhase_mcwt_line.prj
@@ -50,15 +32,6 @@ AddTest(
     REQUIREMENTS NOT OGS_USE_MPI
     DIFF_DATA
     mcwt_1000.vtu twophaseflow_pcs_0_ts_519_t_1000.000000.vtu SATURATION1 saturation 1e-2 1e-4
-)
-AddTest(
-    NAME 1D_TwoPhase_PP_mcwt_2
-    PATH Parabolic/TwoPhaseFlowPP/McWhorter
-    EXECUTABLE ogs
-    EXECUTABLE_ARGS TwoPhase_mcwt_line.prj
-    TESTER vtkdiff
-    REQUIREMENTS NOT OGS_USE_MPI
-    DIFF_DATA
     mcwt_1000.vtu twophaseflow_pcs_0_ts_519_t_1000.000000.vtu PRESSURE1 capillary_pressure 10 1e-3
     mcwt_1000.vtu twophaseflow_pcs_0_ts_519_t_1000.000000.vtu PRESSURE2 gas_pressure 10 1e-3
 )
