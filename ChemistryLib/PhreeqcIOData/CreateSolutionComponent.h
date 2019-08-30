7bfb5fefb1b6f60f94d2cbf0b06b728095e2f251
renchao_lu <renchao.lu@gmail.com>
2019-08-30 17:27:34 +0200

    [CL] Move creation of solution components into a new function.

---
+++ ChemistryLib/PhreeqcIOData/CreateSolutionComponent.h
@@ -0,0 +1,31 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2019, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace BaseLib
+{
+class ConfigTree;
+}
+
+namespace ChemistryLib
+{
+namespace PhreeqcIOData
+{
+struct Component;
+
+std::vector<Component> createSolutionComponents(
+    BaseLib::ConfigTree const& config,
+    std::vector<std::pair<int, std::string>> const&
+        process_id_to_component_name_map);
+}  // namespace PhreeqcIOData
+}  // namespace ChemistryLib
