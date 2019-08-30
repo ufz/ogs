bd332fda4e2e9400cc674ec4c14b99627d847229
renchao_lu <renchao.lu@gmail.com>
2019-08-30 17:01:39 +0200

    [CL] Add types of chemical solvers supported.

---
+++ ChemistryLib/ChemicalSolverType.h
@@ -0,0 +1,19 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2019, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#pragma once
+
+namespace ChemistryLib
+{
+enum class ChemicalSolver
+{
+    Phreeqc,
+    PhreeqcKernel
+};
+}  // namespace ChemistryLib
