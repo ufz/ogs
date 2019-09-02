52d151b43a03280e5efbf061ae6931a863690c54
Norbert Grunwald <Norbert.Grunwald@ufz.de>
2019-09-02 15:20:17 +0200

    2PhaseFlow PP Testa updated / new reference files provided

--- ProcessLib/TwoPhaseFlowWithPP/Tests.cmake
+++ ProcessLib/TwoPhaseFlowWithPP/Tests.cmake
@@ -1,5 +1,5 @@
 AddTest(
-    NAME 2D_TwoPhase_PP_Lia_quad
+    NAME 2D_TwoPhase_PP_Lia_quad_short
     PATH Parabolic/TwoPhaseFlowPP/Liakopoulos
     EXECUTABLE ogs
     EXECUTABLE_ARGS TwoPhase_Lia_quad_short.prj
@@ -12,16 +12,16 @@ AddTest(
     h2_Liako_20.vtu twophaseflow_pcs_0_ts_218_t_20.000000.vtu gas_pressure gas_pressure 20 1e-3
 )
 AddTest(
-    NAME 2D_TwoPhase_PP_Lia_quad_lg
+    NAME 2D_TwoPhase_PP_Lia_quad_large
     PATH Parabolic/TwoPhaseFlowPP/Liakopoulos
     EXECUTABLE ogs
     EXECUTABLE_ARGS TwoPhase_Lia_quad_large.prj
     TESTER vtkdiff
     REQUIREMENTS NOT OGS_USE_MPI
     DIFF_DATA
-    h2_Liako_1198.vtu twophaseflow_pcs_0_ts_1198_t_1000.000000.vtu SATURATION1 saturation 1e-2 1e-3
-    h2_Liako_1198.vtu twophaseflow_pcs_0_ts_1198_t_1000.000000.vtu PRESSURE1 capillary_pressure 20 1e-2
-    h2_Liako_1198.vtu twophaseflow_pcs_0_ts_1198_t_1000.000000.vtu PRESSURE2 gas_pressure 20 1e-2
+    h2_Liako_1198.vtu twophaseflow_pcs_0_ts_1198_t_1000.000000.vtu saturation saturation 1e-2 1e-3
+    h2_Liako_1198.vtu twophaseflow_pcs_0_ts_1198_t_1000.000000.vtu capillary_pressure capillary_pressure 20 1e-2
+    h2_Liako_1198.vtu twophaseflow_pcs_0_ts_1198_t_1000.000000.vtu gas_pressure gas_pressure 20 1e-2
 )
 AddTest(
     NAME 1D_TwoPhase_PP_mcwt
