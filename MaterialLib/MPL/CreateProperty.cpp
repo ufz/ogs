7fea475c6b30d5dbdbbc6fe9833b443b78a3ff77
renchao_lu <renchao.lu@gmail.com>
2019-09-03 14:37:07 +0200

    [MatLib/MPL] Extend linear property parsing.

--- MaterialLib/MPL/CreateProperty.cpp
+++ MaterialLib/MPL/CreateProperty.cpp
@@ -115,30 +115,37 @@ std::unique_ptr<MaterialPropertyLib::Property> createProperty(
             //! \ogs_file_param{properties__property__LinearProperty__reference_value}
             config.getConfigParameter<double>("reference_value");
 
-        auto const& independent_variable_config =
+        std::vector<MaterialPropertyLib::IndependentVariable> ivs;
+        auto const& independent_variables_config =
             //! \ogs_file_param{properties__property__LinearProperty__independent_variable}
-            config.getConfigSubtree("independent_variable");
+            config.getConfigSubtree("independent_variables");
+        for (auto const& independent_variable_config :
+             independent_variables_config.getConfigSubtreeList(
+                 "independent_variable"))
+        {
+            auto const& variable_name =
+                //! \ogs_file_param{properties__property__LinearProperty__independent_variable__variable_name}
+                independent_variable_config.getConfigParameter<std::string>(
+                    "variable_name");
+            auto const reference_condition =
+                //! \ogs_file_param{properties__property__LinearProperty__independent_variable__reference_condition}
+                independent_variable_config.getConfigParameter<double>(
+                    "reference_condition");
+            auto const slope =
+                //! \ogs_file_param{properties__property__LinearProperty__independent_variable__slope}
+                independent_variable_config.getConfigParameter<double>("slope");
 
-        auto const& variable_name =
-            //! \ogs_file_param{properties__property__LinearProperty__independent_variable__variable_name}
-            independent_variable_config.getConfigParameter<std::string>(
-                "variable_name");
-        auto const reference_condition =
-            //! \ogs_file_param{properties__property__LinearProperty__independent_variable__reference_condition}
-            independent_variable_config.getConfigParameter<double>(
-                "reference_condition");
-        auto const slope =
-            //! \ogs_file_param{properties__property__LinearProperty__independent_variable__slope}
-            independent_variable_config.getConfigParameter<double>("slope");
+            MaterialPropertyLib::Variable ivt =
+                MaterialPropertyLib::convertStringToVariable(variable_name);
 
-        MaterialPropertyLib::Variable ivt =
-            MaterialPropertyLib::convertStringToVariable(variable_name);
+            MaterialPropertyLib::IndependentVariable iv{
+                ivt, reference_condition, slope};
 
-        MaterialPropertyLib::IndependentVariable const iv{
-            ivt, reference_condition, slope};
+            ivs.push_back(std::move(iv));
+        }
 
         return std::make_unique<MaterialPropertyLib::LinearProperty>(
-            reference_value, iv);
+            reference_value, ivs);
     }
 
     if (property_type == "Exponential")
