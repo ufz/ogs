ff4c6f12c4050e828e0daf4cf244b7ffa371ed17
renchao_lu <renchao.lu@gmail.com>
2019-09-04 15:12:05 +0200

    [CL] Instantiate object of reaction rate.

---
+++ ChemistryLib/PhreeqcKernelData/ReactionRate.h
@@ -0,0 +1,35 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2019, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace ChemistryLib
+{
+namespace PhreeqcKernelData
+{
+class ReactionRate
+{
+public:
+    ReactionRate(std::string kinetic_reactant_,
+                 std::vector<std::string> statements);
+
+    std::string const& commands() const { return _commands; }
+
+public:
+    std::string const kinetic_reactant;
+
+private:
+    std::string _commands;
+};
+}  // namespace PhreeqcKernelData
+}  // namespace ChemistryLib
