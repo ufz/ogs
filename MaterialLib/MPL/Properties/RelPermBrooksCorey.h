4063c9f7ba15cc98da639fc0dc0e3b5f1d305f55
Norbert Grunwald <Norbert.Grunwald@ufz.de>
2019-09-11 07:44:37 +0200

    extract RelPermBrooksCorey Property create method

--- MaterialLib/MPL/Properties/RelPermBrooksCorey.h
+++ MaterialLib/MPL/Properties/RelPermBrooksCorey.h
@@ -72,40 +72,4 @@ public:
                             double const t) const override;
 };
 
-inline std::unique_ptr<RelPermBrooksCorey> createRelPermBrooksCorey(
-    BaseLib::ConfigTree const& config)
-{
-    // check is reading the parameter, not peeking it...
-    //! \ogs_file_param{prj__media__medium__properties__property__RelPermBrooksCorey}
-    // config.checkConfigParameter("type", "RelPermBrooksCorey");
-    DBUG("Create RelPermBrooksCorey medium property");
-
-    auto const residual_liquid_saturation =
-        //! \ogs_file_param{prj__media__medium__properties__property__RelPermBrooksCorey__residual_liquid_saturation}
-        config.getConfigParameter<double>("residual_liquid_saturation");
-    auto const residual_gas_saturation =
-        //! \ogs_file_param{prj__media__medium__properties__property__RelPermBrooksCorey__residual_gas_saturation}
-        config.getConfigParameter<double>("residual_gas_saturation");
-    auto const min_relative_permeability_liquid =
-        //! \ogs_file_param{prj__media__medium__properties__property__RelPermBrooksCorey__k_rel_min_liquid}
-        config.getConfigParameter<double>("min_relative_permeability_liquid");
-    auto const min_relative_permeability_gas =
-        //! \ogs_file_param{prj__media__medium__properties__property__RelPermBrooksCorey__k_rel_min_gas}
-        config.getConfigParameter<double>("min_relative_permeability_gas");
-    auto const exponent =
-        //! \ogs_file_param{prj__media__medium__properties__property__RelPermBrooksCorey__lambda}
-        config.getConfigParameter<double>("lambda");
-    if (exponent == 0.)
-    {
-        OGS_FATAL("Exponent 'lambda' must be positive.");
-    }
-
-    return std::make_unique<RelPermBrooksCorey>(
-        residual_liquid_saturation,
-        residual_gas_saturation,
-        min_relative_permeability_liquid,
-        min_relative_permeability_gas,
-        exponent);
-}
-
 }  // namespace MaterialPropertyLib
