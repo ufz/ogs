5a37c0326fb769953c80027105e98081c8067ad3
Shuang Chen <shuang.chen@ufz.de>
2019-09-24 17:20:53 +0200

    fixed merge conflict
    fixed merge conflict
    When the MPL is used, several conflicts emerges. This commit will fix it.
    Comment out the BHE pipeline flowrate initialization process, for now it causes conflict

--- ProcessLib/HeatTransportBHE/HeatTransportBHEProcessData.h
+++ ProcessLib/HeatTransportBHE/HeatTransportBHEProcessData.h
@@ -14,8 +14,8 @@
 
 #include "MaterialLib/MPL/MaterialSpatialDistributionMap.h"
 #include "MeshLib/PropertyVector.h"
+#include "ProcessLib/BoundaryCondition/Python/BHEInflowPythonBoundaryConditionPythonSideInterface.h"
 #include "ProcessLib/HeatTransportBHE/BHE/BHETypes.h"
-
 namespace MeshLib
 {
 class Element;
@@ -25,6 +25,28 @@ namespace ProcessLib::HeatTransportBHE
 {
 struct HeatTransportBHEProcessData final
 {
+#ifdef OGS_USE_PYTHON
+    HeatTransportBHEProcessData(
+        std::unique_ptr<MaterialPropertyLib::MaterialSpatialDistributionMap>&&
+            media_map_,
+        std::vector<BHE::BHETypes>&& vec_BHEs_,
+        bool const& if_bhe_network_exist_python_bc_,
+        BHEInflowPythonBoundaryConditionPythonSideInterface* py_bc_object_)
+        : media_map(std::move(media_map_)),
+          _vec_BHE_property(std::move(vec_BHEs_)),
+          if_bhe_network_exist_python_bc(if_bhe_network_exist_python_bc_),
+          py_bc_object(py_bc_object_)
+#else
+        std::unique_ptr<MaterialPropertyLib::MaterialSpatialDistributionMap>&&
+            media_map_,
+        std::vector<BHE::BHETypes>&& vec_BHEs_,
+        bool const& if_bhe_network_exist_python_bc_)
+        : media_map(std::move(media_map_)),
+          _vec_BHE_property(std::move(vec_BHEs_)),
+          if_bhe_network_exist_python_bc(if_bhe_network_exist_python_bc_)
+#endif  // OGS_USE_PYTHON
+    {
+    }
     std::unique_ptr<MaterialPropertyLib::MaterialSpatialDistributionMap>
         media_map;
 
@@ -32,5 +54,11 @@ struct HeatTransportBHEProcessData final
 
     MeshLib::PropertyVector<int> const* _mesh_prop_materialIDs = nullptr;
     std::unordered_map<int, int> _map_materialID_to_BHE_ID{};
+    // if bhe network exists python boundary condition
+    bool if_bhe_network_exist_python_bc;
+#ifdef OGS_USE_PYTHON
+    //! Python object computing BC values.
+    BHEInflowPythonBoundaryConditionPythonSideInterface* py_bc_object;
+#endif
 };
 }  // namespace ProcessLib::HeatTransportBHE
