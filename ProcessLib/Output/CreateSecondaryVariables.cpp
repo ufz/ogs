85a395769f9d6d4670edf4f46554244ba051c136
Dmitri Naumov <github@naumov.de>
2019-11-03 16:18:09 +0100

    [PL] Out; Deprecate sec. vars. type attribute.
    Update docu and make the attribute optional.
    Issue a warning in case it is set.

--- ProcessLib/Output/CreateSecondaryVariables.cpp
+++ ProcessLib/Output/CreateSecondaryVariables.cpp
@@ -37,7 +37,13 @@ void createSecondaryVariables(
     {
         auto const type =
             //! \ogs_file_attr{prj__processes__process__secondary_variables__secondary_variable__type}
-            sec_var_config.getConfigAttribute<std::string>("type");
+            sec_var_config.getConfigAttributeOptional<std::string>("type");
+        if (type)
+        {
+            WARN(
+                "Secondary variable type specification is deprecated and is "
+                "ignored. All secondary variable types are 'static'.");
+        }
 
         auto const internal_name =
             //! \ogs_file_attr{prj__processes__process__secondary_variables__secondary_variable__internal_name}
