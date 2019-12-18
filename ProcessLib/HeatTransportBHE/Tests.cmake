fafb6373e1c35271219db09faf697714cccadf91
Shuang Chen <gechenshuang88@gmail.com>
2019-12-18 10:28:46 +0100

    update the benchmark input and result files
    Change the roughness of pipe to avoid the WARNING message in TESPy new version (0.1.3)
    format the python scripts
    update the benchmark test results
    
    relax the error tolerance
    
    [py] Formatted HeatTransportBHE_3D_3BHEs_array files.
    
    Manually and with yapf.
    
    [py] Removed duplicated instruction.
    modify benchmark input
    
    change the imported module; fixed local variables
    
    change the imported pandas module to only csv_read; import the local variables from the network csv files;
    Add comments in the Benchmark input file
    Delete the unused variables in the script

--- ProcessLib/HeatTransportBHE/Tests.cmake
+++ ProcessLib/HeatTransportBHE/Tests.cmake
@@ -94,7 +94,7 @@ AddTest(
     REQUIREMENTS OGS_USE_PYTHON AND NOT OGS_USE_MPI
     DIFF_DATA
     3bhes_1U_pcs_0_ts_10_t_7200.000000.vtu 3bhes_1U_pcs_0_ts_10_t_7200.000000.vtu temperature_soil temperature_soil 1e-12 1e-13
-    3bhes_1U_pcs_0_ts_10_t_7200.000000.vtu 3bhes_1U_pcs_0_ts_10_t_7200.000000.vtu temperature_BHE1 temperature_BHE1 1e-12 1e-14
-    3bhes_1U_pcs_0_ts_10_t_7200.000000.vtu 3bhes_1U_pcs_0_ts_10_t_7200.000000.vtu temperature_BHE2 temperature_BHE2 1e-12 1e-14
-    3bhes_1U_pcs_0_ts_10_t_7200.000000.vtu 3bhes_1U_pcs_0_ts_10_t_7200.000000.vtu temperature_BHE3 temperature_BHE3 1e-12 1e-14
+    3bhes_1U_pcs_0_ts_10_t_7200.000000.vtu 3bhes_1U_pcs_0_ts_10_t_7200.000000.vtu temperature_BHE1 temperature_BHE1 1e-10 1e-13
+    3bhes_1U_pcs_0_ts_10_t_7200.000000.vtu 3bhes_1U_pcs_0_ts_10_t_7200.000000.vtu temperature_BHE2 temperature_BHE2 1e-10 1e-13
+    3bhes_1U_pcs_0_ts_10_t_7200.000000.vtu 3bhes_1U_pcs_0_ts_10_t_7200.000000.vtu temperature_BHE3 temperature_BHE3 1e-10 1e-13
 )
--- Tests/Data/Parabolic/T/3D_3BHEs_array/pre/3bhes.py
+++ Tests/Data/Parabolic/T/3D_3BHEs_array/pre/3bhes.py
@@ -5,24 +5,30 @@
 # http://www.opengeosys.org/project/license
 ###
 
+# Execute this file to generate TESPy network csv files
 from tespy import cmp, con, nwk, hlp
 from tespy import nwkr
-from sub_btes import btes_para as bp
-
 import numpy as np
 import pandas as pd
-# %% network
-btes = nwk.network(fluids=['water'], T_unit='K', p_unit='bar', h_unit='kJ / kg', T_range=[273.25, 373.15], p_range=[1, 20], h_range=[1, 1000])
 
-#components
+# %% network
+btes = nwk.network(fluids=['water'],
+                   T_unit='K',
+                   p_unit='bar',
+                   h_unit='kJ / kg',
+                   T_range=[273.25, 373.15],
+                   p_range=[1, 20],
+                   h_range=[1, 1000])
+
+# components
 fc_in = cmp.source('from consumer inflow')
 fc_out = cmp.sink('from consumer outflow')
 
 pu = cmp.pump('pump')
 
-sp = cmp.splitter('splitter',num_out = 3)
+sp = cmp.splitter('splitter', num_out=3)
 
-#bhe:
+# bhe:
 bhe_name = 'BHE1'
 assert 'BHE1' in bhe_name, "BHE should be named with 'BHE1'"
 bhe1 = cmp.heat_exchanger_simple(bhe_name)
@@ -33,11 +39,11 @@ bhe_name = 'BHE3'
 assert 'BHE3' in bhe_name, "BHE should be named with 'BHE3'"
 bhe3 = cmp.heat_exchanger_simple(bhe_name)
 
-mg = cmp.merge('merge',num_in = 3)
+mg = cmp.merge('merge', num_in=3)
 
 cons = cmp.heat_exchanger_simple('consumer')
 
-#connections
+# connections
 # inlet
 fc_pu = con.connection(fc_in, 'out1', pu, 'in1')
 
@@ -55,10 +61,8 @@ mg_cons = con.connection(mg, 'out1', cons, 'in1')
 
 cons_fc = con.connection(cons, 'out1', fc_out, 'in1')
 
-btes.add_conns(fc_pu, pu_sp,
-sp_bhe1, sp_bhe2, sp_bhe3,
-bhe1_mg, bhe2_mg, bhe3_mg,
-mg_cons, cons_fc)
+btes.add_conns(fc_pu, pu_sp, sp_bhe1, sp_bhe2, sp_bhe3, bhe1_mg, bhe2_mg,
+               bhe3_mg, mg_cons, cons_fc)
 
 # busses
 heat = con.bus('consumer heat demand')
@@ -67,62 +71,61 @@ btes.add_busses(heat)
 
 # flow_char
 # provide volumetric flow in m^3 / s
-x = np.array([0.00, 0.001952885971862, 0.00390577194372, 0.005858657915586,
-  0.007811543887448, 0.00976442985931, 0.011717315831173,
-  0.013670201803035, 0.015623087774897, 0.017575973746759,
-  0.019528859718621, 0.021481745690483, 0.023434631662345,
-  0.025387517634207, 0.027340403606069, 0.029293289577931,
-  0.031246175549793, 0.033199061521655, 0.035151947493517,
-  0.037104833465379, 0.039057719437241, 0.041010605409104,
-  0.042963491380966, 0.044916377352828, 0.04686926332469,
-  0.048822149296552, 0.050775035268414, 0.052727921240276,
-  0.054680807212138, 0.056633693184
-  ])
+x = np.array([
+    0.00, 0.00001952885971862, 0.00390577194372, 0.005858657915586,
+    0.007811543887448, 0.00976442985931, 0.011717315831173, 0.013670201803035,
+    0.015623087774897, 0.017575973746759, 0.019528859718621, 0.021481745690483,
+    0.023434631662345, 0.025387517634207, 0.027340403606069, 0.029293289577931,
+    0.031246175549793, 0.033199061521655, 0.035151947493517, 0.037104833465379,
+    0.039057719437241, 0.041010605409104, 0.042963491380966, 0.044916377352828,
+    0.04686926332469, 0.048822149296552, 0.050775035268414, 0.052727921240276,
+    0.054680807212138, 0.056633693184
+])
 
 # provide head in Pa
-y = np.array([0.47782539, 0.47725723, 0.47555274, 0.47271192, 0.46873478,
-  0.46362130, 0.45737151, 0.44998538, 0.44146293, 0.43180416,
-  0.4220905, 0.40907762, 0.39600986, 0.38180578, 0.36646537,
-  0.34998863, 0.33237557, 0.31362618, 0.29374046, 0.27271841,
-  0.25056004, 0.22726535, 0.20283432, 0.17726697, 0.15056329,
-  0.12272329, 0.09374696, 0.06363430, 0.03238531, 0.00000000
-  ])*1e5
+y = np.array([
+    0.47782539, 0.47725723, 0.47555274, 0.47271192, 0.46873478, 0.46362130,
+    0.45737151, 0.44998538, 0.44146293, 0.43180416, 0.4220905, 0.40907762,
+    0.39600986, 0.38180578, 0.36646537, 0.34998863, 0.33237557, 0.31362618,
+    0.29374046, 0.27271841, 0.25056004, 0.22726535, 0.20283432, 0.17726697,
+    0.15056329, 0.12272329, 0.09374696, 0.06363430, 0.03238531, 0.00000000
+]) * 1e5
 
 f = hlp.dc_cc(x=x, y=y, is_set=True)
-pu.set_attr( flow_char=f)
+pu.set_attr(flow_char=f)
 
-#components paramerization
+# components paramerization
 # system inlet
-inflow_head = 2 #bar
+inflow_head = 2  # bar
 
-fc_pu.set_attr(p=inflow_head, m = 0.6, fluid={'water': 1})
+fc_pu.set_attr(p=inflow_head, m=0.6, fluid={'water': 1})
 
-#pump
-pu.set_attr(eta_s = 0.90)
+# pump
+pu.set_attr(eta_s=0.90)
 
-#bhes
-bhe1.set_attr(D=0.02733,L=100,ks=0.001)
-bhe2.set_attr(D=0.02733,L=100,ks=0.001)
-bhe3.set_attr(D=0.02733,L=100,ks=0.001)
+# bhes
+bhe1.set_attr(D=0.02733, L=100, ks=0.00001)
+bhe2.set_attr(D=0.02733, L=100, ks=0.00001)
+bhe3.set_attr(D=0.02733, L=100, ks=0.00001)
 
-#consumer
-cons.set_attr(D=0.2,L=20,ks=0.001)
+# consumer
+cons.set_attr(D=0.2, L=20, ks=0.00001)
 
-##connection parametrization
-#Tin:
-pu_sp.set_attr(h= con.ref(cons_fc, 1, 0))
+# connection parametrization
+# Tin:
+pu_sp.set_attr(h=con.ref(cons_fc, 1, 0))
 
-#for BHEs:
-#Tout:
-bhe1_mg.set_attr(T= 303.15)
-bhe2_mg.set_attr(T= 303.15)
-bhe3_mg.set_attr(T= 303.15)
+# for BHEs:
+# Tout:
+bhe1_mg.set_attr(T=303.15)
+bhe2_mg.set_attr(T=303.15)
+bhe3_mg.set_attr(T=303.15)
 
 # consumer heat demand
-heat.set_attr(P=-3000) #W
+heat.set_attr(P=-3000)  # W
 
-#solve
+# solve
 btes.set_printoptions(print_level='info')
 btes.solve('design')
-#save to csv:
+# save to csv:
 btes.save('tespy_nw', structure=True)
