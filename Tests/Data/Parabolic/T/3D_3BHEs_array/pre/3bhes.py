d02793dec9692645c2d1913bc16d5fab0036ba87
Dmitri Naumov <dmitri.naumov@ufz.de>
2020-01-03 18:44:52 +0100

    [PL] BHE; Update copyright years.

--- Tests/Data/Parabolic/T/3D_3BHEs_array/pre/3bhes.py
+++ Tests/Data/Parabolic/T/3D_3BHEs_array/pre/3bhes.py
@@ -1,5 +1,5 @@
 ###
-# Copyright (c) 2012-2019, OpenGeoSys Community (http://www.opengeosys.org)
+# Copyright (c) 2012-2020, OpenGeoSys Community (http://www.opengeosys.org)
 # Distributed under a Modified BSD License.
 # See accompanying file LICENSE.txt or
 # http://www.opengeosys.org/project/license
