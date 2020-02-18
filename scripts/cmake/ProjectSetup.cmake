2182deaf470a90a26dcd9f4926d33096aef0962a
Lars Bilke <lars.bilke@ufz.de>
2020-02-18 12:33:11 +0100

    [CMake] Check for 64 bit before Conan setup.

--- scripts/cmake/ProjectSetup.cmake
+++ scripts/cmake/ProjectSetup.cmake
@@ -1,10 +1,3 @@
-# Check requirements / supported configurations
-if(MSVC AND NOT HAVE_64_BIT AND NOT OGS_32_BIT)
-    message(FATAL_ERROR "Building OGS on Windows with 32-bit is not supported! \
-Either use the correct generator, e.g. 'Visual Studio 14 2015 Win64' or define \
-'-DOGS_32_BIT=ON' if you know what you are doing.")
-endif()
-
 # Set build directories
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/bin)
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/lib)
