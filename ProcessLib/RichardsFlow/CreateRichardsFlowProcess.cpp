f38a5e5bde2b3f0f0b87941d519a2d0aecd2f5bb
Thomas Fischer <thomas.fischer@ufz.de>
2020-03-05 10:34:51 +0100

    [PL/RichardsFlow] Add checkMPLProperties.

--- ProcessLib/RichardsFlow/CreateRichardsFlowProcess.cpp
+++ ProcessLib/RichardsFlow/CreateRichardsFlowProcess.cpp
@@ -23,6 +23,40 @@ namespace ProcessLib
 {
 namespace RichardsFlow
 {
+void checkMPLProperties(
+    MeshLib::Mesh const& mesh,
+    MaterialPropertyLib::MaterialSpatialDistributionMap const& media_map)
+{
+    std::array const requiredPropertyMedium = {
+        MaterialPropertyLib::PropertyType::reference_temperature,
+        MaterialPropertyLib::PropertyType::porosity,
+        MaterialPropertyLib::PropertyType::saturation,
+        MaterialPropertyLib::PropertyType::relative_permeability,
+        MaterialPropertyLib::PropertyType::permeability};
+
+    std::array const requiredPropertyLiquidPhase = {
+        MaterialPropertyLib::PropertyType::viscosity,
+        MaterialPropertyLib::PropertyType::density};
+
+    std::array const requiredPropertySolidPhase = {
+        MaterialPropertyLib::PropertyType::storage};
+
+    for (auto const& element : mesh.getElements())
+    {
+        auto const element_id = element->getID();
+
+        auto const& medium = *media_map.getMedium(element_id);
+        MaterialPropertyLib::checkRequiredProperties(
+            medium, requiredPropertyMedium);
+
+        MaterialPropertyLib::checkRequiredProperties(
+            medium.phase("AqueousLiquid"), requiredPropertyLiquidPhase);
+
+        MaterialPropertyLib::checkRequiredProperties(
+            medium.phase("Solid"), requiredPropertySolidPhase);
+    }
+}
+
 std::unique_ptr<Process> createRichardsFlowProcess(
     std::string name,
     MeshLib::Mesh& mesh,
@@ -76,6 +110,10 @@ std::unique_ptr<Process> createRichardsFlowProcess(
 
     auto media_map =
         MaterialPropertyLib::createMaterialSpatialDistributionMap(media, mesh);
+
+    DBUG("Check the media properties of RichardsFlow process ...");
+    checkMPLProperties(mesh, *media_map.get());
+    DBUG("Media properties verified.");
 
     RichardsFlowProcessData process_data{
         std::move(media_map), specific_body_force, has_gravity, mass_lumping};
