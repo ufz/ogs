922fe60f9e31b88d286054f013f3b9b8fde37b5e
tengfeideng <dengtengfei2016@hotmail.com>
2020-03-06 08:58:06 +0100

    some changes in test file

--- ProcessLib/SmallDeformation/Tests.cmake
+++ ProcessLib/SmallDeformation/Tests.cmake
@@ -60,7 +60,7 @@ if (OGS_USE_MFRONT)
     OgsTest(PROJECTFILE Mechanics/Linear/MFront/square_1e0_orthotropic_xyz.prj)
     OgsTest(PROJECTFILE Mechanics/Linear/MFront/square_1e0_orthotropic_45xy_z.prj)
     OgsTest(PROJECTFILE Mechanics/Linear/MFront/square_1e0_orthotropic_y-xz.prj)
-    OgsTest(PROJECTFILE Mechanics/Linear/MFront/slope.prj)
+    OgsTest(PROJECTFILE Mechanics/MohrCoulombAbboSloan/slope.prj)
 # Linear elastic, no internal state variables, no external state variables.
 AddTest(
     NAME Mechanics_SDL_disc_with_hole_mfront
@@ -104,13 +104,16 @@ AddTest(
     ../../ring_plane_strain_pcs_0_ts_1_t_1.000000.vtu ring_plane_strain_pcs_0_ts_1_t_1.000000.vtu sigma sigma 1e-15 0
 )
 
+#slope stability test with MFront Mohr Coulomb implementation
+#Todo Tengfei ... input correct reference files
 AddTest(
-    NAME SmallDeformation_slope_stability_mfront
-    PATH Mechanics/Linear/MFront/slope_stability
+    NAME Mechanics_slope_stability_mfront
+    PATH Mechanics/MohrCoulombAbboSloan
     EXECUTABLE ogs
     EXECUTABLE_ARGS slope.prj
     TESTER vtkdiff
     REQUIREMENTS NOT OGS_USE_MPI
+    # See also the prj file.
     DIFF_DATA
     ../../slope_pcs_0_ts_300_t_3.000000.vtu slope_pcs_0_ts_300_t_3.000000.vtu displacement displacement 1e-14 0
 )
