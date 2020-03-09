b4f0a742c708590e8b38263363a9425f803b878f
Thomas Fischer <thomas.fischer@ufz.de>
2020-03-09 14:07:00 +0100

    [T] Enable CurveProperty.

--- ProcessLib/RichardsFlow/Tests.cmake
+++ ProcessLib/RichardsFlow/Tests.cmake
@@ -1,13 +1,14 @@
-#AddTest(
-#        NAME 2D_RichardsFlow_h_us_quad_ogs5
-#        PATH Parabolic/Richards
-#        EXECUTABLE ogs
-#        EXECUTABLE_ARGS RichardsFlow_2d_compare_ogs5.prj
-#        TESTER vtkdiff
-#        DIFF_DATA
-#        h_us_quad_1000.vtu richards_pcs_0_ts_100_t_100.000000.vtu PRESSURE1 pressure 1e-1 1e-1
-#    REQUIREMENTS NOT OGS_USE_MPI
-#)
+AddTest(
+        NAME 2D_RichardsFlow_h_us_quad_ogs5
+        PATH Parabolic/Richards
+        EXECUTABLE ogs
+        EXECUTABLE_ARGS RichardsFlow_2d_compare_ogs5.prj
+        TESTER vtkdiff
+        DIFF_DATA
+        h_us_quad_1000.vtu richards_pcs_0_ts_100_t_100.000000.vtu PRESSURE1 pressure 1e-1 1e-1
+    REQUIREMENTS NOT OGS_USE_MPI
+)
+
 #AddTest(
 #        NAME 2D_RichardsFlow_h_us_quad_small
 #        PATH Parabolic/Richards
