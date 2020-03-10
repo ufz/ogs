3e0c153ad1808bc83e395d0bcb72f1a42ab30a9d
Thomas Fischer <thomas.fischer@ufz.de>
2020-03-10 12:53:56 +0100

    [T/RF] Enable curve tests.

--- ProcessLib/RichardsFlow/Tests.cmake
+++ ProcessLib/RichardsFlow/Tests.cmake
@@ -9,59 +9,60 @@ AddTest(
     REQUIREMENTS NOT OGS_USE_MPI
 )
 
-#AddTest(
-#        NAME 2D_RichardsFlow_h_us_quad_small
-#        PATH Parabolic/Richards
-#        EXECUTABLE ogs
-#        EXECUTABLE_ARGS RichardsFlow_2d_small.prj
-#        TESTER vtkdiff
-#        DIFF_DATA
-#        ref_t_1600.000000.vtu richards_pcs_0_ts_1100_t_1600.000000.vtu pressure pressure 1e-8 1e-8
-#    REQUIREMENTS NOT OGS_USE_MPI
-#)
-#AddTest(
-#        NAME LARGE_2D_RichardsFlow_h_us_quad
-#        PATH Parabolic/Richards
-#        RUNTIME 90
-#        EXECUTABLE ogs
-#        EXECUTABLE_ARGS RichardsFlow_2d_large.prj
-#        TESTER vtkdiff
-#        DIFF_DATA
-#        ref_t_20000.000000.vtu richards_pcs_0_ts_18200_t_20000.000000.vtu pressure pressure 1e-8 1e-8
-#    REQUIREMENTS NOT OGS_USE_MPI
-#)
-#
-#AddTest(
-#    NAME 2D_RichardsFlow_h_us_quad_small_PID_adaptive_dt
-#    PATH Parabolic/Richards
-#    EXECUTABLE ogs
-#    EXECUTABLE_ARGS RichardsFlow_2d_small_PID_adaptive_dt.prj
-#    TESTER vtkdiff
-#    DIFF_DATA
-#    ref_t_1600.000000.vtu richards_pcs_0_ts_803_t_1600.000000.vtu pressure pressure 1e-8 1e-3
-## The following three comparisons are used just to check whether the output is
-## made at the fixed times of 50, 100 and 500, which are given in the project
-## file of RichardsFlow_2d_small_adaptive_dt.prj
-#    richards_pcs_0_ts_28_spec_t_50.000000.vtu richards_pcs_0_ts_28_t_50.000000.vtu pressure pressure 1e-10 1e-10
-#    richards_pcs_0_ts_53_spec_t_100.000000.vtu richards_pcs_0_ts_53_t_100.000000.vtu pressure pressure 1e-10 1e-10
-#    richards_pcs_0_ts_253_spec_t_500.000000.vtu richards_pcs_0_ts_253_t_500.000000.vtu pressure pressure 1e-10 1e-10
-#    REQUIREMENTS NOT OGS_USE_MPI
-#)
-#
-#AddTest(
-#    NAME 2D_RichardsFlow_h_us_quad_small_iteration_adaptive_dt
-#    PATH Parabolic/Richards
-#    EXECUTABLE ogs
-#    EXECUTABLE_ARGS RichardsFlow_2d_small_iteration_adaptive_dt.prj
-#    REQUIREMENTS NOT OGS_USE_MPI
-#    RUNTIME 51
-#    # No vtkdiff comparison here, because of the different file names for
-#    # different machines, which again is due to the adaptive time stepping
-#    # scheme. When the output file format can be specified in the project files,
-#    # e.g. in the form Richards_%t where %t is the current time, the output will
-#    # no longer be ambiguous.
-#)
-#
+AddTest(
+        NAME 2D_RichardsFlow_h_us_quad_small
+        PATH Parabolic/Richards
+        EXECUTABLE ogs
+        EXECUTABLE_ARGS RichardsFlow_2d_small.prj
+        TESTER vtkdiff
+        DIFF_DATA
+        ref_t_1600.000000.vtu richards_pcs_0_ts_1100_t_1600.000000.vtu pressure pressure 1e-8 1e-8
+    REQUIREMENTS NOT OGS_USE_MPI
+)
+
+AddTest(
+        NAME LARGE_2D_RichardsFlow_h_us_quad
+        PATH Parabolic/Richards
+        RUNTIME 90
+        EXECUTABLE ogs
+        EXECUTABLE_ARGS RichardsFlow_2d_large.prj
+        TESTER vtkdiff
+        DIFF_DATA
+        ref_t_20000.000000.vtu richards_pcs_0_ts_18200_t_20000.000000.vtu pressure pressure 1e-8 1e-8
+    REQUIREMENTS NOT OGS_USE_MPI
+)
+
+AddTest(
+    NAME 2D_RichardsFlow_h_us_quad_small_PID_adaptive_dt
+    PATH Parabolic/Richards
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS RichardsFlow_2d_small_PID_adaptive_dt.prj
+    TESTER vtkdiff
+    DIFF_DATA
+    ref_t_1600.000000.vtu richards_pcs_0_ts_803_t_1600.000000.vtu pressure pressure 1e-8 1e-3
+# The following three comparisons are used just to check whether the output is
+# made at the fixed times of 50, 100 and 500, which are given in the project
+# file of RichardsFlow_2d_small_adaptive_dt.prj
+    richards_pcs_0_ts_28_spec_t_50.000000.vtu richards_pcs_0_ts_28_t_50.000000.vtu pressure pressure 1e-10 1e-10
+    richards_pcs_0_ts_53_spec_t_100.000000.vtu richards_pcs_0_ts_53_t_100.000000.vtu pressure pressure 1e-10 1e-10
+    richards_pcs_0_ts_253_spec_t_500.000000.vtu richards_pcs_0_ts_253_t_500.000000.vtu pressure pressure 1e-10 1e-10
+    REQUIREMENTS NOT OGS_USE_MPI
+)
+
+AddTest(
+    NAME 2D_RichardsFlow_h_us_quad_small_iteration_adaptive_dt
+    PATH Parabolic/Richards
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS RichardsFlow_2d_small_iteration_adaptive_dt.prj
+    REQUIREMENTS NOT OGS_USE_MPI
+    RUNTIME 51
+    # No vtkdiff comparison here, because of the different file names for
+    # different machines, which again is due to the adaptive time stepping
+    # scheme. When the output file format can be specified in the project files,
+    # e.g. in the form Richards_%t where %t is the current time, the output will
+    # no longer be ambiguous.
+)
+
 # Comparison test for richards mechanics w/o deformations.
 AddTest(
     NAME RichardsMechanics_RichardsFlow_2d_richardsflow
