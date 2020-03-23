838b1072ffd17f3aec86446b21528dadea4871f6
Wenqing Wang <wenqing.wang@ufz.de>
2020-03-23 15:37:23 +0100

    [MPL] Added a class of RegularizedCapillaryVanGenuchten

--- MaterialLib/MPL/Properties/CapillaryPressureSaturation/CapillaryPressureVanGenuchten.cpp
+++ MaterialLib/MPL/Properties/CapillaryPressureSaturation/CapillaryPressureVanGenuchten.cpp
@@ -15,9 +15,21 @@
 #include <cmath>
 
 #include "MaterialLib/MPL/Medium.h"
+#include "MaterialLib/MPL/VariableType.h"
 
 namespace MaterialPropertyLib
 {
+void checkVanGenuchtenExponentRange(const double m)
+{
+    if (!(m > 0 && m < 1))
+    {
+        OGS_FATAL(
+            "The exponent value m = %g of van Genuchten saturation model, is "
+            "out of its range of (0, 1)",
+            m);
+    }
+}
+
 CapillaryPressureVanGenuchten::CapillaryPressureVanGenuchten(
     double const residual_liquid_saturation,
     double const residual_gas_saturation, double const exponent,
@@ -28,13 +40,7 @@ CapillaryPressureVanGenuchten::CapillaryPressureVanGenuchten(
       _p_b(entry_pressure),
       _pc_max(max_capillary_pressure)
 {
-    if (!(_m > 0 && _m < 1))
-    {
-        OGS_FATAL(
-            "The exponent value m = %g of van Genuchten saturation model, is "
-            "out of its range of (0, 1)",
-            _m);
-    }
+    checkVanGenuchtenExponentRange(_m);
 }
 
 PropertyDataType CapillaryPressureVanGenuchten::value(
@@ -100,6 +106,137 @@ PropertyDataType CapillaryPressureVanGenuchten::d2Value(
            (_m * _m * (S - _residual_saturation) * (S - _residual_saturation)) *
            std::pow(1 - val1, -_m - 1) * std::pow(val1, _m - 1) *
            ((1 - _m * _m) * val1 + _m - 1);
+}
+
+CapillaryPressureRegularizedVanGenuchten::
+    CapillaryPressureRegularizedVanGenuchten(
+        double const residual_liquid_saturation,
+        double const residual_gas_saturation,
+        double const exponent,
+        double const entry_pressure,
+        double const max_capillary_pressure)
+    : _saturation_nonwet_r(residual_gas_saturation),
+      _residual_saturation(residual_liquid_saturation),
+      _maximuml_saturation(1. - residual_gas_saturation),
+      _m(exponent),
+      _p_b(entry_pressure),
+      _pc_max(max_capillary_pressure)
+{
+    checkVanGenuchtenExponentRange(_m);
+}
+
+PropertyDataType CapillaryPressureRegularizedVanGenuchten::value(
+    VariableArray const& variable_array,
+    ParameterLib::SpatialPosition const& /*pos*/, double const /*t*/,
+    double const /*dt*/) const
+{
+    const double saturation = std::get<double>(
+        variable_array[static_cast<int>(Variable::liquid_saturation)]);
+
+    double const Sg = 1 - saturation;
+    if (!(Sg < _saturation_nonwet_r || Sg > 1 - _residual_saturation))
+    {
+        return getPcBarvGSg(Sg);
+    }
+    if (Sg < _saturation_nonwet_r)
+    {
+        return getPcBarvGSg(_saturation_nonwet_r) +
+               getdPcdSvGBar(_saturation_nonwet_r) *
+                   (Sg - _saturation_nonwet_r);
+    }
+
+    return getPcBarvGSg(1 - _residual_saturation) +
+           getdPcdSvGBar(1 - _residual_saturation) *
+               (Sg - 1 + _residual_saturation);
+}
+
+PropertyDataType CapillaryPressureRegularizedVanGenuchten::dValue(
+    VariableArray const& variable_array, Variable const /*primary_variable*/,
+    ParameterLib::SpatialPosition const& /*pos*/, double const /*t*/,
+    double const /*dt*/) const
+{
+    const double saturation = std::get<double>(
+        variable_array[static_cast<int>(Variable::liquid_saturation)]);
+
+    double const Sg = 1 - saturation;
+    if (Sg >= _saturation_nonwet_r && Sg <= 1 - _residual_saturation)
+    {
+        return -getdPcdSvGBar(Sg);
+    }
+    if (Sg < _saturation_nonwet_r)
+    {
+        return -getdPcdSvGBar(_saturation_nonwet_r);
+    }
+
+    return -getdPcdSvGBar(1 - _residual_saturation);
+}
+
+PropertyDataType CapillaryPressureRegularizedVanGenuchten::d2Value(
+    VariableArray const& variable_array, Variable const primary_variable1,
+    Variable const /*primary_variable2*/,
+    ParameterLib::SpatialPosition const& pos, double const t,
+    double const dt) const
+{
+    const double saturation = std::get<double>(
+        variable_array[static_cast<int>(Variable::liquid_saturation)]);
+
+    const double perturbation =
+        std::sqrt(std::numeric_limits<double>::epsilon());
+    VariableArray variable_array1;
+    variable_array1[static_cast<int>(
+        MaterialPropertyLib::Variable::liquid_saturation)] =
+        saturation + perturbation;
+
+    PropertyDataType val1 =
+        dValue(variable_array1, primary_variable1, pos, t, dt);
+    PropertyDataType val0 =
+        dValue(variable_array, primary_variable1, pos, t, dt);
+
+    return (std::get<double>(val1) - std::get<double>(val0)) / perturbation;
+}
+
+double CapillaryPressureRegularizedVanGenuchten::getPcBarvGSg(
+    double const Sg) const
+{
+    double const Sg_r = _saturation_nonwet_r;
+    double const S_lr = _residual_saturation;
+    double const S_bar = getSBar(Sg);
+    return getPcvGSg(S_bar) - getPcvGSg(Sg_r + (1 - Sg_r - S_lr) * _xi / 2);
+}
+
+double CapillaryPressureRegularizedVanGenuchten::getSBar(double const Sg) const
+{
+    double const Sg_r = _saturation_nonwet_r;
+    double const S_lr = _residual_saturation;
+    return Sg_r + (1 - _xi) * (Sg - Sg_r) + 0.5 * _xi * (1 - Sg_r - S_lr);
+}
+
+double CapillaryPressureRegularizedVanGenuchten::getPcvGSg(
+    double const Sg) const
+{
+    double const Sg_r = _saturation_nonwet_r;
+    double const S_lr = _residual_saturation;
+    double const S_le = (1 - Sg - S_lr) / (1 - Sg_r - _residual_saturation);
+    return _p_b * std::pow(std::pow(S_le, (-1.0 / _m)) - 1.0, 1.0 - _m);
+}
+
+double CapillaryPressureRegularizedVanGenuchten::getdPcdSvGBar(
+    double const Sg) const
+{
+    double S_bar = getSBar(Sg);
+    return getdPcdSvG(S_bar) * (1 - _xi);
+}
+
+double CapillaryPressureRegularizedVanGenuchten::getdPcdSvG(
+    const double Sg) const
+{
+    double const Sg_r = _saturation_nonwet_r;
+    double const S_lr = _residual_saturation;
+    double const n = 1 / (1 - _m);
+    double const S_le = (1 - Sg - S_lr) / (1 - Sg_r - S_lr);
+    return _p_b * (1 / (_m * n)) * (1 / (1 - S_lr - Sg_r)) *
+           std::pow(std::pow(S_le, (-1 / _m)) - 1, (1 / n) - 1) *
+           std::pow(S_le, (-1 / _m)) / S_le;
 }
 
 }  // namespace MaterialPropertyLib
--- MaterialLib/MPL/Properties/CapillaryPressureSaturation/CapillaryPressureVanGenuchten.h
+++ MaterialLib/MPL/Properties/CapillaryPressureSaturation/CapillaryPressureVanGenuchten.h
@@ -69,4 +69,84 @@ private:
     double const _p_b;                  ///< Entry pressure.
     double const _pc_max;               ///< Maximum capillary pressure.
 };
+
+/**
+ *  \brief This class handles the computation of \f$ p_c(S) \f$ with the
+ * capillary pressure regularization.
+ *
+ *   For the regularized van Genuchten model, please refer to
+ *  \cite marchand2013fully.
+ *
+ *   For the van Genuchen capillary pressure mode,
+ *   \sa MaterialPropertyLib::CapillaryPressureVanGenuchten
+ */
+class CapillaryPressureRegularizedVanGenuchten final : public Property
+{
+public:
+    CapillaryPressureRegularizedVanGenuchten(
+        double const residual_liquid_saturation,
+        double const residual_gas_saturation,
+        double const exponent,
+        double const entry_pressure,
+        double const max_capillary_pressure);
+
+    void setScale(
+        std::variant<Medium*, Phase*, Component*> scale_pointer) override
+    {
+        if (!std::holds_alternative<Medium*>(scale_pointer))
+        {
+            OGS_FATAL(
+                "The property 'CapillaryPressureRegularizedVanGenuchten' is "
+                "implemented on the "
+                "'media' scale only.");
+        }
+        this->_medium = std::get<Medium*>(scale_pointer);
+    }
+
+    /// gets \f$ p_c(S) \f$.
+    PropertyDataType value(VariableArray const& variable_array,
+                           ParameterLib::SpatialPosition const& pos,
+                           double const t,
+                           double const dt) const override;
+
+    /// gets \f$ \frac{\partial p_c(S)}{\partial  S} \f$
+    PropertyDataType dValue(VariableArray const& variable_array,
+                            Variable const variable,
+                            ParameterLib::SpatialPosition const& pos,
+                            double const t,
+                            double const dt) const override;
+
+    /// gets \f$ \frac{\partial^2 p_c(S)}{\partial  S^2} \f$
+    PropertyDataType d2Value(VariableArray const& variable_array,
+                             Variable const variable1, Variable const variable2,
+                             ParameterLib::SpatialPosition const& pos,
+                             double const t, double const dt) const override;
+
+private:
+    Medium* _medium = nullptr;
+    const double _saturation_nonwet_r;  ///< Residual saturation of nonwetting
+    /// Residual saturation of liquid phase.
+    double const _residual_saturation;
+    double const _maximuml_saturation;  ///< Maximum saturation of liquid phase.
+    double const _m;                    ///< Exponent index.
+    double const _p_b;                  ///< Entry pressure.
+    double const _pc_max;               ///< Maximum capillary pressure.
+    /// parameter in regularized van Genuchten model
+    static constexpr double _xi = 1e-5;
+
+    /// Gets regularized capillary pressure via the saturation of the
+    /// non-wetting phase.
+    double getPcBarvGSg(double const Sg) const;
+    /// Gets regularized saturation via the saturation of the non-wetting
+    /// phase.
+    double getSBar(double const Sg) const;
+    /// Gets capillary pressure via the non-wetting phase saturation with the
+    /// original van Genuchten model.
+    double getPcvGSg(double const Sg) const;
+    /// Gets \f$\dfrac{\partial}{\partial {\bar S}_g }\f$.
+    double getdPcdSvGBar(double const Sg) const;
+    /// Gets \f$\dfrac{\partial}{\partial {S}_g }\f$.
+    double getdPcdSvG(double const Sg) const;
+};
+
 }  // namespace MaterialPropertyLib
