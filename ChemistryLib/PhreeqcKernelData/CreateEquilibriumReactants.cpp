15aa56ddc1e94406ce2c35f25c142df80aa9c573
renchao_lu <renchao.lu@gmail.com>
2020-03-28 21:34:48 +0100

    [CL] EquilibriumReactants creation.

---
+++ ChemistryLib/PhreeqcKernelData/CreateEquilibriumReactants.cpp
@@ -0,0 +1,65 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2020, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#include <boost/optional/optional.hpp>
+#include <vector>
+
+#include "BaseLib/ConfigTree.h"
+#include "CreateEquilibriumReactants.h"
+#include "EquilibriumReactants.h"
+#include "MeshLib/Mesh.h"
+
+namespace ChemistryLib
+{
+namespace PhreeqcKernelData
+{
+std::unique_ptr<EquilibriumReactants> createEquilibriumReactants(
+    boost::optional<BaseLib::ConfigTree> const& config,
+    MeshLib::Mesh const& mesh)
+{
+    if (!config)
+    {
+        return nullptr;
+    }
+
+    std::vector<PhaseComponent> phase_components;
+    for (
+        auto const& phase_component_config :
+        //! \ogs_file_param{prj__chemical_system__equilibrium_phases__equilibrium_phase}
+        config->getConfigSubtreeList("equilibrium_phase"))
+    {
+        auto name =
+            //! \ogs_file_param{prj__chemical_system__equilibrium_phases__equilibrium_phase__name}
+            phase_component_config.getConfigParameter<std::string>("name");
+
+        double const initial_amount =
+            //! \ogs_file_param{prj__chemical_system__equilibrium_phases__equilibrium_phase__initial_amount}
+            phase_component_config.getConfigParameter<double>("initial_amount");
+
+        double const saturation_index =
+            //! \ogs_file_param{prj__chemical_system__equilibrium_phases__equilibrium_phase__saturation_index}
+            phase_component_config.getConfigParameter<double>(
+                "saturation_index");
+
+        auto amount = MeshLib::getOrCreateMeshProperty<double>(
+            const_cast<MeshLib::Mesh&>(mesh),
+            name,
+            MeshLib::MeshItemType::Node,
+            1);
+        std::fill(amount->begin(), amount->end(), initial_amount);
+
+        phase_components.emplace_back(
+            std::move(name), initial_amount, saturation_index);
+    }
+
+    return std::make_unique<EquilibriumReactants>(phase_components);
+}
+}  // namespace PhreeqcKernelData
+}  // namespace ChemistryLib
---
+++ ChemistryLib/PhreeqcKernelData/CreateEquilibriumReactants.h
@@ -0,0 +1,36 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2020, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#pragma once
+
+#include <boost/optional/optional_fwd.hpp>
+#include <memory>
+
+#include "EquilibriumReactants.h"
+
+namespace BaseLib
+{
+class ConfigTree;
+}
+
+namespace MeshLib
+{
+class Mesh;
+}
+
+namespace ChemistryLib
+{
+namespace PhreeqcKernelData
+{
+std::unique_ptr<EquilibriumReactants> createEquilibriumReactants(
+    boost::optional<BaseLib::ConfigTree> const& config,
+    MeshLib::Mesh const& mesh);
+}
+}  // namespace ChemistryLib
