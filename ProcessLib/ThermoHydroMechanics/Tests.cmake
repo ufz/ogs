d8854a36d7322ac5050eeb6f8468d0b411f75c1d
joerg <joerg@debian>
2020-03-29 16:20:46 +0200

    add linear shape functions for displacement
    add benchmark files for linear displacement elements
    
    add benchmark files for linear displacement elements
    
    rescaling axis
    
    add benchmark files for linear displacement elements (THM process)
    
    add linear shape functions for displacement
    
    add benchmark files for linear displacement elements
    
    rescaling axis
    
    delete obsolete files

--- ProcessLib/ThermoHydroMechanics/Tests.cmake
+++ ProcessLib/ThermoHydroMechanics/Tests.cmake
@@ -68,3 +68,20 @@ AddTest(
     expected_square_1e0_pcs_0_ts_10_t_50000.000000.vtu square_1e0_pcs_0_ts_10_t_50000.000000.vtu epsilon epsilon 1e-5 1e-5
     expected_square_1e0_pcs_0_ts_10_t_50000.000000.vtu square_1e0_pcs_0_ts_10_t_50000.000000.vtu sigma sigma 1e-5 1e-5
 )
+# ThermoHydroMechanics; Small deformation, linear poroelastic, point heat source consolidation, linear elements for displacement
+AddTest(
+    NAME LARGE_ThermoHydroMechanics_square_1e2_point_heat_injection_lin
+    PATH ThermoHydroMechanics/Linear/Point_injection
+    RUNTIME 400
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS square_1e2_lin.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    DIFF_DATA
+    expected_square_1e0_lin_pcs_0_ts_10_t_50000.000000.vtu square_1e0_lin_pcs_0_ts_10_t_50000.000000.vtu displacement displacement 1e-5 1e-5
+    expected_square_1e0_lin_pcs_0_ts_10_t_50000.000000.vtu square_1e0_lin_pcs_0_ts_10_t_50000.000000.vtu pressure pressure 1e-5 1e-5
+    expected_square_1e0_lin_pcs_0_ts_10_t_50000.000000.vtu square_1e0_lin_pcs_0_ts_10_t_50000.000000.vtu temperature temperature 1e-5 1e-5
+    expected_square_1e0_lin_pcs_0_ts_10_t_50000.000000.vtu square_1e0_lin_pcs_0_ts_10_t_50000.000000.vtu epsilon epsilon 1e-5 1e-5
+    expected_square_1e0_lin_pcs_0_ts_10_t_50000.000000.vtu square_1e0_lin_pcs_0_ts_10_t_50000.000000.vtu sigma sigma 1e-5 1e-5
+)
