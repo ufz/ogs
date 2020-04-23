306725644c0e06d5124448e726928e3f5ed52ec5
Dmitri Naumov <github@naumov.de>
2020-04-23 15:24:12 +0200

    [T/TH2pp] HeatPipe; Update tests for BE.
    New results are slightly different and well within
    the comparison done in the heat-pipe-problem.pdf description.

--- ProcessLib/ThermalTwoPhaseFlowWithPP/Tests.cmake
+++ ProcessLib/ThermalTwoPhaseFlowWithPP/Tests.cmake
@@ -1,15 +1,16 @@
 AddTest(
     NAME 2D_Thermal_TwoPhase_heatpipe
     PATH Parabolic/ThermalTwoPhaseFlowPP/HeatPipe
+    RUNTIME 10
     EXECUTABLE ogs
     EXECUTABLE_ARGS Twophase_HeatPipe_quad_curve_small.prj
     TESTER vtkdiff
     REQUIREMENTS NOT OGS_USE_MPI
     DIFF_DATA
-    ref_t_10000.000000.vtu thermaltwophaseflow_small_pcs_0_ts_10_t_10000.000000.vtu capillary_pressure capillary_pressure 1e-8 1e-10
-    ref_t_10000.000000.vtu thermaltwophaseflow_small_pcs_0_ts_10_t_10000.000000.vtu gas_pressure gas_pressure 1e-8 1e-10
-    ref_t_10000.000000.vtu thermaltwophaseflow_small_pcs_0_ts_10_t_10000.000000.vtu saturation saturation 1e-8 1e-10
-    ref_t_10000.000000.vtu thermaltwophaseflow_small_pcs_0_ts_10_t_10000.000000.vtu temperature temperature 1e-8 1e-10
+    ref_t_10000.000000.vtu thermaltwophaseflow_small_pcs_0_ts_100_t_10000.000000.vtu capillary_pressure capillary_pressure 1e-8 1e-10
+    ref_t_10000.000000.vtu thermaltwophaseflow_small_pcs_0_ts_100_t_10000.000000.vtu gas_pressure gas_pressure 1e-8 1e-10
+    ref_t_10000.000000.vtu thermaltwophaseflow_small_pcs_0_ts_100_t_10000.000000.vtu saturation saturation 1e-8 1e-10
+    ref_t_10000.000000.vtu thermaltwophaseflow_small_pcs_0_ts_100_t_10000.000000.vtu temperature temperature 1e-8 1e-10
 )
 AddTest(
     NAME LARGE_2D_Thermal_TwoPhase_heatpipe
@@ -20,8 +21,8 @@ AddTest(
     TESTER vtkdiff
     REQUIREMENTS NOT OGS_USE_MPI
     DIFF_DATA
-    ref_t_1100000.000000.vtu thermaltwophaseflow_large_pcs_0_ts_2000_t_1100000.000000.vtu capillary_pressure capillary_pressure 5e-6 1e-10
-    ref_t_1100000.000000.vtu thermaltwophaseflow_large_pcs_0_ts_2000_t_1100000.000000.vtu gas_pressure gas_pressure 1e-8 1e-10
-    ref_t_1100000.000000.vtu thermaltwophaseflow_large_pcs_0_ts_2000_t_1100000.000000.vtu saturation saturation 1e-8 1e-10
-    ref_t_1100000.000000.vtu thermaltwophaseflow_large_pcs_0_ts_2000_t_1100000.000000.vtu temperature temperature 1e-8 1e-10
+    ref_t_1100000.000000.vtu thermaltwophaseflow_large_pcs_0_ts_1100_t_1100000.000000.vtu capillary_pressure capillary_pressure 5e-6 1e-10
+    ref_t_1100000.000000.vtu thermaltwophaseflow_large_pcs_0_ts_1100_t_1100000.000000.vtu gas_pressure gas_pressure 1e-8 1e-10
+    ref_t_1100000.000000.vtu thermaltwophaseflow_large_pcs_0_ts_1100_t_1100000.000000.vtu saturation saturation 1e-8 1e-10
+    ref_t_1100000.000000.vtu thermaltwophaseflow_large_pcs_0_ts_1100_t_1100000.000000.vtu temperature temperature 1e-8 1e-10
 )
