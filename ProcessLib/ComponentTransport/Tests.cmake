8009fcba220b922509d237d93672173fbd0ad2be
Thomas Fischer <thomas.fischer@ufz.de>
2020-05-14 10:30:21 +0200

    [T/ComponentTransport] Fix ConTracer.

--- ProcessLib/ComponentTransport/Tests.cmake
+++ ProcessLib/ComponentTransport/Tests.cmake
@@ -946,26 +946,26 @@ AddTest(
     REQUIREMENTS NOT OGS_USE_MPI
     RUNTIME 10
     DIFF_DATA
-    ConTracer_1d_pcs_1_ts_33_t_118800.000000_expected.vtu ConTracer_1d_pcs_1_ts_33_t_118800.000000.vtu pressure pressure 1e-6 1e-10
-    ConTracer_1d_pcs_1_ts_66_t_237600.000000_expected.vtu ConTracer_1d_pcs_1_ts_66_t_237600.000000.vtu pressure pressure 1e-6 1e-10
-    ConTracer_1d_pcs_1_ts_99_t_356400.000000_expected.vtu ConTracer_1d_pcs_1_ts_99_t_356400.000000.vtu pressure pressure 1e-6 1e-10
-    ConTracer_1d_pcs_1_ts_132_t_475200.000000_expected.vtu ConTracer_1d_pcs_1_ts_132_t_475200.000000.vtu pressure pressure 1e-6 1e-10
-    ConTracer_1d_pcs_1_ts_165_t_594000.000000_expected.vtu ConTracer_1d_pcs_1_ts_165_t_594000.000000.vtu pressure pressure 1e-6 1e-10
-    ConTracer_1d_pcs_1_ts_198_t_712800.000000_expected.vtu ConTracer_1d_pcs_1_ts_198_t_712800.000000.vtu pressure pressure 1e-6 1e-10
-    ConTracer_1d_pcs_1_ts_231_t_831600.000000_expected.vtu ConTracer_1d_pcs_1_ts_231_t_831600.000000.vtu pressure pressure 1e-6 1e-10
-    ConTracer_1d_pcs_1_ts_264_t_950400.000000_expected.vtu ConTracer_1d_pcs_1_ts_264_t_950400.000000.vtu pressure pressure 1e-6 1e-10
-    ConTracer_1d_pcs_1_ts_297_t_1069200.000000_expected.vtu ConTracer_1d_pcs_1_ts_297_t_1069200.000000.vtu pressure pressure 1e-6 1e-10
-    ConTracer_1d_pcs_1_ts_329_t_1184400.000000_expected.vtu ConTracer_1d_pcs_1_ts_329_t_1184400.000000.vtu pressure pressure 1e-6 1e-10
-    ConTracer_1d_pcs_1_ts_33_t_118800.000000_expected.vtu ConTracer_1d_pcs_1_ts_33_t_118800.000000.vtu Cs Cs 1e-10 1e-16
-    ConTracer_1d_pcs_1_ts_66_t_237600.000000_expected.vtu ConTracer_1d_pcs_1_ts_66_t_237600.000000.vtu Cs Cs 1e-10 1e-16
-    ConTracer_1d_pcs_1_ts_99_t_356400.000000_expected.vtu ConTracer_1d_pcs_1_ts_99_t_356400.000000.vtu Cs Cs 1e-10 1e-16
-    ConTracer_1d_pcs_1_ts_132_t_475200.000000_expected.vtu ConTracer_1d_pcs_1_ts_132_t_475200.000000.vtu Cs Cs 1e-10 1e-16
-    ConTracer_1d_pcs_1_ts_165_t_594000.000000_expected.vtu ConTracer_1d_pcs_1_ts_165_t_594000.000000.vtu Cs Cs 1e-10 1e-16
-    ConTracer_1d_pcs_1_ts_198_t_712800.000000_expected.vtu ConTracer_1d_pcs_1_ts_198_t_712800.000000.vtu Cs Cs 1e-10 1e-16
-    ConTracer_1d_pcs_1_ts_231_t_831600.000000_expected.vtu ConTracer_1d_pcs_1_ts_231_t_831600.000000.vtu Cs Cs 1e-10 1e-16
-    ConTracer_1d_pcs_1_ts_264_t_950400.000000_expected.vtu ConTracer_1d_pcs_1_ts_264_t_950400.000000.vtu Cs Cs 1e-10 1e-16
-    ConTracer_1d_pcs_1_ts_297_t_1069200.000000_expected.vtu ConTracer_1d_pcs_1_ts_297_t_1069200.000000.vtu Cs Cs 1e-10 1e-16
-    ConTracer_1d_pcs_1_ts_329_t_1184400.000000_expected.vtu ConTracer_1d_pcs_1_ts_329_t_1184400.000000.vtu Cs Cs 1e-10 1e-16
+    ConTracer_1d_pcs_1_ts_33_t_118800.000000_expected.vtu ConTracer_1d_ts_33_t_118800.000000.vtu pressure pressure 1e-6 1e-10
+    ConTracer_1d_pcs_1_ts_66_t_237600.000000_expected.vtu ConTracer_1d_ts_66_t_237600.000000.vtu pressure pressure 1e-6 1e-10
+    ConTracer_1d_pcs_1_ts_99_t_356400.000000_expected.vtu ConTracer_1d_ts_99_t_356400.000000.vtu pressure pressure 1e-6 1e-10
+    ConTracer_1d_pcs_1_ts_132_t_475200.000000_expected.vtu ConTracer_1d_ts_132_t_475200.000000.vtu pressure pressure 1e-6 1e-10
+    ConTracer_1d_pcs_1_ts_165_t_594000.000000_expected.vtu ConTracer_1d_ts_165_t_594000.000000.vtu pressure pressure 1e-6 1e-10
+    ConTracer_1d_pcs_1_ts_198_t_712800.000000_expected.vtu ConTracer_1d_ts_198_t_712800.000000.vtu pressure pressure 1e-6 1e-10
+    ConTracer_1d_pcs_1_ts_231_t_831600.000000_expected.vtu ConTracer_1d_ts_231_t_831600.000000.vtu pressure pressure 1e-6 1e-10
+    ConTracer_1d_pcs_1_ts_264_t_950400.000000_expected.vtu ConTracer_1d_ts_264_t_950400.000000.vtu pressure pressure 1e-6 1e-10
+    ConTracer_1d_pcs_1_ts_297_t_1069200.000000_expected.vtu ConTracer_1d_ts_297_t_1069200.000000.vtu pressure pressure 1e-6 1e-10
+    ConTracer_1d_pcs_1_ts_329_t_1184400.000000_expected.vtu ConTracer_1d_ts_329_t_1184400.000000.vtu pressure pressure 1e-6 1e-10
+    ConTracer_1d_pcs_1_ts_33_t_118800.000000_expected.vtu ConTracer_1d_ts_33_t_118800.000000.vtu Cs Cs 1e-10 1e-16
+    ConTracer_1d_pcs_1_ts_66_t_237600.000000_expected.vtu ConTracer_1d_ts_66_t_237600.000000.vtu Cs Cs 1e-10 1e-16
+    ConTracer_1d_pcs_1_ts_99_t_356400.000000_expected.vtu ConTracer_1d_ts_99_t_356400.000000.vtu Cs Cs 1e-10 1e-16
+    ConTracer_1d_pcs_1_ts_132_t_475200.000000_expected.vtu ConTracer_1d_ts_132_t_475200.000000.vtu Cs Cs 1e-10 1e-16
+    ConTracer_1d_pcs_1_ts_165_t_594000.000000_expected.vtu ConTracer_1d_ts_165_t_594000.000000.vtu Cs Cs 1e-10 1e-16
+    ConTracer_1d_pcs_1_ts_198_t_712800.000000_expected.vtu ConTracer_1d_ts_198_t_712800.000000.vtu Cs Cs 1e-10 1e-16
+    ConTracer_1d_pcs_1_ts_231_t_831600.000000_expected.vtu ConTracer_1d_ts_231_t_831600.000000.vtu Cs Cs 1e-10 1e-16
+    ConTracer_1d_pcs_1_ts_264_t_950400.000000_expected.vtu ConTracer_1d_ts_264_t_950400.000000.vtu Cs Cs 1e-10 1e-16
+    ConTracer_1d_pcs_1_ts_297_t_1069200.000000_expected.vtu ConTracer_1d_ts_297_t_1069200.000000.vtu Cs Cs 1e-10 1e-16
+    ConTracer_1d_pcs_1_ts_329_t_1184400.000000_expected.vtu ConTracer_1d_ts_329_t_1184400.000000.vtu Cs Cs 1e-10 1e-16
 )
 
 AddTest(
