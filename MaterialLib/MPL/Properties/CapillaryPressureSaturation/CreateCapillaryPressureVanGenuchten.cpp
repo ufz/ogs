231593d03591a738f770167acd3f9435e9eafe98
Wenqing Wang <wenqing.wang@ufz.de>
2020-05-14 16:43:00 +0200

    [vanGenuchtenPc] Modified createCapillaryPressureVanGenuchten

--- MaterialLib/MPL/Properties/CapillaryPressureSaturation/CreateCapillaryPressureVanGenuchten.cpp
+++ MaterialLib/MPL/Properties/CapillaryPressureSaturation/CreateCapillaryPressureVanGenuchten.cpp
@@ -11,8 +11,11 @@
 
 #include "CreateCapillaryPressureVanGenuchten.h"
 
+#include <limits>
+
 #include "BaseLib/ConfigTree.h"
 #include "CapillaryPressureVanGenuchten.h"
+#include "GetSaturationVanGenuchten.h"
 #include "MaterialLib/MPL/Property.h"
 
 namespace MaterialPropertyLib
@@ -39,10 +42,25 @@ std::unique_ptr<Property> createCapillaryPressureVanGenuchten(
         config.getConfigParameter<double>("p_b");
     auto const maximum_capillary_pressure =
         //! \ogs_file_param{properties__property__CapillaryPressureVanGenuchten__maximum_capillary_pressure}
-        config.getConfigParameter<double>("maximum_capillary_pressure");
+        config.getConfigParameter<double>(
+            "maximum_capillary_pressure",
+            std::numeric_limits<double>::quiet_NaN());
+
+    // If maximum_capillary_pressure, pc_max, is not given,
+    //    S_at_pc_max = S_L_res  + 1.0e-9.
+    // Otherwise
+    //    S_at_pc_max = max(S_L_res  + 1.0e-9, S(pc_max))
+    const double S_at_pc_max =
+        (maximum_capillary_pressure == std::numeric_limits<double>::quiet_NaN())
+            ? residual_liquid_saturation + 1.0e-9
+            : std::max(residual_liquid_saturation + 1.0e-9,
+                       getSaturationVanGenuchten(
+                           maximum_capillary_pressure, p_b,
+                           residual_liquid_saturation,
+                           1.0 - residual_gas_saturation, exponent));
 
     return std::make_unique<CapillaryPressureVanGenuchten>(
         residual_liquid_saturation, residual_gas_saturation, exponent, p_b,
-        maximum_capillary_pressure);
+        S_at_pc_max);
 }
 }  // namespace MaterialPropertyLib
