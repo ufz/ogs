f3b252e6640d752ab3c728f8cf4ad3149c40ca8d
Thomas Fischer <thomas.fischer@ufz.de>
2020-05-18 11:03:02 +0200

    [T/TMPhaseField] Change output file names to fit new scheme.

--- ProcessLib/ThermoMechanicalPhaseField/Tests.cmake
+++ ProcessLib/ThermoMechanicalPhaseField/Tests.cmake
@@ -7,10 +7,10 @@ AddTest(
     TESTER vtkdiff
     REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
     DIFF_DATA
-    expected_cube_1e0_tm_pcs_2_ts_1_t_100.000000.vtu cube_1e0_tm_pcs_2_ts_1_t_100.000000.vtu displacement displacement 1e-6 1e-6
-    expected_cube_1e0_tm_pcs_2_ts_1_t_100.000000.vtu cube_1e0_tm_pcs_2_ts_1_t_100.000000.vtu temperature temperature 1e-6 1e-6
-    analytical_cube_1e0_tm_pcs_2_ts_1_t_100.000000.vtu cube_1e0_tm_pcs_2_ts_1_t_100.000000.vtu phasefield phasefield 1e-5 1e-5
-    mechanical_cube_1e0_pcs_1_ts_1_t_100.000000.vtu cube_1e0_tm_pcs_2_ts_1_t_100.000000.vtu phasefield phasefield 1e-5 1e-5
+    expected_cube_1e0_tm_pcs_2_ts_1_t_100.000000.vtu cube_1e0_tm_ts_1_t_100.000000.vtu displacement displacement 1e-6 1e-6
+    expected_cube_1e0_tm_pcs_2_ts_1_t_100.000000.vtu cube_1e0_tm_ts_1_t_100.000000.vtu temperature temperature 1e-6 1e-6
+    analytical_cube_1e0_tm_pcs_2_ts_1_t_100.000000.vtu cube_1e0_tm_ts_1_t_100.000000.vtu phasefield phasefield 1e-5 1e-5
+    mechanical_cube_1e0_pcs_1_ts_1_t_100.000000.vtu cube_1e0_tm_ts_1_t_100.000000.vtu phasefield phasefield 1e-5 1e-5
    )
 
 AddTest(
@@ -23,9 +23,9 @@ AddTest(
     TESTER vtkdiff
     REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
     DIFF_DATA
-    expected_beam3d_pcs_2_ts_1_t_10.000000.vtu beam3d_pcs_2_ts_1_t_10.000000.vtu displacement displacement 1e-5 0
-    expected_beam3d_pcs_2_ts_1_t_10.000000.vtu beam3d_pcs_2_ts_1_t_10.000000.vtu phasefield phasefield 1e-6 0
-    expected_beam3d_pcs_2_ts_1_t_10.000000.vtu beam3d_pcs_2_ts_1_t_10.000000.vtu temperature temperature 1e-6 0
+    expected_beam3d_pcs_2_ts_1_t_10.000000.vtu beam3d_ts_1_t_10.000000.vtu displacement displacement 1e-5 0
+    expected_beam3d_pcs_2_ts_1_t_10.000000.vtu beam3d_ts_1_t_10.000000.vtu phasefield phasefield 1e-6 0
+    expected_beam3d_pcs_2_ts_1_t_10.000000.vtu beam3d_ts_1_t_10.000000.vtu temperature temperature 1e-6 0
    )
 
 AddTest(
@@ -38,8 +38,8 @@ AddTest(
     TESTER vtkdiff
     REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
     DIFF_DATA
-    expected_tes_hx3_pcs_2_ts_1_t_20.000000.vtu tes_hx3_pcs_2_ts_1_t_20.000000.vtu phasefield phasefield 1e-6 0
-    expected_tes_hx3_pcs_2_ts_1_t_20.000000.vtu tes_hx3_pcs_2_ts_1_t_20.000000.vtu heat_flux heat_flux 1e-4 0
+    expected_tes_hx3_pcs_2_ts_1_t_20.000000.vtu tes_hx3_ts_1_t_20.000000.vtu phasefield phasefield 1e-6 0
+    expected_tes_hx3_pcs_2_ts_1_t_20.000000.vtu tes_hx3_ts_1_t_20.000000.vtu heat_flux heat_flux 1e-4 0
    )
 
 AddTest(
@@ -52,5 +52,5 @@ AddTest(
     TESTER vtkdiff
     REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
     DIFF_DATA
-    expected_slab_5_pcs_2_ts_1_t_1.000000.vtu slab_5_pcs_2_ts_1_t_1.000000.vtu phasefield phasefield 1e-6 0
+    expected_slab_5_pcs_2_ts_1_t_1.000000.vtu slab_5_ts_1_t_1.000000.vtu phasefield phasefield 1e-6 0
    )
