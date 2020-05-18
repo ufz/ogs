080a80e98d29a6b3349d2afdb1cf1922405336f1
Wenqing Wang <wenqing.wang@ufz.de>
2020-05-18 16:02:25 +0200

    [MPL/vanGenuchten] added checkVanGenuchtenExponentRange

---
+++ MaterialLib/MPL/Utils/CheckVanGenuchtenExponentRange.cpp
@@ -0,0 +1,28 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2020, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ * Created on April 20, 2020, 10:47 AM
+ */
+
+#include "CheckVanGenuchtenExponentRange.h"
+
+#include "BaseLib/Error.h"
+
+namespace MaterialPropertyLib
+{
+void checkVanGenuchtenExponentRange(const double m)
+{
+    if (m <= 0 || m >= 1)
+    {
+        OGS_FATAL(
+            "The exponent value m = {:e} of van Genuchten saturation "
+            "model, is out of its range of(0, 1) ",
+            m);
+    }
+}
+}  // namespace MaterialPropertyLib
---
+++ MaterialLib/MPL/Utils/CheckVanGenuchtenExponentRange.h
@@ -0,0 +1,17 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2020, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ * Created on April 20, 2020, 10:47 AM
+ */
+
+#pragma once
+
+namespace MaterialPropertyLib
+{
+void checkVanGenuchtenExponentRange(const double m);
+}  // namespace MaterialPropertyLib
