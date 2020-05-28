91da563f15a356898c021d4fe4a2943a1d22147f
Wenqing Wang <wenqing.wang@ufz.de>
2020-05-28 16:07:27 +0200

    [Tests] Set createTestProperty as a common function

--- Tests/MaterialLib/TestMPL.h
+++ Tests/MaterialLib/TestMPL.h
@@ -13,9 +13,31 @@
 
 #pragma once
 
-#include "Tests/TestTools.h"
+#include <functional>
+#include <memory>
 
 #include "MaterialLib/MPL/Medium.h"
+
 namespace MPL = MaterialPropertyLib;
 
+namespace BaseLib
+{
+class ConfigTree;
+}
+
+namespace MaterialPropertyLib
+{
+class Property;
+}
+
 std::unique_ptr<MPL::Medium> createTestMaterial(std::string const& xml);
+
+namespace Tests
+{
+std::unique_ptr<MaterialPropertyLib::Property> createTestProperty(
+    const char xml[],
+    std::function<std::unique_ptr<MaterialPropertyLib::Property>(
+        BaseLib::ConfigTree const& config)>
+        createProperty);
+
+}  // namespace Tests
