04f6813b32bb0fe50969ebfe341f23697d304f66
Dmitri Naumov <github@naumov.de>
2020-06-14 11:47:50 +0200

    [T/MPL] Rewrite Exponential property test.
     - Using namespace alias
     - Use Property::value dValue, d2Value
     - Short name for temperature.

--- Tests/MaterialLib/TestMPLExponential.cpp
+++ Tests/MaterialLib/TestMPLExponential.cpp
@@ -8,58 +8,43 @@
  *              http://www.opengeosys.org/project/license
  *
  */
-#include <cmath>
-
 #include <gtest/gtest.h>
 
+#include <cmath>
+
 #include "MaterialLib/MPL/Properties/Exponential.h"
+
+namespace MPL = MaterialPropertyLib;
 
 TEST(MaterialPropertyLib, Exponential)
 {
     double const y_ref = 1e-3;
     double const reference_condition = 20.0;
     double const factor = 1 / 75.0;
-    MaterialPropertyLib::ExponentData const exp_data{
-        MaterialPropertyLib::Variable::temperature, reference_condition, factor};
-    MaterialPropertyLib::Exponential exp_property{"exponential", y_ref,
-                                                  exp_data};
+    MPL::ExponentData const exp_data{MPL::Variable::temperature,
+                                     reference_condition, factor};
+    MPL::Property const& p = MPL::Exponential{"exponential", y_ref, exp_data};
 
-    MaterialPropertyLib::VariableArray variable_array;
-    variable_array[static_cast<int>(
-        MaterialPropertyLib::Variable::temperature)] = 20.;
+    double const T = 20.;
+    MPL::VariableArray variable_array;
+    variable_array[static_cast<int>(MPL::Variable::temperature)] = T;
     ParameterLib::SpatialPosition const pos;
     double const time = std::numeric_limits<double>::quiet_NaN();
     double const dt = std::numeric_limits<double>::quiet_NaN();
-    ASSERT_NEAR(
-        std::get<double>(exp_property.value(variable_array, pos, time, dt)),
-        y_ref * (std::exp(-factor *
-                          (std::get<double>(variable_array[static_cast<int>(
-                               MaterialPropertyLib::Variable::temperature)]) -
-                           reference_condition))),
-        1.e-10);
-    ASSERT_EQ(std::get<double>(exp_property.dValue(
-                  variable_array, MaterialPropertyLib::Variable::phase_pressure,
-                  pos, time, dt)),
+    ASSERT_NEAR(p.template value<double>(variable_array, pos, time, dt),
+                y_ref * (std::exp(factor * (T - reference_condition))),
+                1.e-10);
+    ASSERT_EQ(p.template dValue<double>(
+                  variable_array, MPL::Variable::phase_pressure, pos, time, dt),
               0.0);
+    ASSERT_NEAR(p.template dValue<double>(
+                    variable_array, MPL::Variable::temperature, pos, time, dt),
+                y_ref * factor * std::exp(factor * (T - reference_condition)),
+                1.e-16);
     ASSERT_NEAR(
-        std::get<double>(exp_property.dValue(
-            variable_array, MaterialPropertyLib::Variable::temperature, pos,
-            time, dt)),
-        -y_ref * factor *
-            std::exp(-factor *
-                     (std::get<double>(variable_array[static_cast<int>(
-                          MaterialPropertyLib::Variable::temperature)]) -
-                      reference_condition)),
-        1.e-16);
-    ASSERT_NEAR(
-        std::get<double>(exp_property.d2Value(
-            variable_array, MaterialPropertyLib::Variable::temperature,
-            MaterialPropertyLib::Variable::temperature, pos, time, dt)),
+        p.template d2Value<double>(variable_array, MPL::Variable::temperature,
+                                   MPL::Variable::temperature, pos, time, dt),
         y_ref * std::pow(factor, 2) *
-            std::exp(-factor *
-                     (std::get<double>(variable_array[static_cast<int>(
-                          MaterialPropertyLib::Variable::temperature)]) -
-                      reference_condition)),
+            std::exp(factor * (T - reference_condition)),
         1.e-16);
 }
-
