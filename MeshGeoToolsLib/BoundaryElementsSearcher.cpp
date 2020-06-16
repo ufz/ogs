d223ec1704482230e21e0ec4538845e8b02e3b35
Thomas Fischer <thomas.fischer@ufz.de>
2020-06-16 06:35:25 +0200

    [MGTL] BoundaryElementsSearcher::getBoundaryElements(): generalization.

--- MeshGeoToolsLib/BoundaryElementsSearcher.cpp
+++ MeshGeoToolsLib/BoundaryElementsSearcher.cpp
@@ -47,91 +47,77 @@ BoundaryElementsSearcher::~BoundaryElementsSearcher()
     }
 }
 
+template <typename CacheType, typename GeometryType>
+std::vector<MeshLib::Element*> const& getBoundaryElements(
+    std::vector<CacheType*>& cached_elements,
+    std::function<GeometryType(CacheType const&)> getCachedItem,
+    GeometryType const& item, MeshLib::Mesh const& mesh,
+    MeshNodeSearcher const& mesh_node_searcher,
+    [[maybe_unused]] bool const multiple_nodes_allowed)
+{
+    if (auto const it = find_if(cbegin(cached_elements), cend(cached_elements),
+                                [&](auto const& element) {
+                                    return getCachedItem(*element) == item;
+                                });
+        it != cend(cached_elements))
+    {
+        return (*it)->getBoundaryElements();
+    }
+    // create new boundary elements
+    if constexpr (std::is_convertible<GeometryType, GeoLib::Point>::value)
+    {
+        cached_elements.push_back(new CacheType(mesh, mesh_node_searcher, item,
+                                                multiple_nodes_allowed));
+    }
+    else
+    {
+        cached_elements.push_back(
+            new CacheType(mesh, mesh_node_searcher, item));
+    }
+    return cached_elements.back()->getBoundaryElements();
+}
+
 std::vector<MeshLib::Element*> const&
 BoundaryElementsSearcher::getBoundaryElements(GeoLib::GeoObject const& geoObj,
                                               bool const multiple_nodes_allowed)
 {
-    switch (geoObj.getGeoType()) {
-    case GeoLib::GEOTYPE::POINT:
-        return this->getBoundaryElementsAtPoint(
-            *dynamic_cast<const GeoLib::Point*>(&geoObj),
-            multiple_nodes_allowed);
-        break;
-    case GeoLib::GEOTYPE::POLYLINE:
-        return this->getBoundaryElementsAlongPolyline(*dynamic_cast<const GeoLib::Polyline*>(&geoObj));
-        break;
-    case GeoLib::GEOTYPE::SURFACE:
-        return this->getBoundaryElementsOnSurface(*dynamic_cast<const GeoLib::Surface*>(&geoObj));
-        break;
-    default:
-        const static std::vector<MeshLib::Element*> dummy(0);
-        return dummy;
-    }
-}
-
-std::vector<MeshLib::Element*> const&
-BoundaryElementsSearcher::getBoundaryElementsAtPoint(
-    GeoLib::Point const& point, bool const multiple_nodes_allowed)
-{
-    // look for already saved points and return if found.
-    if (auto const it = find_if(cbegin(_boundary_elements_at_point),
-                                cend(_boundary_elements_at_point),
-                                [&](auto const& boundary_elements) {
-                                    return boundary_elements->getPoint() ==
-                                           point;
-                                });
-        it != cend(_boundary_elements_at_point))
+    switch (geoObj.getGeoType())
     {
-        return (*it)->getBoundaryElements();
+        case GeoLib::GEOTYPE::POINT:
+        {
+            std::function<GeoLib::Point(BoundaryElementsAtPoint const&)>
+                get_cached_item = &BoundaryElementsAtPoint::getPoint;
+            return MeshGeoToolsLib::getBoundaryElements(
+                _boundary_elements_at_point, get_cached_item,
+                *dynamic_cast<const GeoLib::Point*>(&geoObj), _mesh,
+                _mshNodeSearcher, multiple_nodes_allowed);
+        }
+        break;
+        case GeoLib::GEOTYPE::POLYLINE:
+        {
+            std::function<GeoLib::Polyline(
+                BoundaryElementsAlongPolyline const&)>
+                get_cached_item = &BoundaryElementsAlongPolyline::getPolyline;
+            return MeshGeoToolsLib::getBoundaryElements(
+                _boundary_elements_along_polylines, get_cached_item,
+                *dynamic_cast<const GeoLib::Polyline*>(&geoObj), _mesh,
+                _mshNodeSearcher, false);
+        }
+        break;
+        case GeoLib::GEOTYPE::SURFACE:
+        {
+            std::function<GeoLib::Surface(BoundaryElementsOnSurface const&)>
+                get_cached_item = &BoundaryElementsOnSurface::getSurface;
+            return MeshGeoToolsLib::getBoundaryElements(
+                _boundary_elements_along_surfaces, get_cached_item,
+                *dynamic_cast<const GeoLib::Surface*>(&geoObj), _mesh,
+                _mshNodeSearcher, false);
+        }
+        break;
+        default:
+            const static std::vector<MeshLib::Element*> dummy(0);
+            return dummy;
     }
-
-    // create new boundary elements at points.
-    _boundary_elements_at_point.push_back(new BoundaryElementsAtPoint(
-        _mesh, _mshNodeSearcher, point, multiple_nodes_allowed));
-    return _boundary_elements_at_point.back()->getBoundaryElements();
-}
-
-std::vector<MeshLib::Element*> const&
-BoundaryElementsSearcher::getBoundaryElementsAlongPolyline(
-    GeoLib::Polyline const& polyline)
-{
-    // look for already saved polylines and return if found.
-    if (auto const it = find_if(cbegin(_boundary_elements_along_polylines),
-                                cend(_boundary_elements_along_polylines),
-                                [&](auto const& boundary_elements) {
-                                    return &boundary_elements->getPolyline() ==
-                                           &polyline;
-                                });
-        it != cend(_boundary_elements_along_polylines))
-    {
-        return (*it)->getBoundaryElements();
-    }
-
-    // create new boundary elements at points.
-    _boundary_elements_along_polylines.push_back(
-        new BoundaryElementsAlongPolyline(_mesh, _mshNodeSearcher, polyline));
-    return _boundary_elements_along_polylines.back()->getBoundaryElements();
-}
-
-std::vector<MeshLib::Element*> const&
-BoundaryElementsSearcher::getBoundaryElementsOnSurface(
-    GeoLib::Surface const& surface)
-{
-    // look for already saved surfaces and return if found.
-    if (auto const it = find_if(cbegin(_boundary_elements_along_surfaces),
-                                cend(_boundary_elements_along_surfaces),
-                                [&](auto const& boundary_elements) {
-                                    return &boundary_elements->getSurface() ==
-                                           &surface;
-                                });
-        it != cend(_boundary_elements_along_surfaces))
-    {
-        return (*it)->getBoundaryElements();
-    }
-
-    _boundary_elements_along_surfaces.push_back(
-            new BoundaryElementsOnSurface(_mesh, _mshNodeSearcher, surface));
-    return _boundary_elements_along_surfaces.back()->getBoundaryElements();
 }
 
 } // end namespace MeshGeoToolsLib
