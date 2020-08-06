c8eeb4bdb6c8aadb13a23d31dfd97ae99a5d794a
Lars Bilke <lars.bilke@ufz.de>
2020-08-06 17:44:51 +0200

    [prj] Added ability to include files.
    Syntax: <include filename="file.xml" />

--- BaseLib/ConfigTreeUtil.cpp
+++ BaseLib/ConfigTreeUtil.cpp
@@ -11,9 +11,11 @@
 #include "ConfigTreeUtil.h"
 
 #include <boost/property_tree/xml_parser.hpp>
-#include "Logging.h"
+#include <regex>
 
 #include "Error.h"
+#include "filesystem.h"
+#include "Logging.h"
 
 namespace BaseLib
 {
@@ -47,6 +49,17 @@ ConfigTreeTopLevel::checkAndInvalidate()
     ::BaseLib::checkAndInvalidate(_ctree);
 }
 
+// From https://stackoverflow.com/a/1567703/80480
+class line {
+    std::string data;
+public:
+    friend std::istream &operator>>(std::istream &is, line &l) {
+        std::getline(is, l.data);
+        return is;
+    }
+    operator std::string() const { return data; }
+};
+
 ConfigTreeTopLevel
 makeConfigTree(const std::string& filepath, const bool be_ruthless,
                const std::string& toplevel_tag)
@@ -57,7 +70,37 @@ makeConfigTree(const std::string& filepath, const bool be_ruthless,
     //       for our configuration tree implementation to work!
     try
     {
-        read_xml(filepath, ptree,
+        // Searching for <include filename=".." /> - tags and replacing by the
+        // content of the included file.
+        std::ifstream file (filepath);
+        std::stringstream output;
+
+        std::transform( std::istream_iterator<line>( file ),
+            std::istream_iterator<line>(),
+            std::ostream_iterator<std::string>( output, "\n" ),
+            [](std::string const& _line)
+            {
+                const std::regex base_regex(".*<include file=\"(.*)\" ?/>.*");
+                std::smatch base_match;
+                if (std::regex_match(_line, base_match, base_regex))
+                {
+                    if (base_match.size() == 2)
+                    {
+                        std::string include_filename = base_match[1].str();
+                        std::ifstream include_file (include_filename);
+                        std::string include_content(
+                            (std::istreambuf_iterator<char>(include_file) ),
+                            (std::istreambuf_iterator<char>()) );
+                        INFO("Including {:s} into project file.",
+                             include_filename);
+                        return include_content;
+                    }
+                }
+                return _line;
+            }
+        );
+
+        read_xml(output, ptree,
                  boost::property_tree::xml_parser::no_comments |
                      boost::property_tree::xml_parser::trim_whitespace);
     }
