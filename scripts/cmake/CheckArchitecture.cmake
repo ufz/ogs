7832e7b101c58f5bd4869a839bf2271fa405f217
Lars Bilke <lars.bilke@ufz.de>
2020-08-13 09:16:19 +0200

    [CMake] Speed-up by removing CheckTypeSizes.cmake (1 s).

--- scripts/cmake/CheckArchitecture.cmake
+++
@@ -1,6 +0,0 @@
-# Check architecture
-if(MSVC AND NOT HAVE_64_BIT AND NOT OGS_32_BIT)
-    message(FATAL_ERROR "Building OGS on Windows with 32-bit is not supported! \
-Either use the correct generator, e.g. 'Visual Studio 14 2015 Win64' or define \
-'-DOGS_32_BIT=ON' if you know what you are doing.")
-endif()
