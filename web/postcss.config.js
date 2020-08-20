0ce0531b88cb2ca72085ce905c832b389f8ef436
Lars Bilke <lars.bilke@ufz.de>
2020-08-20 08:53:04 +0200

    [web] Simplified css processing.
    No need for SASS.

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,7 +1,6 @@
-var tailwindcss = require('tailwindcss');
 module.exports = {
     plugins: [
-      tailwindcss('./tailwind.config.js'),
+      require('tailwindcss'),
       require('autoprefixer'),
       require('postcss-import-url'),
     ]
