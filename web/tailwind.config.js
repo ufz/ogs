40b081b74b27be380e6ca282c122083a0477d173
Lars Bilke <lars.bilke@ufz.de>
2020-08-20 12:04:45 +0200

    [web] Purge unused css.

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,29 +1,3 @@
-/*
-
-Tailwind - The Utility-First CSS Framework
-
-A project by Adam Wathan (@adamwathan), Jonathan Reinink (@reinink),
-David Hemphill (@davidhemphill) and Steve Schoger (@steveschoger).
-
-Welcome to the Tailwind config file. This is where you can customize
-Tailwind specifically for your project. Don't be intimidated by the
-length of this file. It's really just a big JavaScript object and
-we've done our very best to explain each section.
-
-View the full documentation at https://tailwindcss.com.
-
-
-|-------------------------------------------------------------------------------
-| The default config
-|-------------------------------------------------------------------------------
-|
-| This variable contains the default Tailwind config. You don't have
-| to use it, but it can sometimes be helpful to have available. For
-| example, you may choose to merge your custom configuration
-| values with some of the Tailwind defaults.
-|
-*/
-
 let primaryColor = "#104EB2";
 let accentColor = "#f6993f";
 let textColor = "#333333";
@@ -44,6 +18,9 @@ let mycolors = {
 }
 
 module.exports = {
+  purge: [
+    './layouts/**/*.html',
+  ],
   theme: {
     container: {
       center: true,
