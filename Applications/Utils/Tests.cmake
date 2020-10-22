f7d5a6731c8566a1ddb8c8188ce54425b922b0c0
Lars Bilke <lars.bilke@ufz.de>
2020-10-22 09:39:34 +0200

    [T] Fix AssignRasterDataToMesh ctest paths.

--- Applications/Utils/Tests.cmake
+++ Applications/Utils/Tests.cmake
@@ -426,10 +426,10 @@ AddTest(
 
 AddTest(
     NAME AssignRasterDataToMesh_Test
-    PATH MeshGeoTools
+    PATH MeshGeoToolsLib/Ammer
     WORKING_DIRECTORY ${Data_SOURCE_DIR}/MeshGeoToolsLib/Ammer
     EXECUTABLE AssignRasterDataToMesh
-    EXECUTABLE_ARGS -i Ammer-Homogen100m-Final-TopSurface.vtu -r AmmerGWN.asc -o ${Data_BINARY_DIR}/MeshGeoToolsLib/AmmerGWN.vtu -s GWN -c -n
+    EXECUTABLE_ARGS -i Ammer-Homogen100m-Final-TopSurface.vtu -r AmmerGWN.asc -o ${Data_BINARY_DIR}/MeshGeoToolsLib/Ammer/AmmerGWN.vtu -s GWN -c -n
     REQUIREMENTS NOT OGS_USE_MPI
     TESTER vtkdiff
     DIFF_DATA
