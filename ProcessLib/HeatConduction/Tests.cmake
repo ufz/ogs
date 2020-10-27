5719e4d12033be0ab9b6ec39ef721d18e4793e90
Thomas Fischer <thomas.fischer@ufz.de>
2020-10-27 20:48:47 +0100

    [PL/HeatConduction] Rename files in Tests.cmake.

--- ProcessLib/HeatConduction/Tests.cmake
+++ ProcessLib/HeatConduction/Tests.cmake
@@ -5,8 +5,8 @@ AddTest(
         EXECUTABLE_ARGS line_60_heat.prj
         TESTER vtkdiff
         DIFF_DATA
-        temperature_analytical.vtu line_60_heat_pcs_0_ts_65_t_5078125.000000.vtu Temperature_Analytical_2months temperature 1e-5 1e-5
-        temperature_analytical.vtu line_60_heat_pcs_0_ts_405_t_31640625.000000.vtu Temperature_Analytical_1year temperature 1e-5 1e-5
+        temperature_analytical.vtu line_60_heat_ts_65_t_5078125.000000.vtu Temperature_Analytical_2months temperature 1e-5 1e-5
+        temperature_analytical.vtu line_60_heat_ts_405_t_31640625.000000.vtu Temperature_Analytical_1year temperature 1e-5 1e-5
     REQUIREMENTS NOT OGS_USE_MPI
 )
 
@@ -128,8 +128,8 @@ AddTest(
         EXECUTABLE_ARGS square_1e2_axi.prj
         TESTER vtkdiff
         DIFF_DATA
-        wedge_1e2_axi_ang_0.02_t_2s_extracted_surface.vtu square_1e2_axi_pcs_0_ts_2_t_2.000000.vtu temperature temperature 1.7e-5 1e-5
-        wedge_1e2_axi_ang_0.02_t_2s_extracted_surface.vtu square_1e2_axi_pcs_0_ts_2_t_2.000000.vtu heat_flux_x heat_flux_x 1.7e-5 1e-5
+        wedge_1e2_axi_ang_0.02_t_2s_extracted_surface.vtu square_1e2_axi_ts_2_t_2.000000.vtu temperature temperature 1.7e-5 1e-5
+        wedge_1e2_axi_ang_0.02_t_2s_extracted_surface.vtu square_1e2_axi_ts_2_t_2.000000.vtu heat_flux_x heat_flux_x 1.7e-5 1e-5
     REQUIREMENTS NOT OGS_USE_MPI
 )
 # # WEDGE 1x1 HEATCONDUCTION TEST -- computes reference results for the above test
@@ -150,7 +150,7 @@ AddTest(
     EXECUTABLE_ARGS bhe2d.prj
     TESTER vtkdiff
     DIFF_DATA
-    standard_solution_bhe2d_pcs_0_ts_840_t_72576000.000000.vtu bhe2d_pcs_0_ts_840_t_72576000.000000.vtu temperature temperature 1e-12 0.0
+    standard_solution_bhe2d_ts_840_t_72576000.000000.vtu bhe2d_ts_840_t_72576000.000000.vtu temperature temperature 1e-12 0.0
     REQUIREMENTS NOT OGS_USE_MPI
 )
 
@@ -162,7 +162,7 @@ AddTest(
     EXECUTABLE_ARGS line_1_line_1e2_source_term.prj
     TESTER vtkdiff
     DIFF_DATA
-    line_1_line_1e2_pcs_0_ts_500_t_39062500.000000_reference.vtu line_1_line_1e2_pcs_0_ts_500_t_39062500.000000.vtu temperature temperature 1.4e-11 0.0
+    line_1_line_1e2_ts_500_t_39062500.000000_reference.vtu line_1_line_1e2_ts_500_t_39062500.000000.vtu temperature temperature 1.4e-11 0.0
     REQUIREMENTS NOT OGS_USE_MPI
 )
 
@@ -176,7 +176,7 @@ AddTest(
     TESTER vtkdiff
     REQUIREMENTS OGS_USE_MPI
     DIFF_DATA
-    line_1_line_1e2_pcs_0_ts_500_t_39062500.000000_reference.vtu line_1_line_1e2_pcs_0_ts_500_t_39062500_000000_0.vtu temperature temperature 1e-10 0.0
+    line_1_line_1e2_ts_500_t_39062500.000000_reference.vtu line_1_line_1e2_ts_500_t_39062500_000000_0.vtu temperature temperature 1e-10 0.0
 )
 
 AddTest(
@@ -186,7 +186,7 @@ AddTest(
         EXECUTABLE_ARGS t1_1Dsource.prj
         TESTER vtkdiff
         DIFF_DATA
-        t1_1Dsource_pcs_0_ts_1_t_1.000000.vtu t1_1Dsource_pcs_0_ts_1_t_1.000000.vtu temperature temperature 10e-12 0.0
+        t1_1Dsource_ts_1_t_1.000000.vtu t1_1Dsource_ts_1_t_1.000000.vtu temperature temperature 10e-12 0.0
         REQUIREMENTS NOT OGS_USE_MPI
 )
 
@@ -197,7 +197,7 @@ AddTest(
         EXECUTABLE_ARGS t1_1Dsteady.prj
         TESTER vtkdiff
         DIFF_DATA
-        t1_1Dsteady_pcs_0_ts_1_t_1.000000.vtu t1_1Dsteady_pcs_0_ts_1_t_1.000000.vtu temperature temperature 10e-12 0.0
+        t1_1Dsteady_ts_1_t_1.000000.vtu t1_1Dsteady_ts_1_t_1.000000.vtu temperature temperature 10e-12 0.0
         REQUIREMENTS NOT OGS_USE_MPI
 )
 
@@ -208,8 +208,8 @@ AddTest(
         EXECUTABLE_ARGS t2_1D1bt.prj
         TESTER vtkdiff
         DIFF_DATA
-        t2_1D1bt_pcs_0_ts_500_t_21600.000000.vtu t2_1D1bt_pcs_0_ts_500_t_21600.000000.vtu temperature temperature 10e-12 0.0
-        t2_1D1bt_pcs_0_ts_1000_t_43200.000000.vtu t2_1D1bt_pcs_0_ts_1000_t_43200.000000.vtu temperature temperature 10e-12 0.0
+        t2_1D1bt_ts_500_t_21600.000000.vtu t2_1D1bt_ts_500_t_21600.000000.vtu temperature temperature 10e-12 0.0
+        t2_1D1bt_ts_1000_t_43200.000000.vtu t2_1D1bt_ts_1000_t_43200.000000.vtu temperature temperature 10e-12 0.0
         REQUIREMENTS NOT OGS_USE_MPI
 )
 
@@ -220,8 +220,8 @@ AddTest(
         EXECUTABLE_ARGS t2_1D2bt.prj
         TESTER vtkdiff
         DIFF_DATA
-        t2_1D2bt_pcs_0_ts_3000_t_7776.000000.vtu t2_1D2bt_pcs_0_ts_3000_t_7776.000000.vtu temperature temperature 10e-12 0.0
-        t2_1D2bt_pcs_0_ts_1500_t_3888.000000.vtu t2_1D2bt_pcs_0_ts_1500_t_3888.000000.vtu temperature temperature 10e-12 0.0
+        t2_1D2bt_ts_3000_t_7776.000000.vtu t2_1D2bt_ts_3000_t_7776.000000.vtu temperature temperature 10e-12 0.0
+        t2_1D2bt_ts_1500_t_3888.000000.vtu t2_1D2bt_ts_1500_t_3888.000000.vtu temperature temperature 10e-12 0.0
         REQUIREMENTS NOT OGS_USE_MPI
 )
 
@@ -232,7 +232,7 @@ AddTest(
         EXECUTABLE_ARGS line_source_term.prj
         TESTER vtkdiff
         DIFF_DATA
-        mesh_1_line_100.vtu source_term_at_entire_line_pcs_0_ts_1_t_1.000000.vtu analytical_temperature temperature 1e-13 1e-13
+        mesh_1_line_100.vtu source_term_at_entire_line_ts_1_t_1.000000.vtu analytical_temperature temperature 1e-13 1e-13
         REQUIREMENTS NOT OGS_USE_MPI
 )
 
@@ -248,8 +248,8 @@ AddTest(
         EXECUTABLE_ARGS source_term_left.prj
         TESTER vtkdiff
         DIFF_DATA
-        source_term_left_pcs_0_ts_1_t_1.000000.vtu source_term_left_pcs_0_ts_1_t_1.000000.vtu temperature temperature 1e-15 0.0
-        source_term_left_pcs_0_ts_1_t_1.000000.vtu source_term_left_pcs_0_ts_1_t_1.000000.vtu heat_flux_x heat_flux_x 1e-15 0.0
+        source_term_left_ts_1_t_1.000000.vtu source_term_left_ts_1_t_1.000000.vtu temperature temperature 1e-15 0.0
+        source_term_left_ts_1_t_1.000000.vtu source_term_left_ts_1_t_1.000000.vtu heat_flux_x heat_flux_x 1e-15 0.0
         REQUIREMENTS NOT OGS_USE_MPI
 )
 
@@ -263,8 +263,8 @@ AddTest(
         EXECUTABLE_ARGS line_source_term_x_0.5.prj
         TESTER vtkdiff
         DIFF_DATA
-        source_term_middle_pcs_0_ts_1_t_1.000000.vtu source_term_middle_pcs_0_ts_1_t_1.000000.vtu temperature temperature 7e-15 2e-14
-        source_term_middle_pcs_0_ts_1_t_1.000000.vtu source_term_middle_pcs_0_ts_1_t_1.000000.vtu heat_flux_x heat_flux_x 7e-14 0.0
+        source_term_middle_ts_1_t_1.000000.vtu source_term_middle_ts_1_t_1.000000.vtu temperature temperature 7e-15 2e-14
+        source_term_middle_ts_1_t_1.000000.vtu source_term_middle_ts_1_t_1.000000.vtu heat_flux_x heat_flux_x 7e-14 0.0
         REQUIREMENTS NOT OGS_USE_MPI
 )
 
@@ -276,8 +276,8 @@ AddTest(
         EXECUTABLE_ARGS line_source_term_x_0.5.prj
         TESTER vtkdiff
         DIFF_DATA
-        source_term_middle_restricted_pcs_0_ts_1_t_1.000000.vtu source_term_middle_restricted_pcs_0_ts_1_t_1.000000.vtu temperature temperature 1e-15 0.0
-        source_term_middle_restricted_pcs_0_ts_1_t_1.000000.vtu source_term_middle_restricted_pcs_0_ts_1_t_1.000000.vtu heat_flux_x heat_flux_x 3e-15 4e-7
+        source_term_middle_restricted_ts_1_t_1.000000.vtu source_term_middle_restricted_ts_1_t_1.000000.vtu temperature temperature 1e-15 0.0
+        source_term_middle_restricted_ts_1_t_1.000000.vtu source_term_middle_restricted_ts_1_t_1.000000.vtu heat_flux_x heat_flux_x 3e-15 4e-7
         REQUIREMENTS NOT OGS_USE_MPI
 )
 
@@ -290,8 +290,8 @@ AddTest(
         EXECUTABLE_ARGS line_source_term_x_0.5_y_0.5.prj
         TESTER vtkdiff
         DIFF_DATA
-        3D_line_source_term_pcs_0_ts_1_t_1.000000.vtu 3D_line_source_term_pcs_0_ts_1_t_1.000000.vtu temperature temperature 1e-15 0.0
-        3D_line_source_term_pcs_0_ts_1_t_1.000000.vtu 3D_line_source_term_pcs_0_ts_1_t_1.000000.vtu heat_flux_x heat_flux_x 7e-15 7e-13
+        3D_line_source_term_ts_1_t_1.000000.vtu 3D_line_source_term_ts_1_t_1.000000.vtu temperature temperature 1e-15 0.0
+        3D_line_source_term_ts_1_t_1.000000.vtu 3D_line_source_term_ts_1_t_1.000000.vtu heat_flux_x heat_flux_x 7e-15 7e-13
         REQUIREMENTS NOT OGS_USE_MPI
 )
 
@@ -304,8 +304,8 @@ AddTest(
         EXECUTABLE_ARGS line_source_term_x_0.5_y_0.5_restricted.prj
         TESTER vtkdiff
         DIFF_DATA
-        3D_line_source_term_restricted_pcs_0_ts_1_t_1.000000.vtu 3D_line_source_term_restricted_pcs_0_ts_1_t_1.000000.vtu temperature temperature 1e-15 0.0
-        3D_line_source_term_restricted_pcs_0_ts_1_t_1.000000.vtu 3D_line_source_term_restricted_pcs_0_ts_1_t_1.000000.vtu heat_flux_x heat_flux_x 1.1e-15 5e-12
+        3D_line_source_term_restricted_ts_1_t_1.000000.vtu 3D_line_source_term_restricted_ts_1_t_1.000000.vtu temperature temperature 1e-15 0.0
+        3D_line_source_term_restricted_ts_1_t_1.000000.vtu 3D_line_source_term_restricted_ts_1_t_1.000000.vtu heat_flux_x heat_flux_x 1.1e-15 5e-12
         REQUIREMENTS NOT OGS_USE_MPI
 )
 
@@ -321,7 +321,7 @@ AddTest(
         EXECUTABLE_ARGS line_source_term_in_cylinder.prj
         TESTER vtkdiff
         DIFF_DATA
-        Cylinder_r_1_h_1_prism_49k.vtu 3D_line_source_term_in_cylinder_49k_pcs_0_ts_1_t_1.000000.vtu analytical_solution_temperature temperature 0.2 0.0
+        Cylinder_r_1_h_1_prism_49k.vtu 3D_line_source_term_in_cylinder_49k_ts_1_t_1.000000.vtu analytical_solution_temperature temperature 0.2 0.0
         REQUIREMENTS NOT OGS_USE_MPI
 )
 
@@ -334,6 +334,6 @@ AddTest(
         EXECUTABLE_ARGS line_source_term_in_cylinder.prj
         TESTER vtkdiff
         DIFF_DATA
-        Cylinder_r_1_h_1_prism_286k.vtu 3D_line_source_term_in_cylinder_286k_pcs_0_ts_1_t_1.000000.vtu analytical_solution_temperature temperature 4e-3 0.0
+        Cylinder_r_1_h_1_prism_286k.vtu 3D_line_source_term_in_cylinder_286k_ts_1_t_1.000000.vtu analytical_solution_temperature temperature 4e-3 0.0
         REQUIREMENTS NOT OGS_USE_MPI
 )
