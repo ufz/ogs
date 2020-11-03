c53633a6a6eeab3e5d1f9a9ea87a6b0096a643dc
Thomas Fischer <thomas.fischer@ufz.de>
2020-11-03 11:25:42 +0100

    [PL/TwoPhaseFlow] Rm '_pcs_0' pattern from file names in Test.cmake.

--- ProcessLib/TwoPhaseFlowWithPrho/Tests.cmake
+++ ProcessLib/TwoPhaseFlowWithPrho/Tests.cmake
@@ -7,9 +7,9 @@ AddTest(
     TESTER vtkdiff
     REQUIREMENTS NOT OGS_USE_MPI
     DIFF_DATA
-    reference_t_10000.000000.vtu twophaseflow_pcs_0_ts_10_t_10000.000000.vtu liquid_pressure liquid_pressure 1e-6 1e-12
-    reference_t_10000.000000.vtu twophaseflow_pcs_0_ts_10_t_10000.000000.vtu overall_mass_density overall_mass_density 0 1e-12
-    reference_t_10000.000000.vtu twophaseflow_pcs_0_ts_10_t_10000.000000.vtu saturation saturation 1e-8 0
+    reference_t_10000.000000.vtu twophaseflow_ts_10_t_10000.000000.vtu liquid_pressure liquid_pressure 1e-6 1e-12
+    reference_t_10000.000000.vtu twophaseflow_ts_10_t_10000.000000.vtu overall_mass_density overall_mass_density 0 1e-12
+    reference_t_10000.000000.vtu twophaseflow_ts_10_t_10000.000000.vtu saturation saturation 1e-8 0
 )
 
 AddTest(
@@ -22,12 +22,12 @@ AddTest(
     REQUIREMENTS NOT OGS_USE_MPI
     DIFF_DATA
     reference_t_10000.000000.vtu
-    twophaseflow_adaptive_dt_pcs_0_ts_10_t_10000.000000.vtu liquid_pressure liquid_pressure 1e-6 1e-12
-    reference_t_10000.000000.vtu twophaseflow_adaptive_dt_pcs_0_ts_10_t_10000.000000.vtu overall_mass_density overall_mass_density 1e-10 1e-16
-    reference_t_10000.000000.vtu twophaseflow_adaptive_dt_pcs_0_ts_10_t_10000.000000.vtu saturation saturation 1e-10 0
-    reference_t_100000.000000.vtu twophaseflow_adaptive_dt_pcs_0_ts_108_t_100000.000000.vtu liquid_pressure liquid_pressure 1e-6 1e-12
-    reference_t_100000.000000.vtu twophaseflow_adaptive_dt_pcs_0_ts_108_t_100000.000000.vtu overall_mass_density overall_mass_density 1e-10 1e-16
-    reference_t_100000.000000.vtu twophaseflow_adaptive_dt_pcs_0_ts_108_t_100000.000000.vtu saturation saturation 1e-10 0.0
+    twophaseflow_adaptive_dt_ts_10_t_10000.000000.vtu liquid_pressure liquid_pressure 1e-6 1e-12
+    reference_t_10000.000000.vtu twophaseflow_adaptive_dt_ts_10_t_10000.000000.vtu overall_mass_density overall_mass_density 1e-10 1e-16
+    reference_t_10000.000000.vtu twophaseflow_adaptive_dt_ts_10_t_10000.000000.vtu saturation saturation 1e-10 0
+    reference_t_100000.000000.vtu twophaseflow_adaptive_dt_ts_108_t_100000.000000.vtu liquid_pressure liquid_pressure 1e-6 1e-12
+    reference_t_100000.000000.vtu twophaseflow_adaptive_dt_ts_108_t_100000.000000.vtu overall_mass_density overall_mass_density 1e-10 1e-16
+    reference_t_100000.000000.vtu twophaseflow_adaptive_dt_ts_108_t_100000.000000.vtu saturation saturation 1e-10 0.0
 )
 
 AddTest(
@@ -39,7 +39,7 @@ AddTest(
     TESTER vtkdiff
     REQUIREMENTS NOT OGS_USE_MPI
     DIFF_DATA
-    ref_t_10000.000000.vtu twophaseflow_small_pcs_0_ts_50_t_10000.000000.vtu liquid_pressure liquid_pressure 1 1e-6
-    ref_t_10000.000000.vtu twophaseflow_small_pcs_0_ts_50_t_10000.000000.vtu overall_mass_density overall_mass_density 1e-4 1e-10
-    ref_t_10000.000000.vtu twophaseflow_small_pcs_0_ts_50_t_10000.000000.vtu saturation saturation 1e-6 1e-10
+    ref_t_10000.000000.vtu twophaseflow_small_ts_50_t_10000.000000.vtu liquid_pressure liquid_pressure 1 1e-6
+    ref_t_10000.000000.vtu twophaseflow_small_ts_50_t_10000.000000.vtu overall_mass_density overall_mass_density 1e-4 1e-10
+    ref_t_10000.000000.vtu twophaseflow_small_ts_50_t_10000.000000.vtu saturation saturation 1e-6 1e-10
 )
