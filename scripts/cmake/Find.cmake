f404c70728818970ef4d157adb8fd96d6675ad57
Lars Bilke <lars.bilke@ufz.de>
2020-11-09 14:18:29 +0100

    [CMake] Fix frontend configuration.

--- scripts/cmake/Find.cmake
+++ scripts/cmake/Find.cmake
@@ -51,8 +51,7 @@ find_program(YARN yarn)
 find_program(PIP pip)
 find_program(PANDOC_CITEPROC pandoc-citeproc)
 
-find_program(MODULE_CMD modulecmd
-    PATHS /usr/local/modules/3.2.10-1/Modules/3.2.10/bin)
+find_program(MODULE_CMD lmod PATHS /software/lmod/lmod/libexec)
 
 find_program(SNAKEMAKE snakemake HINTS ${LOCAL_VIRTUALENV_BIN_DIRS})
 find_program(PARSL parsl-visualize HINTS ${LOCAL_VIRTUALENV_BIN_DIRS})
