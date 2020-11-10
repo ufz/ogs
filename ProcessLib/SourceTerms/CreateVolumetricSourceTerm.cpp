8eeefed0c0ded5231af676c7e10cad0eeae2fa07
Thomas Fischer <thomas.fischer@ufz.de>
2020-11-10 12:03:17 +0100

    [PL/ST] Delete LineSourceTerm impl.
    The VolumetricSourceTerm is equal to the
    LineSourceTerm. Consequently, it is possible to
    use VolumetricSourceTerm and remove LineSourceTerm.

--- ProcessLib/SourceTerms/CreateVolumetricSourceTerm.cpp
+++ ProcessLib/SourceTerms/CreateVolumetricSourceTerm.cpp
@@ -20,15 +20,24 @@
 namespace ProcessLib
 {
 std::unique_ptr<SourceTerm> createVolumetricSourceTerm(
-    BaseLib::ConfigTree const& config, MeshLib::Mesh const& source_term_mesh,
+    BaseLib::ConfigTree const& config, unsigned const bulk_mesh_dimension,
+    MeshLib::Mesh const& source_term_mesh,
     std::unique_ptr<NumLib::LocalToGlobalIndexMap> source_term_dof_table,
     std::vector<std::unique_ptr<ParameterLib::ParameterBase>> const& parameters,
     unsigned const integration_order, unsigned const shapefunction_order)
 {
     //! \ogs_file_param{prj__process_variables__process_variable__source_terms__source_term__type}
-    config.checkConfigParameter("type", "Volumetric");
-
-    DBUG("Constructing VolumetricSourceTerm from config.");
+    auto const type = config.peekConfigParameter<std::string>("type");
+    if (type == "Line")
+    {
+        config.checkConfigParameter("type", "Line");
+        DBUG("Constructing LineSourceTerm from config.");
+    }
+    else
+    {
+        config.checkConfigParameter("type", "Volumetric");
+        DBUG("Constructing VolumetricSourceTerm from config.");
+    }
 
     // source term field name
     auto const& volumetric_source_term_parameter_name =
@@ -42,8 +51,8 @@ std::unique_ptr<SourceTerm> createVolumetricSourceTerm(
          volumetric_source_term.name);
 
     return std::make_unique<VolumetricSourceTerm>(
-        source_term_mesh, std::move(source_term_dof_table), integration_order,
-        shapefunction_order, volumetric_source_term);
+        bulk_mesh_dimension, source_term_mesh, std::move(source_term_dof_table),
+        integration_order, shapefunction_order, volumetric_source_term);
 }
 
 }  // namespace ProcessLib
