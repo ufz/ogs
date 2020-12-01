04de9b5c781aa49e5225c159d1e55036ade7c1a6
Lars Bilke <lars.bilke@ufz.de>
2020-12-01 09:52:45 +0100

    [ci] Fixed cppcheck to correctly ignore certain folder.
    -i cannot be used together with --project. Used --suppress instead.

--- scripts/test/cppcheck.in.sh
+++ scripts/test/cppcheck.in.sh
@@ -7,10 +7,9 @@ ${CPPCHECK_TOOL_PATH} \
         --enable=all \
         --inconclusive \
         -j 4 \
-        -i ${PROJECT_BINARY_DIR}/CMakeFiles \
-        -i ${PROJECT_SOURCE_DIR}/ThirdParty \
-        -i ${PROJECT_SOURCE_DIR}/Applications/DataExplorer \
-        -i ${PROJECT_SOURCE_DIR}/Tests \
+        --suppress=*:*/usr/local\* \
+        --suppress=*:*ThirdParty\* \
+        --suppress=*:*Tests\* \
         --template='{\n  "description": "{message}",\n  "location": {\n    "path": "{file}",\n    "lines": {\n      "begin": {line}\n    }\n  }\n},' \
         --output-file=$OUTPUT_FILE \
 
