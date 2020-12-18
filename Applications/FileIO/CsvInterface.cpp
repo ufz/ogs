40a30e4b6f780255fcb683bf553f6140f09b52f0
Thomas Fischer <thomas.fischer@ufz.de>
2020-12-18 09:40:59 +0100

    [A/IO/CsvInterface] Clang format.

--- Applications/FileIO/CsvInterface.cpp
+++ Applications/FileIO/CsvInterface.cpp
@@ -19,7 +19,8 @@
 
 #include "GeoLib/Point.h"
 
-namespace FileIO {
+namespace FileIO
+{
 CsvInterface::CsvInterface() = default;
 
 std::vector<std::string> CsvInterface::getColumnNames(std::string const& fname,
@@ -59,11 +60,12 @@ std::vector<std::string> CsvInterface::getColumnNames(std::string const& fname,
 }
 
 int CsvInterface::readPoints(std::string const& fname, char delim,
-                             std::vector<GeoLib::Point*> &points)
+                             std::vector<GeoLib::Point*>& points)
 {
     std::ifstream in(fname.c_str());
 
-    if (!in.is_open()) {
+    if (!in.is_open())
+    {
         ERR("CsvInterface::readPoints(): Could not open file {:s}.", fname);
         return -1;
     }
@@ -74,7 +76,7 @@ int CsvInterface::readPoints(std::string const& fname, char delim,
     std::size_t line_count(0);
     std::size_t error_count(0);
     std::list<std::string>::const_iterator it;
-    while ( getline(in, line) )
+    while (getline(in, line))
     {
         line_count++;
         std::list<std::string> const fields = BaseLib::splitString(line, delim);
@@ -89,12 +91,15 @@ int CsvInterface::readPoints(std::string const& fname, char delim,
         }
         it = fields.begin();
         std::array<double, 3> point{};
-        try {
+        try
+        {
             point[0] = std::stod(*it);
             point[1] = std::stod(*(++it));
             point[2] = std::stod(*(++it));
             points.push_back(new GeoLib::Point(point[0], point[1], point[2]));
-        } catch (const std::invalid_argument&) {
+        }
+        catch (const std::invalid_argument&)
+        {
             ERR("Error converting data to coordinates in line {:d}.",
                 line_count);
         }
@@ -103,26 +108,29 @@ int CsvInterface::readPoints(std::string const& fname, char delim,
 }
 
 int CsvInterface::readPoints(std::string const& fname, char delim,
-                             std::vector<GeoLib::Point*> &points,
+                             std::vector<GeoLib::Point*>& points,
                              std::string const& x_column_name,
                              std::string const& y_column_name,
                              std::string const& z_column_name)
 {
     std::ifstream in(fname.c_str());
-    std::array<std::string, 3> const column_names = {{x_column_name, y_column_name, z_column_name}};
+    std::array<std::string, 3> const column_names = {
+        {x_column_name, y_column_name, z_column_name}};
 
-    if (!in.is_open()) {
+    if (!in.is_open())
+    {
         ERR("CsvInterface::readPoints(): Could not open file {:s}.", fname);
         return -1;
     }
 
     std::string line;
     getline(in, line);
-    std::array<std::size_t, 3> const column_idx =
-        {{ CsvInterface::findColumn(line, delim, x_column_name),
-           CsvInterface::findColumn(line, delim, y_column_name),
-           (z_column_name.empty()) ?  CsvInterface::findColumn(line, delim, y_column_name) :
-                                      CsvInterface::findColumn(line, delim, z_column_name) }};
+    std::array<std::size_t, 3> const column_idx = {
+        {CsvInterface::findColumn(line, delim, x_column_name),
+         CsvInterface::findColumn(line, delim, y_column_name),
+         (z_column_name.empty())
+             ? CsvInterface::findColumn(line, delim, y_column_name)
+             : CsvInterface::findColumn(line, delim, z_column_name)}};
 
     for (std::size_t i = 0; i < 3; ++i)
     {
@@ -137,14 +145,14 @@ int CsvInterface::readPoints(std::string const& fname, char delim,
 }
 
 int CsvInterface::readPoints(std::string const& fname, char delim,
-                             std::vector<GeoLib::Point*> &points,
-                             std::size_t x_column_idx,
-                             std::size_t y_column_idx,
+                             std::vector<GeoLib::Point*>& points,
+                             std::size_t x_column_idx, std::size_t y_column_idx,
                              std::size_t z_column_idx)
 {
     std::ifstream in(fname.c_str());
 
-    if (!in.is_open()) {
+    if (!in.is_open())
+    {
         ERR("CsvInterface::readPoints(): Could not open file {:s}.", fname);
         return -1;
     }
@@ -153,34 +161,36 @@ int CsvInterface::readPoints(std::string const& fname, char delim,
     {
         z_column_idx = y_column_idx;
     }
-    std::array<std::size_t, 3> const column_idx = {{ x_column_idx, y_column_idx, z_column_idx }};
+    std::array<std::size_t, 3> const column_idx = {
+        {x_column_idx, y_column_idx, z_column_idx}};
 
     return readPoints(in, delim, points, column_idx);
 }
 
-int CsvInterface::readPoints(std::ifstream &in, char delim,
-                             std::vector<GeoLib::Point*> &points,
+int CsvInterface::readPoints(std::ifstream& in, char delim,
+                             std::vector<GeoLib::Point*>& points,
                              std::array<std::size_t, 3> const& column_idx)
 {
-    std::array<std::size_t, 3> order = {{ 0, 1, 2 }};
+    std::array<std::size_t, 3> order = {{0, 1, 2}};
     std::sort(order.begin(), order.end(),
-        [&column_idx](std::size_t idx1, std::size_t idx2) {return column_idx[idx1] < column_idx[idx2];});
-    std::array<std::size_t, 3> const column_advance =
-        {{ column_idx[order[0]],
-           column_idx[order[1]] - column_idx[order[0]],
-           column_idx[order[2]] - column_idx[order[1]] }};
+              [&column_idx](std::size_t idx1, std::size_t idx2) {
+                  return column_idx[idx1] < column_idx[idx2];
+              });
+    std::array<std::size_t, 3> const column_advance = {
+        {column_idx[order[0]], column_idx[order[1]] - column_idx[order[0]],
+         column_idx[order[2]] - column_idx[order[1]]}};
 
     std::string line;
     std::size_t line_count(0);
     std::size_t error_count(0);
     std::list<std::string>::const_iterator it;
 
-    while ( getline(in, line) )
+    while (getline(in, line))
     {
         line_count++;
         std::list<std::string> const fields = BaseLib::splitString(line, delim);
 
-        if (fields.size() < column_idx[order[2]]+1)
+        if (fields.size() < column_idx[order[2]] + 1)
         {
             ERR("Line {:d} contains not enough columns of data. Skipping "
                 "line...",
@@ -191,15 +201,19 @@ int CsvInterface::readPoints(std::ifstream &in, char delim,
 
         std::array<double, 3> point{};
         it = fields.begin();
-        try {
+        try
+        {
             std::advance(it, column_advance[0]);
             point[order[0]] = std::stod(*it);
             std::advance(it, column_advance[1]);
             point[order[1]] = std::stod(*it);
             std::advance(it, column_advance[2]);
-            point[order[2]] = (column_idx[1] == column_idx[2]) ? 0 : std::stod(*it);
+            point[order[2]] =
+                (column_idx[1] == column_idx[2]) ? 0 : std::stod(*it);
             points.push_back(new GeoLib::Point(point[0], point[1], point[2]));
-        } catch (const std::invalid_argument&) {
+        }
+        catch (const std::invalid_argument&)
+        {
             ERR("Error converting data to coordinates in line {:d}.",
                 line_count);
             error_count++;
@@ -208,7 +222,8 @@ int CsvInterface::readPoints(std::ifstream &in, char delim,
     return error_count;
 }
 
-std::size_t CsvInterface::findColumn(std::string const& line, char delim, std::string const& column_name)
+std::size_t CsvInterface::findColumn(std::string const& line, char delim,
+                                     std::string const& column_name)
 {
     std::list<std::string> const fields = BaseLib::splitString(line, delim);
     if (fields.empty())
@@ -246,12 +261,12 @@ bool CsvInterface::write()
 {
     if (_data.empty())
     {
-        ERR ("CsvInterface::write() - No data to write.");
+        ERR("CsvInterface::write() - No data to write.");
         return false;
     }
 
-    std::size_t const n_vecs (_data.size());
-    std::size_t const vec_size (getVectorSize(0));
+    std::size_t const n_vecs(_data.size());
+    std::size_t const vec_size(getVectorSize(0));
 
     if (_writeCsvHeader)
     {
@@ -263,13 +278,13 @@ bool CsvInterface::write()
         _out << "\n";
     }
 
-    for (std::size_t j=0; j<vec_size; ++j)
+    for (std::size_t j = 0; j < vec_size; ++j)
     {
-        writeValue(0,j);
-        for (std::size_t i=1; i<n_vecs; ++i)
+        writeValue(0, j);
+        for (std::size_t i = 1; i < n_vecs; ++i)
         {
             _out << "\t";
-            writeValue(i,j);
+            writeValue(i, j);
         }
         _out << "\n";
     }
@@ -297,11 +312,13 @@ void CsvInterface::writeValue(std::size_t vec_idx, std::size_t in_vec_idx)
 {
     if (_data[vec_idx].type() == typeid(std::vector<std::string>))
     {
-        _out << boost::any_cast<std::vector<std::string>>(_data[vec_idx])[in_vec_idx];
+        _out << boost::any_cast<std::vector<std::string>>(
+            _data[vec_idx])[in_vec_idx];
     }
     else if (_data[vec_idx].type() == typeid(std::vector<double>))
     {
-        _out << boost::any_cast<std::vector<double>>(_data[vec_idx])[in_vec_idx];
+        _out << boost::any_cast<std::vector<double>>(
+            _data[vec_idx])[in_vec_idx];
     }
     else if (_data[vec_idx].type() == typeid(std::vector<int>))
     {
@@ -309,4 +326,4 @@ void CsvInterface::writeValue(std::size_t vec_idx, std::size_t in_vec_idx)
     }
 }
 
-} // end namespace FileIO
+}  // end namespace FileIO
