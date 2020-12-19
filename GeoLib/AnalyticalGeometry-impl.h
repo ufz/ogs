a5ab6732d7d9fb0f6cc2dc29e73012a4743925a8
Thomas Fischer <thomas.fischer@ufz.de>
2020-12-19 09:03:51 +0100

    [GL/MeL] Subst. Vector3 by Eigen::Vector3d.

--- GeoLib/AnalyticalGeometry-impl.h
+++ GeoLib/AnalyticalGeometry-impl.h
@@ -70,9 +70,8 @@ std::pair<Eigen::Vector3d, double> getNewellPlane(
     return std::make_pair(plane_normal, d);
 }
 
-template<class T_MATRIX>
-void compute2DRotationMatrixToX(MathLib::Vector3 const& v,
-        T_MATRIX & rot_mat)
+template <class T_MATRIX>
+void compute2DRotationMatrixToX(Eigen::Vector3d const& v, T_MATRIX& rot_mat)
 {
     const double cos_theta = v[0];
     const double sin_theta = v[1];
@@ -84,35 +83,43 @@ void compute2DRotationMatrixToX(MathLib::Vector3 const& v,
 }
 
 template <class T_MATRIX>
-void compute3DRotationMatrixToX(MathLib::Vector3  const& v,
-        T_MATRIX & rot_mat)
+void compute3DRotationMatrixToX(Eigen::Vector3d const& v, T_MATRIX& rot_mat)
 {
+    double const eps = std::numeric_limits<double>::epsilon();
     // a vector on the plane
-    MathLib::Vector3 yy(0, 0, 0);
-    if (std::abs(v[0]) > 0.0 && std::abs(v[1]) + std::abs(v[2]) < std::numeric_limits<double>::epsilon()) {
+    Eigen::Vector3d yy({0, 0, 0});
+    if (std::abs(v[0]) > 0.0 && std::abs(v[1]) + std::abs(v[2]) < eps)
+    {
         yy[2] = 1.0;
-    } else if (std::abs(v[1]) > 0.0 && std::abs(v[0]) + std::abs(v[2]) < std::numeric_limits<double>::epsilon()) {
+    }
+    else if (std::abs(v[1]) > 0.0 && std::abs(v[0]) + std::abs(v[2]) < eps)
+    {
         yy[0] = 1.0;
-    } else if (std::abs(v[2]) > 0.0 && std::abs(v[0]) + std::abs(v[1]) < std::numeric_limits<double>::epsilon()) {
+    }
+    else if (std::abs(v[2]) > 0.0 && std::abs(v[0]) + std::abs(v[1]) < eps)
+    {
         yy[1] = 1.0;
-    } else {
-        for (unsigned i = 0; i < 3; i++) {
-            if (std::abs(v[i]) > 0.0) {
+    }
+    else
+    {
+        for (unsigned i = 0; i < 3; i++)
+        {
+            if (std::abs(v[i]) > 0.0)
+            {
                 yy[i] = -v[i];
                 break;
             }
         }
     }
     // z"_vec
-    MathLib::Vector3 zz(MathLib::crossProduct(v, yy));
-    zz.normalize();
+    Eigen::Vector3d const zz = v.cross(yy).normalized();
     // y"_vec
-    yy = MathLib::crossProduct(zz, v);
-    yy.normalize();
+    Eigen::Vector3d const y = zz.cross(v).normalized();
 
-    for (unsigned i=0; i<3; ++i) {
+    for (unsigned i = 0; i < 3; ++i)
+    {
         rot_mat(0, i) = v[i];
-        rot_mat(1, i) = yy[i];
+        rot_mat(1, i) = y[i];
         rot_mat(2, i) = zz[i];
     }
 }
