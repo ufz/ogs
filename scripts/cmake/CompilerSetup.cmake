63bc1cfe6e22434141cd95afab515a1b278d733d
Lars Bilke <lars.bilke@ufz.de>
2020-12-21 14:23:34 +0100

    [CMake] Added MSVC compiler check (minimum: 19.24).

--- scripts/cmake/CompilerSetup.cmake
+++ scripts/cmake/CompilerSetup.cmake
@@ -110,6 +110,11 @@ if(COMPILER_IS_GCC OR COMPILER_IS_CLANG OR COMPILER_IS_INTEL)
 endif()
 
 if(MSVC)
+    if(${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS ${ogs.minimum_version.msvc.compiler})
+        message(FATAL_ERROR "Aborting: Visual Studio compiler \
+            ${ogs.minimum_version.msvc.compiler} is required. Found version \
+            ${CMAKE_CXX_COMPILER_VERSION}.")
+    endif()
     if(${MSVC_TOOLSET_VERSION} LESS ${ogs.minimum_version.msvc.toolset})
         message(FATAL_ERROR "Aborting: Visual Studio ${ogs.minimum_version.msvc.year} \
             is required! Found Visual Studio with toolset version \
