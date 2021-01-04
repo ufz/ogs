a0ddf290694b3dc10dbb7876609ed4b2baf41a6b
Tobias Meisel <tobias.meisel@ufz.de>
2021-01-04 13:43:29 +0100

    [MeL/IO] Introduce data type enum for properties

---
+++ MeshLib/IO/XDMF/MeshPropertyDataType.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// TODO (tm) If used on several other places move definition of propertyVector
+enum class MeshPropertyDataType
+{
+    unknown,
+    float64,
+    float32,
+    int32,
+    int64,
+    uint32,
+    uint64,
+    int8,
+    uint8
+};
