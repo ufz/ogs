7097f977621e4683cff339452523a8ce247536e0
Thomas Fischer <thomas.fischer@ufz.de>
2021-01-25 08:29:24 +0100

    [MaL/LA/Eigen] cppcheck: Parameter 'b' can be declared with const.

--- MathLib/LinAlg/Eigen/EigenTools.h
+++ MathLib/LinAlg/Eigen/EigenTools.h
@@ -34,10 +34,10 @@ void applyKnownSolution(EigenMatrix &A, EigenVector &b, EigenVector &/*x*/,
         const std::vector<EigenMatrix::IndexType> &vec_knownX_id,
         const std::vector<double> &vec_knownX_x, double penalty_scaling = 1e+10);
 
-inline
-void applyKnownSolution(Eigen::MatrixXd const &A, Eigen::VectorXd &b, Eigen::VectorXd &/*x*/,
-        const std::vector<Eigen::MatrixXd::Index> &_vec_knownX_id,
-        const std::vector<double> &_vec_knownX_x, double penalty_scaling = 1e+10)
+inline void applyKnownSolution(
+    Eigen::MatrixXd const& A, Eigen::VectorXd const& b, Eigen::VectorXd& /*x*/,
+    const std::vector<Eigen::MatrixXd::Index>& _vec_knownX_id,
+    const std::vector<double>& _vec_knownX_x, double penalty_scaling = 1e+10)
 {
     (void) A; (void) b; (void) _vec_knownX_id; (void) _vec_knownX_x;
     (void) penalty_scaling;
