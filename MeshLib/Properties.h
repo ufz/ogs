ecece0b63dd2247d048049b2bf29c3e62d3f5b2a
Thomas Fischer <thomas.fischer@ufz.de>
2021-01-25 08:01:52 +0100

    [MeL] Properties: Fix cppcheck 'arg. names diff.' warnings.

--- MeshLib/Properties.h
+++ MeshLib/Properties.h
@@ -86,7 +86,7 @@ public:
     /// Checks if a property vector with given type \c T, \c name, \c
     /// mesh_item_type, and \c number_of_components exists.
     template <typename T>
-    bool existsPropertyVector(std::string const& property_name,
+    bool existsPropertyVector(std::string const& name,
                               MeshItemType const mesh_item_type,
                               int const number_of_components) const;
 
