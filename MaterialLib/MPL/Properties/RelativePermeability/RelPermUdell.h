0c68c226f2dd4f93a020c0565d65246719e0a50f
Wenqing Wang <wenqing.wang@ufz.de>
2021-01-25 16:35:38 +0100

    [Doc/RelPermUdell] Fixed the display of the math formulas of doxygen

--- MaterialLib/MPL/Properties/RelativePermeability/RelPermUdell.h
+++ MaterialLib/MPL/Properties/RelativePermeability/RelPermUdell.h
@@ -25,13 +25,16 @@ class Component;
  *        Kent S Udell. "Heat transfer in porous media considering phase change
  *        and capillarity-the heat pipe effect".In:International Journal of Heat
  *        and Mass Transfer 28.2 (1985), pp. 485-495
- *         Definition:
- *          \f$k_{\textrm{rel}}^{\alpha} =
- * \left(s^{\textrm{eff}}_{\alpha}\right)^{3}\f$ where
- *          - \f$k_{\textrm{rel}}^{\alpha}\f$ is relative permeability of phase
+ *
+ *  Definition:
+ *          \f[ k_{\text{rel}}^{\alpha}
+ * =\left(S^{\text{eff}}_{\alpha}\right)^{3}\f] where
+ *          - \f$k_{\text{rel}}^{\alpha}\f$ is relative permeability of phase
  * \f$\alpha\f$
- *          - \f$s^{\textrm{eff}}_{\alpha}\f$ is the effective saturation of
- * phase \f$\alpha\f$ \details This property must be a medium property, it
+ *          - \f$S^{\text{eff}}_{\alpha}\f$ is the effective saturation of
+ * phase \f$\alpha\f$
+ *
+ * \details This property must be a medium property, it
  * computes the permeability reduction due to saturation as function of
  * capillary pressure.
  */
