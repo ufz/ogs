3a3a77def12e7e932ff2a2d21ad1154bfa928e6a
Wenqing Wang <wenqing.wang@ufz.de>
2021-01-27 17:05:09 +0100

    [MPL/RelPermLiakopoulos] Removed the part for the Brook-Corey model.

--- MaterialLib/MPL/Properties/RelativePermeability/RelPermLiakopoulos.cpp
+++ MaterialLib/MPL/Properties/RelativePermeability/RelPermLiakopoulos.cpp
@@ -41,39 +41,32 @@ PropertyDataType RelPermLiakopoulos::value(
         },
         scale_);
     auto const s_L_res = residual_liquid_saturation_;
-    auto const k_rel_min_GR = min_relative_permeability_gas_;
 
     if (s_L <= s_L_res)
     {
-        return Eigen::Vector2d{0., 1.};
+        return 0.0;
     }
 
     if (s_L >= 1.)
     {
-        return Eigen::Vector2d{1., k_rel_min_GR};
+        return 1.0;
     }
 
     auto const a = parameter_a_;
     auto const b = parameter_b_;
-    auto const lambda = exponent_;
-
-    auto const s_eff = (s_L - s_L_res) / (1. - s_L_res);
 
     auto const k_rel_LR = 1. - a * std::pow(1. - s_L, b);
-    auto const k_rel_GR = (1. - s_eff) * (1. - s_eff) *
-                          (1. - std::pow(s_eff, (2. + lambda) / lambda));
 
-    return Eigen::Vector2d{std::max(k_rel_LR, 0.),
-                           std::max(k_rel_GR, k_rel_min_GR)};
+    return std::max(k_rel_LR, 0.);
 }
 
 PropertyDataType RelPermLiakopoulos::dValue(
-    VariableArray const& variable_array, Variable const primary_variable,
+    VariableArray const& variable_array, Variable const variable,
     ParameterLib::SpatialPosition const& pos, double const t,
     double const dt) const
 {
-    (void)primary_variable;
-    assert((primary_variable == Variable::liquid_saturation) &&
+    (void)variable;
+    assert((variable == Variable::liquid_saturation) &&
            "RelPermLiakopoulos::dValue is implemented for "
            " derivatives with respect to liquid saturation only.");
     /// here, an extra computation of saturation is forced, guaranteeing a
@@ -91,22 +84,10 @@ PropertyDataType RelPermLiakopoulos::dValue(
 
     const double s_L_within_range = std::min(std::max(s_L_res, s_L), s_L_max);
 
-    auto const lambda = exponent_;
     auto const a = parameter_a_;
     auto const b = parameter_b_;
 
-    auto const s_eff = (s_L_within_range - s_L_res) / (s_L_max - s_L_res);
-
-    auto const dk_rel_LRdsL = a * b * std::pow(1. - s_L_within_range, b - 1.);
-
-    auto const twoL_L = (2. + lambda) / lambda;
-    auto const s_G_eff = 1. - s_eff;
-    auto const dk_rel_GRdse =
-        -2. * s_G_eff * (1. - std::pow(s_eff, twoL_L)) -
-        twoL_L * std::pow(s_eff, twoL_L - 1.) * s_G_eff * s_G_eff;
-    auto const dk_rel_GRdsL = dk_rel_GRdse * dse_dsL_;
-
-    return Eigen::Vector2d{dk_rel_LRdsL, dk_rel_GRdsL};
+    return a * b * std::pow(1. - s_L_within_range, b - 1.);
 }
 
 }  // namespace MaterialPropertyLib
