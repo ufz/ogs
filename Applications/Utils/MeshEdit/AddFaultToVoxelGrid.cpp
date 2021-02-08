1271491787f50f694106188c07a12cd8ff0a7ba8
rinkk <karsten.rink@ufz.de>
2021-02-08 11:24:38 +0100

    [utils] added tools to create voxel grid from layers and mark faults within

---
+++ Applications/Utils/MeshEdit/AddFaultToVoxelGrid.cpp
@@ -0,0 +1,310 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2021, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ */
+
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <vector>
+
+// ThirdParty
+#include <tclap/CmdLine.h>
+
+#include "GeoLib/AABB.h"
+#include "InfoLib/GitInfo.h"
+#include "MathLib/Point3d.h"
+#include "MeshLib/Elements/Element.h"
+#include "MeshLib/IO/VtkIO/VtuInterface.h"
+#include "MeshLib/IO/readMeshFromFile.h"
+#include "MeshLib/Mesh.h"
+#include "MeshLib/Node.h"
+
+static std::string mat_name = "MaterialIDs";
+
+// tests if point p is located outside of given AABB
+bool testPointOutsideAABB(MathLib::Point3d const& p,
+                          MathLib::Point3d const& min_pnt,
+                          MathLib::Point3d const& max_pnt)
+{
+    for (std::size_t i = 0; i < 3; ++i)
+    {
+        if (p[i] < min_pnt[i] || p[i] > max_pnt[i])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+// tests if a plane and an AABB are intersecting
+// (based on Christer Ericson "Real Time Collision Detection" 5.2.3)
+bool testAABBIntersectingPlane(Eigen::Vector3d const& aabb_centre,
+                               Eigen::Vector3d const& aabb_extent,
+                               Eigen::Vector3d const& plane_normal,
+                               double const pd)
+{
+    double const r = aabb_extent.x() * std::abs(plane_normal.x()) +
+                     aabb_extent.y() * std::abs(plane_normal.y()) +
+                     aabb_extent.z() * std::abs(plane_normal.z());
+    double const s = plane_normal.dot(aabb_centre) - pd;
+    return std::abs(s) > r;
+}
+
+// separating axes test for triangle vs AABB projection
+// (based on Christer Ericson "Real Time Collision Detection" 5.2.9)
+bool paxx(Eigen::Vector3d const& v0, Eigen::Vector3d const& v1,
+          Eigen::Vector3d const& v2, Eigen::Vector3d const& a,
+          Eigen::Vector3d const& e)
+{
+    double const p0 = v0.dot(a);
+    double const p1 = v1.dot(a);
+    double const p2 = v2.dot(a);
+    double const r = e.x() * std::abs(a.x()) + e.y() * std::abs(a.y()) +
+                     e.z() * std::abs(a.z());
+    return std::max(-std::max({p0, p1, p2}), std::min({p0, p1, p2})) <= r;
+}
+
+// tests if a triangle and an AABB are intersecting
+// (based on Christer Ericson "Real Time Collision Detection" 5.2.9)
+bool testTriangleIntersectingAABB(MeshLib::Node const& n0,
+                                  MeshLib::Node const& n1,
+                                  MeshLib::Node const& n2,
+                                  Eigen::Vector3d const& c,
+                                  Eigen::Vector3d const& e)
+{
+    // Translate triangle as conceptually moving AABB to origin
+    Eigen::Vector3d const v0(Eigen::Vector3d(n0.getCoords()) - c);
+    Eigen::Vector3d const v1(Eigen::Vector3d(n1.getCoords()) - c);
+    Eigen::Vector3d const v2(Eigen::Vector3d(n2.getCoords()) - c);
+
+    // Test the three axes corresponding to the face normals of AABB b
+    if (std::max({v0.x(), v1.x(), v2.x()}) < -e.x() ||
+        std::min({v0.x(), v1.x(), v2.x()}) > e.x())
+    {
+        return false;
+    }
+    if (std::max({v0.y(), v1.y(), v2.y()}) < -e.y() ||
+        std::min({v0.y(), v1.y(), v2.y()}) > e.y())
+    {
+        return false;
+    }
+    if (std::max({v0.z(), v1.z(), v2.z()}) < -e.z() ||
+        std::min({v0.z(), v1.z(), v2.z()}) > e.z())
+    {
+        return false;
+    }
+
+    // Compute edge vectors for triangle
+    Eigen::Vector3d const tri_edge0(v1 - v0);
+    Eigen::Vector3d const tri_edge1(v2 - v1);
+    Eigen::Vector3d const tri_edge2(v0 - v2);
+
+    // separating axes
+    std::vector<Eigen::Vector3d> axx(9);
+    axx[0] = Eigen::Vector3d({0, -tri_edge0.z(), tri_edge0.y()});
+    axx[1] = Eigen::Vector3d({0, -tri_edge1.z(), tri_edge1.y()});
+    axx[2] = Eigen::Vector3d({0, -tri_edge2.z(), tri_edge2.y()});
+    axx[3] = Eigen::Vector3d({tri_edge0.z(), 0, -tri_edge0.x()});
+    axx[4] = Eigen::Vector3d({tri_edge1.z(), 0, -tri_edge1.x()});
+    axx[5] = Eigen::Vector3d({tri_edge2.z(), 0, -tri_edge2.x()});
+    axx[6] = Eigen::Vector3d({-tri_edge0.y(), tri_edge0.x(), 0});
+    axx[7] = Eigen::Vector3d({-tri_edge1.y(), tri_edge1.x(), 0});
+    axx[8] = Eigen::Vector3d({-tri_edge2.y(), tri_edge2.x(), 0});
+
+    // testing separating axes against triangle
+    for (auto const& a : axx)
+    {
+        if (!paxx(v0, v1, v2, a, e))
+            return false;
+    }
+
+    // Test separating axis corresponding to triangle face normal
+    Eigen::Vector3d const plane_normal(tri_edge0.cross(tri_edge1));
+    double const pd = plane_normal.dot(v0);
+    return testAABBIntersectingPlane(c, e, plane_normal, pd);
+}
+
+void markFaults(MeshLib::Mesh& mesh, MeshLib::Mesh const& fault,
+                int const fault_id, std::array<double, 3> half_cell_size)
+{
+    auto const& elems = mesh.getElements();
+    std::size_t const n_elems = mesh.getNumberOfElements();
+    auto mat_ids = mesh.getProperties().getPropertyVector<int>(mat_name);
+    auto const& fnodes = fault.getNodes();
+    auto const& felems = fault.getElements();
+    std::size_t const n_felems = fault.getNumberOfElements();
+    GeoLib::AABB fault_aabb(fnodes.cbegin(), fnodes.cend());
+    auto min_pnt = fault_aabb.getMinPoint();
+    auto max_pnt = fault_aabb.getMaxPoint();
+
+    // get bounding box of fault + voxel extent
+    for (std::size_t i = 0; i < 3; ++i)
+    {
+        min_pnt[i] -= half_cell_size[i];
+        max_pnt[i] += half_cell_size[i];
+    }
+
+    // test each voxel grid element vs each fault triangle
+    Eigen::Vector3d const extent(
+        {half_cell_size[0], half_cell_size[1], half_cell_size[2]});
+    for (std::size_t j = 0; j < n_elems; ++j)
+    {
+        // test if bounding box of fault is intersecting voxel
+        auto const& centre_pnt = MeshLib::getCenterOfGravity(*elems[j]);
+        if (testPointOutsideAABB(centre_pnt, min_pnt, max_pnt))
+        {
+            continue;
+        }
+
+        // test if voxel is intersecting triangle
+        Eigen::Vector3d const c(centre_pnt.getCoords());
+        for (auto const* const fault_elem : felems)
+        {
+            if (fault_elem->getDimension() != 2)
+            {
+                continue;
+            }
+
+            if (testTriangleIntersectingAABB(
+                    *fault_elem->getNode(0), *fault_elem->getNode(1),
+                    *fault_elem->getNode(2), c, extent))
+            {
+                (*mat_ids)[j] = fault_id;
+                break;
+            }
+
+            if (fault_elem->getGeomType() == MeshLib::MeshElemType::QUAD)
+            {
+                if (testTriangleIntersectingAABB(
+                        *fault_elem->getNode(0), *fault_elem->getNode(2),
+                        *fault_elem->getNode(3), c, extent))
+                {
+                    (*mat_ids)[j] = fault_id;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+// test if input mesh is voxel grid
+bool isVoxelGrid(MeshLib::Mesh const& mesh)
+{
+    auto const& elements = mesh.getElements();
+    auto all_hex = std::find_if_not(
+        elements.cbegin(), elements.cend(), [&](auto const& e) {
+            return (e->getGeomType() == MeshLib::MeshElemType::HEXAHEDRON);
+        });
+    if (all_hex != elements.cend())
+    {
+        ERR("Input mesh needs to be voxel grid (i.e. equally sized axis "
+            "aligned hexahedra).");
+        return false;
+    }
+    for (auto const& e : elements)
+    {
+        auto const n = e->getNodes();
+        if ((*n[0])[2] != (*n[1])[2] || (*n[1])[2] != (*n[2])[2] ||
+            (*n[4])[2] != (*n[5])[2] || (*n[5])[2] != (*n[6])[2] ||
+            (*n[1])[0] != (*n[2])[0] || (*n[2])[0] != (*n[5])[0] ||
+            (*n[0])[0] != (*n[3])[0] || (*n[3])[0] != (*n[7])[0])
+        {
+            ERR("Input mesh needs to be voxel grid (i.e. equally sized axis "
+                "aligned hexahedra).");
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(int argc, char* argv[])
+{
+    constexpr std::size_t mat_not_set = std::numeric_limits<std::size_t>::max();
+
+    TCLAP::CmdLine cmd(
+        "Marks all elements in a voxel grid (i.e. a structured hex grid, for "
+        "instance created with Layers2Grid or Vtu2Grid) that are intersected "
+        "by a triangulated 2D mesh representing a fault or some other "
+        "significant structure. The material group for those intersected "
+        "elements can be explicitely specified, otherwise the largest existing "
+        "MaterialID will be increased by one.\n\n"
+        "OpenGeoSys-6 software, version " +
+            GitInfoLib::GitInfo::ogs_version +
+            ".\n"
+            "Copyright (c) 2012-2020, OpenGeoSys Community "
+            "(http://www.opengeosys.org)",
+        ' ', GitInfoLib::GitInfo::ogs_version);
+    TCLAP::ValueArg<std::size_t> id_arg(
+        "m", "material", "material id for cells intersected by fault", false,
+        mat_not_set, "non-negative integer");
+    cmd.add(id_arg);
+
+    TCLAP::ValueArg<std::string> output_arg(
+        "o", "output", "name of output mesh (*.vtu)", true, "", "string");
+    cmd.add(output_arg);
+
+    TCLAP::ValueArg<std::string> fault_arg(
+        "f", "fault", "name of mesh representing fault (*.vtu)", true, "",
+        "string");
+    cmd.add(fault_arg);
+
+    TCLAP::ValueArg<std::string> input_arg(
+        "i", "input",
+        "name of the input file list containing the paths the all input layers "
+        "in correct order from top to bottom",
+        true, "", "string");
+    cmd.add(input_arg);
+    cmd.parse(argc, argv);
+
+    std::string const input_name = input_arg.getValue();
+    std::string const fault_name = fault_arg.getValue();
+    std::string const output_name = output_arg.getValue();
+
+    std::unique_ptr<MeshLib::Mesh> mesh(
+        MeshLib::IO::readMeshFromFile(input_name));
+    if (!isVoxelGrid(*mesh))
+    {
+        return EXIT_FAILURE;
+    }
+    auto const& mat_ids = MeshLib::materialIDs(*mesh);
+    if (!mat_ids)
+    {
+        ERR("Input mesh has no material IDs");
+        return EXIT_FAILURE;
+    }
+
+    std::unique_ptr<MeshLib::Mesh> fault(
+        MeshLib::IO::readMeshFromFile(fault_name));
+    if (fault->getDimension() != 2)
+    {
+        ERR("Fault needs to be a 2D mesh.");
+        return EXIT_FAILURE;
+    }
+
+    int fault_id = id_arg.getValue();
+    if (!id_arg.isSet())
+    {
+        auto it = std::max_element(mat_ids->cbegin(), mat_ids->cend());
+        fault_id = *it + 1;
+    }
+
+    std::array<double, 3> half_cell_size;
+    {
+        auto const n = *mesh->getElement(0)->getNode(0);
+        auto const c = MeshLib::getCenterOfGravity(*mesh->getElement(0));
+        half_cell_size[0] = std::abs(c[0] - n[0]);
+        half_cell_size[1] = std::abs(c[1] - n[1]);
+        half_cell_size[2] = std::abs(c[2] - n[2]);
+    };
+
+    markFaults(*mesh, *fault, fault_id, half_cell_size);
+
+    MeshLib::IO::VtuInterface vtu(mesh.get());
+    vtu.writeToFile(output_name);
+    return EXIT_SUCCESS;
+}
---
+++ Applications/Utils/MeshEdit/Layers2Grid.cpp
@@ -0,0 +1,252 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2021, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ */
+
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <vector>
+
+// ThirdParty
+#include <tclap/CmdLine.h>
+
+#include "BaseLib/IO/readStringListFromFile.h"
+#include "GeoLib/AABB.h"
+#include "InfoLib/GitInfo.h"
+#include "MathLib/Point3d.h"
+#include "MeshLib/Elements/Element.h"
+#include "MeshLib/IO/VtkIO/VtuInterface.h"
+#include "MeshLib/IO/readMeshFromFile.h"
+#include "MeshLib/Mesh.h"
+#include "MeshLib/MeshEditing/ProjectPointOnMesh.h"
+#include "MeshLib/MeshEditing/RemoveMeshComponents.h"
+#include "MeshLib/MeshGenerators/MeshGenerator.h"
+#include "MeshLib/MeshSearch/MeshElementGrid.h"
+#include "MeshLib/Node.h"
+
+static std::string mat_name = "MaterialIDs";
+
+// returns the AABB of all layers read so far
+void adjustExtent(std::pair<MathLib::Point3d, MathLib::Point3d>& extent,
+                  MeshLib::Mesh const& mesh)
+{
+    auto const& nodes = mesh.getNodes();
+    GeoLib::AABB aabb(nodes.cbegin(), nodes.cend());
+    for (std::size_t i = 0; i < 3; ++i)
+    {
+        extent.first[i] = std::min(extent.first[i], aabb.getMinPoint()[i]);
+        extent.second[i] = std::max(extent.second[i], aabb.getMaxPoint()[i]);
+    }
+}
+
+// creates a voxel grid of the AABB of all layers
+MeshLib::Mesh* generateInitialMesh(
+    std::pair<MathLib::Point3d, MathLib::Point3d>& extent,
+    std::array<double, 3> const& res)
+{
+    INFO("Creating initial mesh...");
+    std::array<double, 3> mesh_extent{{extent.second[0] - extent.first[0],
+                                       extent.second[1] - extent.first[1],
+                                       extent.second[2] - extent.first[2]}};
+    std::array<std::size_t, 3> const n_cells{
+        {static_cast<std::size_t>(std::ceil(mesh_extent[0] / res[0])),
+         static_cast<std::size_t>(std::ceil(mesh_extent[1] / res[1])),
+         static_cast<std::size_t>(std::ceil(mesh_extent[2] / res[2]))}};
+    for (std::size_t i = 0; i < 3; ++i)
+    {
+        double const tmp_enlarge = n_cells[i] * res[i];
+        double const offset = (tmp_enlarge - mesh_extent[i]) / 2.0;
+        mesh_extent[i] = tmp_enlarge;
+        extent.first[i] -= offset;
+        extent.second[i] += offset;
+    }
+    MeshLib::Mesh* mesh = MeshLib::MeshGenerator::generateRegularHexMesh(
+        mesh_extent[0], mesh_extent[1], mesh_extent[2], n_cells[0], n_cells[1],
+        n_cells[2], extent.first);
+    auto mat_id = mesh->getProperties().createNewPropertyVector<int>(
+        mat_name, MeshLib::MeshItemType::Cell);
+    mat_id->insert(mat_id->end(), mesh->getNumberOfElements(), -1);
+    return mesh;
+}
+
+// sets material IDs for all elements depending on the layers they are located
+// between
+void setMaterialIDs(MeshLib::Mesh& mesh,
+                    std::vector<std::unique_ptr<MeshLib::Mesh>> const& layers,
+                    std::array<double, 3> half_cell_size)
+{
+    INFO("Setting material properties...");
+    std::size_t const n_layers = layers.size();
+    auto const& elems = mesh.getElements();
+    std::size_t const n_elems = mesh.getNumberOfElements();
+    auto mat_ids = mesh.getProperties().getPropertyVector<int>(mat_name);
+    std::vector<double> is_set(n_elems, false);
+    for (int i = n_layers - 1; i >= 0; --i)
+    {
+        INFO("-> Layer {:d}", n_layers - i - 1);
+        MeshLib::MeshElementGrid const grid(*layers[i]);
+        double const max_edge(layers[i]->getMaxEdgeLength());
+        for (std::size_t j = 0; j < n_elems; ++j)
+        {
+            if (is_set[j])
+            {
+                continue;
+            }
+
+            MeshLib::Node const node = MeshLib::getCenterOfGravity(*elems[j]);
+            constexpr double max_val = std::numeric_limits<double>::max();
+            MathLib::Point3d const min_vol{
+                {node[0] - max_edge, node[1] - max_edge, -max_val}};
+            MathLib::Point3d const max_vol{
+                {node[0] + max_edge, node[1] + max_edge, max_val}};
+            auto const& elems = grid.getElementsInVolume(min_vol, max_vol);
+            if (elems.size() > 0)
+            {
+                int x = 3;
+            }
+            auto const* proj_elem =
+                MeshLib::ProjectPointOnMesh::getProjectedElement(elems, node);
+            if (proj_elem == nullptr)
+            {
+                continue;
+            }
+            if (node[2] >
+                MeshLib::ProjectPointOnMesh::getElevation(*proj_elem, node))
+            {
+                (*mat_ids)[j] = n_layers - i - 1;
+            }
+            else
+            {
+                is_set[j] = true;
+            }
+        }
+    }
+    // set all elements above uppermost layer back to -1 so they are
+    // subsequently cut
+    std::replace(mat_ids->begin(), mat_ids->end(),
+                 static_cast<int>(n_layers - 1), -1);
+}
+
+// removes all elements that have not been marked as being located between two
+// layers
+MeshLib::Mesh* removeUnusedElements(MeshLib::Mesh const& mesh)
+{
+    std::vector<std::size_t> marked_elems;
+    auto const mat_ids = *MeshLib::materialIDs(mesh);
+    std::size_t const n_elems = mat_ids.size();
+    for (std::size_t i = 0; i < n_elems; ++i)
+    {
+        if (mat_ids[i] == -1)
+        {
+            marked_elems.push_back(i);
+        }
+    }
+
+    if (marked_elems.size() == mesh.getNumberOfElements())
+    {
+        return nullptr;
+    }
+    return MeshLib::removeElements(mesh, marked_elems, "mesh");
+}
+
+int main(int argc, char* argv[])
+{
+    TCLAP::CmdLine cmd(
+        "Reads a list of 2D unstructured mesh layers and samples them onto a "
+        "structured grid of the same extent. Note, that a large cube size may "
+        "result in an undersampling of the original structure.\nCube sizes are "
+        "defines by x/y/z-parameters. For equilateral cubes, only the "
+        "x-parameter needs to be set.\n\n"
+        "OpenGeoSys-6 software, version " +
+            GitInfoLib::GitInfo::ogs_version +
+            ".\n"
+            "Copyright (c) 2012-2020, OpenGeoSys Community "
+            "(http://www.opengeosys.org)",
+        ' ', GitInfoLib::GitInfo::ogs_version);
+    TCLAP::ValueArg<double> z_arg("z", "cellsize-z",
+                                  "edge length of cubes in z-direction (depth)",
+                                  false, 1000, "floating point number");
+    cmd.add(z_arg);
+
+    TCLAP::ValueArg<double> y_arg(
+        "y", "cellsize-y", "edge length of cubes in y-direction (latitude)",
+        false, 1000, "floating point number");
+    cmd.add(y_arg);
+
+    TCLAP::ValueArg<double> x_arg(
+        "x", "cellsize-x",
+        "edge length of cubes in x-direction (longitude) or all directions, if "
+        "y and z are not set",
+        true, 1000, "floating point number");
+    cmd.add(x_arg);
+
+    TCLAP::ValueArg<std::string> output_arg(
+        "o", "output", "name of output mesh (*.vtu)", true, "", "string");
+    cmd.add(output_arg);
+
+    TCLAP::ValueArg<std::string> input_arg(
+        "i", "input",
+        "name of the input file list containing the paths the all input layers "
+        "in correct order from top to bottom",
+        true, "", "string");
+    cmd.add(input_arg);
+    cmd.parse(argc, argv);
+
+    if ((y_arg.isSet() && !z_arg.isSet()) ||
+        ((!y_arg.isSet() && z_arg.isSet())))
+    {
+        ERR("For equilateral cubes, only x needs to be set. For unequal "
+            "cuboids, all three edge lengths (x/y/z) need to be specified.");
+        return EXIT_FAILURE;
+    }
+
+    double const x_size = x_arg.getValue();
+    double const y_size = (y_arg.isSet()) ? y_arg.getValue() : x_arg.getValue();
+    double const z_size = (z_arg.isSet()) ? z_arg.getValue() : x_arg.getValue();
+    std::array<double, 3> const cellsize = {x_size, y_size, z_size};
+
+    std::string const input_name = input_arg.getValue();
+    std::string const output_name = output_arg.getValue();
+    auto const layer_names = BaseLib::IO::readStringListFromFile(input_name);
+    if (layer_names.size() < 2)
+    {
+        ERR("At least two layers are required to create a 3D Mesh");
+        return EXIT_FAILURE;
+    }
+
+    std::vector<std::unique_ptr<MeshLib::Mesh>> layers;
+    layers.reserve(layer_names.size());
+    constexpr double minval = std::numeric_limits<double>::max();
+    constexpr double maxval = std::numeric_limits<double>::lowest();
+    std::pair<MathLib::Point3d, MathLib::Point3d> extent(
+        MathLib::Point3d{{minval, minval, minval}},
+        MathLib::Point3d{{maxval, maxval, maxval}});
+    for (auto layer : layer_names)
+    {
+        std::unique_ptr<MeshLib::Mesh> mesh(
+            MeshLib::IO::readMeshFromFile(layer));
+        adjustExtent(extent, *mesh);
+        layers.emplace_back(std::move(mesh));
+    }
+
+    std::unique_ptr<MeshLib::Mesh> mesh(generateInitialMesh(extent, cellsize));
+    std::array<double, 3> const half_cell_size{
+        {cellsize[0] / 2.0, cellsize[1] / 2.0, cellsize[2] / 2.0}};
+    setMaterialIDs(*mesh, layers, half_cell_size);
+
+    std::unique_ptr<MeshLib::Mesh> new_mesh(removeUnusedElements(*mesh));
+    if (new_mesh == nullptr)
+    {
+        ERR("Error generating mesh...");
+        return EXIT_FAILURE;
+    }
+
+    MeshLib::IO::VtuInterface vtu(new_mesh.get());
+    vtu.writeToFile(output_name);
+    return EXIT_SUCCESS;
+}
