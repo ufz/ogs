032398f370d50724ac686268c9b09a93b919b94b
Wenqing Wang <wenqing.wang@ufz.de>
2021-02-17 16:03:18 +0100

    [MPL] Registed SoilThermalConductivitySomerton

--- MaterialLib/MPL/CreateProperty.cpp
+++ MaterialLib/MPL/CreateProperty.cpp
@@ -215,6 +215,11 @@ std::unique_ptr<MaterialPropertyLib::Property> createProperty(
         return createLinearSaturationSwellingStress(config);
     }
 
+    if (boost::iequals(property_type, "SoilThermalConductivitySomerton"))
+    {
+        return createSoilThermalConductivitySomerton(config);
+    }
+
     // If none of the above property types are found, OGS throws an error.
     OGS_FATAL("The specified component property type '{:s}' was not recognized",
               property_type);
