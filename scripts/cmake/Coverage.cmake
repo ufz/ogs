51a234950f2bdf0c318ff423de716963bfe7826b
Lars Bilke <lars.bilke@ufz.de>
2021-02-17 13:01:54 +0100

    [CMake,T] Added target ctest_coverage.

--- scripts/cmake/Coverage.cmake
+++ scripts/cmake/Coverage.cmake
@@ -5,7 +5,6 @@ endif()
 
 include(CodeCoverage)
 append_coverage_compiler_flags()
-# TODO: segfault in MeshLibMappedPropertyVector.Double?
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Og")
 
 if(NOT FASTCOV_PATH)
@@ -17,13 +16,32 @@ if(NOT FASTCOV_PATH)
         REQUIRED NO_DEFAULT_PATH
     )
 endif()
+
+if(DEFINED ENV{CI})
+    set(COVERAGE_ADDITIONAL_ARGS SKIP_HTML)
+endif()
+
+# TODO: segfault in MeshLibMappedPropertyVector.Double|Int
 setup_target_for_coverage_fastcov(
     NAME testrunner_coverage
     EXECUTABLE $<TARGET_FILE:testrunner> -l warn --gtest_filter=-MeshLibMappedPropertyVector.*:GeoLib.SearchNearestPointsInDenseGrid
     DEPENDENCIES testrunner
     FASTCOV_ARGS --include ${PROJECT_SOURCE_DIR}
+    ${COVERAGE_ADDITIONAL_ARGS}
     EXCLUDE
         Applications/CLI/
         ProcessLib/
+        Tests/
+)
+
+# TODO: segfault in Vtu2Grid
+setup_target_for_coverage_fastcov(
+    NAME ctest_coverage
+    EXECUTABLE ctest -E "partmesh|Vtu2Grid"
+    DEPENDENCIES all
+    FASTCOV_ARGS --include ${PROJECT_SOURCE_DIR}
+    ${COVERAGE_ADDITIONAL_ARGS}
+    EXCLUDE
+        Applications/CLI/
         Tests/
 )
