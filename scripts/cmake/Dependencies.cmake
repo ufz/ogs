59179505f1fbac8ecec273a4e5c033948262dfe3
Lars Bilke <lars.bilke@ufz.de>
2021-03-11 09:01:15 +0100

    [eve] Use cpm instead of env modules.

--- scripts/cmake/Dependencies.cmake
+++ scripts/cmake/Dependencies.cmake
@@ -133,6 +133,10 @@ if(OGS_USE_XDMF)
         add_library(ZLIB::ZLIB ALIAS zlibstatic)
     endif()
 
+    if(OGS_USE_MPI)
+        set(hdf5_options "HDF5_ENABLE_PARALLEL ON")
+    endif()
+
     string(REPLACE "." "_" HDF5_TAG ${ogs.minimum_version.hdf5})
     CPMFindPackage(
         NAME HDF5
@@ -148,6 +152,7 @@ if(OGS_USE_XDMF)
             "HDF5_BUILD_FORTRAN OFF"
             "HDF5_BUILD_CPP_LIB OFF"
             "HDF5_BUILD_JAVA OFF"
+            ${hdf5_options}
         EXCLUDE_FROM_ALL YES
     )
     if(HDF5_ADDED)
