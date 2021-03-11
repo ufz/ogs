096f0a6ac589e48a00af3024a03db90f68021f68
Lars Bilke <lars.bilke@ufz.de>
2021-03-11 15:46:49 +0100

    [pre-commit] Added cmake-lint.

--- scripts/cmake/test/MoveDataToStore.cmake
+++
@@ -1,7 +0,0 @@
-# Get all files matching .ExternalData_<algo>_<hash>
-file(GLOB_RECURSE FILES "" ${ExternalData_SOURCE_ROOT}/.ExternalData_${ExternalData_LINK_CONTENT}_*)
-foreach(HASH_FILE ${FILES})
-    string(REGEX MATCH [^_]+$ HASH ${HASH_FILE})
-    message("Copying ${HASH_FILE} to ${ExternalData_OBJECT_STORES}/${HASH}")
-    file(RENAME ${HASH_FILE} ${ExternalData_OBJECT_STORES}/${ExternalData_LINK_CONTENT}/${HASH})
-endforeach()
