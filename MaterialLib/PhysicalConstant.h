10a1660b266d5ce990a32ce7098825b2d898b899
montoyav <vanessa.montoya@ufz.de>
2021-03-22 16:59:16 +0000

    Change of nomenclature (in the documentation) of the physical constants in the Material Library

--- MaterialLib/PhysicalConstant.h
+++ MaterialLib/PhysicalConstant.h
@@ -31,7 +31,7 @@ const double CelsiusZeroInKelvin = 273.15;
 const double IdealGasConstant = 8.3144621;
 
 /**
- * Molar masses of certain elements and chemical compounds
+ * Atomic masses of certain elements and molar masses of chemical compounds
  */
 namespace MolarMass
 {
@@ -41,9 +41,9 @@ namespace MolarMass
  * - Source:       http://www.ciaaw.org/pubs/TSAW2013_xls.xls
  * - Date visited: 2015-05-28
  *
- * According to the IUPAC report the molar mass of O is in the range [15.999 03,
+ * According to the IUPAC report the atomic mass of O is in the range [15.999 03,
  * 15.999 77] g/mol
- * and the molar mass of H is in the range [1.007 84, 1.008 11] g/mol
+ * and the atomic mass of H is in the range [1.007 84, 1.008 11] g/mol
  */
 const double Water = 0.018016;  ///< kg \per{mol}
 
@@ -53,7 +53,7 @@ const double Water = 0.018016;  ///< kg \per{mol}
  * - Source:       http://www.ciaaw.org/pubs/TSAW2013_xls.xls
  * - Date visited: 2015-05-28
  *
- * According to the IUPAC report the molar mass of N is in the range [14.006 43,
+ * According to the IUPAC report the atomic mass of N is in the range [14.006 43,
  * 14.007 28] g/mol
  */
 const double N2 = 0.028013;  ///< kg \per{mol}
@@ -64,7 +64,7 @@ const double N2 = 0.028013;  ///< kg \per{mol}
  * - Source:       http://www.ciaaw.org/pubs/TSAW2013_xls.xls
  * - Date visited: 2015-05-28
  *
- * According to the IUPAC report the molar mass of O is in the range [15.999 03,
+ * According to the IUPAC report the atomic mass of O is in the range [15.999 03,
  * 15.999 77] g/mol
  */
 const double O2 = 0.032;  ///< kg \per{mol}
