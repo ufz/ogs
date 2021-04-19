903b122c66603fe47faa6ac3828a4e4046c7c3bf
Lars Bilke <lars.bilke@ufz.de>
2021-04-19 08:22:08 +0200

    [de] Fixed link in comment.

--- Applications/DataExplorer/Base/StrictIntValidator.h
+++ Applications/DataExplorer/Base/StrictIntValidator.h
@@ -18,18 +18,21 @@
 
 /**
  * \brief A validator for an input field which only accepts integers.
- * Source code adapted from [Qt developer fac](http://developer.qt.nokia.com/faq/answer/i_can_still_insert_numbers_outside_the_range_specified_with_a_qdoublevalida)
+ * Source code adapted from Qt developer faq:
+ * https://web.archive.org/web/20100702180529/http://developer.qt.nokia.com/faq/answer/i_can_still_insert_numbers_outside_the_range_specified_with_a_qdoublevalida
  */
 class StrictIntValidator : public QIntValidator
 {
 public:
     StrictIntValidator(int min, int max, QObject* parent = nullptr)
         : QIntValidator(min, max, parent)
-    {}
+    {
+    }
 
     QValidator::State validate(QString& input, int& pos) const override
     {
-        if (input.isEmpty()) return Intermediate;
+        if (input.isEmpty())
+            return Intermediate;
 
         if (QIntValidator::validate(input, pos) != Acceptable)
             return Invalid;
