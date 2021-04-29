abc42b814d841466369d36140bcf9caabc2e64ee
Lars Bilke <lars.bilke@ufz.de>
2021-04-29 11:04:02 +0200

    [cmake] Fix clcache.
    Using clcache-launcher.
    
    https://gitlab.com/timblechmann/clcache-launcher

--- scripts/cmake/CLCacheSetup.cmake
+++ scripts/cmake/CLCacheSetup.cmake
@@ -2,5 +2,12 @@ if(NOT WIN32 OR NOT CLCACHE_TOOL_PATH OR OGS_DISABLE_CLCACHE)
     return()
 endif()
 
-set(CMAKE_C_COMPILER_LAUNCHER ${CLCACHE_TOOL_PATH})
-set(CMAKE_CXX_COMPILER_LAUNCHER ${CLCACHE_TOOL_PATH})
+file(DOWNLOAD
+    https://gitlab.com/timblechmann/clcache-launcher/uploads/c11842f963af3543b448cb400ed5fce9/clcache-launcher.exe
+    ${PROJECT_BINARY_DIR}/clcache-launcher.exe
+)
+
+set(CMAKE_C_COMPILER_LAUNCHER ${PROJECT_BINARY_DIR}/clcache-launcher.exe)
+set(CMAKE_CXX_COMPILER_LAUNCHER ${PROJECT_BINARY_DIR}/clcache-launcher.exe)
+
+message(STATUS "clcache enabled! (${CLCACHE_TOOL_PATH})")
