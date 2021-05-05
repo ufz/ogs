51c2d54325fec0447f1ca0bf4e0a48a81192c6cd
Dmitri Naumov <github@naumov.de>
2021-05-05 11:12:05 +0200

    [PL/HM] Excavation support.

--- ProcessLib/HydroMechanics/HydroMechanicsProcess.cpp
+++ ProcessLib/HydroMechanics/HydroMechanicsProcess.cpp
@@ -362,10 +362,13 @@ void HydroMechanicsProcess<DisplacementDim>::assembleConcreteProcess(
 
     std::vector<std::reference_wrapper<NumLib::LocalToGlobalIndexMap>>
         dof_table = {std::ref(*_local_to_global_index_map)};
-    // Call global assembler for each local assembly item.
-    GlobalExecutor::executeMemberDereferenced(
+
+    ProcessLib::ProcessVariable const& pv = getProcessVariables(process_id)[0];
+
+    GlobalExecutor::executeSelectedMemberDereferenced(
         _global_assembler, &VectorMatrixAssembler::assemble, _local_assemblers,
-        dof_table, t, dt, x, xdot, process_id, M, K, b);
+        pv.getActiveElementIDs(), dof_table, t, dt, x, xdot, process_id, M, K,
+        b);
 }
 
 template <int DisplacementDim>
