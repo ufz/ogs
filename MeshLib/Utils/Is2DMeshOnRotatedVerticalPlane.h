0ccc2d516d0c0cf6f939c0c826a5a6b0d18c3f22
Wenqing Wang <wenqing.wang@ufz.de>
2021-05-05 15:58:10 +0200

    [Mesh/Utils] Added a function to check whether 2D mesh is vertical

---
+++ MeshLib/Utils/Is2DMeshOnRotatedVerticalPlane.h
@@ -0,0 +1,22 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2021, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ * Created on April 22, 2021, 11:52 AM
+ */
+
+#pragma once
+
+#include <vector>
+
+namespace MeshLib
+{
+class Mesh;
+
+bool is2DMeshOnRotatedVerticalPlane(Mesh const& mesh);
+
+};  // namespace MeshLib
