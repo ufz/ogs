7ae4794c0a5775f88ce4e109ceda0a3f4d2926e9
Lars Bilke <lars.bilke@ufz.de>
2021-05-18 11:31:33 +0200

    [ctest] 1 GB output limit.

--- scripts/cmake/test/CTestCustom.in.cmake
+++ scripts/cmake/test/CTestCustom.in.cmake
@@ -10,3 +10,7 @@ foreach(line IN LISTS LINES)
     # and write the (changed) line ...
     file(APPEND "@PROJECT_BINARY_DIR@/CTestTestfile.cmake" "${STRIPPED}\n")
 endforeach()
+
+# Limit test output to 1 GB to prevent lower default limit in Dash-mode
+set(CTEST_CUSTOM_MAXIMUM_FAILED_TEST_OUTPUT_SIZE 1073741824)
+set(CTEST_CUSTOM_MAXIMUM_PASSED_TEST_OUTPUT_SIZE 1073741824)
