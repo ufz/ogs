f77eba8febac7493d1fe0de6b84100a8b80ec5a4
renchao_lu <renchao.lu@gmail.com>
2021-06-02 19:08:19 +0200

    [PL/Stokes] add local assembler interface.

---
+++ ProcessLib/StokesFlow/LocalAssemblerInterface.h
@@ -0,0 +1,25 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2021, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#pragma once
+
+#include "NumLib/Extrapolation/ExtrapolatableElement.h"
+#include "ProcessLib/LocalAssemblerInterface.h"
+
+namespace ProcessLib::StokesFlow
+{
+class StokesFlowLocalAssemblerInterface
+    : public ProcessLib::LocalAssemblerInterface,
+      public NumLib::ExtrapolatableElement
+{
+public:
+    StokesFlowLocalAssemblerInterface() = default;
+};
+}  // namespace ProcessLib::StokesFlow
