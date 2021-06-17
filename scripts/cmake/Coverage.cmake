9f84df9551259ffccabda55221a17212094b40ff
Lars Bilke <lars.bilke@ufz.de>
2021-06-17 15:01:27 +0200

    [coverage] genhtml fix for macOS.

--- scripts/cmake/Coverage.cmake
+++ scripts/cmake/Coverage.cmake
@@ -3,6 +3,16 @@ if(NOT FASTCOV_PATH AND NOT POETRY)
     message(FATAL_ERROR "Code coverage requires either fastcov or poetry.")
 endif()
 
+# https://github.com/linux-test-project/lcov/pull/125
+if(APPLE)
+    file(
+        DOWNLOAD
+        https://raw.githubusercontent.com/linux-test-project/lcov/41d8655951d6898511f98be2a2dbcfbe662f0b17/bin/genhtml
+        ${PROJECT_BINARY_DIR}/bin/genhtml
+    )
+    set(GENHTML_PATH ${PROJECT_BINARY_DIR}/bin/genhtml)
+endif()
+
 include(CodeCoverage)
 append_coverage_compiler_flags()
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Og")
