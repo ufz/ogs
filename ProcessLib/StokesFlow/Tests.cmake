e8298efda0726afd2f21829e79987bf9a34cdd24
renchao_lu <renchao.lu@gmail.com>
2021-06-23 12:45:03 +0200

    [T] 2D planar permeable fracture testcase.

--- ProcessLib/StokesFlow/Tests.cmake
+++ ProcessLib/StokesFlow/Tests.cmake
@@ -1,4 +1,5 @@
 if (NOT OGS_USE_MPI)
     OgsTest(PROJECTFILE StokesFlow/ParallelPlate.prj RUNTIME 2)
     OgsTest(PROJECTFILE StokesFlow/ParallelPlateWithBodyForce.prj RUNTIME 2)
+    OgsTest(PROJECTFILE StokesFlow/2DPlanarPermeableFracture/2DPlanarPermeableFracture.prj RUNTIME 2)
 endif()
