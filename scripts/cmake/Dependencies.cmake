2e39341d1110688588249a87ccd317d03685f2db
Lars Bilke <lars.bilke@ufz.de>
2021-07-02 11:01:24 +0200

    [cpm] Build gtest statically.
    Fixes macOS rpath issue.

--- scripts/cmake/Dependencies.cmake
+++ scripts/cmake/Dependencies.cmake
@@ -20,6 +20,7 @@ if(OGS_BUILD_TESTING)
         GITHUB_REPOSITORY google/googletest
         GIT_TAG 389cb68b87193358358ae87cc56d257fd0d80189
         OPTIONS "INSTALL_GTEST OFF" "gtest_force_shared_crt ON"
+                "BUILD_SHARED_LIBS OFF"
         EXCLUDE_FROM_ALL YES
     )
 
