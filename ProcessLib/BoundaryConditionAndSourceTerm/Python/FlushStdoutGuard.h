36dc528c6d1608e2aa3de0eff35c0453d8e12a44
Wenqing Wang <wenqing.wang@ufz.de>
2021-07-28 12:40:49 +0200

    [ProcessLib/Python] Moved a double defined class to a new file.

---
+++ ProcessLib/BoundaryConditionAndSourceTerm/Python/FlushStdoutGuard.h
@@ -0,0 +1,48 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2021, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#pragma once
+
+namespace
+{
+//! Optionally flushes the standard output upon creation and destruction.
+//! Can be used to improve the debug output readability when printing debug
+//! messages both from OGS and from Python.
+class FlushStdoutGuard final
+{
+public:
+    //! Optionally flushes C++ stdout before running Python code.
+    explicit FlushStdoutGuard(bool const flush) : _flush(flush)
+    {
+        if (!flush)
+        {
+            return;
+        }
+
+        std::cout << std::flush;
+    }
+
+    //! Optionally flushes Python's stdout after running Python code.
+    ~FlushStdoutGuard()
+    {
+        if (!_flush)
+        {
+            return;
+        }
+
+        using namespace pybind11::literals;
+        pybind11::print("end"_a = "", "flush"_a = true);
+    }
+
+private:
+    //! To flush or not to flush.
+    const bool _flush;
+};
+}  // anonymous namespace
--- ProcessLib/BoundaryConditionAndSourceTerm/Python/PythonBoundaryCondition.cpp
+++ ProcessLib/BoundaryConditionAndSourceTerm/Python/PythonBoundaryCondition.cpp
@@ -15,46 +15,10 @@
 #include <iostream>
 
 #include "BaseLib/ConfigTree.h"
+#include "FlushStdoutGuard.h"
 #include "MeshLib/MeshSearch/NodeSearch.h"
 #include "ProcessLib/BoundaryConditionAndSourceTerm/Utils/CreateLocalAssemblers.h"
 #include "PythonBoundaryConditionLocalAssembler.h"
-
-namespace
-{
-//! Optionally flushes the standard output upon creation and destruction.
-//! Can be used to improve the debug output readability when printing debug
-//! messages both from OGS and from Python.
-class FlushStdoutGuard
-{
-public:
-    //! Optionally flushes C++ stdout before running Python code.
-    explicit FlushStdoutGuard(bool const flush) : _flush(flush)
-    {
-        if (!flush)
-        {
-            return;
-        }
-
-        std::cout << std::flush;
-    }
-
-    //! Optionally flushes Python's stdout after running Python code.
-    ~FlushStdoutGuard()
-    {
-        if (!_flush)
-        {
-            return;
-        }
-
-        using namespace pybind11::literals;
-        pybind11::print("end"_a = "", "flush"_a = true);
-    }
-
-private:
-    //! To flush or not to flush.
-    const bool _flush;
-};
-}  // anonymous namespace
 
 namespace ProcessLib
 {
