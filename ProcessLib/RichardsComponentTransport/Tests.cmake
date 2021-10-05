cc0251ff848b43d0ac7329e033a5fffcb62c267a
renchao_lu <renchao.lu@gmail.com>
2021-10-05 17:20:49 +0200

    [T] re-enable the Padilla_NaCl1 test case.

--- ProcessLib/RichardsComponentTransport/Tests.cmake
+++ ProcessLib/RichardsComponentTransport/Tests.cmake
@@ -1,15 +1,5 @@
 if(NOT OGS_USE_MPI)
-    # TODO: re-enable this, see #3178. OgsTest(PROJECTFILE
-    # Parabolic/RichardsComponentTransport/Padilla/Padilla_NaCl1/Padilla_NaCl1.prj
-    # RUNTIME 7)
-    OgsTest(
-        PROJECTFILE
-            Parabolic/RichardsComponentTransport/Padilla/Padilla_NaCl1/Padilla_NaCl1_quadratic.prj
-        RUNTIME 7
-    )
-    OgsTest(
-        PROJECTFILE
-            Parabolic/RichardsComponentTransport/Padilla/Padilla_NaCl6/Padilla_NaCl6.prj
-        RUNTIME 13
-    )
+    OgsTest(PROJECTFILE Parabolic/RichardsComponentTransport/Padilla/Padilla_NaCl1/Padilla_NaCl1.prj RUNTIME 7)
+    OgsTest(PROJECTFILE Parabolic/RichardsComponentTransport/Padilla/Padilla_NaCl1/Padilla_NaCl1_quadratic.prj RUNTIME 7)
+    OgsTest(PROJECTFILE Parabolic/RichardsComponentTransport/Padilla/Padilla_NaCl6/Padilla_NaCl6.prj RUNTIME 13)
 endif()
