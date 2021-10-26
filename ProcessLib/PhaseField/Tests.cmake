a7e0a33a2bc261e4e6bb94f79cbb229ab1c7d27f
KeitaYoshioka <keita.yoshioka@ufz.de>
2021-10-26 12:08:30 +0200

    PhaseField Ctest updated for energy split.
    New test for isotropic added.

--- ProcessLib/PhaseField/Tests.cmake
+++ ProcessLib/PhaseField/Tests.cmake
@@ -28,3 +28,17 @@ AddTest(
         expected_AT2_iso_tension_ts_10_t_1_000000_0.vtu AT2_iso_tension_ts_10_t_1_000000_0.vtu phasefield phasefield 1e-6 0
 )
 
+AddTest(
+    NAME PhaseField_3D_beam_tens_AT1_vd
+    PATH PhaseField/beam
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS AT1_vd_tensile.prj
+    WRAPPER mpirun
+    WRAPPER_ARGS -np 1
+    TESTER vtkdiff
+    REQUIREMENTS OGS_USE_MPI
+    RUNTIME 18
+    DIFF_DATA
+        expected_AT1_vd_tension_ts_10_t_1_000000_0.vtu AT1_vd_tension_ts_10_t_1_000000_0.vtu displacement displacement 1e-5 0
+        expected_AT1_vd_tension_ts_10_t_1_000000_0.vtu AT1_vd_tension_ts_10_t_1_000000_0.vtu phasefield phasefield 1e-6 0
+)
