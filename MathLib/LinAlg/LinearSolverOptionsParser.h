7ea11a0f5a516a4dc418429a7026c99327758e55
Thomas Fischer <thomas.fischer@ufz.de>
2021-12-16 12:48:30 +0100

    [MaL/LinAlg] Generalize the linear solver option parsing.

---
+++ MathLib/LinAlg/LinearSolverOptionsParser.h
@@ -0,0 +1,17 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2021, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#pragma once
+
+namespace MathLib
+{
+template <typename LinearSolverType>
+struct LinearSolverOptionsParser;
+}  // namespace MathLib
