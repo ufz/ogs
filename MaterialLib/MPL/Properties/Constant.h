e4e72bbc78ebc34762dc8a8e119c34dd33e79e42
Wenqing Wang <wenqing.wang@ufz.de>
2022-01-10 15:47:59 +0100

    [MPL/Constant] Add dValue with a return value of 0.0

--- MaterialLib/MPL/Properties/Constant.h
+++ MaterialLib/MPL/Properties/Constant.h
@@ -25,5 +25,18 @@ public:
     /// PropertyDataType definition and sets the protected attribute value_ of
     /// the base class Property to that value.
     Constant(std::string name, PropertyDataType const& v);
+
+    /// If the property is no a scalar constant, the returned scalar zero value
+    /// is used to form a tensor or a vector via calling formEigenTensor or
+    /// FormEigenVector, respectively
+    PropertyDataType dValue(VariableArray const& /*variable_array*/,
+                            VariableArray const& /*variable_array_prev*/,
+                            Variable const /*variable*/,
+                            ParameterLib::SpatialPosition const& /*pos*/,
+                            double const /*t*/,
+                            double const /*dt*/) const override
+    {
+        return 0.0;
+    }
 };
 }  // namespace MaterialPropertyLib
