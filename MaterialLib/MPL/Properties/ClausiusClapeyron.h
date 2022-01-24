d8e217efba65f5d061fa8b56aeec440e5b09285b
Norbert Grunwald <norbert.grunwald@ufz.de>
2022-01-24 22:48:43 +0100

    [MPL] Use molar mass of the component and not that of the phase.
    Therefore, the derivative is zero.

--- MaterialLib/MPL/Properties/ClausiusClapeyron.h
+++ MaterialLib/MPL/Properties/ClausiusClapeyron.h
@@ -67,12 +67,6 @@ private:
                      VariableArray const& variable_array,
                      ParameterLib::SpatialPosition const& pos, double const t,
                      double const dt) const;
-
-    double dMolarMass(std::variant<Medium*, Phase*, Component*> const scale,
-                      VariableArray const& variable_array,
-                      Variable const primary_variable,
-                      ParameterLib::SpatialPosition const& pos, double const t,
-                      double const dt) const;
 };
 
 }  // namespace MaterialPropertyLib
