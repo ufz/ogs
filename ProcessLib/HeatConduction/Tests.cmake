1f39484b065b36be8fef8a32eeff106ec160f59f
Wenqing Wang <wenqing.wang@ufz.de>
2022-02-02 16:11:30 +0100

    [Test/T] Use new name of heat flux

--- ProcessLib/HeatConduction/Tests.cmake
+++ ProcessLib/HeatConduction/Tests.cmake
@@ -131,7 +131,7 @@ AddTest(
         TESTER vtkdiff
         DIFF_DATA
         wedge_1e2_axi_ang_0.02_t_2s_extracted_surface.vtu square_1e2_axi_ts_2_t_2.000000.vtu temperature temperature 1.7e-5 1e-5
-        wedge_1e2_axi_ang_0.02_t_2s_extracted_surface.vtu square_1e2_axi_ts_2_t_2.000000.vtu heat_flux_x heat_flux_x 1.7e-5 1e-5
+        wedge_1e2_axi_ang_0.02_t_2s_extracted_surface.vtu square_1e2_axi_ts_2_t_2.000000.vtu heat_flux heat_flux 1.7e-5 1e-5
     REQUIREMENTS NOT OGS_USE_MPI
 )
 # # WEDGE 1x1 HEATCONDUCTION TEST -- computes reference results for the above test
@@ -269,7 +269,7 @@ AddTest(
         TESTER vtkdiff
         DIFF_DATA
         source_term_left_ts_1_t_1.000000.vtu source_term_left_ts_1_t_1.000000.vtu temperature temperature 1e-15 0.0
-        source_term_left_ts_1_t_1.000000.vtu source_term_left_ts_1_t_1.000000.vtu heat_flux_x heat_flux_x 1e-15 0.0
+        source_term_left_ts_1_t_1.000000.vtu source_term_left_ts_1_t_1.000000.vtu heat_flux heat_flux 1e-15 0.0
         REQUIREMENTS NOT OGS_USE_MPI
 )
 
@@ -284,7 +284,7 @@ AddTest(
         TESTER vtkdiff
         DIFF_DATA
         source_term_middle_ts_1_t_1.000000.vtu source_term_middle_ts_1_t_1.000000.vtu temperature temperature 7e-15 2e-14
-        source_term_middle_ts_1_t_1.000000.vtu source_term_middle_ts_1_t_1.000000.vtu heat_flux_x heat_flux_x 7e-14 0.0
+        source_term_middle_ts_1_t_1.000000.vtu source_term_middle_ts_1_t_1.000000.vtu heat_flux heat_flux 7e-14 0.0
         REQUIREMENTS NOT OGS_USE_MPI
 )
 
@@ -297,7 +297,7 @@ AddTest(
         TESTER vtkdiff
         DIFF_DATA
         source_term_middle_restricted_ts_1_t_1.000000.vtu source_term_middle_restricted_ts_1_t_1.000000.vtu temperature temperature 1e-15 0.0
-        source_term_middle_restricted_ts_1_t_1.000000.vtu source_term_middle_restricted_ts_1_t_1.000000.vtu heat_flux_x heat_flux_x 3e-15 4e-7
+        source_term_middle_restricted_ts_1_t_1.000000.vtu source_term_middle_restricted_ts_1_t_1.000000.vtu heat_flux heat_flux 3e-15 4e-7
         REQUIREMENTS NOT OGS_USE_MPI
 )
 
@@ -311,7 +311,7 @@ AddTest(
         TESTER vtkdiff
         DIFF_DATA
         3D_line_source_term_ts_1_t_1.000000.vtu 3D_line_source_term_ts_1_t_1.000000.vtu temperature temperature 2e-15 0.0
-        3D_line_source_term_ts_1_t_1.000000.vtu 3D_line_source_term_ts_1_t_1.000000.vtu heat_flux_x heat_flux_x 7e-15 7e-13
+        3D_line_source_term_ts_1_t_1.000000.vtu 3D_line_source_term_ts_1_t_1.000000.vtu heat_flux heat_flux 7e-15 7e-13
         REQUIREMENTS NOT OGS_USE_MPI
 )
 
@@ -325,7 +325,7 @@ AddTest(
         TESTER vtkdiff
         DIFF_DATA
         3D_line_source_term_restricted_ts_1_t_1.000000.vtu 3D_line_source_term_restricted_ts_1_t_1.000000.vtu temperature temperature 1e-15 0.0
-        3D_line_source_term_restricted_ts_1_t_1.000000.vtu 3D_line_source_term_restricted_ts_1_t_1.000000.vtu heat_flux_x heat_flux_x 1.1e-15 5e-12
+        3D_line_source_term_restricted_ts_1_t_1.000000.vtu 3D_line_source_term_restricted_ts_1_t_1.000000.vtu heat_flux heat_flux 1.1e-15 5e-12
         REQUIREMENTS NOT OGS_USE_MPI
 )
 
