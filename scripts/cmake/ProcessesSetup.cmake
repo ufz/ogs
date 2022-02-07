738086e3a17d22dd0272be17c5d74eddc892cd5f
Lars Bilke <lars.bilke@ufz.de>
2022-02-07 11:23:41 +0100

    [cmake] Refactored (and fixed) processes setup logic.
    Before:
    
    One could use the CMake variables `OGS_BUILD_PROCESSES` or
    `OGS_BUILD_PROCESS_XYZ` but behavior was not consistent when switching
    back and forth and using both methods. Also building multiple
    processes via `OGS_BUILD_PROCESSES` was broken.
    
    Changed:
    
    - Removed `OGS_BUILD_PROCESS_XYZ` options.
    - Use `OGS_BUILD_PROCESSES` with the following values:
      - ON  -> All processes are built.
      - OFF -> Disable all processes.
      - Select a single process by name, e.g. `TH2M`.
      - Give a ;-separated list of processes, e.g. '"THM;TH2M"'
    - `OGS_BUILD_PROCESSES` can be set via ccmake or cmake-gui with a
      convenient dropdown, although setting multiple processes can only be
      done via the cli!

--- scripts/cmake/ProcessesSetup.cmake
+++ scripts/cmake/ProcessesSetup.cmake
@@ -28,44 +28,47 @@ set(_processes_list
     TwoPhaseFlowWithPrho
 )
 
-# Add a cmake option for each process.
-foreach(process ${_processes_list})
-    option(OGS_BUILD_PROCESS_${process} "Build the ${process} process." ON)
-endforeach()
-
-set(OGS_BUILD_PROCESSES ""
+set(OGS_BUILD_PROCESSES ON
     CACHE STRING "Semicolon-separated list of processes to build"
 )
+
+# Drop-down (cmake-gui), cycle-through list (ccmake) for easiere interactive
+# selection. For enabling multiple selected processes cmake usage via cli is
+# required!
+set_property(
+    CACHE OGS_BUILD_PROCESSES PROPERTY STRINGS ON OFF ${_processes_list}
+)
+
 if(NOT OGS_BUILD_CLI)
     set(OGS_BUILD_PROCESSES OFF "" CACHE STRING "" FORCE)
     message(
         STATUS
             "ATTENTION: OGS_BUILD_CLI=OFF -> OGS_BUILD_PROCESSES is set to OFF too.\n"
             "   If cli is switched on again, remember to switch processes back to on \n"
-            "   too with -DOGS_BUILD_PROCESSES=\"\"!"
+            "   too with -DOGS_BUILD_PROCESSES=ON!"
     )
 endif()
-if(NOT "${OGS_BUILD_PROCESSES}" STREQUAL "")
-    if(${OGS_BUILD_PROCESSES})
-        foreach(process ${OGS_BUILD_PROCESSES})
-            if(NOT "${process}" IN_LIST _processes_list)
-                message(
-                    FATAL_ERROR
-                        "${process} given in OGS_BUILD_PROCESSES is "
-                        "not a valid process name! Valid names are ${_processes_list}"
-                )
-            endif()
-        endforeach()
-        message(STATUS "Enabled processes:")
+
+if("${OGS_BUILD_PROCESSES}" STREQUAL "ON" OR "${OGS_BUILD_PROCESSES}" STREQUAL
+                                             "OFF"
+)
+    if(OGS_BUILD_PROCESSES)
+        message(STATUS "All processes enabled.")
+        set(_enabled_processes ${_processes_list})
     else()
         message(STATUS "All processes disabled.")
+        unset(_enabled_processes)
     endif()
-    foreach(process ${_processes_list})
-        if("${process}" IN_LIST OGS_BUILD_PROCESSES)
-            set(OGS_BUILD_PROCESS_${process} ON CACHE BOOL "" FORCE)
-            message(STATUS "  ${process}")
-        else()
-            set(OGS_BUILD_PROCESS_${process} OFF CACHE BOOL "" FORCE)
+else()
+    foreach(process ${OGS_BUILD_PROCESSES})
+        if(NOT "${process}" IN_LIST _processes_list)
+            message(
+                FATAL_ERROR
+                    "${process} given in OGS_BUILD_PROCESSES is "
+                    "not a valid process name! Valid names are ${_processes_list}"
+            )
         endif()
     endforeach()
+    set(_enabled_processes ${OGS_BUILD_PROCESSES})
+    message(STATUS "Enabled processes: ${OGS_BUILD_PROCESSES}")
 endif()
