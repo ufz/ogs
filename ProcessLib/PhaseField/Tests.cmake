1f53f14f5f9e4e0c77a86e8c78a773f83567d16c
hhutyou <hhutyou@126.com>
2022-02-08 11:46:13 +0100

    [T/PhF] Cohesive linear/exp ctests

--- ProcessLib/PhaseField/Tests.cmake
+++ ProcessLib/PhaseField/Tests.cmake
@@ -59,3 +59,33 @@ AddTest(
         expected_AT1_vd_tension_2core_ts_10_t_1_000000_0.vtu AT1_vd_tension_ts_10_t_1_000000_0.vtu phasefield phasefield 1e-6 0
         expected_AT1_vd_tension_2core_ts_10_t_1_000000_1.vtu AT1_vd_tension_ts_10_t_1_000000_1.vtu phasefield phasefield 1e-6 0
 )
+
+AddTest(
+    NAME PhaseField_3D_beam_tens_COHESIVE_linear_es
+    PATH PhaseField/beam
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS bar_COHESIVE_linear.prj
+    WRAPPER mpirun
+    WRAPPER_ARGS -np 1
+    TESTER vtkdiff
+    REQUIREMENTS OGS_USE_MPI
+    RUNTIME 300
+    DIFF_DATA
+        expected_bar_COHESIVE_linear_ts_10_t_1_000000_0.vtu bar_COHESIVE_linear_ts_10_t_1_000000_0.vtu displacement displacement 1e-5 0
+        expected_bar_COHESIVE_linear_ts_10_t_1_000000_0.vtu bar_COHESIVE_linear_ts_10_t_1_000000_0.vtu phasefield phasefield 1e-6 0
+)
+
+AddTest(
+    NAME PhaseField_3D_beam_tens_COHESIVE_exponential_es
+    PATH PhaseField/beam
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS bar_COHESIVE_exponential.prj
+    WRAPPER mpirun
+    WRAPPER_ARGS -np 1
+    TESTER vtkdiff
+    REQUIREMENTS OGS_USE_MPI
+    RUNTIME 300
+    DIFF_DATA
+        expected_bar_COHESIVE_exponential_ts_10_t_1_000000_0.vtu bar_COHESIVE_exponential_ts_10_t_1_000000_0.vtu displacement displacement 1e-5 0
+        expected_bar_COHESIVE_exponential_ts_10_t_1_000000_0.vtu bar_COHESIVE_exponential_ts_10_t_1_000000_0.vtu phasefield phasefield 1e-6 0
+)
