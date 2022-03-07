52470af382355e78ca2f42b974d4fbfdb79b778a
Thomas Fischer <thomas.fischer@ufz.de>
2022-03-07 09:00:56 +0100

    [PL/RM] Substitute HydraulicFlow by MassFlowRate.

--- ProcessLib/RichardsMechanics/RichardsMechanicsProcess.cpp
+++ ProcessLib/RichardsMechanics/RichardsMechanicsProcess.cpp
@@ -44,7 +44,7 @@ RichardsMechanicsProcess<DisplacementDim>::RichardsMechanicsProcess(
         mesh, "NodalForces", MeshLib::MeshItemType::Node, DisplacementDim);
 
     _hydraulic_flow = MeshLib::getOrCreateMeshProperty<double>(
-        mesh, "HydraulicFlow", MeshLib::MeshItemType::Node, 1);
+        mesh, "MassFlowRate", MeshLib::MeshItemType::Node, 1);
 
     // TODO (naumov) remove ip suffix. Probably needs modification of the mesh
     // properties, s.t. there is no "overlapping" with cell/point data.
