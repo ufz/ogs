6837529a39991cbaf9a68992402569b3b5eb09c8
Thomas Fischer <thomas.fischer@ufz.de>
2022-03-16 06:41:57 +0100

    [A/CLI] First version of script testing the python interface.

---
+++ Applications/CLI/ogs_python_module.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# The OpenGeoSys python library will be created in the lib folder of the build
+# directory
+## export PYTHONPATH=$PYTHONPATH:your-build_directory-here/lib
+
+import sys
+import OpenGeoSys
+
+arguments = ["", sys.argv[1], "-o /tmp/"]
+
+print("Python OpenGeoSys.init ...")
+OpenGeoSys.initialize(arguments)
+print("Python OpenGeoSys.executeSimulation ...")
+OpenGeoSys.executeSimulation()
+print("Python OpenGeoSys.finalize() ...")
+OpenGeoSys.finalize()
+print("Python world.")
