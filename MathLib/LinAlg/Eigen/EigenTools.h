c0994afcafc41ef4bf2f1ef084acab94a9d2e8ea
Thomas Fischer <thomas.fischer@ufz.de>
2022-03-24 12:36:16 +0100

    [MaL] applyKnownSolution(): Rm unused penalty term.

--- MathLib/LinAlg/Eigen/EigenTools.h
+++ MathLib/LinAlg/Eigen/EigenTools.h
@@ -26,13 +26,10 @@ class EigenVector;
  * @param b                 RHS vector
  * @param vec_knownX_id    a vector of known solution entry IDs
  * @param vec_knownX_x     a vector of known solutions
- * @param penalty_scaling value for scaling some matrix and right hand side
- * entries to enforce some conditions, value ignored in the current
- * implementation
  */
 void applyKnownSolution(
     EigenMatrix& A, EigenVector& b, EigenVector& /*x*/,
     const std::vector<EigenMatrix::IndexType>& vec_knownX_id,
-    const std::vector<double>& vec_knownX_x, double penalty_scaling = 1e+10);
+    const std::vector<double>& vec_knownX_x);
 
 }  // namespace MathLib
