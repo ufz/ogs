1c1723cff9b6d7dab85d48c9a7f61febccad9ff3
Christoph Lehmann <christoph.lehmann@ufz.de>
2022-03-28 11:11:52 +0200

    [BL] Extended dynamic span

--- BaseLib/DynamicSpan.h
+++ BaseLib/DynamicSpan.h
@@ -16,7 +16,23 @@ namespace BaseLib
 template <typename T>
 struct DynamicSpan
 {
+    template <typename Iterator>
+    DynamicSpan(Iterator data_, std::size_t size)
+        : data{&*data_ /* manually convert iterator to pointer */}, size_{size}
+    {
+    }
+
+    T* begin() const { return data; }
+    T* end() const { return data + size_; }
+    std::size_t size() const { return size_; }
+    T& operator[](std::size_t i) const { return data[i]; }
+
     T* data;
-    std::size_t size;
+
+private:
+    std::size_t size_;
 };
+
+template <typename T>
+DynamicSpan(T*, std::size_t) -> DynamicSpan<T>;
 }  // namespace BaseLib
