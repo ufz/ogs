f5b127e974a1204322df70f50fd0447f460ca746
Christoph Lehmann <christoph.lehmann@ufz.de>
2022-03-28 11:14:05 +0200

    [MeL] Fixed mapping formulae

--- MeshLib/Elements/MapBulkElementPoint.h
+++ MeshLib/Elements/MapBulkElementPoint.h
@@ -11,7 +11,7 @@
 #pragma once
 
 #include "MathLib/WeightedPoint.h"
-#include "MeshLib/Elements/Elements.h"
+#include "MeshLib/Elements/Element.h"
 
 namespace MeshLib
 {
@@ -20,14 +20,25 @@ namespace MeshLib
 /// the mapping of a point given in local coordinates of a boundary face/element
 /// to the corresponding bulk element point.
 
-/// Maps the given \c point_on_face on the face of the given \c bulk_element to
-/// the given point in the \c bulk_element.
+/// Maps the given \c point_on_face on the face a bulk element of type \c
+/// bulk_element_cell_type to the given point in the bulk element.
 ///
 /// The input and output coordinates are natural coordinates of the surface and
 /// bulk element, respectively. I.e., the output point has one coordinate more
 /// than the input point.
+///
 MathLib::Point3d getBulkElementPoint(
-    MeshLib::Element const& bulk_element,
+    MeshLib::CellType const bulk_element_cell_type,
     std::size_t const bulk_face_id,
     MathLib::WeightedPoint const& point_on_face);
+
+/// Overload provided for convenience.
+inline MathLib::Point3d getBulkElementPoint(
+    MeshLib::Element const& bulk_element,
+    std::size_t const bulk_face_id,
+    MathLib::WeightedPoint const& point_on_face)
+{
+    return getBulkElementPoint(
+        bulk_element.getCellType(), bulk_face_id, point_on_face);
+}
 }  // namespace MeshLib
