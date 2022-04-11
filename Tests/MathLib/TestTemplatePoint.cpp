9e8f57494c4bfed796d7c3aa9f637e97c5bdb87c
Thomas Fischer <thomas.fischer@ufz.de>
2022-04-11 11:11:12 +0200

    [T/MaL] Restrict tests to 3d points.
    Everywhere in OGS TemplatePoint is used in the 3d version.

--- Tests/MathLib/TestTemplatePoint.cpp
+++ Tests/MathLib/TestTemplatePoint.cpp
@@ -20,32 +20,21 @@ using namespace MathLib;
 TEST(MathLib, TemplatePointConstructors)
 {
     // *** test default constructors for different DIM values
-    TemplatePoint<double, 3> p0;
+    TemplatePoint<double> p0;
     // test coordinates of default constructed point
     ASSERT_EQ(0.0, p0[0]);
     ASSERT_EQ(0.0, p0[1]);
     ASSERT_EQ(0.0, p0[2]);
 
-    TemplatePoint<double, 2> p1;
-    ASSERT_EQ(0.0, p1[0]);
-    ASSERT_EQ(0.0, p1[1]);
-
-    TemplatePoint<double, 5> p2;
-    ASSERT_EQ(0.0, p2[0]);
-    ASSERT_EQ(0.0, p2[1]);
-    ASSERT_EQ(0.0, p2[2]);
-    ASSERT_EQ(0.0, p2[3]);
-    ASSERT_EQ(0.0, p2[4]);
-
     // *** test copy constructor
-    TemplatePoint<double, 3> p0_copy(p0);
+    TemplatePoint<double> p0_copy(p0);
     // test equality of coordinates
     ASSERT_EQ(p0[0], p0_copy[0]);
     ASSERT_EQ(p0[1], p0_copy[1]);
     ASSERT_EQ(p0[2], p0_copy[2]);
 
     // *** test constructor taking std::array
-    std::array<double, 3> array = {{0, 1, 2}};
+    std::array array = {0.0, 1.0, 2.0};
     TemplatePoint<double> p3(array);
     ASSERT_EQ(0.0, p3[0]);
     ASSERT_EQ(1.0, p3[1]);
@@ -54,7 +43,7 @@ TEST(MathLib, TemplatePointConstructors)
 
 TEST(MathLib, TemplatePointOperators)
 {
-    TemplatePoint<double, 3> p;
+    TemplatePoint<double> p;
     // access operator
     p[0] = 1.0;
     p[1] = 3.0;
