bd09b11f49f8d725d13b681b604dcac60f138916
Thomas Fischer <thomas.fischer@ufz.de>
2022-04-12 14:38:17 +0200

    [T/MaL/Point3d] Test Point3d instead TemplatePoint.

--- Tests/MathLib/SimpleTestsPoint3d.cpp
+++ Tests/MathLib/SimpleTestsPoint3d.cpp
@@ -1,5 +1,5 @@
 /**
- * @file TestTemplatePoint.cpp
+ * @file TestPoint3d.cpp
  * @date Mar 13, 2014
  *
  * \copyright
@@ -13,21 +13,21 @@
 
 #include <array>
 
-#include "MathLib/TemplatePoint.h"
+#include "MathLib/Point3d.h"
 
 using namespace MathLib;
 
-TEST(MathLib, TemplatePointConstructors)
+TEST(MathLib, Point3dConstructors)
 {
     // *** test default constructors for different DIM values
-    TemplatePoint<double> p0;
+    Point3d p0;
     // test coordinates of default constructed point
     ASSERT_EQ(0.0, p0[0]);
     ASSERT_EQ(0.0, p0[1]);
     ASSERT_EQ(0.0, p0[2]);
 
     // *** test copy constructor
-    TemplatePoint<double> p0_copy(p0);
+    Point3d p0_copy(p0);
     // test equality of coordinates
     ASSERT_EQ(p0[0], p0_copy[0]);
     ASSERT_EQ(p0[1], p0_copy[1]);
@@ -35,15 +35,15 @@ TEST(MathLib, TemplatePointConstructors)
 
     // *** test constructor taking std::array
     std::array array = {0.0, 1.0, 2.0};
-    TemplatePoint<double> p3(array);
+    Point3d p3(array);
     ASSERT_EQ(0.0, p3[0]);
     ASSERT_EQ(1.0, p3[1]);
     ASSERT_EQ(2.0, p3[2]);
 }
 
-TEST(MathLib, TemplatePointOperators)
+TEST(MathLib, Point3dOperators)
 {
-    TemplatePoint<double> p;
+    Point3d p;
     // access operator
     p[0] = 1.0;
     p[1] = 3.0;
