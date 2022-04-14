e95efea8c87acffe284a9d0960b4b82caf3fe07d
Lars Bilke <lars.bilke@ufz.de>
2022-04-14 12:44:09 +0200

    [cmake] Added find Python setup for OGS_USE_PIP.

--- scripts/cmake/PythonSetup.cmake
+++ scripts/cmake/PythonSetup.cmake
@@ -1,15 +1,30 @@
 # cmake-lint: disable=C0103
 
-# Prefer unix location over frameworks (Apple-only)
-set(Python_FIND_FRAMEWORK LAST)
-
-# Prefer more recent Python version
-set(Python_FIND_STRATEGY VERSION)
-set(_python_componets Interpreter)
-if(OGS_USE_PYTHON OR OGS_USE_PIP)
+if(OGS_USE_PIP)
+    set(Python_ROOT_DIR ${PROJECT_BINARY_DIR}/.venv)
     set(CMAKE_REQUIRE_FIND_PACKAGE_Python TRUE)
+    if(NOT EXISTS ${PROJECT_BINARY_DIR}/.venv)
+        execute_process(
+            COMMAND
+                ${CMAKE_COMMAND} -DPROJECT_BINARY_DIR=${PROJECT_BINARY_DIR}
+                -Dpython_version=${ogs.minimum_version.python} -P
+                ${PROJECT_SOURCE_DIR}/scripts/cmake/PythonCreateVirtualEnv.cmake
+            WORKING_DIRECTORY ${PROJECT_BINARY_DIR} COMMAND_ECHO STDOUT
+                              ECHO_OUTPUT_VARIABLE ECHO_ERROR_VARIABLE
+        )
+        unset(_OGS_PYTHON_PACKAGES_SHA1 CACHE)
+    endif()
+else()
+    # Prefer unix location over frameworks (Apple-only)
+    set(Python_FIND_FRAMEWORK LAST)
+
+    # Prefer more recent Python version
+    set(Python_FIND_STRATEGY VERSION)
 endif()
+
+set(_python_componets Interpreter)
 if(OGS_USE_PYTHON)
+    set(CMAKE_REQUIRE_FIND_PACKAGE_Python TRUE)
     list(APPEND _python_componets Development)
 endif()
 find_package(
@@ -20,14 +35,6 @@ if(OGS_USE_PIP)
     set(OGS_PYTHON_PACKAGES ""
         CACHE INTERNAL "List of Python packages to be installed via pip."
     )
-
-    if(NOT EXISTS ${PROJECT_BINARY_DIR}/.venv)
-        execute_process(
-            COMMAND ${Python_EXECUTABLE} -m venv .venv
-            WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-        )
-        unset(_OGS_PYTHON_PACKAGES_SHA1 CACHE)
-    endif()
     set(Python_ROOT_DIR ${PROJECT_BINARY_DIR}/.venv)
     if(MSVC)
         set(Python_EXECUTABLE ${Python_ROOT_DIR}/Scripts/python.exe)
