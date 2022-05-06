6a99dd4e1b9f1ff76efa8cebb5331883c5f9fb35
Lars Bilke <lars.bilke@ufz.de>
2022-05-06 09:05:19 +0200

    [ci] Automatically commit changed cpm cache hash in MR.
    Works only for bilke/ogs. Others can be added as well but may not
    required.

--- scripts/cmake/DownloadCpmCache.cmake
+++ scripts/cmake/DownloadCpmCache.cmake
@@ -4,17 +4,21 @@ if(NOT OGS_DOWNLOAD_CPM_CACHE)
 endif()
 
 file(READ ${PROJECT_SOURCE_DIR}/web/data/versions.json jsonFileString)
-string(JSON _cpm_package_id GET "${jsonFileString}" cpm package_id)
-string(JSON _cpm_package_hash GET "${jsonFileString}" cpm sha256)
-if(NOT EXISTS ${PROJECT_BINARY_DIR}/cpm)
-    message(STATUS "Downloading cpm cache with file id ${_cpm_package_id}")
+string(JSON _cpm_package_file_id GET "${jsonFileString}" cpm package_file_id)
+string(JSON _cpm_package_file_hash GET "${jsonFileString}" cpm
+       package_file_sha256
+)
+set(_cpm_cache_dir ${PROJECT_BINARY_DIR}/.cpm-${_cpm_package_file_id})
+if(NOT EXISTS ${_cpm_cache_dir})
+    message(STATUS "Downloading cpm cache with file id ${_cpm_package_file_id}")
     file(
         DOWNLOAD
-        https://gitlab.opengeosys.org/ogs/ogs/-/package_files/${_cpm_package_id}/download
+        https://gitlab.opengeosys.org/ogs/ogs/-/package_files/${_cpm_package_file_id}/download
         cpm.tar.gz
         SHOW_PROGRESS
-        EXPECTED_HASH SHA256=${_cpm_package_hash}
+        EXPECTED_HASH SHA256=${_cpm_package_file_hash}
     )
     file(ARCHIVE_EXTRACT INPUT cpm.tar.gz)
+    file(RENAME ${PROJECT_BINARY_DIR}/cpm ${_cpm_cache_dir})
 endif()
-set(ENV{CPM_SOURCE_CACHE} ${PROJECT_BINARY_DIR}/cpm)
+set(ENV{CPM_SOURCE_CACHE} ${_cpm_cache_dir})
