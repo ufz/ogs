cf20ab588e198b3038f0c92e9c07f3d03aadd1a2
Lars Bilke <lars.bilke@ufz.de>
2022-05-10 09:55:53 +0200

    [cmake] Refactored MetisSetup.cmake.
    - Use target instead of global properties.
    - Use metis mirror at https://gitlab.opengeosys.org/ogs/libs/metis.git
      to properly set #define IDXTYPEWIDTH to 64.

--- scripts/cmake/MetisSetup.cmake
+++ scripts/cmake/MetisSetup.cmake
@@ -1,63 +1,42 @@
 message(
     STATUS
         "The METIS package is copyrighted by the Regents of the University of Minnesota."
-)
-message(
-    STATUS
-        "Please read the license of the METIS package carefully before you use the METIS."
+        "Please read the license of the METIS package carefully before you use METIS."
 )
 
-set(METIS_PATH ${metis_SOURCE_DIR})
-add_definitions(-DUSE_GKREGEX)
-set(GKLIB_PATH "${METIS_PATH}/GKlib" CACHE PATH "path to GKlib")
-
-if(SHARED)
-    set(METIS_LIBRARY_TYPE SHARED)
-else()
-    set(METIS_LIBRARY_TYPE STATIC)
-endif(SHARED)
-
+# cmake-lint: disable=C0103
+set(GKLIB_PATH ${metis_SOURCE_DIR}/GKlib CACHE INTERNAL "")
 include(${GKLIB_PATH}/GKlibSystem.cmake)
-include_directories(${GKLIB_PATH})
-include_directories(${METIS_PATH}/include)
 
-# From ${METIS_PATH}/libmetis/CMakeLists.txt Removed linking to conan Add this
-# directory for internal users.
-include_directories(BEFORE ${METIS_PATH}/libmetis)
-# Find sources.
-file(GLOB metis_sources ${METIS_PATH}/libmetis/*.c)
-# Build libmetis.
-add_library(ogs_metis ${GKlib_sources} ${metis_sources})
-if(OpenMP_FOUND)
-    target_link_libraries(ogs_metis OpenMP::OpenMP_C)
-endif()
-if(BUILD_SHARED_LIBS)
-    install(TARGETS ogs_metis LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR})
-endif()
-
-if(UNIX)
-    target_link_libraries(ogs_metis m)
-elseif(MSVC)
-    include(GenerateExportHeader)
-    generate_export_header(ogs_metis)
-endif()
-
-# Compile mpmetis
-add_compile_definitions(IDXTYPEWIDTH=64)
-add_definitions(-DSVNINFO="")
-include_directories(${METIS_PATH}/libmetis)
-include_directories(${METIS_PATH}/programs)
-set(METIS_SOURCES
-    ${METIS_PATH}/programs/mpmetis.c ${METIS_PATH}/programs/cmdline_mpmetis.c
-    ${METIS_PATH}/programs/io.c ${METIS_PATH}/programs/stat.c
+# Metis library
+file(GLOB _metis_sources ${metis_SOURCE_DIR}/libmetis/*.c)
+ogs_add_library(ogs_metis ${GKlib_sources} ${_metis_sources})
+target_compile_definitions(ogs_metis PUBLIC USE_GKREGEX)
+target_include_directories(
+    ogs_metis PUBLIC ${metis_SOURCE_DIR}/GKlib ${metis_SOURCE_DIR}/include
+                     ${metis_SOURCE_DIR}/libmetis
 )
-add_executable(mpmetis ${METIS_SOURCES})
-target_link_libraries(mpmetis ogs_metis)
-install(TARGETS mpmetis RUNTIME DESTINATION bin)
-
-# Disable warnings
-if(MSVC)
-    set_target_properties(ogs_metis mpmetis PROPERTIES COMPILE_FLAGS /W0)
-else()
-    set_target_properties(ogs_metis mpmetis PROPERTIES COMPILE_FLAGS -w)
+if(OpenMP_FOUND)
+    target_link_libraries(ogs_metis PUBLIC OpenMP::OpenMP_C)
 endif()
+if(UNIX)
+    target_link_libraries(ogs_metis PUBLIC m)
+endif()
+target_compile_options(
+    ogs_metis PRIVATE $<$<CXX_COMPILER_ID:Clang,AppleClang,GNU>:-w>
+                      $<$<CXX_COMPILER_ID:MSVC>:/W0>
+)
+
+# mpmetis binary
+set(_mpmetis_sources
+    ${metis_SOURCE_DIR}/programs/mpmetis.c
+    ${metis_SOURCE_DIR}/programs/cmdline_mpmetis.c
+    ${metis_SOURCE_DIR}/programs/io.c ${metis_SOURCE_DIR}/programs/stat.c
+)
+ogs_add_executable(mpmetis ${_mpmetis_sources})
+target_compile_definitions(mpmetis PRIVATE SVNINFO="")
+target_link_libraries(mpmetis ogs_metis)
+target_compile_options(
+    mpmetis PRIVATE $<$<CXX_COMPILER_ID:Clang,AppleClang,GNU>:-w>
+                    $<$<CXX_COMPILER_ID:MSVC>:/W0>
+)
