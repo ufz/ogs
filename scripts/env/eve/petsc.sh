f6acf7e0b775762b9ac9c2b65a8cfa55d98d1359
Lars Bilke <lars.bilke@ufz.de>
2022-05-11 10:04:57 +0200

    [cpm] HDF5 enable zlib.

--- scripts/env/eve/petsc.sh
+++ scripts/env/eve/petsc.sh
@@ -9,8 +9,8 @@ source $DIR/mpi.sh
 module load petsc-bilke/3.16.5_foss2020b
 module load HDF5/1.10.7
 
-echo -e "Note: If you want to run a simulation on the cluster be aware of the "\
-     "mixed CPU architecture. There are Sandy-Bridge-based nodes (orte-28, "\
+echo -e "Note: If you want to run a simulation on the cluster be aware of the"\
+     "mixed CPU architecture. There are Sandy-Bridge-based nodes (orte-28,"\
      "frontend2) as well as Skylake-based nodes (orte-40, frontend1)"\
      ".\nConsider setting CMake-option OGS_CPU_ARCHITECTURE to:\n\n"\
      "  -DOGS_CPU_ARCHITECTURE=sandybridge\n"\
