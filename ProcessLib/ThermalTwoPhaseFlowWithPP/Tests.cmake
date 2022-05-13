db3a2fe7f352d7acfb25476bf2a87e9046b3c5db
Boyan Meng <meng.boyan@ufz.de>
2022-05-13 19:56:33 +0200

    [PL/TH2PP] Slightly relax heat pipe test tolerance criteria

--- ProcessLib/ThermalTwoPhaseFlowWithPP/Tests.cmake
+++ ProcessLib/ThermalTwoPhaseFlowWithPP/Tests.cmake
@@ -22,7 +22,7 @@ AddTest(
     REQUIREMENTS NOT OGS_USE_MPI
     DIFF_DATA
     ref_t_1100000.000000.vtu thermaltwophaseflow_large_ts_1100_t_1100000.000000.vtu capillary_pressure capillary_pressure 9e-5 2e-10
-    ref_t_1100000.000000.vtu thermaltwophaseflow_large_ts_1100_t_1100000.000000.vtu gas_pressure gas_pressure 1e-8 1e-10
+    ref_t_1100000.000000.vtu thermaltwophaseflow_large_ts_1100_t_1100000.000000.vtu gas_pressure gas_pressure 5e-7 1e-10
     ref_t_1100000.000000.vtu thermaltwophaseflow_large_ts_1100_t_1100000.000000.vtu saturation saturation 1e-8 1e-10
     ref_t_1100000.000000.vtu thermaltwophaseflow_large_ts_1100_t_1100000.000000.vtu temperature temperature 1e-8 1e-10
 )
