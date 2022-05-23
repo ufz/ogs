1662d8825e32878f48b888e2acb73ea4cc4c0560
Lars Bilke <lars.bilke@ufz.de>
2022-05-23 10:36:29 +0200

    [cmake] Fixes for code coverage on mac.

--- scripts/cmake/Coverage.cmake
+++ scripts/cmake/Coverage.cmake
@@ -5,14 +5,30 @@ if(NOT FASTCOV_PATH AND NOT OGS_USE_PIP)
     )
 endif()
 
+# cmake-lint: disable=E1126
+
 # https://github.com/linux-test-project/lcov/pull/125
 if(APPLE)
+    set(GENHTML_PATH ${PROJECT_BINARY_DIR}/bin/genhtml)
     file(
         DOWNLOAD
         https://raw.githubusercontent.com/linux-test-project/lcov/41d8655951d6898511f98be2a2dbcfbe662f0b17/bin/genhtml
-        ${PROJECT_BINARY_DIR}/bin/genhtml
+        ${GENHTML_PATH}
     )
-    set(GENHTML_PATH ${PROJECT_BINARY_DIR}/bin/genhtml)
+    file(
+        DOWNLOAD
+        https://raw.githubusercontent.com/linux-test-project/lcov/41d8655951d6898511f98be2a2dbcfbe662f0b17/bin/get_version.sh
+        ${PROJECT_BINARY_DIR}/bin/get_version.sh
+    )
+    file(
+        CHMOD
+        ${GENHTML_PATH}
+        ${PROJECT_BINARY_DIR}/bin/get_version.sh
+        FILE_PERMISSIONS
+        OWNER_READ
+        OWNER_WRITE
+        OWNER_EXECUTE
+    )
 endif()
 
 include(CodeCoverage)
@@ -29,54 +45,68 @@ if(DEFINED ENV{CI})
     set(COVERAGE_ADDITIONAL_ARGS SKIP_HTML)
 endif()
 
-setup_target_for_coverage_fastcov(
-    NAME
-    testrunner_coverage
-    BASE_DIRECTORY
-    ${PROJECT_BINARY_DIR}
-    EXECUTABLE
-    $<TARGET_FILE:testrunner>
-    -l
-    warn
-    --gtest_filter=-GeoLib.SearchNearestPointsInDenseGrid
-    DEPENDENCIES
-    testrunner
-    FASTCOV_ARGS
-    --branch-coverage
-    --include
-    ${PROJECT_SOURCE_DIR}
-    ${COVERAGE_ADDITIONAL_ARGS}
-    EXCLUDE
-    Applications/CLI/
-    ProcessLib/
-    Tests/
-)
+if(APPLE)
+    # System gcov does not work:
+    # https://github.com/RPGillespie6/fastcov/issues/36. Search for Homebrew
+    # installed gcov (included in gcc package).
+    find_program(
+        GCOV_PATH NAMES gcov-12 gcov-11 gcov-10 HINTS $ENV{HOMEBREW_PREFIX}/bin
+                                                      REQUIRED
+    )
+endif()
 
-setup_target_for_coverage_fastcov(
-    NAME
-    ctest_coverage
-    BASE_DIRECTORY
-    ${PROJECT_BINARY_DIR}
-    EXECUTABLE
-    ctest
-    DEPENDENCIES
-    all
-    FASTCOV_ARGS
-    --branch-coverage
-    --include
-    ${PROJECT_SOURCE_DIR}
-    ${COVERAGE_ADDITIONAL_ARGS}
-    EXCLUDE
-    Applications/CLI/
-    Tests/
-    POST_CMD
-    perl
-    -i
-    -pe
-    s!${PROJECT_SOURCE_DIR}/!!g
-    ctest_coverage.json
-    NO_DEMANGLE
-)
+if(OGS_BUILD_TESTING)
+    setup_target_for_coverage_fastcov(
+        NAME
+        testrunner_coverage
+        BASE_DIRECTORY
+        ${PROJECT_BINARY_DIR}
+        EXECUTABLE
+        $<TARGET_FILE:testrunner>
+        -l
+        warn
+        --gtest_filter=-GeoLib.SearchNearestPointsInDenseGrid
+        DEPENDENCIES
+        testrunner
+        FASTCOV_ARGS
+        --branch-coverage
+        --include
+        ${PROJECT_SOURCE_DIR}
+        ${COVERAGE_ADDITIONAL_ARGS}
+        EXCLUDE
+        Applications/CLI/
+        ProcessLib/
+        Tests/
+    )
+endif()
+
+if(OGS_BUILD_CLI)
+    setup_target_for_coverage_fastcov(
+        NAME
+        ctest_coverage
+        BASE_DIRECTORY
+        ${PROJECT_BINARY_DIR}
+        EXECUTABLE
+        ctest
+        DEPENDENCIES
+        all
+        FASTCOV_ARGS
+        --branch-coverage
+        --include
+        ${PROJECT_SOURCE_DIR}
+        ${COVERAGE_ADDITIONAL_ARGS}
+        EXCLUDE
+        Applications/CLI/
+        Tests/
+        POST_CMD
+        perl
+        -i
+        -pe
+        s!${PROJECT_SOURCE_DIR}/!!g
+        ctest_coverage.json
+        NO_DEMANGLE
+    )
+endif()
 
 if(UNIX)
     add_custom_target(clean_coverage find . -name '*.gcda' -delete)
