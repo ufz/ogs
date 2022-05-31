524b6e662149f31e0de1c59d05349fb8fba8fb0c
Lars Bilke <lars.bilke@ufz.de>
2022-05-31 14:22:24 +0200

    [CMake] Added a dependency check in AddTest().

--- scripts/cmake/test/AddTest.cmake
+++ scripts/cmake/test/AddTest.cmake
@@ -422,6 +422,9 @@ Use six arguments version of AddTest with absolute and relative tolerances"
             ${PROJECT_SOURCE_DIR}/scripts/cmake/test/AddTestWrapper.cmake
     )
     if(DEFINED AddTest_DEPENDS)
+        if(NOT TEST ${AddTest_DEPENDS})
+            message(FATAL_ERROR "AddTest ${AddTest_Name}: dependency ${AddTest_DEPENDS} does not exist!")
+        endif()
         set_tests_properties(${TEST_NAME} PROPERTIES DEPENDS ${AddTest_DEPENDS})
     endif()
     if(DEFINED MPI_PROCESSORS)
