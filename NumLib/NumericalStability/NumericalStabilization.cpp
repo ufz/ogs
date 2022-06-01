c38d9dedf00805b979c796a5eb5932c77d7e433b
Wenqing Wang <wenqing.wang@ufz.de>
2022-06-01 12:19:05 +0200

    [ProcessLib] Added NumericalStabilization classes and their creator

---
+++ NumLib/NumericalStability/NumericalStabilization.cpp
@@ -0,0 +1,88 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2022, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ * Created on June 2, 2022, 2:40 PM
+ */
+#include "NumericalStabilization.h"
+
+#include "BaseLib/ConfigTree.h"
+#include "BaseLib/Error.h"
+#include "MeshLib/Mesh.h"
+#include "MeshLib/Utils/GetElementSizes.h"
+
+namespace NumLib
+{
+NumericalStabilization::~NumericalStabilization() {}
+
+IsotropicDiffusionStabilization::IsotropicDiffusionStabilization(
+    double const cutoff_velocity,
+    double const tuning_parameter,
+    std::vector<double>&& element_sizes_vector)
+    : NumericalStabilization(cutoff_velocity),
+      tuning_parameter_(tuning_parameter),
+      element_sizes_(std::move(element_sizes_vector))
+{
+    if (tuning_parameter_ < 0 || tuning_parameter_ > 1.0)
+    {
+        OGS_FATAL(
+            "The tuning parameter value {:g} for "
+            "IsotropicDiffusion stabilization is out of range [0, 1]",
+            tuning_parameter_);
+    }
+}
+
+double IsotropicDiffusionStabilization::getExtraDiffusionCoefficient(
+    std::size_t const elemend_id,
+    double const advection_coefficient,
+    double const velocity_norm) const
+{
+    if (velocity_norm < cutoff_velocity_)
+    {
+        return 0.0;
+    }
+    return 0.5 * advection_coefficient * tuning_parameter_ * velocity_norm *
+           element_sizes_[elemend_id];
+}
+
+std::unique_ptr<NumericalStabilization> createNumericalStabilization(
+    MeshLib::Mesh const& mesh, BaseLib::ConfigTree const& config)
+{
+    auto const stabilization_config =
+        //! \ogs_file_param{prj__processes__process__numerical_stabilization}
+        config.getConfigSubtreeOptional("numerical_stabilization");
+    if (!stabilization_config)
+    {
+        return nullptr;
+    }
+
+    const auto type =
+        //! \ogs_file_param{prj__processes__process__numerical_stabilization__type}
+        stabilization_config->getConfigParameter<std::string>("type");
+    if (type.compare("IsotropicDiffusion") == 0)
+    {
+        INFO("Numerical stabilization of {:s} is used", type);
+
+        auto const stabilization_cutoff_velocity_opt =
+            //! \ogs_file_param{prj__processes__process__numerical_stabilization__IsotropicDiffusion__cutoff_velocity}
+            stabilization_config->getConfigParameterOptional<double>(
+                "cutoff_velocity");
+
+        const auto tuning_parameter =
+            //! \ogs_file_param{prj__processes__process__numerical_stabilization__IsotropicDiffusion__tuning_parameter}
+            stabilization_config->getConfigParameter<double>(
+                "tuning_parameter");
+
+        return std::make_unique<IsotropicDiffusionStabilization>(
+            *stabilization_cutoff_velocity_opt,
+            tuning_parameter,
+            MeshLib::getElementSizes(mesh));
+    }
+
+    OGS_FATAL("The stabilization type of {:s} is not available so far", type);
+}
+}  // namespace NumLib
---
+++ NumLib/NumericalStability/NumericalStabilization.h
@@ -0,0 +1,99 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2022, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ * Created on June 1, 2022, 12:14 PM
+ */
+
+#pragma once
+
+#include <memory>
+#include <vector>
+
+namespace MeshLib
+{
+class Mesh;
+}
+
+namespace BaseLib
+{
+class ConfigTree;
+}
+
+namespace NumLib
+{
+/** It defines stabilization method for solving the advection diffusion
+ * transport equation by using the Galerkin finite element method.
+ *
+ * The convection diffusion transport equation takes the form
+ *  \f[
+ *     \frac{\partial u}{\partial t} - \nabla(\mathbf{K}\nabla u)
+ *      + {\mathbf v}\cdot \nabla u  = Q
+ *  \f]
+ *  with \f$u\f$ the primary variable,  \f$\mathbf v\f$ the fluid velocity,
+ *  \f$\mathbf{K}\f$ the diffusion coefficient.
+ */
+class NumericalStabilization
+{
+public:
+    explicit NumericalStabilization(double const cutoff_velocity)
+        : cutoff_velocity_(cutoff_velocity)
+    {
+    }
+
+    virtual ~NumericalStabilization() = 0;
+
+    virtual double getExtraDiffusionCoefficient(
+        std::size_t const /*elemend_id*/,
+        double const /*advection_coefficient*/,
+        double const /*velocity_norm*/) const
+    {
+        return 0.0;
+    };
+
+protected:
+    /// The cutoff velocity. The stabilization is not applied
+    /// if the velocity magnitude is below the cutoff velocity.
+    double const cutoff_velocity_ = 0.0;
+};
+
+/**
+ * It defines an isotropic diffusion stabilization, the simplest stabilization
+ * method.
+ *
+ *  The method adds an artificial isotropic balancing dissipation to the
+ * diffusion coefficient in order to force the Péclet number to be smaller
+ * than 1. The isotropic balancing dissipation is defined as
+ * \f[
+ *       \mathbf{K}_{\delta} = \frac{1}{2}\alpha ||\mathbf v||h \mathbf I
+ *  \f]
+ *  with \f$\alpha \in [0,1] \f$ the tuning parameter, \f$h\f$ the element
+ *  size (e.g. the maximum edge length of element), and \f$\mathbf I\f$ the
+ * identity matrix.
+ */
+class IsotropicDiffusionStabilization final : public NumericalStabilization
+{
+public:
+    IsotropicDiffusionStabilization(double const cutoff_velocity,
+                                    double const tuning_parameter,
+                                    std::vector<double>&& element_sizes_vector);
+
+    double getExtraDiffusionCoefficient(
+        std::size_t const elemend_id,
+        double const advection_coefficient,
+        double const velocity_norm) const override;
+
+private:
+    /// The tuning parameter in the range of [0,1].
+    double const tuning_parameter_ = 0.5;
+    /// Sizes of the elements.
+    std::vector<double> const element_sizes_;
+};
+
+std::unique_ptr<NumericalStabilization> createNumericalStabilization(
+    MeshLib::Mesh const& mesh, BaseLib::ConfigTree const& config);
+}  // namespace NumLib
