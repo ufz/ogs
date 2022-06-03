45dc3431365a329ec18a0ef357b5ad9192c98000
Lars Bilke <lars.bilke@ufz.de>
2022-06-03 12:44:55 +0200

    [pre-commit] Added missing shebangs.

--- scripts/ArchLinux-PKGBUILDs/paraview-opt/paraview.sh
+++ scripts/ArchLinux-PKGBUILDs/paraview-opt/paraview.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env sh
 export PATH=$PATH:/opt/paraview/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/paraview/lib
 export VTK_DIR=/opt/paraview/lib/cmake/paraview-5.8/vtk/vtk-config.cmake
