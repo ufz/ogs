b0de8dba4c5bc8d1f6d47e22dd4b3d6e98a27efe
Christoph Lehmann <christoph.lehmann@ufz.de>
2022-07-05 13:24:40 +0200

    [PL] Added constitutive model/data: SolidThermalExpansion

---
+++ ProcessLib/ThermoRichardsMechanics/Constitutive/SolidThermalExpansion.h
@@ -0,0 +1,32 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2022, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#pragma once
+
+#include "Base.h"
+
+namespace ProcessLib::ThermoRichardsMechanics
+{
+template <int DisplacementDim>
+struct SolidThermalExpansionData
+{
+    KelvinVector<DisplacementDim> solid_linear_thermal_expansivity_vector;
+};
+
+template <int DisplacementDim>
+struct SolidThermalExpansionModel
+{
+    void eval(SpaceTimeData const& x_t, MediaData const& media_data,
+              SolidThermalExpansionData<DisplacementDim>& out) const;
+};
+
+extern template struct SolidThermalExpansionModel<2>;
+extern template struct SolidThermalExpansionModel<3>;
+}  // namespace ProcessLib::ThermoRichardsMechanics
