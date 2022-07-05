256cbba13107536d0ddebb3589342aca6e47a407
Christoph Lehmann <christoph.lehmann@ufz.de>
2022-07-05 13:24:34 +0200

    [PL] Added constitutive model/data: LiquidViscosity

---
+++ ProcessLib/ThermoRichardsMechanics/Constitutive/LiquidViscosity.cpp
@@ -0,0 +1,34 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2022, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#include "LiquidViscosity.h"
+
+#include "MaterialLib/MPL/Utils/FormEigenTensor.h"
+
+namespace ProcessLib::ThermoRichardsMechanics
+{
+template <int DisplacementDim>
+void LiquidViscosityModel<DisplacementDim>::eval(
+    SpaceTimeData const& x_t, MediaData const& media_data,
+    TemperatureData<DisplacementDim> const& T_data,
+    LiquidViscosityData& out) const
+{
+    namespace MPL = MaterialPropertyLib;
+    MPL::VariableArray variables;
+    variables[static_cast<int>(MPL::Variable::temperature)] = T_data.T;
+
+    out.viscosity =
+        media_data.liquid.property(MPL::PropertyType::viscosity)
+            .template value<double>(variables, x_t.x, x_t.t, x_t.dt);
+}
+
+template struct LiquidViscosityModel<2>;
+template struct LiquidViscosityModel<3>;
+}  // namespace ProcessLib::ThermoRichardsMechanics
