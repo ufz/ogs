12e8fde297dbe2f1ef3cae4f3ed3db231e5cf9c1
Christoph Lehmann <christoph.lehmann@ufz.de>
2022-08-02 11:58:39 +0200

    [BL] Use runtime-checked formatting

--- BaseLib/FileTools.cpp
+++ BaseLib/FileTools.cpp
@@ -108,7 +108,8 @@ bool substituteKeyword(std::string& result, std::string& parenthesized_string,
     std::string const generated_fmt_string =
         "{" + precision_specification +
         type_specification[std::type_index(typeid(data))] + "}";
-    result = result.substr(0, begin) + fmt::format(generated_fmt_string, data) +
+    result = result.substr(0, begin) +
+             fmt::vformat(generated_fmt_string, fmt::make_format_args(data)) +
              result.substr(end + 1, result.length() - (end + 1));
     return true;
 }
