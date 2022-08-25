e9993e2c1c6bd5839fa0f8f26ad2debe27b6a819
Norbert Grunwald <norbert.grunwald@ufz.de>
2022-08-25 16:27:37 +0200

    [T/TH2M] Add cTest Ogata-Banks Problem (TH) for TH2M.

---
+++ Tests/Data/TH2M/TH/Ogata-Banks/neumann.py
@@ -0,0 +1,17 @@
+import OpenGeoSys
+
+rhoLR = 1000
+cpL = 2000
+v_x = 1.5e-6
+
+
+class BC_Heat(OpenGeoSys.BoundaryCondition):
+    def getFlux(self, t, coords, primary_vars):
+        temperature = primary_vars[2]
+        print(temperature)
+        value = -rhoLR * cpL * temperature * v_x
+        return (True, value, [0.0, 0.0, 0.0, 0.0, 0.0])
+
+
+# instantiate the BC objects used by OpenGeoSys
+neumann_correction = BC_Heat()
