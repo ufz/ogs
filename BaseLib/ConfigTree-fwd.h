9221e7f34ab0691b09bc576d6abf1e5eb3c12004
Christoph Lehmann <christoph.lehmann@ufz.de>
2022-09-22 13:19:23 +0200

    [BL] Add forward declaring header

---
+++ BaseLib/ConfigTree-fwd.h
@@ -0,0 +1,16 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2022, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#pragma once
+
+namespace BaseLib
+{
+class ConfigTree;
+}  // namespace BaseLib
