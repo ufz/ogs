472d99883d4a83fe1c4cad686ef7b72048847711
Christoph Lehmann <christoph.lehmann@ufz.de>
2022-10-07 13:44:08 +0200

    [App] Do not rely on ogs being a binary

--- Applications/Python/ogs/_internal/wrap_cli_tools.py
+++ Applications/Python/ogs/_internal/wrap_cli_tools.py
@@ -8,7 +8,46 @@ OGS_BIN_DIR = os.path.join(os.path.join(os.path.dirname(__file__), "..", "bin"))
 class CLI:
     def __init__(self):
         for b in binaries_list:
+            if b == "ogs":
+                continue  # command provided separately
             setattr(self, b, CLI._get_run_cmd(b))
+
+    def ogs(self, *args, **kwargs):
+        """
+        This function wraps the commandline tool ogs for easy use from Python.
+
+        It returns the return code of the commandline tool.
+
+        The entries of args are passed as is to the commandline tool.
+        The entries of kwargs are transformed: one-letter keys get a single
+        dash as a prefix, multi-letter keys are prefixed with two dashes,
+        underscores are replaced with dashes.
+
+        Thereby, commandline tools can be used in a "natural" way from Python, e.g.:
+
+        >>> cli = CLI()
+        >>> cli.ogs("--help") # prints a help text
+        ...
+        >>> cli.ogs(help=None) # special, does the same
+        ...
+
+        A more useful example. The following will create a line mesh:
+
+        >>> outfile = "line.vtu"
+        >>> cli.generateStructuredMesh(e="line", lx=1, nx=10, o=outfile)
+        """
+
+        import ogs.simulator as sim
+
+        cmdline = CLI._get_cmdline("ogs", *args, **kwargs)
+        return_code = sim.initialize(cmdline)
+        if return_code != 0:
+            sim.finalize()
+            return return_code
+
+        return_code = sim.executeSimulation()
+        sim.finalize()
+        return return_code
 
     @staticmethod
     def _format_kv(kwargs):
@@ -24,13 +63,16 @@ class CLI:
                 yield f"{v}"
 
     @staticmethod
+    def _get_cmdline(cmd, *args, **kwargs):
+        str_kwargs = [arg for arg in CLI._format_kv(kwargs)]
+        return [cmd] + str_kwargs + list(args)
+
+    @staticmethod
     def _get_run_cmd(attr):
         def run_cmd(*args, **kwargs):
             # TODO provide override via os.environ?
             cmd = os.path.join(OGS_BIN_DIR, attr)
-
-            str_kwargs = [arg for arg in CLI._format_kv(kwargs)]
-            cmdline = [cmd] + str_kwargs + list(args)
+            cmdline = CLI._get_cmdline(cmd, *args, **kwargs)
             return subprocess.call(cmdline)
 
         run_cmd.__doc__ = f"""
@@ -55,7 +97,6 @@ class CLI:
 
             >>> outfile = "line.vtu"
             >>> cli.generateStructuredMesh(e="line", lx=1, nx=10, o=outfile)
-
             """
 
         return run_cmd
