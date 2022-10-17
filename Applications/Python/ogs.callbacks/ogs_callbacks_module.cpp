44bea8ef1c5fa3840868b15dbba68c4ef71b272f
Christoph Lehmann <christoph.lehmann@ufz.de>
2022-10-17 13:02:24 +0200

    [App] Added ogs.callbacks Python module
    ... currently contains Python BCs and STs

---
+++ Applications/Python/ogs.callbacks/ogs_callbacks_module.cpp
@@ -0,0 +1,22 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2022, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#include <pybind11/pybind11.h>
+
+#include "ProcessLib/BoundaryConditionAndSourceTerm/Python/BHEInflowPythonBoundaryConditionModule.h"
+#include "ProcessLib/BoundaryConditionAndSourceTerm/Python/PythonBoundaryConditionModule.h"
+#include "ProcessLib/BoundaryConditionAndSourceTerm/Python/PythonSourceTermModule.h"
+
+PYBIND11_MODULE(callbacks, m)
+{
+    m.attr("__name__") = "ogs.callbacks";
+    ProcessLib::pythonBindBoundaryCondition(m);
+    ProcessLib::bheInflowpythonBindBoundaryCondition(m);
+    ProcessLib::SourceTerms::Python::pythonBindSourceTerm(m);
+}
