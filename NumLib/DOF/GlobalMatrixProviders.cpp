6e75ea39f48c7bf53041c770b0a198a400e8ee0f
Christoph Lehmann <christoph.lehmann@ufz.de>
2022-10-18 08:16:30 +0200

    [NL] Clear matrix provider
    ... prevents segfaults when calling OGS multiple times from the same
    Python session

--- NumLib/DOF/GlobalMatrixProviders.cpp
+++ NumLib/DOF/GlobalMatrixProviders.cpp
@@ -14,25 +14,20 @@
 
 #include "SimpleMatrixVectorProvider.h"
 
-// Initializes the static members of the structs in the header file
-// associated with this file.
-#define INITIALIZE_GLOBAL_MATRIX_VECTOR_PROVIDER(VARNAME)               \
-    static std::unique_ptr<NumLib::SimpleMatrixVectorProvider> VARNAME{ \
-        new NumLib::SimpleMatrixVectorProvider};                        \
-                                                                        \
-    namespace NumLib                                                    \
-    {                                                                   \
-    VectorProvider& GlobalVectorProvider::provider = *(VARNAME);        \
-                                                                        \
-    MatrixProvider& GlobalMatrixProvider::provider = *(VARNAME);        \
-    }
-
-INITIALIZE_GLOBAL_MATRIX_VECTOR_PROVIDER(globalSetupGlobalMatrixVectorProvider)
+static std::unique_ptr<NumLib::SimpleMatrixVectorProvider>
+    globalSetupGlobalMatrixVectorProvider =
+        std::make_unique<NumLib::SimpleMatrixVectorProvider>();
 
 namespace NumLib
 {
+VectorProvider& GlobalVectorProvider::provider =
+    *(globalSetupGlobalMatrixVectorProvider);
+
+MatrixProvider& GlobalMatrixProvider::provider =
+    *(globalSetupGlobalMatrixVectorProvider);
+
 void cleanupGlobalMatrixProviders()
 {
-    globalSetupGlobalMatrixVectorProvider.reset();
+    globalSetupGlobalMatrixVectorProvider->clear();
 }
 }  // namespace NumLib
