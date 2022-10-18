cb14e24ebbc2b2deead4e31ec93f48792b7cc9e1
MostafaMollaali <m.molaali@gmail.com>
2022-10-18 17:55:24 +0200

    [T/PhF] Fix output name in surfing ctest

--- ProcessLib/PhaseField/Tests.cmake
+++ ProcessLib/PhaseField/Tests.cmake
@@ -101,6 +101,6 @@ AddTest(
     REQUIREMENTS OGS_USE_MPI AND OGS_USE_PYTHON
     RUNTIME 18
     DIFF_DATA
-        expected_surfing_ts_20_t_1_000000_0.vtu surfing_ts_20_t_1_000000_0.vtu displacement displacement 1e-5 0
-        expected_surfing_ts_20_t_1_000000_0.vtu surfing_ts_20_t_1_000000_0.vtu phasefield phasefield 1e-6 0
+        expected_surfing_ts_20_t_1_000000_0.vtu surfing_ts_20_t_1.000000.vtu displacement displacement 1e-5 0
+        expected_surfing_ts_20_t_1_000000_0.vtu surfing_ts_20_t_1.000000.vtu phasefield phasefield 1e-6 0
 )
