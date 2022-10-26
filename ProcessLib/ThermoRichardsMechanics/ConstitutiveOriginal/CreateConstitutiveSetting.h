1022c0393ad568ea709ebba4309468a5fb34d965
Christoph Lehmann <christoph.lehmann@ufz.de>
2022-10-26 09:38:17 +0200

    [PL/TRM] Added creation functionality and traits for the new original setting

---
+++ ProcessLib/ThermoRichardsMechanics/ConstitutiveOriginal/CreateConstitutiveSetting.h
@@ -0,0 +1,32 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2022, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#pragma once
+
+#include <map>
+
+#include "ParameterLib/Parameter.h"
+#include "TraitsBase.h"
+
+namespace ProcessLib::ThermoRichardsMechanics::ConstitutiveOriginal
+{
+template <int DisplacementDim>
+struct CreateConstitutiveSetting
+{
+    static std::map<int,
+                    std::unique_ptr<SolidConstitutiveRelation<DisplacementDim>>>
+    createSolidConstitutiveRelations(
+        std::vector<std::unique_ptr<ParameterLib::ParameterBase>> const&
+            parameters,
+        std::optional<ParameterLib::CoordinateSystem> const&
+            local_coordinate_system,
+        BaseLib::ConfigTree const& config);
+};
+}  // namespace ProcessLib::ThermoRichardsMechanics::ConstitutiveOriginal
