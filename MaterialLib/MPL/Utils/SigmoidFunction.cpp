dc71760aa936b32ab89c40cddef9118ea1518648
Dmitri Naumov <dmitri.naumov@ufz.de>
2022-11-25 13:32:51 +0100

    [MPL] Fix numerical issues in exp function eval.
    There is an upper limit of the exp function argument which is now being
    tested to avoid +inf results and floating-point exceptions.

--- MaterialLib/MPL/Utils/SigmoidFunction.cpp
+++ MaterialLib/MPL/Utils/SigmoidFunction.cpp
@@ -21,21 +21,42 @@ SigmoidFunction::SigmoidFunction(double const k, double const T_c)
 
 double SigmoidFunction::value(double const& T) const
 {
-    return 1. / (1. + std::exp(k_ * (T - T_c_)));
+    double const x = k_ * (T - T_c_);
+
+    // Cutting off at the last x producing a (non-normal) return value because
+    // std::exp(x) produces +infinity beyond this point, approximately 709.78.
+    // The reason for using hex representation is that the value is unambiguous.
+    if (x > 0x1.62e42fefa39efp+9)
+    {
+        return 0.;
+    }
+
+    return 1. / (1. + std::exp(x));
 }
 
 double SigmoidFunction::dValue(double const& T) const
 {
-    double f = value(T);
+    double const f = value(T);
+
+    // Skip any further computations because of f^2 below.
+    if (f * f == 0)
+    {
+        return 0;
+    }
 
     return -k_ * std::exp(k_ * (T - T_c_)) * (f * f);
 }
 
 double SigmoidFunction::d2Value(double const& T) const
 {
-    double f = value(T);
-    double fT = dValue(T);
+    double const fT = dValue(T);
 
+    if (fT == 0)
+    {
+        return 0;
+    }
+
+    double const f = value(T);
     return fT * (k_ + 2 * fT / f);
 }
 }  // namespace MaterialPropertyLib
