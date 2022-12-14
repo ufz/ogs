386d8f314bc31e1921c3b3d266f137cd52af2de7
Tymofiy Gerasimov <tymofiy.gerasimov@ufz.de>
2022-12-14 16:21:29 +0100

    [T/HC] Two-phase Stefan problem; ice melting

--- ProcessLib/HeatConduction/Tests.cmake
+++ ProcessLib/HeatConduction/Tests.cmake
@@ -5,6 +5,7 @@ if (NOT OGS_USE_MPI)
     OgsTest(PROJECTFILE Parabolic/T/2D_Robin/square_1e4_robin.prj RUNTIME 1)
     OgsTest(PROJECTFILE Parabolic/T/2D_Robin/square_1e4_robin_newton.xml RUNTIME 1)
     OgsTest(PROJECTFILE Parabolic/T/2D_Ice_melting-forming_manuf_solution/ManSol3_IceWaterMix_Scaled.prj RUNTIME 5)
+    OgsTest(PROJECTFILE Parabolic/T/1D_Two-phase_Stefan_problem_for_ice_melting/Two-phase_Stefan_problem.prj RUNTIME 5)
 endif()
 
 AddTest(
