349e2146b5c157ff3d608ea328b5281e5b9a8274
Christoph Lehmann <christoph.lehmann@ufz.de>
2023-01-10 09:31:00 +0100

    [PL] Added a parser for submesh residuum output config

---
+++ ProcessLib/Output/SubmeshResiduumOutputConfig.h
@@ -0,0 +1,39 @@
+/**
+ * \file
+ *
+ * \copyright
+ * Copyright (c) 2012-2023, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ */
+
+#pragma once
+
+#include "BaseLib/ConfigTree-fwd.h"
+#include "Output.h"
+
+namespace MeshLib
+{
+class Mesh;
+}  // namespace MeshLib
+
+namespace ProcessLib
+{
+struct SubmeshResiduumOutputConfig
+{
+    //! Controls the output on the #meshes.
+    Output output;
+
+    //! The submeshes for whom additional residuum output should
+    //! be produced.
+    //!
+    //! \attention These meshes must be a non-overlapping cover of the entire
+    //! simulation domain (bulk mesh)!
+    std::vector<std::reference_wrapper<MeshLib::Mesh>> meshes;
+};
+
+SubmeshResiduumOutputConfig createSubmeshResiduumOutputConfig(
+    BaseLib::ConfigTree const& config, std::string const& output_directory,
+    std::vector<std::unique_ptr<MeshLib::Mesh>> const& meshes);
+}  // namespace ProcessLib
