c8fbc45dc6b5a3443e3f7ee64a0a175c33da8974
Christoph Lehmann <christoph.lehmann@ufz.de>
2023-01-26 21:38:41 +0100

    [PL] Moved output config to a separate file

--- ProcessLib/Output/CreateOutput.h
+++ ProcessLib/Output/CreateOutput.h
@@ -10,12 +10,8 @@
 
 #pragma once
 
-#include <memory>
-#include <string>
-#include <vector>
-
 #include "ProcessLib/Output/Output.h"
-#include "ProcessLib/Output/OutputDataSpecification.h"
+#include "ProcessLib/Output/OutputConfig.h"
 
 namespace BaseLib
 {
@@ -29,30 +25,6 @@ class Mesh;
 
 namespace ProcessLib
 {
-enum class OutputType : uint8_t
-{
-    vtk,
-    xdmf
-};
-
-struct OutputConfig
-{
-    OutputType output_type;
-    std::string prefix;
-    std::string suffix;
-    bool compress_output;
-    unsigned int number_of_files;
-    std::string data_mode;
-    std::vector<PairRepeatEachSteps> repeats_each_steps;
-    std::set<std::string> output_variables;
-    bool output_extrapolation_residuals;
-    std::vector<std::string> mesh_names_for_output;
-    std::vector<double> fixed_output_times;
-    bool output_iteration_results;
-};
-
-OutputConfig createOutputConfig(const BaseLib::ConfigTree& config);
-
 Output createOutput(OutputConfig&& oc, std::string const& output_directory,
                     std::vector<std::unique_ptr<MeshLib::Mesh>> const& meshes);
 
