2ea25df34afc7012928f7d30df9512d792304f13
Dmitri Naumov <dmitri.naumov@ufz.de>
2023-01-26 14:09:54 +0100

    [PL/THM] Remove N_u_op from ip data
    Slightly better performance, saves up to half of the space
    needed for each integration point.

--- ProcessLib/ThermoHydroMechanics/ThermoHydroMechanicsFEM-impl.h
+++ ProcessLib/ThermoHydroMechanics/ThermoHydroMechanicsFEM-impl.h
@@ -74,15 +74,6 @@ ThermoHydroMechanicsLocalAssembler<ShapeFunctionDisplacement,
             _integration_method.getWeightedPoint(ip).getWeight() *
             sm_u.integralMeasure * sm_u.detJ;
 
-        ip_data.N_u_op = ShapeMatricesTypeDisplacement::template MatrixType<
-            DisplacementDim, displacement_size>::Zero(DisplacementDim,
-                                                      displacement_size);
-        for (int i = 0; i < DisplacementDim; ++i)
-            ip_data.N_u_op
-                .template block<1, displacement_size / DisplacementDim>(
-                    i, i * displacement_size / DisplacementDim)
-                .noalias() = sm_u.N;
-
         ip_data.N_u = sm_u.N;
         ip_data.dNdx_u = sm_u.dNdx;
 
@@ -253,8 +244,6 @@ void ThermoHydroMechanicsLocalAssembler<
     {
         auto const& w = _ip_data[ip].integration_weight;
 
-        auto const& N_u_op = _ip_data[ip].N_u_op;
-
         auto const& N_u = _ip_data[ip].N_u;
         auto const& dNdx_u = _ip_data[ip].dNdx_u;
 
@@ -409,6 +398,18 @@ void ThermoHydroMechanicsLocalAssembler<
 
         auto const rho =
             solid_density * (1 - porosity) + porosity * fluid_density;
+
+        typename ShapeMatricesTypeDisplacement::template MatrixType<
+            DisplacementDim, displacement_size>
+            N_u_op = ShapeMatricesTypeDisplacement::template MatrixType<
+                DisplacementDim, displacement_size>::Zero(DisplacementDim,
+                                                          displacement_size);
+        for (int i = 0; i < DisplacementDim; ++i)
+            N_u_op
+                .template block<1, displacement_size / DisplacementDim>(
+                    i, i * displacement_size / DisplacementDim)
+                .noalias() = N_u;
+
         local_rhs.template segment<displacement_size>(displacement_index)
             .noalias() -=
             (B.transpose() * sigma_eff - N_u_op.transpose() * rho * b) * w;
