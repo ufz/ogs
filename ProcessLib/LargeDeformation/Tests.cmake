2109ffef76ed92d5b577ba57f604e66f7bffde4a
tnagel <nagelt@tcd.ie>
2023-03-01 21:06:51 +0100

    [T/LD] Rigid body rotation Jupyter notebook

--- ProcessLib/LargeDeformation/Tests.cmake
+++ ProcessLib/LargeDeformation/Tests.cmake
@@ -1,3 +1,4 @@
 if (NOT OGS_USE_MPI)
     OgsTest(PROJECTFILE LargeDeformation/RigidBody/square_1e0.prj RUNTIME 1)
+    NotebookTest(NOTEBOOKFILE LargeDeformation/RigidBody/RigidBody.md RUNTIME 15)
 endif()
