149afe2a403015daa36bd5271bd066871c08aaee
Lars Bilke <lars.bilke@ufz.de>
2023-03-07 14:22:57 +0100

    [juwels] Updated modules.

--- scripts/env/juwels/petsc.sh
+++ scripts/env/juwels/petsc.sh
@@ -1,10 +1,17 @@
-module load CMake/3.18.0
-module load GCC/10.3.0
-module load ParaStationMPI/5.4.10-1
-module load PETSc/3.14
+module load Stages/2023
+module load GCC/11.3.0
+module load OpenMPI/4.1.4
+module load CMake/3.23.1
+module load Ninja/1.10.2
+module load Python
+module load Eigen
+module load Boost
+module load git
+module load HDF5/1.12.2
+module load ScaLAPACK/2.2.0-fb
 
 export CC=mpicc
 export CXX=mpic++
 
-echo "Example config: cmake ../ogs -DCMAKE_BUILD_TYPE=Release -DOGS_USE_PETSC=ON"
-echo "Example run: srun bin/ogs -n 3 -t 2 -A ogs6hpc3 bin/ogs ../ogs/Tests/Data/EllipticPETSc/cube_1e3_neumann.prj"
+echo "Example config: cmake --preset release-petsc"
+echo "Example run: srun -n 3 -t 2 -A ogs6hpc4 bin/ogs ../ogs/Tests/Data/EllipticPETSc/cube_1e3_neumann.prj"
