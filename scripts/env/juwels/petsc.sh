e833c1d3cf7341b70dcdb73ff98235ace66eb74f
meisel <tobias.meisel@ufz.de>
2023-03-08 16:40:37 +0100

    [juwels] Limit CMake processes

--- scripts/env/juwels/petsc.sh
+++ scripts/env/juwels/petsc.sh
@@ -13,5 +13,9 @@ module load ScaLAPACK/2.2.0-fb
 export CC=mpicc
 export CXX=mpic++
 
+# CMake may spawn as many processes as available. This may overload the filesystem on JUWELS.
+# It is therefore necessary to limit the number of processes with CMAKE_BUILD_PARALLEL_LEVEL.
+export CMAKE_BUILD_PARALLEL_LEVEL=6
+
 echo "Example config: cmake --preset release-petsc"
 echo "Example run: srun -n 3 -t 2 -A ogs6hpc4 bin/ogs ../ogs/Tests/Data/EllipticPETSc/cube_1e3_neumann.prj"
