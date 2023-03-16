1401fbc1e8d2002a707403efabc4faa8abf1cbcc
Dmitri Naumov <dmitri.naumov@ufz.de>
2023-03-16 13:33:09 +0100

    [PL/THM] Extract ConstitutiveRelationsValues
    for storing temporary values.

--- ProcessLib/ThermoHydroMechanics/IntegrationPointData.h
+++ ProcessLib/ThermoHydroMechanics/IntegrationPointData.h
@@ -151,5 +151,29 @@ struct IntegrationPointDataForOutput
         DisplacementDim, std::numeric_limits<double>::quiet_NaN());
 };
 
+template <int DisplacementDim>
+struct ConstitutiveRelationsValues
+{
+    using DimMatrix =
+        typename MatrixPolicyType::MatrixType<DisplacementDim, DisplacementDim>;
+
+    MathLib::KelvinVector::KelvinMatrixType<DisplacementDim> C;
+    DimMatrix K_over_mu;
+    DimMatrix K_pT_thermal_osmosis;
+    DimMatrix effective_thermal_conductivity;
+    double alpha_biot;
+    double beta;
+    double beta_SR;
+    double c_f;
+    double effective_volumetric_heat_capacity;
+    double fluid_compressibility;
+    double fluid_density;
+    double porosity;
+    double rho;
+
+    // Freezing related values.
+    double J_TT_fr;
+};
+
 }  // namespace ThermoHydroMechanics
 }  // namespace ProcessLib
