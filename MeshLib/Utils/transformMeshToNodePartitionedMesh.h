50b6e3f5d0b62bf3ef21b15ce73b38ac24f476a5
Thomas Fischer <thomas.fischer@ufz.de>
2023-03-20 16:19:00 +0100

    [MeL/U] Docu for transformMeshToNodePartitionedMesh

--- MeshLib/Utils/transformMeshToNodePartitionedMesh.h
+++ MeshLib/Utils/transformMeshToNodePartitionedMesh.h
@@ -16,8 +16,16 @@ namespace MeshLib
 class Mesh;
 class NodePartitionedMesh;
 
-// compute all information necessary to transform the MeL::Mesh mesh into a
-// NodePartitionedMesh subdomain mesh
+/// Function computes all information necessary to transform the MeshLib::Mesh
+/// mesh into a NodePartitionedMesh subdomain mesh. Additional to the usual
+/// name, nodes and elements, the following information is computed:
+/// - a vector of global node ids of the subdomain mesh,
+/// - the number of global nodes (the sum of number of nodes of all subdomain
+/// meshes)
+/// - the number of regular nodes (the sum of number of non-ghost nodes of all
+/// subdomain meshes)
+/// - a vector containing the number of regular base nodes per rank
+/// - a vector containing the number of regular higher order nodes per rank
 std::unique_ptr<MeshLib::NodePartitionedMesh>
 transformMeshToNodePartitionedMesh(NodePartitionedMesh const* const bulk_mesh,
                                    Mesh const* const subdomain_mesh);
