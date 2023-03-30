6be10a078f40d51aabf96ba2ce0bdee69036017e
Lars Bilke <lars.bilke@ufz.de>
2023-03-30 10:33:30 +0000

    [guix] Added channels.scm.
    This file pins the dependency tree.

---
+++ scripts/guix/channels.scm
@@ -0,0 +1,14 @@
+(list (channel
+        (name 'guix-ogs)
+        (url "https://gitlab.opengeosys.org/ogs/inf/guix-ogs.git"))
+      (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch #f)
+        (commit
+          "8e2f32cee982d42a79e53fc1e9aa7b8ff0514714")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
