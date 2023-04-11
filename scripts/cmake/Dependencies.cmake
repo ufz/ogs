2643fe6b1314facc2e1ecec6499a8c4492a56600
Lars Bilke <lars.bilke@ufz.de>
2023-04-11 11:15:12 +0200

    [cpm] Bump nlohman/json to 3.10.5.
    From 3.10 tarball also contains license file.

--- scripts/cmake/Dependencies.cmake
+++ scripts/cmake/Dependencies.cmake
@@ -195,17 +195,17 @@ endif()
 
 CPMFindPackage(
     NAME nlohmann_json
-    VERSION 3.6.1
+    VERSION ${ogs.minimum_version.json}
     # the git repo is incredibly large, so we download the archived include
     # directory
-    URL https://github.com/nlohmann/json/releases/download/v3.6.1/include.zip
-    URL_HASH
-        SHA256=69cc88207ce91347ea530b227ff0776db82dcb8de6704e1a3d74f4841bc651cf
+    URL https://github.com/nlohmann/json/releases/download/v${ogs.minimum_version.json}/include.zip
+    URL_HASH SHA256=${ogs.minimum_version.json_sha}
 )
 if(nlohmann_json_ADDED)
     add_library(nlohmann_json::nlohmann_json INTERFACE IMPORTED)
     target_include_directories(
-        nlohmann_json::nlohmann_json INTERFACE ${nlohmann_json_SOURCE_DIR}
+        nlohmann_json::nlohmann_json
+        INTERFACE ${nlohmann_json_SOURCE_DIR}/include
     )
 endif()
 
