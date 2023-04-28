4630a7260c91544710d4dca8175ddb707fc8df01
Dmitri Naumov <dmitri.naumov@ufz.de>
2023-04-28 17:38:04 +0200

    [PL] Remove sorting of indices before global add
    This is more expensive then not to do it at all.
    For a particular TRM test the assembly time went down
    back to pre-OpenMP from 0.25s to 0.16s.

--- ProcessLib/Assembly/MatrixElementCache.h
+++ ProcessLib/Assembly/MatrixElementCache.h
@@ -59,16 +59,6 @@ struct MatrixElementCacheEntry
         default;
     MatrixElementCacheEntry& operator=(MatrixElementCacheEntry&&) = default;
 
-    static bool compareIndices(MatrixElementCacheEntry<Dim> const& a,
-                               MatrixElementCacheEntry<Dim> const& b)
-    {
-        auto const& ia = a.indices;
-        auto const& ib = b.indices;
-
-        return std::lexicographical_compare(ia.begin(), ia.end(), ib.begin(),
-                                            ib.end());
-    }
-
     bool indicesEqualTo(MatrixElementCacheEntry<Dim> const& other) const
     {
         return std::equal(indices.begin(), indices.end(),
@@ -258,9 +248,6 @@ private:
 
     void addToGlobal()
     {
-        std::sort(cache_.begin(), cache_.end(),
-                  MatrixElementCacheEntry<Dim>::compareIndices);
-
         mat_or_vec_.add(cache_);
         stats_.count_global += cache_.size();
         cache_.clear();
