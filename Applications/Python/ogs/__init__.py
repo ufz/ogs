cfdb760e4d58771cfdf3b46d9331973b3c3e41f7
Lars Bilke <lars.bilke@ufz.de>
2023-06-01 15:50:19 +0200

    [Python] RTLD_GLOBAL is available on Unix only.

--- Applications/Python/ogs/__init__.py
+++ Applications/Python/ogs/__init__.py
@@ -1,8 +1,10 @@
 import os
+import platform
 import sys
 
 from ._internal.wrap_cli_tools import cli  # noqa: F401
 
-# Otherwise runtime undefined symbols, see
-# https://stackoverflow.com/a/60841073/80480
-sys.setdlopenflags(os.RTLD_GLOBAL | os.RTLD_LAZY)
+if platform.system() != "Windows":
+    # Otherwise runtime undefined symbols, see
+    # https://stackoverflow.com/a/60841073/80480
+    sys.setdlopenflags(os.RTLD_GLOBAL | os.RTLD_LAZY)
