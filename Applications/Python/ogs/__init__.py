00b77221dcca6e7b4842cede1943a0d0535029e4
Lars Bilke <lars.bilke@ufz.de>
2023-06-01 11:20:35 +0200

    [Python] Enabled global symbol visibility between modules.

--- Applications/Python/ogs/__init__.py
+++ Applications/Python/ogs/__init__.py
@@ -1,1 +1,8 @@
+import os
+import sys
+
 from ._internal.wrap_cli_tools import cli  # noqa: F401
+
+# Otherwise runtime undefined symbols, see
+# https://stackoverflow.com/a/60841073/80480
+sys.setdlopenflags(os.RTLD_GLOBAL | os.RTLD_LAZY)
