20a2395e7908cb461f94b36e036189feb9963aff
Dmitri Naumov <dmitri.naumov@ufz.de>
2023-07-12 18:01:52 +0200

    [MaL] Add missing doxygen file header

--- MathLib/LinAlg/LinearSolverOptions.cpp
+++ MathLib/LinAlg/LinearSolverOptions.cpp
@@ -1,15 +1,22 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2023, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ */
+
 #include "LinearSolverOptions.h"
-* \file
 
 #include <set>
 
 #include "BaseLib/ConfigTree.h"
 
-    //! Configuration tag names of all known linear solvers for their
-    //! configuration in the project file.
-    //! Add your tag name here when you add a new solver.
-    static std::set<std::string>
-        known_linear_solvers{"eigen", "lis", "petsc"};
+//! Configuration tag names of all known linear solvers for their
+//! configuration in the project file.
+//! Add your tag name here when you add a new solver.
+static std::set<std::string> known_linear_solvers{"eigen", "lis", "petsc"};
 
 namespace MathLib
 {
