4a451a4791cc2b63592d3e0e7a678298fafb9ed3
Lars Bilke <lars.bilke@ufz.de>
2023-07-21 10:55:35 +0200

    [cppcheck] make-target now correctly uses cppcheck.sh.

--- scripts/cmake/CppCheck.cmake
+++ scripts/cmake/CppCheck.cmake
@@ -25,13 +25,6 @@ endif()
 
 add_custom_target(
     cppcheck
-    COMMAND
-        ${CPPCHECK_TOOL_PATH}
-        --project=${PROJECT_BINARY_DIR}/compile_commands.json --language=c++
-        --std=c++20 --enable=all --inconclusive ${CPPCHECK_THREADS} -i
-        ${PROJECT_BINARY_DIR}/CMakeFiles -i ${PROJECT_BINARY_DIR}/_deps -i
-        ${PROJECT_SOURCE_DIR}/ThirdParty -i
-        ${PROJECT_SOURCE_DIR}/Applications/DataExplorer -i
-        ${PROJECT_SOURCE_DIR}/Tests --xml --xml-version=2
-        --output-file=${PROJECT_BINARY_DIR}/cppcheck.log ${PROJECT_SOURCE_DIR}
+    COMMAND ${BASH_TOOL_PATH} cppcheck.sh
+    WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
 )
