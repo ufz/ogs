01b9d8ace2d3a10d1b178fcbeb6394bca6049f98
Dmitri Naumov <github@naumov.de>
2023-07-22 22:05:52 +0200

    [PL/HHPP] Rename x_dot to x_prev

--- ProcessLib/TwoPhaseFlowWithPP/TwoPhaseFlowWithPPLocalAssembler-impl.h
+++ ProcessLib/TwoPhaseFlowWithPP/TwoPhaseFlowWithPPLocalAssembler-impl.h
@@ -45,7 +45,7 @@ namespace MPL = MaterialPropertyLib;
 template <typename ShapeFunction, int GlobalDim>
 void TwoPhaseFlowWithPPLocalAssembler<ShapeFunction, GlobalDim>::assemble(
     double const t, double const dt, std::vector<double> const& local_x,
-    std::vector<double> const& /*local_xdot*/,
+    std::vector<double> const& /*local_x_prev*/,
     std::vector<double>& local_M_data, std::vector<double>& local_K_data,
     std::vector<double>& local_b_data)
 {
