2e1e5eec4471296bd9cdb00a32711cbd8dc5b318
Dmitri Naumov <dmitri.naumov@ufz.de>
2023-07-24 16:11:39 +0200

    [NL/TS] Separate parsing of repeat/dt pairs
    Localizes a variable scope and allows further refactorings,
    especially elemination of the block after the for-loop
    where the last time increment is repeated until end time.

--- NumLib/TimeStepping/Algorithms/CreateFixedTimeStepping.cpp
+++ NumLib/TimeStepping/Algorithms/CreateFixedTimeStepping.cpp
@@ -175,7 +175,6 @@ std::unique_ptr<TimeStepAlgorithm> createFixedTimeStepping(
 
     std::vector<double> delta_ts;
     double t_curr = t_initial;
-    double delta_t = 0.0;
 
     // TODO: consider adding call "listNonEmpty" to config tree
     //! \ogs_file_param{prj__time_loop__processes__process__time_stepping__FixedTimeStepping__timesteps__pair}
@@ -184,13 +183,18 @@ std::unique_ptr<TimeStepAlgorithm> createFixedTimeStepping(
     {
         OGS_FATAL("no timesteps have been given");
     }
+    std::vector<std::pair<std::size_t, double>> repeat_dt_pairs;
     for (auto const pair : range)
     {
-        //! \ogs_file_param{prj__time_loop__processes__process__time_stepping__FixedTimeStepping__timesteps__pair__repeat}
-        auto const repeat = pair.getConfigParameter<std::size_t>("repeat");
-        //! \ogs_file_param{prj__time_loop__processes__process__time_stepping__FixedTimeStepping__timesteps__pair__delta_t}
-        delta_t = pair.getConfigParameter<double>("delta_t");
+        repeat_dt_pairs.emplace_back(
+            //! \ogs_file_param{prj__time_loop__processes__process__time_stepping__FixedTimeStepping__timesteps__pair__repeat}
+            pair.getConfigParameter<std::size_t>("repeat"),
+            //! \ogs_file_param{prj__time_loop__processes__process__time_stepping__FixedTimeStepping__timesteps__pair__delta_t}
+            pair.getConfigParameter<double>("delta_t"));
+    }
 
+    for (auto const& [repeat, delta_t] : repeat_dt_pairs)
+    {
         if (repeat == 0)
         {
             OGS_FATAL("<repeat> is zero.");
@@ -209,6 +213,7 @@ std::unique_ptr<TimeStepAlgorithm> createFixedTimeStepping(
     // append last delta_t until t_end is reached
     if (t_curr <= t_end)
     {
+        auto const delta_t = repeat_dt_pairs.back().second;
         auto const repeat =
             static_cast<std::size_t>(std::ceil((t_end - t_curr) / delta_t));
         t_curr = addTimeIncrement(delta_ts, repeat, delta_t, t_curr);
