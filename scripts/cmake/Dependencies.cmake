95a51392bf4a646ab89e103e91883a0fde822b2c
Lars Bilke <lars.bilke@ufz.de>
2023-07-26 15:09:45 +0200

    [cmake] Suppress gtest warnings.

--- scripts/cmake/Dependencies.cmake
+++ scripts/cmake/Dependencies.cmake
@@ -12,6 +12,10 @@ if(OGS_BUILD_TESTING)
                 "BUILD_SHARED_LIBS OFF"
         EXCLUDE_FROM_ALL YES
     )
+    if(googletest_ADDED AND WIN32)
+        target_compile_options(gtest PRIVATE /EHsc)
+        target_compile_options(gmock PRIVATE /EHsc)
+    endif()
 
     CPMAddPackage(
         NAME autocheck
