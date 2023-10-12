48fb7a7e0c00f9e0096be108e39c3377bbb020be
Christoph Lehmann <christoph.lehmann@ufz.de>
2023-10-12 10:02:02 +0200

    [PL/Refl] Reflected I/O via accessor functions
    ... also enables reflected I/O of std::tuples next to structs.

--- ProcessLib/Reflection/ReflectionIPData.h
+++ ProcessLib/Reflection/ReflectionIPData.h
@@ -19,11 +19,32 @@ namespace ProcessLib::Reflection
 {
 namespace detail
 {
-// Used in metaprogramming to check if the type T has a member "reflect".
 template <typename T>
-constexpr bool has_reflect = requires
+concept has_reflect = requires { T::reflect(); };
+
+template <typename... Ts>
+auto reflect(std::type_identity<std::tuple<Ts...>>)
 {
-    T::reflect();
+    using namespace boost::mp11;
+
+    // The types Ts... must be unique. Duplicate types are incompatible with the
+    // concept of "reflected" I/O: they would lead to duplicate names for the
+    // I/O data.
+    static_assert(mp_is_set<mp_list<Ts...>>::value);
+
+    return reflectWithoutName<std::tuple<Ts...>>(
+        [](auto& tuple_) -> auto& { return std::get<Ts>(tuple_); }...);
+}
+
+template <has_reflect T>
+auto reflect(std::type_identity<T>)
+{
+    return T::reflect();
+}
+
+template <typename T>
+concept is_reflectable = requires {
+    ProcessLib::Reflection::detail::reflect(std::type_identity<T>{});
 };
 
 /**
@@ -241,23 +262,26 @@ void forEachReflectedFlattenedIPDataAccessor(
         reflection_data,
         [&accessor_ip_data_vec_in_loc_asm,
          &accessor_current_level_from_ip_data_vec_element,
-         &callback]<typename Class, typename Member>(
-            ReflectionData<Class, Member> const& refl_data)
+         &callback]<typename Class, typename Accessor>(
+            ReflectionData<Class, Accessor> const& refl_data)
         {
+            using MemberRef = std::invoke_result_t<Accessor, Class const&>;
+            using Member = std::remove_cvref_t<MemberRef>;
+
             auto accessor_member_from_ip_data_vec_element =
-                [level = refl_data.field,
+                [accessor_next_level = refl_data.accessor,
                  accessor_current_level_from_ip_data_vec_element](
                     auto const& ip_data_vec_element) -> Member const&
             {
-                return accessor_current_level_from_ip_data_vec_element(
-                           ip_data_vec_element).*
-                       level;
+                return accessor_next_level(
+                    accessor_current_level_from_ip_data_vec_element(
+                        ip_data_vec_element));
             };
 
-            if constexpr (has_reflect<Member>)
+            if constexpr (is_reflectable<Member>)
             {
                 forEachReflectedFlattenedIPDataAccessor<Dim>(
-                    callback, Member::reflect(),
+                    callback, detail::reflect(std::type_identity<Member>{}),
                     accessor_ip_data_vec_in_loc_asm,
                     accessor_member_from_ip_data_vec_element);
             }
@@ -309,20 +333,42 @@ void forEachReflectedFlattenedIPDataAccessor(ReflData const& reflection_data,
 {
     boost::mp11::tuple_for_each(
         reflection_data,
-        [&callback]<typename Class, typename Member>(
-            ReflectionData<Class, std::vector<Member>> const& refl_data)
+        [&callback]<typename Class, typename Accessor>(
+            ReflectionData<Class, Accessor> const& refl_data)
         {
-            auto accessor_ip_data_vec_in_loc_asm = [ip_data_vector =
-                                                        refl_data.field](
-                LocAsmIF const& loc_asm) -> auto const&
-            {
-                return loc_asm.*ip_data_vector;
-            };
+            static_assert(std::is_same_v<Class, LocAsmIF>,
+                          "The currently processed reflection data is not for "
+                          "the given LocAsmIF but for a different class.");
 
-            if constexpr (detail::has_reflect<Member>)
+            using AccessorResultRef =
+                std::invoke_result_t<Accessor, Class const&>;
+            using AccessorResult = std::remove_cvref_t<AccessorResultRef>;
+
+            // AccessorResult must be a std::vector<SomeType, SomeAllocator>. We
+            // check that, now.
+            static_assert(boost::mp11::mp_is_list<AccessorResult>::
+                              value);  // std::vector<SomeType, SomeAllocator>
+                                       // is a list in the Boost MP11 sense
+            static_assert(
+                std::is_same_v<
+                    AccessorResult,
+                    boost::mp11::mp_rename<AccessorResult, std::vector>>,
+                "We expect a std::vector, here.");
+            // Now, we know that AccessorResult is std::vector<Member>. To be
+            // more specific, AccessorResult is a std::vector<IPData> and Member
+            // is IPData.
+            using Member = typename AccessorResult::value_type;
+
+            auto accessor_ip_data_vec_in_loc_asm =
+                [ip_data_vector_accessor =
+                     refl_data.accessor](LocAsmIF const& loc_asm) -> auto const&
+            { return ip_data_vector_accessor(loc_asm); };
+
+            if constexpr (detail::is_reflectable<Member>)
             {
                 detail::forEachReflectedFlattenedIPDataAccessor<Dim>(
-                    callback, Member::reflect(),
+                    callback,
+                    detail::reflect(std::type_identity<Member>{}),
                     accessor_ip_data_vec_in_loc_asm);
             }
             else
