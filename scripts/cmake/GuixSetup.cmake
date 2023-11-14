70ffc90f428cd9d12c3ca8dd13381e4940db3ddc
Lars Bilke <lars.bilke@ufz.de>
2023-11-14 10:19:33 +0100

    [guix] Version detection.

--- scripts/cmake/GuixSetup.cmake
+++ scripts/cmake/GuixSetup.cmake
@@ -2,7 +2,19 @@ if(NOT DEFINED ENV{GUIX_ENVIRONMENT} AND NOT DEFINED ENV{NIX_BUILD_TOP})
     return()
 endif()
 
-message(STATUS "Guix environment detected: $ENV{GUIX_ENVIRONMENT}")
+if(DEFINED ENV{GUIX_ENVIRONMENT})
+    message(STATUS "Guix environment detected: $ENV{GUIX_ENVIRONMENT}")
+endif()
+if(DEFINED ENV{NIX_BUILD_TOP})
+    message(STATUS "Guix build detected: $ENV{NIX_BUILD_TOP}")
+    string(REGEX MATCH "^/gnu/store/(.*)-(.*)-(.*)$" _guix_version_match
+                 "${CMAKE_INSTALL_PREFIX}"
+    )
+    if("${OGS_VERSION}" STREQUAL "NO_VERSION")
+        set(OGS_VERSION "${CMAKE_MATCH_3}-guix-${CMAKE_MATCH_1}")
+        message(STATUS "Using guix-provided OGS_VERSION=${OGS_VERSION}.")
+    endif()
+endif()
 
 set(GUIX_BUILD ON CACHE BOOL "" FORCE)
 set(OGS_BUILD_TESTING OFF CACHE BOOL "" FORCE) # TODO: not yet supported
