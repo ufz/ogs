af44cc2e344a87fdf1ba7c3c956b48ed28a941b1
Haibing Shao <haibing.shao@ufz.de>
2023-12-02 19:11:36 +0100

    [T] benchmark of FCT feature
    change output prefix
    
    update results
    
    remove LIS and PETSc config
    
    switch on vtkdiff in OgsTest

--- ProcessLib/ComponentTransport/Tests.cmake
+++ ProcessLib/ComponentTransport/Tests.cmake
@@ -808,6 +808,10 @@ if (NOT OGS_USE_MPI)
 endif()
 
 if(NOT OGS_USE_PETSC)
+    OgsTest(PROJECTFILE Parabolic/ComponentTransport/FCT_test/1d_step_func.prj RUNTIME 5)
+endif()
+
+if(NOT OGS_USE_PETSC)
     NotebookTest(
         NOTEBOOKFILE Parabolic/ComponentTransport/ReactiveTransport/DecayChain/GlobalImplicitApproach/performance_measurements.ipynb
         RUNTIME 200
