684eab7b8a2fd6cf3d6dc55a6904aeff678a7d3a
Dmitri Naumov <github@naumov.de>
2023-12-28 14:06:58 +0100

    [ML] Vectorized tensor utilities
    The deformation gradient and alike quantities are second order tensors
    stored as a vector. Convenience operations on these tensors are
    collected in the namespace.
    
    The conversion to a Eigen matrix and back are used so far in tests only
    for comparisons with Eigen operations.

---
+++ MathLib/VectorizedTensor.cpp
@@ -0,0 +1,28 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2024, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ */
+
+#include "VectorizedTensor.h"
+
+namespace MathLib ::VectorizedTensor
+{
+
+bool isTensorConvertibleTo1d(Eigen::Matrix3d const& tensor)
+{
+    return (tensor(0, 1) == 0. && tensor(1, 0) == 0. &&  //
+            tensor(0, 2) == 0. && tensor(2, 0) == 0. &&  //
+            tensor(1, 2) == 0. && tensor(2, 1) == 0.);
+}
+
+bool isTensorConvertibleTo2d(Eigen::Matrix3d const& tensor)
+{
+    return (tensor(0, 2) == 0. && tensor(1, 2) == 0. &&  //
+            tensor(2, 0) == 0. && tensor(2, 1) == 0.);
+}
+
+}  // namespace MathLib::VectorizedTensor
