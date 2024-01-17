bcbcbc2b927ab984875d94358f135567a5e5bcc2
Dmitri Naumov <dmitri.naumov@ufz.de>
2024-01-17 19:18:43 +0100

    [PL/CR] Move SpaceTimeData and PrevState to common
    base for constitutive relations.

---
+++ ProcessLib/ConstitutiveRelations/Base.h
@@ -0,0 +1,53 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2024, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ */
+
+#pragma once
+
+#include "ParameterLib/SpatialPosition.h"
+
+namespace ProcessLib::ConstitutiveRelations
+{
+/// Represents a previous state of type T.
+template <typename T>
+struct PrevState
+{
+    PrevState() = default;
+    explicit PrevState(T const& t) : t{t} {}
+    explicit PrevState(T&& t) : t{std::move(t)} {}
+
+    PrevState<T>& operator=(T const& u)
+    {
+        t = u;
+        return *this;
+    }
+
+    PrevState<T>& operator=(T&& u)
+    {
+        t = std::move(u);
+        return *this;
+    }
+
+    T& operator*() { return t; }
+    T const& operator*() const { return t; }
+
+    T* operator->() { return &t; }
+    T const* operator->() const { return &t; }
+
+private:
+    T t;
+};
+
+struct SpaceTimeData
+{
+    ParameterLib::SpatialPosition x;
+    double t;
+    double dt;
+};
+
+}  // namespace ProcessLib::ConstitutiveRelations
--- ProcessLib/ThermoRichardsMechanics/ConstitutiveCommon/Base.h
+++ ProcessLib/ThermoRichardsMechanics/ConstitutiveCommon/Base.h
@@ -13,10 +13,14 @@
 #include "MaterialLib/MPL/Medium.h"
 #include "MathLib/KelvinVector.h"
 #include "ParameterLib/SpatialPosition.h"
+#include "ProcessLib/ConstitutiveRelations/Base.h"
 #include "ProcessLib/Reflection/ReflectionData.h"
 
 namespace ProcessLib::ThermoRichardsMechanics
 {
+
+using namespace ProcessLib::ConstitutiveRelations;
+
 template <int DisplacementDim>
 using KelvinVector = MathLib::KelvinVector::KelvinVectorType<DisplacementDim>;
 
@@ -74,43 +78,6 @@ constexpr KelvinMatrix<DisplacementDim> KMzero()
 {
     return KelvinMatrix<DisplacementDim>::Zero();
 }
-
-/// Represents a previous state of type T.
-template <typename T>
-struct PrevState
-{
-    PrevState() = default;
-    explicit PrevState(T const& t) : t{t} {}
-    explicit PrevState(T&& t) : t{std::move(t)} {}
-
-    PrevState<T>& operator=(T const& u)
-    {
-        t = u;
-        return *this;
-    }
-
-    PrevState<T>& operator=(T&& u)
-    {
-        t = std::move(u);
-        return *this;
-    }
-
-    T& operator*() { return t; }
-    T const& operator*() const { return t; }
-
-    T* operator->() { return &t; }
-    T const* operator->() const { return &t; }
-
-private:
-    T t;
-};
-
-struct SpaceTimeData
-{
-    ParameterLib::SpatialPosition x;
-    double t;
-    double dt;
-};
 
 struct MediaData
 {
