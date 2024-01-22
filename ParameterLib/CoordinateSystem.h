78999c5d7cf12eb58d95fbe9304e5188d22aac5f
Wenqing Wang <wenqing.wang@ufz.de>
2024-01-22 16:11:15 +0100

    [ParameterLib] Enabled CoordinateSystem have implict bases

--- ParameterLib/CoordinateSystem.h
+++ ParameterLib/CoordinateSystem.h
@@ -25,6 +25,25 @@ namespace ParameterLib
 {
 struct CoordinateSystem final
 {
+    /**
+     * It is used to create a coordinate system with only one base, where the
+     * last base is explicitly given. The other bases are set as implicit and
+     * computed from the given base as follows:
+     *  - For a 2D coordinate system, the unit vector orthogonal to
+     *    the given base is used as the first base,
+     *  - For a 3D coordinate system, the given base vector, \c unit_direction,
+     *    is set as the third base,  \f${\vec e}_2\f$. An arbitrary unit vector
+     *    orthogonal to \f${\vec e}_2\f$ is selected as the second base
+     *    \f$e_1\f$, and the first  base \f${\vec e}_0\f$ is calculated as
+     *    \f${\vec e}_0 = {\vec e}_1 \times {\vec e}_2\f$.
+     *
+     * This type of coordinate system can be used for computations in transverse
+     * isotropic material models.
+     *
+     * @param unit_direction The specified unit direction.
+     */
+    explicit CoordinateSystem(Parameter<double> const& unit_direction);
+
     CoordinateSystem(Parameter<double> const& e0, Parameter<double> const& e1);
 
     CoordinateSystem(Parameter<double> const& e0,
@@ -48,6 +67,10 @@ struct CoordinateSystem final
 
 private:
     std::array<Parameter<double> const*, 3> _base;
+    bool _has_implicit_base;
+
+    Eigen::Matrix<double, 3, 3> transformationFromSingleBase_3d(
+        SpatialPosition const& pos) const;
 };
 
 extern template Eigen::Matrix<double, 2, 2> CoordinateSystem::rotateTensor<2>(
