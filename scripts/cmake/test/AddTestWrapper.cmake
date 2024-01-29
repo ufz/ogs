7d7461e83e7fb659a7f407f8637ec435c02b4464
Lars Bilke <lars.bilke@ufz.de>
2024-01-29 15:38:34 +0100

    [cmake] Simplified AddTestWrapper, now works with PASS_REGULAR_EXPRESSION

--- scripts/cmake/test/AddTestWrapper.cmake
+++ scripts/cmake/test/AddTestWrapper.cmake
@@ -1,31 +1,20 @@
 # IMPORTANT: multiple arguments in one variables have to be in list notation (;)
 # and have to be quoted when passed
 # "-DEXECUTABLE_ARGS=${AddTest_EXECUTABLE_ARGS}"
-
-string(REPLACE ";" " " CMD_STRING
-               "cd ${WORKING_DIRECTORY} && ${WRAPPER_COMMAND} "
-               "${WRAPPER_ARGS} ${EXECUTABLE} ${EXECUTABLE_ARGS}"
-)
-message(STATUS "Test command cleaned:\n${CMD_STRING}")
-
 execute_process(
     COMMAND ${WRAPPER_COMMAND} ${WRAPPER_ARGS} ${EXECUTABLE} ${EXECUTABLE_ARGS}
     WORKING_DIRECTORY ${WORKING_DIRECTORY}
     RESULT_VARIABLE EXIT_CODE
-    OUTPUT_FILE ${LOG_FILE}
-    ERROR_FILE ${LOG_FILE}
+    OUTPUT_VARIABLE LOG
+    ERROR_VARIABLE LOG
+    ECHO_OUTPUT_VARIABLE
+    ECHO_ERROR_VARIABLE
+    COMMAND_ECHO STDOUT
 )
 
-# execute_process(
-#     COMMAND cat ${LOG_FILE}
-#     WORKING_DIRECTORY ${WORKING_DIRECTORY}
-#     RESULT_VARIABLE EXIT_CODE
-# )
-
-if(EXIT_CODE STREQUAL "0")
-    if(DEFINED ENV{CI})
-        file(REMOVE ${LOG_FILE})
-    endif()
-else()
+if(EXIT_CODE STREQUAL "0" AND NOT DEFINED ENV{CI})
+    file(WRITE ${LOG_FILE} "${LOG}")
+elseif(NOT EXIT_CODE STREQUAL "0")
+    file(WRITE ${LOG_FILE} "${LOG}")
     message(FATAL_ERROR "Exit code: ${EXIT_CODE}; log file: ${LOG_FILE}")
 endif()
