e7e0dba43ea0e9a5a7ef03992cdedf94ab79d2c0
Wenqing Wang <wenqing.wang@ufz.de>
2024-02-06 14:42:06 +0100

    [CTest/LIE/M] Update the project files and reference files of
    the remaining benchmarks.

--- ProcessLib/LIE/SmallDeformation/Tests.cmake
+++ ProcessLib/LIE/SmallDeformation/Tests.cmake
@@ -16,7 +16,7 @@ AddTest(
     REQUIREMENTS NOT OGS_USE_MPI
     DIFF_DATA
     single_joint_3D_ts_1_t_1.000000.vtu single_joint_3D_ts_1_t_1.000000.vtu displacement displacement 1e-16 1e-16
-    single_joint_3D_expected_ts_1_t_1.000000.vtu single_joint_3D_ts_1_t_1.000000.vtu displacement_jump1 displacement_jump1 1e-16 1e-16
+    single_joint_3D_ts_1_t_1.000000.vtu single_joint_3D_ts_1_t_1.000000.vtu displacement_jump1 displacement_jump1 1e-16 1e-16
 )
 
 AddTest(
@@ -68,9 +68,10 @@ AddTest(
     TESTER vtkdiff
     REQUIREMENTS NOT OGS_USE_MPI
     DIFF_DATA
-    expected_single_joint_negative_aperture_ts_1_t_1.000000.vtu single_joint_negative_aperture_ts_1_t_1.000000.vtu displacement displacement 1e-16 1e-16
-    expected_single_joint_negative_aperture_ts_1_t_1.000000.vtu single_joint_negative_aperture_ts_1_t_1.000000.vtu displacement_jump1 displacement_jump1 1e-16 1e-16
-    expected_single_joint_negative_aperture_ts_1_t_1.000000.vtu single_joint_negative_aperture_ts_1_t_1.000000.vtu aperture aperture 1e-16 1e-16
+    single_joint_negative_aperture_ts_1_t_1.000000.vtu single_joint_negative_aperture_ts_1_t_1.000000.vtu displacement displacement 1e-16 1e-16
+    single_joint_negative_aperture_ts_1_t_1.000000.vtu single_joint_negative_aperture_ts_1_t_1.000000.vtu displacement_jump1 displacement_jump1 1e-16 1e-16
+    single_joint_negative_aperture_ts_1_t_1.000000.vtu single_joint_negative_aperture_ts_1_t_1.000000.vtu fracture_aperture fracture_aperture 1e-16 1e-16
+    single_joint_negative_aperture_ts_1_t_1.000000.vtu single_joint_negative_aperture_ts_1_t_1.000000.vtu fracture_aperture_avg fracture_aperture_avg 1e-16 1e-16
 )
 
 AddTest(
@@ -82,9 +83,10 @@ AddTest(
     TESTER vtkdiff
     REQUIREMENTS NOT OGS_USE_MPI
     DIFF_DATA
-    expected_single_joint_displacement_controlled_ts_10_t_1.000000.vtu single_joint_displacement_controlled_ts_10_t_1.000000.vtu displacement displacement 1e-16 0
-    expected_single_joint_displacement_controlled_ts_10_t_1.000000.vtu single_joint_displacement_controlled_ts_10_t_1.000000.vtu displacement_jump1 displacement_jump1 1e-16 0
-    expected_single_joint_displacement_controlled_ts_10_t_1.000000.vtu single_joint_displacement_controlled_ts_10_t_1.000000.vtu aperture aperture 1e-16 1e-16
+    single_joint_displacement_controlled_ts_10_t_1.000000.vtu single_joint_displacement_controlled_ts_10_t_1.000000.vtu displacement displacement 1e-16 0
+    single_joint_displacement_controlled_ts_10_t_1.000000.vtu single_joint_displacement_controlled_ts_10_t_1.000000.vtu displacement_jump1 displacement_jump1 1e-16 0
+    single_joint_displacement_controlled_ts_10_t_1.000000.vtu single_joint_displacement_controlled_ts_10_t_1.000000.vtu fracture_aperture fracture_aperture 1e-16 1e-16
+    single_joint_displacement_controlled_ts_10_t_1.000000.vtu single_joint_displacement_controlled_ts_10_t_1.000000.vtu fracture_aperture_avg fracture_aperture_avg 1e-16 1e-16
 )
 
 AddTest(
@@ -96,13 +98,13 @@ AddTest(
     TESTER vtkdiff
     REQUIREMENTS NOT OGS_USE_MPI
     DIFF_DATA
-    expected_two_cracks_branch_pull_ts_1_t_1.000000.vtu two_cracks_branch_pull_ts_1_t_1.000000.vtu displacement displacement 1e-16 1e-16
-    expected_two_cracks_branch_pull_ts_1_t_1.000000.vtu two_cracks_branch_pull_ts_1_t_1.000000.vtu displacement_jump1 displacement_jump1 1e-16 1e-16
-    expected_two_cracks_branch_pull_ts_1_t_1.000000.vtu two_cracks_branch_pull_ts_1_t_1.000000.vtu displacement_jump2 displacement_jump2 1e-16 1e-16
-    expected_two_cracks_branch_pull_ts_1_t_1.000000.vtu two_cracks_branch_pull_ts_1_t_1.000000.vtu sigma_xx sigma_xx 1e-6 1e-6
-    expected_two_cracks_branch_pull_ts_1_t_1.000000.vtu two_cracks_branch_pull_ts_1_t_1.000000.vtu sigma_yy sigma_yy 1e-6 1e-6
-    expected_two_cracks_branch_pull_ts_1_t_1.000000.vtu two_cracks_branch_pull_ts_1_t_1.000000.vtu sigma_xy sigma_xy 1e-6 1e-6
-    expected_two_cracks_branch_pull_ts_1_t_1.000000.vtu two_cracks_branch_pull_ts_1_t_1.000000.vtu f_stress_n f_stress_n 1e-6 5e-6
+    two_cracks_branch_pull_ts_1_t_1.000000.vtu two_cracks_branch_pull_ts_1_t_1.000000.vtu displacement displacement 1e-16 1e-16
+    two_cracks_branch_pull_ts_1_t_1.000000.vtu two_cracks_branch_pull_ts_1_t_1.000000.vtu displacement_jump1 displacement_jump1 1e-16 1e-16
+    two_cracks_branch_pull_ts_1_t_1.000000.vtu two_cracks_branch_pull_ts_1_t_1.000000.vtu displacement_jump2 displacement_jump2 1e-16 1e-16
+    two_cracks_branch_pull_ts_1_t_1.000000.vtu two_cracks_branch_pull_ts_1_t_1.000000.vtu sigma sigma 1e-6 1e-6
+    two_cracks_branch_pull_ts_1_t_1.000000.vtu two_cracks_branch_pull_ts_1_t_1.000000.vtu sigma_avg sigma_avg 1e-6 1e-6
+    two_cracks_branch_pull_ts_1_t_1.000000.vtu two_cracks_branch_pull_ts_1_t_1.000000.vtu fracture_stress fracture_stress 1e-6 5e-6
+    two_cracks_branch_pull_ts_1_t_1.000000.vtu two_cracks_branch_pull_ts_1_t_1.000000.vtu fracture_stress_avg fracture_stress_avg 1e-6 5e-6
 )
 
 AddTest(
@@ -114,14 +116,13 @@ AddTest(
     TESTER vtkdiff
     REQUIREMENTS NOT OGS_USE_MPI
     DIFF_DATA
-    expected_two_cracks_junction_pull_ts_1_t_1.000000.vtu two_cracks_junction_pull_ts_1_t_1.000000.vtu displacement displacement 1e-16 1e-16
-    expected_two_cracks_junction_pull_ts_1_t_1.000000.vtu two_cracks_junction_pull_ts_1_t_1.000000.vtu displacement_jump1 displacement_jump1 1e-16 1e-16
-    expected_two_cracks_junction_pull_ts_1_t_1.000000.vtu two_cracks_junction_pull_ts_1_t_1.000000.vtu displacement_jump2 displacement_jump2 1e-16 1e-16
-    expected_two_cracks_junction_pull_ts_1_t_1.000000.vtu two_cracks_junction_pull_ts_1_t_1.000000.vtu displacement_jump3 displacement_jump3 1e-16 1e-16
-    expected_two_cracks_junction_pull_ts_1_t_1.000000.vtu two_cracks_junction_pull_ts_1_t_1.000000.vtu sigma_xx sigma_xx 1e-6 1e-6
-    expected_two_cracks_junction_pull_ts_1_t_1.000000.vtu two_cracks_junction_pull_ts_1_t_1.000000.vtu sigma_yy sigma_yy 1e-6 1e-6
-    expected_two_cracks_junction_pull_ts_1_t_1.000000.vtu two_cracks_junction_pull_ts_1_t_1.000000.vtu sigma_xy sigma_xy 1e-6 1e-6
-    expected_two_cracks_junction_pull_ts_1_t_1.000000.vtu two_cracks_junction_pull_ts_1_t_1.000000.vtu f_stress_n f_stress_n 1e-6 5e-6
+    two_cracks_junction_pull_ts_1_t_1.000000.vtu two_cracks_junction_pull_ts_1_t_1.000000.vtu displacement displacement 1e-16 1e-16
+    two_cracks_junction_pull_ts_1_t_1.000000.vtu two_cracks_junction_pull_ts_1_t_1.000000.vtu displacement_jump1 displacement_jump1 1e-16 1e-16
+    two_cracks_junction_pull_ts_1_t_1.000000.vtu two_cracks_junction_pull_ts_1_t_1.000000.vtu displacement_jump2 displacement_jump2 1e-16 1e-16
+    two_cracks_junction_pull_ts_1_t_1.000000.vtu two_cracks_junction_pull_ts_1_t_1.000000.vtu displacement_jump3 displacement_jump3 1e-16 1e-16
+    two_cracks_junction_pull_ts_1_t_1.000000.vtu two_cracks_junction_pull_ts_1_t_1.000000.vtu sigma sigma 1e-6 1e-6
+    two_cracks_junction_pull_ts_1_t_1.000000.vtu two_cracks_junction_pull_ts_1_t_1.000000.vtu fracture_stress fracture_stress 1e-6 5e-6
+    two_cracks_junction_pull_ts_1_t_1.000000.vtu two_cracks_junction_pull_ts_1_t_1.000000.vtu fracture_stress_avg fracture_stress_avg 1e-6 5e-6
 )
 
 AddTest(
@@ -133,11 +134,11 @@ AddTest(
     TESTER vtkdiff
     REQUIREMENTS NOT OGS_USE_MPI
     DIFF_DATA
-    expected_sfrac_ts_1_t_1.000000.vtu sfrac_ts_1_t_1.000000.vtu displacement displacement 1e-16 1e-16
-    expected_sfrac_ts_1_t_1.000000.vtu sfrac_ts_1_t_1.000000.vtu displacement_jump1 displacement_jump1 1e-16 1e-16
-    expected_sfrac_ts_1_t_1.000000.vtu sfrac_ts_1_t_1.000000.vtu displacement_jump2 displacement_jump2 1e-16 1e-16
-    expected_sfrac_ts_1_t_1.000000.vtu sfrac_ts_1_t_1.000000.vtu displacement_jump3 displacement_jump3 1e-16 1e-16
-    expected_sfrac_ts_1_t_1.000000.vtu sfrac_ts_1_t_1.000000.vtu displacement_jump4 displacement_jump4 1e-16 1e-16
-    expected_sfrac_ts_1_t_1.000000.vtu sfrac_ts_1_t_1.000000.vtu displacement_jump4 displacement_jump4 1e-16 1e-16
-    expected_sfrac_ts_1_t_1.000000.vtu sfrac_ts_1_t_1.000000.vtu f_stress_n f_stress_n 1e-6 5e-6
+    sfrac_ts_1_t_1.000000.vtu sfrac_ts_1_t_1.000000.vtu displacement displacement 1e-16 1e-16
+    sfrac_ts_1_t_1.000000.vtu sfrac_ts_1_t_1.000000.vtu displacement_jump1 displacement_jump1 1e-16 1e-16
+    sfrac_ts_1_t_1.000000.vtu sfrac_ts_1_t_1.000000.vtu displacement_jump2 displacement_jump2 1e-16 1e-16
+    sfrac_ts_1_t_1.000000.vtu sfrac_ts_1_t_1.000000.vtu displacement_jump3 displacement_jump3 1e-16 1e-16
+    sfrac_ts_1_t_1.000000.vtu sfrac_ts_1_t_1.000000.vtu displacement_jump4 displacement_jump4 1e-16 1e-16
+    sfrac_ts_1_t_1.000000.vtu sfrac_ts_1_t_1.000000.vtu displacement_jump4 displacement_jump4 1e-16 1e-16
+    sfrac_ts_1_t_1.000000.vtu sfrac_ts_1_t_1.000000.vtu fracture_stress_avg fracture_stress_avg 1e-6 5e-6
 )
