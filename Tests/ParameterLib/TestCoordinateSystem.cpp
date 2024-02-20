151966c1139b0a2b1837910975636d8c68af71ae
Wenqing Wang <wenqing.wang@ufz.de>
2024-02-20 10:26:40 +0100

    [UnitTest] Tests to capture all fatal errors in createCoordinateSystem

--- Tests/ParameterLib/TestCoordinateSystem.cpp
+++ Tests/ParameterLib/TestCoordinateSystem.cpp
@@ -52,6 +52,7 @@ public:
 };
 
 // Test ParameterLib::CoordinateSystem::transformation_3d
+
 void testTransformation3D(
     ParameterLib::SpatialPosition const& pos,
     ParameterLib::CoordinateSystem const& coordinate_system,
@@ -242,4 +243,370 @@ TEST(CoordinateSystem, test3D)
 
     // Check CoordinateSystem::transformation_3d
     testTransformation3D(pos, coordinate_system, transform_matrix);
+}
+
+template <int Dimension>
+void testException(
+    std::string const& imput_xml,
+    std::vector<std::unique_ptr<ParameterLib::ParameterBase>> const& parameters,
+    std::string_view const error_message)
+{
+    try
+    {
+        Tests::createCoordinateSystem(imput_xml.data(), parameters);
+    }
+    catch (std::exception& e)
+    {
+        EXPECT_EQ(e.what(), error_message)
+            << "Expected error message:\n"
+            << e.what() << "\nGiven error message:\n"
+            << error_message;
+    }
+}
+
+static std::string const error_message_implicit_explicit_2D =
+    "The case of implicit \"basis_vector_0\" and explicit "
+    "\"basis_vector_1\" is for a 2D coordinate system. The parameter "
+    "for \"basis_vector_1\", e1, must have two components but it has "
+    "3. In addition, \"basis_vector_2\" should not exist in this case.";
+
+static std::string const error_message_explicit_implicit =
+    "Since basis_vector_0 is explicitly defined, basis_vector_1 must "
+    "be explicit as well.";
+
+static std::string const error_message_implicit_base2 =
+    "basis_vector_2 must be explicit.";
+
+TEST(CoordinateSystem, testWrongBase0ComponentNumberException)
+{
+    std::vector<std::unique_ptr<ParameterLib::ParameterBase>> parameters;
+
+    std::vector<double> e0{1.0, 0.0, 0.0, 0.0};
+    parameters.emplace_back(
+        std::make_unique<ParameterLib::ConstantParameter<double>>("e0", e0));
+
+    std::string const error_message =
+        "Basis vector parameter 'e0' must have two or three components, but it "
+        "has 4.";
+
+    std::string const xml =
+        "<local_coordinate_system>"
+        "   <basis_vector_0>e0</basis_vector_0>"
+        "</local_coordinate_system>";
+
+    testException<3>(xml, parameters, error_message);
+}
+
+// Test: basis_vector_2 must not be presented in  the 2D coordinate system
+
+TEST(CoordinateSystem, test2DBase2Exception)
+{
+    std::vector<std::unique_ptr<ParameterLib::ParameterBase>> parameters;
+
+    std::vector<double> e0{1.0, 0.0};
+    parameters.emplace_back(
+        std::make_unique<ParameterLib::ConstantParameter<double>>("e0", e0));
+    std::vector<double> e1{0.0, 1.0};
+    parameters.emplace_back(
+        std::make_unique<ParameterLib::ConstantParameter<double>>("e1", e1));
+
+    std::string const error_message =
+        "The tag \"basis_vector_2\" is not needed for a 2D local coordinate "
+        "system.";
+
+    {
+        std::string const xml =
+            "<local_coordinate_system>"
+            "   <basis_vector_0>e0</basis_vector_0>"
+            "   <basis_vector_1>e1</basis_vector_1>"
+            "   <basis_vector_2>e1</basis_vector_2>"
+            "</local_coordinate_system>";
+        testException<2>(xml, parameters, error_message);
+    }
+    {
+        std::string const xml =
+            "<local_coordinate_system>"
+            "   <basis_vector_0 implicit = \"true\" />"
+            "   <basis_vector_1>e1</basis_vector_1>"
+            "   <basis_vector_2 implicit = \"true\" />"
+            "</local_coordinate_system>";
+        testException<2>(xml, parameters, error_message);
+    }
+}
+
+TEST(CoordinateSystem, test2DWrongComponentNumberException)
+{
+    std::vector<std::unique_ptr<ParameterLib::ParameterBase>> parameters;
+
+    std::vector<double> e0{1.0, 0.0};
+    parameters.emplace_back(
+        std::make_unique<ParameterLib::ConstantParameter<double>>("e0", e0));
+    std::vector<double> e1{0.0, 1.0, 0.0};
+    parameters.emplace_back(
+        std::make_unique<ParameterLib::ConstantParameter<double>>("e1", e1));
+
+    {
+        std::string const xml =
+            "<local_coordinate_system>"
+            "   <basis_vector_0>e0</basis_vector_0>"
+            "   <basis_vector_1>e1</basis_vector_1>"
+            "</local_coordinate_system>";
+        std::string const error_message =
+            "The read parameter `e1' has the wrong number of components"
+            " (3 instead of 2).";
+
+        testException<2>(xml, parameters, error_message);
+    }
+
+    {  // With implicit base
+
+        std::string const xml =
+            "<local_coordinate_system>"
+            "   <basis_vector_0 implicit = \"true\" />"
+            "   <basis_vector_1>e1</basis_vector_1>"
+            "</local_coordinate_system>";
+
+        testException<2>(xml, parameters, error_message_implicit_explicit_2D);
+    }
+}
+
+TEST(CoordinateSystem, test3DWrongComponentNumberException)
+{
+    std::vector<double> e0{1.0, 0.0, 0.0};
+    {  // Wrong component number in base 1
+        std::vector<std::unique_ptr<ParameterLib::ParameterBase>> parameters;
+
+        parameters.emplace_back(
+            std::make_unique<ParameterLib::ConstantParameter<double>>("e0",
+                                                                      e0));
+        std::vector<double> e1{0.0, 1.0};
+        parameters.emplace_back(
+            std::make_unique<ParameterLib::ConstantParameter<double>>("e1",
+                                                                      e1));
+        std::vector<double> e2{0.0, 1.0, 1.0};
+        parameters.emplace_back(
+            std::make_unique<ParameterLib::ConstantParameter<double>>("e2",
+                                                                      e2));
+
+        std::string const xml =
+            "<local_coordinate_system>"
+            "   <basis_vector_0>e0</basis_vector_0>"
+            "   <basis_vector_1>e1</basis_vector_1>"
+            "   <basis_vector_2>e2</basis_vector_2>"
+            "</local_coordinate_system>";
+        std::string const error_message =
+            "The read parameter `e1' has the wrong number of components"
+            " (2 instead of 3).";
+
+        testException<3>(xml, parameters, error_message);
+    }
+
+    {  // Wrong component number in base 2
+        std::vector<std::unique_ptr<ParameterLib::ParameterBase>> parameters;
+
+        parameters.emplace_back(
+            std::make_unique<ParameterLib::ConstantParameter<double>>("e0",
+                                                                      e0));
+
+        std::vector<double> e1{0.0, 1.0, 0.0};
+        parameters.emplace_back(
+            std::make_unique<ParameterLib::ConstantParameter<double>>("e1",
+                                                                      e1));
+        std::vector<double> e2{0.0, 1.0};
+        parameters.emplace_back(
+            std::make_unique<ParameterLib::ConstantParameter<double>>("e2",
+                                                                      e2));
+
+        std::string const error_message =
+            "The read parameter `e2' for tag basis_vector_2 has the wrong "
+            "number of components (2 instead of 3).";
+
+        {
+            std::string const xml =
+                "<local_coordinate_system>"
+                "   <basis_vector_0>e0</basis_vector_0>"
+                "   <basis_vector_1>e1</basis_vector_1>"
+                "   <basis_vector_2>e2</basis_vector_2>"
+                "</local_coordinate_system>";
+
+            testException<3>(xml, parameters, error_message);
+        }
+
+        {  // with implicit bases
+
+            std::string const xml =
+                "<local_coordinate_system>"
+                "   <basis_vector_0 implicit = \"true\" />"
+                "   <basis_vector_1 implicit = \"true\" />"
+                "   <basis_vector_2>e2</basis_vector_2>"
+                "</local_coordinate_system>";
+
+            testException<3>(xml, parameters, error_message);
+        }
+    }
+}
+
+TEST(CoordinateSystem, test2DWrongImplicitBaseException)
+{
+    {  // Two bases are implicit
+        std::vector<std::unique_ptr<ParameterLib::ParameterBase>> parameters;
+
+        std::string const xml =
+            "<local_coordinate_system>"
+            "   <basis_vector_0 implicit = \"true\" />"
+            "   <basis_vector_1 implicit = \"true\" />"
+            "</local_coordinate_system>";
+        std::string const error_message =
+            "Both \"basis_vector_0\" and \"basis_vector_1\" are implicit but "
+            "\"basis_vector_2\" does not exist. If 2D coordinate system is "
+            "considered, please change \"basis_vector_1\" to explicit. If 3D "
+            "coordinate system is considered, please add \"basis_vector_2\".";
+
+        testException<2>(xml, parameters, error_message);
+    }
+
+    {  // base0 is explicit but base1 is implicit
+
+        std::vector<std::unique_ptr<ParameterLib::ParameterBase>> parameters;
+
+        std::vector<double> e0{1.0, 0.0};
+        parameters.emplace_back(
+            std::make_unique<ParameterLib::ConstantParameter<double>>("e0",
+                                                                      e0));
+
+        std::string const xml =
+            "<local_coordinate_system>"
+            "   <basis_vector_0>e0</basis_vector_0>"
+            "   <basis_vector_1 implicit = \"true\" />"
+            "</local_coordinate_system>";
+
+        testException<2>(xml, parameters, error_message_explicit_implicit);
+    }
+}
+
+TEST(CoordinateSystem, test3DWrongImplicitBaseException)
+{
+    // Base attribute: i for implicit and e for explicit
+    // case: e e i
+    {  // base0 and base1 are explicit but base2 is implicit
+
+        std::vector<std::unique_ptr<ParameterLib::ParameterBase>> parameters;
+
+        std::vector<double> e0{1.0, 0.0, 0.0};
+        parameters.emplace_back(
+            std::make_unique<ParameterLib::ConstantParameter<double>>("e0",
+                                                                      e0));
+        std::vector<double> e1{0.0, 1.0, 0.0};
+        parameters.emplace_back(
+            std::make_unique<ParameterLib::ConstantParameter<double>>("e1",
+                                                                      e1));
+
+        std::string const xml =
+            "<local_coordinate_system>"
+            "   <basis_vector_0>e0</basis_vector_0>"
+            "   <basis_vector_1>e0</basis_vector_1>"
+            "   <basis_vector_2 implicit = \"true\" />"
+            "</local_coordinate_system>";
+
+        testException<3>(xml, parameters, error_message_implicit_base2);
+    }
+
+    // case: e i e
+    {
+        std::vector<std::unique_ptr<ParameterLib::ParameterBase>> parameters;
+
+        std::vector<double> e0{1.0, 0.0, 0.0};
+        parameters.emplace_back(
+            std::make_unique<ParameterLib::ConstantParameter<double>>("e0",
+                                                                      e0));
+        std::vector<double> e2{0.0, 0.0, 1.0};
+        parameters.emplace_back(
+            std::make_unique<ParameterLib::ConstantParameter<double>>("e2",
+                                                                      e2));
+        std::string const xml =
+            "<local_coordinate_system>"
+            "   <basis_vector_0>e0</basis_vector_0>"
+            "   <basis_vector_1 implicit = \"true\" />"
+            "   <basis_vector_2>e2</basis_vector_2>"
+            "</local_coordinate_system>";
+
+        testException<3>(xml, parameters, error_message_explicit_implicit);
+    }
+
+    // case: e i i
+    {
+        std::vector<std::unique_ptr<ParameterLib::ParameterBase>> parameters;
+
+        std::vector<double> e0{1.0, 0.0, 0.0};
+        parameters.emplace_back(
+            std::make_unique<ParameterLib::ConstantParameter<double>>("e0",
+                                                                      e0));
+        std::string const xml =
+            "<local_coordinate_system>"
+            "   <basis_vector_0>e0</basis_vector_0>"
+            "   <basis_vector_1 implicit = \"true\" />"
+            "   <basis_vector_2 implicit = \"true\" />"
+            "</local_coordinate_system>";
+
+        testException<3>(xml, parameters, error_message_explicit_implicit);
+    }
+
+    // case: i i i
+    {  // all bases are implicit
+        std::vector<std::unique_ptr<ParameterLib::ParameterBase>> parameters;
+
+        std::string const xml =
+            "<local_coordinate_system>"
+            "   <basis_vector_0 implicit = \"true\" />"
+            "   <basis_vector_1 implicit = \"true\" />"
+            "   <basis_vector_2 implicit = \"true\" />"
+            "</local_coordinate_system>";
+
+        testException<3>(xml, parameters, error_message_implicit_base2);
+    }
+
+    // case: i e i
+    {  // all bases are implicit
+        std::vector<std::unique_ptr<ParameterLib::ParameterBase>> parameters;
+
+        std::vector<double> e1{0.0, 1.0, 0.0};
+        parameters.emplace_back(
+            std::make_unique<ParameterLib::ConstantParameter<double>>("e1",
+                                                                      e1));
+        std::string const xml =
+            "<local_coordinate_system>"
+            "   <basis_vector_0 implicit = \"true\" />"
+            "   <basis_vector_1>e1</basis_vector_1>"
+            "   <basis_vector_2 implicit = \"true\" />"
+            "</local_coordinate_system>";
+
+        testException<3>(xml, parameters, error_message_implicit_explicit_2D);
+    }
+
+    // case: i e e
+    {
+        std::vector<std::unique_ptr<ParameterLib::ParameterBase>> parameters;
+
+        std::vector<double> e1{0.0, 1.0, 0.0};
+        parameters.emplace_back(
+            std::make_unique<ParameterLib::ConstantParameter<double>>("e1",
+                                                                      e1));
+        std::vector<double> e2{0.0, 0.0, 1.0};
+        parameters.emplace_back(
+            std::make_unique<ParameterLib::ConstantParameter<double>>("e2",
+                                                                      e2));
+        std::string const xml =
+            "<local_coordinate_system>"
+            "   <basis_vector_0 implicit = \"true\" />"
+            "   <basis_vector_1>e1</basis_vector_1>"
+            "   <basis_vector_2>e2</basis_vector_2>"
+            "</local_coordinate_system>";
+
+        testException<3>(xml, parameters, error_message_implicit_explicit_2D);
+
+        // For this case, if there is no exception for base1, i.e. with two
+        // components, an exception will be caused due to the existence of
+        // basis_vector_2. This exception has already been tested in
+        // CoordinateSystem.test2DBase2Exception.
+    }
 }
