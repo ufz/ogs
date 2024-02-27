8f00bab658c613c12e87d09d670ecc5dfe42f03d
Wenqing Wang <wenqing.wang@ufz.de>
2024-02-27 17:43:32 +0100

    [InitialStress] Added an argument to creator for mandatory iniital stress type

--- ProcessLib/Common/HydroMechanics/CreateInitialStress.cpp
+++ ProcessLib/Common/HydroMechanics/CreateInitialStress.cpp
@@ -25,7 +25,7 @@ template <int DisplacementDim>
 InitialStress createInitialStress(
     BaseLib::ConfigTree const& config,
     std::vector<std::unique_ptr<ParameterLib::ParameterBase>> const& parameters,
-    MeshLib::Mesh const& mesh)
+    MeshLib::Mesh const& mesh, bool const mandatory_stress_type)
 {
     auto const config_stress0 =
         //! \ogs_file_param{prj__processes__process__initial_stress}
@@ -36,9 +36,22 @@ InitialStress createInitialStress(
         return {};
     }
 
-    auto const stress0_type_str =
+    auto const stress0_type_opt =
         //! \ogs_file_attr{prj__processes__process__initial_stress__type}
-        config_stress0->getConfigAttribute<std::string>("type", "effective");
+        config_stress0->getConfigAttributeOptional<std::string>("type");
+
+    // If the type is mandatory required but it is not presented:
+    if (mandatory_stress_type && !stress0_type_opt)
+    {
+        OGS_FATAL(
+            "The attribute \"type\" of the tag \"initial_stress\" is required "
+            "for the current process setting, for example using subtype = "
+            "\"StressSaturation_StrainPressureTemperature\" in TRM. "
+            "However it is missing.");
+    }
+
+    auto const stress0_type_str =
+        stress0_type_opt ? *stress0_type_opt : "effective";
 
     InitialStress::Type stress0_type;
     if (stress0_type_str == "total")
@@ -69,10 +82,10 @@ InitialStress createInitialStress(
 template InitialStress createInitialStress<2>(
     BaseLib::ConfigTree const& config,
     std::vector<std::unique_ptr<ParameterLib::ParameterBase>> const& parameters,
-    MeshLib::Mesh const& mesh);
+    MeshLib::Mesh const& mesh, bool const mandatory_stress_type);
 template InitialStress createInitialStress<3>(
     BaseLib::ConfigTree const& config,
     std::vector<std::unique_ptr<ParameterLib::ParameterBase>> const& parameters,
-    MeshLib::Mesh const& mesh);
+    MeshLib::Mesh const& mesh, bool const mandatory_stress_type);
 
 };  // namespace ProcessLib
--- ProcessLib/Common/HydroMechanics/CreateInitialStress.h
+++ ProcessLib/Common/HydroMechanics/CreateInitialStress.h
@@ -36,14 +36,14 @@ template <int DisplacementDim>
 InitialStress createInitialStress(
     BaseLib::ConfigTree const& config,
     std::vector<std::unique_ptr<ParameterLib::ParameterBase>> const& parameters,
-    MeshLib::Mesh const& mesh);
+    MeshLib::Mesh const& mesh, bool const mandatory_stress_type = false);
 
 extern template InitialStress createInitialStress<2>(
     BaseLib::ConfigTree const& config,
     std::vector<std::unique_ptr<ParameterLib::ParameterBase>> const& parameters,
-    MeshLib::Mesh const& mesh);
+    MeshLib::Mesh const& mesh, bool const mandatory_stress_type);
 extern template InitialStress createInitialStress<3>(
     BaseLib::ConfigTree const& config,
     std::vector<std::unique_ptr<ParameterLib::ParameterBase>> const& parameters,
-    MeshLib::Mesh const& mesh);
+    MeshLib::Mesh const& mesh, bool const mandatory_stress_type);
 };  // namespace ProcessLib
