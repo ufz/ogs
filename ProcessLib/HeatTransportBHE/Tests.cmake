141da141c54487f09d32a414509106e569cf7f62
Haibing Shao <haibing.shao@ufz.de>
2024-03-25 15:11:39 +0100

    Add Test BeierSanbox for Newton
    use xml in the test config

--- ProcessLib/HeatTransportBHE/Tests.cmake
+++ ProcessLib/HeatTransportBHE/Tests.cmake
@@ -13,6 +13,20 @@ AddTest(
 )
 
 AddTest(
+    NAME HeatTransportBHE_1U_3D_beier_sandbox_Newton
+    PATH Parabolic/T/3D_Beier_sandbox
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS beier_sandbox_newton.xml
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    RUNTIME 20
+    DIFF_DATA
+    beier_sandbox_ts_10_t_600.000000.vtu beier_sandbox_newton_ts_10_t_600.000000.vtu temperature_BHE1 temperature_BHE1 0 5e-15
+    beier_sandbox_ts_10_t_600.000000.vtu beier_sandbox_newton_ts_10_t_600.000000.vtu temperature_soil temperature_soil 0 1e-13
+)
+
+AddTest(
     NAME HeatTransportBHE_1U_beier_sandbox_fixed_power_constant_flow
     PATH Parabolic/T/3D_Beier_sandbox
     RUNTIME 220
