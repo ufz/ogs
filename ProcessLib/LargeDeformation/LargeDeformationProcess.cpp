3557e292bd91b33c12c19219854c7f519bbd8fba
Christoph Lehmann <christoph.lehmann@ufz.de>
2024-04-23 17:07:26 +0200

    [PL/LD] Use generic cell average output

--- ProcessLib/LargeDeformation/LargeDeformationProcess.cpp
+++ ProcessLib/LargeDeformation/LargeDeformationProcess.cpp
@@ -207,6 +207,9 @@ void LargeDeformationProcess<DisplacementDim>::computeSecondaryVariableConcrete(
     GlobalExecutor::executeSelectedMemberOnDereferenced(
         &LocalAssemblerInterface::computeSecondaryVariable, _local_assemblers,
         pv.getActiveElementIDs(), dof_tables, t, dt, x, x_prev, process_id);
+
+    cell_average_data_.computeSecondaryVariable(DisplacementDim,
+                                                _local_assemblers);
 }
 template class LargeDeformationProcess<2>;
 template class LargeDeformationProcess<3>;
