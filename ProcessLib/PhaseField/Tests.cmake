baba6433840ba9ae14aef46bef26135c8c4b6080
Mostafa Mollaali <mostafa.mollaali@ufz.de>
2024-07-04 15:07:09 +0200

    Update meshes-beam benchmark: add a center element

--- ProcessLib/PhaseField/Tests.cmake
+++ ProcessLib/PhaseField/Tests.cmake
@@ -9,8 +9,8 @@ AddTest(
     REQUIREMENTS OGS_USE_MPI
     RUNTIME 18
     DIFF_DATA
-        expected_AT1_iso_tension_ts_10_t_1_000000_0.vtu AT1_iso_tension_ts_10_t_1.000000.vtu displacement displacement 1e-5 0
-        expected_AT1_iso_tension_ts_10_t_1_000000_0.vtu AT1_iso_tension_ts_10_t_1.000000.vtu phasefield phasefield 1e-6 0
+        expected_AT1_iso_tension_ts_10_t_1.000000.vtu AT1_iso_tension_ts_10_t_1.000000.vtu displacement displacement 1e-5 0
+        expected_AT1_iso_tension_ts_10_t_1.000000.vtu AT1_iso_tension_ts_10_t_1.000000.vtu phasefield phasefield 1e-6 0
 )
 
 AddTest(
@@ -24,8 +24,8 @@ AddTest(
     REQUIREMENTS OGS_USE_MPI
     RUNTIME 260
     DIFF_DATA
-        expected_AT2_iso_tension_ts_10_t_1_000000_0.vtu AT2_iso_tension_ts_10_t_1.000000.vtu displacement displacement 1e-5 0
-        expected_AT2_iso_tension_ts_10_t_1_000000_0.vtu AT2_iso_tension_ts_10_t_1.000000.vtu phasefield phasefield 1e-6 0
+        expected_AT2_iso_tension_ts_10_t_1.000000.vtu AT2_iso_tension_ts_10_t_1.000000.vtu displacement displacement 1e-5 0
+        expected_AT2_iso_tension_ts_10_t_1.000000.vtu AT2_iso_tension_ts_10_t_1.000000.vtu phasefield phasefield 1e-6 0
 )
 
 AddTest(
@@ -39,8 +39,8 @@ AddTest(
     REQUIREMENTS OGS_USE_MPI
     RUNTIME 18
     DIFF_DATA
-        expected_AT1_vd_tension_ts_10_t_1_000000_0.vtu AT1_vd_tension_ts_10_t_1.000000.vtu displacement displacement 1e-5 0
-        expected_AT1_vd_tension_ts_10_t_1_000000_0.vtu AT1_vd_tension_ts_10_t_1.000000.vtu phasefield phasefield 1e-6 0
+        expected_AT1_vd_tension_ts_10_t_1.000000.vtu AT1_vd_tension_ts_10_t_1.000000.vtu displacement displacement 1e-5 0
+        expected_AT1_vd_tension_ts_10_t_1.000000.vtu AT1_vd_tension_ts_10_t_1.000000.vtu phasefield phasefield 1e-6 0
 )
 
 AddTest(
