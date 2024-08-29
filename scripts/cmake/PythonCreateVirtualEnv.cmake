2e26f249e0f3974cd74f22acf8f11833b029d354
Lars Bilke <lars.bilke@ufz.de>
2024-08-29 09:49:04 +0200

    [py] Use uv over pip.
    https://docs.astral.sh/uv

--- scripts/cmake/PythonCreateVirtualEnv.cmake
+++ scripts/cmake/PythonCreateVirtualEnv.cmake
@@ -1,6 +1,4 @@
 # cmake-lint: disable=C0103
-message(STATUS "┌─ PythonCreateVirtualEnv.cmake")
-list(APPEND CMAKE_MESSAGE_INDENT "│    ")
 
 # Prefer more recent Python version
 set(Python_FIND_STRATEGY VERSION)
@@ -14,9 +12,21 @@ if(${Python_VERSION} VERSION_GREATER_EQUAL 3.9)
     set(_upgrade_deps --upgrade-deps)
 endif()
 
+if(UV_TOOL_PATH)
+    set(_create_venv_command ${UV_TOOL_PATH} venv --python ${Python_EXECUTABLE})
+    set(_venv_tool "uv")
+else()
+    set(_create_venv_command ${Python_EXECUTABLE} -m venv ${_upgrade_deps}
+                             .venv
+    )
+    set(_venv_tool "pip")
+endif()
+
+message(STATUS "┌─ PythonCreateVirtualEnv.cmake (using ${_venv_tool})")
+list(APPEND CMAKE_MESSAGE_INDENT "│    ")
+
 execute_process(
-    COMMAND ${Python_EXECUTABLE} -m venv ${_upgrade_deps} .venv
-    WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+    COMMAND ${_create_venv_command} WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
 )
 
 list(POP_BACK CMAKE_MESSAGE_INDENT)
