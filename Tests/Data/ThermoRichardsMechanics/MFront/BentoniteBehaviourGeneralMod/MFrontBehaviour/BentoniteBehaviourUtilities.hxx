837eadd036c5f13205e478bc887ac856dcacb45f
Christoph Lehmann <christoph.lehmann@ufz.de>
2024-08-29 07:53:08 +0200

    [T/TRM] Added bentonite MFront/GeneralMod behaviour

---
+++ Tests/Data/ThermoRichardsMechanics/MFront/BentoniteBehaviourGeneralMod/MFrontBehaviour/BentoniteBehaviourUtilities.hxx
@@ -0,0 +1,232 @@
+/*!
+ * \file   BentoniteBehaviourUtilities.hxx
+ * \brief  This header defines a set of auxiliary functions for the conversion
+ * of the gradients, the thermodynamic forces and the consistent tangent
+ * operator
+ * \author Thomas Helfer
+ * \date   20/04/2020
+ *
+ * \copyright
+ * Copyright (c) 2012-2024, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ */
+
+#ifndef LIB_TFEL_MATERIAL_BENTONITEBEHAVIOURUTILITIES_HXX
+#define LIB_TFEL_MATERIAL_BENTONITEBEHAVIOURUTILITIES_HXX
+
+#include "TFEL/Math/stensor.hxx"
+#include "TFEL/Math/tvector.hxx"
+
+namespace bentonite_behaviour_utilities
+{
+
+//! a simple alias
+using real = double;
+//! a simple alias
+using size_type = unsigned short;
+
+namespace internals
+{
+
+template <size_type N>
+struct NormalizeTangentOperator;
+
+template <>
+struct NormalizeTangentOperator<2u>
+{
+    template <typename Stensor4>
+    static void exe(Stensor4& Dt)
+    {
+        constexpr const auto cste = tfel::math::Cste<real>::sqrt2;
+        Dt(0, 3) *= cste;
+        Dt(1, 3) *= cste;
+        Dt(2, 3) *= cste;
+        Dt(3, 0) *= cste;
+        Dt(3, 1) *= cste;
+        Dt(3, 2) *= cste;
+        Dt(3, 3) *= 2;
+    }  // end of exe
+};  // end of struct NormalizeTangentOperator<2u>
+
+template <>
+struct NormalizeTangentOperator<3u>
+{
+    template <typename Stensor4>
+    static void exe(Stensor4& Dt)
+    {
+        constexpr const auto cste = tfel::math::Cste<real>::sqrt2;
+        Dt(0, 3) *= cste;
+        Dt(1, 3) *= cste;
+        Dt(2, 3) *= cste;
+        Dt(0, 4) *= cste;
+        Dt(1, 4) *= cste;
+        Dt(2, 4) *= cste;
+        Dt(0, 5) *= cste;
+        Dt(1, 5) *= cste;
+        Dt(2, 5) *= cste;
+        Dt(3, 0) *= cste;
+        Dt(3, 1) *= cste;
+        Dt(3, 2) *= cste;
+        Dt(4, 0) *= cste;
+        Dt(4, 1) *= cste;
+        Dt(4, 2) *= cste;
+        Dt(5, 0) *= cste;
+        Dt(5, 1) *= cste;
+        Dt(5, 2) *= cste;
+        Dt(3, 3) *= 2;
+        Dt(3, 4) *= 2;
+        Dt(3, 5) *= 2;
+        Dt(4, 3) *= 2;
+        Dt(4, 4) *= 2;
+        Dt(4, 5) *= 2;
+        Dt(5, 3) *= 2;
+        Dt(5, 4) *= 2;
+        Dt(5, 5) *= 2;
+    }  // end of exe
+};  // end of struct NormalizeTangentOperator<2u>
+
+}  // end of namespace internals
+
+/*!
+ * \brief convert the gradient from `MFront` and store them in an array
+ * that is to be passed to the Bentonite behaviour.
+ * This array will contain:
+ * [eps11, eps22, eps33, gamma12, gamma13, gamma23, s, T]
+ * \note One must take into account that `MFront` stores the strain as follows:
+ * [eps11, eps22, eps33, sqrt(2) * eps12, sqrt(2) * eps13, sqrt(2) * eps23]
+ * \note The temperature is not a gradient in the `MFront` sense
+ * \param[in] e: strain
+ * \param[in] p_L: liquid pressure
+ * \param[in] T: temperature
+ * \return an array containing the gradients for the Bentonite behaviour
+ */
+template <size_type N>
+tfel::math::tvector<8u, real> convert_gradients_from_mfront(
+    const tfel::math::stensor<N, real>& e, const real p_L, const real T)
+{
+    constexpr auto size = tfel::math::StensorDimeToSize<N>::value;
+    constexpr const auto cste = tfel::math::Cste<real>::sqrt2;
+    auto r = tfel::math::tvector<8u, real>{};
+    tfel::fsalgo::copy<size>::exe(e.begin(), r.begin());
+    tfel::fsalgo::fill<6u - size>::exe(r.begin() + e.size(), real(0));
+    for (size_type i = 3; i != e.size(); ++i)
+    {
+        r[i] *= cste;
+    }
+    r[6] = p_L / 1000.0; /* converting the the ogs pressure in Pa into kPa
+                            required by the MFront/generalmod */
+    r[7] = T;
+    return r;
+}  // end of convert_gradients_from_mfront
+
+/*!
+ * \brief convert the thermodynamic forces from `MFront` and store them in an
+ * array that is to be passed to the Bentonite behaviour. This array will
+ * contain: [signet11, signet22, signet33, signet12, signet13, signet23, Sr]
+ * \note One must take into account that `MFront` stores the stress as follows:
+ * [sig11, sig22, sig33, sqrt(2) * sig12, sqrt(2) * sig13, sqrt(2) * sig23]
+ * \param[in] sig: stress
+ * \param[in] Sr: saturation
+ * \return an array containing the thermodynamic forces for the Bentonite
+ * behaviour
+ */
+template <size_type N>
+tfel::math::tvector<7u, real> convert_thermodynamic_forces_from_mfront(
+    const tfel::math::stensor<N, real>& sig, const real Sr)
+{
+    constexpr auto size = tfel::math::StensorDimeToSize<N>::value;
+    constexpr const auto icste = tfel::math::Cste<real>::isqrt2;
+    auto r = tfel::math::tvector<7u, real>{};
+    tfel::fsalgo::copy<size>::exe(sig.begin(), r.begin());
+    tfel::fsalgo::fill<6u - size>::exe(r.begin() + sig.size(), real(0));
+    for (size_type i = 3; i != sig.size(); ++i)
+    {
+        r[i] *= icste;
+    }
+    for (size_type i = 0; i != sig.size(); ++i)
+    {
+        r[i] /= 1000.0; /* Pa to kPa conversion */
+    }
+    r[6] = Sr;
+    return r;
+}  // end of convert_gradients_from_mfront
+
+/*!
+ * \brief convert the thermodynamic forces computed for the Bentonite behaviour
+ * to `MFront`. \param[out] sig: stress \param[out] Sr: saturation \param[in]
+ * tf: the thermodynamic forces computed for the Bentonite behaviour
+ */
+template <size_type N>
+void convert_thermodynamic_forces_to_mfront(
+    tfel::math::stensor<N, real>& sig,
+    real& Sr,
+    const tfel::math::tvector<7u, real>& tf)
+{
+    constexpr auto size = tfel::math::StensorDimeToSize<N>::value;
+    constexpr const auto cste = tfel::math::Cste<real>::sqrt2;
+    tfel::fsalgo::copy<size>::exe(tf.begin(), sig.begin());
+    for (size_type i = 3; i != sig.size(); ++i)
+    {
+        sig[i] *= cste;
+    }
+    for (size_type i = 0; i != sig.size(); ++i)
+    {
+        sig[i] *= 1000.0; /* kPa to Pa conversion */
+    }
+    Sr = tf[6];
+}  // end of convert_gradients_from_mfront
+
+/*!
+ * \brief convert the derivative of the stress tensor with respect to the strain
+ * tensor to the `MFront` conventions. \tparam N: space dimension \tparam
+ * Stensor4: type of the fourth order tensor \param[in,out] Dt: fourth order
+ * tensor to be converted
+ */
+template <size_type N, typename Stensor4>
+void normalize_tangent_operator(Stensor4& Dt)
+{
+    internals::NormalizeTangentOperator<N>::exe(Dt);
+}  // end of normalize_tangent_operator
+
+/*!
+ * \brief convert the tangent operator returned by the Bentonite behaviour to
+ * the tangent operator blocks defined in `MFront` \tparam Stensor4: type of the
+ * fourth order tensor \tparam Stensor: type of the second order tensor
+ * \param[out] dsig_ddeto: derivative of the stress tensor with respect to the
+ * strain tensor \param[out] dsig_dds: derivative of the stress tensor with
+ * respect to the suction \param[out] dSr_dds: derivative of the saturation with
+ * respect to the suction \param[in] Dtg: tangent operator returned by the
+ * Bentonite behaviour
+ */
+template <typename Stensor4, typename Stensor>
+void convert_tangent_operator(Stensor4& dsig_ddeto,
+                              Stensor& dsig_ddp_L,
+                              real& dSr_ddp_L,
+                              const tfel::math::tmatrix<7, 8, real>& Dtg)
+{
+    constexpr const auto cste = tfel::math::Cste<real>::sqrt2;
+    constexpr const auto N = tfel::math::StensorTraits<Stensor>::dime;
+    for (size_type i = 0; i != tfel::math::StensorDimeToSize<N>::value; ++i)
+    {
+        for (size_type j = 0; j != tfel::math::StensorDimeToSize<N>::value; ++j)
+        {
+            dsig_ddeto(i, j) = 1000.0 * Dtg(i, j); /* kPa to Pa coversion */
+        }
+        dsig_ddp_L[i] = Dtg(i, 6);
+    }
+    normalize_tangent_operator<N>(dsig_ddeto);
+    // normalise the derivative of the stress tensor with respect to the suction
+    for (size_type i = 3; i != dsig_ddp_L.size(); ++i)
+    {
+        dsig_ddp_L[i] *= cste;
+    }
+    dSr_ddp_L =
+        Dtg(6, 6) /
+        1000.0; /* suction to liquid pressure, and kPa to Pa conversion */
+}  // end of convert_tangent_operator
+
+}  // end of namespace bentonite_behaviour_utilities
+
+#endif /* LIB_TFEL_MATERIAL_BENTONITEBEHAVIOURUTILITIES_HXX */
---
+++ Tests/Data/ThermoRichardsMechanics/MFront/BentoniteBehaviourGeneralMod/MFrontBehaviour/generalmod.h
@@ -0,0 +1,195 @@
+/*
+    Copyright (c) since 2003, David Masin (masin@natur.cuni.cz)
+
+    Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+    3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef GENERALMOD_H
+#define GENERALMOD_H
+
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include "errno.h"
+
+#ifndef max2
+ #define max2(a,b) ((a)>(b)?(a):(b))
+#endif
+
+#ifndef min2
+ #define min2(a,b) ((a)<(b)?(a):(b))
+#endif
+
+/*****************************General model, main structure***************************************/
+
+struct General_model {
+
+  //Numbers of variables
+  int nparms, nstatev, ngstrain, ngstress, voidrat_index, Sr_index;
+  static const int max_ngstrain = 8;
+  static const int max_ngstress = 7;
+  static const int max_nstatev  = 20;
+  static const int max_nparms   = 40;
+  static const int nrkf_parms   = 5;
+  static const int nrkf_statev  = 4;
+  bool debug;
+
+  General_model(int np, int ns, int ngstra, int ngstre, int vri, int Sri) : nparms(np), nstatev(ns), ngstrain(ngstra), ngstress(ngstre), voidrat_index(vri), Sr_index(Sri) {};
+
+  //Updated soil_model with Runge-Kutta schemes
+  virtual long soil_model(double strain_gen[], double stress_gen[], double qstatev[],
+        double dstrain_gen[], double dtime, double *DDtan_gen, double parms[], double rkf_statev[], double rkf_parms[], int flag, int kinc=0, int ipp=0) {return(0);};
+
+  virtual bool calc_fsigq(double signet[9], double suction, double Temper, double *qstatev,
+        double deps[9], double dsuction, double dTemper, double dtime,
+        double dsignet[9], double &Sr_new, double dqstatev[], int kinc) {return(0);};
+
+  //Functions for general stress-full 9x9 convention conversion
+  void convert_from_general(double general[], double stressstrain[], double othervar[], int nothv, int flag);
+  void convert_to_general(double general[], double stressstrain[], double othervar[], int nothv, int flag);
+  void convert4th_to_abaqus(double DDfull[3][3][3][3], double DDabq[6][6]);
+  void map_indices_fulltoabq(int fulli, int fullj, int &abq);
+  void map_indices_abqtofull(int &fulli, int &fullj, int abq);
+
+  //Functions for mathematical operations
+  double gscalar_power(double a, double b);
+  double gscalar_dabs(double k);
+  void garray_set(double *ptr, double value, long int n);
+  double garray_size(double a[], long int n);
+  void garray_move(double from[], double to[], long int n);
+  int garray_copy (double source[], double destination[], int length);
+  int garray_rcopy (double source[], int source_index, double destination[], int destination_index, int number_of_elements);
+  void garray_add(double a[], double b[], double c[], long int n);
+  void garray_subtract (double a[], double b[], double c[], long int n);
+  void garray_multiply(double a[], double b[], double c, long int n);
+  int garray_addmult (double source1[], double source2[], double multiplier, double destination[], int length);
+  int garray_swap (double a[], double b[], int length);
+  double garray_snorm(double a[], long fi, long nc);
+  double garray_inproduct(double a[], double b[], long int n);
+  double gtrace(double a[3][3]);
+  double gmatrix_determinant(double a[], long int n);
+  void gmatrix_ab(double *a, double *b, double *c, long int n, long int m, long int k);
+  void gmatrix_a4b(double a[3][3][3][3],  double b[], double c[]);
+  double tensor_dbldot_prod (double a[], double b[], double k);
+  double round_to_digits(double value, int digits);
+  long test_math_err();
+
+  //Runge-Kutta schemes (functions adapted from SIFEL)
+  long calc_rkf(double strain_gen[], double stress_gen[], double qstatev[], double dstrain_gen[], double dtime,
+          double parms[], double rkf_statev[], double rkf_parms[], int kinc);
+  long adfwdeuler (double strain_gen[], double stress_gen[], double qstatev[], double dstrain_gen[], double dtime,
+          double parms[], double rkf_statev[], double rkf_parms[], int kinc);
+  long rkf23 (double strain_gen[], double stress_gen[], double qstatev[], double dstrain_gen[], double dtime,
+          double parms[], double rkf_statev[], double rkf_parms[], int kinc);
+  long rkf23bs (double strain_gen[], double stress_gen[], double qstatev[], double dstrain_gen[], double dtime,
+          double parms[], double rkf_statev[], double rkf_parms[], int kinc);
+  long rkf34 (double strain_gen[], double stress_gen[], double qstatev[], double dstrain_gen[], double dtime,
+          double parms[], double rkf_statev[], double rkf_parms[], int kinc);
+  long rkf45 (double strain_gen[], double stress_gen[], double qstatev[], double dstrain_gen[], double dtime,
+          double parms[], double rkf_statev[], double rkf_parms[], int kinc);
+  long fwdeulerfsub (double strain_gen[], double stress_gen[], double qstatev[], double dstrain_gen[], double dtime,
+          double parms[], double rkf_statev[], double rkf_parms[], int kinc);
+
+  //Additional routines
+  long rkf_redstep (double rc, double &h, double hmin);
+  long check_math_error();
+  virtual long correct_statev_values(double strain_gen[], double stress_gen[], double qstatev[], double dstrain_gen[], int call) {return(0);};
+  virtual long correct_DDtan_gen(double strain_gen[], double stress_gen[], double *DDtan_gen) {return(0);};
+};
+
+/****************** Hypoplasticity for unsaturated soils, expansive soils with *******************/
+/****************** double-porosity structure, thermal effects. Updated version ***********************************/
+
+struct Hypoplasti_unsat_expansive_thermal : General_model {
+  Hypoplasti_unsat_expansive_thermal() : General_model(c_nparms, c_nstatev, c_ngstrain, c_ngstress, c_voidrat_index, c_Sr_index){};
+
+  static const int c_ngstrain = 8;
+  static const int c_ngstress = 7;
+  static const int c_nstatev  = 9;
+  static const int c_nparms   = 23;
+  static const int c_voidrat_index = 0;
+  static const int c_Sr_index = 2;
+  static const bool debug     = false;
+
+  //Model parameters and other constants
+  double phic, lambda, kappa, N, nuparam, Ocparam;
+  double nparam, lparam, nTparam, lTparam, mparam;
+  double alpha_s, kappam, smstar, emstar, mmparam, csh;
+  double sairentry0, eM0, Trparam, aTparam, bTparam;
+  double lambdap0, aer, pt, alphaG, alphanu, alphaE;
+  double rlambdascan;
+
+  //Updated soil_model with Runge-Kutta schemes
+  long soil_model(double strain_gen[], double stress_gen[], double qstatev[],
+        double dstrain_gen[], double dtime, double *DDtan_gen, double parms[], double rkf_statev[], double rkf_parms[], int flag, int kinc=0, int ipp=0);
+
+  /* functions which distinguish original and updated versions */
+  virtual double give_rlambda(double suction, double a_scan, double dsuction, double sewM, double SrM, double& rlambda_for_ascan, double& rlambda_for_se);
+  virtual void give_fm(double& fm, double& fm_lambda_act, double suction, double sewM, double re, bool swelling);
+  virtual double give_demdpm(double pefsat, double em);
+  virtual double init_em_pefsat(double pefsat);
+
+  //Model-specific routines
+  void initialise_variables(double strain_gen[], double stress_gen[], double qstatev[], double dstrain_gen[], double dtime,
+          double parms[], int kinc);
+  void initialise_parameters(double parms[]);
+  bool calc_fsigq(double signet[9], double suction, double Temper, double *qstatev,
+        double deps[9], double dsuction, double dTemper, double dtime,
+        double dsignet[9], double &Sr_new, double dqstatev[], int kinc);
+  void update_hisv(double signet[9], double suction, double Temper, double *qstatev,
+        double deps[9], double dsuction, double dTemper,
+        double dsignet[9], double &Sr, double *dqstatev, int flag, double fu, int kinc);
+  void make_sig_unsat(double sig[3*3], double suction, double SrM,
+        double tensor_unsat[3*3], double &scalar_unsat);
+  double calc_trdepm(double signet[9], double suction, double Temper, double em, double dsuction,
+        double dTemper, double dsignet[9], double fu);
+  void calc_N_lambda_dpeds(double &Nuse, double &lambda_use, double &dpeds_divpe, double &dpedT_divpe,
+        double signet[9], double suction, double Temper, double *qstatev);
+  void give_LNH(double signet[9], double dsuction, double Temper, double qstatev[],
+        double hypo_L[3][3][3][3], double hypo_N[3][3], double H_unsat[3][3], double HT_unsat[3][3], bool swelling,
+        double &fu);
+  void direct_stiffness_matrix(double strain_gen[], double stress_gen[], double qstatev[], double dstrain_gen[], double dtime, double *DDtan_gen, double parms[], int kinc, int flag);
+  double give_SrM_new(double suction, double SrM, double eM, double Temper, double sewM, double se, double dsuction, double deM, double dTemper, double a_scan, int flag);
+
+  //Additional routines
+  long correct_statev_values(double strain_gen[], double stress_gen[], double qstatev[], double dstrain_gen[], int call);
+  long correct_DDtan_gen(double strain_gen[], double stress_gen[], double *DDtan_gen);
+};
+
+/****************** Hypoplasticity for unsaturated soils, expansive soils with *******************/
+/****************** double-porosity structure, thermal effects. ***********************************/
+/* Version "Mašín, D. (2017). Coupled thermohydromechanical double structure model for expansive soils. ASCE Journal of Engineering Mechanics 143, No. 9."
+*/
+
+struct Hypoplasti_unsat_expansive_thermal_2017 : Hypoplasti_unsat_expansive_thermal {
+  Hypoplasti_unsat_expansive_thermal_2017() : Hypoplasti_unsat_expansive_thermal(){};
+
+  /* functions which distinguish original and updated versions */
+  virtual double give_rlambda(double suction, double a_scan, double dsuction, double sewM, double SrM, double& rlambda_for_ascan, double& rlambda_for_se);
+  virtual void give_fm(double& fm, double& fm_lambda_act, double suction, double sewM, double re, bool swelling);
+};
+
+/****************** Hypoplasticity for unsaturated soils, expansive soils with *******************/
+/****************** double-porosity structure, thermal effects. ***********************************/
+/* Version "Svoboda, J., Mašín, D., Najser, J. Vašíček, R., Hanusová, I. and Hausmannová, L. (2022). BCV bentonite hydromechanical behaviour and modelling. Acta Geotechnica (in print).
+"
+*/
+
+struct Hypoplasti_unsat_expansive_thermal_2022 : Hypoplasti_unsat_expansive_thermal {
+  Hypoplasti_unsat_expansive_thermal_2022() : Hypoplasti_unsat_expansive_thermal(){};
+
+  /* functions which distinguish original and updated versions */
+  virtual double give_rlambda(double suction, double a_scan, double dsuction, double sewM, double SrM, double& rlambda_for_ascan, double& rlambda_for_se);
+  virtual void give_fm(double& fm, double& fm_lambda_act, double suction, double sewM, double re, bool swelling);
+};
+
+
+#endif
