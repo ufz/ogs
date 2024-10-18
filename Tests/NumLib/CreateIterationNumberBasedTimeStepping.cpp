40ee4a1f29aefc7f4ed71ac24c7f8718638b9c69
Tom Fischer <thomas.fischer@ufz.de>
2024-10-18 09:56:02 +0200

    [T] Add simple test for NumLib::createIterationNumberBasedTimeStepping()

---
+++ Tests/NumLib/CreateIterationNumberBasedTimeStepping.cpp
@@ -0,0 +1,30 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2024, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#include "NumLib/TimeStepping/Algorithms/CreateIterationNumberBasedTimeStepping.h"
+
+#include <gtest/gtest.h>
+
+#include <numeric>
+
+#include "NumLib/TimeStepping/Algorithms/TimeStepAlgorithm.h"
+
+TEST(NumLibCreateIterationNumberBasedTimeStepping,
+     InitialTimeGreaterThanEndTime)
+{
+    double const t_initial = 1;
+    double const t_end = 0;
+
+    NumLib::IterationNumberBasedTimeSteppingParameters parameters{
+        t_initial, t_end, 0, 0, 0, {}, {}};
+    EXPECT_ANY_THROW(auto fixed_time_step_algorithm =
+                         NumLib::createIterationNumberBasedTimeStepping(
+                             std::move(parameters), {}));
+}
