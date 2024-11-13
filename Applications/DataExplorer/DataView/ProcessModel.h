e706bff977da321c06e676a9a0bb56b8684b5997
Dmitri Naumov <dmitri.naumov@ufz.de>
2024-11-13 11:17:13 +0100

    Fix copyright headers

--- Applications/DataExplorer/DataView/ProcessModel.h
+++ Applications/DataExplorer/DataView/ProcessModel.h
@@ -5,11 +5,10 @@
  * \brief  Definition of the ProcessModel class.
  *
  * \copyright
- * Copyright (c) 2013, OpenGeoSys Community (http://www.opengeosys.org)
+ * Copyright (c) 2012-2024, OpenGeoSys Community (http://www.opengeosys.org)
  *            Distributed under a Modified BSD License.
  *              See accompanying file LICENSE.txt or
  *              http://www.opengeosys.org/project/license
- *
  */
 
 #pragma once
