96a8c8792ff21c5dcfe8aade978c5f53575111d7
Lars Bilke <lars.bilke@ufz.de>
2025-01-15 16:30:08 +0100

    [guix] Update channels to use upstream petsc-openmpi-package.
    Also adapted MKL find logic to not require pkg-config when GUIX_BUILD.
    
    https://gitlab.opengeosys.org/ogs/inf/guix-ogs/-/commit/e73d22eea4e26c657b4bfe6cd29cda31a9827768
    https://codeberg.org/guix-science/guix-science-nonfree/pulls/40

--- scripts/guix/channels.scm
+++ scripts/guix/channels.scm
@@ -2,13 +2,13 @@
         (name 'guix-ogs)
         (url "https://gitlab.opengeosys.org/ogs/inf/guix-ogs.git")
         (branch "master")
-        (commit "901d7522bfdb710814f26f7858fb55f57769371f"))
+        (commit "e73d22eea4e26c657b4bfe6cd29cda31a9827768"))
       (channel
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "31fe177a97bacec643180cc5bcf8805a6cb07481")
+          "522732d5c15e44fc9e061f36a41f7129edfee66f")
         (introduction
           (make-channel-introduction
             "cdf1d7dded027019f0ebbd5d6f0147b13dfdd28d"
