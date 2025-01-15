7bf6ed46c8386cf809bdb3adf1b820c7f98a478b
Lars Bilke <lars.bilke@ufz.de>
2025-01-15 17:22:28 +0100

    [guix] Pin dependent channels.

--- scripts/guix/channels.scm
+++ scripts/guix/channels.scm
@@ -4,6 +4,23 @@
         (branch "master")
         (commit "e73d22eea4e26c657b4bfe6cd29cda31a9827768"))
       (channel
+        (name 'guix-science-nonfree)
+        (url "https://codeberg.org/guix-science/guix-science-nonfree.git")
+        (commit "7bb0689a9b02b96b75a527f43a6806872f1227cb")
+        (introduction
+         (make-channel-introduction "58661b110325fd5d9b40e6f0177cc486a615817e"
+          (openpgp-fingerprint
+           "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
+      (channel
+        (name 'guix-science)
+        (url "https://codeberg.org/guix-science/guix-science.git")
+        (commit "0d6b15ba682968ee89b46da39a92f261f0e6299b")
+        (introduction
+         (make-channel-introduction
+          "b1fe5aaff3ab48e798a4cce02f0212bc91f423dc"
+          (openpgp-fingerprint
+           "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
+      (channel
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
