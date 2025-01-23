17a96c89b6861f91a11bc6573ff23c2692c436ef
Dmitri Naumov <dmitri-jurievic.naumov@ifgt.tu-freiberg.de>
2025-01-23 23:38:40 +0100

    [ParL] Replace optionals with a bitset
    Saves 16 bytes; one bitset (8) vs three optionals (24).
    Remove only internally used clear() function from I/F.

--- ParameterLib/SpatialPosition.h
+++ ParameterLib/SpatialPosition.h
@@ -10,6 +10,7 @@
 
 #pragma once
 
+#include <bitset>
 #include <optional>
 
 #include "MathLib/Point3d.h"
@@ -31,54 +32,77 @@ public:
     SpatialPosition(std::optional<std::size_t> const& node_id,
                     std::optional<std::size_t> const& element_id,
                     std::optional<MathLib::Point3d> const& coordinates)
-        : _node_id(node_id), _element_id(element_id), _coordinates(coordinates)
     {
+        setAll(node_id, element_id, coordinates);
     }
 
-    std::optional<std::size_t> getNodeID() const { return _node_id; }
-    std::optional<std::size_t> getElementID() const { return _element_id; }
-    std::optional<MathLib::Point3d> const& getCoordinates() const
+    std::optional<std::size_t> getNodeID() const
     {
-        return _coordinates;
+        return flags[node_bit] ? std::make_optional(_node_id) : std::nullopt;
+    }
+    std::optional<std::size_t> getElementID() const
+    {
+        return flags[element_bit] ? std::make_optional(_element_id)
+                                  : std::nullopt;
+    }
+    std::optional<MathLib::Point3d> const getCoordinates() const
+    {
+        return flags[coordinates_bit] ? std::make_optional(_coordinates)
+                                      : std::nullopt;
     }
 
     void setNodeID(std::size_t node_id)
     {
-        clear();
+        flags.reset();
+        flags.set(node_bit);
         _node_id = node_id;
     }
 
     void setElementID(std::size_t element_id)
     {
-        clear();
+        flags.reset();
+        flags.set(element_bit);
         _element_id = element_id;
     }
 
     void setCoordinates(MathLib::Point3d const& coordinates)
     {
         _coordinates = coordinates;
+        flags.set(coordinates_bit);
     }
 
     void setAll(std::optional<std::size_t> const& node_id,
                 std::optional<std::size_t> const& element_id,
                 std::optional<MathLib::Point3d> const& coordinates)
     {
-        _node_id = node_id;
-        _element_id = element_id;
-        _coordinates = coordinates;
-    }
+        flags.reset();
+        if (node_id)
+        {
+            _node_id = *node_id;
+            flags.set(node_bit);
+        }
 
-    void clear()
-    {
-        _node_id = std::nullopt;
-        _element_id = std::nullopt;
-        _coordinates = std::nullopt;
+        if (element_id)
+        {
+            _element_id = *element_id;
+            flags.set(element_bit);
+        }
+        if (coordinates)
+        {
+            _coordinates = *coordinates;
+            flags.set(coordinates_bit);
+        }
     }
 
 private:
-    std::optional<std::size_t> _node_id;
-    std::optional<std::size_t> _element_id;
-    std::optional<MathLib::Point3d> _coordinates;
+    std::size_t _node_id = 0;
+    std::size_t _element_id = 0;
+    MathLib::Point3d _coordinates{};
+
+    std::bitset<3> flags{};
+    static constexpr std::size_t node_bit = 0;
+    static constexpr std::size_t element_bit = 1;
+    static constexpr std::size_t coordinates_bit = 2;
 };
 
 }  // namespace ParameterLib
