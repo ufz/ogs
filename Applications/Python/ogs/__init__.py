1e35494100dc9e9de205372bd89f6c5794ffc32d
Lars Bilke <lars.bilke@ufz.de>
2025-02-06 13:02:23 +0100

    [ci] Fixing linux wheel.

--- Applications/Python/ogs/__init__.py
+++ Applications/Python/ogs/__init__.py
@@ -1,13 +1,9 @@
 import importlib.util
-import os
 
 from ._internal.wrap_cli_tools import cli  # noqa: F401
 
-if "PEP517_BUILD_BACKEND" in os.environ:
-    # on wheel build config.py is not generated at the beginning of the build
-    # but later after CMake has run
-    config_spec = importlib.util.find_spec(".config", package="ogs")
-    if config_spec is not None:
-        from .config import *  # noqa: F403
-else:
+# on wheel build config.py is not generated at the beginning of the build
+# but later after CMake has run
+config_spec = importlib.util.find_spec(".config", package="ogs")
+if config_spec is not None:
     from .config import *  # noqa: F403
