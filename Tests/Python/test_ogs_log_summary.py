c7da727b64854a0a804e550787305f88759977ba
Christoph Lehmann <christoph.lehmann@ufz.de>
2025-04-03 07:57:27 +0200

    [T] Deduplicated code

--- Tests/Python/test_ogs_log_summary.py
+++ Tests/Python/test_ogs_log_summary.py
@@ -8,24 +8,23 @@ from ogs.dev.ogs_log_summary import aggregate_log_files, run
 
 res_path = Path(__file__).parent / "res/test_ogs_log_summary"
 
+TEST_CASES = [
+    (
+        "arch-ci-job-ogs-ThermoRichardsMechanics_MFront_BentoniteBehaviourGeneralMod230824_02-ramp_bentonite_ramped_Neumann_BC.txt",
+        "ThermoRichardsMechanics/MFront/BentoniteBehaviourGeneralMod230824/02-ramp/bentonite_ramped_Neumann_BC.prj",
+    ),
+    (
+        "petsc-ci-job-ogs-ThermoRichardsMechanics_MFront_BentoniteBehaviourGeneralMod230824_02-ramp_bentonite_ramped_Neumann_BC.txt",
+        "ThermoRichardsMechanics/MFront/BentoniteBehaviourGeneralMod230824/02-ramp/bentonite_ramped_Neumann_BC.prj",
+    ),
+    (
+        "windows-ogs-HydroMechanics_StaggeredScheme_ConsolidationBenchmark_consolidation_benchmark-omp.txt",
+        "HydroMechanics/StaggeredScheme/ConsolidationBenchmark/consolidation_benchmark.prj",
+    ),
+]
 
-@pytest.mark.parametrize(
-    ("log_file", "prj_file_expected"),
-    [
-        (
-            "arch-ci-job-ogs-ThermoRichardsMechanics_MFront_BentoniteBehaviourGeneralMod230824_02-ramp_bentonite_ramped_Neumann_BC.txt",
-            "ThermoRichardsMechanics/MFront/BentoniteBehaviourGeneralMod230824/02-ramp/bentonite_ramped_Neumann_BC.prj",
-        ),
-        (
-            "petsc-ci-job-ogs-ThermoRichardsMechanics_MFront_BentoniteBehaviourGeneralMod230824_02-ramp_bentonite_ramped_Neumann_BC.txt",
-            "ThermoRichardsMechanics/MFront/BentoniteBehaviourGeneralMod230824/02-ramp/bentonite_ramped_Neumann_BC.prj",
-        ),
-        (
-            "windows-ogs-HydroMechanics_StaggeredScheme_ConsolidationBenchmark_consolidation_benchmark-omp.txt",
-            "HydroMechanics/StaggeredScheme/ConsolidationBenchmark/consolidation_benchmark.prj",
-        ),
-    ],
-)
+
+@pytest.mark.parametrize(("log_file", "prj_file_expected"), TEST_CASES)
 def test_aggregation(log_file, prj_file_expected):
     log_path = res_path / log_file
 
@@ -50,25 +49,8 @@ def test_aggregation(log_file, prj_file_expected):
     ).all()  # the "maximum" file name must be the same for all fields
 
 
-@pytest.mark.parametrize(
-    ("log_file", "snippet_file_expected"),
-    [
-        (
-            "arch-ci-job-ogs-ThermoRichardsMechanics_MFront_BentoniteBehaviourGeneralMod230824_02-ramp_bentonite_ramped_Neumann_BC.txt",
-            "ThermoRichardsMechanics/MFront/BentoniteBehaviourGeneralMod230824/02-ramp/bentonite_ramped_Neumann_BC.prj",
-        ),
-        (
-            "petsc-ci-job-ogs-ThermoRichardsMechanics_MFront_BentoniteBehaviourGeneralMod230824_02-ramp_bentonite_ramped_Neumann_BC.txt",
-            "ThermoRichardsMechanics/MFront/BentoniteBehaviourGeneralMod230824/02-ramp/bentonite_ramped_Neumann_BC.prj",
-        ),
-        (
-            "windows-ogs-HydroMechanics_StaggeredScheme_ConsolidationBenchmark_consolidation_benchmark-omp.txt",
-            "HydroMechanics/StaggeredScheme/ConsolidationBenchmark/consolidation_benchmark.prj",
-        ),
-    ],
-)
+@pytest.mark.parametrize(("log_file", "snippet_file_expected"), TEST_CASES)
 def test_xml_snippet(tmp_path, log_file, snippet_file_expected):
-    # Note: this test uses pytest's tmp_path fixture, cf. https://docs.pytest.org/en/7.1.x/how-to/tmp_path.html
     log_path = res_path / log_file
 
     run([log_path], tmp_path, snippet_out=True, csv_out=False, verbose=False)
