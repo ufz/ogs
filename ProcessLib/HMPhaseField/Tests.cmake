19761334211475237c019861f305883e07e8db2a
MostafaMollaali <m.molaali@gmail.com>
2025-06-05 09:09:57 +0200

    Adding GreatCell JN for HMPF process

--- ProcessLib/HMPhaseField/Tests.cmake
+++ ProcessLib/HMPhaseField/Tests.cmake
@@ -164,3 +164,7 @@ AddTest(
         expected_KGD_4core_cohesive_linear_softening_ts_5_t_200_000000_2.vtu KGD_4core_cohesive_linear_softening_ts_5_t_200_000000_2.vtu pressure pressure 1e-8 1e-5
         expected_KGD_4core_cohesive_linear_softening_ts_5_t_200_000000_3.vtu KGD_4core_cohesive_linear_softening_ts_5_t_200_000000_3.vtu pressure pressure 1e-8 1e-5
 )
+
+if( OGS_USE_PETSC)
+    NotebookTest(NOTEBOOKFILE HMPhaseField/GreatCell/GreatCell.py RUNTIME 600)
+endif()
