aba10c81cc421f66e2fc4c4a7383f11f2ff4c078
Tom Fischer <thomas.fischer@ufz.de>
2025-06-18 10:57:33 +0200

    [PL/TimeLoop] Additional scope isn't necessary

--- ProcessLib/TimeLoop.cpp
+++ ProcessLib/TimeLoop.cpp
@@ -692,16 +692,13 @@ TimeLoop::solveCoupledEquationSystemsByStaggeredScheme(
 
     _last_step_rejected = nonlinear_solver_status.error_norms_met;
 
+    for (auto const& process_data : _per_process_data)
     {
-        for (auto const& process_data : _per_process_data)
-        {
-            auto& pcs = process_data->process;
-            int const process_id = process_data->process_id;
-            auto& ode_sys = *process_data->tdisc_ode_sys;
-            pcs.solveReactionEquation(_process_solutions,
-                                      _process_solutions_prev, t(), dt, ode_sys,
-                                      process_id);
-        }
+        auto& pcs = process_data->process;
+        int const process_id = process_data->process_id;
+        auto& ode_sys = *process_data->tdisc_ode_sys;
+        pcs.solveReactionEquation(_process_solutions, _process_solutions_prev,
+                                  t(), dt, ode_sys, process_id);
     }
 
     return nonlinear_solver_status;
