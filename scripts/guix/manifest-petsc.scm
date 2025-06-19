76d8a0c56ce518a6c6bd27fa72aa9ac20b8a1a46
Lars Bilke <lars.bilke@ufz.de>
2025-06-19 11:29:24 +0200

    [guix] Added manifests for serial and petsc build.

---
+++ scripts/guix/manifest-petsc.scm
@@ -0,0 +1,21 @@
+(use-modules (guix transformations)
+             (guix utils)
+             (gnu packages base))
+
+(define current-dir (getcwd))
+
+(define transform1
+  (options->transformation
+    `((with-c-toolchain . "ogs-petsc=gcc-toolchain@13.3.0")
+      (with-source . ,(string-append "ogs-petsc=" current-dir))
+      (with-commit . "eigen=9000b3767770f6dd0f4cfb12f4e19c71921885a4")
+      (without-tests . "eigen")
+      (with-configure-flag . "vtk=-DVTK_MODULE_USE_EXTERNAL_VTK_eigen=OFF")
+      (with-source . "vtk=https://www.vtk.org/files/release/9.3/VTK-9.3.1.tar.gz")
+      (with-input ."openmpi=openmpi@4.1.6"))))
+
+(packages->manifest
+ (list
+  (transform1 (specification->package "ogs-petsc"))
+  (specification->package "coreutils")
+  (specification->package "bash"))) ; required for squashfs container image
