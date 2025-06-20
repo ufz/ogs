d467e3845eef3658c41f698623a88259e3ad357a
Dmitri Naumov <dmitri-jurievic.naumov@ifgt.tu-freiberg.de>
2025-06-20 17:51:16 +0200

    fix(MeL): Check face id validity and error message
    When the input mesh has degenerate elements or other
    topological issues, even running checkMesh on such a
    mesh is not possible and resulted in a segv.
    Error message should guide the user to incorrect elements
    and nodes used in the neighbour identification.

--- MeshLib/Elements/Element.cpp
+++ MeshLib/Elements/Element.cpp
@@ -68,8 +68,28 @@ std::optional<unsigned> Element::addNeighbor(Element* e)
                 // similar to be sure e is a neighbour of this
                 if ((++count) >= dim)
                 {
-                    _neighbors[this->identifyFace(face_nodes)] = e;
-                    return std::optional<unsigned>(e->identifyFace(face_nodes));
+                    auto const this_face_id = this->identifyFace(face_nodes);
+                    if (this_face_id == std::numeric_limits<unsigned>::max())
+                    {
+                        OGS_FATAL(
+                            "Could not find face of {} for the face nodes [{}, "
+                            "{}, {}].",
+                            *this, *face_nodes[0], *face_nodes[1],
+                            *face_nodes[2]);
+                    }
+                    _neighbors[this_face_id] = e;
+                    auto const neighbors_face_id = e->identifyFace(face_nodes);
+                    if (neighbors_face_id ==
+                        std::numeric_limits<unsigned>::max())
+                    {
+                        OGS_FATAL(
+                            "Could not find face of the neighbour {} for the "
+                            "face nodes [{}, {}, {}].\n"
+                            "For the {} the face id {} was identified.",
+                            *e, *face_nodes[0], *face_nodes[1], *face_nodes[2],
+                            *this, this_face_id);
+                    }
+                    return neighbors_face_id;
                 }
             }
         }
