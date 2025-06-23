34f3210bdd40e86ffdf0f7fe1f3f36b0b57e7b11
Lars Bilke <lars.bilke@ufz.de>
2025-06-23 12:48:27 +0200

    [guix] Added manifests for mkl and petsc-mkl.

---
+++ scripts/guix/manifest-mkl.scm
@@ -0,0 +1,19 @@
+(use-modules (guix transformations)
+             (guix utils)
+             (gnu packages base))
+
+(define current-dir
+  (getcwd))
+
+(define transform1
+  (options->transformation `((with-source unquote
+                                          (string-append "ogs-serial="
+                                                         current-dir))
+                             (with-commit . "eigen=9000b3767770f6dd0f4cfb12f4e19c71921885a4")
+                             (without-tests . "eigen")
+                             (with-configure-flag . "vtk=-DVTK_MODULE_USE_EXTERNAL_VTK_eigen=OFF")
+                             (with-source . "vtk=https://www.vtk.org/files/release/9.3/VTK-9.3.1.tar.gz"))))
+
+(packages->manifest (list (transform1 (specification->package "ogs-mkl"))
+                          (specification->package "coreutils")
+                          (specification->package "bash")))
