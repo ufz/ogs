0925718e263286b3f4ab4915fa36863a5dba0c4e
Lars Bilke <lars.bilke@ufz.de>
2025-06-23 12:38:42 +0200

    [guix] Toolchain definition is now in ogs package.

--- scripts/guix/manifest.scm
+++ scripts/guix/manifest.scm
@@ -6,8 +6,7 @@
 
 (define transform1
   (options->transformation
-    `((with-c-toolchain . "ogs-serial=gcc-toolchain@13.3.0")
-      (with-source . ,(string-append "ogs-serial=" current-dir))
+    `((with-source . ,(string-append "ogs-serial=" current-dir))
       (with-commit . "eigen=9000b3767770f6dd0f4cfb12f4e19c71921885a4")
       (without-tests . "eigen")
       (with-configure-flag . "vtk=-DVTK_MODULE_USE_EXTERNAL_VTK_eigen=OFF")
