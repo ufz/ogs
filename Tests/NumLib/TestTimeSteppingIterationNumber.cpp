70b1f1d5213a5678393ea773b1c7a268ea4359d6
Tom Fischer <thomas.fischer@ufz.de>
2025-07-11 12:43:55 +0200

    [T/NL] Extract test from Sandwich_0c model

--- Tests/NumLib/TestTimeSteppingIterationNumber.cpp
+++ Tests/NumLib/TestTimeSteppingIterationNumber.cpp
@@ -443,3 +443,95 @@ TEST(NumLib, TimeSteppingIterationNumberBased_simple2)
     ASSERT_ARRAY_NEAR(expected_vec_t, vec_t, expected_vec_t.size(),
                       std::numeric_limits<double>::epsilon());
 }
+
+// extracted from Sandwich-0c-Aqeel-R5DeCoT68
+TEST(NumLib, TimeSteppingIterationNumberBasedSandwich_0c)
+{
+    // *** initialization of IterationNumberBaseTimeStepping object
+    auto iter_times_vector =
+        ranges::views::iota(1, 21) | ranges::to<std::vector>;
+
+    std::vector<double> multiplier_vector = {
+        5,      4.25,   3.5,    2.75,   2,      1.6333, 1.2667,
+        0.9,    0.8333, 0.7667, 0.7,    0.6333, 0.5667, 0.5,
+        0.4333, 0.3667, 0.3,    0.2333, 0.1667, 0.1};
+
+    std::vector<double> fixed_output_times = {
+        0,       432000,  440640,  950400,  959040,  3024000, 3032640,
+        5961600, 6480000, 7689600, 7776000, 7819200, 12528000};
+
+    NumLib::MultiplyerInterpolationType const multiplier_interpolation_type =
+        NumLib::MultiplyerInterpolationType::PiecewiseConstant;
+    // *** original data
+    double const t_initial = 0.0;
+    double const t_end = 12528000.0;
+    double const min_dt = 0.001;
+    double const max_dt = 432000;
+    double const initial_dt = 120;
+    NumLib::IterationNumberBasedTimeStepping alg(
+        t_initial, t_end, min_dt, max_dt, initial_dt,
+        multiplier_interpolation_type, std::move(iter_times_vector),
+        std::move(multiplier_vector), std::move(fixed_output_times));
+    // *** end initialization of IterationNumberBaseTimeStepping object
+
+    std::vector<int> const rejected_steps = {6, 8, 10, 12, 14, 14, 24};
+
+    std::vector<int> const nr_iterations = {
+        0,  4, 3, 9, 3, 5, 5,  // <- time step 6 rejected
+        3,                     // <- time step 6 repeated
+        3,  8,                 // <- time step 8 rejected
+        10,                    // <- time step 8 repeated
+        4,  4,                 // <- time step 10 rejected
+        3,                     // <- time step 10 repeated
+        3,  6,                 // <- time step 12 rejected
+        3,                     // <- time step 12 repeated
+        4,  8,                 // <- time step 14 rejected
+        17,                    // <- time step 14 rejected again
+        3,                     // <- time step 14 repeated (and accepted)
+        9,  4, 5, 5, 4, 3, 3, 3, 3, 11,  // <- time step 24 rejected
+        3                                // <- time step 24 repeated
+    };
+
+    // current timestep sizes:
+    std::vector<double> const expected_vec_t = {
+        0,
+        120,
+        450,
+        1605,
+        2567.4614999999999,
+        5936.0767500000002,
+        12673.30725,         // ts 6 rejected
+        6609.7998000000007,  // ts 6 repeated
+        8967.8304749999988,
+        17220.937837499994,  // ts 8 rejected
+        16395.627101249993,  // ts 8 repeated
+        22090.518774595865,
+        37751.470876297011,  // ts 10 rejected
+        23656.61398476598,   // ts 10 repeated
+        29137.947220361384,
+        48322.613544945292,  // ts 12 rejected
+        31056.413852819776,  // ts 12 repeated
+        37771.047066424137,
+        56236.288403836123,  // ts 14 rejected
+        54389.764270094922,  // ts 14 rejected
+        42756.662227525369,  // ts 14 repeated
+        60206.315291379709,
+        74747.111189489529,
+        114734.29990929153,
+        194708.67734889555,
+        354657.43222810357,
+        432000,
+        440640,
+        470880,
+        576720,
+        947160,  // ts 24 rejected
+        579960,  // ts 24 repeated
+        591300};
+
+    std::vector<double> const vec_t =
+        timeStepping(alg, nr_iterations, fixed_output_times, rejected_steps);
+
+    EXPECT_EQ(expected_vec_t.size(), vec_t.size());
+    ASSERT_ARRAY_NEAR(expected_vec_t, vec_t, expected_vec_t.size(),
+                      5e4 * std::numeric_limits<double>::epsilon());
+}
