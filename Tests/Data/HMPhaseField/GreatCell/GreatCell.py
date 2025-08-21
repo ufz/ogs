b75fbcf0e275df3a04eb15f52d965bcf4aabb46a
MostafaMollaali <m.molaali@gmail.com>
2025-08-21 15:29:13 +0200

    use cwd=mesh_dir for mesh tools to simplify path handling

--- Tests/Data/HMPhaseField/GreatCell/GreatCell.py
+++ Tests/Data/HMPhaseField/GreatCell/GreatCell.py
@@ -549,39 +549,75 @@ plotter.enable_parallel_projection()
 plotter.show()
 
 # %% vscode={"languageId": "python"}
-# %cd {mesh_path}
+mesh_dir = Path(mesh_path).resolve()
+
 run(
-    "NodeReordering -i domain.vtu -o domain.vtu",
-    shell=True,
+    [
+        "NodeReordering",
+        "-i",
+        str(mesh_dir.joinpath("domain.vtu")),
+        "-o",
+        str(mesh_dir.joinpath("domain.vtu")),
+    ],
+    cwd=mesh_dir,
     check=True,
 )
 
+physical_groups = [
+    "physical_group_DSS1.vtu",
+    "physical_group_DSS1a.vtu",
+    "physical_group_DSS2.vtu",
+    "physical_group_DSS2a.vtu",
+    "physical_group_DSS3.vtu",
+    "physical_group_DSS3a.vtu",
+    "physical_group_DSS4.vtu",
+    "physical_group_DSS4a.vtu",
+    "physical_group_DSS5.vtu",
+    "physical_group_DSS5a.vtu",
+    "physical_group_DSS6.vtu",
+    "physical_group_DSS6a.vtu",
+    "physical_group_DSS7.vtu",
+    "physical_group_DSS7a.vtu",
+    "physical_group_DSS8.vtu",
+    "physical_group_DSS8a.vtu",
+    "physical_group_PEE1.vtu",
+    "physical_group_PEE1a.vtu",
+    "physical_group_PEE2.vtu",
+    "physical_group_PEE2a.vtu",
+    "physical_group_PEE3.vtu",
+    "physical_group_PEE3a.vtu",
+    "physical_group_PEE4.vtu",
+    "physical_group_PEE4a.vtu",
+    "physical_group_PEE5.vtu",
+    "physical_group_PEE5a.vtu",
+    "physical_group_PEE6.vtu",
+    "physical_group_PEE6a.vtu",
+    "physical_group_PEE7.vtu",
+    "physical_group_PEE7a.vtu",
+    "physical_group_PEE8.vtu",
+    "physical_group_PEE8a.vtu",
+    "physical_group_p_bottom.vtu",
+    "physical_group_p_left.vtu",
+    "physical_group_p_right.vtu",
+    "physical_group_p_top.vtu",
+    "physical_group_Inlet.vtu",
+]
+
+group_paths = [str(mesh_dir.joinpath(name)) for name in physical_groups]
+
 run(
-    "identifySubdomains -f -m domain.vtu -- "
-    "physical_group_DSS1.vtu physical_group_DSS1a.vtu "
-    "physical_group_DSS2.vtu physical_group_DSS2a.vtu "
-    "physical_group_DSS3.vtu physical_group_DSS3a.vtu "
-    "physical_group_DSS4.vtu physical_group_DSS4a.vtu "
-    "physical_group_DSS5.vtu physical_group_DSS5a.vtu "
-    "physical_group_DSS6.vtu physical_group_DSS6a.vtu "
-    "physical_group_DSS7.vtu physical_group_DSS7a.vtu "
-    "physical_group_DSS8.vtu physical_group_DSS8a.vtu "
-    "physical_group_PEE1.vtu physical_group_PEE1a.vtu "
-    "physical_group_PEE2.vtu physical_group_PEE2a.vtu "
-    "physical_group_PEE3.vtu physical_group_PEE3a.vtu "
-    "physical_group_PEE4.vtu physical_group_PEE4a.vtu "
-    "physical_group_PEE5.vtu physical_group_PEE5a.vtu "
-    "physical_group_PEE6.vtu physical_group_PEE6a.vtu "
-    "physical_group_PEE7.vtu physical_group_PEE7a.vtu "
-    "physical_group_PEE8.vtu physical_group_PEE8a.vtu "
-    "physical_group_p_bottom.vtu physical_group_p_left.vtu "
-    "physical_group_p_right.vtu physical_group_p_top.vtu "
-    "physical_group_Inlet.vtu ",
-    shell=True,
+    [
+        "identifySubdomains",
+        "-f",
+        "-m",
+        str(mesh_dir.joinpath("domain.vtu")),
+        "--",
+        *group_paths,
+    ],
+    cwd=mesh_dir,
     check=True,
 )
 
-# %cd -
 
 # %% [markdown]
 # ---
@@ -943,39 +979,77 @@ plotter.show()
 
 
 # %% vscode={"languageId": "python"}
-# %cd {mesh_path_VPF}
+mesh_dir = Path(mesh_path_VPF).resolve()
+
 run(
-    "NodeReordering -i domain.vtu -o domain.vtu",
-    shell=True,
+    [
+        "NodeReordering",
+        "-i",
+        str(mesh_dir.joinpath("domain.vtu")),
+        "-o",
+        str(mesh_dir.joinpath("domain.vtu")),
+    ],
+    cwd=mesh_dir,
     check=True,
 )
 
+physical_groups = [
+    "physical_group_DSS1.vtu",
+    "physical_group_DSS1a.vtu",
+    "physical_group_DSS2.vtu",
+    "physical_group_DSS2a.vtu",
+    "physical_group_DSS3.vtu",
+    "physical_group_DSS3a.vtu",
+    "physical_group_DSS4.vtu",
+    "physical_group_DSS4a.vtu",
+    "physical_group_DSS5.vtu",
+    "physical_group_DSS5a.vtu",
+    "physical_group_DSS6.vtu",
+    "physical_group_DSS6a.vtu",
+    "physical_group_DSS7.vtu",
+    "physical_group_DSS7a.vtu",
+    "physical_group_DSS8.vtu",
+    "physical_group_DSS8a.vtu",
+    "physical_group_PEE1.vtu",
+    "physical_group_PEE1a.vtu",
+    "physical_group_PEE2.vtu",
+    "physical_group_PEE2a.vtu",
+    "physical_group_PEE3.vtu",
+    "physical_group_PEE3a.vtu",
+    "physical_group_PEE4.vtu",
+    "physical_group_PEE4a.vtu",
+    "physical_group_PEE5.vtu",
+    "physical_group_PEE5a.vtu",
+    "physical_group_PEE6.vtu",
+    "physical_group_PEE6a.vtu",
+    "physical_group_PEE7.vtu",
+    "physical_group_PEE7a.vtu",
+    "physical_group_PEE8.vtu",
+    "physical_group_PEE8a.vtu",
+    "physical_group_p_bottom.vtu",
+    "physical_group_p_left.vtu",
+    "physical_group_p_right.vtu",
+    "physical_group_p_top.vtu",
+    "physical_group_Inlet.vtu",
+    "physical_group_Outlet_R_embeddedFracture.vtu",
+    "physical_group_Outlet_R_fullFracture.vtu",
+    "physical_group_Outlet_L_fullFracture.vtu",
+]
+
+group_paths = [str(mesh_dir.joinpath(name)) for name in physical_groups]
+
 run(
-    "identifySubdomains -f -m domain.vtu -- "
-    "physical_group_DSS1.vtu physical_group_DSS1a.vtu "
-    "physical_group_DSS2.vtu physical_group_DSS2a.vtu "
-    "physical_group_DSS3.vtu physical_group_DSS3a.vtu "
-    "physical_group_DSS4.vtu physical_group_DSS4a.vtu "
-    "physical_group_DSS5.vtu physical_group_DSS5a.vtu "
-    "physical_group_DSS6.vtu physical_group_DSS6a.vtu "
-    "physical_group_DSS7.vtu physical_group_DSS7a.vtu "
-    "physical_group_DSS8.vtu physical_group_DSS8a.vtu "
-    "physical_group_PEE1.vtu physical_group_PEE1a.vtu "
-    "physical_group_PEE2.vtu physical_group_PEE2a.vtu "
-    "physical_group_PEE3.vtu physical_group_PEE3a.vtu "
-    "physical_group_PEE4.vtu physical_group_PEE4a.vtu "
-    "physical_group_PEE5.vtu physical_group_PEE5a.vtu "
-    "physical_group_PEE6.vtu physical_group_PEE6a.vtu "
-    "physical_group_PEE7.vtu physical_group_PEE7a.vtu "
-    "physical_group_PEE8.vtu physical_group_PEE8a.vtu "
-    "physical_group_p_bottom.vtu physical_group_p_left.vtu "
-    "physical_group_p_right.vtu physical_group_p_top.vtu "
-    "physical_group_Inlet.vtu physical_group_Outlet_R_embeddedFracture.vtu "
-    "physical_group_Outlet_R_fullFracture.vtu physical_group_Outlet_L_fullFracture.vtu",
-    shell=True,
+    [
+        "identifySubdomains",
+        "-f",
+        "-m",
+        str(mesh_dir.joinpath("domain.vtu")),
+        "--",
+        *group_paths,
+    ],
+    cwd=mesh_dir,
     check=True,
 )
-# %cd -
 
 # %% [markdown]
 # ---
