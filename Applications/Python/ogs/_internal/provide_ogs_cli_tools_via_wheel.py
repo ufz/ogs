719cd0ec5949c5ff735d8d0b52a4adf4fbf16633
Lars Bilke <lars.bilke@ufz.de>
2025-08-25 15:04:14 +0200

    Removed PEP517_BUILD_BACKEND logic.

--- Applications/Python/ogs/_internal/provide_ogs_cli_tools_via_wheel.py
+++ Applications/Python/ogs/_internal/provide_ogs_cli_tools_via_wheel.py
@@ -7,6 +7,10 @@ from . import OGS_USE_PATH
 from .binaries_list import binaries_list
 from .get_bin_dir import get_bin_dir
 
+OGS_BIN_DIR = get_bin_dir()
+if platform.system() == "Windows":
+    os.add_dll_directory(OGS_BIN_DIR)
+
 
 # Not used when OGS_USE_PATH is true!
 def ogs():
@@ -35,27 +39,22 @@ def ogs_with_args(argv):
     return return_code
 
 
-if "PEP517_BUILD_BACKEND" not in os.environ:
-    OGS_BIN_DIR = get_bin_dir()
+def _program(name, args):
+    exe = OGS_BIN_DIR / name
+    env = None  # by default use unmodified environment
+    if OGS_USE_PATH:
+        exe = name
+        env = os.environ.copy()
+        # prevent infinite recursion if OGS in PATH happens to be this very
+        # script
+        env["OGS_USE_PATH"] = "0"
+        print(f"OGS_USE_PATH is true: {name} from $PATH is used!")
+    return subprocess.run([exe] + args, env=env).returncode  # noqa: PLW1510
 
-    if platform.system() == "Windows":
-        os.add_dll_directory(OGS_BIN_DIR)
 
-    def _program(name, args):
-        exe = OGS_BIN_DIR / name
-        env = None  # by default use unmodified environment
-        if OGS_USE_PATH:
-            exe = name
-            env = os.environ.copy()
-            # prevent infinite recursion if OGS in PATH happens to be this very
-            # script
-            env["OGS_USE_PATH"] = "0"
-            print(f"OGS_USE_PATH is true: {name} from $PATH is used!")
-        return subprocess.run([exe] + args, env=env).returncode  # noqa: PLW1510
-
-    FUNC_TEMPLATE = """def {0}(): raise SystemExit(_program("{0}", sys.argv[1:]))"""
-    for f in binaries_list:
-        if f == "ogs" and not OGS_USE_PATH:
-            continue  # provided by separate function
-        # When OGS_USE_PATH is true then ogs()-function above is not used!
-        exec(FUNC_TEMPLATE.format(f))
+FUNC_TEMPLATE = """def {0}(): raise SystemExit(_program("{0}", sys.argv[1:]))"""
+for f in binaries_list:
+    if f == "ogs" and not OGS_USE_PATH:
+        continue  # provided by separate function
+    # When OGS_USE_PATH is true then ogs()-function above is not used!
+    exec(FUNC_TEMPLATE.format(f))
