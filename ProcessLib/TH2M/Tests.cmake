ac646ded1ebb6f07e60a554ea97f56421d5db641
Dmitri Naumov <dmitri-jurievic.naumov@ifgt.tu-freiberg.de>
2025-09-02 09:57:21 +0200

    test(TH2M): verify sigma_total output
    also move the test definitions into the project file, but
    relaxed tolerances for sigma.

--- ProcessLib/TH2M/Tests.cmake
+++ ProcessLib/TH2M/Tests.cmake
@@ -45,6 +45,7 @@ if (NOT OGS_USE_MPI)
     OgsTest(PROJECTFILE TH2M/submesh_residuum_assembly/p_G.xml RUNTIME 1)
     OgsTest(PROJECTFILE TH2M/submesh_residuum_assembly/p_cap.xml RUNTIME 1)
     OgsTest(PROJECTFILE TH2M/submesh_residuum_assembly/u.xml RUNTIME 1)
+    OgsTest(PROJECTFILE TH2M/TotalInitialStress/total_initial_stress_H2M.prj RUNTIME 1)
 endif()
 
 # TH2M 1d heat diffusion w/ Dirichlet-BC
@@ -443,24 +444,6 @@ AddTest(
     results_heatpipe_strict_ts_23_t_40000_000000_2.vtu results_heatpipe_strict_ts_23_t_40000_000000_2.vtu gas_density gas_density 1e-9 1e-8
     results_heatpipe_strict_ts_23_t_40000_000000_2.vtu results_heatpipe_strict_ts_23_t_40000_000000_2.vtu porosity porosity 1e-9 1e-8
     results_heatpipe_strict_ts_23_t_40000_000000_2.vtu results_heatpipe_strict_ts_23_t_40000_000000_2.vtu saturation saturation 1e-9 1e-8
-)
-
-AddTest(
-    NAME TH2MTotalInitialStress
-    PATH TH2M/TotalInitialStress
-    EXECUTABLE ogs
-    EXECUTABLE_ARGS total_initial_stress_H2M.prj
-    WRAPPER time
-    TESTER vtkdiff
-    REQUIREMENTS NOT OGS_USE_MPI
-    RUNTIME 1
-    DIFF_DATA
-    total_initial_stress_H2M_ts_0_t_0.000000.vtu total_initial_stress_H2M_ts_0_t_0.000000.vtu capillary_pressure capillary_pressure 1.0e-10 1.e-10
-    total_initial_stress_H2M_ts_0_t_0.000000.vtu total_initial_stress_H2M_ts_0_t_0.000000.vtu p_GR p_GR 1.0e-10 1.e-10
-    total_initial_stress_H2M_ts_0_t_0.000000.vtu total_initial_stress_H2M_ts_0_t_0.000000.vtu sigma sigma 1.0e-8 1.e-10
-    total_initial_stress_H2M_ts_1_t_1000000.000000.vtu total_initial_stress_H2M_ts_1_t_1000000.000000.vtu capillary_pressure capillary_pressure 1.0e-10 1.e-10
-    total_initial_stress_H2M_ts_1_t_1000000.000000.vtu total_initial_stress_H2M_ts_1_t_1000000.000000.vtu p_GR p_GR 1.e-10 1.e-10
-    total_initial_stress_H2M_ts_1_t_1000000.000000.vtu total_initial_stress_H2M_ts_1_t_1000000.000000.vtu sigma sigma 1.0e-8 1.e-10
 )
 
 if(NOT OGS_USE_PETSC)
