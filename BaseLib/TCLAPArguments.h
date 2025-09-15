8f12ea5bb54e436a715873b42147e3cf6d1be76c
Lars Bilke <lars.bilke@ufz.de>
2025-09-15 10:48:51 +0200

    [BL] Extracted log-level argument.

---
+++ BaseLib/TCLAPArguments.h
@@ -0,0 +1,36 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2025-2025, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ */
+#pragma once
+
+#include <tclap/CmdLine.h>
+
+#include <memory>
+#include <vector>
+
+namespace BaseLib
+{
+
+inline std::unique_ptr<TCLAP::ValueArg<std::string>> makeLogLevelArg()
+{
+    static std::vector<std::string> allowed_log_levels{"none", "error", "warn",
+                                                       "info", "debug", "all"};
+    static auto* allowed_log_levels_vals =
+        new TCLAP::ValuesConstraint<std::string>(allowed_log_levels);
+
+    return std::make_unique<TCLAP::ValueArg<std::string>>(
+        "l", "log-level", "the verbosity of logging messages", false,
+#ifdef NDEBUG
+        "info",
+#else
+        "all",
+#endif
+        allowed_log_levels_vals);
+}
+
+}  // namespace BaseLib
