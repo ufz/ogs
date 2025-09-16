2203b79e66f0b8d908131015c5259908d668add0
Dmitri Naumov <dmitri-jurievic.naumov@ifgt.tu-freiberg.de>
2025-09-16 12:24:28 +0200

    refactor(BL): move log level argument to own cpp

---
+++ BaseLib/TCLAPArguments.cpp
@@ -0,0 +1,34 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2025, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ */
+
+#include "TCLAPArguments.h"
+
+#include <vector>
+
+namespace BaseLib
+{
+
+std::unique_ptr<TCLAP::ValueArg<std::string>> makeLogLevelArg()
+{
+    static std::vector<std::string> allowed_log_levels{"none", "error", "warn",
+                                                       "info", "debug", "all"};
+    static auto* allowed_log_levels_vals =
+        new TCLAP::ValuesConstraint<std::string>(allowed_log_levels);
+
+    return std::make_unique<TCLAP::ValueArg<std::string>>(
+        "l", "log-level", "the verbosity of logging messages", false,
+#ifdef NDEBUG
+        "info",
+#else
+        "all",
+#endif
+        allowed_log_levels_vals);
+}
+
+}  // namespace BaseLib
