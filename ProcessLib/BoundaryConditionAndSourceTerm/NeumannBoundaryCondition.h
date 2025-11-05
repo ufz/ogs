624f56d950a09b9ac41dea15767665d7ea50b407
Tom Fischer <thomas.fischer@ufz.de>
2025-11-05 11:14:57 +0100

    refactor(PL/BC&ST): Neumann: separate parsing from creation

--- ProcessLib/BoundaryConditionAndSourceTerm/NeumannBoundaryCondition.h
+++ ProcessLib/BoundaryConditionAndSourceTerm/NeumannBoundaryCondition.h
@@ -20,8 +20,13 @@ using NeumannBoundaryCondition =
     GenericNaturalBoundaryCondition<NeumannBoundaryConditionData,
                                     NeumannBoundaryConditionLocalAssembler>;
 
+std::pair<std::string, std::optional<std::string>>
+parseNeumannBoundaryCondition(BaseLib::ConfigTree const& config);
+
 std::unique_ptr<NeumannBoundaryCondition> createNeumannBoundaryCondition(
-    BaseLib::ConfigTree const& config, MeshLib::Mesh const& bc_mesh,
+    std::string const& parameter_name,
+    std::optional<std::string> const& area_parameter_name,
+    MeshLib::Mesh const& bc_mesh,
     NumLib::LocalToGlobalIndexMap const& dof_table, int const variable_id,
     int const component_id, unsigned const integration_order,
     unsigned const shapefunction_order, unsigned const global_dim,
