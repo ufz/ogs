2ae51bbd42fb0eb54c59f8848721f38ea9763984
Wenqing Wang <wenqing.wang@ufz.de>
2025-12-04 14:22:15 +0100

    feat(FD_Jacobian)replaced `component_magnitudes` and `relative_epsilons` with
     only one parameter `epsilons`.

--- ProcessLib/CreateForwardDifferencesJacobianAssembler.cpp
+++ ProcessLib/CreateForwardDifferencesJacobianAssembler.cpp
@@ -21,47 +21,34 @@ createForwardDifferencesJacobianAssembler(BaseLib::ConfigTree const& config)
     //! \ogs_file_param{prj__processes__process__jacobian_assembler__type}
     config.checkConfigParameter("type", "ForwardDifferences");
 
-    // TODO make non-optional.
-    //! \ogs_file_param{prj__processes__process__jacobian_assembler__ForwardDifferences__relative_epsilons}
-    auto rel_eps = config.getConfigParameterOptional<std::vector<double>>(
-        "relative_epsilons");
-    //! \ogs_file_param{prj__processes__process__jacobian_assembler__ForwardDifferences__component_magnitudes}
-    auto comp_mag = config.getConfigParameterOptional<std::vector<double>>(
-        "component_magnitudes");
-
-    if (rel_eps.has_value() != comp_mag.has_value())
+    // TODO: to be removed
     {
-        OGS_FATAL(
-            "Either both or none of <relative_epsilons> and "
-            "<component_magnitudes> have to be specified.");
-    }
-
-    std::vector<double> abs_eps;
-
-    if (rel_eps)
-    {
-        if (rel_eps->size() != comp_mag->size())
+        std::vector<std::string> const deprecated_keys{"relative_epsilons",
+                                                       "component_magnitudes"};
+        for (auto const& key : deprecated_keys)
         {
-            OGS_FATAL(
-                "The numbers of components of  <relative_epsilons> and "
-                "<component_magnitudes> do not match.");
-        }
+            auto const deprecated_parameter =
+                //! \ogs_file_special
+                config.getConfigParameterOptional<std::vector<double>>(key);
 
-        abs_eps.resize(rel_eps->size());
-        for (std::size_t i = 0; i < rel_eps->size(); ++i)
-        {
-            abs_eps[i] = (*rel_eps)[i] * (*comp_mag)[i];
+            if ((deprecated_parameter.has_value() &&
+                 !deprecated_parameter->empty()))
+            {
+                OGS_FATAL(
+                    "Using {:s} is not allowed anymore as of r6.5.6. Please "
+                    "replace <relative_epsilons> and <component_magnitudes> "
+                    "with <epsilons> to specify variable component "
+                    "perturbations (excluding deformation).",
+                    key);
+            }
         }
     }
-    else
-    {
-        // By default 1e-8 is used as epsilon for all components.
-        // TODO: remove this default value.
-        abs_eps.emplace_back(1e-8);
-    }
+
+    //! \ogs_file_param{prj__processes__process__jacobian_assembler__ForwardDifferences__epsilons}
+    auto epsilons = config.getConfigParameter<std::vector<double>>("epsilons");
 
     return std::make_unique<ForwardDifferencesJacobianAssembler>(
-        std::move(abs_eps));
+        std::move(epsilons));
 }
 
 }  // namespace ProcessLib
