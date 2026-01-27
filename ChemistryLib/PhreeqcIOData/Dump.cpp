17550d4517779ebec65f162fe41c88491d464903
Haibing Shao <haibing.shao@ufz.de>
2026-01-27 11:09:19 +0100

    build: read dump from string
    clang-format

--- ChemistryLib/PhreeqcIOData/Dump.cpp
+++ ChemistryLib/PhreeqcIOData/Dump.cpp
@@ -4,6 +4,7 @@
 #include "Dump.h"
 
 #include <iostream>
+#include <sstream>
 
 namespace ChemistryLib
 {
@@ -31,6 +32,43 @@ void Dump::readDumpFile(std::istream& in,
     std::string aqueous_solution_prev_;
     std::size_t chemical_system_id = 0;
     while (std::getline(in, line))
+    {
+        if (line.find("USE reaction_pressure none") != std::string::npos)
+        {
+            break;
+        }
+
+        if (line.find("SOLUTION_RAW") != std::string::npos)
+        {
+            aqueous_solution_prev_ =
+                "SOLUTION_RAW " +
+                std::to_string(num_chemical_systems + chemical_system_id + 1) +
+                "\n";
+            continue;
+        }
+
+        aqueous_solution_prev_ += line + "\n";
+
+        if (line.find("-gammas") != std::string::npos)
+        {
+            aqueous_solutions_prev.push_back(aqueous_solution_prev_);
+            aqueous_solution_prev_.clear();
+            ++chemical_system_id;
+        }
+    }
+}
+
+void Dump::readDumpFromString(std::string const& dump_content,
+                              std::size_t const num_chemical_systems)
+{
+    aqueous_solutions_prev.clear();
+    aqueous_solutions_prev.reserve(num_chemical_systems);
+
+    std::istringstream iss(dump_content);
+    std::string line;
+    std::string aqueous_solution_prev_;
+    std::size_t chemical_system_id = 0;
+    while (std::getline(iss, line))
     {
         if (line.find("USE reaction_pressure none") != std::string::npos)
         {
