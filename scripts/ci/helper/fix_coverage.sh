2bcbf2cea5c003272ad74984970096224fbd9ca4
Lars Bilke <lars.bilke@ufz.de>
2026-02-17 17:49:07 +0100

    [ci] Enable python coverage.

--- scripts/ci/helper/fix_coverage.sh
+++ scripts/ci/helper/fix_coverage.sh
@@ -2,18 +2,34 @@
 
 SRC_ROOT="$1"
 
-xmlstarlet ed \
-  -u '//class/@filename' \
-  -x 'concat("Applications/Python/", substring-after(., "site-packages/"))' \
-  -u '//package/@name' -x 'substring-after(., "site-packages.")' \
-  -d '//sources/source' \
-  -s '//sources' -t elem -n source -v "$SRC_ROOT" \
-  pytest-coverage.xml > pytest-coverage-rel.xml
+gcovr_args=""
 
-xmlstarlet ed \
-  -u '//class/@filename' \
-  -x 'concat("Tests/Data/", substring-after(., "Tests/Data/"))' \
-  -u '//package/@name' -x 'substring-after(., "ogs.")' \
-  -d '//sources/source' \
-  -s '//sources' -t elem -n source -v "$SRC_ROOT" \
-  notebook-coverage.xml > notebook-coverage-rel.xml
+if [[ -f "pytest-coverage.xml" ]]; then
+  xmlstarlet ed \
+    -u '//class/@filename' \
+    -x 'concat("Applications/Python/", substring-after(., "site-packages/"))' \
+    -u '//package/@name' -x 'substring-after(., "site-packages.")' \
+    -d '//sources/source' \
+    -s '//sources' -t elem -n source -v "$SRC_ROOT" \
+    pytest-coverage.xml > pytest-coverage-rel.xml
+  gcovr_args="--cobertura-add-tracefile pytest-coverage-rel.xml"
+fi
+
+if [[ -f "notebook-coverage.xml" ]]; then
+  xmlstarlet ed \
+    -u "//class/@filename[starts-with(., \"$SRC_ROOT/\")]" \
+    -x "substring-after(., \"$SRC_ROOT/\")" \
+    -u '//package/@name' -x 'substring-after(., "ogs.")' \
+    -d '//sources/source' \
+    -s '//sources' -t elem -n source -v "$SRC_ROOT" \
+    notebook-coverage.xml > notebook-coverage-rel.xml
+  gcovr_args="${gcovr_args} --cobertura-add-tracefile notebook-coverage-rel.xml"
+fi
+
+mkdir -p python-html
+uvx gcovr \
+  ${gcovr_args} \
+  --html-details python-html/coverage.html \
+  --exclude '.*/ogs/config.py' \
+  -r ${SRC_ROOT} \
+  -s
