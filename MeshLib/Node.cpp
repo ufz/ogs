f144f7b6cc387988131a70dd340f0b869aa0fb1c
Tom Fischer <thomas.fischer@ufz.de>
2026-02-27 11:43:15 +0100

    feature: Debugging requires higher output precision

--- MeshLib/Node.cpp
+++ MeshLib/Node.cpp
@@ -25,7 +25,11 @@ Node::Node(double x, double y, double z, std::size_t id)
 
 std::ostream& operator<<(std::ostream& os, MeshLib::Node const& n)
 {
-    return os << "node #" << n.getID() << " { "
-              << static_cast<MathLib::Point3d const&>(n) << "}";
+    auto const default_precision = os.precision();
+    os << "node #" << n.getID() << " { "
+       << std::setprecision(std::numeric_limits<double>::digits10 + 1)
+       << static_cast<MathLib::Point3d const&>(n) << "}";
+    os << std::setprecision(default_precision);
+    return os;
 }
 }  // namespace MeshLib
