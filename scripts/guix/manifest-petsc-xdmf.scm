0544b5ec6fd2eafa5d9ace829729f4481a722d48
Lars Bilke <lars.bilke@ufz.de>
2026-03-16 13:57:00 +0100

    [ci] Adding xdmfdiff to eve petsc container.
    Fixes job "test container eve".

---
+++ scripts/guix/manifest-petsc-xdmf.scm
@@ -0,0 +1,19 @@
+(use-modules (guix transformations)
+             (guix utils)
+             (gnu packages base))
+
+(load "manifest-common.scm")
+
+(define current-dir
+  (getcwd))
+
+(define transform1
+  (options->transformation (append `((with-source unquote
+                                                  (string-append "ogs-petsc-xdmf="
+                                                                 current-dir)))
+                                   (manifest-eigen-transform-options)
+                                   '((with-input . "openmpi=openmpi@4.1.6")))))
+
+(packages->manifest (append (manifest-runtime-packages (transform1 (specification->package
+                                                                    "ogs-petsc-xdmf")))
+                            (list (specification->package "openmpi@4.1.6"))))
