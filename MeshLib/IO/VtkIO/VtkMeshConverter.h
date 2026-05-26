989bac906476b0c8806e6b3cb3c57d99c58ddaa2
Thomas Fischer <thomas.fischer@ufz.de>
2026-05-26 13:03:05 +0200

    cleanup(VtkMeshConverter): remove unused static method

--- MeshLib/IO/VtkIO/VtkMeshConverter.h
+++ MeshLib/IO/VtkIO/VtkMeshConverter.h
@@ -54,37 +54,6 @@ private:
         std::size_t const imgWidth,
         MeshElemType elem_type);
 
-    /// Creates a scalar array/mesh property based on pixel values
-    template <typename T>
-    static void fillPropertyVector(MeshLib::PropertyVector<T>& prop_vec,
-                                   std::vector<double> const& pix_val,
-                                   std::vector<bool> const& pix_vis,
-                                   const std::size_t& imgHeight,
-                                   const std::size_t& imgWidth,
-                                   MeshElemType elem_type)
-    {
-        for (std::size_t i = 0; i < imgHeight; i++)
-        {
-            for (std::size_t j = 0; j < imgWidth; j++)
-            {
-                if (!pix_vis[i * imgWidth + j])
-                {
-                    continue;
-                }
-                T val(static_cast<T>(pix_val[i * (imgWidth + 1) + j]));
-                if (elem_type == MeshElemType::TRIANGLE)
-                {
-                    prop_vec.push_back(val);
-                    prop_vec.push_back(val);
-                }
-                else if (elem_type == MeshElemType::QUAD)
-                {
-                    prop_vec.push_back(val);
-                }
-            }
-        }
-    }
-
     static void convertArray(vtkDataArray& array,
                              MeshLib::Properties& properties,
                              MeshLib::MeshItemType type);
