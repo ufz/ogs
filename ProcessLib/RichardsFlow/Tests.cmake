0bf0a0a4f151a089121fdd8b3591cff18de043ce
Lars Bilke <lars.bilke@ufz.de>
2026-06-19 12:05:02 +0200

    Migrate RichardsFlow ogs tests

--- ProcessLib/RichardsFlow/Tests.cmake
+++ ProcessLib/RichardsFlow/Tests.cmake
@@ -45,30 +45,11 @@ AddTest(
     REQUIREMENTS NOT (OGS_USE_MPI OR OGS_USE_LIS)
 )
 
-AddTest(
-    NAME 2D_RichardsFlow_h_us_quad_small_PID_adaptive_dt
-    PATH Parabolic/Richards
-    EXECUTABLE ogs
-    EXECUTABLE_ARGS RichardsFlow_2d_small_PID_adaptive_dt.prj
-    TESTER vtkdiff
-    DIFF_DATA
-    richards_pcs_PID_adaptive_dt_t_1600.vtu  richards_pcs_PID_adaptive_dt_t_1600.vtu pressure pressure 1e-8 1e-9
-    richards_pcs_PID_adaptive_dt_t_1600.vtu  richards_pcs_PID_adaptive_dt_t_1600.vtu saturation saturation 1e-8 1e-9
-# The following three comparisons are used just to check whether the output is
-# made at the fixed times of 10, 50, 100 and 500, which are given in the project
-# file of RichardsFlow_2d_small_adaptive_dt.prj
-    richards_pcs_PID_adaptive_dt_t_10.vtu  richards_pcs_PID_adaptive_dt_t_10.vtu pressure pressure 1e-8 1e-9
-    richards_pcs_PID_adaptive_dt_t_10.vtu  richards_pcs_PID_adaptive_dt_t_10.vtu saturation saturation 1e-8 1e-9
-    richards_pcs_PID_adaptive_dt_t_50.vtu  richards_pcs_PID_adaptive_dt_t_50.vtu pressure pressure 1e-8 1e-9
-    richards_pcs_PID_adaptive_dt_t_50.vtu  richards_pcs_PID_adaptive_dt_t_50.vtu saturation saturation 1e-8 1e-9
-    richards_pcs_PID_adaptive_dt_t_100.vtu  richards_pcs_PID_adaptive_dt_t_100.vtu pressure pressure 1e-8 1e-9
-    richards_pcs_PID_adaptive_dt_t_100.vtu  richards_pcs_PID_adaptive_dt_t_100.vtu saturation saturation 1e-8 1e-9
-    richards_pcs_PID_adaptive_dt_t_500.vtu  richards_pcs_PID_adaptive_dt_t_500.vtu pressure pressure 1e-8 1e-9
-    richards_pcs_PID_adaptive_dt_t_500.vtu  richards_pcs_PID_adaptive_dt_t_500.vtu saturation saturation 1e-8 1e-9
-    richards_pcs_PID_adaptive_dt_t_1000.vtu  richards_pcs_PID_adaptive_dt_t_1000.vtu pressure pressure 1e-8 1e-9
-    richards_pcs_PID_adaptive_dt_t_1000.vtu  richards_pcs_PID_adaptive_dt_t_1000.vtu saturation saturation 1e-8 1e-9
-    REQUIREMENTS NOT OGS_USE_MPI
-)
+if(NOT OGS_USE_MPI)
+    OgsTest(
+        PROJECTFILE Parabolic/Richards/RichardsFlow_2d_small_PID_adaptive_dt.prj
+    )
+endif()
 
 AddTest(
     NAME 2D_RichardsFlow_h_us_quad_small_iteration_adaptive_dt
@@ -94,8 +75,6 @@ AddTest(
     richards_pcs_PID_adaptive_dt_t_1600.vtu 2D_RichardsFlow_h_us_quad_small_iteration_adaptive_dt_t_1600.000000.vtu 1e-8 1e-9
 )
 
-
-
 #PETSc/MPI
 #AddTest(
 #    NAME 2D_RichardsFlow_h_us_quad_small_PID_adaptive_dt
@@ -111,16 +90,8 @@ AddTest(
 #)
 
 # Comparison test for richards mechanics w/o deformations.
-AddTest(
-    NAME Parallel_RichardsMechanics_RichardsFlow_2d_richardsflow
-    PATH RichardsMechanics
-    EXECUTABLE ogs
-    EXECUTABLE_ARGS RichardsFlow_2d_richardsflow.prj
-    WRAPPER mpirun
-    WRAPPER_ARGS -np 1
-    TESTER vtkdiff
-    REQUIREMENTS OGS_USE_MPI
-    DIFF_DATA
-    RichardsFlow_2d_richardsflow_ts_99_t_1900.000000.vtu RichardsFlow_2d_richardsflow_ts_99_t_1900.000000.vtu pressure pressure 5e-8 1e-10
-    RichardsFlow_2d_richardsflow_ts_99_t_1900.000000.vtu RichardsFlow_2d_richardsflow_ts_99_t_1900.000000.vtu saturation saturation 1e-10 1e-11
-)
+if(OGS_USE_MPI)
+    OgsTest(PROJECTFILE RichardsMechanics/RichardsFlow_2d_richardsflow_mpi.xml
+            WRAPPER mpirun -np 1
+    )
+endif()
