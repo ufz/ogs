7f4cbadb50590d5dd2fa79d09c645791bbab185e
Lars Bilke <lars.bilke@ufz.de>
2026-06-19 13:09:32 +0200

    Migrate ThermoRichardsMechanics ogs tests

--- ProcessLib/ThermoRichardsMechanics/Tests.cmake
+++ ProcessLib/ThermoRichardsMechanics/Tests.cmake
@@ -223,39 +223,16 @@ AddTest(
     PointHeatSource_quarter_002_2nd_r_lt_2_ts_10_t_50000.000000.vtu PointHeatSource_quarter_002_2nd_r_lt_2_ts_10_t_50000.000000.vtu NodalForces NodalForces 4e-8 0
 )
 
-AddTest(
-    NAME ThermoRichardsMechanics_TaskCDECOVALEX2023
-    PATH ThermoRichardsMechanics/TaskCDECOVALEX2023
-    EXECUTABLE ogs
-    EXECUTABLE_ARGS Decovalex-0.prj
-    WRAPPER time
-    TESTER vtkdiff
-    REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
-    RUNTIME 15
-    DIFF_DATA
-    Decovalex-0_ts_10_t_864000.000000.vtu Decovalex-0_ts_10_t_864000.000000.vtu sigma sigma 1e-9 1e-8
-    Decovalex-0_ts_10_t_864000.000000.vtu Decovalex-0_ts_10_t_864000.000000.vtu displacement displacement 1e-10 1e-12
-    Decovalex-0_ts_10_t_864000.000000.vtu Decovalex-0_ts_10_t_864000.000000.vtu saturation saturation 1e-10 1e-12
-    Decovalex-0_ts_10_t_864000.000000.vtu Decovalex-0_ts_10_t_864000.000000.vtu temperature temperature 5e-10 1e-12
-    Decovalex-0_ts_10_t_864000.000000.vtu Decovalex-0_ts_10_t_864000.000000.vtu velocity velocity 1e-10 1e-12
-    Decovalex-0_ts_10_t_864000.000000.vtu Decovalex-0_ts_10_t_864000.000000.vtu liquid_density liquid_density 1e-10 1e-11
-)
+if(NOT (OGS_USE_LIS OR OGS_USE_MPI))
+    OgsTest(
+        PROJECTFILE ThermoRichardsMechanics/TaskCDECOVALEX2023/Decovalex-0.prj
+        RUNTIME 15
+    )
+endif()
 
-AddTest(
-    NAME ThermoRichardsMechanics_CTF1
-    PATH ThermoRichardsMechanics/CTF1
-    EXECUTABLE ogs
-    EXECUTABLE_ARGS CTF1.prj
-    WRAPPER time
-    TESTER vtkdiff
-    REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
-    RUNTIME 3
-    DIFF_DATA
-    CTF1_14.000000.vtu CTF1_14.000000.vtu sigma sigma 1e-9 1e-8
-    CTF1_14.000000.vtu CTF1_14.000000.vtu displacement displacement 1e-10 1e-12
-    CTF1_14.000000.vtu CTF1_14.000000.vtu saturation saturation 1e-10 1e-12
-    CTF1_14.000000.vtu CTF1_14.000000.vtu temperature temperature 1e-10 1e-12
-)
+if(NOT (OGS_USE_LIS OR OGS_USE_MPI))
+    OgsTest(PROJECTFILE ThermoRichardsMechanics/CTF1/CTF1.prj RUNTIME 3)
+endif()
 
 AddTest(
     NAME ThermoRichardsMechanics_CreepBGRa_SimpleAxisymmetricCreep
@@ -273,49 +250,23 @@ AddTest(
 )
 
 #PETSc
-AddTest(
-    NAME ParallelFEM_ThermoRichardsMechanics_3D_ThermoElastic_Stress_Analysis
-    PATH ThermoRichardsMechanics/Simple3DThermoMechanicsFromTM
-    RUNTIME 40
-    EXECUTABLE ogs
-    EXECUTABLE_ARGS cube_1e3.prj
-    WRAPPER mpirun
-    WRAPPER_ARGS -np 3
-    TESTER vtkdiff
-    REQUIREMENTS OGS_USE_MPI
-    LABELS "petsc-mumps"
-    DIFF_DATA
-    cube_1e3_tm_ts_17_t_72000_000000_0.vtu cube_1e3_tm_ts_17_t_72000_000000_0.vtu velocity velocity 1e-10 1e-9
-    cube_1e3_tm_ts_17_t_72000_000000_1.vtu cube_1e3_tm_ts_17_t_72000_000000_1.vtu velocity velocity 1e-10 1e-9
-    cube_1e3_tm_ts_17_t_72000_000000_0.vtu cube_1e3_tm_ts_17_t_72000_000000_0.vtu displacement displacement 1e-10 1e-9
-    cube_1e3_tm_ts_17_t_72000_000000_1.vtu cube_1e3_tm_ts_17_t_72000_000000_1.vtu displacement displacement 1e-10 1e-9
-    cube_1e3_tm_ts_17_t_72000_000000_0.vtu cube_1e3_tm_ts_17_t_72000_000000_0.vtu sigma sigma 1e-7 1e-9
-    cube_1e3_tm_ts_17_t_72000_000000_1.vtu cube_1e3_tm_ts_17_t_72000_000000_1.vtu sigma sigma 1e-7 1e-9
-    cube_1e3_tm_ts_17_t_72000_000000_0.vtu cube_1e3_tm_ts_17_t_72000_000000_0.vtu epsilon epsilon 1e-10 1e-9
-    cube_1e3_tm_ts_17_t_72000_000000_1.vtu cube_1e3_tm_ts_17_t_72000_000000_1.vtu epsilon epsilon 1e-10 1e-9
-)
+if(OGS_USE_MPI)
+    OgsTest(
+        PROJECTFILE
+            ThermoRichardsMechanics/Simple3DThermoMechanicsFromTM/cube_1e3.prj
+        WRAPPER mpirun -np 3
+        RUNTIME 40 LABELS "petsc-mumps"
+    )
+endif()
 
-AddTest(
-    NAME ParallelFEM_ThermoRichardsMechanics_FullySaturatedFlowMechanics
-    PATH ThermoRichardsMechanics/FullySaturatedFlowMechanics/PETSc
-    RUNTIME 10
-    EXECUTABLE ogs
-    EXECUTABLE_ARGS flow_fully_saturated_petsc.prj
-    WRAPPER mpirun
-    WRAPPER_ARGS -np 2
-    TESTER vtkdiff
-    REQUIREMENTS OGS_USE_MPI
-    DIFF_DATA
-    flow_fully_saturated_ts_2_t_2_000000_0.vtu flow_fully_saturated_ts_2_t_2_000000_0.vtu pressure pressure 1e-10 1e-9
-    flow_fully_saturated_ts_2_t_2_000000_0.vtu flow_fully_saturated_ts_2_t_2_000000_0.vtu velocity velocity 1e-10 1e-9
-    flow_fully_saturated_ts_2_t_2_000000_1.vtu flow_fully_saturated_ts_2_t_2_000000_1.vtu velocity velocity 1e-10 1e-9
-    flow_fully_saturated_ts_2_t_2_000000_0.vtu flow_fully_saturated_ts_2_t_2_000000_0.vtu displacement displacement 1e-10 1e-9
-    flow_fully_saturated_ts_2_t_2_000000_1.vtu flow_fully_saturated_ts_2_t_2_000000_1.vtu displacement displacement 1e-10 1e-9
-    flow_fully_saturated_ts_2_t_2_000000_0.vtu flow_fully_saturated_ts_2_t_2_000000_0.vtu sigma sigma 1e-10 1e-9
-    flow_fully_saturated_ts_2_t_2_000000_1.vtu flow_fully_saturated_ts_2_t_2_000000_1.vtu sigma sigma 1e-10 1e-9
-    flow_fully_saturated_ts_2_t_2_000000_0.vtu flow_fully_saturated_ts_2_t_2_000000_0.vtu epsilon epsilon 1e-10 1e-9
-    flow_fully_saturated_ts_2_t_2_000000_1.vtu flow_fully_saturated_ts_2_t_2_000000_1.vtu epsilon epsilon 1e-10 1e-9
-)
+if(OGS_USE_MPI)
+    OgsTest(
+        PROJECTFILE
+            ThermoRichardsMechanics/FullySaturatedFlowMechanics/PETSc/flow_fully_saturated_petsc.prj
+        WRAPPER mpirun -np 2
+        RUNTIME 10
+    )
+endif()
 
 AddTest(
     NAME ParallelFEM_ThermoRichardsMechanics_point_heat_injection
@@ -348,71 +299,23 @@ AddTest(
     PointHeatSource_ts_10_t_50000_000000_2.vtu PointHeatSource_ts_10_t_50000_000000_2.vtu sigma sigma 1e-10 1.0e-6
 )
 
-AddTest(
-    NAME ParallelFEM_ThermoRichardsMechanics_point_heat_injection_gml
-    PATH ThermoRichardsMechanics/PointHeatSource
-    RUNTIME 5
-    EXECUTABLE ogs
-    EXECUTABLE_ARGS point_heat_source_2D_gml.prj
-    WRAPPER mpirun
-    WRAPPER_ARGS -np 3
-    TESTER vtkdiff
-    REQUIREMENTS OGS_USE_MPI
-    LABELS "petsc-mumps"
-    DIFF_DATA
-    PointHeatSource_gml_ts_10_t_50000_000000_0.vtu PointHeatSource_gml_ts_10_t_50000_000000_0.vtu displacement displacement 1e-10 1.0e-9
-    PointHeatSource_gml_ts_10_t_50000_000000_0.vtu PointHeatSource_gml_ts_10_t_50000_000000_0.vtu pressure pressure 1e-10 1.0e-6
-    PointHeatSource_gml_ts_10_t_50000_000000_0.vtu PointHeatSource_gml_ts_10_t_50000_000000_0.vtu temperature temperature 1e-10 1.0e-9
-    PointHeatSource_gml_ts_10_t_50000_000000_0.vtu PointHeatSource_gml_ts_10_t_50000_000000_0.vtu epsilon epsilon 1e-10 1.0e-9
-    PointHeatSource_gml_ts_10_t_50000_000000_0.vtu PointHeatSource_gml_ts_10_t_50000_000000_0.vtu sigma sigma 1e-10 1.0e-6
-#
-    PointHeatSource_gml_ts_10_t_50000_000000_1.vtu PointHeatSource_gml_ts_10_t_50000_000000_1.vtu displacement displacement 1e-10 1.0e-9
-    PointHeatSource_gml_ts_10_t_50000_000000_1.vtu PointHeatSource_gml_ts_10_t_50000_000000_1.vtu pressure pressure 1e-10 1.0e-6
-    PointHeatSource_gml_ts_10_t_50000_000000_1.vtu PointHeatSource_gml_ts_10_t_50000_000000_1.vtu temperature temperature 1e-10 1.0e-9
-    PointHeatSource_gml_ts_10_t_50000_000000_1.vtu PointHeatSource_gml_ts_10_t_50000_000000_1.vtu epsilon epsilon 1e-10 1.0e-9
-    PointHeatSource_gml_ts_10_t_50000_000000_1.vtu PointHeatSource_gml_ts_10_t_50000_000000_1.vtu sigma sigma 1e-10 1.0e-6
-#
-    PointHeatSource_gml_ts_10_t_50000_000000_2.vtu PointHeatSource_gml_ts_10_t_50000_000000_2.vtu displacement displacement 1e-10 1.0e-9
-    PointHeatSource_gml_ts_10_t_50000_000000_2.vtu PointHeatSource_gml_ts_10_t_50000_000000_2.vtu pressure pressure 1e-10 1.0e-6
-    PointHeatSource_gml_ts_10_t_50000_000000_2.vtu PointHeatSource_gml_ts_10_t_50000_000000_2.vtu temperature temperature 1e-10 1.0e-9
-    PointHeatSource_gml_ts_10_t_50000_000000_2.vtu PointHeatSource_gml_ts_10_t_50000_000000_2.vtu epsilon epsilon 1e-10 1.0e-9
-    PointHeatSource_gml_ts_10_t_50000_000000_2.vtu PointHeatSource_gml_ts_10_t_50000_000000_2.vtu sigma sigma 1e-10 1.0e-6
-)
+if(OGS_USE_MPI)
+    OgsTest(
+        PROJECTFILE
+            ThermoRichardsMechanics/PointHeatSource/point_heat_source_2D_gml.prj
+        WRAPPER mpirun -np 3
+        RUNTIME 5 LABELS "petsc-mumps"
+    )
+endif()
 
-AddTest(
-    NAME ParallelFEM_ThermoRichardsMechanics_TaskCDECOVALEX2023
-    PATH ThermoRichardsMechanics/TaskCDECOVALEX2023
-    RUNTIME 10
-    EXECUTABLE ogs
-    EXECUTABLE_ARGS Decovalex-0.prj
-    WRAPPER mpirun
-    WRAPPER_ARGS -np 3
-    TESTER vtkdiff
-    REQUIREMENTS OGS_USE_MPI
-    LABELS "petsc-mumps"
-    DIFF_DATA
-#
-    Decovalex-0_ts_10_t_864000_000000_0.vtu Decovalex-0_ts_10_t_864000_000000_0.vtu displacement displacement 1e-10 1.0e-9
-    Decovalex-0_ts_10_t_864000_000000_0.vtu Decovalex-0_ts_10_t_864000_000000_0.vtu pressure pressure 1e-10 1.0e-6
-    Decovalex-0_ts_10_t_864000_000000_0.vtu Decovalex-0_ts_10_t_864000_000000_0.vtu saturation saturation 1e-10 1.0e-6
-    Decovalex-0_ts_10_t_864000_000000_0.vtu Decovalex-0_ts_10_t_864000_000000_0.vtu temperature temperature 1e-10 1.0e-9
-    Decovalex-0_ts_10_t_864000_000000_0.vtu Decovalex-0_ts_10_t_864000_000000_0.vtu epsilon epsilon 1e-10 1.0e-9
-    Decovalex-0_ts_10_t_864000_000000_0.vtu Decovalex-0_ts_10_t_864000_000000_0.vtu sigma sigma 1e-10 1.0e-6
-#
-    Decovalex-0_ts_10_t_864000_000000_1.vtu Decovalex-0_ts_10_t_864000_000000_1.vtu displacement displacement 1e-10 1.0e-9
-    Decovalex-0_ts_10_t_864000_000000_1.vtu Decovalex-0_ts_10_t_864000_000000_1.vtu pressure pressure 1e-10 1.0e-6
-    Decovalex-0_ts_10_t_864000_000000_1.vtu Decovalex-0_ts_10_t_864000_000000_1.vtu saturation saturation 1e-10 1.0e-6
-    Decovalex-0_ts_10_t_864000_000000_1.vtu Decovalex-0_ts_10_t_864000_000000_1.vtu temperature temperature 1e-10 1.0e-9
-    Decovalex-0_ts_10_t_864000_000000_1.vtu Decovalex-0_ts_10_t_864000_000000_1.vtu epsilon epsilon 1e-10 1.0e-9
-    Decovalex-0_ts_10_t_864000_000000_1.vtu Decovalex-0_ts_10_t_864000_000000_1.vtu sigma sigma 1e-10 1.0e-6
-#
-    Decovalex-0_ts_10_t_864000_000000_2.vtu Decovalex-0_ts_10_t_864000_000000_2.vtu displacement displacement 1e-10 1.0e-9
-    Decovalex-0_ts_10_t_864000_000000_2.vtu Decovalex-0_ts_10_t_864000_000000_2.vtu pressure pressure 1e-10 1.0e-6
-    Decovalex-0_ts_10_t_864000_000000_2.vtu Decovalex-0_ts_10_t_864000_000000_2.vtu saturation saturation 1e-10 1.0e-6
-    Decovalex-0_ts_10_t_864000_000000_2.vtu Decovalex-0_ts_10_t_864000_000000_2.vtu temperature temperature 1e-10 1.0e-9
-    Decovalex-0_ts_10_t_864000_000000_2.vtu Decovalex-0_ts_10_t_864000_000000_2.vtu epsilon epsilon 1e-10 1.0e-9
-    Decovalex-0_ts_10_t_864000_000000_2.vtu Decovalex-0_ts_10_t_864000_000000_2.vtu sigma sigma 1e-10 1.0e-6
-)
+if(OGS_USE_MPI)
+    OgsTest(
+        PROJECTFILE
+            ThermoRichardsMechanics/TaskCDECOVALEX2023/Decovalex-0_mpi.xml
+        WRAPPER mpirun -np 3
+        RUNTIME 10 LABELS "petsc-mumps"
+    )
+endif()
 # ThermoRichardsMechanics; thermo_osmosis and thermo_filtration effects, linear poroelastic, column consolidation
 AddTest(
     NAME ThermoRichardsMechanics_thermo_osmosis_filtration_effects_Column
@@ -502,7 +405,10 @@ if(OGS_USE_MFRONT AND (NOT OGS_USE_LIS))
         )
     endif()
 
-    add_subdirectory("${CMAKE_SOURCE_DIR}/Tests/Data/ThermoRichardsMechanics/MFront/BentoniteBehaviourGeneralMod/MFrontBehaviour/" "${CMAKE_BINARY_DIR}/Tests/Data/ThermoRichardsMechanics/MFront/BentoniteBehaviourGeneralMod/MFrontBehaviour/")
+    add_subdirectory(
+        "${CMAKE_SOURCE_DIR}/Tests/Data/ThermoRichardsMechanics/MFront/BentoniteBehaviourGeneralMod/MFrontBehaviour/"
+        "${CMAKE_BINARY_DIR}/Tests/Data/ThermoRichardsMechanics/MFront/BentoniteBehaviourGeneralMod/MFrontBehaviour/"
+    )
 
     OgsTest(PROJECTFILE ThermoRichardsMechanics/MFront/BentoniteBehaviourGeneralMod/0d_confined_compression/confined_compression.prj RUNTIME 19)
     OgsTest(PROJECTFILE ThermoRichardsMechanics/MFront/BentoniteBehaviourGeneralMod/0d_resaturation/resaturation.prj RUNTIME 4)
