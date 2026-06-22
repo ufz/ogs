2fc2edb194e5dba3dcb97fc5a4dcce861105b9bf
Lars Bilke <lars.bilke@ufz.de>
2026-06-22 11:59:06 +0200

    [T] Migrate one remaining test in HydroMechanics.

--- ProcessLib/LIE/HydroMechanics/Tests.cmake
+++ ProcessLib/LIE/HydroMechanics/Tests.cmake
@@ -51,26 +51,14 @@ if(NOT (OGS_USE_LIS OR OGS_USE_MPI))
 endif()
 
 # Note: OGS_USE_LIS takes longer runtime.
-AddTest(
-    NAME LIE_HydroMechanics_GreatCellWithBBar/HM2b_LIE_F
-    PATH LIE/HydroMechanics/GreatCellWithBBar
-    EXECUTABLE ogs
-    EXECUTABLE_ARGS HM2b_LIE_F.prj -m mesh_GreatCell_embeddedFracture
-    WRAPPER time
-    TESTER vtkdiff
-    REQUIREMENTS NOT OGS_USE_MPI
-    RUNTIME 30
-    DIFF_DATA
-    HM2b_LIE_F_Greywacke_ts_69_t_3500.000000.vtu HM2b_LIE_F_Greywacke_ts_69_t_3500.000000.vtu pressure pressure 2e-8 5.2e-14
-    HM2b_LIE_F_Greywacke_ts_69_t_3500.000000.vtu HM2b_LIE_F_Greywacke_ts_69_t_3500.000000.vtu displacement displacement 1e-15 1e-15
-    HM2b_LIE_F_Greywacke_ts_69_t_3500.000000.vtu HM2b_LIE_F_Greywacke_ts_69_t_3500.000000.vtu displacement_jump1 displacement_jump1 1e-15 1e-15
-    HM2b_LIE_F_Greywacke_ts_69_t_3500.000000.vtu HM2b_LIE_F_Greywacke_ts_69_t_3500.000000.vtu fracture_stress fracture_stress 3.5e-9 0
-    HM2b_LIE_F_Greywacke_ts_69_t_3500.000000.vtu HM2b_LIE_F_Greywacke_ts_69_t_3500.000000.vtu fracture_permeability fracture_permeability 1e-15 1e-15
-    HM2b_LIE_F_Greywacke_ts_69_t_3500.000000.vtu HM2b_LIE_F_Greywacke_ts_69_t_3500.000000.vtu fracture_aperture fracture_aperture 1e-15 1e-15
-    HM2b_LIE_F_Greywacke_ts_69_t_3500.000000.vtu HM2b_LIE_F_Greywacke_ts_69_t_3500.000000.vtu sigma sigma 5.1e-7 0
-    HM2b_LIE_F_Greywacke_ts_69_t_3500.000000.vtu HM2b_LIE_F_Greywacke_ts_69_t_3500.000000.vtu epsilon epsilon 1e-15 1e-15
-    HM2b_LIE_F_Greywacke_ts_69_t_3500.000000.vtu HM2b_LIE_F_Greywacke_ts_69_t_3500.000000.vtu velocity velocity 1e-15 1e-15
-)
+if(NOT OGS_USE_MPI)
+    OgsTest(
+        PROJECTFILE LIE/HydroMechanics/GreatCellWithBBar/HM2b_LIE_F.prj
+        RUNTIME 30
+        EXECUTABLE_ARGS
+            -m ${Data_SOURCE_DIR}/LIE/HydroMechanics/GreatCellWithBBar/mesh_GreatCell_embeddedFracture
+    )
+endif()
 
 if(NOT (OGS_USE_PETSC OR OGS_USE_LIS))
     NotebookTest(NOTEBOOKFILE LIE/HydroMechanics/GreatCellWithBBar/great_cell_LIE.py RUNTIME 60)
