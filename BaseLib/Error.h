ccd2aefe56900bbd0d3f31492b05dce7db3afda8
Dmitri Naumov <dmitri-jurievic.naumov@ifgt.tu-freiberg.de>
2026-07-30 19:21:52 +0200

    refactor(BL): remove OGS_FATAL_ABORT option
    OGS_FATAL always throws std::runtime_error. The abort path was never
    enabled in CI, presets, or .gitlab-ci.yml. Simplifies Error.h by
    removing conditional compilation and unused <cstdlib> include.

--- BaseLib/Error.h
+++ BaseLib/Error.h
@@ -3,21 +3,10 @@
 
 #pragma once
 
-#include <cstdlib>
+#include <stdexcept>
 
 #include "Logging.h"
 
-#ifdef OGS_FATAL_ABORT
-#define OGS_FATAL(...)                                                  \
-    {                                                                   \
-        auto const formatted_va_args = fmt::format(__VA_ARGS__);        \
-        BaseLib::console->critical("{}:{} {}() {}", __FILE__, __LINE__, \
-                                   __FUNCTION__, formatted_va_args);    \
-        BaseLib::console->flush();                                      \
-        std::abort();                                                   \
-    }
-#else  // OGS_FATAL_ABORT
-#include <stdexcept>
 #define OGS_FATAL(...)                                                  \
     {                                                                   \
         auto const formatted_va_args = fmt::format(__VA_ARGS__);        \
@@ -26,4 +15,3 @@
         BaseLib::console->flush();                                      \
         throw std::runtime_error(formatted_va_args);                    \
     }
-#endif  // OGS_FATAL_ABORT
