eadda2a6f20d3dc026c5c37c9ec1a0580a152fbf
Lars Bilke <lars.bilke@ufz.de>
2026-08-28 13:15:51 +0200

    [web] Placeholder feminibenchmarks.

---
+++ Tests/Data/TH2M/feminibenchmarks/FE_Mini_Benchmarks.py
@@ -0,0 +1,42 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.1
+#   kernelspec:
+#     display_name: python_3.11
+#     language: python
+#     name: python3
+# ---
+
+# %% [raw]
+# +++
+# title = "FE mini benchmarks"
+# date = "2026-08-28"
+# author = "Jörg Buchwald"
+# web_subsection = "th2m"
+# models = ["url"]
+# +++
+
+
+# %% [markdown]
+"""
+<div class="note">
+
+### Note: Placeholder benchmark
+
+This notebook documents the planned OpenGeoSys representation of the FE mini benchmarks. The original project files and reference results
+will be added when supplied by the authors. It intentionally does not run an
+OGS simulation yet.
+
+</div>
+
+
+## Reference
+
+{{< bib "buchwald2025" >}}
+
+"""
